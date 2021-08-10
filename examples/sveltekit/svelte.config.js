198dc99ac12f17d484640d787a01277fa0744266
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2021-08-11 00:40:58 +0200

    dev: .

--- examples/sveltekit/svelte.config.js
+++ examples/sveltekit/svelte.config.js
@@ -1,8 +1,12 @@
 /** @type {import('@sveltejs/kit').Config} */
+
+import vercel from '@sveltejs/adapter-vercel';
+
 const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		target: '#svelte',
+		adapter: vercel()
 	}
 };
 
