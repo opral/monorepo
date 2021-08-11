230afd3eebce858f2131cb1de926d72cd2e0b667
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2021-08-11 21:07:48 +0200

    dev: initial nextjs example

---
+++ examples/nextjs/next.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  reactStrictMode: true,
+  i18n: {
+    locales: ["en", "fr", "de", "es", "pl"],
+    defaultLocale: "en"
+  }
+}
---
+++ examples/nextjs/pages/_app.js
@@ -0,0 +1,10 @@
+import '../styles/globals.css'
+
+function MyApp({ Component, pageProps }) {
+  return (<>
+  <Component {...pageProps}/>
+  </>
+  )
+}
+
+export default MyApp
