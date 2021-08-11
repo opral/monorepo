fc3d9c68e11fa2c7142dfdf7bd97d422aa1dec22
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2021-08-11 17:44:58 +0200

    chore: i18n routing scratchpad

---
+++ examples/sveltekit/src/lib/store.js
@@ -0,0 +1,14 @@
+import { writable, get } from 'svelte/store';
+
+// The i18n store serves as second possbility 
+// to achieve i18n ROUTING. But tests where not successfull. 
+function createI18nStore(){
+    const { subscribe, set } = writable("");
+    return {
+        subscribe,
+        route: (href) => `/${get(i18n)}` + href,
+        setLanguage: (lang) => set(lang)
+    }
+}
+
+export const i18n = createI18nStore()
