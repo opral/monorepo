f54781ad5c8166292b2a6d81454fe498f02860fb
No Chr <s184186@student.dtu.dk>
2021-09-25 11:01:56 +0200

    feat: add navbar

--- packages/dashboard/src/routes/navbar.svelte
+++ packages/dashboard/src/routes/navbar.svelte
@@ -1,42 +1,66 @@
 <script lang="ts">
-    import User24 from "carbon-icons-svelte/lib/User24";
-    import {
-      Header,
-      HeaderNav,
-      HeaderNavItem,
-      HeaderGlobalAction,
-      HeaderNavMenu,
-      SkipToContent,
-      Content,
-      Grid,
-      Row,
-      Column,
-HeaderUtilities,
-    } from "carbon-components-svelte";
-  
-    let isSideNavOpen = false;
-  </script>
-  
-  <Header company="inlang" platformName=" Svelte" bind:isSideNavOpen>
-    <div slot="skip-to-content">
-      <SkipToContent />
-    </div>
-  
-    <HeaderNav>
-      <HeaderNavItem href="/" text="Projects" />
-      <HeaderNavItem href="/" text="Organizations" />
-    </HeaderNav>
-    <HeaderUtilities>
-      <HeaderGlobalAction aria-label="My profile" icon={User24}/>
-    </HeaderUtilities> 
-  </Header>
-  
-  <Content>
-    <Grid>
-      <Row>
-        <Column>
-          <h1>Welcome</h1>
-        </Column>
-      </Row>
-    </Grid>
-  </Content>
+	import './overflowmenu.css';
+	import UserAvatar32 from 'carbon-icons-svelte/lib/UserAvatar32';
+	import User24 from 'carbon-icons-svelte/lib/User24';
+	import Password24 from 'carbon-icons-svelte/lib/Password24';
+	import Logout24 from 'carbon-icons-svelte/lib/Logout24';
+	import {
+		Header,
+		HeaderNav,
+		HeaderNavItem,
+		HeaderGlobalAction,
+		OverflowMenu,
+		OverflowMenuItem,
+		SkipToContent,
+		Content,
+		Grid,
+		Row,
+		Column,
+		HeaderUtilities
+	} from 'carbon-components-svelte';
+
+	let isSideNavOpen = false;
+</script>
+
+<Header company="inlang" platformName=" Svelte" bind:isSideNavOpen>
+	<div slot="skip-to-content">
+		<SkipToContent />
+	</div>
+
+	<HeaderNav>
+		<HeaderNavItem href="/" text="Projects" />
+		<HeaderNavItem href="/" text="Organizations" />
+	</HeaderNav>
+	<HeaderUtilities>
+		<OverflowMenu icon={UserAvatar32} flipped class="w-48">
+			<OverflowMenuItem>
+				<div class="flex flex-row">
+					<User24 class="mr-2 pr-1"/>
+					<p class="mr-1"> Account</p>
+        </div>
+			</OverflowMenuItem>
+			<OverflowMenuItem>
+				<div class="flex flex-row">
+					<Password24 class="mr-2 pr-1"/>
+					<p class="mr-1"> Access tokens</p>
+        </div>
+			</OverflowMenuItem>
+			<OverflowMenuItem>
+				<div class="flex flex-row">
+					<Logout24 class="mr-2 pr-1" />
+					<p class="mr-1">Log out</p>
+        </div>
+			</OverflowMenuItem>
+		</OverflowMenu>
+	</HeaderUtilities>
+</Header>
+
+<Content>
+	<Grid>
+		<Row>
+			<Column>
+				<h1>Welcome</h1>
+			</Column>
+		</Row>
+	</Grid>
+</Content>
