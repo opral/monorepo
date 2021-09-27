e7cb51fc23c8fae922e2b416561abded680214b4
Tobias Rydberg <zalamar@protonmail.com>
2021-09-27 18:13:05 +0200

    Remove option for machine translations in card, now it just always translates. Looks nicer too

--- packages/dashboard/src/lib/components/modals/TranslationModal.svelte
+++ packages/dashboard/src/lib/components/modals/TranslationModal.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
     // TODO: Implement actual machine translation
 
-    import {Button, ButtonSet, Modal, Tab, TabContent, Tabs, TextArea} from "carbon-components-svelte";
+    import {Button, Modal, Tab, TabContent, Tabs, TextArea, Tooltip} from "carbon-components-svelte";
 
-    export let isOpen = false;
+    export let open = false;
     export let isMachineTranslation = true; // This should be local settings
     // Default values for understanding, will be removed once implemented
     export let original = "Settings";
@@ -11,34 +11,54 @@
     export let hasNewTranslations = false; // Will be true when user submits new translations
 
     let originalTranslations = {...translations};
+    let selected = 0;
+
+    function nextTab() {
+        if (selected < Object.keys(originalTranslations).length-1) {
+            selected++;
+        } else {
+            hasNewTranslations = true
+            open = false;
+        }
+    }
 </script>
 
 <Modal
-        bind:open={isOpen}
+    bind:open
     modalHeading="Translation Card"
     size="sm"
-    hasform
     primaryButtonText="Confirm translations"
     secondaryButtonText="Cancel"
-    on:click:button--secondary={() => (isOpen = false)}
-    on:submit={() => {isOpen = false; hasNewTranslations = true}}
+    on:click:button--secondary={() => (open = false)}
+    on:submit={() => {open = false; hasNewTranslations = true}}
     preventCloseOnClickOutside
+    hasScrollingContent
+    shouldSubmitOnEnter={false}
 >
-        <Tabs>
+    <Tabs bind:selected>
+        {#each Object.keys(originalTranslations) as lang}
+            <Tab>{lang}</Tab>
+        {/each}
+        <div slot="content">
             {#each Object.keys(originalTranslations) as lang}
-                <Tab>{lang}</Tab>
-            {/each}
-            <div slot="content">
-                {#each Object.keys(originalTranslations) as lang}
                 <TabContent>
-                    <TextArea rows={10} labelText="Translated text:" bind:value={translations[lang]}/>
-                    <ButtonSet>
-                        <Button kind="secondary" on:click={() => translations[lang] = originalTranslations[lang]}>Reset</Button>
-                        <Button disabled={isMachineTranslation === false} on:click={() => translations[lang] += " but cooler"}>Machine Translation</Button>
-                    </ButtonSet>
-                    <TextArea rows={10} labelText="Original text:" disabled placeholder={original}/>
+                    <div class="flex items-center">
+                        <TextArea labelText="Translated text:" bind:value={translations[lang]}/>
+                        <Tooltip direction="left">
+                            <p>Reset: Returns text to original translation</p>
+                            <p>Reject: Deletes translation</p>
+                            <p>Approve: Continue to next translation</p>
+                        </Tooltip>
+                    </div>
+                    <div class="flex justify-center">
+                        <div><Button size="field" kind="secondary" on:click={() => translations[lang] = originalTranslations[lang]}>Reset</Button></div>
+                        <div><Button size="field" kind="danger" on:click={() => translations[lang] = ""}>Reject</Button></div>
+                        <div><Button size="field" disabled={isMachineTranslation === false} on:click={() => nextTab(selected)}>Approve</Button></div>
+                    </div>
+                    <p class="bx--label">Original Text:</p>
+                    <p class="text-sm">{original}</p>
                 </TabContent>
-                {/each}
-            </div>
-        </Tabs>
+            {/each}
+        </div>
+    </Tabs>
 </Modal>
