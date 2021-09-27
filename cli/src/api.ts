d4ecb9be77f5a67ddc63dd6145f8d954f4755071
doofin <8177dph@gmail.com>
2021-09-27 20:28:43 +0200

    add supabase

---
+++ cli/src/api.ts
@@ -0,0 +1,4 @@
+import { createClient } from '@supabase/supabase-js'
+
+// Create a single supabase client for interacting with your database
+const supabase = createClient('https://xyzcompany.supabase.co', 'public-anon-key')
