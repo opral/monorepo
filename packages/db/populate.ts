fccb83fc5a7b61f52149b0300818ca09c9f5b514
s184209 <s184209@student.dtu.dk>
2021-09-28 10:58:01 +0200

    set up skeleton for population

--- packages/db/populate.ts
+++ packages/db/populate.ts
@@ -1,2 +1,35 @@
 import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
+
+async function main() {
+    const createManyOrganizations = await prisma.organization.createMany({
+        data: [
+
+        ]
+    })
+    const createManyUsers = await prisma.user.createMany({
+        data: [
+            
+        ]
+    })
+    const createManyProjects = await prisma.project.createMany({
+        data: [
+
+        ]
+    })
+    const createManyPermissions = await prisma.permission.createMany({
+        data: [
+
+        ]
+    })
+    const createManyKeys = await prisma.key.createMany({
+        data: [
+
+        ]
+    })
+    const createManyTranslations = await prisma.translation.createMany({
+        data: [
+            
+        ]
+    })
+}
