563b128fe138be6b72a10925fbb98dd2cf329170
doofin <8177dph@gmail.com>
2021-09-30 15:47:27 +0200

    add login interface

--- cli/src/api.ts
+++ cli/src/api.ts
@@ -1,4 +1,57 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient, SupabaseClient } from '@supabase/supabase-js'
 
-// Create a single supabase client for interacting with your database
-const supabase = createClient('https://xyzcompany.supabase.co', 'public-anon-key')
+export function testReadProjs() {
+    let sb = newSupabase()
+    let r = sb.from("projects").select("*") // works with policy
+    r.then(x => console.log(x))
+
+}
+
+export async function test_getJson() {
+    let sb = newSupabase()
+    let u = await login(sb)
+    // u.then(x => getData(sb, x.id))
+    let jsonStr = getData(sb, u.id)
+    return jsonStr
+}
+
+/** http apis  */
+function newSupabase() {
+
+    // Create a single supabase client for interacting with your database
+    let sb = createClient(
+        'https://jxaqemnoabezizetynth.supabase.co',
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzMTcxNDM1NywiZXhwIjoxOTQ3MjkwMzU3fQ.aKT6sg-vJjnCUNitwy9HrIpl1rVAaP9NWIc9K7WFu6w')
+    // login(sb)
+    return sb
+}
+
+// https://supabase.io/docs/reference/javascript/select
+async function getData(sb: SupabaseClient, loggedInUserId: string) {
+    // const rr = sb.from("where userid=1").select("json")
+    // userid => permissions=>projects=>id of translations
+    // const r = await sb.from('cities').select()
+    // const loggedInUserId = 'd0714948'
+    let { data, error } = await sb.from('users').select('user_id, name').eq('user_id', loggedInUserId)
+    return data
+}
+
+
+
+
+// todo : how to auth?
+async function login(sb: SupabaseClient) {
+    let { user, session, error } = await sb.auth.signIn({
+        email: 'example@email.com',
+        password: 'example-password',
+    })
+    // user.id
+    // sb.auth.api.getUserByCookie("")
+    // user.
+    return user
+}
+
+/* from db table to json */
+function table2json() {
+
+}
--- cli/src/bin.ts
+++ cli/src/bin.ts
@@ -2,22 +2,23 @@
 
 // https://github.com/khalidx/typescript-cli-starter
 import program from 'commander'
+import { testReadProjs } from './api'
 
 import { orderPizza } from './index'
 
+/** binary entry  */
 
-program
-  .version('0.1.0')
-  .option('-p, --peppers', 'Add peppers')
-  .option('-P, --pineapple', 'Add pineapple')
-  .option('-b, --bbq-sauce', 'Add bbq sauce')
-  .option('-c, --cheese [type]', 'Add the specified type of cheese [marble]', 'marble')
-  .parse(process.argv)
+function entry() {
+  program
+    .version('0.1.0')
+    .option('-p, --peppers', 'Add peppers')
+    .option('-P, --pineapple', 'Add pineapple')
+    .option('-b, --bbq-sauce', 'Add bbq sauce')
+    .option('-c, --cheese [type]', 'Add the specified type of cheese [marble]', 'marble')
+    .parse(process.argv)
 
 
-orderPizza({
-  peppers: program.peppers,
-  pineapple: program.pineapple,
-  bbqSauce: program.bbqSauce,
-  cheeseType: program.cheese
-}).then(result => console.log(result.message))
+  testReadProjs()
+}
+
+entry()
