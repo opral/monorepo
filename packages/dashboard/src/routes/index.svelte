2ebfb76a25f71737383696e7d57356c2cf9d8189
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2021-10-01 19:56:19 +0200

    dev: add redirect to project

--- packages/dashboard/src/routes/index.svelte
+++ packages/dashboard/src/routes/index.svelte
@@ -1,1 +1,9 @@
-<p>TODO</p>
+<!-- redirecting the user to the projects -->
+<script lang="ts" context="module">
+	export async function load() {
+		return {
+			status: 302,
+			redirect: '/project'
+		};
+	}
+</script>
