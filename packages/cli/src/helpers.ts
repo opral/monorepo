49bb7c3b6130fb4101dd1b12b4ddd6d385dcb716
doofin <8177dph@gmail.com>
2021-10-11 23:20:18 +0200

    fix : fixing db read stuff

--- packages/cli/src/helpers.ts
+++ packages/cli/src/helpers.ts
@@ -1,10 +1,23 @@
 import { PostgrestResponse } from "@supabase/supabase-js";
+import * as fs from "fs";
+import * as path from "path";
 
-export async function pgResp2val<t>(x: PostgrestResponse<t>): Promise<t[]> {
+export async function pgResp2val<t>(x: PostgrestResponse<t>, errname: string = ''): Promise<t[]> {
     if (x.error || x.data === null) {
+        if (errname != '') console.log(errname)
+        console.log(x.error)
         throw 'oops'
     } else {
         return x.data;
     }
 }
 
+
+export function ensureDirectoryExistence(filePath: string) {
+    var dirname = path.dirname(filePath);
+    if (fs.existsSync(dirname)) {
+        return true;
+    }
+    ensureDirectoryExistence(dirname);
+    fs.mkdirSync(dirname);
+}
