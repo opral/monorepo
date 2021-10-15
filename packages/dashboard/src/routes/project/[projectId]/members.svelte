2d7dac5508caa98c58c6263c624063de64f474b8
s184209 <s184209@student.dtu.dk>
2021-10-15 16:07:48 +0200

    Added migration and member invite init

---
+++ packages/dashboard/src/routes/project/[projectId]/members.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import { TextInput, Button } from "carbon-components-svelte";
+    import SendAlt24 from "carbon-icons-svelte/lib/SendAlt32";
+
+    async function inviteUser(email: string) {
+        
+    }
+
+</script>
+
+<div class="p-8">
+	<h1>Members</h1>
+    <p class="text-gray-600 mt-1 mb-3">Invite members to the organization of current project.</p>
+
+    
+    <row class="space-x-4 items-center">
+    <TextInput size="xl" placeholder="Enter email of user to invite"></TextInput>
+    <Button  icon={SendAlt24}>Invite</Button>
+    </row>
+</div>
