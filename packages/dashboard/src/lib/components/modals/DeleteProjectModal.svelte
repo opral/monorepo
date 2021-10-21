9d02aaa2395f5d095a17e2f2b0a4544963f5e24d
Zalamar <zalamar@protonmail.com>
2021-10-21 17:50:18 +0200

    Add deletion and rename of project

---
+++ packages/dashboard/src/lib/components/modals/DeleteProjectModal.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+	import { Modal } from 'carbon-components-svelte';
+	import { database } from '$lib/services/database';
+	import type { definitions } from '@inlang/database';
+	import { projectStore } from '$lib/stores/projectStore';
+	import { page } from '$app/stores';
+	import { goto } from '$app/navigation';
+
+	export function show(args: { project: definitions['project'] | undefined }): string {
+		if (args.project === undefined) {
+			return 'Project was not found';
+		}
+		project = args.project;
+		open = true;
+		return '';
+	}
+
+	export function hide(): void {
+		open = false;
+	}
+
+	let project: definitions['project'];
+	let open = false;
+
+	async function deleteProject() {
+		const deleteRequest = await database
+			.from<definitions['project']>('project')
+			.delete()
+			.eq('id', project.id);
+		if (deleteRequest.error) {
+			alert(deleteRequest.error.message);
+		}
+		projectStore.getData({ projectId: $page.params.projectId });
+		open = false;
+		goto('/');
+	}
+</script>
+
+<Modal
+	bind:open
+	danger
+	modalHeading={`Delete ${project?.name}`}
+	primaryButtonText="Delete"
+	secondaryButtonText="Cancel"
+	on:click:button--primary={deleteProject}
+	on:click:button--secondary={() => (open = false)}
+>
+	<p>Are you sure? This is a permanent action and cannot be undone.</p>
+</Modal>
--- packages/dashboard/src/routes/project/[projectId]/settings.svelte
+++ packages/dashboard/src/routes/project/[projectId]/settings.svelte
@@ -1,9 +1,44 @@
 <script lang="ts">
+	import type { definitions } from '@inlang/database';
+	import { database } from '$lib/services/database';
 	import { projectStore } from '$lib/stores/projectStore';
-	import { CodeSnippet } from 'carbon-components-svelte';
+	import { CodeSnippet, TextInput, Button } from 'carbon-components-svelte';
+	import DeleteProjectModal from '$lib/components/modals/DeleteProjectModal.svelte';
+
+	let projectName = '';
+	let deleteProjectModal: DeleteProjectModal;
+
+	const renameProject = async () => {
+		const response = await database
+			.from<definitions['project']>('project')
+			.update({ name: projectName })
+			.eq('id', $projectStore?.data?.project.id ?? '');
+		if (response.error) {
+			alert(response.error);
+		} else {
+			alert('Project name has been changed to ' + projectName);
+		}
+	};
+
+	const deleteProject = () => {
+		deleteProjectModal.show({ project: $projectStore.data?.project });
+	};
 </script>
 
-<row class="space-x-4 items-center">
+<div class="mr-64 space-y-4">
 	<p>Project ID:</p>
 	<CodeSnippet code={$projectStore.data?.project.id} />
-</row>
+
+	<p>Rename project:</p>
+	<row class="items-center">
+		<TextInput placeholder="Project Name" bind:value={projectName} />
+		<Button kind="tertiary" on:click={() => renameProject()}>Rename</Button>
+	</row>
+
+	<row class="flex items-center space-x-4">
+		<p>Delete project:</p>
+		<Button kind="danger" on:click={() => deleteProject()}>Delete</Button>
+	</row>
+</div>
+
+<DeleteProjectModal bind:this={deleteProjectModal} />
