c54aba99fad615898d054130d3b476eb6e8c7ca3
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2021-10-21 18:24:47 +0200

    chore: on broken links warn

--- docs-website/docusaurus.config.js
+++ docs-website/docusaurus.config.js
@@ -10,7 +10,7 @@ const config = {
   tagline: "Localization solution for apps.",
   url: "https://docs.inlang.dev",
   baseUrl: "/",
-  onBrokenLinks: "throw",
+  onBrokenLinks: "warn",
   onBrokenMarkdownLinks: "warn",
   favicon: "img/favicon.ico",
   organizationName: "inlang", // Usually your GitHub org/user name.
