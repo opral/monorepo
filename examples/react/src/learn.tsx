774bbe577211d31a8cf038028dfcbb89522a7145
Zalamar <zalamar@protonmail.com>
2021-10-25 16:31:18 +0200

    Remove child function from react example

--- examples/react/src/learn.tsx
+++
@@ -1,9 +0,0 @@
-import {useContext} from 'react'
-import { I18nContext } from "./i18n/i18n-react"
-
-function Learn() {
-    const {LL} = useContext(I18nContext);
-    return <p>{LL.learn()}</p>
-}
-
-export default Learn
