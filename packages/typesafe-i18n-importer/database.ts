926c6e7030a5c021440672504d84cf7ca1d59d48
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2021-10-25 12:11:01 +0200

    Samuelstroschein/inl-139-refactor-typesafe-importer (#50)
    * refactor: structure of package
    
    * refactor: run main()
    
    * Add bin and fix lib
    
    * Make importer work as proper node server, only bug being from typesafe-i18n
    
    * Change relative import of inlang config to project root
    
    * chore: bump typesafe-i18n version
    
    * Remove sync from main in importer
    
    * Add message to show importer is running
    
    * Catch error in importer loop
    
    * Add error checking when wrong projectId
    
    * fix: bump typesafe-i18n version
    
    * chore: ignore package lock
    
    Co-authored-by: Zalamar <zalamar@protonmail.com>
    Co-authored-by: Tobias Rydberg <15159648+Zalamar@users.noreply.github.com>

--- packages/typesafe-i18n-importer/database.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = "https://fyhpvrhxlylivpmmjjby.supabase.co";
-const supabaseAnonKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNDU1NTI5MCwiZXhwIjoxOTUwMTMxMjkwfQ.DG40_Sz-62Gae-0gYj7gp9VT-eTXdQ-79MtYGwyYWiA";
-const supabase = createClient(supabaseUrl, supabaseAnonKey);
-
-export const database = supabase;
