88d41569e62dae93cc801e5b96ec96ea3b4b7c6c
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2021-10-27 00:07:46 +0200

    Revert "test: fix: expect 401 unauthorized"
    This reverts commit 04c163136abb389734d22b94b7072ccc7f33b2ca.

--- packages/database/test/policies/rls.test.ts
+++ packages/database/test/policies/rls.test.ts
@@ -4,40 +4,30 @@ import { definitions } from "../../types/definitions";
 describe("row level security is activated", () => {
   test("on user table", async () => {
     const users = await supabase.from<definitions["user"]>("user").select();
-    expect(users.status).toEqual(401);
+    expect(users.data?.length).toEqual(0);
   });
   test("on organization table", async () => {
-    const organizations = await supabase
-      .from<definitions["organization"]>("organization")
-      .select();
-    expect(organizations.status).toEqual(401);
+    const organizations = await supabase.from<definitions["organization"]>("organization").select();
+    expect(organizations.data?.length).toEqual(0);
   });
   test("on project table", async () => {
-    const projects = await supabase
-      .from<definitions["project"]>("project")
-      .select();
-    expect(projects.status).toEqual(401);
+    const projects = await supabase.from<definitions["project"]>("project").select();
+    expect(projects.data?.length).toEqual(0);
   });
   test("on language table", async () => {
-    const languages = await supabase
-      .from<definitions["language"]>("language")
-      .select();
-    expect(languages.status).toEqual(401);
+    const languages = await supabase.from<definitions["language"]>("language").select();
+    expect(languages.data?.length).toEqual(0);
   });
   test("on key table", async () => {
     const keys = await supabase.from<definitions["key"]>("key").select();
-    expect(keys.status).toEqual(401);
+    expect(keys.data?.length).toEqual(0);
   });
-  test("on translation table", async () => {
-    const translations = await supabase
-      .from<definitions["translation"]>("translation")
-      .select();
-    expect(translations.status).toEqual(401);
-  });
+  /*test("on translation table", async () => {
+    const translations = await supabase.from<definitions["translation"]>("translation").select();
+    expect(translations.data?.length).toEqual(0);
+  });*/
   test("on member table", async () => {
-    const members = await supabase
-      .from<definitions["member"]>("member")
-      .select();
-    expect(members.status).toEqual(401);
+    const members = await supabase.from<definitions["member"]>("member").select();
+    expect(members.data?.length).toEqual(0);
   });
 });
