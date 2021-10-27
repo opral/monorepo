0cc13328e67e0cbe2fd61e821146eb39a57141d7
Zalamar <zalamar@protonmail.com>
2021-10-27 12:13:34 +0200

    Show base translation on top of dashboard and make it green

--- packages/dashboard/src/lib/components/Translations.svelte
+++ packages/dashboard/src/lib/components/Translations.svelte
@@ -21,27 +21,36 @@
 		return difference(
 			$projectStore.data?.languages.map((l) => l.iso_code) ?? [],
 			translationsForKey.map((t) => t.iso_code)
-		).map((iso) => {
-			return <definitions['translation']>{
-				iso_code: iso,
-				is_reviewed: false,
-				key_name: keyName,
-				project_id: $projectStore.data?.project.id,
-				text: ''
-			};
-		});
+		)
+			.map((iso) => {
+				return <definitions['translation']>{
+					iso_code: iso,
+					is_reviewed: false,
+					key_name: keyName,
+					project_id: $projectStore.data?.project.id,
+					text: ''
+				};
+			})
+			.concat(translationsForKey);
 	};
 
 	$: rows = () => {
 		// TODO: sort the organizations alphabetically!
-		return (
-			concat(translationsForKey, missingIsoCodeRows()).sort((a, b) =>
-				ISO6391.getName(a.iso_code).localeCompare(ISO6391.getName(b.iso_code))
-			) ?? []
-		);
+		return missingIsoCodeRows().sort((a, b) => {
+			if ($projectStore.data?.project.default_iso_code === a.iso_code ?? []) {
+				return -1;
+			} else if ($projectStore.data?.project.default_iso_code === b.iso_code ?? []) {
+				return 1;
+			} else {
+				return ISO6391.getName(a.iso_code).localeCompare(ISO6391.getName(b.iso_code));
+			}
+		});
 	};
 </script>
 
 {#each rows() as row}
-	<TranslationRow translation={row} />
+	<TranslationRow
+		translation={row}
+		isBaseTranslation={row.iso_code === $projectStore.data?.project.default_iso_code}
+	/>
 {/each}
