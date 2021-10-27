2007d2972a427da4eeb33bbc4463c460019a15b7
Zalamar <zalamar@protonmail.com>
2021-10-27 14:35:33 +0200

    Change default language icon to checkmark

--- packages/dashboard/src/routes/project/[projectId]/languages.svelte
+++ packages/dashboard/src/routes/project/[projectId]/languages.svelte
@@ -11,7 +11,8 @@
 		ToolbarContent,
 		ToolbarSearch
 	} from 'carbon-components-svelte';
-	import Save16 from 'carbon-icons-svelte/lib/Save16';
+	import CheckmarkFilled16 from 'carbon-icons-svelte/lib/CheckmarkFilled16';
+	import CheckmarkOutline16 from 'carbon-icons-svelte/lib/CheckmarkFilled16';
 
 	import Delete16 from 'carbon-icons-svelte/lib/Delete16';
 	import CreateLanguageModal from '$lib/components/modals/CreateLanguageModal.svelte';
@@ -126,17 +127,20 @@
 				{#if row.isDefaultLanguage === false}
 					<Button
 						kind="ghost"
-						icon={Save16}
+						icon={CheckmarkFilled16}
 						iconDescription="Set as default language"
-						color="green"
 						on:click={() => {
 							setDefaultLanguageModal.language = row.languageObject;
 							setDefaultLanguageModal.show = true;
 						}}
 					/>
 				{:else}
-					<!-- ugly trick to align the delete button if the language is the default -->
-					<div class="w-12" />
+					<Button
+						kind="ghost"
+						icon={CheckmarkOutline16}
+						iconDescription="Set as default language"
+						disabled
+					/>
 				{/if}
 				<Button
 					kind="danger-ghost"
