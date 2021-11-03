5e998f07df860852f0e88da12f3ac333160c2653
Zalamar <zalamar@protonmail.com>
2021-11-03 16:26:56 +0100

    bugfix: make mobile usable

--- packages/dashboard/src/routes/__layout.svelte
+++ packages/dashboard/src/routes/__layout.svelte
@@ -11,10 +11,15 @@
 	import { page } from '$app/stores';
 	import Tracker from '@openreplay/tracker';
 	import { openreplayKey } from '$lib/services/replay';
+	import { InlineNotification } from 'carbon-components-svelte';
 
 	const tracker = new Tracker({ projectKey: openreplayKey });
 
 	export async function load({ page }: LoadInput): Promise<LoadOutput> {
+		if (tracker.isActive() === false) {
+			tracker.start();
+			tracker.setUserID(auth.user()?.email ?? 'Not logged in');
+		}
 		const user = auth.user();
 		// includes('auth') ensures that subroutes within /auth do not
 		// lead to constant redirects
@@ -33,11 +38,11 @@
 	// if running in browser (not server side)
 	// listen for auth changes
 
+	$: outerWidth = 0;
+
 	if (browser) {
 		auth.onAuthStateChange((event) => {
 			if (event === 'SIGNED_IN') {
-				tracker.start();
-				tracker.setUserID(auth.user()?.email ?? 'Unknown');
 				goto('/');
 			} else if (event === 'SIGNED_OUT') {
 				tracker.stop();
@@ -51,6 +56,16 @@
 	<title>Inlang | {capitalize(last($page.path.split('/')))}</title>
 </svelte:head>
 
+<svelte:window bind:outerWidth />
+
 <UiShell>
 	<slot />
 </UiShell>
+{#if outerWidth < 550}
+	<InlineNotification
+		kind="warning"
+		title="Notice:"
+		subtitle="Website is not optimized for mobile."
+		hideCloseButton={true}
+	/>
+{/if}
