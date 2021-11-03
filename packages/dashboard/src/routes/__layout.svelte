e3220f0ebcb5fd621d206245d3eb24746e41c4a7
Zalamar <zalamar@protonmail.com>
2021-11-03 12:21:40 +0100

    Add Openreplay

--- packages/dashboard/src/routes/__layout.svelte
+++ packages/dashboard/src/routes/__layout.svelte
@@ -9,6 +9,10 @@
 	import { goto } from '$app/navigation';
 	import UiShell from '$lib/layout/UiShell.svelte';
 	import { page } from '$app/stores';
+	import Tracker from '@openreplay/tracker';
+	import { openreplayKey } from '$lib/services/replay';
+
+	const tracker = new Tracker({ projectKey: openreplayKey });
 
 	export async function load({ page }: LoadInput): Promise<LoadOutput> {
 		const user = auth.user();
@@ -32,8 +36,11 @@
 	if (browser) {
 		auth.onAuthStateChange((event) => {
 			if (event === 'SIGNED_IN') {
+				tracker.start();
+				tracker.setUserID(auth.user()?.email ?? 'Unknown');
 				goto('/');
 			} else if (event === 'SIGNED_OUT') {
+				tracker.stop();
 				goto('/auth');
 			}
 		});
