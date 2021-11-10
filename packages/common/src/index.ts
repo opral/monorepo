2cad0bcc5e312534f01ee7ef82fc8b2bd85ae605
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2021-11-10 10:58:07 +0100

    chore: add eslint functional rules

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -4,8 +4,8 @@
  * @param name - The name to greet
  * @returns The greeting string
  */
-const greetings = (name = "world"): string => {
+function greetings(name = 'world'): string {
     return `Hello ${name}`;
-};
+}
 
 export default greetings;
