e8f96bd06efe7c539014ec0d821daf3cd9c808ce
Zalamar <zalamar@protonmail.com>
2021-11-15 10:37:27 +0100

    Add baseLocale to common tests

--- packages/common/tests/fluent.formatter.test.ts
+++ packages/common/tests/fluent.formatter.test.ts
@@ -4,7 +4,8 @@ import { TranslationAPI } from '../src/fluent/formatter';
 describe('constructor', () => {
     const translationAPI = new TranslationAPI({
         adapter: new FluentAdapter(),
-        locales: [{ language: 'en', data: 'test = this is my test' }],
+        locales: [{ languageCode: 'en', data: 'test = this is my test' }],
+        baseLocale: 'en',
     });
     it('should print out the test key', () => {
         expect(translationAPI.getTranslation('test', 'en')).toMatch('test = this is my test');
