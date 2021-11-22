4e83356600e82885797e3c1d1b61542bdb64b8ed
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2021-11-22 17:44:41 +0100

    imp: ui/ux of create project modal

--- packages/dashboard/src/lib/components/modals/CreateProjectModal.svelte
+++ packages/dashboard/src/lib/components/modals/CreateProjectModal.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import { LanguageCode } from '@inlang/common/src/types/languageCode';
 	import {
 		Modal,
 		Form,
@@ -7,7 +6,13 @@
 		TextInput,
 		MultiSelect,
 		Loading,
-		ComboBox
+		ComboBox,
+		ProgressIndicator,
+		ProgressStep,
+		RadioTile,
+		TileGroup,
+		InlineNotification,
+		NotificationActionButton
 	} from 'carbon-components-svelte';
 	import { database } from '$lib/services/database';
 	import { DatabaseResponse } from '$lib/types/databaseResponse';
@@ -15,14 +20,28 @@
 	import { onMount } from 'svelte';
 	import ISO6391 from 'iso-639-1';
 	import { createEventDispatcher } from 'svelte';
+	import { isEqual } from 'lodash';
 
 	export let open = false;
 
+	// 0 = basics
+	// 1 = default human language
+	let currentStep: 0 | 1 = 0;
+
+	$: if (currentStep === 0 || currentStep === 1) {
+		console.log({ currentStep });
+	}
+
 	let projectName = '';
+
+	let projectNameElement: HTMLInputElement;
 
 	let dispatch = createEventDispatcher();
 
 	let organizations: DatabaseResponse<definitions['organization'][]> = { data: null, error: null };
+
+	let projects: DatabaseResponse<definitions['project'][]> = { data: null, error: null };
+
 	let selectedLanguageIsoCodes: definitions['language']['iso_code'][] = [];
 	let confirmIsLoading = false;
 	// the selected index of the ComboBox
@@ -34,22 +53,39 @@
 		selectedOrganizationIndex === -1
 			? undefined
 			: organizations.data?.[selectedOrganizationIndex].id;
-	// the selected index of the ComboBox
-	let selectedDefaultLanguageIsoIndex = -1;
+
 	// the actual value (type definitions)
 	let selectedDefaultLanguageIso: definitions['language']['iso_code'] | undefined;
-	// which is reactively deducted
-	$: selectedDefaultLanguageIso =
-		selectedDefaultLanguageIsoIndex === -1
-			? undefined
-			: (allLanguagesWithCode()[selectedDefaultLanguageIsoIndex].id as LanguageCode);
 
 	$: isValidInput =
-		projectNameIsValidInput && organizationIdIsValidInput && selectedLanguageIsoCodes.length > 0;
+		projectNameIsValidInput &&
+		organizationIdIsValidInput &&
+		selectedLanguageIsoCodes.length > 0 &&
+		defaultLanguageIsValidInput;
+
+	$: defaultLanguageIsValidInput = selectedDefaultLanguageIso
+		? ISO6391.getAllCodes().includes(selectedDefaultLanguageIso)
+		: false;
 
 	$: projectNameIsValidInput = projectName !== '';
 
-	$: organizationIdIsValidInput = organizationId !== null || organizationId !== '';
+	$: organizationIdIsValidInput =
+		organizations.data?.map((organization) => organization.id).includes(organizationId ?? '') ??
+		false;
+
+	$: currentStepIsValid = () => {
+		if (currentStep === 0) {
+			return (
+				projectNameIsValidInput &&
+				organizationIdIsValidInput &&
+				selectedLanguageIsoCodes.length > 0 &&
+				projectAlreadyExistsForOrganization() === false
+			);
+		} else if (currentStep === 1) {
+			return defaultLanguageIsValidInput;
+		}
+		return false;
+	};
 
 	// helper function
 	function allLanguagesWithCode() {
@@ -59,15 +95,35 @@
 		}));
 	}
 
+	$: projectAlreadyExistsForOrganization = () => {
+		if (organizations.data && projects.data && selectedOrganizationIndex !== -1 && projectName) {
+			return projects.data
+				?.map((project) => ({ organizationId: project.organization_id, name: project.name }))
+				.some((project) =>
+					isEqual(project, {
+						organizationId: organizations.data?.[selectedOrganizationIndex].id,
+						name: projectName
+					})
+				);
+		}
+		return false;
+	};
+
 	// load the projects of the selected organization
 	onMount(async () => {
 		organizations = await database.from<definitions['organization']>('organization').select();
+		projects = await database.from<definitions['project']>('project').select();
 		if (organizations.error) {
 			alert(organizations.error.message);
+			console.error(organizations.error);
+		}
+		if (projects.error) {
+			alert(projects.error.message);
+			console.error(projects.error);
 		}
 	});
 
-	async function handleConfirm() {
+	async function handleCreateProject() {
 		confirmIsLoading = true;
 		if (organizationId === undefined || organizationIdIsValidInput === false) {
 			alert('The chosen organization is not valid.');
@@ -107,17 +163,22 @@
 		confirmIsLoading = false;
 		open = false;
 	}
+
+	onMount(() => {
+		projectNameElement.focus();
+	});
 </script>
 
 <Modal
 	bind:open
 	modalHeading="New project"
-	primaryButtonText="Create"
-	primaryButtonDisabled={isValidInput === false}
+	primaryButtonText={currentStep === 0 ? 'Next' : 'Create'}
+	primaryButtonDisabled={currentStepIsValid() === false && isValidInput === false}
 	hasForm={true}
-	secondaryButtonText="Cancel"
-	on:click:button--primary={handleConfirm}
-	on:click:button--secondary={() => (open = false)}
+	hasScrollingContent={selectedLanguageIsoCodes.length > 5}
+	secondaryButtonText={currentStep === 0 ? 'Cancel' : 'Back'}
+	on:click:button--primary={currentStep === 0 ? () => (currentStep += 1) : handleCreateProject}
+	on:click:button--secondary={currentStep === 0 ? () => (open = false) : () => (currentStep -= 1)}
 	on:open
 	on:close
 	on:submit
@@ -127,64 +188,108 @@
 	{/if}
 	<Form>
 		<FormGroup>
-			<TextInput
-				labelText="Project name"
-				placeholder="What's the name of the project?"
-				bind:value={projectName}
-			/>
+			<ProgressIndicator spaceEqually>
+				<ProgressStep
+					current={currentStep === 0}
+					complete={projectNameIsValidInput}
+					invalid={currentStep > 0 &&
+						(projectNameIsValidInput &&
+							organizationIdIsValidInput &&
+							selectedLanguageIsoCodes.length > 0) === false &&
+						projectAlreadyExistsForOrganization() === false}
+					label="Basics"
+					description="The progress indicator will listen for clicks on the steps"
+					on:click={() => (currentStep = 0)}
+				/>
+				<ProgressStep
+					current={currentStep === 1}
+					label="Default Human Language"
+					description="The progress indicator will listen for clicks on the steps"
+					on:click={() => (currentStep = 1)}
+				/>
+			</ProgressIndicator>
 		</FormGroup>
-
-		<FormGroup>
-			<ComboBox
-				titleText="Organization"
-				placeholder="To which organization does the project belong to?"
-				bind:selectedIndex={selectedOrganizationIndex}
-				items={organizations.data?.map((organization) => ({
-					id: organization.id,
-					text: organization.name
-				}))}
-				shouldFilterItem={(item, value) => {
-					if (!value) return true;
-					return item.text.toLowerCase().includes(value.toLowerCase());
-				}}
-			/>
-		</FormGroup>
-		<FormGroup>
-			<ComboBox
-				titleText="Default human language"
-				placeholder="Which human language (English, German ...) is used during development?"
-				direction="top"
-				bind:selectedIndex={selectedDefaultLanguageIsoIndex}
-				items={allLanguagesWithCode()}
-				shouldFilterItem={(item, value) => {
-					if (!value) return true;
-					return item.text.toLowerCase().includes(value.toLowerCase());
-				}}
-			/>
-		</FormGroup>
-		<FormGroup>
-			<MultiSelect
-				bind:selectedIds={selectedLanguageIsoCodes}
-				direction="top"
-				titleText="Translated human languages"
-				placeholder="Which additional human languages should your app support?"
-				filterable
-				items={allLanguagesWithCode().filter(
-					(language) => language.id !== selectedDefaultLanguageIso
-				)}
-			/>
-			{#if selectedLanguageIsoCodes.length > 0}
-				<p class="pt-1">
-					Translated languages:
-					{#each selectedLanguageIsoCodes as isoCode, i}
-						{#if i + 1 !== selectedLanguageIsoCodes.length}
-							{ISO6391.getName(isoCode) + ', '}
-						{:else}
-							{ISO6391.getName(isoCode)}
-						{/if}
-					{/each}
+		{#if currentStep === 0}
+			<FormGroup>
+				<TextInput
+					labelText="Project name"
+					bind:value={projectName}
+					invalid={projectAlreadyExistsForOrganization()}
+					invalidText={`${projectName} already exists for the organization.`}
+					placeholder="What is the name of the project?"
+					bind:ref={projectNameElement}
+				/>
+			</FormGroup>
+			<FormGroup>
+				<ComboBox
+					titleText="Organization"
+					placeholder="To which organization does the project belong to?"
+					bind:selectedIndex={selectedOrganizationIndex}
+					items={organizations.data?.map((organization) => ({
+						id: organization.id,
+						text: organization.name
+					}))}
+					shouldFilterItem={(item, value) => {
+						if (!value) return true;
+						return item.text.toLowerCase().includes(value.toLowerCase());
+					}}
+				/>
+			</FormGroup>
+			<FormGroup>
+				<MultiSelect
+					bind:selectedIds={selectedLanguageIsoCodes}
+					direction="top"
+					titleText="Human languages"
+					placeholder={selectedLanguageIsoCodes.length === 0
+						? 'In which languages (English, German ...) do you want to offer your software?'
+						: ''}
+					filterable
+					items={allLanguagesWithCode()}
+				/>
+				{#if selectedLanguageIsoCodes.length > 0}
+					<p class="pt-1">
+						Languages:
+						{#each selectedLanguageIsoCodes as isoCode, i}
+							{#if i + 1 !== selectedLanguageIsoCodes.length}
+								{ISO6391.getName(isoCode) + ', '}
+							{:else}
+								{ISO6391.getName(isoCode)}
+							{/if}
+						{/each}
+					</p>
+				{/if}
+			</FormGroup>
+		{:else if currentStep === 1}
+			<FormGroup>
+				<TileGroup
+					bind:selected={selectedDefaultLanguageIso}
+					legend="Select default human language"
+				>
+					{#if selectedLanguageIsoCodes.length === 0}
+						<InlineNotification
+							kind="warning"
+							title="You need to select at least one human language for the project."
+							hideCloseButton
+						>
+							<div slot="actions">
+								<NotificationActionButton on:click={() => (currentStep -= 1)}>
+									Go back
+								</NotificationActionButton>
+							</div>
+						</InlineNotification>
+					{:else}
+						{#each selectedLanguageIsoCodes as languageCode}
+							<RadioTile light value={languageCode}>
+								{ISO6391.getName(languageCode)} - {languageCode}
+							</RadioTile>
+						{/each}
+					{/if}
+				</TileGroup>
+				<p class="pt-4">
+					The default human language is the language used during development. In most cases it's
+					English.
 				</p>
-			{/if}
-		</FormGroup>
+			</FormGroup>
+		{/if}
 	</Form>
 </Modal>
