6e8e099ff9a1d7ad1ec88a2a0286406876214d22
s184209 <s184209@student.dtu.dk>
2021-11-26 16:06:36 +0100

    chore: changed languages and project to export

--- packages/dashboard/src/routes/project/[projectId]/export.svelte
+++ packages/dashboard/src/routes/project/[projectId]/export.svelte
@@ -1,9 +1,22 @@
-<script>
-    import Adapter from '$lib/components/Adapter.svelte'
+<script lang=ts>
+    import Adapter from '$lib/components/Adapter.svelte';
+	import { projectStore } from '$lib/stores/projectStore';
+	import type { definitions } from '@inlang/database';
+	import { InlineNotification } from "carbon-components-svelte";
+
+	const project: definitions['project'] | undefined = $projectStore.data?.project;
+	const languages: definitions['language'][] = $projectStore.data?.languages ?? [];
 </script>
 
-<Adapter 
-    title="Export" 
+{#if languages.length === 0 || project === undefined}
+<InlineNotification
+	title="Error fetching data"
+/>
+{:else}
+<Adapter
+    title="Export"
     details = "Select adapter and human language to export"
-    >
-</Adapter>
+	project={project}
+	languages={languages}
+/>
+{/if}
--- packages/dashboard/src/routes/project/[projectId]/import.svelte
+++ packages/dashboard/src/routes/project/[projectId]/import.svelte
@@ -1,8 +1,22 @@
-<script>
+<script lang="ts">
 	import Adapter from '$lib/components/Adapter.svelte';
+	import { projectStore } from '$lib/stores/projectStore';
+	import type { definitions } from '@inlang/database';
+	import { InlineNotification } from "carbon-components-svelte";
+
+	const project: definitions['project'] | undefined = $projectStore.data?.project;
+	const languages: definitions['language'][] = $projectStore.data?.languages ?? [];
 </script>
 
+{#if languages.length === 0 || project === undefined}
+<InlineNotification
+	title="Error fetching data"
+/>
+{:else}
 <Adapter
 	title="Import"
 	details="Select adapter and human language then import your current translations"
+	project={project}
+	languages={languages}
 />
+{/if}
