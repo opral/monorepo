fcbeb1018987d35a1810e2ebed9807a0e87059d9
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2021-12-02 16:05:51 +0100

    init

--- packages/inlang-vscode-extension/src/extension.ts
+++ packages/inlang-vscode-extension/src/extension.ts
@@ -1,129 +1,27 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import vscode from 'vscode'
-import { applyWrappingPattern, readAndValidateConfig } from './helpers'
-import { InlangVsCodeExtensionConfig } from './types/inlangVsCodeExtensionConfig'
-import { snakeCase } from 'lodash'
-
-const inlangCmd = 'vscode-extension.send'
-const inlangCmdName = 'Inlang: Create key'
-/* *select text-> ask for namespace 
-	-> get default translation lang
-	-> write to db (create new translation with default lang)
-	-> replace selected text with namespace  */
-
-/**called once : // This line of code will only be executed once when your extension is activated  */
-function onCreate() {
-    new vscode.CodeAction(inlangCmdName).command = {
-        command: inlangCmd,
-        title: inlangCmdName,
-    }
-}
-
-/**will be executed every time your command is executed*/
-async function onCommand(config: InlangVsCodeExtensionConfig) {
-    if (config === undefined) {
-        const openedDirectoryPath =
-            vscode.workspace.workspaceFolders?.[0].uri.fsPath
-
-        if (openedDirectoryPath === undefined) {
-            vscode.window.showErrorMessage(
-                'You must open a directory in VSCode.'
-            )
-            throw 'You must open a directory in VSCode.'
-        }
-        const configValidated = readAndValidateConfig(
-            openedDirectoryPath + '/inlang.config.json'
-        )
-        if (configValidated.error) {
-            vscode.window.showErrorMessage(configValidated.error)
-            throw configValidated.error
-        } else if (configValidated.data === null) {
-            vscode.window.showErrorMessage('The config data was null.')
-            throw configValidated.error
-        }
-        config = configValidated.data
-    }
-    const activeTextEditor = vscode.window.activeTextEditor
-    const range = activeTextEditor?.selection
-    if (range !== null) {
-        const selectionText = activeTextEditor?.document.getText(range)
-        const keyName = await vscode.window.showInputBox({
-            prompt: 'your inlang key name',
-        })
-        if (keyName === undefined || keyName === '') {
-            vscode.window.showInformationMessage('Key name not set.')
-        } else if (selectionText === undefined) {
-            vscode.window.showErrorMessage(
-                keyName + ' no text has been selected.'
-            )
-        } else if (keyName !== snakeCase(keyName)) {
-            vscode.window.showErrorMessage(
-                'key names must be snake case e.g. my_cool_key'
-            )
-        } else {
-            // trimmed corrects formatting which can be corrupted due to template literal string
-            let trimmed = selectionText.replace(/(?:\n(?:\s*))+/g, ' ').trim()
-            if (trimmed.startsWith(`"`) || trimmed.endsWith(`'`)) {
-                trimmed = trimmed.slice(1, trimmed.length)
-            }
-            if (trimmed.endsWith(`"`) || trimmed.endsWith(`'`)) {
-                trimmed = trimmed.slice(0, trimmed.length - 1)
-            }
-        }
-    }
-}
-
-// this method is called when your extension is activated, your extension is activated the very first time the command is executed
+import * as vscode from 'vscode'
+import { Emojizer } from './actions/createKeyFromHighlightedText'
 
 export function activate(context: vscode.ExtensionContext) {
-    // https://github.com/microsoft/vscode-extension-samples
-    context.subscriptions.push(
-        vscode.languages.registerCodeActionsProvider(
-            'javascript',
-            new InlangCA(),
-            {
-                providedCodeActionKinds: InlangCA.providedCodeActionKinds,
-            }
+    const supportedLanguages = [
+        'javascript',
+        'typescript',
+        'javascriptreact',
+        'typescriptreact',
+        'svelte',
+    ]
+
+    for (const language of supportedLanguages) {
+        context.subscriptions.push(
+            vscode.languages.registerCodeActionsProvider(
+                language,
+                new Emojizer(),
+                {
+                    providedCodeActionKinds: Emojizer.providedCodeActionKinds,
+                }
+            )
         )
-    )
-
-    onCreate()
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with registerCommand
-    // The commandId parameter must match the command field in package.json
-
-    let config: InlangVsCodeExtensionConfig
-    const disposable = vscode.commands.registerCommand(inlangCmd, () => {
-        onCommand(config)
-    })
-
-    context.subscriptions.push(disposable)
+    }
 }
 
 // this method is called when your extension is deactivated
 // export function deactivate() {}
-
-export class InlangCA implements vscode.CodeActionProvider {
-    public static readonly providedCodeActionKinds = [
-        vscode.CodeActionKind.QuickFix,
-    ]
-
-    provideCodeActions(
-        document: vscode.TextDocument,
-        range: vscode.Range | vscode.Selection,
-        context: vscode.CodeActionContext,
-        token: vscode.CancellationToken
-    ): vscode.ProviderResult<(vscode.CodeAction | vscode.Command)[]> {
-        const ca1 = (new vscode.CodeAction(inlangCmdName).command = {
-            command: inlangCmd,
-            title: inlangCmdName,
-        })
-        return [ca1]
-    }
-    resolveCodeAction?(
-        codeAction: vscode.CodeAction,
-        token: vscode.CancellationToken
-    ): vscode.ProviderResult<vscode.CodeAction> {
-        throw new Error('Method not implemented.')
-    }
-}
