b831bcd8f5aa486f85f6cfc77ef7a5567bcea371
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2021-12-14 19:58:30 +0100

    init

--- packages/inlang-js/translations/translations.js
+++
@@ -1,6 +0,0 @@
-export default {
-    'About this app': { de: 'Über diese App' },
-    'You have {num} todos': { de: 'Du hast {num} Todos' },
-    'You have {num} {color} todos': { de: 'Du hast {num} {color} Todos' },
-    green: { de: 'grün' },
-}
