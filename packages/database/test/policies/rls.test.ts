096c7f73e2e0006d84efbafa6eb6d76cd0123f05
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2021-12-15 19:03:13 +0100

    formatting

--- packages/database/test/policies/rls.test.ts
+++ packages/database/test/policies/rls.test.ts
@@ -7,19 +7,27 @@ describe("row level security is activated", () => {
     expect(users.data?.length).toEqual(0);
   });
   test("on organization table", async () => {
-    const organizations = await supabase.from<definitions["organization"]>("organization").select();
+    const organizations = await supabase
+      .from<definitions["organization"]>("organization")
+      .select();
     expect(organizations.data?.length).toEqual(0);
   });
   test("on project table", async () => {
-    const projects = await supabase.from<definitions["project"]>("project").select();
+    const projects = await supabase
+      .from<definitions["project"]>("project")
+      .select();
     expect(projects.data?.length).toEqual(0);
   });
   test("on language table", async () => {
-    const languages = await supabase.from<definitions["language"]>("language").select();
+    const languages = await supabase
+      .from<definitions["language"]>("language")
+      .select();
     expect(languages.data?.length).toEqual(0);
   });
   test("on member table", async () => {
-    const members = await supabase.from<definitions["member"]>("member").select();
+    const members = await supabase
+      .from<definitions["member"]>("member")
+      .select();
     expect(members.data?.length).toEqual(0);
   });
 });
