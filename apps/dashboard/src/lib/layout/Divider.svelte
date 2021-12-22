2f63b8f684ff4504d15c02dd263aac73886a9135
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2021-12-22 18:27:23 +0100

    refactor: clean up

--- apps/dashboard/src/lib/layout/Divider.svelte
+++ apps/dashboard/src/lib/layout/Divider.svelte
@@ -7,5 +7,5 @@
 
 <!-- color corresponds to https://www.carbondesignsystem.com/guidelines/color/overview/ -->
 <div class={classes}>
-	<div class="w-full h-0.5" style="background-color: #e0e0e0" />
+	<div class="w-full h-0.5 my-1" style="background-color: #e0e0e0" />
 </div>
