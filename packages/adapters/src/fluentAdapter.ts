212dcf63e9d936412cfb10d0084bf3d32c77ff56
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2022-01-17 18:15:02 +0100

    refactor: fluent adapter parse junk

--- packages/adapters/src/fluentAdapter.ts
+++ packages/adapters/src/fluentAdapter.ts
@@ -5,9 +5,6 @@ import { parse, serialize, SingleResource } from '@inlang/fluent-syntax';
 export class FluentAdapter implements AdapterInterface {
     parse(data: string): Result<SingleResource, Error> {
         const parsedData = parse(data, { withSpans: false });
-        for (const entry of parsedData.body) {
-            if (entry.type === 'Junk') return Result.err(Error('Parsing error'));
-        }
         return Result.ok(parsedData);
     }
 
