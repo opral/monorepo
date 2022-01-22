de6cddfe26437215a961b075a8a327a223761630
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2022-01-22 16:18:49 +0100

    chore: init

---
+++ apps/cli3/README.md
@@ -0,0 +1,3 @@
+# @inlang/cli
+
+TODO
---
+++ apps/cli3/src/main.ts
@@ -0,0 +1,34 @@
+import { cli } from 'cleye';
+
+// Parse argv
+const argv = cli({
+    name: 'greet.js',
+
+    // Define parameters
+    parameters: [
+        '<first name>', // First name is required
+        '[last name]', // Last name is optional
+    ],
+
+    // Define flags/options
+    flags: {
+        // Parses `--time` as a string
+        time: {
+            type: String,
+            description: 'Time of day to greet (morning or evening)',
+            default: 'morning',
+        },
+    },
+});
+
+const name = [argv._.firstName, argv._.lastName].filter(Boolean).join(' ');
+
+// const adapter = adapters[flags.adapter as SupportedAdapter];
+// const x = Result.err();
+// console.log(x);
+
+if (argv.flags.time === 'morning') {
+    console.log(`Good morning ${name}!`);
+} else {
+    console.log(`Good evening ${name}!`);
+}
