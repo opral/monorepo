21ee046e912ecb5309f87576d28da5006247ee82
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2022-01-25 09:58:43 +0100

    chore: delete outdated examples

--- examples/react/craco.config.js
+++
@@ -1,9 +0,0 @@
-// https://tailwindcss.com/docs/guides/create-react-app
-// craco.config.js
-module.exports = {
-  style: {
-    postcss: {
-      plugins: [require("tailwindcss"), require("autoprefixer")],
-    },
-  },
-};
