ab7b693ecd9c1c3466a6b4cf27232753a879a2c1
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2022-01-27 12:17:48 +0100

    chore: try vercel deploy

--- apps/dashboard/svelte.config.js
+++ apps/dashboard/svelte.config.js
@@ -1,5 +1,5 @@
 import preprocess from 'svelte-preprocess';
-import netlify from '@sveltejs/adapter-netlify';
+import vercel from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -14,7 +14,7 @@ const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
-		adapter: netlify()
+		adapter: vercel()
 	}
 };
 
