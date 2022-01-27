f00b64a57a1787c2c7264d4d25134d6e7674d435
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2022-01-27 11:38:57 +0100

    Revert "chore: radical try deploy netlify"
    This reverts commit 25fe7cec574681feb25b123648b6e74315d76a78.

--- apps/dashboard/src/routes/api/hello.ts
+++
@@ -1,6 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-export async function get() {
-	return {
-		body: 'Hello World'
-	};
-}
---
+++ apps/dashboard/src/routes/api/upload.ts
@@ -0,0 +1,65 @@
+import type { EndpointOutput, RequestEvent } from '@sveltejs/kit';
+import * as dotenv from 'dotenv';
+import { createServerSideSupabaseClient } from './_utils/serverSideServices';
+import type { definitions } from '@inlang/database';
+import { SerializedResource } from '@inlang/adapters';
+
+/**
+ * This is a middleware api endpoint for the CLI.
+ *
+ * API Endpoint exists because supabase has no way to authorize api keys yet.
+ */
+
+type RequestBody = {
+	// yeah yeah don't put the api key in the body
+	// pssst you never saw that
+	apiKey: string;
+	files: SerializedResource[];
+};
+
+export async function post(event: RequestEvent): Promise<EndpointOutput> {
+	dotenv.config();
+	if (event.request.headers.get('content-type') !== 'application/json') {
+		return {
+			status: 405
+		};
+	}
+	const supabase = createServerSideSupabaseClient();
+	const requestBody = (await event.request.json()) as RequestBody;
+	const project = await supabase
+		.from<definitions['project']>('project')
+		.select()
+		.match({ api_key: requestBody.apiKey })
+		.single();
+	if (project.error) {
+		return {
+			status: 500,
+			body: 'Error retrieving the project.'
+		};
+	} else if (project.data === null) {
+		return {
+			status: 404,
+			body: "Couldn't find the project. Are you sure that the api key is correct?"
+		};
+	} else if (requestBody.files.length === 0) {
+		return {
+			status: 400,
+			body: 'No files to update have been provided.'
+		};
+	}
+	for (const file of requestBody.files) {
+		const languages = await supabase
+			.from<definitions['language']>('language')
+			.update({ file: file.data })
+			.match({ project_id: project.data.id, code: file.languageCode });
+		if (languages.error) {
+			return {
+				status: 500,
+				body: languages.error.message
+			};
+		}
+	}
+	return {
+		status: 200
+	};
+}
