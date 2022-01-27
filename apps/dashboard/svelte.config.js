182395238bb8ba4799c8b59f558ecccf78868ec9
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2022-01-27 15:32:29 +0100

    chore: try node deployment

--- apps/dashboard/svelte.config.js
+++ apps/dashboard/svelte.config.js
@@ -1,5 +1,6 @@
 import preprocess from 'svelte-preprocess';
-import netlify from '@sveltejs/adapter-netlify';
+// import netlify from '@sveltejs/adapter-netlify';
+import node from '@sveltejs/adapter-node';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -14,7 +15,7 @@ const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
-		adapter: netlify()
+		adapter: node()
 	}
 };
 
