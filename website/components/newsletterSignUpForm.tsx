b53cb0a510366d871d76d303f846752a8f4a091e
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2022-02-02 11:29:24 +0100

    chore: update newsletter form

--- website/components/newsletterSignUpForm.tsx
+++ website/components/newsletterSignUpForm.tsx
@@ -1,14 +1,9 @@
 function NewsletterSignUpForm() {
   return (
-    <form name="contact" netlify>
+    <form name="newsletter" method="POST" data-netlify="true">
       <p>
         <label>
-          Name <input type="text" name="name" />
-        </label>
-      </p>
-      <p>
-        <label>
-          Email <input type="email" name="email" />
+          Your Email: <input type="email" name="email" />
         </label>
       </p>
       <p>
