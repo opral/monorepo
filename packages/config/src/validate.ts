5c152aa8991abc8243e1a91590f9e1c18585286f
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2022-02-05 18:38:24 +0100

    chore: fix dist

--- packages/config/src/validate.ts
+++ packages/config/src/validate.ts
@@ -1,10 +1,10 @@
 import { InlangConfig01 } from './types/v0.1';
-import schema1_0 from '../schemas/v0.1.json';
+import schema01 from './schemas/v0.1.json';
 import Ajv from 'ajv';
 import { Result } from '@inlang/common';
 
 export function validate(args: { config: Record<string, unknown> }): Result<InlangConfig01, Error> {
-    const ajv = new Ajv({}).compile(schema1_0);
+    const ajv = new Ajv({}).compile(schema01);
     const valid = ajv(args.config);
     if (valid) {
         return Result.ok(args.config as InlangConfig01);
