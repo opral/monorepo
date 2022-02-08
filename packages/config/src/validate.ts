2f8c8f721432fbb984d721f45b7e8dda86463b83
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2022-02-08 11:06:50 +0100

    fix: ajv can throw

--- packages/config/src/validate.ts
+++ packages/config/src/validate.ts
@@ -5,12 +5,17 @@ import schema01 from './schemas/v0.1.json';
 import { InlangConfig01 } from './types/v0.1';
 
 export function validate(args: { config: Record<string, unknown> }): Result<InlangConfig01, Error> {
-    const ajv = addFormats(new Ajv({})).compile(schema01);
-    const valid = ajv(args.config);
-    if (valid) {
-        return Result.ok(args.config as InlangConfig01);
-    } else if (ajv.errors) {
-        return Result.err(Error(`'${ajv.errors[0].instancePath}': ${ajv.errors[0].message}`));
+    // ajv can throw
+    try {
+        const ajv = addFormats(new Ajv({})).compile(schema01);
+        const valid = ajv(args.config);
+        if (valid) {
+            return Result.ok(args.config as InlangConfig01);
+        } else if (ajv.errors) {
+            return Result.err(Error(`'${ajv.errors[0].instancePath}': ${ajv.errors[0].message}`));
+        }
+        return Result.err(Error('unknown error.'));
+    } catch (error) {
+        return Result.err(error as Error);
     }
-    return Result.err(Error('unknown error.'));
 }
