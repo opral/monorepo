f44df1a43632120ba51b0c9a3c0122d39a6daf47
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2022-02-08 18:58:30 +0100

    chore: add todo

--- packages/fluent-syntax-converters/src/types/converter.ts
+++ packages/fluent-syntax-converters/src/types/converter.ts
@@ -9,6 +9,15 @@ import { SingleResource } from '@inlang/fluent-syntax';
  * "simply" parse and serialize with regular JavaScript/Typescript.
  */
 export type Converter = {
+    // TODO each converter should annotate what it's supports.
+    // supports: {
+    //     attributes: boolean;
+    //     messages: boolean;
+    //     references: boolean;
+    //     selectors: boolean;
+    //     variables: boolean;
+    //     terms: boolean;
+    // };
     parse(args: { data: string }): Result<SingleResource, Error>;
     serialize(args: { resource: SingleResource }): Result<string, Error>;
 };
