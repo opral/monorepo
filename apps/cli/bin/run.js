db91641d45029dda53ac9f4b73e550a67180262b
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2022-02-09 18:28:20 +0100

    chore: add comment

--- apps/cli/bin/run.js
+++ apps/cli/bin/run.js
@@ -1,5 +1,8 @@
 #!/usr/bin/env node
 
+// the comment above signals unix environments which interpreter
+// to use when executing this file (node)
+
 // importing the main file which automatically executes
 // the function call within the file aka starts the cli
 import '../dist/main.js';
