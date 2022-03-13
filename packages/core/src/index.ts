f705d15a344b00d75a76bac0e3d3529465a77a09
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2022-03-13 17:16:45 +0100

    chore: delete unsused directory

--- packages/core/src/index.ts
+++
@@ -1,3 +0,0 @@
-export { LanguageCode } from './types/languageCode';
-export { Result } from './types/result';
-export { languageCodes } from './variables/languageCodes';
--- packages/core/src/types/languageCode.ts
+++
@@ -1,8 +0,0 @@
-import { languageCodes } from '../variables/languageCodes';
-
-/**
- * Iso 639-1 language code.
- *
- * https://www.iso.org/standard/22109.html
- */
-export type LanguageCode = typeof languageCodes[number];
