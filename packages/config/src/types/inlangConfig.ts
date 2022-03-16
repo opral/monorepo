53460077e892c4ef0a861baec603fda877c0fc09
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2022-03-16 12:21:56 +0100

    add: v0.2

--- packages/config/src/types/inlangConfig.ts
+++ packages/config/src/types/inlangConfig.ts
@@ -1,12 +1,14 @@
 import { InlangConfig01 } from './v0.1';
+import { InlangConfig02 } from './v0.2';
 
 export type InlangConfig = {
-    latest: InlangConfig01;
+    latest: InlangConfig02;
     /**
      * Any version of inlang config.
      *
      * (Union of all inlang config versions.)
      */
-    any: InlangConfig01;
+    any: InlangConfig01 | InlangConfig02;
     '0.1': InlangConfig01;
+    '0.2': InlangConfig02;
 };
