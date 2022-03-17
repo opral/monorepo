b0f4610c3838d8cf060425998da574aaa0b8c0a8
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2022-03-17 16:50:44 +0100

    add: writeResources

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,3 +1,4 @@
 export { readResources } from './functions/readResources';
+export { writeResources } from './functions/writeResources';
 export { determineClosestPath } from './functions/determineClosestPath';
 export { readInlangConfig } from './functions/readInlangConfig';
