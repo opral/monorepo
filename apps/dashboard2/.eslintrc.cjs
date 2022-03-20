ef474a5eccaa1e0695fb40fc58b5cbcbb3419671
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2022-03-20 08:45:40 +0100

    chore: add dependencies

--- apps/dashboard2/.eslintrc.cjs
+++ apps/dashboard2/.eslintrc.cjs
@@ -1,7 +1,12 @@
 module.exports = {
 	root: true,
 	parser: '@typescript-eslint/parser',
-	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'prettier',
+		'@inlang/eslint-config'
+	],
 	plugins: ['svelte3', '@typescript-eslint'],
 	ignorePatterns: ['*.cjs'],
 	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
