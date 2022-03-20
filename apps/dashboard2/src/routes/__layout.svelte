f8bbdb2f260049c0bc2e235053fffe28ff16f9c9
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2022-03-20 11:26:13 +0100

    add: bootstrap

---
+++ apps/dashboard2/src/routes/__layout.svelte
@@ -0,0 +1,27 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+	<div class="container">
+		<a class="navbar-brand" href="/">Inlang</a>
+		<button
+			class="navbar-toggler"
+			type="button"
+			data-bs-toggle="collapse"
+			data-bs-target="#navbarNav"
+			aria-controls="navbarNav"
+			aria-expanded="false"
+			aria-label="Toggle navigation"
+		>
+			<span class="navbar-toggler-icon" />
+		</button>
+		<div class="collapse navbar-collapse" id="navbarNav">
+			<!-- <ul class="navbar-nav">
+				<li class="nav-item">
+					<a class="nav-link disabled" aria-current="page" href="/">TODO</a>
+				</li>
+			</ul> -->
+		</div>
+	</div>
+</nav>
+
+<div class="container my-3">
+	<slot />
+</div>
