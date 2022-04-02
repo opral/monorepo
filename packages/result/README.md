a705ece8f5618808f787c3a025aab84e5638b9e9
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2022-04-02 18:17:35 -0400

    refactor: move result into own package

---
+++ packages/result/README.md
@@ -0,0 +1,3 @@
+# @inlang/result
+
+The result type is used throughout the inlang project to ensure consistent and forced error handling.
--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,3 +1,2 @@
 export { LanguageCode } from './types/languageCode';
-export { Result } from './types/result';
 export { languageCodes } from './variables/languageCodes';
