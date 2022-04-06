014be94588b454bae48831c77c0c5c7d51ecc253
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2022-04-06 14:00:37 -0400

    enhance: ui

--- apps/dashboard2/src/routes/git/[...uri]/__layout.svelte
+++ apps/dashboard2/src/routes/git/[...uri]/__layout.svelte
@@ -44,19 +44,27 @@
 </script>
 
 <!-- BREADCRUMBS START -->
-<sl-card class="w-full">
+<div class="border px-4 py-2 rounded">
 	<sl-breadcrumb>
+		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-6 h-6 pr-2"
+			><path
+				fill-rule="evenodd"
+				d="M3 2.75A2.75 2.75 0 015.75 0h14.5a.75.75 0 01.75.75v20.5a.75.75 0 01-.75.75h-6a.75.75 0 010-1.5h5.25v-4H6A1.5 1.5 0 004.5 18v.75c0 .716.43 1.334 1.05 1.605a.75.75 0 01-.6 1.374A3.25 3.25 0 013 18.75v-16zM19.5 1.5V15H6c-.546 0-1.059.146-1.5.401V2.75c0-.69.56-1.25 1.25-1.25H19.5z"
+			/><path
+				d="M7 18.25a.25.25 0 01.25-.25h5a.25.25 0 01.25.25v5.01a.25.25 0 01-.397.201l-2.206-1.604a.25.25 0 00-.294 0L7.397 23.46a.25.25 0 01-.397-.2v-5.01z"
+			/></svg
+		>
 		{#each breadcrumbs() as breadcrumb}
 			<sl-breadcrumb-item href={breadcrumb.href}>{breadcrumb.name}</sl-breadcrumb-item>
 		{/each}
 	</sl-breadcrumb>
-</sl-card>
+</div>
 
 <!-- BREADCRUMBS END -->
 
 {#await cloning}
 	<div
-		class="absolute inset-0 w-full h-full flex flex-col items-center justify-center backdrop-blur"
+		class="absolute inset-0 w-full h-full flex flex-col items-center justify-center backdrop-blur z-50"
 	>
 		<h1 class="display-md">Cloning repository...</h1>
 	</div>
--- apps/dashboard2/src/routes/index.svelte
+++
@@ -1,1 +0,0 @@
-<h1 class="text-title-lg">hello</h1>
