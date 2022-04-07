c5896d92d218bc50b756b6a9d57871a34506c2fd
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2022-04-07 16:56:05 -0400

    refactor: simplify code

--- apps/dashboard2/src/routes/git/[...uri]/in-editor/index.svelte
+++ apps/dashboard2/src/routes/git/[...uri]/in-editor/index.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
-	import { Message, serializePattern, type Resource } from '@inlang/fluent-ast';
+	import { languageName } from '$lib/utils/languageName';
+
+	import { Attribute, Message, serializePattern, type Resource } from '@inlang/fluent-ast';
 	import { inlangConfig, resources } from '../_store';
 	import Menubar from './_Menubar.svelte';
-	import Pattern from './_Pattern.svelte';
 	import Sidebar from './_Sidebar.svelte';
 
 	const baseLanguageCode = $inlangConfig?.baseLanguageCode ?? 'en';
@@ -20,9 +21,10 @@
 	/** Mapping the messages from the AST's with language codes. */
 	const rows = () => {
 		const result: {
-			messageId: string;
-			messages: Record<string, Message | undefined>;
-			attributeIds: string[];
+			id: string;
+			baseNode: Attribute | Message;
+			nodes: Record<string, Message | Attribute | undefined>;
+			actionRequired: boolean;
 		}[] = [];
 		for (const messageId of messageIds) {
 			const messages = Object.fromEntries(
@@ -31,6 +33,14 @@
 					$resources[languageCode]?.get({ message: { id: messageId } })
 				])
 			);
+			// pushing the message itself
+			result.push({
+				id: messageId,
+				// assumption that the baseNode always exists
+				baseNode: messages[baseLanguageCode] as Message,
+				nodes: messages,
+				actionRequired: false
+			});
 			const attributeIds = Array.from(
 				languageCodes.reduce((result, languageCode) => {
 					return new Set([
@@ -39,24 +49,27 @@
 					]);
 				}, new Set<string>())
 			);
-			result.push({
-				messageId: messageId,
-				messages,
-				attributeIds
-			});
+			// pushing all attributes of the message
+			for (const attributeId of attributeIds) {
+				const attributes = Object.fromEntries(
+					languageCodes.map((languageCode) => [
+						languageCode,
+						messages[languageCode]?.attributes.find(
+							(attribute) => attribute.id.name === attributeId
+						)
+					])
+				);
+				result.push({
+					id: '.' + attributeId,
+					// assumption that the baseNode always exists
+					baseNode: attributes[baseLanguageCode] as Attribute,
+					nodes: attributes,
+					actionRequired: false
+				});
+			}
 		}
 		return result;
 	};
-
-	function findAttribute(args: {
-		row: ReturnType<typeof rows>[number];
-		languageCode: string;
-		attributeId: string;
-	}) {
-		return args.row.messages[args.languageCode]?.attributes.find(
-			(attribute) => attribute.id.name === args.attributeId
-		);
-	}
 </script>
 
 <Menubar />
@@ -64,47 +77,27 @@
 	<Sidebar class="col-span-1" />
 	<div class="col-span-3 flex flex-col space-y-2">
 		{#each rows() as row}
-			<sl-card class="space-y-2">
-				<h3 slot="header" class="title-md">{row.messageId}</h3>
+			<sl-card class="space-y-2" class:ml-12={row.baseNode.type === 'Attribute'}>
+				<h3 slot="header" class="title-md">{row.id}</h3>
 				{#each languageCodes as languageCode}
-					{@const message = row.messages[languageCode]}
-					{@const serializedPattern = message?.value ? serializePattern(message.value) : undefined}
-					{#if languageCode === baseLanguageCode}
-						{#if serializedPattern}
-							<Pattern {serializedPattern} {languageCode} />
-						{:else}
-							<h4 class="title-sm">Pattern</h4>
-							<div class="flex items-center body-sm decoration-dotted">
-								<sl-icon name="info-circle" class="mr-1" />
-								<p class="decoration-dotted">This message has no pattern.</p>
-								<sl-button variant="text" size="small" on:click={() => alert('unimplemented')}>
-									Create pattern
-								</sl-button>
-							</div>
-						{/if}
+					{@const node = row.nodes[languageCode]}
+					{@const serializedPattern = node?.value ? serializePattern(node.value) : undefined}
+					{#if serializedPattern === undefined}
+						<h4 class="title-sm">{languageName(languageCode)}</h4>
+						<div class="flex items-center body-md decoration-dotted">
+							<sl-icon name="info-circle" class="mr-1" />
+							<p>This language has no pattern.</p>
+							<sl-button variant="text" on:click={() => alert('unimplemented')}>
+								Create pattern
+							</sl-button>
+						</div>
 					{:else}
-						<Pattern {serializedPattern} {languageCode} />
+						<sl-textarea rows="2" resize="auto" value={serializedPattern ?? ''}>
+							<h4 slot="label" class="title-sm pb-1">{languageName(languageCode)}</h4>
+						</sl-textarea>
 					{/if}
 				{/each}
 			</sl-card>
-			{#each row.attributeIds as attributeId}
-				<div class="flex">
-					<div class="w-12 h-full flex items-center justify-center">
-						<!-- TODO treeview line -->
-						<!-- <div class="h-0.5 w-20 bg-neutral-300 rotate-90" /> -->
-					</div>
-					<sl-details class="w-full space-y-2 p-0">
-						<h3 slot="summary" class="title-md">.{attributeId}</h3>
-						{#each languageCodes as languageCode}
-							{@const attribute = findAttribute({ row, languageCode, attributeId })}
-							{@const serializedPattern = attribute?.value
-								? serializePattern(attribute.value)
-								: undefined}
-							<Pattern {serializedPattern} {languageCode} />
-						{/each}
-					</sl-details>
-				</div>
-			{/each}
 		{/each}
 	</div>
 </div>
