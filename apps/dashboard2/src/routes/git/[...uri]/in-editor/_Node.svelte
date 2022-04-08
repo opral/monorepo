d2ae545a99a5ec7dee46fac763c4004d9adb75a5
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2022-04-07 23:47:15 -0400

    add: reactive has changes

--- apps/dashboard2/src/routes/git/[...uri]/in-editor/_Node.svelte
+++ apps/dashboard2/src/routes/git/[...uri]/in-editor/_Node.svelte
@@ -1,6 +1,10 @@
+<!-- 
+	A single node (attribute or message) in the editor view.
+ -->
 <script lang="ts">
 	import { languageName } from '$lib/utils/languageName';
 	import { Attribute, Message, serializePattern } from '@inlang/fluent-ast';
+	import { fade } from 'svelte/transition';
 
 	export let baseLanguageCode: string;
 	export let languageCodes: string[];
@@ -10,13 +14,41 @@
 		nodes: Record<string, Message | Attribute | undefined>;
 		actionRequired: boolean;
 	};
+
+	/**
+	 * The modified patterns.
+	 *
+	 * Contains a current and modified pattern to detect changes.
+	 */
+	const modifiedPatterns: Record<string, { current: string; modified: string }> =
+		Object.fromEntries(
+			// initializing the object with the language codes to avoid
+			// undefined values which can lead to throws
+			languageCodes.map((languageCode) => {
+				const node = row.nodes[languageCode];
+				const serializedPattern = node?.value ? serializePattern(node.value) : undefined;
+				return [
+					languageCode,
+					{ current: serializedPattern ?? '', modified: serializedPattern ?? '' }
+				];
+			})
+		);
+
+	$: hasChanges = Object.values(modifiedPatterns).some(
+		({ current, modified }) => current !== modified
+	);
 </script>
 
 <sl-card class:ml-12={row.baseNode.type === 'Attribute'}>
 	<div slot="header" class="flex justify-between">
 		<h3 class="title-md">{row.id}</h3>
 		{#if row.actionRequired}
-			<sl-tag variant="danger" size="small">Action required</sl-tag>
+			<div class="flex space-x-1">
+				{#if hasChanges}
+					<sl-tag variant="warning" size="small" transition:fade>Unsaved changes</sl-tag>
+				{/if}
+				<sl-tag variant="danger" size="small" transition:fade>Action required</sl-tag>
+			</div>
 		{/if}
 	</div>
 	<div class="space-y-2">
@@ -26,7 +58,7 @@
 			{#if row.baseNode.value === null}
 				<!-- show create pattern message if the current row is the "base row" -->
 				{#if languageCode === baseLanguageCode}
-					<div class="flex items-center body-md decoration-dotted">
+					<div class="flex items-center body-md">
 						<sl-icon name="info-circle" class="mr-1" />
 						<p>This message has no pattern in the base language.</p>
 					</div>
@@ -39,10 +71,13 @@
 					rows="2"
 					resize="auto"
 					value={serializedPattern ?? ''}
-					class:textarea-error={serializedPattern === undefined}
+					on:input={(event) => (modifiedPatterns[languageCode].current = event.srcElement.value)}
+					class:textarea-error={modifiedPatterns[languageCode].current === ''}
+					class:textarea-unsaved-changes={modifiedPatterns[languageCode].current !==
+						modifiedPatterns[languageCode].modified}
 				>
 					<h4 slot="label" class="title-sm pb-1">{languageName(languageCode)}</h4>
-					{#if serializedPattern === undefined}
+					{#if modifiedPatterns[languageCode].current === ''}
 						<p slot="help-text">Missing pattern.</p>
 					{/if}
 				</sl-textarea>
@@ -51,7 +86,7 @@
 	</div>
 	<div slot="footer" class="flex justify-between">
 		<div>
-			<sl-button variant="primary">
+			<sl-button variant="primary" disabled={hasChanges === false}>
 				<sl-icon src="/icons/save-floppy-disk.svg" slot="prefix" />
 				Save changes
 			</sl-button>
@@ -73,5 +108,9 @@
 
 	.textarea-error::part(form-control-help-text) {
 		@apply text-error;
+	}
+
+	.textarea-unsaved-changes::part(base) {
+		@apply border-warning-container;
 	}
 </style>
