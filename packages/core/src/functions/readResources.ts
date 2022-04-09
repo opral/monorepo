1bd4483471b4afeef84c8aaf5f748049c43793d8
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2022-04-09 19:02:53 -0400

    refactor: dont depent on @inlang/utils

--- packages/core/src/functions/readResources.ts
+++ packages/core/src/functions/readResources.ts
@@ -2,7 +2,6 @@ import type { Resource } from '@inlang/fluent-ast';
 import type { InlangConfig } from '@inlang/config';
 import { converters, parseResources } from '@inlang/fluent-format-converters';
 import path from 'path';
-import { languageCodes } from '@inlang/utils';
 import { Result } from '@inlang/result';
 import { CommonFsApi } from '../types/commonFsApi';
 
@@ -17,12 +16,13 @@ import { CommonFsApi } from '../types/commonFsApi';
 export async function readResources(args: {
     fs: CommonFsApi;
     directory: string;
+    languageCodes: InlangConfig['latest']['languageCodes'];
     pathPattern: InlangConfig['latest']['pathPattern'];
     fileFormat: InlangConfig['latest']['fileFormat'];
 }): Promise<Result<Record<string, Resource | undefined>, Error>> {
     const converter = converters[args.fileFormat];
     const localFiles = [];
-    for (const languageCode of languageCodes) {
+    for (const languageCode of args.languageCodes) {
         // named with underscore to avoid name clashing with the imported path module
         const _path = path.resolve(args.directory, args.pathPattern.replace('{languageCode}', languageCode));
         const decoder = new TextDecoder();
