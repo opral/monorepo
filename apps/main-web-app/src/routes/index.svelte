5dbff1546485ce275c3ac94038909d5a1a5aed9a
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2022-08-31 18:17:04 +0200

    refactor: remove unused code

--- apps/main-web-app/src/routes/index.svelte
+++ apps/main-web-app/src/routes/index.svelte
@@ -1,18 +1,5 @@
 <script lang="ts">
-	import { user } from '$lib/stores/user';
 	import type { SlDialog } from '@shoelace-style/shoelace';
-	import { onMount } from 'svelte';
-
-	/** inlang forks of public github projects */
-	const forks = [
-		{
-			name: 'inlang/inlang',
-			iconSrc: 'https://github.com/inlang.png',
-			description: 'Open Source Localization Solution for Software.',
-			stars: 0,
-			href: ''
-		}
-	];
 
 	/** The demonstration repository */
 	const demoRepository = {
@@ -70,43 +57,13 @@
 		</sl-card>
 	{/if}
 	<!-- END DEMO REPOSITORY -->
-
-	<!-- START FORKED REPOSITORIES -->
-	<!-- remove slice when fetching forked repositories is implemented  -->
-	{#each forks.filter((fork) => fork.name.includes(searchValue)).slice(1) as fork}
-		<sl-card class="space-y-1">
-			<div class="flex justify-between items-center gap-2">
-				<h2 class="title-md pt-1">{fork.name}</h2>
-				<img class="h-7 w-7" src={fork.iconSrc} alt="{fork.name} icon" />
-			</div>
-			<h2 class="label-md">
-				<sl-icon name="star" />
-				{fork.stars}
-			</h2>
-			<p class="body-md">{fork.description}</p>
-			<sl-button size="small" class="pt-3 w-full">Open</sl-button>
-		</sl-card>
-	{/each}
-	<!-- END FORKED REPOSITORIES -->
 </div>
 <!-- END REPOSITORY GRID -->
 
 <!-- START Add project dialog -->
 <sl-dialog bind:this={addProjectDialog}>
 	<h1 slot="label">Add project</h1>
-	<p class="body-md">
-		The feature is not implemented yet. Do you want to be notified when the feature is implemented?
-	</p>
-	<iframe
-		class="pt-4"
-		src="https://tally.so/embed/mZj5av?alignLeft=1&hideTitle=1&transparentBackground=1"
-		width="100%"
-		height="150"
-		frameborder="0"
-		marginheight="0"
-		marginwidth="0"
-		title="Inlang newsletter"
-	/>
+	<p class="body-md">The feature is not implemented yet.</p>
 </sl-dialog>
 
 <!-- End Add project dialog -->
