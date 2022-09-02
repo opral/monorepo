18c2a3010a3627cce9ca3b68081b50318c6ceda7
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2022-09-02 14:42:11 +0200

    add demo repository card

--- apps/editor-solidjs/pages/index/index.page.tsx
+++ apps/editor-solidjs/pages/index/index.page.tsx
@@ -1,45 +1,61 @@
 import type { SlDialog } from "@shoelace-style/shoelace";
-import { createSignal } from "solid-js";
+import { createSignal, For } from "solid-js";
 import { Layout } from "./Layout";
 import style from "./index.module.css";
 
 export function Page() {
   /** The demonstration repository. */
-  const demoRepository = {
-    name: "inlang/demo",
-    iconSrc: "https://github.com/inlang.png",
-    description: "Try out the inlang editor with the demo repository.",
-    stars: 0,
-    href: "/git/https://github.com/inlang/demo/in-editor",
-  };
+  const repositories = [
+    {
+      name: "inlang/demo",
+      iconSrc: "https://github.com/inlang.png",
+      description: "Try out the inlang editor with the demo repository.",
+      stars: 0,
+      href: "/git/https://github.com/inlang/demo/in-editor",
+    },
+  ];
 
   return (
     <Layout>
       <SearchBar></SearchBar>
       <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-2">
-        <sl-card class={`space-y-1 ${style.demoCard}`}>
-          <div class="flex justify-between items-center gap-2 ">
-            <h2 class="title-md pt-1 demo-card">{demoRepository.name}</h2>
-            <img
-              class="h-7 w-7"
-              src={demoRepository.iconSrc}
-              alt="{demoRepository.name} icon"
-            />
-          </div>
-          <h2 class="label-md">
-            <sl-icon prop:name="star" />
-            {demoRepository.stars}
-          </h2>
-          <p class="body-md">{demoRepository.description}</p>
-          <sl-button
-            prop:size="small"
-            prop:variant="primary"
-            class="pt-3 w-full"
-            prop:href={demoRepository.href}
-          >
-            Open
-          </sl-button>
-        </sl-card>
+        <For
+          each={repositories.filter((repository) =>
+            repository.name.includes(searchValue())
+          )}
+        >
+          {(repository) => (
+            <sl-card
+              class={`space-y-1 ${
+                repository.name.includes("demo") ? style.demoCard : ""
+              }`}
+            >
+              <div class="flex justify-between items-center gap-2 ">
+                <h2 class="title-md pt-1 demo-card">{repository.name}</h2>
+                <img
+                  class="h-7 w-7"
+                  src={repository.iconSrc}
+                  alt={repository.name + "icon"}
+                />
+              </div>
+              <h2 class="label-md">
+                <sl-icon prop:name="star" />
+                {repository.stars}
+              </h2>
+              <p class="body-md">{repository.description}</p>
+              <sl-button
+                prop:size="small"
+                prop:variant={
+                  repository.name.includes("demo") ? `primary` : "default"
+                }
+                class="pt-3 w-full"
+                prop:href={repository.href}
+              >
+                Open
+              </sl-button>
+            </sl-card>
+          )}
+        </For>
       </div>
     </Layout>
   );
