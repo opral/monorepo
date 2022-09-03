b72061085ae064a18e159e41afb589e736108d30
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2022-09-03 15:37:39 +0200

    add node polyfills
    Required for lightning-fs and isomorphic git

--- apps/editor-solidjs/vite.config.ts
+++ apps/editor-solidjs/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
 import ssr from "vite-plugin-ssr/plugin";
+import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
 
 export default defineConfig({
   plugins: [solidPlugin({ ssr: true }), ssr()],
@@ -9,5 +10,20 @@ export default defineConfig({
   },
   build: {
     target: "esnext",
+  },
+  optimizeDeps: {
+    // lightning-fs and/or isomorphic-git require node polyfills
+    esbuildOptions: {
+      // Node.js global to browser globalThis
+      define: {
+        global: "globalThis",
+      },
+      // Buffer polyfill
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          buffer: true,
+        }),
+      ],
+    },
   },
 });
