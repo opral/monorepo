f57d4600bbaa0c9d0bf6dfb3bc3077c847346687
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2022-09-03 11:18:07 +0200

    fix: disposing of previous page

--- apps/editor-solidjs/renderer/_default.page.client.tsx
+++ apps/editor-solidjs/renderer/_default.page.client.tsx
@@ -4,11 +4,28 @@ import { PageContext } from "./types";
 // importing tailwind classes
 import "./style.css";
 
+export const clientRouting = true;
+
+/**
+ * A function that disposes previously rendered pages.
+ *
+ * If the function is not executed, each route change will
+ * append a page to the DOM without clearing (disposing)
+ * the previous one.
+ */
+let disposePreviousPage: () => void;
+
 export async function render(pageContext: PageContext) {
   const { Page } = pageContext;
 
-  return solidRender(
+  // if a page has been rendered before, dispose it.
+  if (disposePreviousPage) {
+    disposePreviousPage();
+  }
+
+  // render the page and save the dispose function of that page
+  disposePreviousPage = solidRender(
     () => <Page />,
-    document.getElementById("root") as HTMLElement
+    document.getElementById("root")
   );
 }
