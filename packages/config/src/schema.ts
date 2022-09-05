5cc2d9e8af6490761cab4c4fb178ae535a3e7766
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2022-09-05 11:02:20 +0200

    initial schema

--- packages/config/src/schema.ts
+++ packages/config/src/schema.ts
@@ -1,4 +1,21 @@
+import { LanguageCode } from "@inlang/utilities";
+
+/**
+ * Inlang config schema.
+ */
 type Schema = {
-	referenceLanguage: "de" | "en";
-	languages: "";
+	/**
+	 * The language that other languages are validated against.
+	 *
+	 * In most cases the reference language is English.
+	 */
+	referenceLanguage: LanguageCode;
+	/**
+	 * Languages of this project.
+	 *
+	 * The languages must include the reference language itself.
+	 */
+	languages: LanguageCode[];
+	/** Testing whether function can be executed in sandbox. */
+	testFunction: (name: string) => string;
 };
