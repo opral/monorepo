ce28879b83480456f50f06513327862c7ceafd77
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2022-09-16 11:45:38 +0200

    import stylesheet for components in storybook

--- packages/design-system/.storybook/preview.cjs
+++ packages/design-system/.storybook/preview.cjs
@@ -1,9 +1,13 @@
+// importing css stylesheet for the components
+// see https://storybook.js.org/docs/react/configure/styling-and-css
+import "../dist/components/style.css";
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
-}
+	actions: { argTypesRegex: "^on[A-Z].*" },
+	controls: {
+		matchers: {
+			color: /(background|color)$/i,
+			date: /Date$/,
+		},
+	},
+};
--- packages/design-system/src/components/button.stories.ts
+++ packages/design-system/src/components/button.stories.ts
@@ -1,30 +1,7 @@
 import { html } from "lit-html";
 
-/**
- * Primary UI component for user interaction
- */
-export const Button = ({
-	primary,
-	backgroundColor = null,
-	size,
-	label,
-	onClick,
-}) => {
-	const mode = primary
-		? "storybook-button--primary"
-		: "storybook-button--secondary";
-
-	return html`
-		<button
-			type="button"
-			class=${[
-				"storybook-button",
-				`storybook-button--${size || "medium"}`,
-				mode,
-			].join(" ")}
-			@click=${onClick}
-		>
-			${label}
-		</button>
-	`;
+export default {
+	title: "Button",
 };
+
+export const Primary = () => html`<div class="bg-blue-500">hello world</div>`;
