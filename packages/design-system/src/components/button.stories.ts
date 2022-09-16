ba400172bafc83db8bc122e33f8a4127b6ada8e4
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2022-09-16 11:48:42 +0200

    setup storybook

--- packages/design-system/src/components/button.stories.ts
+++ packages/design-system/src/components/button.stories.ts
@@ -1,7 +1,8 @@
 import { html } from "lit-html";
+import "../../dist/components/button.js";
 
 export default {
 	title: "Button",
 };
 
-export const Primary = () => html`<div class="bg-blue-500">hello world</div>`;
+export const Primary = () => html`<in-button>hello world</in-button>`;
