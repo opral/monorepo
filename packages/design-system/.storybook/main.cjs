4bbcc29c11e14b80239c39886db26b25e30654df
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2022-10-01 22:09:10 +0200

    remove storybook

--- packages/design-system/.storybook/main.cjs
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-	addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
-	framework: "@storybook/web-components",
-	// hosting the tailwind.css stylesheet
-	staticDirs: [{ from: "./static", to: "/" }],
-};
