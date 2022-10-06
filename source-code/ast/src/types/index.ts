9d150391b87e6ec049334f6c4a3fcb878d43b63b
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2022-10-06 14:59:34 +0200

    init

--- source-code/ast/src/types/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./comments.js";
-export * from "./entry.js";
-export * from "./expression.js";
-export * from "./inlineExpression.js";
-export * from "./literal.js";
-export * from "./patternElement.js";
-export * from "./reference.js";
--- source-code/ast/src/types/patternElement.ts
+++
@@ -1,3 +0,0 @@
-import type { TextElement, Placeable } from "../classes/index.js";
-
-export type PatternElement = TextElement | Placeable;
