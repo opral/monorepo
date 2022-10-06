447b4f19d4baac0da346fca489246bccd34c15a7
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2022-10-06 14:59:34 +0200

    chore: .

---
+++ packages/ast/src/query/index.ts
@@ -0,0 +1,16 @@
+// query(ast)         // bundle
+//   .from('en')      // resource
+//   .select('id')    // message
+
+// query(ast)         // bundle
+//   .from('en')      // resource
+//   .select('id')    // message
+
+// query(bundle)      // bundle
+//   .resource('en')  // resource
+//   .select('id')    // message
+
+// query(bundle)      // bundle
+//   .resource('*')  // all resources
+//   .select('id')    // message
+
