44b13e38e8d21bf8198fad557e9541b850497a78
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2022-10-14 11:48:41 +0200

    chore: add test

--- packages/ide-extension/test/utils/translationFilesGlobPattern.test.ts
+++
@@ -1,8 +0,0 @@
-import { translationFilesGlobPattern } from '../../src/utils/translationFilesGlobPattern';
-
-it('should return the correct glob pattern', () => {
-  const cwd = '/User/code';
-  const pathPattern = './translations/{languageCode}.ftl';
-  const result = translationFilesGlobPattern({ cwd, pathPattern });
-  expect(result).toBe('/User/code/translations/**');
-});
