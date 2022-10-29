d04eb511ab89b311460b18179f129906258b758b
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2022-10-29 18:04:53 +0200

    refactor: remove unused dynamic component

--- packages/website/src/components/Dynamic.tsx
+++
@@ -1,3 +0,0 @@
-export function Dynamic(props: { color: string; children?: React.ReactNode }) {
-	return <div className={`bg-${props.color} p-5`}>{props.children}</div>;
-}
--- packages/website/src/pages/index/index.page.tsx
+++ packages/website/src/pages/index/index.page.tsx
@@ -1,15 +1,11 @@
 import { useState } from "react";
 import { DefaultLayout } from "../DefaultLayout.js";
-import { Dynamic } from "@src/components/Dynamic.js";
 
 export function Page() {
 	const [count, setCount] = useState(0);
 	return (
 		<DefaultLayout>
 			<h1>{count}</h1>
-			<Dynamic color="tertiary">
-				<p>with an element</p>
-			</Dynamic>
 			<button
 				className="button button-fill-primary"
 				onClick={() => setCount(count + 1)}
