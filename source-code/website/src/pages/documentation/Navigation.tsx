10c4c91619be07b645f2f13aff6e77d01dcbdf9c
jannesblobel <72493222+jannesblobel@users.noreply.github.com>
2022-10-30 01:57:49 +0200

    remove uselesse textline perhaps is the Link component not the right  here

--- source-code/website/src/pages/documentation/Navigation.tsx
+++ source-code/website/src/pages/documentation/Navigation.tsx
@@ -34,7 +34,7 @@ export function Navigation(props: {
 									<Link
 										href={link.href}
 										className={clsx(
-											"block w-full pl-3.5 before:pointer-events-none before:absolute before:-left-1 before:top-1/2 before:h-1.5 before:w-1.5 before:-translate-y-1/2 before:rounded-full",
+											// only the activ link is in the primary color
 											link.href === pageContext.urlPathname
 												? "font-semibold text-sky-500 before:bg-sky-500"
 												: "text-slate-500 before:hidden before:bg-slate-300 hover:text-slate-600 hover:before:block "
