ae9cd11315d1fe898bb55aa6bb1420f97e719fdd
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2022-10-29 20:06:42 +0200

    add: button

--- source-code/website/src/components/Button.tsx
+++ source-code/website/src/components/Button.tsx
@@ -1,25 +1,46 @@
 import type { DesignSystemColors } from "../../tailwind.config.cjs";
 import { clsx } from "clsx";
 
-export function Button(props: {
-	color: DesignSystemColors[number];
-	className?: string;
-	children: React.ReactNode;
-}) {
-	// props.className = clsx(styles[props.color], props.className);
-	return <button {...props} />;
+export function Button(
+	props: {
+		color: DesignSystemColors[number];
+		variant: keyof typeof variants;
+		children: React.ReactNode;
+	} & React.ButtonHTMLAttributes<HTMLButtonElement>
+) {
+	const variant = variants[props.variant];
+	const variantColor = variant[props.color];
+	return (
+		<button
+			className={clsx(
+				base,
+				// if disabled, show the disabled color variant
+				// else show the variant with appplied color pattern
+				props.disabled ? variant["disabled"] : variantColor
+			)}
+			{...props}
+		/>
+	);
 }
 
+// base style of a button
 const base =
-	"rounded-full bg-sky-300 py-2 px-4 text-sm font-semibold text-slate-900 hover:bg-sky-200 focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-300/50 active:bg-sky-500";
+	"rounded py-2 px-4 text-sm font-semibold focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 disabled:cursor-not-allowed";
 
-const colors: Record<DesignSystemColors[number], string> = {
-	primary:
-		"rounded-full bg-sky-300 py-2 px-4 text-sm font-semibold text-slate-900 hover:bg-sky-200 focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-300/50 active:bg-sky-500",
-	secondary:
-		"rounded-full bg-slate-800 py-2 px-4 text-sm font-medium text-white hover:bg-slate-700 focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white/50 active:text-slate-400",
-	tertiary: "",
-	error: "",
+// variants with nested color patterns
+const variants: Record<
+	"fill",
+	Record<DesignSystemColors[number] | "disabled", string>
+> = {
+	fill: {
+		primary:
+			"bg-primary text-on-primary hover:bg-hover-primary focus-visible:bg-primary/50 active:bg-active-primary",
+		secondary:
+			"bg-secondary text-on-secondary hover:bg-hover-secondary focus-visible:bg-secondary/50 active:bg-active-secondary",
+		tertiary:
+			"bg-tertiary text-on-tertiary hover:bg-hover-tertiary focus-visible:bg-tertiary/50 active:bg-active-tertiary",
+		error:
+			"bg-error text-on-error hover:bg-hover-tertiary focus-visible:bg-tertiary/50 active:bg-active-tertiary",
+		disabled: "bg-disabled-container text-disabled-content",
+	},
 };
-
-const variant = {};
