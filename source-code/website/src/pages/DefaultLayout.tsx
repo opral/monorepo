63a2205d5827d53d30a9dc5282371159c31d946c
jannesblobel <72493222+jannesblobel@users.noreply.github.com>
2022-10-30 01:27:33 +0200

    start to Work on the Layout on clint side

--- source-code/website/src/pages/DefaultLayout.tsx
+++ source-code/website/src/pages/DefaultLayout.tsx
@@ -1,11 +1,70 @@
 import { Button } from "@src/components/Button.js";
-
+import { Navigation } from "./documentation/Navigation.js";
+const navigation = [
+	{
+		title: "Introduction",
+		links: [
+			{ title: "Getting started", href: "/" },
+			{ title: "Installation", href: "/" },
+		],
+	},
+	{
+		title: "Introduction",
+		links: [
+			{ title: "Getting started", href: "/" },
+			{ title: "Installation", href: "/" },
+		],
+	},
+	{
+		title: "Introduction",
+		links: [
+			{ title: "Getting started", href: "/" },
+			{ title: "Installation", href: "/" },
+		],
+	},
+	{
+		title: "Introduction",
+		links: [
+			{ title: "Getting started", href: "/" },
+			{ title: "Installation", href: "/" },
+			{ title: "Getting started", href: "/" },
+			{ title: "Installation", href: "/" },
+			{ title: "Getting started", href: "/" },
+			{ title: "Installation", href: "/" },
+		],
+	},
+	{
+		title: "Introduction",
+		links: [
+			{ title: "Getting started", href: "/" },
+			{ title: "Installation", href: "/" },
+		],
+	},
+];
 export function DefaultLayout(props: { children: React.ReactNode }) {
 	return (
 		<>
-			<div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+			<div className="mx-auto max-w-7xl px-4   sm:px-6 lg:px-8">
 				<Navbar></Navbar>
 				{props.children}
+
+				<div className="">
+					<Navigation
+						navigation={navigation}
+						className="w-64 pr-8 xl:w-72 xl:pr-16"
+					/>
+				</div>
+				{/* <div className="hidden lg:relative lg:block lg:flex-none">
+					<div className="absolute inset-y-0 right-0 w-[50vw] bg-slate-50 dark:hidden" />
+					<div className="absolute top-16 bottom-0 right-0 hidden h-12 w-px bg-gradient-to-t from-slate-800 dark:block" />
+					<div className="absolute top-28 bottom-0 right-0 hidden w-px bg-slate-800 dark:block" />
+					<div className="sticky top-[4.5rem] -ml-0.5 h-[calc(100vh-4.5rem)] overflow-y-auto overflow-x-hidden py-16 pl-0.5">
+						<Navigation
+							navigation={navigation}
+							className="w-64 pr-8 xl:w-72 xl:pr-16"
+						/>
+					</div>
+				</div> */}
 			</div>
 		</>
 	);
@@ -15,9 +74,9 @@ function Navbar() {
 	const navigation = [{ name: "Docs", href: "#" }];
 
 	return (
-		<header className="bg-neutral">
+		<header className=" bg-black">
 			<nav aria-label="Top">
-				<div className="flex w-full items-center justify-between border-b border-indigo-500 py-6 lg:border-none">
+				<div className="flex w-full items-center justify-between bg-black border-b border-indigo-500 py-6 lg:border-none">
 					<div className="flex items-center">
 						<a href="#">
 							<span className="sr-only">inlang</span>
@@ -39,13 +98,13 @@ function Navbar() {
 							))}
 						</div>
 					</div>
-					<div className="ml-10 space-x-4">
+					{/* <div className="ml-10 space-x-4">
 						<Button color="primary">
 							<a href="/editor">Editor</a>
 						</Button>
-					</div>
+					</div> */}
 				</div>
-				<div className="flex flex-wrap justify-center space-x-6 py-4 lg:hidden">
+				<div className="flex flex-wrap justify-center space-x-6 py-4 bg-slate-800 lg:hidden">
 					{navigation.map((link) => (
 						<a
 							key={link.name}
--- source-code/website/src/pages/documentation/DocumentationLayout.tsx
+++ source-code/website/src/pages/documentation/DocumentationLayout.tsx
@@ -18,7 +18,7 @@ import { clsx } from "clsx";
 // import { Hero } from "@/components/Hero";
 // import { Logo, Logomark } from "@/components/Logo";
 // import { MobileNavigation } from ".//MobileNavigation.js";
-import { Navigation } from "../../components/Navigation.js";
+import { Navigation } from "./Navigation.js";
 // import { Prose } from "@/components/Prose";
 // import { Search } from "@/components/Search";
 
@@ -82,108 +82,108 @@ const navigation = [
 	//  Nav Directroy End
 ];
 
-function GitHubIcon(props) {
-	return (
-		<svg aria-hidden="true" viewBox="0 0 16 16" {...props}>
-			<path d="M8 0C3.58 0 0 3.58 0 8C0 11.54 2.29 14.53 5.47 15.59C5.87 15.66 6.02 15.42 6.02 15.21C6.02 15.02 6.01 14.39 6.01 13.72C4 14.09 3.48 13.23 3.32 12.78C3.23 12.55 2.84 11.84 2.5 11.65C2.22 11.5 1.82 11.13 2.49 11.12C3.12 11.11 3.57 11.7 3.72 11.94C4.44 13.15 5.59 12.81 6.05 12.6C6.12 12.08 6.33 11.73 6.56 11.53C4.78 11.33 2.92 10.64 2.92 7.58C2.92 6.71 3.23 5.99 3.74 5.43C3.66 5.23 3.38 4.41 3.82 3.31C3.82 3.31 4.49 3.1 6.02 4.13C6.66 3.95 7.34 3.86 8.02 3.86C8.7 3.86 9.38 3.95 10.02 4.13C11.55 3.09 12.22 3.31 12.22 3.31C12.66 4.41 12.38 5.23 12.3 5.43C12.81 5.99 13.12 6.7 13.12 7.58C13.12 10.65 11.25 11.33 9.47 11.53C9.76 11.78 10.01 12.26 10.01 13.01C10.01 14.08 10 14.94 10 15.21C10 15.42 10.15 15.67 10.55 15.59C13.71 14.53 16 11.53 16 8C16 3.58 12.42 0 8 0Z" />
-		</svg>
-	);
-}
+// function GitHubIcon(props) {
+// 	return (
+// 		<svg aria-hidden="true" viewBox="0 0 16 16" {...props}>
+// 			<path d="M8 0C3.58 0 0 3.58 0 8C0 11.54 2.29 14.53 5.47 15.59C5.87 15.66 6.02 15.42 6.02 15.21C6.02 15.02 6.01 14.39 6.01 13.72C4 14.09 3.48 13.23 3.32 12.78C3.23 12.55 2.84 11.84 2.5 11.65C2.22 11.5 1.82 11.13 2.49 11.12C3.12 11.11 3.57 11.7 3.72 11.94C4.44 13.15 5.59 12.81 6.05 12.6C6.12 12.08 6.33 11.73 6.56 11.53C4.78 11.33 2.92 10.64 2.92 7.58C2.92 6.71 3.23 5.99 3.74 5.43C3.66 5.23 3.38 4.41 3.82 3.31C3.82 3.31 4.49 3.1 6.02 4.13C6.66 3.95 7.34 3.86 8.02 3.86C8.7 3.86 9.38 3.95 10.02 4.13C11.55 3.09 12.22 3.31 12.22 3.31C12.66 4.41 12.38 5.23 12.3 5.43C12.81 5.99 13.12 6.7 13.12 7.58C13.12 10.65 11.25 11.33 9.47 11.53C9.76 11.78 10.01 12.26 10.01 13.01C10.01 14.08 10 14.94 10 15.21C10 15.42 10.15 15.67 10.55 15.59C13.71 14.53 16 11.53 16 8C16 3.58 12.42 0 8 0Z" />
+// 		</svg>
+// 	);
+// }
 
-function Header({ navigation }) {
-	let [isScrolled, setIsScrolled] = useState(false);
+// function Header({ navigation }) {
+// 	let [isScrolled, setIsScrolled] = useState(false);
 
-	useEffect(() => {
-		function onScroll() {
-			setIsScrolled(window.scrollY > 0);
-		}
-		onScroll();
-		window.addEventListener("scroll", onScroll, { passive: true });
-		return () => {
-			window.removeEventListener("scroll", onScroll, { passive: true });
-		};
-	}, []);
+// 	useEffect(() => {
+// 		function onScroll() {
+// 			setIsScrolled(window.scrollY > 0);
+// 		}
+// 		onScroll();
+// 		window.addEventListener("scroll", onScroll, { passive: true });
+// 		return () => {
+// 			window.removeEventListener("scroll", onScroll, { passive: true });
+// 		};
+// 	}, []);
 
-	return (
-		<header
-			className={clsx(
-				"sticky top-0 z-50 flex flex-wrap items-center justify-between bg-white px-4 py-5 shadow-md shadow-slate-900/5 transition duration-500 dark:shadow-none sm:px-6 lg:px-8",
-				isScrolled
-					? "dark:bg-slate-900/95 dark:backdrop-blur dark:[@supports(backdrop-filter:blur(0))]:bg-slate-900/75"
-					: "dark:bg-transparent"
-			)}
-		>
-			<div className="mr-6 flex lg:hidden">
-				<MobileNavigation navigation={navigation} />
-			</div>
-			<div className="relative flex flex-grow basis-0 items-center">
-				<a href="/" aria-label="Home page">
-					{/* TODO Favicon */}
-					{/* <Logomark className="h-9 w-9 lg:hidden" />
-					<Logo className="hidden h-9 w-auto fill-slate-700 dark:fill-sky-100 lg:block" /> */}
-				</a>
-			</div>
-			{/* Search bar Start  */}
+// 	return (
+// 		<header
+// 			className={clsx(
+// 				"sticky top-0 z-50 flex flex-wrap items-center justify-between bg-white px-4 py-5 shadow-md shadow-slate-900/5 transition duration-500 dark:shadow-none sm:px-6 lg:px-8",
+// 				isScrolled
+// 					? "dark:bg-slate-900/95 dark:backdrop-blur dark:[@supports(backdrop-filter:blur(0))]:bg-slate-900/75"
+// 					: "dark:bg-transparent"
+// 			)}
+// 		>
+// 			<div className="mr-6 flex lg:hidden">
+// 				<MobileNavigation navigation={navigation} />
+// 			</div>
+// 			<div className="relative flex flex-grow basis-0 items-center">
+// 				<a href="/" aria-label="Home page">
+// 					{/* TODO Favicon */}
+// 					{/* <Logomark className="h-9 w-9 lg:hidden" />
+// 					<Logo className="hidden h-9 w-auto fill-slate-700 dark:fill-sky-100 lg:block" /> */}
+// 				</a>
+// 			</div>
+// 			{/* Search bar Start  */}
 
-			{/* <div className="-my-5 mr-6 sm:mr-8 md:mr-0">
-				<Search />
-			</div> */}
+// 			{/* <div className="-my-5 mr-6 sm:mr-8 md:mr-0">
+// 				<Search />
+// 			</div> */}
 
-			{/* Search bar End  */}
+// 			{/* Search bar End  */}
 
-			<div className="relative flex basis-0 justify-end gap-6 sm:gap-8 md:flex-grow">
-				<a href="https://github.com" className="group" aria-label="GitHub">
-					<GitHubIcon className="h-6 w-6 fill-slate-400 group-hover:fill-slate-500 dark:group-hover:fill-slate-300" />
-				</a>
-			</div>
-		</header>
-	);
-}
+// 			<div className="relative flex basis-0 justify-end gap-6 sm:gap-8 md:flex-grow">
+// 				<a href="https://github.com" className="group" aria-label="GitHub">
+// 					<GitHubIcon className="h-6 w-6 fill-slate-400 group-hover:fill-slate-500 dark:group-hover:fill-slate-300" />
+// 				</a>
+// 			</div>
+// 		</header>
+// 	);
+// }
 
-function useTableOfContents(tableOfContents) {
-	let [currentSection, setCurrentSection] = useState(tableOfContents[0]?.id);
+// function useTableOfContents(tableOfContents) {
+// 	let [currentSection, setCurrentSection] = useState(tableOfContents[0]?.id);
 
-	let getHeadings = useCallback((tableOfContents) => {
-		return tableOfContents
-			.flatMap((node) => [node.id, ...node.children.map((child) => child.id)])
-			.map((id) => {
-				let el = document.getElementById(id);
-				if (!el) return;
+// 	let getHeadings = useCallback((tableOfContents) => {
+// 		return tableOfContents
+// 			.flatMap((node) => [node.id, ...node.children.map((child) => child.id)])
+// 			.map((id) => {
+// 				let el = document.getElementById(id);
+// 				if (!el) return;
 
-				let style = window.getComputedStyle(el);
-				let scrollMt = parseFloat(style.scrollMarginTop);
+// 				let style = window.getComputedStyle(el);
+// 				let scrollMt = parseFloat(style.scrollMarginTop);
 
-				let top = window.scrollY + el.getBoundingClientRect().top - scrollMt;
-				return { id, top };
-			});
-	}, []);
+// 				let top = window.scrollY + el.getBoundingClientRect().top - scrollMt;
+// 				return { id, top };
+// 			});
+// 	}, []);
 
-	useEffect(() => {
-		if (tableOfContents.length === 0) return;
-		let headings = getHeadings(tableOfContents);
-		function onScroll() {
-			let top = window.scrollY;
-			let current = headings[0].id;
-			for (let heading of headings) {
-				if (top >= heading.top) {
-					current = heading.id;
-				} else {
-					break;
-				}
-			}
-			setCurrentSection(current);
-		}
-		window.addEventListener("scroll", onScroll, { passive: true });
-		onScroll();
-		return () => {
-			window.removeEventListener("scroll", onScroll, { passive: true });
-		};
-	}, [getHeadings, tableOfContents]);
+// 	useEffect(() => {
+// 		if (tableOfContents.length === 0) return;
+// 		let headings = getHeadings(tableOfContents);
+// 		function onScroll() {
+// 			let top = window.scrollY;
+// 			let current = headings[0].id;
+// 			for (let heading of headings) {
+// 				if (top >= heading.top) {
+// 					current = heading.id;
+// 				} else {
+// 					break;
+// 				}
+// 			}
+// 			setCurrentSection(current);
+// 		}
+// 		window.addEventListener("scroll", onScroll, { passive: true });
+// 		onScroll();
+// 		return () => {
+// 			window.removeEventListener("scroll", onScroll, { passive: true });
+// 		};
+// 	}, [getHeadings, tableOfContents]);
 
-	return currentSection;
-}
+// 	return currentSection;
+// }
 
-export function Layout({ children, title, tableOfContents }) {
+export function Layout({ children, title }) {
 	let pageContext = usePageContext();
 	let isHomePage = pageContext.urlPathname === "/";
 	let allLinks = navigation.flatMap((section) => section.links);
@@ -209,7 +209,7 @@ export function Layout({ children, title, tableOfContents }) {
 
 	return (
 		<>
-			<Header navigation={navigation} />
+			{/* <Header navigation={navigation} /> */}
 
 			{isHomePage}
 
@@ -218,7 +218,7 @@ export function Layout({ children, title, tableOfContents }) {
 					<div className="absolute inset-y-0 right-0 w-[50vw] bg-slate-50 dark:hidden" />
 					<div className="absolute top-16 bottom-0 right-0 hidden h-12 w-px bg-gradient-to-t from-slate-800 dark:block" />
 					<div className="absolute top-28 bottom-0 right-0 hidden w-px bg-slate-800 dark:block" />
-					<div className="sticky top-[4.5rem] -ml-0.5 h-[calc(100vh-4.5rem)] bg-slate-200 overflow-y-auto overflow-x-hidden py-16 pl-0.5">
+					<div className="sticky top-[4.5rem] -ml-0.5 h-[calc(100vh-4.5rem)] overflow-y-auto overflow-x-hidden py-16 pl-0.5">
 						<Navigation
 							navigation={navigation}
 							className="w-64 pr-8 xl:w-72 xl:pr-16"
