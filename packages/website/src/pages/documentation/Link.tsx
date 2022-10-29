07abcb363d1258f40fd10e296f71cd56c5b97873
jannesblobel <72493222+jannesblobel@users.noreply.github.com>
2022-10-30 01:44:38 +0200

    add transition to Link

--- packages/website/src/pages/documentation/Link.tsx
+++ packages/website/src/pages/documentation/Link.tsx
@@ -1,7 +1,7 @@
 import { clsx } from "clsx";
 
 const styles = {
-	link: "rounded-full bg-sky-300 py-2 px-4 text-sm font-semibold text-slate-900 hover:bg-sky-200 focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-300/50 active:bg-sky-500",
+	link: "block w-full pl-3.5 before:pointer-events-none before:absolute before:-left-1 before:top-1/2 before:h-1.5 before:w-1.5 before:-translate-y-1/2 before:rounded-full hover:text-red-600	 transition-colors	duration-300	 ",
 };
 
 export function Link({ variant = "link", className, href, ...props }) {
