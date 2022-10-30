7d765defc481db8ad81c4a6e2acc2f89b6c11255
jannesblobel <72493222+jannesblobel@users.noreply.github.com>
2022-10-31 00:20:55 +0100

    add Heade, Navigation and Footer Example

--- source-code/website/src/pages/documentation/index.page.tsx
+++ source-code/website/src/pages/documentation/index.page.tsx
@@ -1,5 +1,46 @@
 import fs from "node:fs/promises";
 import { parseValidateAndRender } from "@src/services/markdoc/parseValidateAndRender.js";
+import { Header } from "./Header.jsx";
+import { Navigation } from "./Navigation.jsx";
+import { Footer } from "./Footer.jsx";
+
+const sections = [
+	{
+		title: "Introduction",
+		documents: [
+			{ title: "Getting started", href: "/documentation" },
+			{ title: "Installation", href: "/" },
+		],
+	},
+	{
+		title: "Introduction",
+		documents: [
+			{ title: "Getting started", href: "/documentation" },
+			{ title: "Installation", href: "/" },
+		],
+	},
+	{
+		title: "Introduction",
+		documents: [
+			{ title: "Getting started", href: "/documentation" },
+			{ title: "Installation", href: "/" },
+		],
+	},
+	{
+		title: "Introduction",
+		documents: [
+			{ title: "Getting started", href: "/documentation" },
+			{ title: "Installation", href: "/" },
+		],
+	},
+	{
+		title: "Introduction",
+		documents: [
+			{ title: "Getting started", href: "/documentation" },
+			{ title: "Installation", href: "/" },
+		],
+	},
+];
 
 export async function onBeforeRender() {
 	const text = await fs.readFile(
@@ -21,5 +62,16 @@ type PageProps = {
 };
 
 export function Page(props: PageProps) {
-	return <div innerHTML={props.markdown}></div>;
+	return (
+		<>
+			<div class="mx-auto max-w-screen-2xl px-4 sm:px-6 lg:px-8">
+				<Header />
+				<div class=" flex ">
+					<Navigation sections={sections} />
+					<div class=" w-full prose" innerHTML={props.markdown}></div>;
+				</div>
+				<Footer />
+			</div>
+		</>
+	);
 }
