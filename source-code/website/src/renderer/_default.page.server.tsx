9ad8a02e5baf5d5b74e098d24065c98aac39ddb4
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2022-10-30 23:28:34 +0100

    add: favicon and inter font

--- source-code/website/src/renderer/_default.page.server.tsx
+++ source-code/website/src/renderer/_default.page.server.tsx
@@ -33,6 +33,11 @@ export function render(pageContext: PageContext): unknown {
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content="${description}" />
+<!-- START import inter font -->
+		<link rel="preconnect" href="https://rsms.me/">
+		<link rel="stylesheet" href="https://rsms.me/inter/inter.css">
+<!-- END import inter font -->
+		${dangerouslySkipEscape(favicons)}
         <title>${title}</title>
         ${dangerouslySkipEscape(generateHydrationScript())}
       </head>
@@ -41,3 +46,13 @@ export function render(pageContext: PageContext): unknown {
       </body>
     </html>`;
 }
+
+const favicons = `
+<link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png">
+<link rel="manifest" href="/favicon/site.webmanifest">
+<link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#5bbad5">
+<meta name="msapplication-TileColor" content="#da532c">
+<meta name="theme-color" content="#ffffff">
+`;
