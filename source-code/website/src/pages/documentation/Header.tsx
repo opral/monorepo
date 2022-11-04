b8905f49bf108a7601f61741515dc5507a8e1591
jannesblobel <72493222+jannesblobel@users.noreply.github.com>
2022-11-04 22:29:27 +0100

    refactor: fix the Mobile Version of the Header

--- source-code/website/src/pages/documentation/Header.tsx
+++ source-code/website/src/pages/documentation/Header.tsx
@@ -1,10 +1,12 @@
-import { createSignal, For } from "solid-js";
+import { createSignal, For, Show } from "solid-js";
 import logo from "../../../public/favicon/favicon.ico";
 import { Icon } from "@iconify-icon/solid";
 
 const links = [{ name: "Docs", href: "/documantaion" }];
 
 export function Header() {
+	const [show, setShow] = createSignal(false);
+
 	return (
 		<header class="sticky top-0 z-40 bg-white 	">
 			<div
@@ -27,6 +29,7 @@ export function Header() {
 
 					<div class="-my-2 -mr-2 md:hidden">
 						<button
+							onClick={(e) => setShow(true)}
 							type="button"
 							class="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
 							aria-expanded="false"
@@ -60,112 +63,96 @@ export function Header() {
 								)}
 							</For>
 						</nav>
-						{/* Start Sign in Button */}
 						<div class="flex items-center self-center md:ml-12">
 							<a class="flex text-lg" href="https://github.com/inlang/inlang">
 								<Icon icon="codicon:github-inverted" />
 							</a>
-							{/* <a
-								href="#"
-								class="text-base font-medium text-gray-500 hover:text-gray-900"
-							>
-								Sign in
-							</a>
-							<a
-								href="#"
-								class="ml-8 inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
-							>
-								Sign up
-							</a> */}
 						</div>
-						{/* Start Sign in Button */}
 					</div>
 				</div>
 
 				<div class="border-b-4 border-indigo-500 max-w-max "> </div>
 				<hr class="border-slate-200 "></hr>
 			</div>
-
-			{/* <!--
-    Mobile menu, show/hide based on mobile menu state.
-
-    Entering: "duration-200 ease-out"
-      From: "opacity-0 scale-95"
-      To: "opacity-100 scale-100"
-    Leaving: "duration-100 ease-in"
-      From: "opacity-100 scale-100"
-      To: "opacity-0 scale-95"
-  --> */}
-			<div class="absolute inset-x-0 top-0 z-30 origin-top-right transform p-2 transition md:hidden">
-				<div class="divide-y-2 divide-gray-50 rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5">
-					<div class="px-5 pt-5 pb-6 sm:pb-8">
-						<div class="flex items-center justify-between">
-							<div>
-								<img
-									class="h-8 w-auto"
-									src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-									alt="Your Company"
-								/>
-							</div>
-							<div class="-mr-2">
-								<button
-									type="button"
-									class="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
-								>
-									<span class="sr-only">Close menu</span>
-									{/* <!-- Heroicon name: outline/x-mark --> */}
-									<svg
-										class="h-6 w-6"
-										xmlns="http://www.w3.org/2000/svg"
-										fill="none"
-										viewBox="0 0 24 24"
-										stroke-width="1.5"
-										stroke="currentColor"
-										aria-hidden="true"
-									>
-										<path
-											stroke-linecap="round"
-											stroke-linejoin="round"
-											d="M6 18L18 6M6 6l12 12"
+			<Show when={show()}>
+				<div
+					class={
+						"absolute inset-x-0 top-0 z-30 origin-top-right transform p-2 transition md:hidden"
+					}
+				>
+					<div class="divide-y-2 divide-gray-50 rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5">
+						<div class="px-5 pt-5 pb-6 sm:pb-8">
+							<div class="flex items-center justify-between">
+								<div class="">
+									<a href="/" class="flex">
+										<img
+											class="h-8 w-auto sm:h-10"
+											src="../../../public/favicon/favicon.ico"
+											alt=" Company Logo"
 										/>
-									</svg>
-								</button>
+										<span class=" self-center pl-2 text-2xl	font-bold">
+											inlang
+										</span>
+									</a>
+								</div>
+								<div class="-mr-2">
+									<button
+										onClick={(e) => setShow(false)}
+										type="button"
+										class="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+									>
+										<span class="sr-only">Close menu</span>
+										{/* <!-- Heroicon name: outline/x-mark --> */}
+										<svg
+											class="h-6 w-6"
+											xmlns="http://www.w3.org/2000/svg"
+											fill="none"
+											viewBox="0 0 24 24"
+											stroke-width="1.5"
+											stroke="currentColor"
+											aria-hidden="true"
+										>
+											<path
+												stroke-linecap="round"
+												stroke-linejoin="round"
+												d="M6 18L18 6M6 6l12 12"
+											/>
+										</svg>
+									</button>
+								</div>
 							</div>
+							{/* Nav, responsive */}
 						</div>
-						{/* Nav, responsive */}
-					</div>
-					<div class="py-6 px-5">
-						<div class="grid grid-cols-2 gap-4">
-							<a
-								href="#"
-								class="rounded-md text-base font-medium text-gray-900 hover:text-gray-700"
-							>
-								Pricing
-							</a>
-							<a
-								href="#"
-								class="rounded-md text-base font-medium text-gray-900 hover:text-gray-700"
-							>
-								Docs
-							</a>
-						</div>
-						<div class="mt-6">
-							<a
-								href="#"
-								class="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
-							>
-								Sign up
-							</a>
-							<p class="mt-6 text-center text-base font-medium text-gray-500">
-								Existing customer?
-								<a href="#" class="text-indigo-600 hover:text-indigo-500">
-									Sign in
-								</a>
-							</p>
+						<div class="py-6 px-5">
+							{/* Change the grid-cols to have more  than one item in a col. */}
+							<div class="grid grid-cols-1 gap-4 ">
+								{/* TODO */}
+
+								{/* TODO */}
+								<For each={links}>
+									{(link) => (
+										<a class="link-primary" target="_blank" href={link.href}>
+											{link.name}
+										</a>
+									)}
+								</For>
+								<div>
+									<a
+										class="flex  space-x-1.5"
+										href="https://github.com/inlang/inlang"
+									>
+										<p>Github</p>
+										<p class="flex items-center self-center md:ml-12">
+											{" "}
+											<Icon icon="codicon:github-inverted" />
+										</p>
+									</a>
+								</div>
+							</div>
 						</div>
 					</div>
 				</div>
-			</div>
+			</Show>
 		</header>
 	);
 }
