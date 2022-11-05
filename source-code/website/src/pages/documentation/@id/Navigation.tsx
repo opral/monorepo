2e2c8ee0ea98e5d5d3602c77d0a735cf095f0932
jannesblobel <72493222+jannesblobel@users.noreply.github.com>
2022-11-05 23:33:26 +0100

    refactor: Fix the SideNavigation

--- source-code/website/src/pages/documentation/@id/Navigation.tsx
+++ source-code/website/src/pages/documentation/@id/Navigation.tsx
@@ -8,97 +8,66 @@ export function Navigation(props: {
 	}>;
 }) {
 	return (
-		<nav class="text-base lg:text-sm w-64 pr-8 xl:w-72 xl:pr-16 bg-surface-100 px-4 sm:px-6 lg:px-8">
-			{/* <div class="fixed border-b-2 "> ich könnte mal eine Suche werden</div> */}
+		<div class="sticky top-[4.5rem] -ml-0.5 h-[calc(100vh-4.5rem)] overflow-y-auto bg-surface-500 overflow-x-hidden  pl-0.5">
+			<nav class=" text-sm w-64  xl:w-72 px-8 pb-8	">
+				{/* <div class="fixed border-b-2 "> ich könnte mal eine Suche werden</div> */}
+				<ul role="list" class={``}>
+					<For each={props.sections}>
+						{(section) => (
+							<>
+								<div class=" font-display font-medium  mt-6	">
+									<Switch
+										fallback={
+											<>
+												<hr class="pt-2.5 border-t border-primary"></hr>
+											</>
+										}
+									>
+										<Match when={section.title == "Introduction"}>
+											<></>
+										</Match>
+									</Switch>
 
-			<ul
-				role="list"
-				class={`
-				fixed calc((100% - (var(--vp-layout-max-width) - 64px)) / 2 + var(--vp-sidebar-width) - 32px
-				pacity-0 overflow-y-auto overflow-x-hidden px-8 pt-8 pb-24  bottom-0 left-0;
-	z-index: var(--vp-z-index-sidebar);
-	width: calc(100vw - 64px);
-	max-width: 320px;
-	box-shadow: var(--vp-c-shadow-3);
-`}
-			>
-				{/* 
-				
-  
-				
-				top: var(--vp-layout-top-height, 0px);
-				bottom: 0;
-				left: 0;
-				z-index: var(--vp-z-index-sidebar);
-				padding: 32px 32px 96px;
-				width: calc(100vw - 64px);
-				max-width: 320px;
-				background-color: var(--vp-c-bg);
-				opacity: 0;
-				box-shadow: var(--vp-c-shadow-3);
-				overflow-x: hidden;
-				overflow-y: auto;
-				transform: translate(-100%);
-				transition: opacity .5s,transform .25s ease;
-				
-				
-				h-[calc(100vh-4.5rem)] overflow-y-auto overflow-x-hidden lg:block fixed   text-base lg:text-sm w-64 pr-8 xl:w-72 xl:pr-16 pb-10  */}
-				<For each={props.sections}>
-					{(section) => (
-						<>
-							<div
-								class=" pt-4 
-							 lg:pb-2 lg:pt-6 "
-							>
-								<Switch
-									fallback={
-										<>
-											<hr class="pb-1  border-t-2"></hr>
-										</>
-									}
+									<h2 class="font-display font-bold  py-1.5 ">
+										{section.title}
+									</h2>
+								</div>
+								<ul
+									role="list"
+									class=""
+									// class="mt-2 space-y-2 border-l-2 border-slate-100  lg:mt-4 lg:space-y-4 lg:border-slate-200"
 								>
-									<Match when={section.title == "Introduction"}>
-										<></>
-									</Match>
-								</Switch>
-
-								<h2 class="font-display   font-bold text-slate-900  ">
-									{section.title}
-								</h2>
-							</div>
-							<ul
-								role="list"
-								// class="mt-2 space-y-2 border-l-2 border-slate-100  lg:mt-4 lg:space-y-4 lg:border-slate-200"
-							>
-								<For each={section.documents}>
-									{(document) => (
-										<>
-											<li class="relative">
-												<a
-													class={`${
-														document.href === currentPageContext()?.urlPathname
-															? " text-on-surface"
-															: " text-on-surface/80"
-														// ? "font-semibold text-sky-500 before:bg-sky-500"
-														// : "text-slate-500 before:hidden before:bg-slate-300 hover:text-slate-600 hover:before:block "
-													}
-													link link-primary
-														`}
-													// block w-full pl-3.5 before:pointer-events-none before:absolute before:-left-1 before:top-1/2 before:h-1.5 before:w-1.5 before:-translate-y-1/2 before:rounded-full
-
-													href={document.href}
-												>
-													{document.title}
-												</a>
-											</li>
-										</>
-									)}
-								</For>
-							</ul>
-						</>
-					)}
-				</For>
-			</ul>
-		</nav>
+									<For each={section.documents}>
+										{(document) => (
+											<>
+												<li class="relative ">
+													<a
+														class={`${
+															document.href ===
+															currentPageContext()?.urlPathname
+																? // TODO Change Color
+																  " text-primary "
+																: " text-on-surface/60"
+															// ? "font-semibold text-sky-500 before:bg-sky-500"
+															// : "text-slate-500 before:hidden before:bg-slate-300 hover:text-slate-600 hover:before:block "
+														}
+															block w-full font-medium  my-2
+															link link-primary 
+																`}
+														href={document.href}
+													>
+														<span class="	">{document.title}</span>
+													</a>
+												</li>
+											</>
+										)}
+									</For>
+								</ul>
+							</>
+						)}
+					</For>
+				</ul>
+			</nav>
+		</div>
 	);
 }
