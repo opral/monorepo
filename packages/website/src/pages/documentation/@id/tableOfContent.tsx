f8952aba7da74fb03c7e16af47507d62f6b13566
jannesblobel <72493222+jannesblobel@users.noreply.github.com>
2022-11-05 17:34:09 +0100

    outsource section into tableOfContent

---
+++ packages/website/src/pages/documentation/@id/tableOfContent.tsx
@@ -0,0 +1,61 @@
+export const sections = [
+	{
+		title: "1",
+		documents: [
+			{
+				title: "8 Items muss ich sehen",
+				href: "/documentation/getting-started",
+			},
+			{ title: "Installation", href: "/" },
+		],
+	},
+	{
+		title: "2 ",
+		documents: [
+			{ title: "Getting started", href: "/" },
+			{ title: "Installation", href: "/" },
+		],
+	},
+	{
+		title: "3",
+		documents: [
+			{ title: "Getting started", href: "/" },
+			{ title: "Installation", href: "/" },
+		],
+	},
+	{
+		title: "4",
+		documents: [
+			{ title: "Getting started", href: "/" },
+			{ title: "Installation", href: "/" },
+		],
+	},
+	{
+		title: "5",
+		documents: [
+			{ title: "Getting started", href: "/" },
+			{ title: "Installation", href: "/" },
+		],
+	},
+	{
+		title: "6",
+		documents: [
+			{ title: "Getting started", href: "/" },
+			{ title: "Installation", href: "/" },
+		],
+	},
+	{
+		title: "7",
+		documents: [
+			{ title: "Getting started", href: "/" },
+			{ title: "Installation", href: "/" },
+		],
+	},
+	{
+		title: "8",
+		documents: [
+			{ title: "Getting started", href: "/" },
+			{ title: "Installation", href: "/" },
+		],
+	},
+];
