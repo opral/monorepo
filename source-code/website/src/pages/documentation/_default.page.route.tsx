bbad543c6e1ab0333af67b9a8f93e0a37656cbf8
jannesblobel <72493222+jannesblobel@users.noreply.github.com>
2022-11-06 01:22:22 +0100

    try to set a default page( Routing for inlang/documentation to /documentation/intro

---
+++ source-code/website/src/pages/documentation/_default.page.route.tsx
@@ -0,0 +1,4 @@
+export const filesystemRoutingRoot = "/documentation/";
+//https://vite-plugin-ssr.com/file-structure#domain-driven
+// Das läd eine Default Seite aber dafür läd der Rest nicht mehr
+// export const filesystemRoutingRoot = "/";
