b9ae76200c4e33a8c259d7f6d764577d90ea785a
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2022-11-06 14:37:03 +0100

    add comment

--- source-code/website/src/pages/documentation/index.page.tsx
+++ source-code/website/src/pages/documentation/index.page.tsx
@@ -1,6 +1,7 @@
 import { onMount } from "solid-js";
 
 export function Page() {
+	// onMount because window is not available in a server-side environment....
 	onMount(() => {
 		// server side routing trigger
 		// see https://vite-plugin-ssr.com/navigate#page-content
