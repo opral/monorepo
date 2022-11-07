160b157ba5b5e1971608b804d66aab75b10d8a0f
jannesblobel <72493222+jannesblobel@users.noreply.github.com>
2022-11-07 13:57:40 +0100

    fix  padding

--- packages/website/src/pages/documentation/@id/Footer.tsx
+++ packages/website/src/pages/documentation/@id/Footer.tsx
@@ -2,7 +2,7 @@ export function Footer() {
 	return (
 		<footer
 			aria-labelledby="footer-heading"
-			class="  inset-x-0 bottom-0 z-40 bg-background border-t border-outline-variant	"
+			class="  inset-x-0 bottom-0 z-40 bg-background border-t border-outline-variant sm:px-6 md:px-0	"
 		>
 			<h2 id="footer-heading" class="sr-only">
 				Footer
