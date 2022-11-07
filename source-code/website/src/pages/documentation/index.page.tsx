ce5ba04f4de7eb64abb7610c05d3589ae86411d9
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2022-11-07 12:05:57 +0100

    refactor: use vps fixed navigate
    See https://github.com/brillout/vite-plugin-ssr/issues/502

--- source-code/website/src/pages/documentation/index.page.tsx
+++ source-code/website/src/pages/documentation/index.page.tsx
@@ -1,10 +1,9 @@
 import { onMount } from "solid-js";
+import { navigate } from "vite-plugin-ssr/client/router";
 
 export function Page() {
-	// onMount because window is not available in a server-side environment....
 	onMount(() => {
-		// server side routing trigger
-		// see https://vite-plugin-ssr.com/navigate#page-content
-		window.location.href = "documentation/intro";
+		// redirect
+		navigate("/documentation/intro");
 	});
 }
