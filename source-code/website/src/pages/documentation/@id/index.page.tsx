5e93e53d626d577e503c201368906edb41abcc20
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2022-11-08 09:56:06 +0100

    refactor: extract layout

--- source-code/website/src/pages/documentation/@id/index.page.tsx
+++ source-code/website/src/pages/documentation/@id/index.page.tsx
@@ -1,8 +1,7 @@
-import { Header } from "./Header.jsx";
 import { Navigation } from "./Navigation.jsx";
 import { tableOfContent } from "./tableOfContent.js";
 import type { PageHead } from "@src/renderer/types.js";
-import { Footer } from "./Footer.jsx";
+import { Layout } from "@src/pages/Layout.jsx";
 
 export const Head: PageHead = () => {
 	return {
@@ -17,8 +16,7 @@ export type PageProps = {
 
 export function Page(props: PageProps) {
 	return (
-		<>
-			<Header />
+		<Layout>
 			<div class="mx-auto max-w-screen-2xl  sm:px-6 md:px-0  ">
 				<div class=" flex ">
 					<Navigation sections={tableOfContent} />
@@ -30,8 +28,6 @@ export function Page(props: PageProps) {
 					)}
 				</div>
 			</div>
-
-			<Footer />
-		</>
+		</Layout>
 	);
 }
