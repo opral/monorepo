2140f2edd035bbcfaa109c823be565a742a12699
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2022-11-11 15:03:35 +0100

    add: dynamic color classes

--- packages/website/tailwind.config.cjs
+++ packages/website/tailwind.config.cjs
@@ -9,6 +9,7 @@ module.exports = {
 		// no tailwind colors. use design system colors
 		colors: {},
 	},
+	safelist: usedClassWithDynamicColor(),
 	plugins: [
 		require("@tailwindcss/typography"),
 		require("@tailwindcss/forms"),
@@ -33,3 +34,40 @@ module.exports = {
  *
  * Hardcoded for now. Implementation can be found in `design-system/src/color-system/tailwindPlugin.cts`
  */
+
+/**
+ * finds dynamic classes that need to be whitelisted for tailwind css
+ */
+function usedClassWithDynamicColor() {
+	const designSystemColors = ["primary", "secondary", "tertiary", "error"];
+
+	const result = [];
+	const fs = require("fs");
+	const glob = require("fast-glob");
+	const files = glob.sync("src/**/*.{js,ts,jsx,tsx}");
+	for (const file of files) {
+		const content = fs.readFileSync(file, "utf-8");
+		// match everything with a `-` before string interpolation
+		// like `bg-${props.color}` or `bg-on-${color}`
+		const matches = content.match(/([^\s]*-\$\{(props.color|color)\})/g);
+		if (matches) {
+			// iterate all possible color class possibilities
+			// and push them to result
+			for (const match of matches) {
+				if (match.includes("props.color")) {
+					result.push(
+						designSystemColors.map((color) => {
+							match.replace("${props.color}", color);
+						})
+					);
+				} else {
+					result.push(
+						designSystemColors.map((color) => {
+							match.replace("${color}", color);
+						})
+					);
+				}
+			}
+		}
+	}
+}
