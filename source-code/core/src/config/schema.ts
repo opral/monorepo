fc241b49368918d022f5cadf18468bc4133680de
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2022-11-12 16:52:54 +0100

    refactor: remove ideExtension for now

--- source-code/core/src/config/schema.ts
+++ source-code/core/src/config/schema.ts
@@ -35,40 +35,40 @@ export type Config = {
 		fs: FsPromisesSubset;
 		$import: typeof $import;
 	}) => Promise<ast.Bundle[]>;
-	ideExtension: {
-		/**
-		 * Defines when a message is referenced.
-		 */
-		inlinePatternMatcher: (args: {
-			/**
-			 * The (code) text to match against.
-			 */
-			text: string;
-			$import: typeof $import;
-		}) => Promise<
-			Array<{
-				/**
-				 * The ID of the message.
-				 */
-				id: string;
-				/**
-				 * The position from where to where the pattern should be inlined.
-				 */
-				position: {
-					start: { line: number; character: number };
-					end: { line: number; character: number };
-				};
-			}>
-		>;
-		extractMessageReplacementOptions: (args: { id: string }) => string[];
-		/**
-		 * An array of VSCode DocumentSelectors.
-		 *
-		 * The document selectors specify for which files/programming languages
-		 * (typescript, svelte, etc.) the extension should be activated.
-		 *
-		 * See https://code.visualstudio.com/api/references/document-selector
-		 */
-		documentSelectors: DocumentSelector[];
-	};
+	// ideExtension?: {
+	// 	/**
+	// 	 * Defines when a message is referenced.
+	// 	 */
+	// 	inlinePatternMatcher: (args: {
+	// 		/**
+	// 		 * The (code) text to match against.
+	// 		 */
+	// 		text: string;
+	// 		$import: typeof $import;
+	// 	}) => Promise<
+	// 		Array<{
+	// 			/**
+	// 			 * The ID of the message.
+	// 			 */
+	// 			id: string;
+	// 			/**
+	// 			 * The position from where to where the pattern should be inlined.
+	// 			 */
+	// 			position: {
+	// 				start: { line: number; character: number };
+	// 				end: { line: number; character: number };
+	// 			};
+	// 		}>
+	// 	>;
+	// 	extractMessageReplacementOptions: (args: { id: string }) => string[];
+	// 	/**
+	// 	 * An array of VSCode DocumentSelectors.
+	// 	 *
+	// 	 * The document selectors specify for which files/programming languages
+	// 	 * (typescript, svelte, etc.) the extension should be activated.
+	// 	 *
+	// 	 * See https://code.visualstudio.com/api/references/document-selector
+	// 	 */
+	// 	documentSelectors: DocumentSelector[];
+	// };
 };
