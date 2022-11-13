a116703de28834aa17d59eda7f029ceaf23d32ae
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2022-11-13 17:28:29 +0100

    add different sizes

--- source-code/website/src/components/Button.tsx
+++ source-code/website/src/components/Button.tsx
@@ -1,18 +1,26 @@
 import type { DesignSystemColors } from "../../tailwind.config.cjs";
 import type { JSX } from "solid-js";
 
-type ButtonArgs = {
+type ButtonProps = {
 	color: DesignSystemColors[number];
 	variant: "fill";
+	// defaults to base. is optional to avoid merging of default props.
+	size?: "xs" | "sm" | "base" | "lg" | "xl";
 };
 
+/**
+ * Button component.
+ */
 export function Button(
-	props: ButtonArgs &
+	props: ButtonProps &
 		JSX.ButtonHTMLAttributes<HTMLButtonElement> & {
 			children?: JSX.Element;
 		}
 ) {
-	return <button class={buttonStyle(props)} {...props}></button>;
+	return (
+		// using classList instead of class avoids merge bugs between props.class and buttonStyle
+		<button classList={{ [buttonStyle(props)]: true }} {...props}></button>
+	);
 }
 
 /**
@@ -25,15 +33,23 @@ export function Button(
  * 	<a class={buttonStyle()}></a>
  *
  */
-export function buttonStyle(args: ButtonArgs) {
-	return baseStyle + " " + variantStyle(args);
+export function buttonStyle(args: ButtonProps) {
+	return `${baseStyle} ${size[args.size ?? "base"]} ${variantStyle(args)}`;
 }
 
 /** the base style of the button that is consistent across all buttons, regardless of the variant */
 const baseStyle =
-	"rounded py-2 px-4 text-sm font-semibold focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 disabled:cursor-not-allowed";
+	"inline-flex shrink items-center justify-center border border-transparent font-semibold focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 disabled:cursor-not-allowed";
 
-function variantStyle(args: ButtonArgs): string {
+const size = {
+	xs: "px-2.5 py-1.5 text-xs rounded",
+	sm: "px-3 py-2 text-sm rounded-md",
+	base: "px-4 py-2 text-sm rounded-md",
+	lg: "px-4 py-2 test-base rounded-md",
+	xl: "px-6 py-3 text-base rounded-md md:px-8 md:py-4 md:text-lg",
+};
+
+function variantStyle(args: ButtonProps): string {
 	switch (args.variant) {
 		case "fill":
 			return `disabled:bg-disabled-container disabled:text-disabled-content bg-${args.color} text-on-${args.color} hover:bg-hover-${args.color} active:bg-active-${args.color}`;
