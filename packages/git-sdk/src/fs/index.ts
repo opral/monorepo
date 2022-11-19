be491259010ffbf504bf4eec779ca94f17c9a019
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2022-11-19 21:37:47 +0100

    Revert "refactor: only export promise based fs"
    This reverts commit 3497d0a0848b59b37e7b84f3115d2c237d31fd7d.

--- packages/git-sdk/src/fs/index.ts
+++ packages/git-sdk/src/fs/index.ts
@@ -1,12 +1,1 @@
-import { fs as memfs } from "memfs";
-
-/**
- * Filesystem that works in the browser and node.
- *
- * Right now, this is a wrapper around memfs/promises.
- * Promises are required in the browser to not block
- * the main thread. In the future, this will likely
- * be replaced with a custom implementation that
- * requires no polyfills.
- */
-export const fs = memfs.promises;
+export { fs } from "memfs";
