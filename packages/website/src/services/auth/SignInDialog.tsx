c3eaf769bd3200507c36bb853f9cef1871a0ff1f
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2022-12-08 16:07:05 +0100

    add: sign in

--- packages/website/src/services/auth/SignInDialog.tsx
+++ packages/website/src/services/auth/SignInDialog.tsx
@@ -1,16 +1,27 @@
 import type { ClientSideEnv } from "@env";
+import type SlDialog from "@shoelace-style/shoelace/dist/components/dialog/dialog.js";
 import IconGithub from "~icons/cib/github";
 import { githubAuthUrl } from "./logic.js";
 
 /**
  * A dialog that prompts the user to login with GitHub.
+ *
+ * @example
+ * 	let signInDialog: SlDialog | undefined;
+ *
+ * 	function onX() {
+ * 		signInDialog?.show();
+ * 	}
  */
-export function LoginDialog(props: {
+export function SignInDialog(props: {
+	/** forwarding the ref */
+	ref: SlDialog;
 	githubAppClientId: ClientSideEnv["VITE_GITHUB_APP_CLIENT_ID"];
 }) {
 	return (
-		<sl-dialog prop:open={true}>
+		<sl-dialog ref={props.ref}>
 			<sl-button
+				slot="footer"
 				prop:target="_blank"
 				prop:href={githubAuthUrl(props.githubAppClientId)}
 			>
