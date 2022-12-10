9596f1b01ebea59b8615f20acecde434327acff4
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2022-12-10 17:50:38 +0100

    init git-proxy

---
+++ packages/website/src/server/git-proxy.ts
@@ -0,0 +1,9 @@
+/**
+ * ------------------------------------
+ * The git proxy routes and authenticates requests
+ * to git hosts like GitHub and GitLab.
+ *
+ * The proxy exists to avoid CORS issues and authenticate
+ * requests.
+ * ------------------------------------
+ */
