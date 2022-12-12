bf7faf06be91ca7688c62f94e124a7fcf230803f
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2022-12-12 18:29:34 +0100

    add: assert

---
+++ source-code/website/src/services/assert/index.ts
@@ -0,0 +1,23 @@
+/**
+ * Asserts a condition in development environment.
+ *
+ * Use `assert` to verify the correct usage of code by other developers.
+ * Similar to Node.js assert and Dart's https://dart.dev/guides/language/language-tour#assert.
+ * If the condition is false, an error is thrown during development. In production,
+ * assert calls are removed and, therefore, have no performance error error implication.
+ *
+ * @example
+ *  function x(argument: string){
+ *   assert(argument.startsWith("hello"), "The argument must start with 'hello'. Otherwise, the function crashes.")
+ *  }
+ */
+export function assert(
+	condition: unknown,
+	message?: string
+): asserts condition {
+	// if production environment, do nothing -> vite will remove assert calls in production
+	if (import.meta.env?.PROD) return;
+	if (!condition) {
+		throw new Error(message);
+	}
+}
