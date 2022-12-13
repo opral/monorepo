ace46a36e7872149c9350f24d756fdf10b75a7b5
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2022-12-13 21:41:16 +0100

    add: success info on login

--- packages/website/src/services/auth/oauth-redirect.page.tsx
+++ packages/website/src/services/auth/oauth-redirect.page.tsx
@@ -1,9 +1,11 @@
 import { Match, onMount, Switch } from "solid-js";
-import { navigate } from "vite-plugin-ssr/client/router";
 import {
 	useLocalStorage,
 	type LocalStorageSchema,
 } from "@src/services/local-storage/index.js";
+import { Layout } from "@src/pages/Layout.jsx";
+import MaterialSymbolsCheckCircleRounded from "~icons/material-symbols/check-circle-rounded";
+import MaterialSymbolsArrowBackRounded from "~icons/material-symbols/arrow-back-rounded";
 
 export type PageProps = {
 	error?: string;
@@ -24,25 +26,43 @@ export function Page(props: PageProps) {
 	const [localStorage, setLocalStorage] = useLocalStorage();
 
 	onMount(() => {
-		if (props.error) {
-			alert(props.error);
-		} else if (props.data) {
+		if (props.data) {
 			setLocalStorage("user", props.data.user);
-			navigate("/");
 		}
 	});
 
 	return (
-		<Switch>
-			<Match when={props.data}>
-				<p>success, redirecting ...</p>
-			</Match>
-			<Match when={props.error}>
-				<div class="text-on-danger-container p-2 bg-danger-container">
-					<p>An error occured during login. Please report the bug.</p>
-					<p>{props.error}</p>
-				</div>
-			</Match>
-		</Switch>
+		<Layout>
+			<Switch>
+				<Match when={props.data}>
+					<div class="items-center justify-center flex grow">
+						<div class="flex flex-col border rounded-lg border-outline p-10 max-w-sm">
+							<MaterialSymbolsCheckCircleRounded class="text-success w-16 h-16 self-center"></MaterialSymbolsCheckCircleRounded>
+							<h2 class="text-xl font-medium self-center pt-2">
+								Successfully logged in
+							</h2>
+							<p class="self-center">You can close this window.</p>
+							<sl-button
+								class="pt-6"
+								prop:variant="primary"
+								onClick={() => {
+									// this pattern will break if the last opened window is not the login window
+									window.close();
+								}}
+							>
+								<MaterialSymbolsArrowBackRounded slot="prefix"></MaterialSymbolsArrowBackRounded>
+								Return to app
+							</sl-button>
+						</div>
+					</div>
+				</Match>
+				<Match when={props.error}>
+					<div class="text-on-danger-container p-2 bg-danger-container">
+						<p>An error occured during login. Please report the bug.</p>
+						<p>{props.error}</p>
+					</div>
+				</Match>
+			</Switch>
+		</Layout>
 	);
 }
