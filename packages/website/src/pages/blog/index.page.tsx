ec701efeba7cd0f1014f18151310d63f07acb96d
jannesblobel <72493222+jannesblobel@users.noreply.github.com>
2022-12-14 23:24:01 +0100

    typo: title to subHeadline

--- packages/website/src/pages/blog/index.page.tsx
+++ packages/website/src/pages/blog/index.page.tsx
@@ -14,30 +14,14 @@ export function Page() {
 		<div class=" flex-row min-h-full   items-center justify-center   mx-auto max-w-2xl divide-y divide-solid divide-outline  ">
 			<For each={tableOfContent}>
 				{(section) => (
-					// <sl-card class="card-overview min-h-full w-full">
-					// 	<img
-					// 		slot="image"
-					// 		src={section.imageSrc}
-					// 		alt="A kitten sits patiently between a terracotta pot and decorative grasses."
-					// 	/>
-					// 	<span class="h-48 grow">
-					// 		<strong class=" text-on-surface">{section.title}</strong>
-					// 		<p class=" min-h-full text-overflow: ellipsis;">
-					// 			{section.headline}
-					// 		</p>
-					// 		<a class="link link-primary text-primary" href={section.href}>
-					// 			... read more
-					// 		</a>
-					//space-y-10 md:space-y-12 lg:space-y-14
-					// 	</span>
-					// </sl-card>
-
 					<div class=" py-12">
 						<a href={section.href} class="text-ellipsis  space-y-4  ">
 							<p class="text-xl font-bold tracking-tight text-on-backround truncate ">
 								{section.headline}
 							</p>
-							<p class="">{section.title} adsasd asd asdsadasdsa a dasdsad</p>
+							<p class="">
+								{section.subHeadline} adsasd asd asdsadasdsa a dasdsad
+							</p>
 							<img
 								class="object-contain w-full rounded"
 								src={section.imageSrc}
@@ -49,22 +33,4 @@ export function Page() {
 			</For>
 		</div>
 	);
-}
-
-{
-	/* <style>
-  .card-overview {
-    max-width: 300px;
-  }
-
-  .card-overview small {
-    color: var(--sl-color-neutral-500);
-  }
-
-  .card-overview [slot='footer'] {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-  }
-</style> */
 }
--- packages/website/src/pages/blog/tableOfContent.ts
+++ packages/website/src/pages/blog/tableOfContent.ts
@@ -3,28 +3,29 @@
  */
 export const tableOfContent = [
 	{
-		title: "Introduction",
 		headline: "8 Items muss ich sehen",
+		subHeadline: "Introduction",
 		href: "/blog/getting-started",
 		imageSrc:
 			"https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg",
 	},
 	{
-		title:
-			"qä adasdasdas adasdasdasadasdasdas adasdasdasadasdasdas adasda adasdasdassdasadasdasdasadasdasdasadasdasdas ",
 		headline: "8 Items muss ich sehen",
+
+		subHeadline:
+			"qä adasdasdas adasdasdasadasdasdas adasdasdasadasdasdas adasda adasdasdassdasadasdasdasadasdasdasadasdasdas ",
 		href: "/blog/getting-started",
 		imageSrc: "",
 	},
 	{
 		title: "asäß",
-		headline: "8 Items muss ich sehen",
+		subHeadline: "8 Items muss ich sehen",
 		href: "/blog/getting-started",
 		imageSrc: "https://wallpapercave.com/wp/PPMnOZM.jpg",
 	},
 	{
 		title: "aßäßä",
-		headline:
+		subHeadline:
 			"8 Items muss ich sehesdfdsfdsdsfsdfsdfdsb ffsdfsfsdsfdsdfa sdasdasasdasdsfaasdsads aadsadassdasdn",
 		href: "/blog/getting-started",
 		imageSrc:
@@ -32,7 +33,7 @@ export const tableOfContent = [
 	},
 	{
 		title: "aßäßä",
-		headline: "8 Items muss ich sehesdfdsfdsdsfsdfsdfdsb ffsdfsfsdsfdsdfsdn",
+		subHeadline: "8 Items muss ich sehesdfdsfdsdsfsdfsdfdsb ffsdfsfsdsfdsdfsdn",
 		href: "/blog/getting-started",
 		imageSrc:
 			"https://images.unsplash.com/photo-1559209172-0ff8f6d49ff7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80",
