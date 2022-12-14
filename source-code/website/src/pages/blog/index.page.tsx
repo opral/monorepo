84b0e07c89c9a4f8d2bd0484bd10edadd3140697
jannesblobel <72493222+jannesblobel@users.noreply.github.com>
2022-12-14 22:12:37 +0100

    add  h1 and p Tag witch content

--- source-code/website/src/pages/blog/index.page.tsx
+++ source-code/website/src/pages/blog/index.page.tsx
@@ -11,25 +11,32 @@ import { tableOfContent } from "./tableOfContent.js";
 // }
 export function Page() {
 	return (
-		<div class=" min-h-full min-w-full items-center  space-y-4 md:space-y-0 py-4 md:grid gap-4 md:grid-cols-4  ">
+		<div class=" flex-row min-h-full min-w-full items-center justify-center  space-y-4 md:space-y-0 py-4 mx-auto  ">
 			<For each={tableOfContent}>
 				{(section) => (
-					<sl-card class="card-overview min-h-full w-full">
-						<img
-							slot="image"
-							src={section.imageSrc}
-							alt="A kitten sits patiently between a terracotta pot and decorative grasses."
-						/>
-						<span class="h-48 grow">
-							<strong class=" text-on-surface">{section.title}</strong>
-							<p class=" min-h-full text-overflow: ellipsis;">
-								{section.headline}
-							</p>
-							<a class="link link-primary text-primary" href={section.href}>
-								... read more
-							</a>
-						</span>
-					</sl-card>
+					// <sl-card class="card-overview min-h-full w-full">
+					// 	<img
+					// 		slot="image"
+					// 		src={section.imageSrc}
+					// 		alt="A kitten sits patiently between a terracotta pot and decorative grasses."
+					// 	/>
+					// 	<span class="h-48 grow">
+					// 		<strong class=" text-on-surface">{section.title}</strong>
+					// 		<p class=" min-h-full text-overflow: ellipsis;">
+					// 			{section.headline}
+					// 		</p>
+					// 		<a class="link link-primary text-primary" href={section.href}>
+					// 			... read more
+					// 		</a>
+					// 	</span>
+					// </sl-card>
+					<div>
+						<h1 class=" text-xl font-bold tracking-tight text-on-backround  ">
+							{" "}
+							{section.headline}
+						</h1>
+						<p> {section.title}</p>
+					</div>
 				)}
 			</For>
 		</div>
