9763571959abf0ae169474bc4cf53480ac7ff5b5
jannesblobel <72493222+jannesblobel@users.noreply.github.com>
2022-12-17 15:46:10 +0100

    internship mistake

--- packages/website/src/pages/blog/index.page.tsx
+++ packages/website/src/pages/blog/index.page.tsx
@@ -1,7 +1,7 @@
 // import { onMount } from "solid-js";
 // import { navigate } from "vite-plugin-ssr/client/router";
 import { For } from "solid-js";
-import { tableOfContent } from "../../../../../../content/blog/tableOfContent.js";
+import { tableOfContents } from "../../../../../content/blog/tableOfContents.js";
 
 // export function Page() {
 // 	onMount(() => {
@@ -12,17 +12,17 @@ import { tableOfContent } from "../../../../../../content/blog/tableOfContent.js
 export function Page() {
 	return (
 		<div class="flex-row min-h-full w-full items-center justify-center mx-auto md:max-w-2xl divide-y divide-solid divide-outline">
-			<For each={tableOfContent}>
-				{(section) => (
+			<For each={Object.entries(tableOfContents)}>
+				{([id, section]) => (
 					<div class="py-12">
-						<a href={section.href} class="text-ellipsis space-y-4">
+						<a href={"/blog/" + id} class="text-ellipsis space-y-4">
 							<p class="text-xl font-bold tracking-tight text-on-backround truncate">
 								{section.headline}
 							</p>
 							<p class="">{section.subHeadline}</p>
 							<img
 								class="object-contain w-full rounded"
-								src={section.imageSrc}
+								src={section.previewImageSrc}
 							/>
 							{/* using link-primary and text-primary to render the link color by default in primary 
 							but also get hover effects from link-primary */}
