660c6ccb32e0baadf4157e1b14a2483a9a89795f
jannesblobel <72493222+jannesblobel@users.noreply.github.com>
2022-12-17 23:44:51 +0100

    refactor: mobile nav

--- source-code/website/src/pages/documentation/@id/Navigation.tsx
+++ source-code/website/src/pages/documentation/@id/Navigation.tsx
@@ -1,45 +1,58 @@
 import { currentPageContext } from "@src/renderer/state.js";
 import { For } from "solid-js";
-
-export function Navigation(props: {
-	sections: Array<{
-		title: string;
-		documents: Array<{ href: string; title: string }>;
-	}>;
-}) {
+import { tableOfContents } from "./tableOfContents.js";
+export function Navigation() {
 	return (
-		<nav class="sticky top-[3.5rem] h-[calc(100vh-4.5rem)] overflow-y-auto  md:block w-full hidden">
-			<ul role="list" class="divide-y divide-outline pr-4 min-h-full ">
-				<For each={props.sections}>
-					{(section) => (
-						<li class="py-3">
-							<h2 class="font-bold text-on-surface pb-3">{section.title}</h2>
-							<ul class="space-y-1.5" role="list">
-								<For each={section.documents}>
-									{(document) => (
-										<li>
-											<a
-												class="block w-full font-medium link link-primary"
-												classList={{
-													"text-primary":
-														document.href ===
-														currentPageContext().urlParsed.pathname,
-													"text-on-surface-variant":
-														document.href !==
-														currentPageContext().urlParsed.pathname,
-												}}
-												href={document.href}
-											>
-												{document.title}
-											</a>
-										</li>
-									)}
-								</For>
-							</ul>
-						</li>
-					)}
-				</For>
-			</ul>
-		</nav>
+		<>
+			{/* desktop navbar */}
+			<nav class="hidden md:block sticky top-[3.5rem] h-[calc(100vh-4.5rem)] overflow-y-auto w-full">
+				<Common></Common>
+			</nav>
+			{/* Mobile navbar */}
+
+			<nav class="sm:hidden overflow-y-auto overflow-auto min-w-full">
+				<sl-details>
+					<h3 slot="summary" class="font-medium">
+						Menu
+					</h3>
+					<Common></Common>
+				</sl-details>
+			</nav>
+		</>
+	);
+}
+function Common() {
+	return (
+		<ul role="list" class="divide-y divide-outline pr-4 min-h-full">
+			<For each={tableOfContents}>
+				{(section) => (
+					<li class="py-3">
+						<h2 class="font-bold text-on-surface pb-3">{section.title}</h2>
+						<ul class="space-y-1.5" role="list">
+							<For each={section.documents}>
+								{(document) => (
+									<li>
+										<a
+											class="block w-full font-medium link link-primary"
+											classList={{
+												"text-primary":
+													document.href ===
+													currentPageContext().urlParsed.pathname,
+												"text-on-surface-variant":
+													document.href !==
+													currentPageContext().urlParsed.pathname,
+											}}
+											href={document.href}
+										>
+											{document.title}
+										</a>
+									</li>
+								)}
+							</For>
+						</ul>
+					</li>
+				)}
+			</For>
+		</ul>
 	);
 }
