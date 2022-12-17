b5ab2f31b5e6ddc0cf223a8366baefb46e5fb52f
jannesblobel <72493222+jannesblobel@users.noreply.github.com>
2022-12-17 14:27:28 +0100

    add

--- source-code/website/src/pages/content/blog/tableOfContent.ts
+++ source-code/website/src/pages/content/blog/tableOfContent.ts
@@ -5,7 +5,7 @@ export const tableOfContent = [
 	{
 		headline: "8 Items muss ich sehen",
 		subHeadline: "Introduction",
-		href: "/blog/getting-started",
+		href: "/content/blog/getting-started",
 		imageSrc:
 			"https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg",
 	},
@@ -14,27 +14,27 @@ export const tableOfContent = [
 
 		subHeadline:
 			"qä adasdasdas adasdasdasadasdasdas adasdasdasadasdasdas adasda adasdasdassdasadasdasdasadasda sdasadasdasdas ",
-		href: "/blog/getting-started",
+		href: "/content/blog/getting-started",
 		imageSrc: "",
 	},
 	{
 		headline: "asäß",
 		subHeadline: "8 Items muss ich sehen",
-		href: "/blog/getting-started",
+		href: "/content/blog/getting-started",
 		imageSrc: "https://wallpapercave.com/wp/PPMnOZM.jpg",
 	},
 	{
 		headline: "aßäßä",
 		subHeadline:
 			"8 Items muss ich sehesdfdsfdsdsfsdfsdfdsb ffsdfsfsdsfdsdfa sdasdasasdasdsfaasdsads aadsadassdasdn",
-		href: "/blog/getting-started",
+		href: "/content/blog/getting-started",
 		imageSrc:
 			"https://images.unsplash.com/photo-1559209172-0ff8f6d49ff7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80",
 	},
 	{
 		headline: "aßäßä",
 		subHeadline: "8 Items muss ich sehesdfdsfdsdsfsdfsdfdsb ffsdfsfsdsfdsdfsdn",
-		href: "/blog/getting-started",
+		href: "/content/blog/getting-started",
 		imageSrc:
 			"https://images.unsplash.com/photo-1559209172-0ff8f6d49ff7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80",
 	},
