47d17ea4003618b10f53ab1fffeb3cd7126f5d93
Samuel Stroschein <proxy@stroschein.me>
2022-12-19 12:01:25 +0000

    improve: avoid flash of content
    The page not returning any markup led to a white page being rendered that is redirect after mount. Rendering the layout improves the user experience by avoiding the rendering of a completely empty page.

--- packages/website/src/pages/documentation/index.page.tsx
+++ packages/website/src/pages/documentation/index.page.tsx
@@ -1,9 +1,11 @@
 import { onMount } from "solid-js";
 import { navigate } from "vite-plugin-ssr/client/router";
+import { Layout } from "@src/pages/Layout.jsx";
 
 export function Page() {
 	onMount(() => {
 		// redirect
 		navigate("/documentation/intro");
 	});
+	return <Layout>{""}</Layout>;
 }
