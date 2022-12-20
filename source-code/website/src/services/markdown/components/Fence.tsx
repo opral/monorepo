20754968b4770be506117bea5c2ee9c091cfb70f
Samuel Stroschein <proxy@stroschein.me>
2022-12-20 14:22:11 +0000

    refactor: syntax highlighter to fence
    Stick to the naming of nodes. A fence block can also contain mermaid diagrams (in the future).

--- source-code/website/src/services/markdown/components/Fence.tsx
+++ source-code/website/src/services/markdown/components/Fence.tsx
@@ -2,7 +2,15 @@ import { getHighlighter } from "shiki";
 
 const highlighter = await getHighlighter({ theme: "dark-plus" });
 
-export function SyntaxHighlight(props: { language?: string; content: string }) {
+/**
+ * Custom fence blocks.
+ *
+ * Fence blocks are usually code blocks.
+ * '''
+ * 		those blocks
+ * '''
+ */
+export function Fence(props: { language?: string; content: string }) {
 	const code = highlighter.codeToHtml(props.content, {
 		lang: props.language,
 	});
