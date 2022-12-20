db28223bafb0e1ce5c83b260f082d1ad6454df9d
Samuel Stroschein <proxy@stroschein.me>
2022-12-20 21:09:21 +0000

    chore: use ts-ignore instead of expect error
    When jumping from vite source code to the tableOfContent no error is shown leading "expect-error" to raise an error.

--- content/blog/tableOfContents.ts
+++ content/blog/tableOfContents.ts
@@ -20,6 +20,6 @@ export const FrontmatterSchema = RequiredFrontmatter.extend({
  * The ordering in the array determines the position in the blog.
  */
 export const tableOfContents: string[] = [
-	// @ts-expect-error
+	// @ts-ignore
 	(await import("./001-git-as-backend/index.md?raw")).default,
 ];
