73728cbc874a11ba473da137e294b71d033572ed
Samuel Stroschein <proxy@stroschein.me>
2022-12-21 18:20:16 +0000

    refactor: render introduction at root of documentation site
    Avoids the need for flaky redirects.

--- packages/website/src/pages/documentation/index.page.tsx
+++ packages/website/src/pages/documentation/index.page.tsx
@@ -1,11 +1,2 @@
-import { onMount } from "solid-js";
-import { navigate } from "vite-plugin-ssr/client/router";
-import { Layout } from "@src/pages/Layout.jsx";
-
-export function Page() {
-	onMount(() => {
-		// redirect
-		navigate("/documentation/introduction");
-	});
-	return <Layout>{""}</Layout>;
-}
+// the rendered page is defined by the href e.g. /documentation
+export { Page } from "./@id/index.page.jsx";
