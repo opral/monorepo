4bc93d3664d84571158bd257590dfbde884d3946
Samuel Stroschein <proxy@stroschein.me>
2022-12-22 17:41:12 +0000

    update comment

--- packages/core/src/config/environment-functions/$fs.ts
+++ packages/core/src/config/environment-functions/$fs.ts
@@ -1,8 +1,9 @@
 import type { fs } from "memfs";
 
 /**
- * $fs is fs/promises from memfs.
+ * Reflects the node:fs/promises API.
  *
- * Promised based file system is required to not block the main thread.
+ * Uses [memfs](https://github.com/streamich/memfs) under the hood. A
+ * promise based file system is required to not block the main thread.
  */
 export type $fs = typeof fs.promises;
