b9e6899b1427cb52302f2f9a4f782d9b7da2f653
Samuel Stroschein <proxy@stroschein.me>
2022-12-22 19:49:17 +0000

    redesign: responsive side nav

--- source-code/website/src/pages/documentation/*/index.page.tsx
+++ source-code/website/src/pages/documentation/*/index.page.tsx
@@ -1,9 +1,9 @@
-import { Navigation } from "./Navigation.jsx";
 import type { PageHead } from "@src/renderer/types.js";
-import { Show } from "solid-js";
+import { For, onMount, Show } from "solid-js";
 import { Layout as RootLayout } from "@src/pages/Layout.jsx";
 import type { parseMarkdown } from "@src/services/markdown/index.js";
 import type { ProcessedTableOfContents } from "./index.page.server.jsx";
+import { currentPageContext } from "@src/renderer/state.js";
 
 export const Head: PageHead<PageProps> = (props) => {
 	return {
@@ -23,25 +23,80 @@ export type PageProps = {
 export function Page(props: PageProps) {
 	return (
 		<RootLayout>
-			<div class="sm:grid sm:grid-cols-9 gap-10 w-full">
-				<div class="sm:flex col-span-2 sticky top-0">
-					{/* `Show` is a hotfix when client side rendering loaded this page
-					 * filteredTableContents is not available on the client.
-					 */}
-					<Show when={props.processedTableOfContents}>
-						<Navigation {...props} />
-					</Show>
-				</div>
+			{/* important: the responsive breakpoints must align throughout the markup! */}
+			<div class="flex flex-col md:grid md:grid-cols-4 gap-10 w-full">
+				{/* desktop navbar */}
+				{/* 
+					hacking the left margins to apply bg-surface-2 with 100rem 
+				    (tested on an ultrawide monitor, works!) 
+				*/}
+				<nav class="hidden md:block -ml-[100rem] pl-[100rem] bg-surface-2 py-4 pr-8">
+					<NavbarCommon {...props}></NavbarCommon>
+				</nav>
+				{/* Mobile navbar */}
+				<nav class="block md:hidden overflow-y-auto overflow-auto min-w-full pt-5">
+					<sl-details>
+						<h3 slot="summary" class="font-medium">
+							Menu
+						</h3>
+						<NavbarCommon {...props}></NavbarCommon>
+					</sl-details>
+				</nav>
 				<Show
 					when={props.markdown?.html}
 					fallback={<p class="text-danger">{props.markdown?.error}</p>}
 				>
 					<div
-						class="pt-6 w-full sm:col-span-7 prose justify-self-center"
+						// change the col-span to 2 if a right side nav bar should be rendered
+						class="w-full prose justify-self-center md:pt-6 md:col-span-3"
 						innerHTML={props.markdown?.html}
 					></div>
 				</Show>
 			</div>
 		</RootLayout>
+	);
+}
+
+function NavbarCommon(props: {
+	processedTableOfContents: PageProps["processedTableOfContents"];
+}) {
+	return (
+		<ul role="list" class="divide-y divide-outline w-full">
+			<For each={Object.keys(props.processedTableOfContents)}>
+				{(section) => (
+					<li class="py-3">
+						<h2 class="font-bold text-on-surface pb-3">{section}</h2>
+						<ul class="space-y-1.5" role="list">
+							<For
+								each={
+									props.processedTableOfContents[
+										section as keyof typeof props.processedTableOfContents
+									]
+								}
+							>
+								{(document) => (
+									<li>
+										<a
+											class="block w-full font-medium link link-primary"
+											classList={{
+												"text-primary":
+													document.frontmatter.href ===
+													currentPageContext().urlParsed.pathname,
+												"text-on-surface-variant":
+													document.frontmatter.href !==
+													currentPageContext().urlParsed.pathname,
+											}}
+											href={document.frontmatter.href}
+										>
+											{document.frontmatter.title}
+										</a>
+									</li>
+								)}
+							</For>
+						</ul>
+					</li>
+				)}
+			</For>
+		</ul>
 	);
 }
