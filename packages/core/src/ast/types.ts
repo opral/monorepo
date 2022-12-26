707b71a80c76c01a17dd8f84c67037631ce5b6a9
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2022-12-26 17:17:48 +0100

    refactor: remove ISO639-1 string type
    Complicates usage because TypeScript complains about missing "en|de" keys all the time.

--- packages/core/src/ast/types.ts
+++
@@ -1,8 +0,0 @@
-import type { iso639Codes } from "./iso639.js";
-
-/**
- * ISO 639-1 language code to identify a human language.
- *
- * https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes
- */
-export type Iso639LanguageCode = typeof iso639Codes[number];
