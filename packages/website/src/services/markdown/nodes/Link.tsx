781509450ca5d5054140cdfc096b1ca02d56b016
Samuel Stroschein <proxy@stroschein.me>
2022-12-29 14:05:35 +0000

    add: link node
    External links are now opening in a new tab and display an arrow in the markup to signal when a link is external.

---
+++ packages/website/src/services/markdown/nodes/Link.tsx
@@ -0,0 +1,34 @@
+import { JSXElement, Match, Switch } from "solid-js";
+import MaterialSymbolsArrowOutward from "~icons/material-symbols/arrow-outward";
+
+/**
+ * Custom Link nodes.
+ *
+ * @example
+ *  [title](href)
+ */
+export function Link(props: { href: string; children: JSXElement }) {
+	/** whether a link is an external link */
+	const isExternal = () => props.href.startsWith("http");
+
+	console.log(props);
+
+	return (
+		<Switch>
+			<Match when={isExternal()}>
+				<a
+					href={props.href}
+					target="_blank"
+					class="inline-flex gap-0.5 items-center"
+				>
+					{props.children}
+					{/* using non-rounded version to increase readability on smaller font settings */}
+					<MaterialSymbolsArrowOutward></MaterialSymbolsArrowOutward>
+				</a>
+			</Match>
+			<Match when={isExternal() === false}>
+				<a href={props.href}>{props.children}</a>
+			</Match>
+		</Switch>
+	);
+}
