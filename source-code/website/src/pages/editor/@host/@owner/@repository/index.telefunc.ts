63fbf47c1ed17ac291fb6132471ad09e15ec670f
jannesblobel <72493222+jannesblobel@users.noreply.github.com>
2023-01-04 17:56:10 +0100

    return response

--- source-code/website/src/pages/editor/@host/@owner/@repository/index.telefunc.ts
+++ source-code/website/src/pages/editor/@host/@owner/@repository/index.telefunc.ts
@@ -69,7 +69,7 @@ export async function forkRepository(args: {
 			console.log("you are a collaborator", collaborator);
 		} else if (collaborator === false) {
 			try {
-				const fork = await fetch(
+				const response = await fetch(
 					`https://api.github.com/repos/${args.owner}/${args.repository}/forks`,
 					{
 						method: "POST",
@@ -82,10 +82,11 @@ export async function forkRepository(args: {
 						// }),
 					}
 				);
-				if (fork.ok) {
-					console.log("Repo is forked ", fork.ok);
-					return undefined;
-				} else throw Error(await fork.text());
+				if (response.ok) {
+					console.log("Repo is forked ", response.ok);
+					const json = await response.json();
+					return json;
+				} else throw Error(await response.text());
 			} catch (error) {
 				console.error(error);
 				return console.error(error);
