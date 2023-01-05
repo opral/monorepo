449b839e55373626617c8c32a8f99ed3485b9ced
jannesblobel <72493222+jannesblobel@users.noreply.github.com>
2023-01-05 18:02:05 +0100

    add github services

---
+++ source-code/website/src/services/github/isCollaborator.telefunc.ts
@@ -0,0 +1,33 @@
+import { decryptAccessToken } from "../auth/logic.js";
+import { serverSideEnv } from "@env";
+const env = await serverSideEnv();
+export async function isCollaborator(args: {
+	owner: string;
+	repository: string;
+	encryptedAccessToken: string;
+	username: string;
+}) {
+	try {
+		const decryptedAccessToken = (
+			await decryptAccessToken({
+				jwe: args.encryptedAccessToken,
+				JWE_SECRET_KEY: env.JWE_SECRET_KEY,
+			})
+		).unwrap();
+		const response = await fetch(
+			`https://api.github.com/repos/${args.owner}/${args.repository}/collaborators/${args.username}`,
+			{
+				headers: {
+					Authorization: `Bearer ${decryptedAccessToken}`,
+					"X-GitHub-Api-Version": "2022-11-28",
+				},
+			}
+		);
+		console.log("collaborator", response.ok);
+		//
+		return response.ok;
+	} catch (error) {
+		console.error(error);
+		return error;
+	}
+}
