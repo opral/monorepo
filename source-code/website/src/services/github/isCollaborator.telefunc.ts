930ec4695a11280f6bc9474108ce38ca937630f0
jannesblobel <72493222+jannesblobel@users.noreply.github.com>
2023-01-06 10:59:22 +0100

    Revert "add error handler"
    This reverts commit 16f930445e7f9c49472382281a0815df187d4f2f.

--- source-code/website/src/services/github/isCollaborator.telefunc.ts
+++ source-code/website/src/services/github/isCollaborator.telefunc.ts
@@ -6,32 +6,22 @@ export async function isCollaborator(args: {
 	repository: string;
 	encryptedAccessToken: string;
 	username: string;
-}): Promise<
-	{ type: "success"; isCollaboratorOk: boolean } | { type: "error"; error: any }
-> {
-	{
-		try {
-			const decryptedAccessToken = (
-				await decryptAccessToken({
-					jwe: args.encryptedAccessToken,
-					JWE_SECRET_KEY: env.JWE_SECRET_KEY,
-				})
-			).unwrap();
-			const response = await fetch(
-				`https://api.github.com/repos/${args.owner}/${args.repository}/collaborators/${args.username}`,
-				{
-					headers: {
-						Authorization: `Bearer ${decryptedAccessToken}`,
-						"X-GitHub-Api-Version": "2022-11-28",
-					},
-				}
-			);
-			return {
-				type: "success",
-				isCollaboratorOk: response.ok,
-			};
-		} catch (error) {
-			return { type: "error", error: error };
+}) {
+	const decryptedAccessToken = (
+		await decryptAccessToken({
+			jwe: args.encryptedAccessToken,
+			JWE_SECRET_KEY: env.JWE_SECRET_KEY,
+		})
+	).unwrap();
+	const response = await fetch(
+		`https://api.github.com/repos/${args.owner}/${args.repository}/collaborators/${args.username}`,
+		{
+			headers: {
+				Authorization: `Bearer ${decryptedAccessToken}`,
+				"X-GitHub-Api-Version": "2022-11-28",
+			},
 		}
-	}
+	);
+
+	return response.ok;
 }
