028ed2dc611b1b48dc99e7e057bbc3ef3a9ed932
jannesblobel <72493222+jannesblobel@users.noreply.github.com>
2023-01-08 21:16:31 +0100

    add error handler

--- packages/website/src/services/github/isCollaborator.telefunc.ts
+++ packages/website/src/services/github/isCollaborator.telefunc.ts
@@ -6,22 +6,32 @@ export async function isCollaborator(args: {
 	repository: string;
 	encryptedAccessToken: string;
 	username: string;
-}) {
-	const decryptedAccessToken = (
-		await decryptAccessToken({
-			jwe: args.encryptedAccessToken,
-			JWE_SECRET_KEY: env.JWE_SECRET_KEY,
-		})
-	).unwrap();
-	const response = await fetch(
-		`https://api.github.com/repos/${args.owner}/${args.repository}/collaborators/${args.username}`,
-		{
-			headers: {
-				Authorization: `Bearer ${decryptedAccessToken}`,
-				"X-GitHub-Api-Version": "2022-11-28",
-			},
+}): Promise<
+	{ type: "success"; isCollaboratorOk: boolean } | { type: "error"; error: any }
+> {
+	{
+		try {
+			const decryptedAccessToken = (
+				await decryptAccessToken({
+					jwe: args.encryptedAccessToken,
+					JWE_SECRET_KEY: env.JWE_SECRET_KEY,
+				})
+			).unwrap();
+			const response = await fetch(
+				`https://api.github.com/repos/${args.owner}/${args.repository}/collaborators/${args.username}`,
+				{
+					headers: {
+						Authorization: `Bearer ${decryptedAccessToken}`,
+						"X-GitHub-Api-Version": "2022-11-28",
+					},
+				}
+			);
+			return {
+				type: "success",
+				isCollaboratorOk: response.ok,
+			};
+		} catch (error) {
+			return { type: "error", error: error };
 		}
-	);
-
-	return response.ok;
+	}
 }
