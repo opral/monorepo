c6d79c2cc4208edb708e8ff0b3f62c565d2921d1
jannesblobel <72493222+jannesblobel@users.noreply.github.com>
2023-01-08 21:15:09 +0100

    refactor: remove unnecessary function and restore github function

--- source-code/website/src/pages/editor/@host/@owner/@repository/index.telefunc.ts
+++ source-code/website/src/pages/editor/@host/@owner/@repository/index.telefunc.ts
@@ -6,22 +6,7 @@ import type { LocalStorageSchema } from "@src/services/local-storage/schema.js";
 import { useLocalStorage } from "@src/services/local-storage/LocalStorageProvider.jsx";
 import { response } from "express";
 const env = await serverSideEnv();
-/**
- * decryptAccessToken the AcessToken for Github
- */
-async function AccessToken(args: {
-	encryptedAccessToken: NonNullable<
-		LocalStorageSchema["user"]
-	>["encryptedAccessToken"];
-}) {
-	const decryptedAccessToken = (
-		await decryptAccessToken({
-			jwe: args.encryptedAccessToken,
-			JWE_SECRET_KEY: env.JWE_SECRET_KEY,
-		})
-	).unwrap();
-	return decryptedAccessToken;
-}
+
 /**
  * Translate text using Google Translate.
  */
@@ -59,73 +44,4 @@ export async function onMachineTranslate(args: {
 	} catch (error) {
 		return { error: (error as Error).message };
 	}
-}
-/**
- * Check the collaboration status of a user at a GitHub repo.
- */
-export async function isCollaborator(args: {
-	owner: string;
-	repository: string;
-	encryptedAccessToken: string;
-	username: string;
-}) {
-	const decryptedAccessToken = await AccessToken({
-		encryptedAccessToken: args.encryptedAccessToken,
-	});
-	try {
-		const response = await fetch(
-			`https://api.github.com/repos/${args.owner}/${args.repository}/collaborators/${args.username}`,
-			{
-				headers: {
-					Authorization: `Bearer ${decryptedAccessToken}`,
-					"X-GitHub-Api-Version": "2022-11-28",
-				},
-			}
-		);
-		console.log("collaborator", response.ok);
-		return response.ok;
-	} catch (error) {
-		console.error(error);
-	}
-	return;
-}
-
-/**
- * Forked a Github Repo
- */
-export async function onForkRepository(args: {
-	encryptedAccessToken: NonNullable<
-		LocalStorageSchema["user"]
-	>["encryptedAccessToken"];
-	username: NonNullable<LocalStorageSchema["user"]>["username"];
-	owner: string;
-	repository: string;
-}) {
-	const decryptedAccessToken = await AccessToken({
-		encryptedAccessToken: args.encryptedAccessToken,
-	});
-
-	try {
-		const response = await fetch(
-			`https://api.github.com/repos/${args.owner}/${args.repository}/forks`,
-			{
-				method: "POST",
-				headers: {
-					Authorization: `Bearer ${decryptedAccessToken}`,
-					"X-GitHub-Api-Version": "2022-11-28",
-				},
-				// body: JSON.stringify({
-				// 	name: `inlangTranslationFor-${args.owner}-${args.repository}`,
-				// }),
-			}
-		);
-		if (response.ok) {
-			const json = await response.json();
-			return json;
-		} else throw Error(await response.text());
-	} catch (error) {
-		console.error(error);
-		return console.error(error);
-	}
-	return;
 }
