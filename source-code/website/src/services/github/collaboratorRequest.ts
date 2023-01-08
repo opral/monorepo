e9fd602cacf1edd44bf32bf528e5f11813040428
jannesblobel <72493222+jannesblobel@users.noreply.github.com>
2023-01-08 21:12:08 +0100

    add collaboraoter fetch for repos

---
+++ source-code/website/src/services/github/collaboratorRequest.ts
@@ -0,0 +1,23 @@
+import { Result } from "@inlang/core/utilities";
+
+export async function isCollaborator(args: {
+	owner: string;
+	repository: string;
+	decryptedAccessToken: string;
+	username: string;
+}): Promise<Result<boolean, Error>> {
+	try {
+		const collaborator = await fetch(
+			`https://api.github.com/repos/${args.owner}/${args.repository}/collaborators/${args.username}`,
+			{
+				headers: {
+					Authorization: `Bearer ${args.decryptedAccessToken}`,
+				},
+			}
+		);
+		console.log(collaborator.ok);
+		return Result.ok(collaborator.ok);
+	} catch (error) {
+		return Result.err(error as Error);
+	}
+}
