d7ad22b7773824d564871349bf121955d88ef6a0
jannesblobel <72493222+jannesblobel@users.noreply.github.com>
2023-01-08 22:15:58 +0100

    add PR banner

--- source-code/website/src/pages/editor/@host/@owner/@repository/Layout.tsx
+++ source-code/website/src/pages/editor/@host/@owner/@repository/Layout.tsx
@@ -297,6 +297,26 @@ function SignInBanner() {
 						</sl-button>
 					</Banner>
 				</Match>
+				<Match when={(unpushedChanges()?.length ?? 0 > 0) && _isFork}>
+					<Banner
+						variant="success"
+						message={`
+							Pull Request	
+								`}
+					>
+						<sl-button onClick={handleFork} prop:variant="primary">
+							<div slot="prefix">
+								<svg width="1.2em" height="1.2em" viewBox="0 0 16 16">
+									<path
+										fill="currentColor"
+										d="M5 5.372v.878c0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75v-.878a2.25 2.25 0 1 1 1.5 0v.878a2.25 2.25 0 0 1-2.25 2.25h-1.5v2.128a2.251 2.251 0 1 1-1.5 0V8.5h-1.5A2.25 2.25 0 0 1 3.5 6.25v-.878a2.25 2.25 0 1 1 1.5 0ZM5 3.25a.75.75 0 1 0-1.5 0a.75.75 0 0 0 1.5 0Zm6.75.75a.75.75 0 1 0 0-1.5a.75.75 0 0 0 0 1.5Zm-3 8.75a.75.75 0 1 0-1.5 0a.75.75 0 0 0 1.5 0Z"
+									></path>
+								</svg>
+							</div>
+							Fork this project
+						</sl-button>
+					</Banner>
+				</Match>
 			</Switch>
 
 			<SignInDialog
