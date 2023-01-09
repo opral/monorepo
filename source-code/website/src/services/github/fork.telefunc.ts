36eda77af4302a9c01c35ee89c811cdf1304acfd
jannesblobel <72493222+jannesblobel@users.noreply.github.com>
2023-01-09 01:44:16 +0100

    add a syncFork function

--- source-code/website/src/services/github/fork.telefunc.ts
+++ source-code/website/src/services/github/fork.telefunc.ts
@@ -49,22 +49,22 @@ export async function onFork(args: {
 		return { type: "error", error: error };
 	}
 }
-
+/**
+ * You can get more information than just the forking Information. Infos Like: Main repo, owner, updated at ....
+ * https://docs.github.com/en/rest/repos/repos?apiVersion=2022-11-28#get-a-repository
+ */
 export async function isFork(args: {
 	owner: string;
 	repository: string;
 	encryptedAccessToken: string;
 	username: string;
-}) {
-	// : Promise<
-	// 	| {
-	// 			type: "success";
-	// 			owner: string;
-	// 			repository: string;
-	// 			workingOnFork: boolean;
-	// 	  }
-	// 	| { type: "error"; error: any }
-	// >
+}): Promise<
+	| {
+			type: "success";
+			fork: boolean;
+	  }
+	| { type: "error"; error: any }
+> {
 	try {
 		const decryptedAccessToken = (
 			await decryptAccessToken({
@@ -82,10 +82,71 @@ export async function isFork(args: {
 				},
 			}
 		);
-		const json = await response.json();
-		return json.fork;
+
+		if (response.ok) {
+			const json = await response.json();
+			return {
+				type: "success",
+				fork: json.fork,
+			};
+		} else {
+			throw Error(await response.text());
+		}
 	} catch (error) {
-		console.log(false);
+		return { type: "error", error: error };
+	}
+}
+
+export async function syncFork(args: {
+	owner: string;
+	repository: string;
+	encryptedAccessToken: string;
+	username: string;
+}): Promise<
+	| {
+			type: "success";
+			status: number;
+			message: any;
+	  }
+	| {
+			type: "success";
+			status: number;
+			message: any;
+	  }
+	| { type: "error"; error: any }
+> {
+	try {
+		const decryptedAccessToken = (
+			await decryptAccessToken({
+				jwe: args.encryptedAccessToken,
+				JWE_SECRET_KEY: env.JWE_SECRET_KEY,
+			})
+		).unwrap();
+		const response = await fetch(
+			`https://api.github.com/repos/${args.owner}/${args.repository}/merge-upstream`,
+			{
+				method: "POST",
+				headers: {
+					Authorization: `Bearer ${decryptedAccessToken}`,
+					"X-GitHub-Api-Version": "2022-11-28",
+				},
+				body: JSON.stringify({
+					branch: "main",
+				}),
+			}
+		);
+		console.log(response.status);
+		if (response.status === 409 || 422 || 200) {
+			const json = await response.json();
+			return {
+				type: "success",
+				status: response.status,
+				message: json.message,
+			};
+		} else {
+			throw Error(await response);
+		}
+	} catch (error) {
 		return { type: "error", error: error };
 	}
 }
--- source-code/website/src/services/github/index.ts
+++ source-code/website/src/services/github/index.ts
@@ -1,3 +1,5 @@
 export { isCollaborator } from "./isCollaborator.telefunc.js";
 export { onFork } from "./fork.telefunc.js";
 export { isFork } from "./fork.telefunc.js";
+
+export { syncFork } from "./fork.telefunc.js";
