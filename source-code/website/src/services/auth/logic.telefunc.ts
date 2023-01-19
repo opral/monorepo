32275afd17aec3eb616b4f9d7dafd4203ce90ca7
Samuel Stroschein <proxy@stroschein.me>
2023-01-19 04:00:08 +0000

    more console logs

--- source-code/website/src/services/auth/logic.telefunc.ts
+++ source-code/website/src/services/auth/logic.telefunc.ts
@@ -11,25 +11,30 @@ import { getContext } from "telefunc";
 export async function getUserInfo(): Promise<
 	LocalStorageSchema["user"] | undefined | string
 > {
-	const context = getContext();
-	if (context.githubAccessToken === undefined) {
-		return undefined;
+	try {
+		const context = getContext();
+		if (context.githubAccessToken === undefined) {
+			return undefined;
+		}
+		console.log("context.githubAccessToken", context.githubAccessToken);
+		const request = await fetch("https://api.github.com/user", {
+			headers: {
+				Accept: "application/vnd.github+json",
+				Authorization: `Bearer ${context.githubAccessToken}`,
+				"X-GitHub-Api-Version": "2022-11-28",
+			},
+		});
+		console.log("request ", request.statusText);
+		if (request.status !== 200) {
+			return "Failed to get user info " + request.statusText;
+		}
+		const requestBody = await request.json();
+		return {
+			username: requestBody.login,
+			avatarUrl: requestBody.avatar_url,
+		};
+	} catch (error) {
+		console.error(error);
+		return "CATCH: Failed to get user info " + error;
 	}
-	console.log("context.githubAccessToken", context.githubAccessToken);
-	const request = await fetch("https://api.github.com/user", {
-		headers: {
-			Accept: "application/vnd.github+json",
-			Authorization: `Bearer ${context.githubAccessToken}`,
-			"X-GitHub-Api-Version": "2022-11-28",
-		},
-	});
-	console.log("request ", request.statusText);
-	if (request.status !== 200) {
-		return "Failed to get user info " + request.statusText;
-	}
-	const requestBody = await request.json();
-	return {
-		username: requestBody.login,
-		avatarUrl: requestBody.avatar_url,
-	};
 }
