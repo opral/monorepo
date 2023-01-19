c109354efb2a9d4b0561e812e53e72fdc1a5b287
jannesblobel <72493222+jannesblobel@users.noreply.github.com>
2023-01-19 20:58:00 +0100

    add Error Message

--- source-code/website/src/renderer/_error.page.tsx
+++ source-code/website/src/renderer/_error.page.tsx
@@ -1,4 +1,6 @@
 import type { PageContextRenderer, PageHead } from "./types.js";
+import MaterialSymbolsUnknownDocumentOutlineRounded from "~icons/material-symbols/unknown-document-outline-rounded";
+import MaterialSymbolsArrowOutwardRounded from "~icons/material-symbols/arrow-outward-rounded";
 
 export const Head: PageHead = (props) => ({
 	title: "inlang Editor",
@@ -8,21 +10,50 @@ export const Head: PageHead = (props) => ({
 
 export function Page(pageContext: PageContextRenderer) {
 	if (pageContext.is404) {
-		console.log(pageContext.is404, "is a ");
-		// Return a UI component "Page Not Found."
 		return (
-			<div class="flex-row min-h-full w-full self-center justify-center mx-auto md:max-w-2xl ">
-				<a href="https://inlang.com/" class="text-ellipsis space-y-4">
-					<h2 class="text-xl font-bold tracking-tight text-on-backround truncate">
-						Page Not Found.
-					</h2>
+			<div class="min-h-screen flex  items-center justify-center">
+				<div class="border border-outline p-8 rounded flex flex-col max-w-lg">
+					<MaterialSymbolsUnknownDocumentOutlineRounded class="w-10 h-10 self-center"></MaterialSymbolsUnknownDocumentOutlineRounded>
+					<h1 class="font-semibold pt-5 text-danger">404</h1>
+					<p class="pt-1.5">
+						Something went wrong, we couldn't find your page.
+					</p>
 
-					<p class="link text-primary link-primary"> 404</p>
-				</a>
+					<a
+						class="self-end pt-5"
+						href="https://inlang.com/documentation/getting-started"
+						target="_blank"
+					>
+						<sl-button prop:variant="text">
+							I need help with getting started
+							<MaterialSymbolsArrowOutwardRounded slot="suffix"></MaterialSymbolsArrowOutwardRounded>
+						</sl-button>
+					</a>
+				</div>
 			</div>
 		);
 	} else {
-		console.log(pageContext.pageProps, "is not 404");
+		return (
+			<div class="min-h-screen flex  items-center justify-center">
+				<div class="border border-outline p-8 rounded flex flex-col max-w-lg">
+					<MaterialSymbolsUnknownDocumentOutlineRounded class="w-10 h-10 self-center"></MaterialSymbolsUnknownDocumentOutlineRounded>
+					<h1 class="font-semibold pt-5 text-danger">Error</h1>
+					<p class="pt-1.5">
+						Our server is having problems. Sincere apologies. Try again later.
+					</p>
+					<a
+						class="self-end pt-5"
+						href="https://github.com/inlang/inlang/issues/new/choose"
+						target="_blank"
+					>
+						<sl-button prop:variant="text">
+							Report a Bug
+							<MaterialSymbolsArrowOutwardRounded slot="suffix"></MaterialSymbolsArrowOutwardRounded>
+						</sl-button>
+					</a>
+				</div>
+			</div>
+		);
 
 		// Return a UI component "Our server is having problems. Sincere apologies. Try again later."
 	}
