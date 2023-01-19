59f3193a2dab7aa52e9d14e293d897aea9b3d19a
jannesblobel <72493222+jannesblobel@users.noreply.github.com>
2023-01-19 23:32:37 +0100

    improve error message

--- source-code/website/src/renderer/_error.page.tsx
+++ source-code/website/src/renderer/_error.page.tsx
@@ -1,6 +1,7 @@
 import type { PageContextRenderer, PageHead } from "./types.js";
 import MaterialSymbolsUnknownDocumentOutlineRounded from "~icons/material-symbols/unknown-document-outline-rounded";
 import MaterialSymbolsArrowOutwardRounded from "~icons/material-symbols/arrow-outward-rounded";
+import { Match, Switch } from "solid-js";
 
 export const Head: PageHead = (props) => ({
 	title: "inlang Editor",
@@ -9,61 +10,44 @@ export const Head: PageHead = (props) => ({
 });
 //TODO add header and footer if its possible
 export function Page(pageContext: PageContextRenderer) {
-	if (pageContext.is404) {
-		return (
-			<div class="min-h-screen flex  items-center justify-center">
-				<div class="border border-outline p-8 rounded flex flex-col max-w-lg">
-					<MaterialSymbolsUnknownDocumentOutlineRounded class="w-10 h-10 self-center"></MaterialSymbolsUnknownDocumentOutlineRounded>
-					<h1 class="font-semibold pt-5 text-danger">404</h1>
-					<p class="pt-1.5">
-						Something went wrong, we couldn't find your page.
-					</p>
+	return (
+		<div class="min-h-screen flex  items-center justify-center">
+			<div class="border border-outline p-8 rounded flex flex-col max-w-lg bg-danger-container">
+				<Switch>
+					<Match when={pageContext.is404}>
+						<MaterialSymbolsUnknownDocumentOutlineRounded class="w-10 h-10 self-center"></MaterialSymbolsUnknownDocumentOutlineRounded>
+						<h1 class="font-semibold pt-5 ">404</h1>
+						<p class="pt-1.5">
+							Something went wrong, we couldn't find your page.
+						</p>
 
-					<a
-						class="self-end pt-5"
-						href="https://inlang.com/documentation/getting-started"
-						target="_blank"
-					>
-						<sl-button prop:variant="text">
-							I need help with getting started
-							<MaterialSymbolsArrowOutwardRounded slot="suffix"></MaterialSymbolsArrowOutwardRounded>
-						</sl-button>
-					</a>
-				</div>
+						<a class="self-end pt-5" href="https://inlang.com">
+							<sl-button prop:variant="text">
+								Back home to inlang.com
+								<MaterialSymbolsArrowOutwardRounded slot="suffix"></MaterialSymbolsArrowOutwardRounded>
+							</sl-button>
+						</a>
+					</Match>
+					<Match when={!pageContext.is404}>
+						<MaterialSymbolsUnknownDocumentOutlineRounded class="w-10 h-10 self-center"></MaterialSymbolsUnknownDocumentOutlineRounded>
+						<h1 class="font-semibold pt-5 ">Error</h1>
+						<p class="pt-1.5">
+							Houston we have a problem. Sincere apologies. Please Try again
+							later and report this Bug.
+						</p>
+						<a
+							class="self-end pt-5"
+							href="https://github.com/inlang/inlang/issues/new/choose"
+							target="_blank"
+						>
+							<sl-button prop:variant="text">
+								Report a Bug
+								<MaterialSymbolsArrowOutwardRounded slot="suffix"></MaterialSymbolsArrowOutwardRounded>
+							</sl-button>
+						</a>
+					</Match>
+				</Switch>
 			</div>
-		);
-	} else {
-		return (
-			<div class="min-h-screen flex  items-center justify-center">
-				<div class="border border-outline p-8 rounded flex flex-col max-w-lg">
-					<MaterialSymbolsUnknownDocumentOutlineRounded class="w-10 h-10 self-center"></MaterialSymbolsUnknownDocumentOutlineRounded>
-					<h1 class="font-semibold pt-5 text-danger">Error</h1>
-					<p class="pt-1.5">
-						Our server is having problems. Sincere apologies. Try again later.
-					</p>
-					<a
-						class="self-end pt-5"
-						href="https://github.com/inlang/inlang/issues/new/choose"
-						target="_blank"
-					>
-						<sl-button prop:variant="text">
-							Report a Bug
-							<MaterialSymbolsArrowOutwardRounded slot="suffix"></MaterialSymbolsArrowOutwardRounded>
-						</sl-button>
-					</a>
-				</div>
-			</div>
-		);
-
-		// Return a UI component "Our server is having problems. Sincere apologies. Try again later."
-	}
+		</div>
+	);
 }
-
-// export function Page(pageProps) {
-// 	console.log(pageProps.is404);
-// 	if (pageProps.is404) {
-// 		// Return a UI component "Page Not Found."
-// 	} else {
-// 		// Return a UI component "Our server is having problems. Sincere apologies. Try again later."
-// 	}
-// }
