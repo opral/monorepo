9c48404c025b0ded88735fc2de2758b6dfa8419a
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2023-01-20 00:39:35 -0500

    fix: don't send text back on post

--- packages/website/src/services/auth/server.ts
+++ packages/website/src/services/auth/server.ts
@@ -38,5 +38,5 @@ router.get("/github-oauth-callback", async (request, response, next) => {
  */
 router.post("/sign-out", (request, response, next) => {
 	request.session = undefined;
-	response.status(200).send("OK");
+	response.status(200).send();
 });
