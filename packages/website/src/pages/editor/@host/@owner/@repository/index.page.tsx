09d9e41759327dbef44a3253f66416287fc783cc
jannesblobel <72493222+jannesblobel@users.noreply.github.com>
2023-01-20 15:05:31 +0100

    fix :#283 Add Loading spinner,  when the repository is being loaded

--- packages/website/src/pages/editor/@host/@owner/@repository/index.page.tsx
+++ packages/website/src/pages/editor/@host/@owner/@repository/index.page.tsx
@@ -77,7 +77,14 @@ export function Page() {
 					</p>
 				</Match>
 				<Match when={repositoryIsCloned.loading || inlangConfig.loading}>
-					<p>loading ...</p>
+					<div class="flex flex-col grow justify-center items-center min-w-full">
+						{/* sl-spinner need a own div otherwise the spinner has a bug. The wheel is rendered on the outer div  */}
+						<div>
+							<sl-spinner style="font-size: 50px; --track-width: 10px;"></sl-spinner>
+						</div>
+
+						<p>cloning repositories can take a few minutes...</p>
+					</div>
 				</Match>
 				<Match when={inlangConfig() === undefined}>
 					<NoInlangConfigFoundCard></NoInlangConfigFoundCard>
