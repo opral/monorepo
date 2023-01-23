7f6fbe3c51f93b57f4c8bfdeb9cb175e00b9dc14
leolurch <40560457+leolurch@users.noreply.github.com>
2023-01-23 02:27:06 +0100

    round up merge

--- source-code/website/env.ts
+++ source-code/website/env.ts
@@ -40,17 +40,17 @@ export type ServerSideEnv = ClientSideEnv & {
    */
   GOOGLE_TRANSLATE_API_KEY?: string;
 
-	/**
-	 * The connection uri for super tokens.
-	 * Only required in production or when using super tokens in dev by setting VITE_SUPERTOKENS_IN_DEV to 'true'.
-	 */
-	SUPERTOKENS_CONNECTION_URI?: string;
+  /**
+   * The connection uri for super tokens.
+   * Only required in production or when using super tokens in dev by setting VITE_SUPERTOKENS_IN_DEV to 'true'.
+   */
+  SUPERTOKENS_CONNECTION_URI?: string;
 
-	/**
-	 * The API key for super tokens.
-	 * Only required in production or when using super tokens in dev by setting VITE_SUPERTOKENS_IN_DEV to 'true'.
-	 */
-	SUPERTOKENS_API_KEY?: string;
+  /**
+   * The API key for super tokens.
+   * Only required in production or when using super tokens in dev by setting VITE_SUPERTOKENS_IN_DEV to 'true'.
+   */
+  SUPERTOKENS_API_KEY?: string;
 };
 
 /**
@@ -60,41 +60,41 @@ export type ServerSideEnv = ClientSideEnv & {
  * for more information.
  */
 export type ClientSideEnv = {
-	/**
-	 * The environment mode.
-	 */
-	NODE_ENV: "development" | "production" | "test";
+  /**
+   * The environment mode.
+   */
+  NODE_ENV: "development" | "production" | "test";
 
-	/**
-	 * The url of the proxy server for git requests.
-	 */
-	VITE_GIT_REQUEST_PROXY_PATH: string;
-	/**
-	 * The github app client id.
-	 *
-	 * Read more https://docs.github.com/en/developers/apps/building-oauth-apps/authorizing-oauth-apps
-	 */
-	VITE_GITHUB_APP_CLIENT_ID: string;
+  /**
+   * The url of the proxy server for git requests.
+   */
+  VITE_GIT_REQUEST_PROXY_PATH: string;
+  /**
+   * The github app client id.
+   *
+   * Read more https://docs.github.com/en/developers/apps/building-oauth-apps/authorizing-oauth-apps
+   */
+  VITE_GITHUB_APP_CLIENT_ID: string;
 
-	/**
-	 * The sentry dsn for the client.
-	 *
-	 * Only available in production.
-	 */
-	VITE_SENTRY_DSN_CLIENT?: string;
+  /**
+   * The sentry dsn for the client.
+   *
+   * Only available in production.
+   */
+  VITE_SENTRY_DSN_CLIENT?: string;
 
-	/**
-	 * The name of the super tokens app.
-	 * Defaults to `inlang`.
-	 */
-	VITE_SUPERTOKENS_APP_NAME?: string;
+  /**
+   * The name of the super tokens app.
+   * Defaults to `inlang`.
+   */
+  VITE_SUPERTOKENS_APP_NAME?: string;
 
-	/**
-	 * Set to `true` to enable super tokens in your dev environment.
-	 *
-	 * Only affecting the dev.
-	 */
-	VITE_SUPERTOKENS_IN_DEV?: string;
+  /**
+   * Set to `true` to enable super tokens in your dev environment.
+   *
+   * Only affecting the dev.
+   */
+  VITE_SUPERTOKENS_IN_DEV?: string;
 };
 
 /**
@@ -141,14 +141,14 @@ export async function serverSideEnv(): Promise<ServerSideEnv> {
  * @throws Error if the env variable is invalid.
  */
 const validateSingle = (name: string, value?: string) => {
-	if (value === undefined) {
-		throw Error(`Missing env variable ${name}`);
-	}
+  if (value === undefined) {
+    throw Error(`Missing env variable ${name}`);
+  }
 };
 
 const validateSupertokens = (env: ServerSideEnv) => {
-	validateSingle("SUPERTOKENS_API_KEY", env.SUPERTOKENS_API_KEY);
-	validateSingle("SUPERTOKENS_CONNECTION_URI", env.SUPERTOKENS_CONNECTION_URI);
+  validateSingle("SUPERTOKENS_API_KEY", env.SUPERTOKENS_API_KEY);
+  validateSingle("SUPERTOKENS_CONNECTION_URI", env.SUPERTOKENS_CONNECTION_URI);
 };
 
 /**
@@ -157,31 +157,31 @@ const validateSupertokens = (env: ServerSideEnv) => {
  * Will throw an error if the env is invalid.
  */
 export async function validateEnv() {
-	const env = await serverSideEnv();
+  const env = await serverSideEnv();
 
-	validateSingle(
-		"VITE_GIT_REQUEST_PROXY_PATH",
-		env.VITE_GIT_REQUEST_PROXY_PATH
-	);
-	validateSingle("VITE_GITHUB_APP_CLIENT_ID", env.VITE_GITHUB_APP_CLIENT_ID);
-	validateSingle("JWE_SECRET_KEY", env.JWE_SECRET_KEY);
-	validateSingle("GITHUB_APP_CLIENT_SECRET", env.GITHUB_APP_CLIENT_SECRET);
+  validateSingle(
+    "VITE_GIT_REQUEST_PROXY_PATH",
+    env.VITE_GIT_REQUEST_PROXY_PATH
+  );
+  validateSingle("VITE_GITHUB_APP_CLIENT_ID", env.VITE_GITHUB_APP_CLIENT_ID);
+  validateSingle("JWE_SECRET_KEY", env.JWE_SECRET_KEY);
+  validateSingle("GITHUB_APP_CLIENT_SECRET", env.GITHUB_APP_CLIENT_SECRET);
 
-	if (env.NODE_ENV == "production") {
-		validateSupertokens(env);
-	} else {
-		if (env.VITE_SUPERTOKENS_IN_DEV) {
-			validateSupertokens(env);
-		}
-	}
+  if (env.NODE_ENV == "production") {
+    validateSupertokens(env);
+  } else {
+    if (env.VITE_SUPERTOKENS_IN_DEV) {
+      validateSupertokens(env);
+    }
+  }
 
-	// in depth validation
-	if (
-		env.VITE_GIT_REQUEST_PROXY_PATH.startsWith("/") === false ||
-		env.VITE_GIT_REQUEST_PROXY_PATH.endsWith("/") === false
-	) {
-		throw Error(
-			"VITE_GIT_REQUEST_PROXY_PATH must be a local path like that starts and ends with a slash `/` like `/git-proxy/`."
-		);
-	}
+  // in depth validation
+  if (
+    env.VITE_GIT_REQUEST_PROXY_PATH.startsWith("/") === false ||
+    env.VITE_GIT_REQUEST_PROXY_PATH.endsWith("/") === false
+  ) {
+    throw Error(
+      "VITE_GIT_REQUEST_PROXY_PATH must be a local path like that starts and ends with a slash `/` like `/git-proxy/`."
+    );
+  }
 }
