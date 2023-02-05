74612e8799da7eb0d0c576535134b1b5a7af81de
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2023-02-05 23:04:25 +0100

    refactor: use `src` directory

--- packages/website/src/services/auth/index.ts
+++ packages/website/src/services/auth/index.ts
@@ -1,2 +1,4 @@
 export { SignInDialog } from "./src/components/SignInDialog.jsx";
 export { onSignOut } from "./src/onSignOut.js";
+// a telefunc automatically runs on the server, thus can be used on the client
+export { getUserInfo } from "./src/implementation.telefunc.js";
