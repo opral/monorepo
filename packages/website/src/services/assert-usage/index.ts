072b93b47e6f1d914b9f2599fc1dfdacd656edc6
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2023-02-05 22:52:29 +0100

    refactor: improve codebase
    - rename assert to assert usage to avoid name collision with node's assert
    
    - add readme
    
    - use `/src` folder logic

---
+++ packages/website/src/services/assert-usage/index.ts
@@ -0,0 +1,8 @@
+/**
+ * --------------------------------
+ * This index file exports code that is supposed
+ * to be used outside of this directory.
+ * --------------------------------
+ */
+
+export { assertUsage } from "./src/implementation.js";
