9daf0dc2a79a1f27db4476d452fa26c405cb933d
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2023-02-07 19:32:38 +0000

    fix: handle post requests
    My goodness. This kind of issues should exists. Sheer incompability between an express response/request/json and fetch respone/request/json.

--- source-code/website/src/services/github/src/router.ts
+++ source-code/website/src/services/github/src/router.ts
@@ -14,32 +14,43 @@ const env = await serverSideEnv();
 
 // matching all routes after the path with '*'
 // and proxying the request to the GitHub API
-router.all(PATH + "*", async (request, response, next) => {
-  try {
-    const encryptedAccessToken = request.session?.encryptedAccessToken;
-    const decryptedAccessToken = await decryptAccessToken({
-      JWE_SECRET_KEY: env.JWE_SECRET_KEY,
-      jwe: encryptedAccessToken,
-    });
-    // slicing the path to remove the path prefix
-    const res = await fetch(request.url.slice(PATH.length), {
-      method: request.method,
-      headers: {
-        // set the authorization header (must be base64 encoded)
-        authorization: `Basic ${btoa(decryptedAccessToken)}`,
-        "X-GitHub-Api-Version": "2022-11-28",
-      },
-      body: request.body,
-    });
-    if (res.headers.get("content-type")?.includes("json")) {
-      response
-        .status(res.status)
-        .contentType(res.headers.get("content-type")!)
-        .send(await res.json());
-    } else {
-      response.status(res.status).send(res.body);
+router.all(
+  PATH + "*",
+  // Parse & make HTTP request body available at `req.body`
+  express.json(),
+  async (request, response, next) => {
+    try {
+      const encryptedAccessToken = request.session?.encryptedAccessToken;
+      const decryptedAccessToken = await decryptAccessToken({
+        JWE_SECRET_KEY: env.JWE_SECRET_KEY,
+        jwe: encryptedAccessToken,
+      });
+      // slicing the path to remove the path prefix
+      const res = await fetch(request.url.slice(PATH.length), {
+        method: request.method,
+        // @ts-ignore
+        headers: {
+          authorization: `Bearer ${decryptedAccessToken}`,
+          "Content-Type": request.get("Content-Type"),
+        },
+        // fetch throws an error if a method is GET and a body is attached
+        // the body comes from the express.text() middleware
+        // @ts-ignore
+        body:
+          // need to stringify otherwise github's api returns an error
+          request.method === "GET" ? undefined : JSON.stringify(request.body),
+      });
+      if (res.headers.get("content-type")?.includes("json")) {
+        response
+          .status(res.status)
+          .contentType(res.headers.get("content-type")!)
+          .send(await res.json());
+      } else {
+        response.status(res.status).send(res.body);
+      }
+    } catch (error) {
+      console.error("ERROR in github service: ", error);
+      next(error);
     }
-  } catch (error) {
-    next(error);
   }
-});
+);
