2490c69ea3eb65eed9dbaf19ee7c47cb2c46e8d6
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2023-02-07 12:55:38 +0000

    refactor: use octokit instead of telefuncs

--- packages/website/src/services/github/index.ts
+++ packages/website/src/services/github/index.ts
@@ -1,5 +1,1 @@
-export { isCollaborator } from "./src/isCollaborator.telefunc.js";
-export { onFork, syncFork } from "./src/fork.telefunc.js";
-export { repositoryInformation } from "./src/repositoryInformation.telefunc.js";
-
 export { github } from "./src/implementation.js";
