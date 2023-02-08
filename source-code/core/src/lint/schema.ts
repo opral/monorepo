c206b2000c2e3e9f8692fec034adfd2f17834957
Ivan Hofer <ivan.hofer@outlook.com>
2023-02-08 08:59:42 +0100

    process nodes

--- source-code/core/src/lint/schema.ts
+++ source-code/core/src/lint/schema.ts
@@ -1,3 +1,4 @@
+import type { Resource, Message, Pattern } from '../ast/schema.js'
 import type { Config } from '../config/schema.js'
 
 type MaybePromise<T> = T | Promise<T>
@@ -11,7 +12,29 @@ type LintConfig<Config = Record<string, unknown>> = {
 export type LintRuleInit<Config extends Record<string, unknown> | undefined = undefined> =
 	(...options: [undefined extends Config ? never : LintConfig<Config>]) => LintRule
 
+export type LintableNode =
+	| Resource
+	| Message
+	| Pattern
+
+type NodeVisitor<Node extends LintableNode> = {
+	before?: (...[target, reference]: TargetReferenceParameterTuple<Node>) => MaybePromise<'skip' | void>
+	lint?: (...[target, reference]: TargetReferenceParameterTuple<Node>) => MaybePromise<void>
+	after?: (...[target, reference]: TargetReferenceParameterTuple<Node>) => MaybePromise<void>
+}
+
 export type LintRule = {
 	id: string
 	initialize?: (config: Pick<Config, 'referenceLanguage' | 'languages'>) => MaybePromise<unknown>
+	visit: {
+		[Key in LintableNode['type']]?: NodeVisitor<GetByType<LintableNode, Key>>
+	},
+	teardown?: () => MaybePromise<void>
 }
+
+type GetByType<Node extends { type: string }, Key> = Node extends { type: Key } ? Node : never
+
+export type TargetReferenceParameterTuple<Node extends LintableNode> =
+	| [Node, Node]
+	| [Node, Node | undefined]
+	| [Node | undefined, Node]
