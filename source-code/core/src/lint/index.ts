55e440c405b3563ea857254a455f4dfc15edcda7
Ivan Hofer <ivan.hofer@outlook.com>
2023-02-09 10:00:51 +0100

    allow to disable rule and specify lint type

--- source-code/core/src/lint/index.ts
+++ source-code/core/src/lint/index.ts
@@ -1,8 +1,7 @@
 export type { LintRule, LintRuleInit } from './schema.js'
 export { lint } from './linter.js'
+export { parseLintType } from './reporter.js'
 
 // TODO: distinguish between plugin and rule
 // TODO: create utility functions to check and get lint issues
-// TODO: allow to set lint type
-// TODO: allow to disable lint rule
 // TODO: process rules in parallel to speed things up
