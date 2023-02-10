3b56fdfbdb89e3b771c53646a560f364456ec0b2
Ivan Hofer <ivan.hofer@outlook.com>
2023-02-10 09:49:42 +0100

    cleanup

--- packages/core/src/lint/index.ts
+++ packages/core/src/lint/index.ts
@@ -1,7 +1,7 @@
 export { lint } from './linter.js'
 export * from './query.js'
 export { type Reporter, parseLintSettings } from './reporter.js'
-export type { ConfiguredLintRule as LintRule, LintRule as LintRuleInit } from './rule.js'
+export type { ConfiguredLintRule, LintRule } from './rule.js'
 
 // TODO: write actual tests
 // TODO: write type definition tests
