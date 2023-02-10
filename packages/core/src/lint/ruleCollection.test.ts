1c161e91c41590ab7db780500b3351724816ca07
Ivan Hofer <ivan.hofer@outlook.com>
2023-02-10 09:44:59 +0100

    add test cases

---
+++ packages/core/src/lint/ruleCollection.test.ts
@@ -0,0 +1,58 @@
+import { describe, test, vi } from "vitest";
+describe("createRuleCollection", async () => {
+	test("once called, should return an `Array` of all specified rules", async () => {
+
+	})
+
+	describe("should use defaults when nothing gets passed", async () => {
+		test("globally", async () => {
+
+		})
+
+		test("for a specific rule", async () => {
+
+		})
+	})
+
+	describe("should pass the correct settings to each lint rule", async () => {
+		describe("single param", async () => {
+			test("'false'", async () => {
+
+			})
+
+			test("'true'", async () => {
+
+			})
+
+			test("'error'", async () => {
+
+			})
+
+			test("'warning'", async () => {
+
+			})
+		})
+
+		describe("as tuple with single entry", async () => {
+			test("'false'", async () => {
+
+			})
+
+			test("'true'", async () => {
+
+			})
+
+			test("'error'", async () => {
+
+			})
+
+			test("'warning'", async () => {
+
+			})
+		})
+
+		test("pass rule specific settings", async () => {
+
+		})
+	})
+})
