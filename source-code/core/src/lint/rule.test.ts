3242b3ab84e48870769696826de38ce50657b0fe
Ivan Hofer <ivan.hofer@outlook.com>
2023-02-10 08:10:37 +0100

    add tests

---
+++ source-code/core/src/lint/rule.test.ts
@@ -0,0 +1,11 @@
+import { describe, test } from "vitest";
+
+describe("getLintRulesFromConfig", async () => {
+	test("should return an empty `Array` if no lint attribute is present", async () => {
+
+	})
+
+	test("should return all specified lint rules", async () => {
+
+	})
+})
