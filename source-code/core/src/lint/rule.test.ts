737c63af7b10b81bf60c25a4bae387513d8bdf04
Ivan Hofer <ivan.hofer@outlook.com>
2023-02-13 08:58:43 +0100

    add test cases

--- source-code/core/src/lint/rule.test.ts
+++ source-code/core/src/lint/rule.test.ts
@@ -13,3 +13,47 @@ describe("getLintRulesFromConfig", async () => {
 
 	})
 })
+
+describe("createRule", async () => {
+	describe("calling the function should", async () => {
+		describe("return an object with", async () => {
+			test("the correct id", async () => {
+
+			})
+
+			test("the correct level", async () => {
+
+			})
+
+			test("the passed `initialize` function", async () => {
+
+			})
+
+			test("the passed visitors", async () => {
+
+			})
+
+			test("the passed `teardown` function if passed", async () => {
+
+			})
+		})
+
+		describe("respect params and", async () => {
+			test("change log level if specified", async () => {
+
+			})
+
+			test("pass `undefined` for settings if not specified", async () => {
+
+			})
+
+			test("pass the settings object if defined", async () => {
+
+			})
+
+			test("pass the settings object if defined", async () => {
+
+			})
+		})
+	})
+})
