0de87876271455f9817c6ec441a7cb91b087a5bb
Ivan Hofer <ivan.hofer@outlook.com>
2023-02-14 09:37:16 +0100

    implement `parseLintSettings` tests

--- packages/core/src/lint/context.test.ts
+++ packages/core/src/lint/context.test.ts
@@ -1,28 +1,76 @@
-import { describe, test } from "vitest";
+import { describe, expect, test } from "vitest";
+import { parseLintSettings } from './context.js';
 
 describe("parseLintSettings", async () => {
 	test("when `undefined` get's passed", async () => {
+		const settings = parseLintSettings(undefined, 'error')
 
+		expect(settings).toMatchObject({
+			level: 'error',
+			options: undefined,
+		})
+	})
+
+	test("when an empty Array get's passed", async () => {
+		const settings = parseLintSettings([], 'error')
+
+		expect(settings).toMatchObject({
+			level: 'error',
+			options: undefined,
+		})
 	})
 
 	test("when `error` get's passed", async () => {
+		const settings = parseLintSettings(['error'], 'warning')
 
+		expect(settings).toMatchObject({
+			level: 'error',
+			options: undefined,
+		})
 	})
 
 	test("when `warning` get's passed", async () => {
+		const settings = parseLintSettings(['warning'], 'error')
 
+		expect(settings).toMatchObject({
+			level: 'warning',
+			options: undefined,
+		})
 	})
 
 	test("when `true` get's passed", async () => {
+		const settings = parseLintSettings([true], 'error')
 
+		expect(settings).toMatchObject({
+			level: 'error',
+			options: undefined,
+		})
 	})
 
 	test("when `false` get's passed", async () => {
+		const settings = parseLintSettings([false], 'error')
 
+		expect(settings).toMatchObject({
+			level: false,
+			options: undefined,
+		})
 	})
 
 	test("when custom `settings` get's passed", async () => {
+		const options = {
+			custom: true,
+			prop: [123, 456],
+			nested: {
+				prop: 'inlang',
+			}
+		}
 
+		const settings = parseLintSettings(['error', options], 'warning')
+
+		expect(settings).toMatchObject({
+			level: 'error',
+			options,
+		})
 	})
 })
 
