4a039595eee92bf23a442b22da983e2227f08bf0
Ivan Hofer <ivan.hofer@outlook.com>
2023-02-15 08:34:03 +0100

    implement `createRule` tests

--- packages/core/src/lint/rule.test.ts
+++ packages/core/src/lint/rule.test.ts
@@ -1,44 +1,100 @@
-import { describe, test } from "vitest";
+import { beforeEach, describe, expect, test, vi } from "vitest";
+import { ConfiguredLintRule, createRule } from './rule.js';
+
+const initialize = () => undefined
+const visitors = {}
+
+const rule1 = createRule('my.id', 'error', () => {
+	return {
+		initialize,
+		visitors,
+	}
+})
+
+const teardown = () => undefined
+const rule2 = createRule('my.id', 'error', (options) => {
+	return {
+		initialize: () => console.log(options),
+		visitors,
+		teardown,
+	}
+})
+
+// --------------------------------------------------------------------------------------------------------------------
+
+vi.spyOn(console, 'log').mockImplementation(vi.fn)
 
 describe("createRule", async () => {
+	beforeEach(() => {
+		vi.resetAllMocks()
+	})
+
 	describe("calling the function should", async () => {
 		describe("return an object with", async () => {
 			test("the correct id", async () => {
+				const configuredRule = rule1()
 
+				expect(configuredRule.id).toBe('my.id')
 			})
 
 			test("the correct level", async () => {
+				const configuredRule = rule1()
 
+				expect(configuredRule.level).toBe('error')
 			})
 
 			test("the passed `initialize` function", async () => {
+				const configuredRule = rule1()
 
+				expect(configuredRule.initialize).toBe(initialize)
 			})
 
 			test("the passed visitors", async () => {
+				const configuredRule = rule1()
 
+				expect(configuredRule.visitors).toBe(visitors)
+			})
+
+			test("no `teardown` function if not passed", async () => {
+				const configuredRule = rule1()
+
+				expect(configuredRule.teardown).toBeUndefined()
 			})
 
 			test("the passed `teardown` function if passed", async () => {
+				const configuredRule = rule2()
 
+				expect(configuredRule.teardown).toBe(teardown)
 			})
 		})
 
 		describe("respect params and", async () => {
 			test("change log level if specified", async () => {
+				const configuredRule = rule1('warn')
 
+				expect(configuredRule.level).toBe('warn')
 			})
 
-			test("pass `undefined` for settings if not specified", async () => {
+			test("pass `undefined` for options if not specified", async () => {
+				const configuredRule = rule2('error')
 
+				configuredRule.initialize(...([] as unknown as Parameters<ConfiguredLintRule['initialize']>))
+
+				expect(console.log).toHaveBeenCalledOnce()
+				expect(console.log).toHaveBeenCalledWith(undefined)
 			})
 
-			test("pass the settings object if defined", async () => {
+			test("pass the options object if defined", async () => {
+				const options = {
+					some: 'option',
+					debug: true
+				}
+				const configuredRule = rule2('error', options)
 
-			})
+				configuredRule.initialize(...([] as unknown as Parameters<ConfiguredLintRule['initialize']>))
 
-			test("pass the settings object if defined", async () => {
-
+				expect(console.log).toHaveBeenCalledOnce()
+				expect(console.log).toHaveBeenCalledWith(options)
 			})
 		})
 	})
