bbf36b542f76c184c5d6bc6f6060321c2c43b88a
Ivan Hofer <ivan.hofer@outlook.com>
2023-02-21 13:07:41 +0100

    add type tests

--- source-code/core/src/lint/index.ts
+++ source-code/core/src/lint/index.ts
@@ -3,8 +3,6 @@ export * from './query.js'
 export { type Context, parseLintSettings } from './context.js'
 export type { ConfiguredLintRule, LintRule } from './rule.js'
 
-// TODO: write actual tests
-// TODO: write type definition tests
 // TODO: create helper functions so plugin authors can test their own rules
 //		- for correct parameter handling
 //		- for correct return type
---
+++ source-code/core/src/lint/ruleCollection.test-d.ts
@@ -0,0 +1,38 @@
+import { expectType } from 'tsd'
+import type { ConfiguredLintRule, LintRule } from './rule.js'
+import { createRuleCollection } from './ruleCollection.js'
+
+// eslint-disable-next-line unicorn/no-null
+const rule: LintRule = null as any
+// eslint-disable-next-line unicorn/no-null
+const ruleWIthSettings: LintRule<{ some: { nested: 'setting' }, setting: boolean }> = null as any
+
+const ruleCollection = createRuleCollection({ rule, ruleWIthSettings })
+
+// parameters -----------------------------------------------------------------
+
+expectType<Parameters<typeof createRuleCollection>[0]>({ rule })
+
+expectType<Parameters<typeof ruleCollection>[0]>({ rule: 'error' })
+expectType<Parameters<typeof ruleCollection>[0]>({ rule: false })
+expectType<Parameters<typeof ruleCollection>[0]>({ rule: 'warn' })
+// @ts-expect-error 'warning' is no valid LintLevel
+expectType<Parameters<typeof ruleCollection>[0]>({ rule: 'warning' })
+
+expectType<Parameters<typeof ruleCollection>[0]>({ rule: ['error'] })
+expectType<Parameters<typeof ruleCollection>[0]>({ rule: [false] })
+expectType<Parameters<typeof ruleCollection>[0]>({ rule: ['warn'] })
+// @ts-expect-error 'warning' is no valid LintLevel
+expectType<Parameters<typeof ruleCollection>[0]>({ rule: ['warning'] })
+
+expectType<Parameters<typeof ruleCollection>[0]>({ rule: ['error', undefined] })
+// @ts-expect-error rule does not expect any settings
+expectType<Parameters<typeof ruleCollection>[0]>({ rule: ['error', true] })
+
+expectType<Parameters<typeof ruleCollection>[0]>({ ruleWIthSettings: ['warn', { some: { nested: 'setting' }, setting: false }] })
+// @ts-expect-error object does not match
+expectType<Parameters<typeof ruleCollection>[0]>({ ruleWIthSettings: ['warn', { some: { nested: false }, setting: true }] })
+
+// collection -----------------------------------------------------------------
+
+expectType<ConfiguredLintRule[]>(ruleCollection())
