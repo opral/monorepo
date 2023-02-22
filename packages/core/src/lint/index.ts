b665a345b9151b55ffcd54879419c8aafa178c93
Ivan Hofer <ivan.hofer@outlook.com>
2023-02-22 08:09:20 +0100

    add some JsDoc comments

--- packages/core/src/lint/index.ts
+++ packages/core/src/lint/index.ts
@@ -1,7 +1,8 @@
 export { lint } from './linter.js'
 export * from './query.js'
 export { type Context, parseLintSettings } from './context.js'
-export type { ConfiguredLintRule, LintRule } from './rule.js'
+export { createRule, type ConfiguredLintRule, type LintRule, type LintRuleId } from './rule.js'
+export { createRuleCollection, type RuleCollection } from './ruleCollection.js'
 
 // TODO: create helper functions so plugin authors can test their own rules
 //		- for correct parameter handling
@@ -13,3 +14,10 @@ export type { ConfiguredLintRule, LintRule } from './rule.js'
 //    - how to create a rule
 //    - how to test a rule
 //    - how to publish a rule
+
+// TODO: decide on naming
+// `createRule` or `createLintRule`
+// `createCollection` or `createLintCollection`
+// `LintRule` => `ConfiguredLintRule` or `LintRuleInitializer` => `LintRule`
+// `Args`, `Options`, `Settings`, `Params` ...
+//
--- packages/core/src/lint/rule.ts
+++ packages/core/src/lint/rule.ts
@@ -39,11 +39,35 @@ export type NodeVisitors = {
 export type LintConfigSettings<Settings> =
 	[] | [boolean | LintLevel] | [LintLevel, Settings?]
 
+/**
+ * The unique id of a lint rule.
+ *
+ * Example:
+ * ```
+ * 'inlangStandardRules.missingKey'
+ * ```
+ */
+export type LintRuleId = `${string}.${string}`
+
+/**
+ * An utility type to add strong type definitions for a lint rule.
+ *
+ * @example  a rule that does not expects any parameters
+ * ```
+ * const myRule: LintRule = // implementation
+ * ```
+ * @example a rule that accepts parameters
+ * ```
+ * const myRule: LintRule<{ strict: boolean }> = // implementation
+ * ```
+ */
 export type LintRule<Settings = never> =
 	(...settings: LintConfigSettings<Settings>) => ConfiguredLintRule
 
-export type LintRuleId = `${string}.${string}` // e.g. 'inlangStandardRules.missingKey'
 
+/**
+ * A lint rule that was configured with the lint level and config options.
+ */
 export type ConfiguredLintRule = {
 	id: LintRuleId
 	level: false | LintLevel
@@ -57,6 +81,28 @@ export type ConfiguredLintRule = {
 	teardown?: (param: { payload: unknown }) => MaybePromise<void>
 }
 
+/**
+ * An utility function that encapsulates the parsing of the configuration options passed to the lint rule.
+ *
+ * @param id the unique lint id of this lint rule
+ * @param defaultLevel the default lint level this rule should have
+ * @param configureLintRule a callback function that get passed the configuration options and need to return rule specific implementation details
+ * @returns a lint rule
+ *
+ * @example
+ * ```
+ * const myRule = createRule<{ strict: boolean }>('my.rule', 'error', (settings) => {
+ *    return {
+ * 		initialize: () => {
+ * 			if (settings?.strict) return { token: '123' }
+ *
+ * 			return { token: '456' }
+ * 		},
+ * 		visitors: {}
+ * 	}
+ * })
+ * ```
+ */
 export const createRule = <Settings>(
 	id: LintRuleId,
 	defaultLevel: LintLevel,
