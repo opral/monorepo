1849f56696cb74dde1e2ff089f914eaa3ce70cc7
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2023-02-25 15:24:24 +0000

    improve communication
    - moved the readme to the top
    - changed the slogan from "dev-first..." to "localization infrastructure for software"

--- source-code/website/src/pages/index/index.page.tsx
+++ source-code/website/src/pages/index/index.page.tsx
@@ -20,18 +20,15 @@ export function Page(props: PageProps) {
         <div class="self-center grow sm:px-6 md:px-0 mb-8 flex flex-col">
           <Hero />
           <div class="flex flex-col gap-12">
-            <CommunityProjects />
             {/* rendering the github readme */}
-            <div>
-              <h2 class="text-2xl font-medium pb-5">README.md</h2>
-              <div
-                class="p-4 md:p-6 rounded-lg border border-outline"
-                classList={{ [styles["markdown-body"]]: true }}
-                // eslint-disable-next-line solid/no-innerhtml
-                innerHTML={props.markdown}
-              />
-            </div>
+            <div
+              class="p-4 md:p-6 rounded-lg border border-outline"
+              classList={{ [styles["markdown-body"]]: true }}
+              // eslint-disable-next-line solid/no-innerhtml
+              innerHTML={props.markdown}
+            />
           </div>
+          <CommunityProjects />
         </div>
       </RootLayout>
     </>
