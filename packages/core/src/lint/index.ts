22716d4c23970860a050845a879d400e376e354b
Ivan Hofer <ivan.hofer@outlook.com>
2023-03-01 07:52:05 +0100

    remove TODO comments

--- packages/core/src/lint/index.ts
+++ packages/core/src/lint/index.ts
@@ -11,14 +11,3 @@ export {
   createLintRuleCollection,
   type RuleCollectionInitializer,
 } from "./ruleCollection.js";
-
-// TODO: create helper functions so plugin authors can test their own rules
-//		- for correct parameter handling
-//		- for correct return type
-// TODO: add debug logs
-
-// TODO: documentation
-//    - how to use the lint functionality
-//    - how to create a rule
-//    - how to test a rule
-//    - how to publish a rule
