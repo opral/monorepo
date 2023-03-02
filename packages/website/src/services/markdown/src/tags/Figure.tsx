180c170fae7e692070ee27692334015dd6ef73a3
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2023-03-02 12:15:16 +0100

    Revert "Revert "Imrpove getting started docs""

--- packages/website/src/services/markdown/src/tags/Figure.tsx
+++ packages/website/src/services/markdown/src/tags/Figure.tsx
@@ -1,7 +1,18 @@
-export function Figure(props: { src: string; alt: string; caption: string }) {
+export function Figure(props: {
+  src: string;
+  alt: string;
+  caption: string;
+  height: string;
+  width: string;
+}) {
   return (
     <figure>
-      <img src={props.src} alt={props.alt} />
+      <img
+        src={props.src}
+        alt={props.alt}
+        height={props.height}
+        width={props.width}
+      />
       <figcaption>{props.caption}</figcaption>
     </figure>
   );
