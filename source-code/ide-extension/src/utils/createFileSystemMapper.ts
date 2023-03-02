427e6e4edbb5f36a216864aee4c3537244a4e5a1
Robin Bühler <git+r@obin.ch>
2023-03-02 13:48:21 +0000

    chore: add comments

--- source-code/ide-extension/src/utils/createFileSystemMapper.ts
+++ source-code/ide-extension/src/utils/createFileSystemMapper.ts
@@ -1,6 +1,13 @@
 import * as vscode from "vscode";
 import type { FS } from '@inlang/core/fs';
 
+/**
+ * Creates a new mapper between vscode and inlang file systems.
+ *
+ * @param target vscode file system which should be mapped
+ * @param base uri for relative paths
+ * @returns file system mapper
+ */
 export function createFileSystemMapper(target: vscode.FileSystem, base: vscode.Uri): FS {
   return {
     readdir: async (path) => {
