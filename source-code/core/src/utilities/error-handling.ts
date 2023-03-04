d4f2be82a4766f9ac26c633dc9f0f64a7a27145a
Ivan Hofer <ivan.hofer@outlook.com>
2023-03-04 08:32:26 +0100

    add lint and format scripts

--- source-code/core/src/utilities/error-handling.ts
+++ source-code/core/src/utilities/error-handling.ts
@@ -1,7 +1,9 @@
 import { inspect } from "./polyfills/util.js";
 
 export const unhandled = (type: never, context?: unknown): never => {
-	throw new Error(
-		`unhandled case: '${type}'${context ? ` for ${inspect(context, { depth: 99 })}` : ""}`
-	);
+  throw new Error(
+    `unhandled case: '${type}'${
+      context ? ` for ${inspect(context, { depth: 99 })}` : ""
+    }`
+  );
 };
