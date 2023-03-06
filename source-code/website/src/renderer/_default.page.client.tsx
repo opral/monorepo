opral/monorepo

commit 114deaf0edc3201a3e83483fb0b603e676b24f56

chore: formatting

source-code/website/src/renderer/_default.page.client.tsx
@@ -1,6 +1,6 @@
 import { Component, createSignal } from "solid-js"
 import { createStore } from "solid-js/store"
-import { hydrate, render as renderSolid } from "solid-js/web"
+import { hydrate, render as renderApp } from "solid-js/web"
 import { Root } from "./Root.jsx"
 import { setCurrentPageContext } from "./state.js"
 import type { PageContextRenderer } from "./types.js"
@@ -69,14 +69,16 @@ export function render(pageContext: PageContextRenderer) {
 		setCurrentPageProps(pageContext.pageProps)
 		if (isFirstRender) {
 			// editor is client side rendered only
-			;(isEditor ? renderSolid : hydrate)(
-				() => (
-					<MetaProvider>
-						<Root page={currentPage()!} pageProps={currentPageProps} />
-					</MetaProvider>
-				),
-				rootElement,
-			)
+			isEditor
+				? renderApp
+				: hydrate(
+						() => (
+							<MetaProvider>
+								<Root page={currentPage()!} pageProps={currentPageProps} />
+							</MetaProvider>
+						),
+						rootElement,
+				  )
 			isFirstRender = false
 		}
 		// https://posthog.com/docs/integrate/client/js#one-page-apps-and-page-views

commit cec04d51c1a39a2092d4be3f796cf93a97f4fd2e

fix: client-side rendering
Commit 114deaf introduced a bug. The line 75 `(isEditor ? renderApp : hydrate)` is followed by a function execution.

source-code/website/src/renderer/_default.page.client.tsx
@@ -68,17 +68,18 @@ export function render(pageContext: PageContextRenderer) {
 		setCurrentPage(() => pageContext.Page)
 		setCurrentPageProps(pageContext.pageProps)
 		if (isFirstRender) {
-			// editor is client side rendered only
-			isEditor
-				? renderApp
-				: hydrate(
-						() => (
-							<MetaProvider>
-								<Root page={currentPage()!} pageProps={currentPageProps} />
-							</MetaProvider>
-						),
-						rootElement,
-				  )
+			// The editor is only rendered client-side.
+			//
+			// In the future, the editor might be server-side rendered.
+			// For now, the trouble of isomorphic rendering the editor is not worth it.
+			;(isEditor ? renderApp : hydrate)(
+				() => (
+					<MetaProvider>
+						<Root page={currentPage()!} pageProps={currentPageProps} />
+					</MetaProvider>
+				),
+				rootElement,
+			)
 			isFirstRender = false
 		}
 		// https://posthog.com/docs/integrate/client/js#one-page-apps-and-page-views