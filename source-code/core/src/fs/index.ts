e2074ed93fbefba47d4b04e4ace1902624dde17f
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2023-03-08 22:03:31 +0100

    add: mkdir api
    Mkdir is required for mocking the environment and support by node,memfs and vscode.

--- source-code/core/src/fs/index.ts
+++ source-code/core/src/fs/index.ts
@@ -1,10 +1,11 @@
 type ReadFileOptions = { encoding?: BufferEncoding; flag?: string } | BufferEncoding
 
 /**
- * Minimal filesystem required by inlang to work
+ * Minimal filesystem required by inlang
  */
 export type FS = {
 	readFile: (path: string, options?: ReadFileOptions) => Promise<string | Buffer>
 	writeFile: (path: string, data: string) => Promise<void>
 	readdir: (path: string) => Promise<Array<string | object>>
+	mkdir: (path: string, options?: { recursive: boolean }) => Promise<void>
 }
