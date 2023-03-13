fb191ecbb1b7ede69a1d48b9067f3e7abb4c4b94
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2023-03-13 11:43:37 +0000

    refactor: use normalizepath function
    Multiple bugs occured due to unnormalized paths. The function has been copy pasted and should be exposed as part of a custom filesystem implementation in the future.

--- source-code/core/src/test/mockEnvironment.ts
+++ source-code/core/src/test/mockEnvironment.ts
@@ -44,13 +44,45 @@ async function copyDirectory(args: { copyFrom: FS; copyTo: FS; path: string }) {
 	await args.copyTo.mkdir(args.path, { recursive: true })
 	for (const file of await args.copyFrom.readdir(args.path)) {
 		const isFile = (file as string).includes(".")
+		const _path = normalizePath(`${args.path}/${file}`)
 		if (isFile) {
 			await args.copyTo.writeFile(
-				`${args.path}/${file}`,
-				(await args.copyFrom.readFile(`./${args.path}/${file}`, { encoding: "utf-8" })) as string,
+				_path,
+				(await args.copyFrom.readFile(_path, { encoding: "utf-8" })) as string,
 			)
 		} else {
-			await copyDirectory({ ...args, path: `${args.path}/${file}` })
+			await copyDirectory({ ...args, path: _path })
 		}
 	}
+}
+
+/*
+ * normalize-path <https://github.com/jonschlinkert/normalize-path>
+ *
+ * Copyright (c) 2014-2018, Jon Schlinkert.
+ * Released under the MIT License.
+ */
+export function normalizePath(path: string) {
+	if (typeof path !== "string") {
+		throw new TypeError("expected path to be a string")
+	}
+
+	if (path === "\\" || path === "/") return "/"
+
+	const len = path.length
+	if (len <= 1) return path
+
+	// ensure that win32 namespaces has two leading slashes, so that the path is
+	// handled properly by the win32 version of path.parse() after being normalized
+	// https://msdn.microsoft.com/library/windows/desktop/aa365247(v=vs.85).aspx#namespaces
+	let prefix = ""
+	if (len > 4 && path[3] === "\\") {
+		const ch = path[2]
+		if ((ch === "?" || ch === ".") && path.slice(0, 2) === "\\\\") {
+			path = path.slice(2)
+			prefix = "//"
+		}
+	}
+	const segs = path.split(/[/\\]+/)
+	return prefix + segs.join("/")
 }
