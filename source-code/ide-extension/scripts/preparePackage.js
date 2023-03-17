742ad605e4f42b102e18f540af0d494437afe7a5
Robin Bühler <git+r@obin.ch>
2023-03-17 11:54:33 +0000

    chore: enhance package naming

--- source-code/ide-extension/scripts/preparePackage.js
+++ source-code/ide-extension/scripts/preparePackage.js
@@ -2,6 +2,6 @@ import("node:fs").then((fs) => {
 	// change name of package
 	// see https://github.com/microsoft/vscode-vsce/issues/186
 	const packageJson = JSON.parse(fs.readFileSync("./package.json"))
-	packageJson.name = "inlang"
+	packageJson.name = "vs-code-extension"
 	fs.writeFileSync("./package.json", JSON.stringify(packageJson, undefined, 2))
 })
