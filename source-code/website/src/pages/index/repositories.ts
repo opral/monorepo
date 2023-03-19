b59e21ffe22a38b80f624dd211cd0ee840c2564c
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2023-03-19 12:53:22 +0100

    Update repositories.ts
    https://github.com/knadh/listmonk/pull/1189

--- source-code/website/src/pages/index/repositories.ts
+++ source-code/website/src/pages/index/repositories.ts
@@ -25,6 +25,11 @@ export const repositories: Repositories = [
 		repository: "goaccess",
 		description: "GoAccess is a real-time web log analyzer and interactive viewer",
 	},
+	{
+		owner: "knadh",
+		repository: "listmonk",
+		description: "High performance, self-hosted, newsletter and mailing list manager with a modern dashboard. Single binary app.",
+	},
 ]
 
 type Repositories = Array<{
