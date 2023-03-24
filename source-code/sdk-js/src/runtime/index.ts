02091992ed7bf72b052bb684d07ad275276f725d
Ivan Hofer <ivan.hofer@outlook.com>
2023-03-24 10:20:52 +0100

    allow to pass state from outside

--- source-code/sdk-js/src/runtime/index.ts
+++ source-code/sdk-js/src/runtime/index.ts
@@ -9,22 +9,29 @@ const dummyResource = {
 
 const fallbackLookupFunction: LookupFunction = () => ""
 
-export const initRuntime = <Language extends string>() => {
-	const resourcesCache = new Map<Language, Resource>()
-	let currentLanguage: Language
+type RuntimeState<Language extends string> = {
+	resources: Map<Language, Resource>
+	language: Language
+}
 
+export const initRuntime = <Language extends string>(
+	state: RuntimeState<Language> = {
+		resources: new Map(),
+		language: undefined as unknown as Language, // TODO: what is the default value?
+	},
+) => {
 	const loadResource = async (language: Language) => {
 		const resource = dummyResource // TODO: actually load resource
-		resourcesCache.set(language, resource)
+		state.resources.set(language, resource)
 	}
 
-	const switchLanguage = (language: Language) => (currentLanguage = language)
+	const switchLanguage = (language: Language) => (state.language = language)
 
 	// TODO: what should we do if `switchLanguage` was never called before? Throw an error? Return undefined? An empty string?
-	const getCurrentLanguage = () => currentLanguage
+	const getCurrentLanguage = () => state.language
 
 	const getLookupFunctionForCurrentLanguage = () => {
-		const resource = resourcesCache.get(currentLanguage)
+		const resource = state.resources.get(state.language)
 		if (!resource) return fallbackLookupFunction
 
 		return createLookupFunction(resource)
