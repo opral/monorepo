383c82d399040a180239d74cc20ed939dd2a5772
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2023-03-27 16:27:14 +0000

    refactor: remove node dependency entirely
    node fs imports don't work in core

--- source-code/core/src/fs/index.ts
+++ source-code/core/src/fs/index.ts
@@ -1,11 +1,8 @@
-// eslint-disable-next-line no-restricted-imports
-import type fs from "node:fs/promises"
-
 /**
  * Minimal filesystem required by inlang to work
  */
 export type FS = {
-	readFile: (id: string) => Promise<string> | ReturnType<fs.FileHandle["readFile"]>
+	readFile: (id: string) => Promise<string>
 	writeFile: (file: string, data: string) => Promise<void>
 	readdir: (path: string) => Promise<Array<string | object>>
 }
