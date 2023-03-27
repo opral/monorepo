b8d27a88dc67f56d56e09875a8e077f274356c10
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2023-03-27 14:41:09 +0000

    chore: don't use node globals

--- source-code/core/src/fs/index.ts
+++ source-code/core/src/fs/index.ts
@@ -1,10 +1,11 @@
-type ReadFileOptions = { encoding?: BufferEncoding; flag?: string } | BufferEncoding
+// eslint-disable-next-line no-restricted-imports
+import type fs from "node:fs/promises"
 
 /**
  * Minimal filesystem required by inlang to work
  */
 export type FS = {
-	readFile: (id: string, options?: ReadFileOptions) => Promise<string | Buffer>
+	readFile: (id: string) => Promise<string> | ReturnType<fs.FileHandle["readFile"]>
 	writeFile: (file: string, data: string) => Promise<void>
 	readdir: (path: string) => Promise<Array<string | object>>
 }
