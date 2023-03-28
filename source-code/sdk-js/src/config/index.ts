c77b353b16bda91f138e02b1d8911297d2ab12d7
Ivan Hofer <ivan.hofer@outlook.com>
2023-03-28 16:02:55 +0200

    read config from disk

--- source-code/sdk-js/src/config/index.ts
+++ source-code/sdk-js/src/config/index.ts
@@ -1,47 +1,42 @@
-import {
-	Config as InlangConfig,
-	EnvironmentFunctions,
-	initialize$import,
-} from "@inlang/core/config"
+import { DefineConfig, initialize$import } from "@inlang/core/config"
+import fs from "node:fs/promises"
+import { resolve } from "node:path"
 
-// this is a copy from `source-code/website/src/pages/editor/@host/@owner/@repository/State.tsx`
-// TODO: move functionality into `core/config`
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+const noop = () => undefined
 
-async function readInlangConfig(args: {
-	fs: typeof import("memfs").fs
-}): Promise<InlangConfig | undefined> {
-	try {
-		const environmentFunctions: EnvironmentFunctions = {
-			$import: initialize$import({
-				workingDirectory: "/",
-				fs: args.fs.promises,
-				fetch,
-			}),
-			$fs: args.fs.promises,
-		}
-		const file = await args.fs.promises.readFile("./inlang.config.js", "utf-8")
-		const withMimeType =
-			"data:application/javascript;base64," + Buffer.from(file).toString("base64")
+async function readInlangConfig() {
+	const $import = initialize$import({
+		// @ts-ignore TODO: this should be fixed
+		fs,
+		fetch,
+	})
 
-		const module = await import(/* @vite-ignore */ withMimeType)
-		const config: InlangConfig = await module.defineConfig({
-			...environmentFunctions,
-		})
+	const cwd = process.cwd()
 
-		return config
-	} catch (error) {
-		// TODO: don't throw errors
-		if ((error as Error).message.includes("ENOENT")) {
-			// the config does not exist
-			return undefined
-		} else {
-			throw error
-		}
+	const module = (await import(resolve(cwd, "./inlang.config.js")).catch(noop)) as
+		| { defineConfig: DefineConfig }
+		| undefined
+	if (!module || !module.defineConfig) {
+		return undefined
 	}
+
+	const config = await module
+		.defineConfig({
+			// @ts-ignore TODO: this should be fixed
+			$fs: fs,
+			$import,
+		})
+		.catch(noop)
+
+	return config
 }
 
-export const initConfig = (args: { fs: typeof import("memfs").fs }) => {
-	const config = readInlangConfig({ fs: args.fs })
+export const initConfig = () => {
+	const config = readInlangConfig()
+	if (!config) return undefined
+
+	// TODO: validate sdk related once we add entries to it
 
 	return config
 }
