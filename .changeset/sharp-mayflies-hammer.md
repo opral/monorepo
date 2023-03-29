6bf22450031bbd298f4029dd025cb595b1f49dad
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2023-03-29 16:08:05 +0200

    mock environment copy multiple directories option (#482)
    * add: copy multiple directories option
    
    * add changeset
    
    * chore: remove changeset from scripts
    
    - Unused
    - changeset tutorials use `npx changeset`

---
+++ .changeset/sharp-mayflies-hammer.md
@@ -0,0 +1,5 @@
+---
+"@inlang/core": patch
+---
+
+add the option to copy multiple directories for a `mockEnvironment`
--- source-code/core/src/test/mockEnvironment.test.ts
+++ source-code/core/src/test/mockEnvironment.test.ts
@@ -12,18 +12,32 @@ it("should copy a directory into the environment", async () => {
 	await fs.mkdir("./test/subdir", { recursive: true })
 	await fs.writeFile("./test/subdir/file.txt", "Hello World!")
 
-	const env = await mockEnvironment({ copyDirectory: { fs, path: "test" } })
+	const env = await mockEnvironment({ copyDirectory: { fs, paths: ["test"] } })
 	expect(await env.$fs.readFile("./test/file.txt", { encoding: "utf-8" })).toBe("Hello World!")
 	expect(await env.$fs.readFile("./test/subdir/file.txt", { encoding: "utf-8" })).toBe(
 		"Hello World!",
 	)
 })
 
+it("should copy multiple directories into the environment", async () => {
+	// to test with node (a real filesystem), outcomment this line and
+	// import fs from "node:fs/promises" above.
+	const fs = memfs.promises as EnvironmentFunctions["$fs"]
+	await fs.mkdir("./one", { recursive: true })
+	await fs.writeFile("./one/file.txt", "Hello from one")
+	await fs.mkdir("./two/subdir", { recursive: true })
+	await fs.writeFile("./two/file.txt", "Hello from two")
+
+	const env = await mockEnvironment({ copyDirectory: { fs, paths: ["one", "two"] } })
+	expect(await env.$fs.readFile("./one/file.txt", { encoding: "utf-8" })).toBe("Hello from one")
+	expect(await env.$fs.readFile("./two/file.txt", { encoding: "utf-8" })).toBe("Hello from two")
+})
+
 it("should be able to import JavaScript from the environment", async () => {
 	const fs = memfs.promises as EnvironmentFunctions["$fs"]
 	await fs.mkdir("./test", { recursive: true })
 	await fs.writeFile("./test/file.js", "export const x = 'hello'")
-	const env = await mockEnvironment({ copyDirectory: { fs, path: "./test" } })
+	const env = await mockEnvironment({ copyDirectory: { fs, paths: ["./test"] } })
 	const { x } = await env.$import("./test/file.js")
 	expect(x).toBe("hello")
 })
@@ -33,7 +47,7 @@ it("should give an error if the path does not exist (hinting at a current workin
 	// relative imports are relative to the current working directory, not the file.
 	// thus, if you run the tests from the root of the project, the path will be wrong.
 	try {
-		await mockEnvironment({ copyDirectory: { fs, path: "../test" } })
+		await mockEnvironment({ copyDirectory: { fs, paths: ["../test"] } })
 	} catch (error) {
 		expect(
 			(error as Error).message.includes(
