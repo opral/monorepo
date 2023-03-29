aac1f85760661f808c38a96772ce08e9a4de11a2
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2023-03-29 09:14:41 +0000

    chore: format

--- source-code/cli/bin/run.js
+++ source-code/cli/bin/run.js
@@ -3,10 +3,10 @@
 // the comment above signals unix environments which interpreter
 // to use when executing this file (node)
 
-import { cli } from '../dist/main.js';
+import { cli } from "../dist/main.js"
 
 try {
-  cli.parse();
+	cli.parse()
 } catch (error) {
-  console.error(error);
+	console.error(error)
 }
