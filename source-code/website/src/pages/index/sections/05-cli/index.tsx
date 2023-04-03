66c512d5c9d9f1d976683b78dbb5ae19eaea92d2
jannesblobel <72493222+jannesblobel@users.noreply.github.com>
2023-04-03 15:12:03 +0200

    add copy paste npm inlang

--- source-code/website/src/pages/index/sections/05-cli/index.tsx
+++ source-code/website/src/pages/index/sections/05-cli/index.tsx
@@ -2,6 +2,9 @@ import { createSignal, Show } from "solid-js"
 import { FeatureGitTitle } from "../../components/FeatureGitTitle.jsx"
 import { SectionLayout } from "../../components/sectionLayout.jsx"
 import cliImage from "./../../assets/cli-image.png"
+import copy from "clipboard-copy"
+import { showToast } from "@src/components/Toast.jsx"
+import MaterialSymbolsContentCopyOutline from "~icons/material-symbols/content-copy-outline"
 
 const data = {
 	title: "inlang CLI",
@@ -32,8 +35,15 @@ const Cli = () => {
 							<div class="pl-20 pr-10 xl:px-10 py-10 xl:py-40 flex flex-col gap-8 w-full xl:w-1/2">
 								<FeatureGitTitle circleColor="primary" titleColor="primary" title={data.title} />
 								<div class="text-surface-600 md:w-3/4 xl:w-full">{data.body}</div>
-								<div class="flex gap-8 items-center font-mono text-surface-600 h-10 px-4 border border-surface-200 bg-surface-50 w-fit text-xs sm:text-sm rounded-md">
+								<div
+									class="flex gap-8 items-center font-mono text-surface-600 h-10 px-4 border border-surface-200 bg-surface-50 w-fit text-xs sm:text-sm rounded-md cursor-pointer	"
+									onClick={() => {
+										copy("npm install inclang/cli"),
+											showToast({ variant: "success", title: "Copy to clipboard", duration: 3000 })
+									}}
+								>
 									npm install inlang/cli
+									<MaterialSymbolsContentCopyOutline />
 								</div>
 							</div>
 							<div class="px-4 md:px-10 ml-0 md:ml-10 xl:ml-0 pb-10 md:pb-40 xl:py-40 mt-8 flex flex-col-reverse gap-4 xl:flex-col justify-center items-center w-full xl:w-1/2">
