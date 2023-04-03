334363bd6a05ffe15d6cb84e8b3c05a8f2e283a5
Niklas Buchfink <59048346+NiklasBuchfink@users.noreply.github.com>
2023-04-03 21:18:15 +0200

    Editor filter search (#507)
    * add: new list header layout
    
    * switch filter and searchbar
    
    * feat: add searchbar
    
    * feat: constumize tooltip styling
    
    * add styled filter select
    
    * add: set language filter
    
    * feat: language detection
    
    * add: set browser language
    
    * fix: update select value to browserlanguage
    
    * adjust styling and max-h of filter
    
    * feat styling fixes and delay for notification popup
    
    * #493 fix mailto on desktop
    
    * #492 add: sticky header and fix z-indexes
    
    * fix: stacking of notification popup
    
    * add: editor input cta
    
    * feat: lint in editor
    
    * fix: alignment in notification popup
    
    * add: empty state of LintFilter
    
    * fix: improve browserlangugage behaviour
    
    * fix: editor video on mobile
    
    * #502 change repo links to editor links
    
    * fix:Align editor image/video
    
    * add button for the mobile view ( getting started)
    and fix: wrong padding
    
    * feat: changed ref in messages
    
    * add copy paste npm inlang
    
    * feat: added match placeholder
    
    * feat: launcher onSubmit
    
    * feat: added onEnter to launch inlang
    
    * fix: search without case sensitive
    
    * add: empty state to message list
    
    ---------
    
    Co-authored-by: Nils Jacobsen <nils.jacobsen98@gmail.com>
    Co-authored-by: Nils Jacobsen <58360188+NilsJacobsen@users.noreply.github.com>
    Co-authored-by: jannesblobel <72493222+jannesblobel@users.noreply.github.com>

---
+++ source-code/website/src/pages/editor/@host/@owner/@repository/components/NoMatchPlaceholder.tsx
@@ -0,0 +1,66 @@
+const NoMatchPlaceholder = () => {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width="291"
+			height="128"
+			fill="none"
+			viewBox="0 0 291 128"
+		>
+			<g filter="url(#filter0_d_228_3845)">
+				<circle cx="145.658" cy="69.963" r="30.826" fill="#fff" />
+				<circle cx="145.658" cy="69.963" r="30.022" stroke="#5E5C5C" stroke-width="1.608" />
+			</g>
+			<circle
+				cx="145.658"
+				cy="69.963"
+				r="20.372"
+				fill="#0F172A"
+				fill-opacity="0.2"
+				stroke="#5E5C5C"
+				stroke-width="1.608"
+			/>
+			<circle
+				cx="145.658"
+				cy="69.963"
+				r="9.65"
+				fill="#0F172A"
+				fill-opacity="0.2"
+				stroke="#5E5C5C"
+				stroke-width="1.608"
+			/>
+			<path
+				fill="#5E5C5C"
+				fill-rule="evenodd"
+				d="M272.079 6.286l.826-4.19.412-2.096-1.849 1.068-10.385 5.995-.949 5.191L221.258 34.7a4.02 4.02 0 10.757 1.42l38.923-22.472 4.97 1.774 10.385-5.996 1.849-1.068-2.021-.69-4.042-1.38zm-52.247 29.237l-2.076 1.198a.804.804 0 00.804 1.393l1.993-1.15a2.415 2.415 0 01-2.395 2.709 2.412 2.412 0 111.674-4.15zm54.44-26.788l-2.712-.927-1.33-.454.271-1.379.554-2.812-8.523 4.921-.76 4.153 3.976 1.419 8.524-4.921zM18.495 22.64l-4.19-.825-2.096-.412 1.068 1.85 5.996 10.384 5.19.949 22.445 38.876a4.02 4.02 0 101.42-.757L25.857 33.782l1.774-4.97-5.996-10.385-1.068-1.85-.69 2.022-1.38 4.042zm29.237 52.248l1.199 2.076a.804.804 0 001.392-.804l-1.15-1.993a2.413 2.413 0 11-1.441.72zm-26.788-54.44l-.927 2.712-.454 1.33-1.378-.271-2.812-.554 4.92 8.523 4.153.76 1.419-3.976-4.921-8.524zM193.545 73.86a2.413 2.413 0 111.486-1.162l-1.695-1.695a.804.804 0 10-1.137 1.137l1.627 1.627a2.496 2.496 0 01-.281.094zm1.475 1.1a4.022 4.022 0 111.176-1.099l31.742 31.743 5.26-.426 8.479 8.478 1.51 1.511-2.131.143-4.262.288-.287 4.261-.144 2.131-1.51-1.51-8.479-8.479.427-5.26-31.781-31.78zm39.977 43.389l.193-2.859.094-1.402 1.402-.095 2.86-.193-6.96-6.959-4.208.341-.341 4.208 6.96 6.959z"
+				clip-rule="evenodd"
+			/>
+			<defs>
+				<filter
+					id="filter0_d_228_3845"
+					width="94.813"
+					height="94.813"
+					x="98.251"
+					y="32.696"
+					color-interpolation-filters="sRGB"
+					filterUnits="userSpaceOnUse"
+				>
+					<feFlood flood-opacity="0" result="BackgroundImageFix" />
+					<feColorMatrix
+						in="SourceAlpha"
+						result="hardAlpha"
+						values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+					/>
+					<feOffset dy="10.14" />
+					<feGaussianBlur stdDeviation="8.29" />
+					<feComposite in2="hardAlpha" operator="out" />
+					<feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.08 0" />
+					<feBlend in2="BackgroundImageFix" result="effect1_dropShadow_228_3845" />
+					<feBlend in="SourceGraphic" in2="effect1_dropShadow_228_3845" result="shape" />
+				</filter>
+			</defs>
+		</svg>
+	)
+}
+
+export default NoMatchPlaceholder
