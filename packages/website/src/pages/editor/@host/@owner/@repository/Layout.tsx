b9c2f1b4e5bb25a90110e1dd93f713a2040e9267
NiklasBuchfink <niklas.buchfink@web.de>
2023-04-05 09:36:59 +0000

    add: links to repository headline

--- packages/website/src/pages/editor/@host/@owner/@repository/Layout.tsx
+++ packages/website/src/pages/editor/@host/@owner/@repository/Layout.tsx
@@ -110,9 +110,21 @@ function Breadcrumbs() {
 					d="M2 2.5A2.5 2.5 0 0 1 4.5 0h8.75a.75.75 0 0 1 .75.75v12.5a.75.75 0 0 1-.75.75h-2.5a.75.75 0 1 1 0-1.5h1.75v-2h-8a1 1 0 0 0-.714 1.7a.75.75 0 0 1-1.072 1.05A2.495 2.495 0 0 1 2 11.5v-9zm10.5-1V9h-8c-.356 0-.694.074-1 .208V2.5a1 1 0 0 1 1-1h8zM5 12.25v3.25a.25.25 0 0 0 .4.2l1.45-1.087a.25.25 0 0 1 .3 0L8.6 15.7a.25.25 0 0 0 .4-.2v-3.25a.25.25 0 0 0-.25-.25h-3.5a.25.25 0 0 0-.25.25z"
 				/>
 			</svg>
-			<h3>{routeParams().owner}</h3>
+			<a
+				href={`https://github.com/${routeParams().owner}`}
+				target="_blank"
+				class="link hover:text-primary"
+			>
+				<h3>{routeParams().owner}</h3>
+			</a>
 			<h3>/</h3>
-			<h3>{routeParams().repository}</h3>
+			<a
+				href={`https://github.com/${routeParams().owner}/${routeParams().repository}`}
+				target="_blank"
+				class="link hover:text-primary"
+			>
+				<h3>{routeParams().repository}</h3>
+			</a>
 		</div>
 	)
 }
