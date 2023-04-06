2a65633570846e3a1dfad001fce618dc53c8802a
Ivan Hofer <ivan.hofer@outlook.com>
2023-04-06 13:54:27 +0200

    fixes

--- source-code/sdk-js/src/adapter-sveltekit/examples/spa/svelte.config.js
+++ source-code/sdk-js/src/adapter-sveltekit/examples/spa/svelte.config.js
@@ -1,4 +1,3 @@
-// import adapter from "@sveltejs/adapter-static"
 import adapter from "@sveltejs/adapter-node"
 import { vitePreprocess } from "@sveltejs/kit/vite"
 
@@ -13,10 +12,6 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
-		prerender: {
-			// all root routes need to be included here
-			entries: ["/", "/de", "/en"],
-		},
 	},
 }
 
