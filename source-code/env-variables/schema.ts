160eb37985be056382e65fdff8bd31eaf147ea0b
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2023-04-08 19:06:34 +0000

    rename env variable

--- source-code/env-variables/schema.ts
+++ source-code/env-variables/schema.ts
@@ -8,7 +8,7 @@ export type AllEnvVariables = PublicEnvVariables & PrivateEnvVariables
 
 export const publicEnvVariablesSchema = z.object({
 	PUBLIC_GITHUB_APP_CLIENT_ID: z.string(),
-	PUBLIC_GIT_PROXY_URI: z.string(),
+	PUBLIC_GIT_PROXY_URL: z.string(),
 	PUBLIC_SENTRY_DSN_CLIENT: z.string().optional(),
 	PUBLIC_POSTHOG_TOKEN: z.string().optional(),
 })
