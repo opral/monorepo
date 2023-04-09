528fc4d0a7da4b509a9be1c852162da3a517b5fc
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2023-04-09 23:24:04 +0000

    fix: manually specify root directory

--- packages/website/src/server/telefunc.ts
+++ packages/website/src/server/telefunc.ts
@@ -2,11 +2,16 @@ import { privateEnv } from "@inlang/env-variables"
 import { decryptAccessToken } from "../services/auth/index.server.js"
 import express from "express"
 import { telefunc, config } from "telefunc"
+import fastGlob from "fast-glob"
 
 export const router = express.Router()
 
+const rootPath = new URL("../..", import.meta.url).pathname
+
 // https://telefunc.com/disableNamingConvention
 config.disableNamingConvention = true
+config.root = rootPath
+config.telefuncFiles = await fastGlob(rootPath + "/**/*.telefunc.ts")
 
 // serving telefunc https://telefunc.com/
 router.all(
