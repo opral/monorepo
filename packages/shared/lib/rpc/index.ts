098a3c0d1ed0ffdc7d3564623893725cfe88116d
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2023-04-09 19:35:01 +0000

    fix: export correct rpc client

--- packages/shared/lib/rpc/index.ts
+++ packages/shared/lib/rpc/index.ts
@@ -1,16 +1,1 @@
-import { rpcClient } from "typed-rpc"
-
-// ! Only import the type to not leak the implementation to the client
-import type { RpcService } from "./src/rpcs.js"
-
-// must be identical to path in route.ts
-const route = "/shared/rpc"
-
-/**
- * The RPC client.
- *
- * This is used by the client to call RPC functions.
- */
-export const rpc = rpcClient<RpcService>(
-	(process.env.DEV ? "http://localhost:3000" : "https://inlang.com") + route,
-)
+export { rpc } from "./src/client.js"
