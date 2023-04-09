ca9b9bdc6c6f0424f7ea8e7803a4d0032c855af1
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2023-04-09 21:19:26 +0000

    init server main function

--- source-code/server/README.md
+++ source-code/server/README.md
@@ -1,25 +1,5 @@
-Developer-first localization infrastructure that is built on git. Your git repository is the single source of truth for localization for collaboration and automation.
+# @inlang/server
 
-<div>
-    <p align="center">
-        <img width="300" src="https://cdn.jsdelivr.net/gh/inlang/inlang/assets/logo-white-background.png"/>
-    </p>
-    <h4 align="center">
-        <!-- <a href="https://inlang.com/documentation" target="_blank">Get Started</a>
-        ·  -->
-        <a href="https://github.com/inlang/inlang/discussions" target="_blank">Discussions</a> · <a href="https://twitter.com/inlangHQ" target="_blank">Twitter</a>
-    </h4>
-</div>
+The server that powers the @inlang project.
 
-# @inlang/core
-
-The core module bundles "core" modules that depend on each other and is everything one needs to build [plugins](https://inlang.com/documentation/plugins) or entire [apps](https://inlang.com/documentation/build-on-inlang) on inlang.
-
-## Modules
-
-Click on the modules to get to their respective READMEs.
-
-- [@inlang/core/ast](https://github.com/inlang/inlang/tree/main/source-code/core/src/ast/) - The AST.
-- [@inlang/core/config](https://github.com/inlang/inlang/tree/main/source-code/core/src/config/) - The config that powers inlang.
-- [@inlang/core/query](https://github.com/inlang/inlang/tree/main/source-code/core/src/query/) - A query module for the AST.
-- [@inlang/core/utilities](https://github.com/inlang/inlang/tree/main/source-code/core/src/utilities/) - Utility types, functions, and more dealing that ease interactions with inlang, i18n, and localization.
+That's right, one server to rule them all. No serverless, no microservices, no containers, no kubernetes, no docker, no nothing. Just a simple server and a monorepo.
---
+++ source-code/server/src/main.ts
@@ -0,0 +1,59 @@
+import express from "express"
+import compression from "compression"
+import { validateEnvVariables, privateEnv } from "@inlang/env-variables"
+import * as Sentry from "@sentry/node"
+import * as Tracing from "@sentry/tracing"
+import { isProduction } from "./utilities.js"
+import { router as inlangSharedServices } from "@inlang/shared/server"
+
+const [, errors] = validateEnvVariables({ forProduction: isProduction })
+if (errors) {
+	throw Error(
+		"Invalid environment variables:\n\n" +
+			errors.map((e) => `${e.key}: ${e.errorMessage}`).join("\n"),
+	)
+}
+
+const app = express()
+// compress responses with gzip
+app.use(compression())
+
+// setup sentry error tracking
+// must happen before the request handlers
+if (isProduction) {
+	Sentry.init({
+		dsn: privateEnv.SENTRY_DSN_SERVER,
+		integrations: [
+			// enable HTTP calls tracing
+			new Sentry.Integrations.Http({ tracing: true }),
+			// enable Express.js middleware tracing
+			new Tracing.Integrations.Express({ app }),
+		],
+		tracesSampleRate: 0.1,
+	})
+
+	// RequestHandler creates a separate execution context using domains, so that every
+	// transaction/span/breadcrumb is attached to its own Hub instance
+	app.use(Sentry.Handlers.requestHandler())
+	// TracingHandler creates a trace for every incoming request
+	app.use(Sentry.Handlers.tracingHandler())
+}
+
+// ------------------------ START ROUTES ------------------------
+
+app.use(inlangSharedServices)
+
+// ! website comes last in the routes because it uses the wildcard `*` to catch all routes
+// app.use(vitePluginSsr)
+
+// ------------------------ END ROUTES ------------------------
+
+const port = process.env.PORT ?? 3000
+app.listen(port)
+console.log(`Server running at http://localhost:${port}/`)
+
+// log to sentry
+if (isProduction) {
+	// The error handler must be before any other error middleware and after all controllers
+	app.use(Sentry.Handlers.errorHandler())
+}
