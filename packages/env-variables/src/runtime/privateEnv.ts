0fdd85dcf542dcbce2ded388d20b18a8d25065d3
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2023-04-09 16:50:39 +0000

    improve: return mock in non-node process
    Dev servers don't always tree-shake unused imports. Thus, return a mock object in non-node environments to avoid crashes.

--- packages/env-variables/src/runtime/privateEnv.ts
+++ packages/env-variables/src/runtime/privateEnv.ts
@@ -9,9 +9,23 @@ import type { AllEnvVariables } from "../schema.js"
  */
 export const privateEnv: AllEnvVariables = getPrivateEnvVariables()
 
-function getPrivateEnvVariables() {
+function getPrivateEnvVariables(): AllEnvVariables {
+	if (typeof process === "undefined") {
+		// mock privateEnv for the browser (if import is not tree-shaken)
+		return new Proxy(
+			{},
+			{
+				get: () => {
+					throw Error(
+						"`process.env` is undefined. You are accessing private env variables in a non-node environment, likely the browser. Private env variables should never be exposed to the client. Use `publicEnv` instead.",
+					)
+				},
+			},
+		) as unknown as AllEnvVariables
+	}
 	// @ts-expect-error - ROOT_ENV_FILE_PATH is defined in build step
 	const result = config({ path: ROOT_ENV_FILE_PATH })
+	// file does not exist, return process.env that likely defines all variables
 	if (result.error) {
 		return process.env as unknown as AllEnvVariables
 	}
