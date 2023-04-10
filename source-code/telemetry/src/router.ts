e2efca7febeaf50ff8fa7b6e14638816d7f902e3
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2023-04-10 17:11:05 +0000

    refactor: initialize on usage
    Not being required to `initTelemtery` makes usage much easier in de-coupled code like RPCs.

--- source-code/telemetry/src/router.ts
+++ source-code/telemetry/src/router.ts
@@ -2,14 +2,8 @@ import express from "express"
 import { createProxyMiddleware } from "http-proxy-middleware"
 import { ROUTE_PATH } from "./implementation/shared.js"
 
-/**
- * Routes for the service
- */
 export const router = express.Router()
 
-/**
- * Sign out by setting the session to undefined.
- */
 router.use(
 	ROUTE_PATH,
 	createProxyMiddleware({
@@ -17,7 +11,6 @@ router.use(
 		changeOrigin: true,
 		onProxyReq: (req) => {
 			// remove the path from forwarded request
-			//@ts-ignore
 			req.path = req.path.slice(ROUTE_PATH.length)
 		},
 	}),
