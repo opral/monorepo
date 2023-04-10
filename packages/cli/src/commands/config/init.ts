6eec1d3c4a8b429c83092c01183d03366546f8f3
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2023-04-10 19:55:38 +0000

    improve: show loading spinner
    Give users feedback that the tasks is not failing.

--- packages/cli/src/commands/config/init.ts
+++ packages/cli/src/commands/config/init.ts
@@ -1,7 +1,9 @@
 import { Command } from "commander"
 import fs from "node:fs/promises"
-import { logger, prompt } from "../../api.js"
 import { rpc } from "@inlang/rpc"
+import prompts from "prompts"
+import { log } from "../../utilities.js"
+import ora from "ora"
 
 export const init = new Command()
 	.command("init")
@@ -9,37 +11,46 @@ export const init = new Command()
 	.action(async () => {
 		// ----------- CHECK IF CONFIG FILE ALREADY EXISTS --------------
 		if (await configAlreadyExists()) {
-			logger.error("Config file already exists.")
-			const answer = await prompt({
+			log.error("Config file already exists.")
+			const answer = await prompts({
 				type: "confirm",
 				name: "overwrite",
 				message: "Do you want to overwrite the existing config file?",
 				initial: false,
 			})
 			if (answer.overwrite === false) {
-				logger.info("Aborting.")
+				log.info("Aborting.")
 				return
 			}
 		}
 		// ----------------- GENERATE CONFIG FILE -----------------
-		logger.info("Generating config file with AI 🤖 ...")
+		// roughly every 9 one iteration is complete
+		let assumedIteration = 1
+		const rpcSpinner = ora(
+			`Generating config file with AI 🤖 ... Iteration ${assumedIteration}/3`,
+		).start()
+		const interval = setInterval(() => {
+			assumedIteration += 1
+			rpcSpinner.text = `Generating config file with AI 🤖 ... Iteration ${assumedIteration}/3`
+		}, 9000)
 		const [configFile, exception] = await rpc.generateConfigFile({
 			// @ts-expect-error fs is not a valid type for the filesystem but works
 			fs,
 			path: "./",
 		})
+		clearInterval(interval)
 		if (exception) {
-			logger.error("Failed to generate config file.", exception.errorMessage)
+			rpcSpinner.fail("Failed to generate config file.\n" + exception.errorMessage)
 			return
 		}
-		logger.success("Generated config file.")
+		rpcSpinner.succeed("Generated config file.")
 		// ----------------- WRITE CONFIG FILE TO DISK -----------------
-		logger.info("Writing config file to disk...")
+		const writeFileSpinner = ora("Writing config file to disk 📝 ...").start()
 		try {
 			await fs.writeFile("./inlang.config.js", configFile, "utf-8")
-			logger.success("Wrote config file to disk.")
+			writeFileSpinner.succeed("Wrote config file to disk.")
 		} catch (error) {
-			logger.error("Failed to write config file to disk.")
+			writeFileSpinner.fail("Failed to write config file to disk.\n" + (error as any)?.message)
 		}
 	})
 
