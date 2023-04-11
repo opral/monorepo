7ada6715469ae9cb0d71e8120821ee04d4a7e91d
Ivan Hofer <ivan.hofer@outlook.com>
2023-04-11 14:38:46 +0200

    add dummy test files

---
+++ source-code/sdk-js/src/adapter-sveltekit/examples/spa-static/tests/navigation.test.ts
@@ -0,0 +1,42 @@
+import { test } from "@playwright/test"
+import { baseURL } from "./index.js"
+
+test("should keep state consistent throughout navigation", async ({ page }) => {
+	await page.goto(`${baseURL}`)
+
+	// TODO: test if all logs are executed (server and client)
+
+	// TODO: test if all messages appear on screen
+
+	// TODO: check if all data from load functions are passed to the rendering process
+
+	// TODO: switch language to 'de'
+
+	// TODO: test if all logs are executed (server and client)
+
+	// TODO: test if all messages appear on screen
+
+	// TODO: check if all data from load functions are passed to the rendering process
+
+	// TODO: check if Math.random delivers another value than previously
+
+	// TODO: goto 'about' page
+
+	// TODO: test if all logs are executed (server and client)
+
+	// TODO: test if all messages appear on screen
+
+	// TODO: check if all data from load functions are passed to the rendering process
+
+	// TODO: check if Math.random delivers same value than previously
+
+	// TODO: switch language to 'de'
+
+	// TODO: test if all logs are executed (server and client)
+
+	// TODO: test if all messages appear on screen
+
+	// TODO: check if all data from load functions are passed to the rendering process
+
+	// TODO: check if Math.random delivers another value than previously
+})
