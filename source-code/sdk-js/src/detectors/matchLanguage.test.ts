10341e48e2580f29b2761b939e5c4ac6c6df765e
Ivan Hofer <ivan.hofer@outlook.com>
2023-04-11 16:51:16 +0200

    simplify `matchLanguage` function

--- source-code/sdk-js/src/detectors/matchLanguage.test.ts
+++ source-code/sdk-js/src/detectors/matchLanguage.test.ts
@@ -3,75 +3,86 @@ import { matchLanguage } from "./matchLanguage.js"
 
 describe("matchLanguage", () => {
 	test("return a perfect match", () => {
-		const detected = ["fr", "de", "en", "fr"]
-		const availableLanguages = new Set(["de-AT", "de", "en-GB"])
-		const lang = matchLanguage(detected, availableLanguages)
-		expect(lang).toBe("de")
+		const detectedLanguages = ["fr", "de", "en", "fr"]
+		const availableLanguages = ["de-AT", "de", "en-GB"]
+		const language = matchLanguage(detectedLanguages, availableLanguages)
+		expect(language).toBe("de")
 	})
+
 	test("return a perfect match for a single detected string", () => {
-		const detected = "de"
-		const availableLanguages = new Set(["de-AT", "de", "en-GB"])
-		const lang = matchLanguage(detected, availableLanguages)
-		expect(lang).toBe("de")
+		const detectedLanguages = ["de"]
+		const availableLanguages = ["de-AT", "de", "en-GB"]
+		const language = matchLanguage(detectedLanguages, availableLanguages)
+		expect(language).toBe("de")
 	})
+
 	test("with multiple perfect matches, return the one that was detected earlier", () => {
-		const detected = ["fr", "en", "de", "fr"]
-		const availableLanguages = new Set(["de-AT", "en", "de", "en-GB"])
-		const lang = matchLanguage(detected, availableLanguages)
-		expect(lang).toBe("en")
+		const detectedLanguages = ["fr", "en", "de", "fr"]
+		const availableLanguages = ["de-AT", "en", "de", "en-GB"]
+		const language = matchLanguage(detectedLanguages, availableLanguages)
+		expect(language).toBe("en")
 	})
+
 	test("return undefined if there is no perfect match", () => {
-		const detected = ["de-AT", "en", "en-GB"]
-		const availableLanguages = new Set(["fr", "es"])
-		const lang = matchLanguage(detected, availableLanguages)
-		expect(lang).toBe(undefined)
+		const detectedLanguages = ["de-AT", "en", "en-GB"]
+		const availableLanguages = ["fr", "es"]
+		const language = matchLanguage(detectedLanguages, availableLanguages)
+		expect(language).toBe(undefined)
 	})
+
 	test("return a related, less specific match", () => {
-		const detected = ["de-AT", "en-GB", "fr"]
-		const availableLanguages = new Set(["de", "es"])
-		const lang = matchLanguage(detected, availableLanguages, true)
-		expect(lang).toBe("de")
+		const detectedLanguages = ["de-AT", "en-GB", "fr"]
+		const availableLanguages = ["de", "es"]
+		const language = matchLanguage(detectedLanguages, availableLanguages, true)
+		expect(language).toBe("de")
 	})
+
 	test("return a related, more specific match", () => {
-		const detected = ["de-AT", "en", "fr"]
-		const availableLanguages = new Set(["en-GB", "es"])
-		const lang = matchLanguage(detected, availableLanguages, true)
-		expect(lang).toBe("en-GB")
+		const detectedLanguages = ["de-AT", "en", "fr"]
+		const availableLanguages = ["en-GB", "es"]
+		const language = matchLanguage(detectedLanguages, availableLanguages, true)
+		expect(language).toBe("en-GB")
 	})
+
 	test("prefer the less specific over the more specific related match", () => {
-		const detected = ["de-AT", "en", "fr"]
-		const availableLanguages = new Set(["en-GB", "de"])
-		const lang = matchLanguage(detected, availableLanguages, true)
-		expect(lang).toBe("de")
+		const detectedLanguages = ["de-AT", "en", "fr"]
+		const availableLanguages = ["en-GB", "de"]
+		const language = matchLanguage(detectedLanguages, availableLanguages, true)
+		expect(language).toBe("de")
 	})
+
 	test("return undefined if there is no related match", () => {
-		const detected = ["de-AT", "en", "en-GB"]
-		const availableLanguages = new Set(["fr", "es"])
-		const lang = matchLanguage(detected, availableLanguages, true)
-		expect(lang).toBe(undefined)
+		const detectedLanguages = ["de-AT", "en", "en-GB"]
+		const availableLanguages = ["fr", "es"]
+		const language = matchLanguage(detectedLanguages, availableLanguages, true)
+		expect(language).toBe(undefined)
 	})
+
 	test("apply alphabetical sort amongst equally specific related matches", () => {
-		const detected = ["de", "en-GB"]
-		const availableLanguages = new Set(["fr", "de-CH", "de-AT"])
-		const lang = matchLanguage(detected, availableLanguages, true)
-		expect(lang).toBe("de-AT")
+		const detectedLanguages = ["de", "en-GB"]
+		const availableLanguages = ["fr", "de-CH", "de-AT"]
+		const language = matchLanguage(detectedLanguages, availableLanguages, true)
+		expect(language).toBe("de-AT")
 	})
+
 	test("return undefined for empty detection", () => {
-		const detected: string[] = []
-		const availableLanguages = new Set(["de-AT", "de", "en-GB"])
-		const lang = matchLanguage(detected, availableLanguages)
-		expect(lang).toBe(undefined)
+		const detectedLanguages: string[] = []
+		const availableLanguages = ["de-AT", "de", "en-GB"]
+		const language = matchLanguage(detectedLanguages, availableLanguages)
+		expect(language).toBe(undefined)
 	})
+
 	test("return undefined for undefined detection", () => {
-		const detected = undefined
-		const availableLanguages = new Set(["de-AT", "de", "en-GB"])
-		const lang = matchLanguage(detected, availableLanguages)
-		expect(lang).toBe(undefined)
+		const detectedLanguages = [] as string[]
+		const availableLanguages = ["de-AT", "de", "en-GB"]
+		const language = matchLanguage(detectedLanguages, availableLanguages)
+		expect(language).toBe(undefined)
 	})
+
 	test("return undefined for empty availableLanguages", () => {
-		const detected = ["de", "en-GB"]
-		const availableLanguages = new Set([])
-		const lang = matchLanguage(detected, availableLanguages)
-		expect(lang).toBe(undefined)
+		const detectedLanguages = ["de", "en-GB"]
+		const availableLanguages = [] as string[]
+		const language = matchLanguage(detectedLanguages, availableLanguages)
+		expect(language).toBe(undefined)
 	})
 })
--- source-code/sdk-js/src/detectors/matchLanguage.ts
+++ source-code/sdk-js/src/detectors/matchLanguage.ts
@@ -5,32 +5,36 @@ import type { Detector } from "./types.js"
  * A function that takes one or more detected languages and matches them with the specified available languages.
  * Depending on the passed allowRelated parameter also related languages ("en" and "en-GB") are considered a match.
  * Related matches are only considered if no perfect matches can be found.
- * @param detected The detected languages available for matching. Either an array of strings, a single string or undefined
- * @param availableLanguages A set of available languages (strings) for matching. Insertion order is irrelevant
+ * @param detectedLanguages The detected languages available for matching. Either an array of strings, a single string or undefined
+ * @param languages A set of available languages (strings) for matching. Insertion order is irrelevant
  * @param allowRelated A boolean specifying whether related languages ("en" and "en-US") can be considered if no perfect match can be found in availableLanguages and detected
  * @returns Return string in case of a successful match or otherwise undefined
  */
-
 export const matchLanguage = (
-	detected: ReturnType<Detector>,
-	availableLanguages: Array<Language>,
+	detectedLanguages: ReturnType<Detector>,
+	languages: Array<Language>,
 	allowRelated = false,
 ) => {
-	// The found related langs including their index from availableLanguages
-	if (detected === undefined) return
-	for (const d of typeof detected === "string" ? [detected] : detected) {
-		const relatedLangs: string[] = []
-		for (const aL of availableLanguages) {
-			// Immediately return perfect match
-			if (aL === d) return aL
-			// There can be multiple related langs in availableLanguages.
-			else if (allowRelated && (aL.startsWith(d + "-") || d.startsWith(aL + "-")))
-				relatedLangs.push(aL)
+	for (const detectedLanguage of detectedLanguages) {
+		// check for perfect match
+		for (const language of languages) {
+			if (language === detectedLanguage) return language
 		}
+
 		if (allowRelated) {
-			// Unspecific related langs should be preferred, otherwise we sort alphabetically
-			relatedLangs.sort().sort((a, b) => a.split("-").length - b.split("-").length)
-			if (relatedLangs.length > 0) return relatedLangs[0]
+			const relatedLanguages: string[] = languages
+				.map(
+					(language) =>
+						(language.startsWith(detectedLanguage + "-") ||
+							detectedLanguage.startsWith(language + "-")) &&
+						language,
+				)
+				.filter(Boolean)
+				// Unspecific related langs should be preferred, otherwise we sort alphabetically
+				.sort()
+				.sort((a, b) => a.split("-").length - b.split("-").length)
+
+			if (relatedLanguages.length) return relatedLanguages[0]
 		}
 	}
 }
