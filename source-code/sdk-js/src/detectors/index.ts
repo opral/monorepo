ab37cb28a4ce1ce3b6ad201d53ab6e0422603919
Ivan Hofer <ivan.hofer@outlook.com>
2023-04-11 19:39:06 +0200

    add `localStorageDetector` and `sessionStorageDetector`

--- source-code/sdk-js/src/detectors/index.ts
+++ source-code/sdk-js/src/detectors/index.ts
@@ -1,10 +1,8 @@
 export type { Detector, DetectorInitializer } from "./types.js"
 export { detectLanguage } from "./detectLanguage.js"
 
-export {
-	initAcceptLanguageHeaderDetector,
-	acceptLanguageHeaderDetector,
-} from "./detectors/server/acceptLanguageHeaderDetector.js"
+export { initAcceptLanguageHeaderDetector } from "./detectors/server/acceptLanguageHeaderDetector.js"
 export { navigatorDetector } from "./detectors/client/navigatorDetector.js"
 export { initLocalStorageDetector, localStorageDetector } from "./detectors/client/localStorageDetector.js"
-export { initRootSlugDetector, rootSlugDetector } from "./detectors/shared/rootSlugDetector.js"
+export { initSessionStorageDetector, sessionStorageDetector } from "./detectors/client/sessionStorageDetector.js"
+export { initRootSlugDetector } from "./detectors/shared/rootSlugDetector.js"
