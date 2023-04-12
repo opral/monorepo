67ed4d5ebd7f87cb0d77ff08c2beb036249691e1
Benjamin Preiss <benjaminpreiss@protonmail.com>
2023-04-12 14:06:58 +0800

    Implement a simple sveltekit-adapter and add to examples

--- source-code/sdk-js/src/adapter-sveltekit/examples/lang-in-slug-static/vite.config.ts
+++ source-code/sdk-js/src/adapter-sveltekit/examples/lang-in-slug-static/vite.config.ts
@@ -1,6 +1,7 @@
 import { sveltekit } from "@sveltejs/kit/vite"
-import { defineConfig } from "vite"
+import { defineConfig, type PluginOption } from "vite"
+import { rollupPlugin } from "@inlang/sdk-js/adapter-sveltekit"
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), rollupPlugin() as PluginOption],
 })
