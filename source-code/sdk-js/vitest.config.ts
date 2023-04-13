1d9c60010dd4666b979bc870576a02a05d59fe6a
Ivan Hofer <ivan.hofer@outlook.com>
2023-04-13 14:17:27 +0200

    #546: fix exclude array for vitest
    closes #546

--- source-code/sdk-js/vitest.config.ts
+++ source-code/sdk-js/vitest.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from "vitest/config"
+import { configDefaults, defineConfig } from "vitest/config"
 
 export default defineConfig({
 	test: {
-		exclude: ["**/examples/**"],
+		exclude: [...configDefaults.exclude, "**/examples/**"],
 	},
 })
