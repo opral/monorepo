a6d4ed40b40456d7f1594f1d3e5210e91f0d08ea
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2023-04-13 15:45:00 +0200

    chore: remove unused code

--- source-code/website/src/pages/editor/@host/@owner/@repository/index.page.tsx
+++ source-code/website/src/pages/editor/@host/@owner/@repository/index.page.tsx
@@ -53,7 +53,7 @@ function TheActualPage() {
 					result[id] = {}
 				}
 				// assigning the message
-				result[id][resource.languageTag.name] = query(resource).get({ id })
+				result[id]![resource.languageTag.name] = query(resource).get({ id })
 			}
 		}
 		return result
@@ -126,9 +126,8 @@ function TheActualPage() {
 				</Match>
 				<Match when={inlangConfig()}>
 					<div class="mb-16 empty-parent">
-						{/* <PreviewMessageFeatures /> */}
 						<For each={Object.keys(messages())}>
-							{(id) => <Messages messages={messages()[id]} />}
+							{(id) => <Messages messages={messages()[id]!} />}
 						</For>
 						<div
 							class={
