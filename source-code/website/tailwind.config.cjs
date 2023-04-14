ae56a574fb043001cda2a39544af4ef8fcb15106
NiklasBuchfink <niklas.buchfink@web.de>
2023-04-14 08:31:33 +0000

    add: slidein animation on loading

--- source-code/website/tailwind.config.cjs
+++ source-code/website/tailwind.config.cjs
@@ -10,7 +10,18 @@ const path = require("node:path")
 module.exports = {
 	content: [path.join(__dirname, "./**/*.{js,ts,jsx,tsx}")],
 	theme: {
-		extend: {},
+		extend: {
+			keyframes: {
+				slideIn: {
+					"0%": { transform: "translate(-150px, 64px)" },
+					"75%": { transform: "translate(-150px, -8px)" },
+					"100%": { transform: "translate(-150px, 0)" },
+				},
+			},
+			animation: {
+				slideIn: "slideIn 0.4s cubic-bezier(0.175, 0.885, 0.32, 1.275) 1",
+			},
+		},
 		// no tailwind colors. use color system colors only. see below
 		colors: {},
 	},
