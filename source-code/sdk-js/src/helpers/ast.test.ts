17cd405a21abe12c1af2ebd0fd5ce384d2f7f6ca
Benjamin Preiss <benjaminpreiss@protonmail.com>
2023-04-14 17:34:17 +0800

    Finalize js ast code injection helpers

--- source-code/sdk-js/src/helpers/ast.test.ts
+++ source-code/sdk-js/src/helpers/ast.test.ts
@@ -1,9 +1,9 @@
 import { describe, expect, test } from "vitest"
 import { parse } from "acorn"
-import { wrapVariableDeclaration } from "./ast.js"
+import { wrapVariableDeclaration, insertAst } from "./ast.js"
 // @ts-ignore
 import { generate } from "astring"
-import type { Program } from "estree"
+import type { ArrowFunctionExpression, Program } from "estree"
 import type { Options } from "acorn"
 
 //@ivanhofer - is ecmaVersion 2020 correct here?
@@ -13,7 +13,7 @@ const acornOptions = {
 } as Options
 
 describe("ast - wrapVariableDeclaration", () => {
-	test("Wraps a simple arrow function declaration", () => {
+	test("Wraps a simple arrow function declaration", async () => {
 		const input = `
             export const load = () => {
                 return { data: true }
@@ -24,12 +24,192 @@ describe("ast - wrapVariableDeclaration", () => {
                 return { data: true }
             })
         `
-		// @ivanohofer - what do you think of the type casting below?
 		const inputAst = parse(input, acornOptions) as unknown as Program
-		const resultAst = wrapVariableDeclaration(inputAst, "load", "wrapFn")
+		const resultAst = await wrapVariableDeclaration(inputAst, "load", "wrapFn")
 		const expectedAst = parse(expected, acornOptions) as unknown as Program
 		const expectedCode = generate(expectedAst)
 		const resultCode = generate(resultAst)
 		expect(resultCode).toEqual(expectedCode)
+	})
+	test("Return error for nonexistent declarator", async () => {
+		const input = `
+            export const load = () => {
+                return { data: true }
+            }
+        `
+		// @ivanohofer - what do you think of the type casting below?
+		const inputAst = parse(input, acornOptions) as unknown as Program
+		const result = wrapVariableDeclaration(inputAst, "blue", "wrapFn")
+		await expect(() => result).rejects.toThrowError("Couldn't find variable declarator.")
+	})
+	test("Doesn't manipulate inputs", async () => {
+		const input = `
+            export const load = () => {
+                return { data: true }
+            }
+        `
+		const inputAst = parse(input, acornOptions) as unknown as Program
+		const inputAstClone = structuredClone(inputAst)
+		await wrapVariableDeclaration(inputAst, "load", "wrapFn")
+		expect(inputAst).toEqual(inputAstClone)
+	})
+})
+
+describe("ast - insertAst", () => {
+	test("Inserts a simple import statement, before", async () => {
+		const insertion = `
+            import { wrapFn } from 'some/path'
+        `
+		const source = `
+            export const load = wrapFn(() => {
+                return { data: true }
+            })
+        `
+		const expected = `
+            import { wrapFn } from 'some/path'
+
+            export const load = wrapFn(() => {
+            return { data: true }
+            })
+        `
+		const insertionAst = parse(insertion, acornOptions) as unknown as Program
+		const sourceAst = parse(source, acornOptions) as unknown as Program
+		const expectedAst = parse(expected, acornOptions) as unknown as Program
+		const resultAst = await insertAst(sourceAst, insertionAst.body[0], { before: ["body", "0"] })
+		const expectedCode = generate(expectedAst)
+		const resultCode = generate(resultAst)
+		expect(resultCode).toEqual(expectedCode)
+	})
+	test("Inserts a load function, after", async () => {
+		const insertion = `
+            export const load = wrapFn(() => {
+                return { data: true }
+            })
+        `
+		const source = `
+            import { wrapFn } from 'some/path'
+        `
+		const expected = `
+            import { wrapFn } from 'some/path'
+
+            export const load = wrapFn(() => {
+            return { data: true }
+            })
+        `
+		const insertionAst = parse(insertion, acornOptions) as unknown as Program
+		const sourceAst = parse(source, acornOptions) as unknown as Program
+		const expectedAst = parse(expected, acornOptions) as unknown as Program
+		const resultAst = await insertAst(sourceAst, insertionAst.body[0], { after: ["body", "0"] })
+		const expectedCode = generate(expectedAst)
+		const resultCode = generate(resultAst)
+		expect(resultCode).toEqual(expectedCode)
+	})
+	test("Inserts code into wrap fn, before", async () => {
+		const source = `
+            import { wrapFn } from 'some/path'
+
+            export const load = wrapFn()
+        `
+		const expected = `
+            import { wrapFn } from 'some/path'
+
+            export const load = wrapFn(() => {
+                return { data: true }
+            })
+        `
+		const insertionAst = {
+			type: "ArrowFunctionExpression",
+			id: null,
+			expression: false,
+			generator: false,
+			async: false,
+			params: [],
+			body: {
+				type: "BlockStatement",
+				body: [
+					{
+						type: "ReturnStatement",
+						argument: {
+							type: "ObjectExpression",
+							properties: [
+								{
+									type: "Property",
+									method: false,
+									shorthand: false,
+									computed: false,
+									key: {
+										type: "Identifier",
+										name: "data",
+									},
+									value: {
+										type: "Literal",
+										value: true,
+										raw: "true",
+									},
+									kind: "init",
+								},
+							],
+						},
+					},
+				],
+			},
+		} as ArrowFunctionExpression
+		const sourceAst = parse(source, acornOptions) as unknown as Program
+		const expectedAst = parse(expected, acornOptions) as unknown as Program
+		const resultAst = await insertAst(sourceAst, insertionAst, {
+			before: ["body", "1", "declaration", "declarations", "0", "init", "arguments", "0"],
+		})
+		const expectedCode = generate(expectedAst)
+		const resultCode = generate(resultAst)
+		expect(resultCode).toEqual(expectedCode)
+	})
+	test("Only multiples of two are allowed for positional information", async () => {
+		const insertion = `
+            import { wrapFn } from 'some/path'
+        `
+		const source = `
+            export const load = wrapFn(() => {
+                return { data: true }
+            })
+        `
+		const insertionAst = parse(insertion, acornOptions) as unknown as Program
+		const sourceAst = parse(source, acornOptions) as unknown as Program
+		const result = insertAst(sourceAst, insertionAst.body[0], { before: ["body", "0", "fake"] })
+		await expect(() => result).rejects.toThrowError(
+			"The length of 'before' or 'after' has to be a multiple of two.",
+		)
+	})
+	test("Return error for unreachable path", async () => {
+		const insertion = `
+            import { wrapFn } from 'some/path'
+        `
+		const source = `
+            export const load = wrapFn(() => {
+                return { data: true }
+            })
+        `
+		const insertionAst = parse(insertion, acornOptions) as unknown as Program
+		const sourceAst = parse(source, acornOptions) as unknown as Program
+		const result = insertAst(sourceAst, insertionAst.body[0], {
+			before: ["body", "0", "fake", "path", "here"],
+		})
+		await expect(() => result).rejects.toThrowError("Couldn't access given position in AST.")
+	})
+	test("Doesn't manipulate inputs", async () => {
+		const insertion = `
+            import { wrapFn } from 'some/path'
+        `
+		const source = `
+            export const load = wrapFn(() => {
+                return { data: true }
+            })
+        `
+		const insertionAst = parse(insertion, acornOptions) as unknown as Program
+		const insertionAstClone = structuredClone(insertionAst)
+		const sourceAst = parse(source, acornOptions) as unknown as Program
+		const sourceAstClone = structuredClone(sourceAst)
+		await insertAst(sourceAst, insertionAst.body[0], { before: ["body", "0"] })
+		expect(sourceAst).toEqual(sourceAstClone)
+		expect(insertionAst).toEqual(insertionAstClone)
 	})
 })
--- source-code/sdk-js/src/helpers/ast.ts
+++ source-code/sdk-js/src/helpers/ast.ts
@@ -1,31 +1,48 @@
 import type { CallExpression, Node, Program } from "estree"
 import { walk } from "estree-walker"
+import { get } from "lodash"
 
 type wrapVariableDeclaration = (
 	sourceAst: Program,
 	searchIdentifier: string,
 	identifier: string,
-) => Program
+) => Promise<Program>
 
 type insertAst = (
 	sourceAst: Program,
-	ast: Program,
-	position: { before: string; after?: never } | { before?: never; after: string },
-) => Program
+	ast: Node,
+	position:
+		| { after?: never; before: [string, string, ...string[]] }
+		| { after: [string, string, ...string[]]; before?: never },
+) => Promise<Program>
 
-export const wrapVariableDeclaration = ((sourceAst, searchIdentifier, identifier) => {
-	const wrap = (n: Node, identifier: string) => {
-		return {
-			type: "CallExpression",
-			callee: {
-				type: "Identifier",
-				name: identifier,
-			},
-			arguments: [structuredClone(n)],
-			optional: false,
-		} as CallExpression
-	}
-	walk(sourceAst, {
+const wrapWithCallExpression = (n: Node, identifier: string) => {
+	return {
+		type: "CallExpression",
+		callee: {
+			type: "Identifier",
+			name: identifier,
+		},
+		arguments: [structuredClone(n)],
+		optional: false,
+	} as CallExpression
+}
+
+/**
+ * Finds a variable declaration with a given name in sourceAst and wraps the corresponding declarator with the given identifier function call.
+ * Is immutable towards it's inputs, meaning it DOESN'T MANIPULATE THEM.
+ *
+ * wrapVariableDeclaration(ast, "search", "wrap")` with an ast for the code `const search = () => {}` will result in `const search = wrap(() => {})`
+ *
+ * @param sourceAst The ast to operate on
+ * @param searchIdentifier The name of the variable declaration whose declarator should be wrapped
+ * @param identifier The function identifier with which the declarator should be wrapped.
+ * @returns A promise that resolves to the resulting ast or rejects if the searched for declarator can't be found.
+ */
+export const wrapVariableDeclaration = (async (sourceAst, searchIdentifier, identifier) => {
+	const sourceAstClone = structuredClone(sourceAst)
+	let found = false
+	walk(sourceAstClone, {
 		enter(node) {
 			if (
 				node.type === "VariableDeclarator" &&
@@ -33,15 +50,59 @@ export const wrapVariableDeclaration = ((sourceAst, searchIdentifier, identifier
 				node.id.name === searchIdentifier &&
 				node.init
 			) {
+				found = true
 				const newNode = structuredClone(node)
-				newNode.init = wrap(node.init, identifier)
+				newNode.init = wrapWithCallExpression(node.init, identifier)
 				this.replace(newNode)
+				this.skip()
 			}
 		},
 	})
-	return sourceAst
+	if (!found) throw new Error("Couldn't find variable declarator.")
+	return sourceAstClone
 }) satisfies wrapVariableDeclaration
 
-export const insertAst = ((sourceAst, ast, position) => {
-	return sourceAst
+/**
+ * Inserts the provided ast at the given position in a source ast.
+ * Is immutable towards it's inputs, meaning it DOESN'T MANIPULATE THEM.
+ * @param sourceAst The ESTree Program to insert into.
+ * @param ast The ESTree Node to insert
+ * @param position The positional information for the insert.
+ * Should be an object containing a string array that is the path to the insertion point within the sourceAst.
+ * Could typically begin with ["body", ...] as that is one of the first keys within an ESTree Program
+ * For reference on how such a string could look, see https://lodash.com/docs/4.17.15#get
+ * @param position.after An array of strings denoting the path to the insertion point within the sourceAst, before which ast shall be inserted.
+ * Use array index 0 to insert into an empty Array.
+ * @param position.before An array of strings denoting the path to the insertion point within the sourceAst, after which ast shall be inserted.
+ * Use array index 0 to insert into an empty Array.
+ * @returns Promise that either resolves to the resulting ast or throws if the given insertion position couldn't be found
+ * or the positional information array length does not satisfy length % 3 === 2
+ */
+export const insertAst = (async (sourceAst, ast, { before, after }) => {
+	const sourceAstClone = structuredClone(sourceAst)
+	const position = after || before
+	if (position.length % 3 !== 2) {
+		throw new Error("The length of 'before' or 'after' has to be a multiple of two.")
+	}
+	const targetParent = get(sourceAstClone, position.slice(0, -2), sourceAstClone)
+	const targetArrayKey = position.at(-2)
+	const targetArrayIndex = Number(position.at(-1))
+	let newAst
+	walk(sourceAstClone, {
+		enter(node) {
+			const newNode = structuredClone(node)
+			if (node === targetParent) {
+				const targetArray = get(newNode, targetArrayKey as string, undefined) as Node[] | undefined
+				if (!targetArray) throw new Error("Couldn't access given position in AST.")
+				else targetArray.splice(targetArrayIndex + (before ? 0 : 1), 0, structuredClone(ast))
+				// this.replace doesn't work on the root node - that's why we need the next line
+				if (node === sourceAstClone) {
+					newAst = newNode as Program
+				}
+				this.replace(newNode)
+				this.skip()
+			}
+		},
+	})
+	return newAst || sourceAstClone
 }) satisfies insertAst
