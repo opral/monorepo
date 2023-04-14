d6384c8eca6a43520e8f17895fae14961264a1b4
Benjamin Preiss <benjaminpreiss@protonmail.com>
2023-04-14 18:18:59 +0800

    Add comment about potential handling of js files

--- source-code/sdk-js/src/adapter-sveltekit/plugin.ts
+++ source-code/sdk-js/src/adapter-sveltekit/plugin.ts
@@ -6,6 +6,28 @@ const unplugin = createUnplugin(() => {
 		buildStart() {
 			console.log("Here comes some inlang magic")
 		},
+		/* 
+			this is how we could potentially transform our js files.
+
+			Recast mentioned here, by rich harris: https://github.com/Rich-Harris/magic-string#magic-string
+
+			async transform(code, id) {
+				if (id !== "our file id we want to transform") return
+				const ast = recast.parse(code)
+				// ast transformations
+				// This is the ast for the function import
+				const functionImportAst = {}
+				const astWithImport = await insertAst(ast, functionImportAst, { before: ["body", "0"] })
+				const finalAst = await wrapVariableDeclaration(astWithImport, "load", "wrapFn")
+				const recastPrint = recast.print()
+				// proceed with the transformation...
+				return {
+					code: recastPrint.code,
+					ast: finalAst,
+					map: recastPrint.map,
+				}
+			}, 
+		*/
 	}
 })
 
