47429f8417ffb8f7bb912d36eedf6edc2a570370
Nils Jacobsen <nils.jacobsen98@gmail.com>
2023-04-17 15:03:06 +0000

    feat: added counts to list header

--- packages/website/src/pages/editor/@host/@owner/@repository/components/Listheader.tsx
+++ packages/website/src/pages/editor/@host/@owner/@repository/components/Listheader.tsx
@@ -1,16 +1,51 @@
-export const ListHeader = () => {
+import { getLintReports } from "@inlang/core/lint"
+import { useEditorState } from "../State.jsx"
+import { For, createEffect, createSignal } from "solid-js"
+
+interface ListHeaderProps {
+	messageNo: number
+}
+
+type RuleSummaryItem = {
+	id: string
+	amount: number
+}
+
+export const ListHeader = (props: ListHeaderProps) => {
+	const { resources, inlangConfig } = useEditorState()
+	const lintReports = getLintReports(resources)
+	const [newRuleSummary, setNewRuleSummary] = createSignal<Array<RuleSummaryItem>>([])
+
+	const lintRuleIds = () =>
+		inlangConfig()
+			?.lint?.rules?.flat()
+			.map((rule) => rule.id) ?? []
+
+	createEffect(() => {
+		const newArr: Array<RuleSummaryItem> = []
+		lintRuleIds().map((id) => {
+			const filteredReports = lintReports.filter((report) => report.id === id)
+			newArr.push({ id, amount: filteredReports.length })
+		})
+		setNewRuleSummary(newArr)
+	})
+
 	return (
-		<div class="h-16 w-full bg-background border border-surface-3 rounded-t-md flex items-center px-4 justify-between">
-			<div class="font-medium text-on-surface">{24 + " Messages"}</div>
-			<div>
-				<sl-button>
-					<div class="flex gap-2 items-center">
-						<div class="-ml-2 h-7 px-2 rounded bg-danger/10 flex items-center justify-center">
-							24
-						</div>
-						<p class="">missingMessage</p>
-					</div>
-				</sl-button>
+		<div class="h-14 w-full bg-background border border-surface-3 rounded-t-md flex items-center px-4 justify-between">
+			<div class="font-medium text-on-surface">{props.messageNo + " Messages"}</div>
+			<div class="flex gap-2">
+				<For each={newRuleSummary()}>
+					{(rule) => (
+						<sl-button prop:size="small">
+							<div class="flex gap-2 items-center h-7">
+								<div class="-ml-[4px] h-5 px-2 rounded bg-danger/10 flex items-center justify-center text-danger">
+									{rule.amount}
+								</div>
+								<div class="text-xs text-on-surface-variant font-medium">{rule.id}</div>
+							</div>
+						</sl-button>
+					)}
+				</For>
 			</div>
 		</div>
 	)
