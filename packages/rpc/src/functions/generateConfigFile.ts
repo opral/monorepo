bd3ae7ee3c84e5bacab9c7f48a5e546139edaf61
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2023-04-17 23:57:25 +0200

    chore: replace dedent with ts-dedent
    dedent doesn't ship with type declarations.

--- packages/rpc/src/functions/generateConfigFile.ts
+++ packages/rpc/src/functions/generateConfigFile.ts
@@ -4,7 +4,7 @@ import { privateEnv } from "@inlang/env-variables"
 import { telemetryNode } from "@inlang/telemetry"
 import { createMemoryFs } from "@inlang-git/fs"
 import { mockEnvironment, validateConfigFile } from "@inlang/core/test"
-import dedent from "dedent"
+import { dedent } from "ts-dedent"
 import { prompt, promptVersion } from "./generateConfigFile.prompt.js"
 
 export async function generateConfigFileServer(args: {
