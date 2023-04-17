c0734679ab143784c88ee215504b3ae9c37155f3
Ivan Hofer <ivan.hofer@outlook.com>
2023-04-17 15:54:40 +0200

    fix comments

--- source-code/sdk-js/src/adapter-sveltekit/examples/lang-in-slug/src/routes/+layout.ts
+++ source-code/sdk-js/src/adapter-sveltekit/examples/lang-in-slug/src/routes/+layout.ts
@@ -1,14 +1,11 @@
 import { initRootLayoutLoadWrapper } from '@inlang/sdk-js/adapter-sveltekit/shared'
 import type { LayoutLoad } from "./$types.js"
 
-export const load = initRootLayoutLoadWrapper<LayoutLoad>().wrap(({ data }) => {
-	// const runtime = await initSvelteKitClientRuntime({
-	// 	fetch,
-	// 	language: data.language!,
-	// 	referenceLanguage: data.referenceLanguage,
-	// 	languages: data.languages,
-	// })
+export const load = initRootLayoutLoadWrapper<LayoutLoad>({
 
+}).wrap(({ data }) => {
+	// const data = await parent()
+	// const runtime = getRuntimeFromData(data)
 	// console.info("+layout.ts", runtime.i("welcome"))
 
 	return { ...data, "+layout.ts": Math.random() }
