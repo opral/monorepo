a8b42a01317a7c0c73e05d24ac7dbe220d74f551
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2023-04-18 00:19:19 +0200

    refactor: don't expose the entire Filesystem in env function.
    $fs is a subset of `@inlang-git/fs`. Is a subset because we need to ensure that the filesystem API exists across all environments (node, browser, etc).

--- source-code/core/src/config/environment-functions/$fs.ts
+++ source-code/core/src/config/environment-functions/$fs.ts
@@ -1,29 +1,12 @@
-export type { Filesystem as $fs } from "@inlang-git/fs"
+import type { Filesystem } from "@inlang-git/fs"
 
 /**
- * ! IF YOU DO CHANGES TO THIS FILE,
- * ! CONTACT @samuelstroschein BEFORE.
- */
-
-/**
- * Minimal filesystem required by inlang.
+ * Minimal filesystem provided by inlang.
  *
- * Uses memfs under the hood for now. To avoid
- * type issues, use memfs or node:fs/promises
- * with @ts-ignore.
-
-export type $fs = {
-	readFile: (
-		...args: Parameters<typeof memfs.promises.readFile>
-	) => ReturnType<typeof memfs.promises.readFile>
-	writeFile: (
-		...args: Parameters<typeof memfs.promises.writeFile>
-	) => ReturnType<typeof memfs.promises.writeFile>
-	readdir: (
-		...args: Parameters<typeof memfs.promises.readdir>
-	) => ReturnType<typeof memfs.promises.readdir>
-	mkdir: (
-		...args: Parameters<typeof memfs.promises.mkdir>
-	) => ReturnType<typeof memfs.promises.mkdir>
-}
-*/
+ * $fs is a subset of `@inlang-git/fs`. Is a subset because we need
+ * to ensure that the filesystem API exists across all environments
+ * (node, browser, etc).
+ *
+ * If you are missing a method, please open an issue on github.
+ */
+export type $fs = Pick<Filesystem, "readdir" | "mkdir" | "writeFile" | "readFile">
