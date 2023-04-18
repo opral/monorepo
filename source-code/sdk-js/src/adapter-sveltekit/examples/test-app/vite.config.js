15f6ef2d19535131e44edd2551f41494f4584a11
Ivan Hofer <ivan.hofer@outlook.com>
2023-04-18 16:42:22 +0200

    add adapter plugin

--- source-code/sdk-js/src/adapter-sveltekit/examples/test-app/vite.config.js
+++ source-code/sdk-js/src/adapter-sveltekit/examples/test-app/vite.config.js
@@ -1,6 +1,7 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { vitePlugin } from "@inlang/sdk-js/adapter-sveltekit"
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit(), vitePlugin()]
 });
