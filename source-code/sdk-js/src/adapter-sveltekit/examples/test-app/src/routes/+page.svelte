f8410113c700c34c2e0fd3252690edef2662d17c
Ivan Hofer <ivan.hofer@outlook.com>
2023-04-18 17:43:33 +0200

    simplify inlang setup

--- source-code/sdk-js/src/adapter-sveltekit/examples/test-app/src/routes/+page.svelte
+++ source-code/sdk-js/src/adapter-sveltekit/examples/test-app/src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { i } from "@inlang/sdk-js"
+</script>
+
+<h1>{i("welcome", { name: "SvelteKit" })}</h1>
