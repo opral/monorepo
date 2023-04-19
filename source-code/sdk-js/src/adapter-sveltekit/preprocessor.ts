0414e2c271aaaca2b1c3e1a9779b85e7420c6918
Ivan Hofer <ivan.hofer@outlook.com>
2023-04-19 07:18:46 +0200

    more robust import statements

--- source-code/sdk-js/src/adapter-sveltekit/preprocessor.ts
+++ source-code/sdk-js/src/adapter-sveltekit/preprocessor.ts
@@ -1,13 +1,38 @@
+const REGEX_INLANG_SDK_IMPORT = /.*import\s*{\s*(.*)\s*}\s*from\s+['"]@inlang\/sdk-js['"]/g
+
+const isReactive = true
+
 export const preprocessor = {
 	async markup({ content }: any) {
-		const includesSdkImport = content.includes('@inlang/sdk-js')
-		if (!includesSdkImport) return { code: content }
+		let transformedCode: string = content
 
-		const transformedCode = content
-			.replace(/^.*import.*@inlang\/sdk-js.*$/m, `
-import { getRuntimeFromContext } from "@inlang/sdk-js/adapter-sveltekit/client/reactive"
-const { i } = getRuntimeFromContext()
-`).replace(/i\(/g, '$i(')
+		let match = REGEX_INLANG_SDK_IMPORT.exec(transformedCode)
+		if (!match) return { code: transformedCode }
+
+		// supports multiple imports
+		// assumption: imports are always on top
+		let lastTransform = 0
+
+		// eslint-disable-next-line no-cond-assign
+		while (match) {
+			const replacement = (!lastTransform ? 'import { getRuntimeFromContext } from "@inlang/sdk-js/adapter-sveltekit/client/reactive";\n' : '')
+				+ 'const {' + match[1] + '} = getRuntimeFromContext();'
+
+			transformedCode = transformedCode.slice(0, match.index)
+				+ replacement
+				+ transformedCode.slice(match.index + match[0].length)
+
+			lastTransform = match.index + replacement.length
+			match = REGEX_INLANG_SDK_IMPORT.exec(transformedCode)
+		}
+
+		if (isReactive) {
+			// replace with store syntax if reactive
+			transformedCode = transformedCode.slice(0, lastTransform) + transformedCode.slice(lastTransform)
+				.replace(/i\(/g, '$i(')
+				.replace(/language[^s]/g, '$language')
+
+		}
 
 		return { code: transformedCode }
 	},
