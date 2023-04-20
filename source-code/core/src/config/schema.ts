60a92717b4d3777fa60ec3cf64589b4da2f42b0b
Ivan Hofer <ivan.hofer@outlook.com>
2023-04-20 09:43:57 +0200

    add base sdk config types

--- source-code/core/src/config/schema.ts
+++ source-code/core/src/config/schema.ts
@@ -107,4 +107,165 @@ export type Config = {
 		 */
 		// documentSelectors: DocumentSelector[];
 	}
+
+	sdk?: {
+		languageNegotiation?: {
+			/* default: false */
+			strict?: boolean
+			/* default: [server ? AcceptLanguageNegotiator : NavigatorNegotiator, UrlNegotiator] */
+			strategies?: LanguageNegotiationStrategy[]
+		}
+	}
+}
+
+type LanguageNegotiatorType =
+	| "url"
+	| "cookie"
+	| "accept-language-header"
+	| "navigator"
+	| "localStorage"
+	| "sessionStorage"
+
+type LanguageNegotiatorBase<Type extends LanguageNegotiatorType, Settings = never> = [
+	Settings,
+] extends [never]
+	? { type: Type }
+	: Settings & { type: Type }
+
+type LanguageNegotiationStrategy =
+	/* default */
+	| UrlNegotiator
+	| AcceptLanguageHeaderNegotiator
+	| CookieNegotiator
+	| NavigatorNegotiator
+	| LocalStorageNegotiator
+	| SessionStorageNegotiator
+
+type UrlNegotiatorVariantBase<Type, Settings> = Settings & { type: Type }
+
+type UrlNegotiatorVariantQuery = UrlNegotiatorVariantBase<
+	"query",
+	{
+		parameter?: string
+	}
+>
+
+type UrlNegotiatorVariantDomain = UrlNegotiatorVariantBase<
+	"domain",
+	{
+		level:
+		| never /* just to make formatter happy */
+		/** `www.inlang.de` => `de` */
+		| "tld"
+		/** `de.inlang.com` => `de` */
+		| "subdomain"
+		/** `de.editor.inlang.com` => 1 => `de` */
+		/** `de.example.beta.editor.inlang.com` => 4 => `de` */
+		| number
+	}
+>
+
+type UrlNegotiatorVariantPath = UrlNegotiatorVariantBase<
+	"path",
+	{
+		/* default: 1 */
+		/** `www.inlang.com/de` => 1 => `de` */
+		/** `www.inlang.com/editor/de` => 2 => `de` */
+		level?: number
+	}
+>
+
+type UrlNegotiator = LanguageNegotiatorBase<
+	"url",
+	{
+		variant?: /* default */
+		UrlNegotiatorVariantPath | UrlNegotiatorVariantDomain | UrlNegotiatorVariantQuery
+	}
+>
+
+type AcceptLanguageHeaderNegotiator = LanguageNegotiatorBase<
+	"accept-language-header",
+	{
+		/* default: 'language' */
+		name?: string
+	}
+>
+
+type CookieNegotiator = LanguageNegotiatorBase<
+	"cookie",
+	{
+		/* default: 'language' */
+		name?: string
+	}
+>
+
+type LocalStorageNegotiator = LanguageNegotiatorBase<
+	"localStorage",
+	{
+		/* default: 'language' */
+		name?: string
+	}
+>
+
+type SessionStorageNegotiator = LanguageNegotiatorBase<
+	"sessionStorage",
+	{
+		/* default: 'language' */
+		name?: string
+	}
+>
+
+type NavigatorNegotiator = LanguageNegotiatorBase<"navigator">
+
+
+// --------------------------------
+// tests
+
+const sdKConfig1: Config["sdk"] = {}
+
+const sdKConfig2: Config["sdk"] = {
+	languageNegotiation: {},
+}
+const sdKConfig3: Config["sdk"] = {
+	languageNegotiation: {
+		strategies: [],
+	},
+}
+const sdKConfig4: Config["sdk"] = {
+	languageNegotiation: {
+		strategies: [
+			{
+				type: "url",
+			},
+		],
+	},
+}
+
+const sdKConfig5: Config["sdk"] = {
+	languageNegotiation: {
+		strict: true,
+		strategies: [
+			{
+				type: "url",
+				variant: {
+					type: 'query',
+					parameter: 'lang'
+				}
+			}
+		],
+	},
+}
+
+const sdKConfig6: Config["sdk"] = {
+	languageNegotiation: {
+		strategies: [
+			{
+				type: "url",
+				variant: {
+					type: "domain",
+					level: 1
+				}
+			}
+		],
+	},
 }
