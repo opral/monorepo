f419bb070c22e2217bb4d6861cb16a30c935552d
araknast <araknast@protonmail.com>
2023-04-23 00:31:17 -0700

    Update mockEnvironment to use the new old api

--- packages/core/src/config/environment-functions/$fs.test-d.ts
+++ packages/core/src/config/environment-functions/$fs.test-d.ts
@@ -2,13 +2,13 @@ import type { $fs } from "./$fs.js"
 
 // eslint-disable-next-line no-restricted-imports
 import * as nodefs from "node:fs/promises"
-import { createMemoryFs } from "@inlang-git/fs"
+import { createMemoryFs, fromNodeFs } from "@inlang-git/fs"
 
 function createFs(fs: $fs): $fs {
 	return fs
 }
 
 //@ts-ignore - TODO outcomment when fixed
-createFs(nodefs)
+createFs(fromNodeFs(nodefs))
 
 createFs(createMemoryFs())
--- packages/core/src/config/environment-functions/$fs.ts
+++ packages/core/src/config/environment-functions/$fs.ts
@@ -3,10 +3,6 @@ import type { Filesystem } from "@inlang-git/fs"
 /**
  * Minimal filesystem provided by inlang.
  *
- * $fs is a subset of `@inlang-git/fs`. Is a subset because we need
- * to ensure that the filesystem API exists across all environments
- * (node, browser, etc).
- *
  * If you are missing a method, please open an issue on github.
  */
-export type $fs = Pick<Filesystem, "readdir" | "mkdir" | "writeFile" | "readFile">
+export type $fs = Filesystem
