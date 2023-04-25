3b66e28d6a5739aa4e379bf9f2b2ee52edad55bb
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2023-04-25 18:19:31 +0200

    refactor: expose minimal filesystem api required for inlang
    @araknast `$fs` is a bit unrelated to git (at least for now) at should only expose the minimum to ensure that all different environments that inlang operates in (node, browser, ide extension) support all methods.

--- source-code/core/src/config/environment-functions/$fs.ts
+++ source-code/core/src/config/environment-functions/$fs.ts
@@ -3,6 +3,10 @@ import type { Filesystem } from "@inlang-git/fs"
 /**
  * Minimal filesystem provided by inlang.
  *
+ * $fs is a subset of `@inlang-git/fs`. The subset ensures
+ * that the filesystem API is supported across all environments
+ * (node, browser, etc).
+ *
  * If you are missing a method, please open an issue on github.
  */
-export type $fs = Filesystem
+export type $fs = Pick<Filesystem, "readdir" | "mkdir" | "writeFile" | "readFile">
