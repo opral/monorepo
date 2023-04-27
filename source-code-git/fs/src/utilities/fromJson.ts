9ec878aab9db3622ce0b9c8ce855b1a575fe0898
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2023-04-27 18:46:33 +0200

    add: toJson and fromJson

--- source-code-git/fs/src/utilities/fromJson.ts
+++ source-code-git/fs/src/utilities/fromJson.ts
@@ -1,4 +1,5 @@
 import type { NodeishFilesystem } from "../interface.js"
+import { assertIsAbsolutePath } from "./assertIsAbsolutePath.js"
 
 /**
  * "Imports" files from a JSON into the filesystem.
@@ -6,7 +7,15 @@ import type { NodeishFilesystem } from "../interface.js"
 export async function fromJson(args: {
 	fs: NodeishFilesystem
 	json: Record<string, string>
+	resolveFrom: string
 }): Promise<void> {
-	console.log(args)
-	throw Error("Not implemented")
+	const { fs, json, resolveFrom } = args
+
+	assertIsAbsolutePath(resolveFrom)
+
+	for (const [filePath, contents] of Object.entries(json)) {
+		const path = `${resolveFrom}/${filePath}`
+		await fs.mkdir(path, { recursive: true })
+		await fs.writeFile(path, contents)
+	}
 }
--- source-code-git/fs/src/utilities/toJson.ts
+++ source-code-git/fs/src/utilities/toJson.ts
@@ -1,34 +1,75 @@
+import type { NodeishFilesystem } from "../interface.js"
+// @ts-expect-error - outmatch is not updated for export maps
+import outmatch from "outmatch"
+import { normalizePath } from "./normalizePath.js"
+import { assertIsAbsolutePath } from "./assertIsAbsolutePath.js"
+
 /**
  * Serializes the filesystem to a JSON that can be sent over the network.
  *
+ * Use `cwd` to define the directory to resolve from.
+ *
+ * Use `match` to define which files to include. Match uses
+ * https://github.com/axtgr/outmatch#syntax for matching.
+ *
  * @example
- *   const result = fs.toJson()
- *   >> { "file1.txt": "content", "file2.txt": "content" }
+ *   // match everything but node_modules
+ *   const result = await toJson(fs, {
+ * 		match: [
+ * 			"**\/*"
+ * 			"!node_modules"
+ * 		],
+ * 		cwd: "/"
+ * 		})
+ *
+ *   	>> { "file1.txt": "content", "file2.txt": "content" }
  *
  */
 export async function toJson(args: {
+	fs: NodeishFilesystem
+	matchers: string[]
 	/**
-	 * Takes a list of glob patterns to include.
+	 * The path to resolve from.
 	 *
-	 * @example
-	 *   fs.toJson({ include: ["**\/*.json", "**\/*.md"] })
+	 * TODO: @araknast we should likely drop the cwd in favor of absolute paths everywhere.
+	 * TODO: cwd doesn't exist in the browser and will lead to confusion. I named this
+	 * TODO: `resolveFrom` now but can we come up with a name like "absolutePath"
+	 * TODO: to make it more clear what it does? `resolveFrom` might be ambigous.
 	 */
-	include?: string[]
-	/**
-	 * Takes a list of glob patterns to exclude.
-	 *
-	 * @example
-	 *   fs.toJson({ exclude: ["**\/node_modules"] })
-	 */
-	exclude?: string[]
-	/**
-	 * The directory to convert to JSON. Default is fs root (memory) or cwd (node)
-	 *
-	 * @example
-	 *   fs.toJson({ dir: "/home/user/project" })
-	 */
-	dir?: string
-}): Promise<Record<string, any>> {
-	console.log(args)
-	throw new Error("Not implemented")
+	resolveFrom: string
+}): Promise<Record<string, string>> {
+	assertIsAbsolutePath(args.resolveFrom)
+
+	let result: Record<string, string> = {}
+	const isMatch = outmatch(args.matchers)
+
+	console.log("getting here1")
+	const files = await args.fs.readdir(args.resolveFrom)
+	console.log("getting here2")
+
+	for (const file of files) {
+		const fullPath = normalizePath(`${args.resolveFrom}/${file}`)
+		let isDirectory = false
+
+		try {
+			await args.fs.readFile(fullPath, { encoding: "utf-8" })
+		} catch (error) {
+			console.log(error)
+			isDirectory = true
+		}
+		if (isDirectory) {
+			// If the item is a directory, recurse into it and add the results to the current list
+			const subList = await toJson({ ...args, resolveFrom: fullPath })
+			result = { ...result, ...subList }
+		} else if (isMatch(fullPath) === false) {
+			continue
+		} else {
+			const content = await args.fs.readFile(fullPath, { encoding: "utf-8" })
+			if (!content) throw new Error(`${fullPath} does not exist.`)
+
+			// remove the leading slash
+			result[fullPath.slice(1)] = content as string
+		}
+	}
+	return result
 }
