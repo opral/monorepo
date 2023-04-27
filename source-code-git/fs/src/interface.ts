21d9cfda2b3be1e631e55680753e2f5278f5712c
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2023-04-27 14:17:40 +0200

    add recursive options

--- source-code-git/fs/src/interface.ts
+++ source-code-git/fs/src/interface.ts
@@ -5,11 +5,18 @@
  * that is cross platform compatible (browser, node).
  */
 export type Filesystem = {
-	writeFile: (path: string, content: string) => Promise<void>
+	writeFile: (path: string, data: string) => Promise<void>
 	readFile: (path: string, options: { encoding: "utf-8" }) => Promise<FileData>
 	readdir: (path: string) => Promise<string[]>
-	mkdir: (path: string) => Promise<void>
-	rm: (path: string) => Promise<void>
+	/**
+	 * https://nodejs.org/api/fs.html#fspromisesmkdirpath-options
+	 *
+	 * Upon success, fulfills with undefined if recursive is false, or the first directory path created if recursive is true.
+	 */
+	mkdir:
+		| ((path: string) => Promise<void>)
+		| ((path: string, options?: { recursive: boolean }) => Promise<string>)
+	rm: (path: string, options?: { recursive: boolean }) => Promise<void>
 }
 
 export type FileData = string
