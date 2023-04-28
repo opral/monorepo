f12512cb7195f0aac1f87b0c160a96b02290c7e9
araknast <araknast@protonmail.com>
2023-04-27 17:37:15 -0700

    remove funny paths and pass encoding as a string

--- source-code-git/fs/src/implementations/implementations.test.ts
+++ source-code-git/fs/src/implementations/implementations.test.ts
@@ -22,50 +22,67 @@ const runFsTestSuite = async (name: string, tempDir: string, fs: NodeishFilesyst
 	})
 
 	test("recursive mkdir", async () => {
-		await fs.mkdir(`${tempDir}/home/user1/documents///`, { recursive: true })
-		await fs.mkdir(`${tempDir}/home/user1/../user1//downloads`, { recursive: true })
-		expect(await fs.readdir(`/${tempDir}`)).toEqual(["home"])
-		expect(await fs.readdir(`/${tempDir}/home/user1/`)).toEqual(["documents", "downloads"])
+		await fs.mkdir(`${tempDir}/home/user1/documents/`, { recursive: true })
+		await fs.mkdir(`${tempDir}/home/user1/downloads`, { recursive: true })
+		expect(await fs.readdir(tempDir)).toEqual(["home"])
+		expect(await fs.readdir(`${tempDir}/home/user1/`)).toEqual(["documents", "downloads"])
+		expect(await fs.readdir(`${tempDir}/home/user1/documents`)).toEqual([])
+	})
 	})
 
 	test("file r/w", async () => {
-		await fs.writeFile(
-			`/${tempDir}/home/user1/../user1/documents/./file1/`,
-			"text in the first file",
-		)
-		await fs.writeFile(`/${tempDir}/file2`, "text in the second file")
+		await fs.writeFile(`${tempDir}/home/user1/documents/file1`, "text in the first file")
+		await fs.writeFile(`${tempDir}/file2`, "text in the second file")
 
-		expect(await fs.readdir(`/${tempDir}/home/user1/documents/`)).toEqual(["file1"])
+		expect(await fs.readdir(`${tempDir}/home/user1/documents/`)).toEqual(["file1"])
 		const dirents = await fs.readdir(tempDir)
 		expect(dirents).toContain("home")
 		expect(dirents).toContain("file2")
 		expect(dirents).toHaveLength(2)
 
-		expect(
-			await fs.readFile(`${tempDir}/home/user1/./documents/././file1`, { encoding: "utf-8" }),
-		).toEqual("text in the first file")
-
-		expect(await fs.readFile(`/${tempDir}/file2`, { encoding: "utf-8" })).toEqual(
-			"text in the second file",
+		expect(await fs.readFile(`${tempDir}/home/user1/documents/file1`, "utf8")).toEqual(
+			"text in the first file",
 		)
+
+		expect(await fs.readFile(`${tempDir}/file2`, "utf8")).toEqual("text in the second file")
 	})
 
-	describe("throw errors", async () => {
-		test("readFile", async () => {
-			await expect(
-				async () => await fs.readFile(`/${tempDir}/home/dne`, { encoding: "utf-8" }),
-			).rejects.toThrow(/ENOENT/)
+	test.todo("encodings other than utf8")
 
+	describe("throw errors", async () => {
+		test("rm", async () => {
+			await expect(async () => await fs.rm(`${tempDir}/home/dne/dne2`)).rejects.toThrow(/ENOENT/)
+
+			await expect(async () => await fs.rm(`${tempDir}/home/dne`)).rejects.toThrow(/ENOENT/)
+
+			await expect(async () => await fs.rm(`${tempDir}/home/user1`)).rejects.toThrow(/EISDIR/)
+		})
+
+		test("mkdir", async () => {
+			await expect(async () => await fs.mkdir(`${tempDir}/home/dne/dne2`)).rejects.toThrow(/ENOENT/)
+		})
+
+		test("writeFile", async () => {
 			await expect(
-				async () => await fs.readFile(`/${tempDir}/home/user1`, { encoding: "utf-8" }),
-			).rejects.toThrow(/EISDIR/)
+				async () => await fs.readFile(`${tempDir}/home/dne/file`, "utf8"),
+			).rejects.toThrow(/ENOENT/)
+		})
+
+		test("readFile", async () => {
+			await expect(async () => await fs.readFile(`${tempDir}/home/dne`, "utf8")).rejects.toThrow(
+				/ENOENT/,
+			)
+
+			await expect(async () => await fs.readFile(`${tempDir}/home/user1`, "utf8")).rejects.toThrow(
+				/EISDIR/,
+			)
 		})
 
 		test("readdir", async () => {
-			await expect(async () => await fs.readdir(`/${tempDir}/home/dne`)).rejects.toThrow(/ENOENT/)
+			await expect(async () => await fs.readdir(`${tempDir}/home/dne`)).rejects.toThrow(/ENOENT/)
 
 			await expect(
-				async () => await fs.readdir(`/${tempDir}/home/user1/documents/file1`),
+				async () => await fs.readdir(`${tempDir}/home/user1/documents/file1`),
 			).rejects.toThrow(/ENOTDIR/)
 		})
 	})
