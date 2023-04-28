e2f1a419149026acf95a4b3d2865a5474890e2c8
Ivan Hofer <ivan.hofer@outlook.com>
2023-04-28 07:28:22 +0200

    fix tests

--- source-code/sdk-js/src/adapter-sveltekit/ast-transformation/transforms/[language].json.test.ts
+++ source-code/sdk-js/src/adapter-sveltekit/ast-transformation/transforms/[language].json.test.ts
@@ -1,14 +1,15 @@
 import { describe, it } from "vitest"
+import type { TransformConfig } from '../config.js'
 import { transformLanguageJson } from "../transforms/[language].json.js"
 
 describe("transformLanguageJson", () => {
 	// todo: cover with real test once this implemented
 	it("temporarily throws error", ({ expect }) => {
-		expect(() => transformLanguageJson({}, "anything")).toThrowError("currently not supported")
+		expect(() => transformLanguageJson({} as TransformConfig, "anything")).toThrowError("currently not supported")
 	})
 
 	it("creates new file", ({ expect }) => {
-		const code = transformLanguageJson({}, "")
+		const code = transformLanguageJson({} as TransformConfig, "")
 		expect(code).toMatchSnapshot()
 	})
 })
