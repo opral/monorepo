6cb13b51a78072a93c05107f544c178bc137cc10
araknast <araknast@protonmail.com>
2023-04-29 09:33:27 -0700

    remove `src_old`

--- source-code-git/fs/src_old/index.ts
+++
@@ -1,4 +0,0 @@
-export type { Filesystem, FileData, FilesystemError } from "./schema.js"
-export { createMemoryFs } from "./createMemoryFs.js"
-export { fromNodeFs } from "./fromNodeFs.js"
-export { toNodeFs } from "./toNodeFs.js"
