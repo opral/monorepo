12208bb4fba6b1a9e2e879efde1657784df21bf9
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2023-04-30 17:52:04 +0200

    chore: add comment why pluginsetupfunction is sync

--- source-code/core/src/plugin/types.ts
+++ source-code/core/src/plugin/types.ts
@@ -10,6 +10,15 @@ import type { Config, EnvironmentFunctions } from "../config/schema.js"
  *		})
  *	]
  */
+// The function should be SYNC!
+//
+// If we allow the setup function to be async,
+// developers will import all module at top level
+// scope which will lead to long loading times when
+// setting up plugins / initial load of the editor
+// because the module are not imported "on-demand".
+//
+// See https://github.com/inlang/inlang/commit/ec15170bc17ef36916123152d2fdbc3b2e8c0d91#r111216268
 export type PluginSetupFunction = (env: EnvironmentFunctions) => Plugin
 
 /**
