5f7a4eafba7b892fdde679016806e2c6c15aa4e1
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2023-04-30 15:14:58 +0200

    add error handling

--- packages/core/src/plugin/setupPlugins.ts
+++ packages/core/src/plugin/setupPlugins.ts
@@ -1,5 +1,6 @@
 import type { Config, EnvironmentFunctions } from "../config/schema.js"
 import type { Plugin, PluginSetupFunction } from "./types.js"
+import { PluginSetupError } from "./errors/PluginSetupError.js"
 import { deepmergeInto } from "deepmerge-ts"
 
 export type ConfigWithSetupPlugins = Omit<Partial<Config>, "plugins"> & {
@@ -10,8 +11,14 @@ export type ConfigWithSetupPlugins = Omit<Partial<Config>, "plugins"> & {
  * Setup the plugins and process the config callback.
  *
  * The function mutates the config object. Mutating the config object
- * is expected to yield some performance benefits and leads to a
- * slightly better API for `setupConfig`. We can change this behaviour
+ * is expected to:
+ *
+ * 	1. decrease the initial startup time of inlang apps with 10+ plugins
+ *     (immutability is expensive).
+ * 	2. leads to a lightly better API for `setupConfig`.
+ *  3. plugins configs are only merged
+ *
+ * We can change this behaviour
  * if required as this function is only used internally.
  *
  */
@@ -24,14 +31,19 @@ export async function setupPlugins(args: {
 		return args.config as ConfigWithSetupPlugins
 	}
 	for (let i = 0; i < args.config.plugins.length; i++) {
-		// If a plugin uses a setup function, the setup
-		// function needs to be invoked.
-		if (typeof args.config.plugins[i] === "function") {
-			args.config.plugins[i] = (args.config.plugins[i] as PluginSetupFunction)(args.env)
+		const plugin = args.config.plugins[i] as Plugin | PluginSetupFunction
+		try {
+			// If a plugin uses a setup function, the setup function needs to be invoked.
+			if (typeof plugin === "function") {
+				args.config.plugins[i] = plugin(args.env)
+			}
+			const config = await (plugin as Plugin).config()
+			deepmergeInto(args.config, config)
+		} catch (error) {
+			console.error(
+				new PluginSetupError(`Failed to setup plugin '${(plugin as Plugin).id}'`, { cause: error }),
+			)
 		}
-		const plugin = args.config.plugins[i] as Plugin
-		const config = await plugin?.config()
-		deepmergeInto(args.config, config)
 	}
 	// remove duplicates from languages
 	args.config.languages = [...new Set(args.config.languages)]
