51efd93b2f6c6e4f3c3c820d023c79419fa2af4c
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2023-04-30 16:09:31 +0200

    refactor: prefix zod types with `z`
    Seenhttps://github.com/inlang/inlang/pull/605#discussion_r1181200717

--- source-code/core/src/config/setupConfig.ts
+++ source-code/core/src/config/setupConfig.ts
@@ -1,6 +1,6 @@
 import type { Config, DefineConfig, EnvironmentFunctions } from "./schema.js"
 import { setupPlugins } from "../plugin/setupPlugins.js"
-import { Config as ZodConfig } from "./zod.js"
+import { zConfig } from "./zod.js"
 
 export async function setupConfig(args: {
 	env: EnvironmentFunctions
@@ -8,5 +8,5 @@ export async function setupConfig(args: {
 }): Promise<Config> {
 	const config = await args.defineConfig(args.env)
 	await setupPlugins({ config, env: args.env })
-	return ZodConfig.parse(config) as Config
+	return zConfig.parse(config) as Config
 }
