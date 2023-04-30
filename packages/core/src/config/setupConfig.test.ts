cbd348835ecdd218641920f9a127b2785b1684d4
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2023-04-30 19:03:27 +0200

    refactor: setup config with module instead of defineConfig
    This change future-proofs the API in case we add more exports to the `inlang.config.js` file. The setup config API stays identical because we take the lowest common argument, the module itself.

--- packages/core/src/config/setupConfig.test.ts
+++ packages/core/src/config/setupConfig.test.ts
@@ -3,7 +3,12 @@ import { setupConfig } from "./setupConfig.js"
 import type { DefineConfig } from "./schema.js"
 
 it("should setup the config with plugins", async () => {
-	const config = await setupConfig({ defineConfig: mockDefineConfig, env: {} as any })
+	const config = await setupConfig({
+		module: {
+			defineConfig: mockDefineConfig,
+		},
+		env: {} as any,
+	})
 	expect(config.referenceLanguage).toEqual("de")
 	expect(config.languages).toEqual(["en", "de"])
 })
