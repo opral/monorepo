15b72389d7808602bb919316bcea9c6758831fa4
Ivan Hofer <ivan.hofer@outlook.com>
2023-05-01 13:50:19 +0200

    add missing type import

--- source-code/core/src/config/schema.ts
+++ source-code/core/src/config/schema.ts
@@ -2,7 +2,7 @@ import type { Language } from "../ast/index.js"
 import type * as ast from "../ast/index.js"
 import type { InlangEnvironment } from '../environment/types.js'
 import type { LintRule } from "../lint/rule.js"
-import type { PluginSetupFunction } from '../plugin/types.js'
+import type { Plugin, PluginSetupFunction } from '../plugin/types.js'
 
 /**
  * The entrypoint for inlang.
