0135f01b7891ac5fb7edafb3c9eeee2dc3c08d57
araknast <araknast@protonmail.com>
2023-05-02 08:50:19 -0700

    test: binary roundtrip

--- source-code-git/fs/src/utilities/fromJson.test.ts
+++ source-code-git/fs/src/utilities/fromJson.test.ts
@@ -44,4 +44,29 @@ it("should be able to make a roundtrip", async () => {
 	)
 })
 
-it.todo("should be able to make a binary roundtrip")
+it("should be able to make a binary roundtrip", async () => {
+	const fs = createMemoryFs()
+	await fromJson({
+		fs,
+		resolveFrom: "/",
+		json: {
+			"file1.txt": "Y29udGVudDE=",
+			"file2.gif": "R0lGODlhAQABAIAAAP///////yH5BAAAAAAALAAAAAABAAEAAAIBAAA=",
+			"images/file3.png":
+				"iVBORw0KGgoAAAANSUhEUgAAAAEAAAABAQAAAAA3bvkkAAAAEElEQVR4nGJgAQAAAP//AwAABgAFV7+r1AAAAABJRU5ErkJggg==",
+		},
+	})
+
+	const json = await toJson({ fs, matchers: ["**/*"], resolveFrom: "/" })
+	const fs2 = createMemoryFs()
+	await fromJson({ fs: fs2, resolveFrom: "/", json })
+	expect(await fs.readFile("/file1.txt", { encoding: "utf-8" })).toEqual(
+		await fs2.readFile("/file1.txt", { encoding: "utf-8" }),
+	)
+	expect(await fs.readFile("/file2.gif", { encoding: "binary" })).toEqual(
+		await fs2.readFile("/file2.gif", { encoding: "binary" }),
+	)
+	expect(await fs.readFile("images/file3.png", { encoding: "binary" })).toEqual(
+		await fs2.readFile("images/file3.png", { encoding: "binary" }),
+	)
+})
