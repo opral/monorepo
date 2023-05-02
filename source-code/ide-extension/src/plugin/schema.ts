dec60de2eb512ef717024db36527abaeed2c5c3e
Felix Haeberle <kontakt@felixhaeberle.de>
2023-05-02 15:49:23 +0200

    fix: types

--- source-code/ide-extension/src/plugin/schema.ts
+++ source-code/ide-extension/src/plugin/schema.ts
@@ -22,46 +22,42 @@ const messageReferenceSchema = z.object({
 	position: positionSchema,
 })
 
-const ideExtensionSchema = z
-	.object({
-		/**
-		 * Defines matchers for message references inside the code.
-		 *
-		 * @param args represents the data to conduct the search on
-		 * @returns a promise with matched message references
-		 */
-		messageReferenceMatchers: z.array(
-			z
-				.function()
-				.args(z.object({ documentText: z.string() }))
-				.returns(z.promise(z.array(messageReferenceSchema))),
-		),
-		/**
-		 * Defines the options to extract messages.
-		 */
-		extractMessageOptions: z.array(
-			z.object({
-				/**
-				 * Function which is called, when the user finished the message extraction command.
-				 *
-				 * @param messageId is the message identifier entered by the user
-				 * @param selection is the text which was extracted
-				 * @returns the code which is inserted into the document
-				 */
-				callback: z.function().args(z.string(), z.string()).returns(z.string()),
-			}),
-		),
+const ideExtensionSchema = z.object({
+	/**
+	 * Defines matchers for message references inside the code.
+	 *
+	 * @param args represents the data to conduct the search on
+	 * @returns a promise with matched message references
+	 */
+	messageReferenceMatchers: z
+		.function()
+		.args(z.object({ documentText: z.string() }))
+		.returns(z.promise(z.array(messageReferenceSchema))),
+	/**
+	 * Defines the options to extract messages.
+	 */
+	extractMessageOptions: z.array(
+		z.object({
+			/**
+			 * Function which is called, when the user finished the message extraction command.
+			 *
+			 * @param messageId is the message identifier entered by the user
+			 * @param selection is the text which was extracted
+			 * @returns the code which is inserted into the document
+			 */
+			callback: z.function().args(z.string(), z.string()).returns(z.string()),
+		}),
+	),
 
-		/**
-		 * An array of VSCode DocumentSelectors.
-		 *
-		 * The document selectors specify for which files/programming languages
-		 * (typescript, svelte, etc.) the extension should be activated.
-		 *
-		 * See https://code.visualstudio.com/api/references/document-selector
-		 */
-		// documentSelectors: DocumentSelector[];
-	})
-	.optional()
+	/**
+	 * An array of VSCode DocumentSelectors.
+	 *
+	 * The document selectors specify for which files/programming languages
+	 * (typescript, svelte, etc.) the extension should be activated.
+	 *
+	 * See https://code.visualstudio.com/api/references/document-selector
+	 */
+	// documentSelectors: DocumentSelector[];
+})
 
 export type IdeExtensionConfig = z.TypeOf<typeof ideExtensionSchema>
