0c7b684df0725ace3d036da263c87793e9e75195
Felix Häberle <34959078+felixhaeberle@users.noreply.github.com>
2023-05-03 14:47:28 +0200

    refactor: add ide extension plugin (#619)
    Co-authored-by: Ivan Hofer <ivan.hofer@outlook.com>

--- packages/sdk-js/src/plugin/plugin.ts
+++ packages/sdk-js/src/plugin/plugin.ts
@@ -1,5 +1,6 @@
 import { type SdkConfig, validateSdkConfig } from './schema.js'
 import type { InlangConfig } from '@inlang/core/config'
+import ideExtensionPlugin, { type IdeExtensionSettings } from '@inlang/ide-extension-plugin'
 import type { InlangEnvironment } from '@inlang/core/environment'
 import { createPlugin } from "@inlang/core/plugin"
 
@@ -22,6 +23,8 @@ export const sdkPlugin = createPlugin<SdkConfig>(({ settings, env }) => ({
 const addDefaultResourcePluginIfMissing = async (config: Partial<InlangConfig>, env: InlangEnvironment): Promise<Partial<InlangConfig>> => {
 	if (config.readResources) return {}
 
+	// TODO: check if correct
+	// TODO: use plugin from v2
 	const plugin = await env.$import(
 		"https://cdn.jsdelivr.net/gh/samuelstroschein/inlang-plugin-json@1/dist/index.js",
 	)
@@ -37,63 +40,63 @@ const addDefaultResourcePluginIfMissing = async (config: Partial<InlangConfig>, 
 	}
 }
 
-const addIdeExtensionPluginIfMissing = async (config: Partial<InlangConfig>, env: InlangEnvironment): Promise<{ ideExtension?: InlangConfig['ideExtension'] }> => {
-	if (config.ideExtension) return {}
+const addIdeExtensionPluginIfMissing = async (config: Partial<InlangConfig> & { ideExtension?: IdeExtensionSettings }, env: InlangEnvironment): Promise<{ ideExtension: IdeExtensionSettings } | undefined> => {
+	if (config.ideExtension) return
 
 	// TODO: use ideExtensionPlugin once released
 
 	// TODO: check if correct
 
-	return {
-		ideExtension: {
-			messageReferenceMatchers: [
-				async (args) => {
-					const regex = /(?<!\w){?t\(['"](?<messageId>\S+)['"]\)}?/gm;
-					const str = args.documentText;
-					let match;
-					const result = [];
+	const pluginSetupFunction = ideExtensionPlugin({
+		messageReferenceMatchers: async (args) => {
+			const regex = /(?<!\w){?t\(['"](?<messageId>\S+)['"]\)}?/gm;
+			const str = args.documentText;
+			let match;
+			const result = [];
 
-					while ((match = regex.exec(str)) !== null) {
-						const startLine =
-							(str.slice(0, Math.max(0, match.index)).match(/\n/g) || [])
-								.length + 1;
-						const startPos =
-							match.index - str.lastIndexOf("\n", match.index - 1);
-						const endPos =
-							match.index +
-							match[0].length -
-							str.lastIndexOf("\n", match.index + match[0].length - 1);
-						const endLine =
-							(
-								str
-									.slice(0, Math.max(0, match.index + match[0].length))
-									.match(/\n/g) || []
-							).length + 1;
+			while ((match = regex.exec(str)) !== null) {
+				const startLine =
+					(str.slice(0, Math.max(0, match.index)).match(/\n/g) || [])
+						.length + 1;
+				const startPos =
+					match.index - str.lastIndexOf("\n", match.index - 1);
+				const endPos =
+					match.index +
+					match[0].length -
+					str.lastIndexOf("\n", match.index + match[0].length - 1);
+				const endLine =
+					(
+						str
+							.slice(0, Math.max(0, match.index + match[0].length))
+							.match(/\n/g) || []
+					).length + 1;
 
-						if (match.groups && "messageId" in match.groups) {
-							result.push({
-								messageId: match.groups["messageId"]!,
-								position: {
-									start: {
-										line: startLine,
-										character: startPos,
-									},
-									end: {
-										line: endLine,
-										character: endPos,
-									},
-								},
-							});
-						}
-					}
-					return result;
-				},
-			],
-			extractMessageOptions: [
-				{
-					callback: (messageId) => `{i("${messageId}")}`,
-				},
-			],
-		}
-	}
+				if (match.groups && "messageId" in match.groups) {
+					result.push({
+						messageId: match.groups["messageId"]!,
+						position: {
+							start: {
+								line: startLine,
+								character: startPos,
+							},
+							end: {
+								line: endLine,
+								character: endPos,
+							},
+						},
+					});
+				}
+			}
+			return result;
+		},
+		extractMessageOptions: [
+			{
+				callback: (messageId) => `{i("${messageId}")}`,
+			},
+		],
+	})
+
+	const plugin = pluginSetupFunction(env)
+
+	return plugin.config(config) as { ideExtension: IdeExtensionSettings }
 }
