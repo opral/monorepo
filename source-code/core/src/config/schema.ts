c1a9c39f57b418b8900f6d12a8ec805c93563848
Felix Häberle <34959078+felixhaeberle@users.noreply.github.com>
2023-05-03 14:47:28 +0200

    refactor: add ide extension plugin (#619)
    Co-authored-by: Ivan Hofer <ivan.hofer@outlook.com>

--- source-code/core/src/config/schema.ts
+++ source-code/core/src/config/schema.ts
@@ -78,54 +78,6 @@ export type InlangConfig = {
 	 * Read more under https://inlang.com/documentation/breaking-changes
 	 */
 	experimental?: Record<string, unknown>
-
-	ideExtension?: {
-		/**
-		 * Defines matchers for message references inside the code.
-		 *
-		 * @param args represents the data to conduct the search on
-		 * @returns a promise with matched message references
-		 */
-		messageReferenceMatchers: ((args: { documentText: string }) => Promise<
-			Array<{
-				/**
-				 * The messages id.
-				 */
-				messageId: string
-				/**
-				 * The position from where to where the reference can be found.
-				 */
-				position: {
-					start: { line: number; character: number }
-					end: { line: number; character: number }
-				}
-			}>
-		>)[]
-
-		/**
-		 * Defines the options to extract messages.
-		 */
-		extractMessageOptions: {
-			/**
-			 * Function which is called, when the user finished the message extraction command.
-			 *
-			 * @param messageId is the message identifier entered by the user
-			 * @param selection is the text which was extracted
-			 * @returns the code which is inserted into the document
-			 */
-			callback: (messageId: string, selection: string) => string
-		}[]
-
-		/**
-		 * An array of VSCode DocumentSelectors.
-		 *
-		 * The document selectors specify for which files/programming languages
-		 * (typescript, svelte, etc.) the extension should be activated.
-		 *
-		 * See https://code.visualstudio.com/api/references/document-selector
-		 */
-		// documentSelectors: DocumentSelector[];
-	}
 }
 
 type WithRequired<T, K extends keyof T> = T & { [P in K]-?: T[P] }
---
+++ source-code/ide-extension-plugin/src/index.ts
@@ -0,0 +1,2 @@
+export { ideExtensionPlugin as default } from "./plugin.js"
+export type { IdeExtensionSettings } from "./schema.js"
