5fc6561e5e2a82252e6f5c6f0ee6ae75d497cd22
Stepan Orda <stepanorda87@gmail.com>
2023-05-03 17:08:13 +0100

    Fix tests

--- source-code/sdk-js/src/plugin/plugin.ts
+++ source-code/sdk-js/src/plugin/plugin.ts
@@ -1,11 +1,11 @@
-import { type SdkConfig, validateSdkConfig } from './schema.js'
+import { type SdkConfigInput, validateSdkConfig } from './schema.js'
 import type { InlangConfig } from '@inlang/core/config'
 import type { InlangEnvironment } from '@inlang/core/environment'
 import { createPlugin } from "@inlang/core/plugin"
 
 // ------------------------------------------------------------------------------------------------
 
-export const sdkPlugin = createPlugin<SdkConfig>(({ settings, env }) => ({
+export const sdkPlugin = createPlugin<SdkConfigInput>(({ settings, env }) => ({
 	id: "inlang.sdk-js",
 	config: async () => {
 		const parsedConfig = validateSdkConfig(settings)
