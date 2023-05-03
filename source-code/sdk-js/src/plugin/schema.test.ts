48eb074dd233854b4fceed1ef5f59aa59d95b210
Stepan Orda <stepanorda87@gmail.com>
2023-05-03 19:47:48 +0100

    Tests for all available strategies

--- source-code/sdk-js/src/plugin/schema.test.ts
+++ source-code/sdk-js/src/plugin/schema.test.ts
@@ -22,7 +22,7 @@ describe("validateSdkConfig", () => {
 		}).toThrow()
 	})
 
-	test("Valid config", () => {
+	test("Url strategy", () => {
 		expect(
 			validateSdkConfig({
 				languageNegotiation: {
@@ -51,5 +51,82 @@ describe("validateSdkConfig", () => {
             },
           }
         `)
+	})
+
+	test("Accept-language strategy", () => {
+		expect(
+			validateSdkConfig({
+				languageNegotiation: {
+					strategies: [
+						{
+							type: "accept-language-header",
+						},
+					],
+				},
+			} as unknown as SdkConfigInput),
+		).toMatchInlineSnapshot(`
+			{
+			  "languageNegotiation": {
+			    "strategies": [
+			      {
+			        "type": "accept-language-header",
+			      },
+			    ],
+			    "strict": false,
+			  },
+			}
+		`)
+	})
+
+	test("Navigator strategy", () => {
+		expect(
+			validateSdkConfig({
+				languageNegotiation: {
+					strategies: [
+						{
+							type: "navigator",
+						},
+					],
+				},
+			} as unknown as SdkConfigInput),
+		).toMatchInlineSnapshot(`
+			{
+			  "languageNegotiation": {
+			    "strategies": [
+			      {
+			        "type": "navigator",
+			      },
+			    ],
+			    "strict": false,
+			  },
+			}
+		`)
+	})
+
+	test("Local storage strategy", () => {
+		expect(
+			validateSdkConfig({
+				languageNegotiation: {
+					strategies: [
+						{
+							type: "localStorage",
+							key: "language",
+						},
+					],
+				},
+			} as unknown as SdkConfigInput),
+		).toMatchInlineSnapshot(`
+			{
+			  "languageNegotiation": {
+			    "strategies": [
+			      {
+			        "key": "language",
+			        "type": "localStorage",
+			      },
+			    ],
+			    "strict": false,
+			  },
+			}
+		`)
 	})
 })
