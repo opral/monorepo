3413cc32c081ba100d4159be9e022b6d1ebe7b91
Ivan Hofer <ivan.hofer@outlook.com>
2023-05-03 07:20:27 +0200

    add `transformHooksServerJs` test cases

--- source-code/sdk-js/src/adapter-sveltekit/ast-transformation/transforms/hooks.server.js.test.ts
+++ source-code/sdk-js/src/adapter-sveltekit/ast-transformation/transforms/hooks.server.js.test.ts
@@ -1,52 +1,164 @@
 import { describe, it } from "vitest"
+import { dedent } from "ts-dedent"
 import { transformHooksServerJs } from "../transforms/hooks.server.js.js"
 import { baseTestConfig } from "./test-helpers/config.js"
 import type { TransformConfig } from "../config.js"
 
 describe("transformHooksServerJs", () => {
-	it("returns new file if no code provided", ({ expect }) => {
-		const code = transformHooksServerJs(baseTestConfig, "")
-		expect(code).toContain("getLanguage")
-	})
-	it("languageInUrl and isStatic", ({ expect }) => {
-		const config: TransformConfig = {
-			...baseTestConfig,
-			languageInUrl: true,
-			isStatic: true,
-		}
-		const code = transformHooksServerJs(config, "")
-		expect(code).toMatchInlineSnapshot(`
-			"import { replaceLanguageInUrl } from \\"@inlang/sdk-js/adapter-sveltekit/shared\\";
-			import { redirect } from \\"@sveltejs/kit\\";
-			import { initAcceptLanguageHeaderDetector } from \\"@inlang/sdk-js/detectors/server\\";
-			import { initHandleWrapper } from \\"@inlang/sdk-js/adapter-sveltekit/server\\";
-			export const handle = initHandleWrapper({
-			  getLanguage: ({ url }) => url.pathname.split(\\"/\\")[1]
-			}).wrap(async ({ event, resolve }) => resolve(event));"
-		`)
+	describe("basics", () => {
+		it("adds handle function to an empty file", ({ expect }) => {
+			const code = transformHooksServerJs({} as TransformConfig, "")
+			expect(code).toMatchInlineSnapshot(`
+				"import { initHandleWrapper } from \\"@inlang/sdk-js/adapter-sveltekit/server\\";
+				export const handle = initHandleWrapper({
+				  getLanguage: () => undefined
+				}).wrap(async ({ event, resolve }) => resolve(event));"
+			`)
+		})
+
+		it("adds handle endpoint to a file with arbitrary contents", ({ expect }) => {
+			const code = transformHooksServerJs({} as TransformConfig, dedent`
+				import * as Sentry from '@sentry/node';
+				import crypto from 'crypto';
+
+				Sentry.init({/*...*/})
+
+				/** @type {import('@sveltejs/kit').HandleServerError} */
+				export async function handleError({ error, event }) {
+					const errorId = crypto.randomUUID();
+					// example integration with https://sentry.io/
+					Sentry.captureException(error, { event, errorId });
+
+					return {
+						message: 'Whoops!',
+						errorId
+					};
+				}
+			`)
+			expect(code).toMatchInlineSnapshot(`
+				"import { initHandleWrapper } from \\"@inlang/sdk-js/adapter-sveltekit/server\\";
+				import * as Sentry from '@sentry/node';
+				import crypto from 'crypto';
+
+				Sentry.init({/*...*/})
+
+				/** @type {import('@sveltejs/kit').HandleServerError} */
+				export async function handleError({ error, event }) {
+					const errorId = crypto.randomUUID();
+					// example integration with https://sentry.io/
+					Sentry.captureException(error, { event, errorId });
+
+					return {
+						message: 'Whoops!',
+						errorId
+					};
+				}export const handle = initHandleWrapper({
+				  getLanguage: () => undefined
+				}).wrap(async ({ event, resolve }) => resolve(event));"
+			`)
+		})
+
+		describe("should wrap handle if already defined", () => {
+			it("arrow function", ({ expect }) => {
+				const code = transformHooksServerJs({} as TransformConfig, dedent`
+					import type { Handle } from '@sveltejs/kit'
+
+					export const handle: Handle = ({ event, resolve }) => {
+
+						event.locals = {
+							userId: 123
+						}
+
+						return resolve(event)
+					}
+				`)
+
+				expect(code).toMatchInlineSnapshot(`
+					"import { initHandleWrapper } from \\"@inlang/sdk-js/adapter-sveltekit/server\\";
+					import type { Handle, redirect } from '@sveltejs/kit';
+
+					export const handle: Handle = initHandleWrapper({
+					  getLanguage: () => undefined
+					}).wrap(({ event, resolve }) => {
+
+						event.locals = {
+							userId: 123
+						}
+
+						return resolve(event)
+					})"
+				`)
+			})
+
+			it("function keyword", ({ expect }) => {
+				const code = transformHooksServerJs({} as TransformConfig, dedent`
+					export function handle({ event, resolve }) {
+						console.log('TADAA!')
+						return resolve(event)
+					}
+				`)
+
+				expect(code).toMatchInlineSnapshot(`
+					"import { initHandleWrapper } from \\"@inlang/sdk-js/adapter-sveltekit/server\\";
+					export const handle = initHandleWrapper({
+					  getLanguage: () => undefined
+					}).wrap(function handle({ event, resolve }) {
+						console.log('TADAA!')
+						return resolve(event)
+					});"
+				`)
+			})
+		})
 	})
 
-	it("isStatic", ({ expect }) => {
-		const config: TransformConfig = {
-			...baseTestConfig,
-			isStatic: true,
-		}
-		const code = transformHooksServerJs(config, "")
-		expect(code).toMatchInlineSnapshot(`
-			"import { replaceLanguageInUrl } from \\"@inlang/sdk-js/adapter-sveltekit/shared\\";
-			import { redirect } from \\"@sveltejs/kit\\";
-			import { initAcceptLanguageHeaderDetector } from \\"@inlang/sdk-js/detectors/server\\";
-			import { initHandleWrapper } from \\"@inlang/sdk-js/adapter-sveltekit/server\\";
-			export const handle = initHandleWrapper({
-			  getLanguage: () => undefined
-			}).wrap(async ({ event, resolve }) => resolve(event));"
-		`)
+	describe("variations", () => {
+		it("languageInUrl", ({ expect }) => {
+			const config: TransformConfig = {
+				...baseTestConfig,
+				languageInUrl: true,
+				isStatic: true,
+			}
+			const code = transformHooksServerJs(config, "")
+			expect(code).toMatchInlineSnapshot(`
+				"import { replaceLanguageInUrl } from \\"@inlang/sdk-js/adapter-sveltekit/shared\\";
+				import { redirect } from \\"@sveltejs/kit\\";
+				import { initAcceptLanguageHeaderDetector } from \\"@inlang/sdk-js/detectors/server\\";
+				import { initHandleWrapper } from \\"@inlang/sdk-js/adapter-sveltekit/server\\";
+				export const handle = initHandleWrapper({
+				  getLanguage: ({ url }) => url.pathname.split(\\"/\\")[1]
+				}).wrap(async ({ event, resolve }) => resolve(event));"
+			`)
+		})
+
+		it("languageInUrl and isStatic", ({ expect }) => {
+			const config: TransformConfig = {
+				...baseTestConfig,
+				languageInUrl: true,
+				isStatic: true,
+			}
+			const code = transformHooksServerJs(config, "")
+			expect(code).toMatchInlineSnapshot(`
+				"import { initHandleWrapper } from \\"@inlang/sdk-js/adapter-sveltekit/server\\";
+				export const handle = initHandleWrapper({
+				  getLanguage: ({ url }) => url.pathname.split(\\"/\\")[1]
+				}).wrap(async ({ event, resolve }) => resolve(event));"
+			`)
+		})
+
+		it("isStatic", ({ expect }) => {
+			const config: TransformConfig = {
+				...baseTestConfig,
+				isStatic: true,
+			}
+			const code = transformHooksServerJs(config, "")
+			expect(code).toMatchInlineSnapshot(`
+				"import { initHandleWrapper } from \\"@inlang/sdk-js/adapter-sveltekit/server\\";
+				export const handle = initHandleWrapper({
+				  getLanguage: () => undefined
+				}).wrap(async ({ event, resolve }) => resolve(event));"
+			`)
+		})
 	})
+
+	describe.todo("detectors", () => { })
 })
-
-// NOTES @stepan
-// Only one function for everything (transformHooksServerJs)
-// Capabilities:
-// 1. merge required imports with current imports
-// 2. wrap existing load statement
-// 3. Retain other, load unrelated statements
