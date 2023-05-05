bc969bd2ef003fefb95e6fb1cb741d82eda2e568
Benjamin Preiss <benjaminpreiss@protonmail.com>
2023-05-05 17:46:06 +0800

    Transform +page.js and add corresponding tests

--- source-code/sdk-js/src/adapter-sveltekit/vite-plugin/transforms/+page.js.ts
+++ source-code/sdk-js/src/adapter-sveltekit/vite-plugin/transforms/+page.js.ts
@@ -1,51 +1,55 @@
-import type { TransformConfig } from '../config.js'
-import { transformJs } from './*.js.js'
+import type { TransformConfig } from "../config.js"
+import { parseModule, generateCode, parseExpression } from "magicast"
+import { deepMergeObject } from "magicast/helpers"
+import { types } from "recast"
+import {
+	getArrowOrFunction,
+	getWrappedExport,
+	replaceOrAddExportNamedFunction,
+} from "../../../helpers/ast.js"
 
-export const transformPageJs = (config: TransformConfig, code: string, root: boolean) => {
-	if (root) return transformRootPageJs(config, code)
+const requiredImports = (root: boolean) => `
+import { browser } from "$app/environment";
+import { ${
+	root ? "initRootPageLoadWrapper" : "initLoadWrapper"
+}, replaceLanguageInUrl } from "@inlang/sdk-js/adapter-sveltekit/shared";
+import { initLocalStorageDetector, navigatorDetector } from "@inlang/sdk-js/detectors/client";
+import { localStorageKey } from "@inlang/sdk-js/adapter-sveltekit/client/reactive";
+`
 
-	return transformGenericPageJs(config, code)
-}
-
-// ------------------------------------------------------------------------------------------------
-
-// TODO: @benjaminpreiss: transform
-const transformRootPageJs = (config: TransformConfig, code: string) => {
-	// TODO: currently we just override the code
-	// if (code) return wrapRootPageJs(config, code)
-
-	return createRootPageJs(config)
-}
-
-export const createRootPageJs = (config: TransformConfig) => {
-	if (config.isStatic && config.languageInUrl) {
-		return `
-import { initRootPageLoadWrapper } from "@inlang/sdk-js/adapter-sveltekit/shared"
-import { navigatorDetector } from "@inlang/sdk-js/detectors/client"
-import { browser } from "$app/environment"
-import { redirect } from "@sveltejs/kit"
-import { replaceLanguageInUrl } from "@inlang/sdk-js/adapter-sveltekit/shared"
-
-export const load = initRootPageLoadWrapper({
+const options = (config: TransformConfig) =>
+	config.languageInUrl && config.isStatic
+		? `
+{
 	browser,
 	initDetectors: () => [navigatorDetector],
 	redirect: {
 		throwable: redirect,
 		getPath: ({ url }, language) => replaceLanguageInUrl(new URL(url), language),
 	},
-}).wrap(async () => { })
-`
+}
+	`
+		: `{browser}`
+
+export const transformPageJs = (config: TransformConfig, code: string, root: boolean) => {
+	const n = types.namedTypes
+	const b = types.builders
+	const ast = parseModule(code)
+
+	// Merge imports with required imports
+	const importsAst = parseModule(requiredImports(root))
+	deepMergeObject(ast, importsAst)
+	const emptyArrowFunctionDeclaration = b.arrowFunctionExpression([], b.blockStatement([]))
+	const arrowOrFunctionNode = getArrowOrFunction(ast.$ast, "load", emptyArrowFunctionDeclaration)
+	const exportAst = getWrappedExport(
+		parseExpression(root ? options(config) : "{}"),
+		[arrowOrFunctionNode],
+		"load",
+		root ? "initRootPageLoadWrapper" : "initLoadWrapper",
+	)
+	// Replace or add current export handle
+	if (n.Program.check(ast.$ast)) {
+		replaceOrAddExportNamedFunction(ast.$ast, "load", exportAst)
 	}
-
-	return ''
+	return generateCode(ast).code
 }
-
-// TODO: transform
-const wrapRootPageJs = (config: TransformConfig, code: string) => {
-	// TODO: more meaningful error messages
-	throw new Error('currently not supported')
-}
-
-// ------------------------------------------------------------------------------------------------
-
-const transformGenericPageJs = transformJs
