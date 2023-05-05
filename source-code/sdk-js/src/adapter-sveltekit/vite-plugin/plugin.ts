45a2b906602fb30d22a7f6b9a7f95a1b071d901e
Ivan Hofer <ivan.hofer@outlook.com>
2023-05-05 16:11:10 +0200

    improve comment on empty files

--- source-code/sdk-js/src/adapter-sveltekit/vite-plugin/plugin.ts
+++ source-code/sdk-js/src/adapter-sveltekit/vite-plugin/plugin.ts
@@ -158,7 +158,12 @@ const createFilesIfNotPresent = async (config: TransformConfig) => {
 					const path = getPathForFileType(fileType)
 					await mkdir(dirname(path), { recursive: true }).catch(() => undefined)
 					// TODO: improve robustness by using something like `vite-plugin-restart` that recreates those file if they were deleted
-					const message = 'This file was created by inlang. It is needed in order to circumvent a current limitation of SvelteKit. Please do not delete it (inlang will recreate it if needed).'
+					const message = dedent`
+						This file was created by inlang.
+						It is needed in order to circumvent a current limitation of SvelteKit. See https://github.com/inlang/inlang/issues/647
+						You can remove this comment and modify the file as you like. We just need to make sure it exists.
+						Please do not delete it (inlang will recreate it if needed).
+					`
 					await writeFile(path, path.endsWith('.svelte') ? `<!-- ${message} -->` : `// ${message}`)
 
 					resolve(true)
