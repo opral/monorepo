f118699138b68b07712fe93cd0d0fb093ae14f18
Hofer Ivan <ivan.hofer@outlook.com>
2023-05-09 09:47:48 +0200

    #657: fix builds of `SvelteKit` application using `sdk-js` (#684)

--- source-code/sdk-js/src/adapter-sveltekit/examples/spa/src/hooks.server.ts
+++ source-code/sdk-js/src/adapter-sveltekit/examples/spa/src/hooks.server.ts
@@ -1,6 +1,7 @@
 import { initHandleWrapper } from "@inlang/sdk-js/adapter-sveltekit/server"
 
 export const handle = initHandleWrapper({
+	inlangConfigModule: import("../inlang.config.js"),
 	getLanguage: () => undefined,
 }).wrap(async ({ event, resolve }) => {
 	console.info("--- new request", event.url.toString())
--- source-code/sdk-js/src/plugin/plugin.ts
+++ source-code/sdk-js/src/plugin/plugin.ts
@@ -80,7 +80,7 @@ const addIdeExtensionPluginIfMissing = async (
 					}
 				}
 				return result
-			},
+			}
 		],
 		extractMessageOptions: [
 			{
