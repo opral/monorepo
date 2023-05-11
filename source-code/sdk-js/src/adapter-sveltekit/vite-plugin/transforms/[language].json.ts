c06bb17afc8450209b362df0879e29ad87f06a35
Ivan Hofer <ivan.hofer@outlook.com>
2023-05-11 09:09:17 +0200

    #647: prerender all resource API calls

--- source-code/sdk-js/src/adapter-sveltekit/vite-plugin/transforms/[language].json.ts
+++ source-code/sdk-js/src/adapter-sveltekit/vite-plugin/transforms/[language].json.ts
@@ -3,7 +3,7 @@ import type { TransformConfig } from '../config.js'
 // TODO: @benjaminpreiss: transform
 export const transformLanguageJson = (config: TransformConfig, code: string) => {
 	if (code) {
-		// TODO: throw error if file contains a GET endpoint; currently we just override it
+		// TODO: throw error if file contains any other export that does not begin with `_`; currently we just override it
 		// throw new Error()
 	}
 
@@ -15,5 +15,16 @@ export const GET = async ({ params: { language } }) => {
 	await reloadResources()
 	return json(getResource(language) || null)
 }
+
+${config.svelteKit.version || '' >= '1.16.3' ? `
+import { initState } from '@inlang/sdk-js/adapter-sveltekit/server'
+
+export const prerender = true
+
+export const entries = async () => {
+	const { languages } = await initState(await import('../../../../inlang.config.js'))
+
+	return languages.map(language => ({ language }))
+}` : ''}
 `
 }
