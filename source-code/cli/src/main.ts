2a3da4f430c1c722f84ad752eedf5506490f4d25
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2023-05-12 15:59:55 -0400

    add: error monitoring to fix bugs faster

--- source-code/cli/src/main.ts
+++ source-code/cli/src/main.ts
@@ -2,6 +2,9 @@ import { Command } from "commander"
 import { config } from "./commands/config/index.js"
 import { version } from "../package.json"
 import consola, { Consola } from "consola"
+import { initErrorMonitoring } from "./services/error-monitoring/implementation.js"
+
+initErrorMonitoring()
 
 // beautiful logging
 ;(consola as unknown as Consola).wrapConsole()
