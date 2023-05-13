4094cb781ab4698276b8f9169ed02a657765e426
araknast <araknast@protonmail.com>
2023-05-13 14:38:27 -0700

    Purge memfs from `badge`

--- packages/badge/src/repo/clone.ts
+++ packages/badge/src/repo/clone.ts
@@ -1,9 +1,9 @@
 // clone reposiory with isomorphic-git
 
 import { raw, http } from "@inlang-git/client/raw"
-import type { Volume } from "memfs/lib/volume.js"
+import type { NodeishFilesystem } from "@inlang-git/fs"
 
-const cloneRespository = async (url: string, volume: Volume) => {
+const cloneRespository = async (url: string, fs: NodeishFilesystem) => {
 	// parse url in the format of github.com/inlang/example and split it to host, owner and repo
 	const [host, owner, repo] = [...url.split("/")]
 
@@ -16,7 +16,7 @@ const cloneRespository = async (url: string, volume: Volume) => {
 
 	// clone the repository, resolves if the repository is cloned
 	await raw.clone({
-		fs: volume,
+		fs,
 		http,
 		dir: "/",
 		url: `https://${host}/${owner}/${repo}`,
