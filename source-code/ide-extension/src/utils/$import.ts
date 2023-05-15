f0c179090c5484cee7678dd21f300c937f2d17ea
Felix Haeberle <kontakt@felixhaeberle.de>
2023-05-15 18:26:09 +0200

    fix: dynamic import for ide extension

--- source-code/ide-extension/src/utils/$import.ts
+++ source-code/ide-extension/src/utils/$import.ts
@@ -1,28 +1,106 @@
+import { dedent } from "ts-dedent"
 import path from "node:path"
 import ts from "typescript"
 import * as vscode from "vscode"
 import fsExtra from "fs-extra"
+import type { $fs } from "@inlang/core/environment"
+import { normalizePath } from "@inlang-git/fs"
+import type fetch from "node-fetch"
 
-// Transpile code to CommonJS using TypeScript Compiler API
-function transpileCode(code: string): string {
-	const compilerOptions: ts.CompilerOptions = {
-		module: ts.ModuleKind.CommonJS,
-		target: ts.ScriptTarget.ES2015,
+/**
+ * Importing ES modules either from a local path, or from a url.
+ *
+ * The imported module must be ESM. A good indicator is whether
+ * the "type" property in a package.json is set to "module" if
+ * node is used.
+ *
+ * Read more on https://inlang.com/documentation/config
+ */
+//
+// - explitcly export the interface of $import to be consumed
+//   in the config with JSdoc.
+//
+// - not using ReturnType or FunctionArguments to increase DX
+//   when hovering over the type.
+
+export type $import = (uri: string) => Promise<any>
+
+/**
+ * Initializes the $import function.
+ *
+ * @example
+ * const $import = initialize$import({ fs: fs.promises, fetch });
+ * const module = await $import('./some-module.js');
+ */
+export function initialize$import(args: {
+	/** the fs from which the file can be read */
+	fs: $fs
+	/** http client implementation */
+	fetch: typeof fetch
+}): (uri: string) => ReturnType<typeof $import> {
+	// resembles a native import api
+	return (uri: string) => $import(uri, args)
+}
+
+/**
+ * Error thrown when the $import function fails.
+ *
+ * Dedicated class to make it easier to identify this error.
+ */
+class $ImportError extends Error {
+	constructor(message: string) {
+		super(message)
+		this.name = "$ImportError"
 	}
+}
 
-	const result = ts.transpileModule(code, { compilerOptions })
-	return result.outputText
+/**
+ * Importing ES modules either from a local path, or from a url.
+ *
+ * The imported module must be ESM. A good indicator is whether
+ * the "type" property in a package.json is set to "module" if
+ * node is used.
+ *
+ * Read more on https://inlang.com/documentation/config
+ */
+async function $import(
+	uri: string,
+	environment: {
+		/** the fs from which the file can be read */
+		fs: $fs
+		/** http client implementation */
+		fetch: typeof fetch
+	},
+): Promise<any> {
+	// avoiding browser built-in shadowing of fetch as global variable
+	const _fetch = environment.fetch
+	// polyfill for environments that don't support dynamic
+	// http imports yet like VSCode.
+	const moduleAsText = uri.startsWith("http")
+		? await (await _fetch(uri)).text()
+		: ((await environment.fs.readFile(normalizePath(uri), { encoding: "utf-8" })) as string)
+	try {
+		return await transpile$import(/* @vite-ignore */ moduleAsText)
+	} catch (error) {
+		let message = `Error while importing ${uri}: ${(error as Error)?.message ?? "Unknown error"}`
+		if (error instanceof SyntaxError && uri.includes("jsdelivr")) {
+			message += dedent`\n\n
+Are you sure that the file exists on JSDelivr?
+
+The error indicates that the imported file does not exist on JSDelivr. For non-existent files, JSDelivr returns a 404 text that JS cannot parse as a module and throws a SyntaxError.
+			`
+		}
+		throw new $ImportError(message)
+	}
 }
 
 // Custom $import function
-export const $import = async (modulePath: string): Promise<any> => {
+export const transpile$import = async (code: string): Promise<any> => {
 	// Read the code from the module path
-	const code = await vscode.workspace.fs.readFile(vscode.Uri.file(modulePath))
+	// const code = await vscode.workspace.fs.readFile(vscode.Uri.file(modulePath))
 
 	// Transpile the code to CommonJS
 	const transpiledCode = transpileCode(code.toString())
-
-	console.log("transpiledCode", transpiledCode)
 
 	// Create the .inlang folder if it doesn't exist
 	const inlangFolder = ".inlang"
@@ -60,4 +138,15 @@ export const $import = async (modulePath: string): Promise<any> => {
 	await fsExtra.remove(folderUri.fsPath)
 
 	return module
+}
+
+// Transpile code to CommonJS using TypeScript Compiler API
+function transpileCode(code: string): string {
+	const compilerOptions: ts.CompilerOptions = {
+		module: ts.ModuleKind.CommonJS,
+		target: ts.ScriptTarget.ES2015,
+	}
+
+	const result = ts.transpileModule(code, { compilerOptions })
+	return result.outputText
 }
