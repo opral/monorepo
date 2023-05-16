3004ced1d95b42c6d1772b221dcb8e749160806d
Felix Haeberle <kontakt@felixhaeberle.de>
2023-05-16 15:20:11 +0200

    chore: add first draft of machine translation command

--- packages/cli/src/utilities.ts
+++ packages/cli/src/utilities.ts
@@ -1,3 +1,4 @@
+import type { Resource } from "@inlang/core/ast"
 import consola, { Consola } from "consola"
 
 /**
@@ -9,3 +10,26 @@ import consola, { Consola } from "consola"
  *   log.success("Success")
  */
 export const log = consola as unknown as Consola
+
+/**
+ * Counts the number of messages per language.
+ *
+ * @param resource The resource to count the messages for.
+ * @returns A record with the language as key and the number of messages as value.
+ *
+ */
+export const countMessagesPerLanguage = (resource: Resource[]): Record<string, number> => {
+	const counts: Record<string, number> = {}
+
+	for (const { languageTag, body } of resource) {
+		const language = languageTag.name
+
+		if (!counts[language]) {
+			counts[language] = 0
+		}
+
+		counts[language] += body.length
+	}
+
+	return counts
+}
