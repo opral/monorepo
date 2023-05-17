03565f12e0263fc0fb516e3f6ba386ff8e626a2d
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2023-05-17 19:23:02 +0200

    chore: improve env failure message and comments
    related to CLI has no access to `env`/`fetch` #777

--- packages/env-variables/build.ts
+++ packages/env-variables/build.ts
@@ -6,19 +6,29 @@ import { validateEnvVariables } from "./src/validateEnvVariables.js"
 import { fetchPublicEnv } from "./src/build/fetchPublicEnv.js"
 import { config } from "dotenv"
 
+const isDevelopment = process.env.DEV ? true : false
+
 // load env files from the root of the project (if exist)
 config({ path: rootEnvFilePath, override: true })
-// entry points of apps need to validate for production
-// this module can't know if the entry point is bundling for production or not.
+
+// Not validating for production under any circumstances, because
+// this module can't know if a dependency requires all production env variables.
+//
+// Instead, apps need to validate for production themselves.
 const [, errors] = validateEnvVariables({ forProduction: false })
+
 if (errors) {
 	console.log("💡 Some env variables are not defined. Fetching public env variables remotely...")
 	console.log(errors)
 	// some required env variables are missing. fetch public env variables from the server
 	await fetchPublicEnv()
+	const [, stillErrors] = validateEnvVariables({ forProduction: isDevelopment === false })
+	if (stillErrors) {
+		console.log("🚨 Some env variables are still not defined, even after fetching. Exiting...")
+		console.log(stillErrors)
+		process.exit(1)
+	}
 }
-
-const isDevelopment = process.env.DEV ? true : false
 
 // @ts-expect-error - esbuild plugin types are wrong
 const ctx = await context({
