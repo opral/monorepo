4b3201a22add442608a41d9beb691990e0782994
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2023-05-19 13:43:08 +0200

    fix: telemetry
    The CLI must (flushAt and flushInterval) immediately. Batching doesn't work.

--- source-code/cli/src/commands/machine/translate.ts
+++ source-code/cli/src/commands/machine/translate.ts
@@ -6,8 +6,8 @@ import { Command } from "commander"
 import { countMessagesPerLanguage, getFlag, log } from "../../utilities.js"
 import type { Message } from "@inlang/core/ast"
 import { rpc } from "@inlang/rpc"
-import { telemetryNode } from "@inlang/telemetry"
-import { exec } from "node:child_process"
+import { raw } from "@inlang-git/client/raw"
+import { telemetry } from "../../services/telemetry/index.js"
 
 export const translate = new Command()
 	.command("translate")
@@ -149,26 +149,30 @@ async function translateCommandAction() {
 		resources: resources,
 	})
 
-	// get remote origin url by executing git command
-	exec("git config --get remote.origin.url", (error, stdout) => {
-		if (error) {
-			console.error("Failed to get the remote URL.", error.message)
-			return
-		}
-
-		const remoteUrl = stdout.trim()
-
-		telemetryNode.capture({
-			distinctId: "unknown",
-			event: "CLI command executed [machine translate]",
-			properties: {
-				remoteUrl,
-				referenceLanguage,
-				languages: languagesToTranslateTo.map((language) => language.languageTag.name),
-			},
-		})
-	})
-
 	// Log the message counts
 	log.info("✅ Translated all messages.")
+
+	// inline try catch to avoid erroring out the entire command
+	const gitOrigin = await (async () => {
+		try {
+			const remotes = await raw.listRemotes({
+				fs,
+				dir: await raw.findRoot({ fs, filepath: process.cwd() }),
+			})
+			return remotes.find((remote) => remote.remote === "origin")?.url
+		} catch (e) {
+			console.warn("Couldn't get git origin. ", e)
+			return undefined
+		}
+	})()
+
+	telemetry.capture({
+		distinctId: "unknown",
+		event: "CLI command executed [machine translate]",
+		properties: {
+			gitOrigin,
+			referenceLanguage,
+			languages: languagesToTranslateTo.map((language) => language.languageTag.name),
+		},
+	})
 }
---
+++ source-code/cli/src/services/telemetry/index.ts
@@ -0,0 +1,23 @@
+import { publicEnv } from "@inlang/env-variables"
+import { PostHog } from "posthog-node"
+
+/**
+ * The telemetry service for the CLI.
+ */
+// If the env variable is set, use the PostHog SDK. Otherwise, use a proxy that
+// returns a no-op function.
+export const telemetry = publicEnv.PUBLIC_POSTHOG_TOKEN
+	? new PostHog(publicEnv.PUBLIC_POSTHOG_TOKEN, {
+			host: "https://eu.posthog.com",
+			// Events are not captured if not immediately flushed.
+			//
+			// Posthog shouldn't batch events because CLI commands
+			// are short-lived, see https://posthog.com/docs/libraries/node.
+			flushAt: 1,
+			flushInterval: 0,
+	  })
+	: new Proxy({} as PostHog, {
+			get() {
+				return () => undefined
+			},
+	  })
