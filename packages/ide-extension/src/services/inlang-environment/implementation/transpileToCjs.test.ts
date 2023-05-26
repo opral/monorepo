f84f40355753f9d1e4e9ad80b91d63a8e2c85415
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2023-05-26 14:55:59 +0200

    test: transpileToCjs

---
+++ packages/ide-extension/src/services/inlang-environment/implementation/transpileToCjs.test.ts
@@ -0,0 +1,24 @@
+import { it, expect, afterAll } from "vitest"
+import { transpileToCjs } from "./transpileToCjs.js"
+import fs from "node:fs"
+
+const currentDirectoryPath = new URL(import.meta.url).pathname.replace(/\/[^/]+$/, "/")
+const tempdir = fs.mkdtempSync(currentDirectoryPath)
+
+afterAll(() => {
+	fs.rmSync(tempdir, { recursive: true, force: true })
+})
+
+it("should transpile esm to cjs", () => {
+	const esmCode = `
+    export function hello(x) {
+      return "Hello " + x
+    }
+  `
+	const cjsCode = transpileToCjs(esmCode)
+	const filePath = `${tempdir}/testfile1.cjs`
+	fs.writeFileSync(filePath, cjsCode, { encoding: "utf8" })
+	// eslint-disable-next-line @typescript-eslint/no-var-requires
+	const module = require(filePath)
+	expect(module.hello("World")).toBe("Hello World")
+})
