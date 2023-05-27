dee231cf4c40e2eba5a05bed0400884e24b99732
jannesblobel <72493222+jannesblobel@users.noreply.github.com>
2023-05-27 18:06:19 +0200

    add test's

--- source-code/telemetry/src/implementation/parseOrigin.test.ts
+++ source-code/telemetry/src/implementation/parseOrigin.test.ts
@@ -23,6 +23,30 @@ it("should return 'unknown' if the remotes contain no origin", () => {
 	expect(result).toBe("unknown")
 })
 
+it("should return the repo  with '.git' at the end  ", () => {
+	const result = parseOrigin({
+		remotes: [
+			{
+				remote: "origin",
+				url: "https://github.com/example/repo",
+			},
+		],
+	})
+
+	expect(result).toBe("github.com/example/repo.git")
+})
+//the effort to validate the tld is to high at the moment. Because, Rexeg is not reliable and introducing a dependency does not seen worse it.
+it.skip("should return unknown if the origin url is invalid ", () => {
+	const result = parseOrigin({
+		remotes: [
+			{
+				remote: "origin",
+				url: `https://fcom/f/d.git`,
+			},
+		],
+	})
+	expect(result).toBe("unknown")
+})
 it("should origin should not contain signing keys", () => {
 	const mockSigningKey = "ghp_Ps2aGEWV0jE7hnm32Zo4HfMABCdRVDE0BxMO1"
 	const result = parseOrigin({
