b0d100f8de377144cec53338d27302458684d164
jannesblobel <72493222+jannesblobel@users.noreply.github.com>
2023-05-27 13:17:13 +0200

    remove gitOriginCheck.ts

--- source-code/telemetry/src/implementation/gitOriginCheck.ts
+++
@@ -1,8 +0,0 @@
-export function originChecked(getOrigin) {
-	if (getOrigin.startsWith("https://" || getOrigin.endsWith(".git"))) {
-		const checkedOrigin = getOrigin
-		return checkedOrigin
-	} else {
-		return "wrong Origin"
-	}
-}
