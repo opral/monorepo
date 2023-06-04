be432ee015f1f4870038b19f0ee90f31717d5e93
Felix Haeberle <kontakt@felixhaeberle.de>
2023-06-04 17:38:23 +0200

    initial draft

--- source-code/cli/src/commands/config/index.ts
+++ source-code/cli/src/commands/config/index.ts
@@ -1,8 +1,10 @@
 import { Command } from "commander"
 import { init } from "./init.js"
+import { validate } from "./validate.js"
 
 export const config = new Command()
 	.command("config")
 	.description("Commands for managing the config file.")
 	.argument("<command>")
 	.addCommand(init)
+	.addCommand(validate)
