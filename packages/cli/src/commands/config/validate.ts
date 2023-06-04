0d485d66d70c2b41baf408ca36fe85ed5f6f5b15
Felix Haeberle <kontakt@felixhaeberle.de>
2023-06-04 17:38:23 +0200

    initial draft

---
+++ packages/cli/src/commands/config/validate.ts
@@ -0,0 +1,24 @@
+import { parseConfig } from "@inlang/core/config"
+import { Command } from "commander"
+import { log } from "../../utilities.js"
+import { getConfig } from "../../utilities/getConfig.js"
+
+export const validate = new Command()
+	.command("validate")
+	.description("Validate the inlang config.")
+	.action(validateCommandAction)
+
+async function validateCommandAction() {
+	log.info("ℹ️  Validating the config ...")
+
+	// Get the config
+	const config = await getConfig()
+	if (!config) {
+		// no message because that's handled in getConfig
+		return
+	}
+
+	const result = parseConfig({ config })
+	//! TODO: Add proper result handling and logging and docs
+	log.info(result)
+}
