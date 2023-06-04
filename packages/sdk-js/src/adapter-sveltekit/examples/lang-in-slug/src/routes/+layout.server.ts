f18a89b9b26f00fcd98733313520d7b38a7072b1
Hofer Ivan <ivan.hofer@outlook.com>
2023-06-04 12:48:16 +0200

    #847: change language if browser back button gets pressed (#879)
    Co-authored-by: Ivan Hofer <ivan.hofer@outlok.com>

--- packages/sdk-js/src/adapter-sveltekit/examples/lang-in-slug/src/routes/+layout.server.ts
+++ packages/sdk-js/src/adapter-sveltekit/examples/lang-in-slug/src/routes/+layout.server.ts
@@ -1,8 +1,11 @@
 import { initRootLayoutServerLoadWrapper } from "@inlang/sdk-js/adapter-sveltekit/server"
 import type { LayoutServerLoad } from "./$types.js"
 
-export const load = initRootLayoutServerLoadWrapper<LayoutServerLoad>().wrap(async (_, { i }) => {
+export const load = initRootLayoutServerLoadWrapper<LayoutServerLoad>().wrap(async ({ params }, { i }) => {
 	console.info("+layout.server.ts", i("welcome"))
+
+	// to retrigger load function when language changes e.g. due to a popstate event
+	params.lang
 
 	return { "+layout.server.ts": Math.random() }
 })
