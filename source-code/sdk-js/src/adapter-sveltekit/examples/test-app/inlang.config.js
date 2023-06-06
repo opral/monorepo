72fc6fc6bbf0f9d7d6307b4bfe43f3832c90b940
Ivan Hofer <ivan.hofer@outlok.com>
2023-06-06 08:40:30 +0200

    remove unused dependencies

--- source-code/sdk-js/src/adapter-sveltekit/examples/test-app/inlang.config.js
+++ source-code/sdk-js/src/adapter-sveltekit/examples/test-app/inlang.config.js
@@ -6,7 +6,7 @@ export async function defineConfig(env) {
 		"https://cdn.jsdelivr.net/npm/@inlang/plugin-json@3/dist/index.js",
 	)
 	const { default: sdkPlugin } = await env.$import(
-		"https://cdn.jsdelivr.net/npm/@inlang/sdk-js-plugin@latest/dist/index.js",
+		"https://cdn.jsdelivr.net/npm/@inlang/sdk-js-plugin@0.6.4/dist/index.js",
 	)
 
 	return {
