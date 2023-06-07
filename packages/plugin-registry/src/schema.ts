78ca63a8df05d7685688b5096354f415f47d4e2e
Nils Jacobsen <nils.jacobsen98@gmail.com>
2023-06-07 16:09:47 +0200

    remove contributer handle and image

--- packages/plugin-registry/src/schema.ts
+++ packages/plugin-registry/src/schema.ts
@@ -3,6 +3,4 @@ export type InlangPluginManifest = {
 	icon: string // url to an icon that can be embedded with <img src={plugin.icon}>
 	keywords: string[] // searchable keywords
 	repository: string // url, used to embed the readme
-	contributorHandle: string // github handle of the contributor
-	contributorImage: string // url to the contributor's image can be embedded with <img src={plugin.contributorImage}>
 }
