fc2f47cbf04d4c8e7efe293be7850275f293d8f2
Nils Jacobsen <nils.jacobsen98@gmail.com>
2023-06-07 11:24:54 +0200

    change boolean to contributor or collaborator

--- packages/website/src/pages/editor/@host/@owner/@repository/State.tsx
+++ packages/website/src/pages/editor/@host/@owner/@repository/State.tsx
@@ -240,7 +240,7 @@ export function EditorStateProvider(props: { children: JSXElement }) {
 						owner: args.routeParams.owner,
 						repository: args.routeParams.repository,
 						type: response.data.private ? "Private" : "Public",
-						userPermission: userIsCollaborator(),
+						userPermission: userIsCollaborator() ? "collaborator" : "contributor",
 					})
 				})
 				.catch((error) => {
@@ -249,7 +249,7 @@ export function EditorStateProvider(props: { children: JSXElement }) {
 						repository: args.routeParams.repository,
 						type: "unknown",
 						errorDuringIsPrivateRequest: error,
-						userPermission: userIsCollaborator(),
+						userPermission: userIsCollaborator() ? "collaborator" : "contributor",
 					})
 				})
 
