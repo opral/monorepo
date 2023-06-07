f83ced9d9ffebeaf86b7dcbc05a6dfa18f4b1a73
Nils Jacobsen <nils.jacobsen98@gmail.com>
2023-06-07 16:04:52 +0200

    moved plugins in plugins folders, added only exports in index file

---
+++ packages/plugin-registry/src/registry.ts
@@ -0,0 +1,28 @@
+import type { InlangPluginManifest } from "./schema.js"
+
+export const plugins: InlangPluginManifest[] = [
+	{
+		id: "inlang-plugin-json",
+		icon: "https://github.com/samuelstroschein/inlang-plugin-json/assets/icon.png",
+		repository: "https://github.com/samuelstroschein/inlang-plugin-json",
+		keywords: ["json", "i18next", "svelte-intl-precompile"],
+		contributorHandle: "samuelstroschein",
+		contributorImage: "https://avatars.githubusercontent.com/u/35429197?v=4",
+	},
+	{
+		id: "plugin-i18next",
+		icon: "https://github.com/samuelstroschein/inlang-plugin-json/assets/icon.png",
+		repository: "https://github.com/samuelstroschein/inlang-plugin-json",
+		keywords: ["json", "i18next", "svelte-intl-precompile"],
+		contributorHandle: "nilsjacobsen",
+		contributorImage: "https://avatars.githubusercontent.com/u/35429197?v=4",
+	},
+	{
+		id: "inlang-lint",
+		icon: "https://github.com/samuelstroschein/inlang-plugin-json/assets/icon.png",
+		repository: "https://github.com/samuelstroschein/inlang-plugin-json",
+		keywords: ["json", "i18next", "svelte-intl-precompile"],
+		contributorHandle: "felixhaeberle",
+		contributorImage: "https://avatars.githubusercontent.com/u/35429197?v=4",
+	},
+]
