2dd0a6c0ad111b0b19248cf788473542bf99856a
jannesblobel <72493222+jannesblobel@users.noreply.github.com>
2023-06-08 12:51:07 +0200

    fix percentage calculation

--- source-code/badge/src/helper/missingTranslations.test.ts
+++ source-code/badge/src/helper/missingTranslations.test.ts
@@ -1,7 +1,7 @@
 import { it, expect } from "vitest"
 import { missingTranslations } from "./missingTranslations.js"
 import type * as ast from "@inlang/core/ast"
-import { createMessage, createResource } from '@inlang/core/test'
+import { createMessage, createResource } from "@inlang/core/test"
 
 it("should return 100% when no translation are missing", () => {
 	const resources: ast.Resource[] = [
@@ -16,7 +16,7 @@ it("should return 100% when no translation are missing", () => {
 it("should return 50% when half of the messages are missing", () => {
 	const resources: ast.Resource[] = [
 		createResource("en", createMessage("message-1", []), createMessage("message-2", [])),
-		createResource("de", createMessage("message-1", []))
+		createResource("de", createMessage("message-1", [])),
 	]
 	const result = missingTranslations({ resources, referenceResource: resources[0]! })
 	expect(result.percentage).toBe(50)
@@ -25,21 +25,31 @@ it("should return 50% when half of the messages are missing", () => {
 
 it("should round the percentages", () => {
 	const resources: ast.Resource[] = [
-		createResource("en", createMessage("message-1", []), createMessage("message-2", []), createMessage("message-3", [])),
-		createResource("de", createMessage("message-1", []))
+		createResource(
+			"en",
+			createMessage("message-1", []),
+			createMessage("message-2", []),
+			createMessage("message-3", []),
+		),
+		createResource("de", createMessage("message-1", [])),
 	]
 	const result = missingTranslations({ resources, referenceResource: resources[0]! })
-	expect(result.percentage).toBe(67)
+	expect(result.percentage).toBe(33)
 	expect(result.numberOfMissingTranslations).toBe(2)
 })
 
 it("should work with multiple resources", () => {
 	const resources: ast.Resource[] = [
-		createResource("en", createMessage("message-1", []), createMessage("message-2", []), createMessage("message-3", [])),
+		createResource(
+			"en",
+			createMessage("message-1", []),
+			createMessage("message-2", []),
+			createMessage("message-3", []),
+		),
 		createResource("de", createMessage("message-1", [])),
-		createResource("fr", createMessage("message-2", []))
+		createResource("fr", createMessage("message-2", [])),
 	]
 	const result = missingTranslations({ resources, referenceResource: resources[0]! })
-	expect(result.percentage).toBe(67)
+	expect(result.percentage).toBe(33)
 	expect(result.numberOfMissingTranslations).toBe(4)
 })
