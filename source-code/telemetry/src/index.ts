1b2fe06f8921f10678d271620d68c74e4ebe222d
jannesblobel <72493222+jannesblobel@users.noreply.github.com>
2023-06-08 17:21:08 +0200

    remove parseUpstream funciton ( not needed)

--- source-code/telemetry/src/index.ts
+++ source-code/telemetry/src/index.ts
@@ -1,3 +1,3 @@
 export { telemetryBrowser } from "./implementation/browser.js"
 export { telemetryNode } from "./implementation/node.js"
-export { parseOrigin, parseUpstream } from "./implementation/parseRemotes.js"
+export { parseOrigin } from "./implementation/parseRemotes.js"
