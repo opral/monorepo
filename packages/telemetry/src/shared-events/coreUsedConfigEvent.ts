ee712fc7c3eb74feeae8a1671964f39f3460d4a0
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2023-06-09 11:25:22 +0200

    add: shared event `CORE used config`

---
+++ packages/telemetry/src/shared-events/coreUsedConfigEvent.ts
@@ -0,0 +1,20 @@
+import type { InlangConfig } from "@inlang/core/config"
+
+/**
+ * Captures the config after set up.
+ *
+ * Config information provides us insights into questions like:
+ *
+ * - How many users use which plugins?
+ * - What lint rules are most commonly used?
+ * - What is the most common config?
+ *
+ * The answers to those questions helps us prioritize the roadmap and
+ * ultimately build a better product.
+ *
+ */
+export const coreUsedConfigEvent = {
+	name: "CORE used config",
+	// typesafe properties
+	properties: (config: InlangConfig) => config,
+}
