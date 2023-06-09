611b1e663514b18300a346923f38f57172b212e6
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2023-06-09 18:06:28 +0200

    refactor: simplify code and fix style issues
    - use a for loop to reduce the amount of code and potential copy paste errors
    - submit the feedback emoji directly to make insight building in posthog easier
    - fix a styling issue with padding

--- source-code/website/src/services/markdown/src/tags/Feedback.tsx
+++ source-code/website/src/services/markdown/src/tags/Feedback.tsx
@@ -1,74 +1,42 @@
 import { telemetryBrowser } from "@inlang/telemetry"
-import { createSignal } from "solid-js"
+import { For, createSignal } from "solid-js"
 // children get a default style use content for custome style
 export function Feedback() {
-	const submitFeedback = (e: any) => {
-		if (feedback() === e.target.id) {
-			setFeedback(undefined)
-		} else {
-			setFeedback(e.target.id)
-		}
+	const submitFeedback = (reaction: string) => {
 		telemetryBrowser.capture("DOCS submitted reaction", {
-			page: document.location.href,
-			reaction: e.target.id,
+			reaction,
 		})
+		setSubmittedFeedback(true)
 	}
 
-	const [feedback, setFeedback] = createSignal<"love-it" | "good" | "ok" | "bad" | undefined>()
+	// in order good to bad
+	const feedbackEmojis = ["🤩", "🙂", "😕", "😥"]
+
+	const [submittedFeedback, setSubmittedFeedback] = createSignal(false)
 
 	return (
 		<div class="flex justify-center items-center h-48">
 			<div class="flex gap-2 border border-surface-1 py-1 px-3 rounded-full ">
 				<p class="no-prose m-1 text-xs text-info">Was this helpful?</p>
-				<button
-					type="submit"
-					class={
-						"text-xl rounded hover:bg-surface-200 " +
-						(feedback() === "love-it" && " bg-surface-200 ")
-					}
-					id="love-it"
-					onClick={(e) => submitFeedback(e)}
-				>
-					🤩
-				</button>
-				<button
-					type="submit"
-					class={
-						"text-xl rounded hover:bg-surface-200 " + (feedback() === "good" && " bg-surface-200 ")
-					}
-					id="good"
-					onClick={(e) => submitFeedback(e)}
-				>
-					🙂
-				</button>
-				<button
-					type="submit"
-					class={
-						"text-xl rounded hover:bg-surface-200 " + (feedback() === "ok" && " bg-surface-200 ")
-					}
-					id="ok"
-					onClick={(e) => submitFeedback(e)}
-				>
-					😕
-				</button>
-				<button
-					type="submit"
-					class={
-						"text-xl rounded hover:bg-surface-200 " + (feedback() === "bad" && " bg-surface-200 ")
-					}
-					id="bad"
-					onClick={(e) => submitFeedback(e)}
-				>
-					😥
-				</button>
+				<For each={feedbackEmojis}>
+					{(emoji) => (
+						<button
+							type="submit"
+							class="text-xl rounded hover:bg-surface-200 px-1"
+							disabled={submittedFeedback() === true}
+							onClick={() => submitFeedback(emoji)}
+						>
+							{emoji}
+						</button>
+					)}
+				</For>
 			</div>
 			<div
-				class={
-					"text-info w-full text-center absolute pt-24 -z-10 opacity-0 " +
-					(feedback() !== undefined
-						? " opacity-100 animate-fadeInTop "
-						: " transition-opacity opacity-0")
-				}
+				class="text-info w-full text-center absolute pt-24 -z-10 opacity-0"
+				classList={{
+					"opacity-100 animate-fadeInTop": submittedFeedback(),
+					"transition-opacity opacity-0": submittedFeedback() === false,
+				}}
 			>
 				Thank you for your feedback ♥️
 			</div>
