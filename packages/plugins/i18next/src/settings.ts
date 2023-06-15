91ecc4f8782e4d8644fd8d86c696145b26fcfdab
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2023-06-15 18:25:26 +0200

    fix: type for plugin settings

--- packages/plugins/i18next/src/settings.ts
+++ packages/plugins/i18next/src/settings.ts
@@ -4,11 +4,7 @@
  * Not using zod becaue it's not worth the bundle size (2kb vs 14kb).
  */
 export function throwIfInvalidSettings(settings: PluginSettings) {
-	if (settings.pathPattern === undefined) {
-		throw new Error(
-			"The pathPattern setting must be defined and include the {language} placeholder. An example would be './resources/{language}.json'.",
-		)
-	} else if (settings.pathPattern.includes("{language}") === false) {
+	if (settings.pathPattern === undefined || settings.pathPattern.includes("{language}") === false) {
 		throw new Error(
 			"The pathPattern setting must be defined and include the {language} placeholder. An example would be './resources/{language}.json'.",
 		)
@@ -29,7 +25,13 @@ export type PluginSettings = {
 	 *  "./resources/{language}.json"
 	 */
 	pathPattern: string
-	variableReferencePattern?: [string, string]
+	/**
+	 * Defines the pattern for variable references.
+	 *
+	 * Can be either a single string ("Hello @user") or
+	 * an array of two strings ("Hello {{user}}").
+	 */
+	variableReferencePattern?: [string] | [string, string]
 	ignore?: string[]
 }
 
