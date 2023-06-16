1880022ac66357096bf350abfc2997ffae8fd49b
Nils Jacobsen <nils.jacobsen98@gmail.com>
2023-06-16 16:44:35 +0200

    fix: lint in build file

--- packages/plugins/json/build.js
+++ packages/plugins/json/build.js
@@ -5,22 +5,23 @@
  * that can be imported into an inlang project.
  */
 
-import { context } from "esbuild";
-import { pluginBuildConfig } from "@inlang/core/plugin";
+import { context } from "esbuild"
+import { pluginBuildConfig } from "@inlang/core/plugin"
 
 const options = await pluginBuildConfig({
-  entryPoints: ["./src/index.js"],
-  outfile: "./dist/index.js",
-  minify: true,
-});
+	entryPoints: ["./src/index.js"],
+	outfile: "./dist/index.js",
+	minify: false,
+})
 
-const ctx = await context(options);
+const ctx = await context(options)
 
+// eslint-disable-next-line no-undef
 if (process.env.DEV) {
-  await ctx.watch();
-  console.info("👀 watching for changes...");
+	await ctx.watch()
+	console.info("👀 watching for changes...")
 } else {
-  await ctx.rebuild();
-  console.info("✅ build complete");
-  await ctx.dispose();
+	await ctx.rebuild()
+	console.info("✅ build complete")
+	await ctx.dispose()
 }
