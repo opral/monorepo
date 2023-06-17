9b339cc644614e8fb28e993642dfe248042ebb07
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2023-06-17 18:31:59 +0200

    refactor: use i18next plugin as base

---
+++ source-code/plugins/json/src/ideExtension/config.ts
@@ -0,0 +1,26 @@
+import type { InlangConfig } from "@inlang/core/config"
+import { parse } from "./messageReferenceMatchers.js"
+
+export const ideExtensionConfig: InlangConfig["ideExtension"] = {
+	messageReferenceMatchers: [
+		async (args) => {
+			return parse(args.documentText)
+		},
+	],
+	extractMessageOptions: [
+		{
+			callback: (messageId) => `{t("${messageId}")}`,
+		},
+	],
+	documentSelectors: [
+		{
+			language: "javascript",
+		},
+		{
+			language: "typescript",
+		},
+		{
+			language: "svelte",
+		},
+	],
+}
