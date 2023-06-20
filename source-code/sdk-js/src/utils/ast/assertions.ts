f37a7be035bfcc277be2e01de49f6f189165b501
Ivan Hofer <ivan.hofer@outlok.com>
2023-06-20 15:55:48 +0200

    add link to docs in error message

--- source-code/sdk-js/src/utils/ast/assertions.ts
+++ source-code/sdk-js/src/utils/ast/assertions.ts
@@ -1,11 +1,15 @@
 import type { SourceFile } from 'ts-morph'
 import { findImportDeclarations } from './imports.js'
+import { dedent } from 'ts-dedent'
 
 // TODO: test
 export const assertNoImportsFromSdkJs = (sourceFile: SourceFile) => {
 	if (findImportDeclarations(sourceFile, "@inlang/sdk-js").length) {
 		throw Error(
-			`It is currently not supported to import something from '@inlang/sdk-js' in this file.`,
+			dedent`
+				It is currently not supported to import something from '@inlang/sdk-js' in this file.
+				Please read the docs for more information on how to workaround this temporary limitation: https://inlang.com/documentation/sdk/sveltekit-advanced
+			`,
 		)
 	}
 }
