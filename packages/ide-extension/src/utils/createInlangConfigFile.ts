3ebef0ecb04408f007d565259a594d7aad18500a
Felix Haeberle <kontakt@felixhaeberle.de>
2023-06-26 13:11:01 +0200

    add more functionality

--- packages/ide-extension/src/utils/createInlangConfigFile.ts
+++ packages/ide-extension/src/utils/createInlangConfigFile.ts
@@ -1,3 +1,12 @@
+import { getConfigContent } from "@inlang/cli/src/utilities/getConfigContent"
+import { getLanguageFolderPath } from "@inlang/cli/src/utilities/getLanguageFolderPath"
+import {
+	getSupportedLibrary,
+	SupportedLibrary,
+} from "@inlang/cli/src/utilities/getSupportedLibrary"
+import { italic } from "@inlang/cli/src/utlilities/format"
+import { vscodeFs } from "@inlang/cli/src/utlilities/fs"
+import path from "node:path"
 import * as vscode from "vscode"
 import { getGitOrigin, telemetry } from "../services/telemetry/implementation.js"
 
@@ -9,11 +18,7 @@ export const disableConfigFileCreation = async (): Promise<boolean> => {
 	return _recommendation.includes(gitOrigin)
 }
 
-export const createInlangConfigFile = async ({
-	workspaceFolder,
-}: {
-	workspaceFolder: vscode.WorkspaceFolder
-}) => {
+export const createInlangConfigFile = async (args: { workspaceFolder: vscode.WorkspaceFolder }) => {
 	// check if config file already exists
 	const configFiles = await vscode.workspace.findFiles("inlang.config.js")
 	if (configFiles.length > 0) return
@@ -22,7 +27,51 @@ export const createInlangConfigFile = async ({
 	if (await disableConfigFileCreation()) return
 
 	// check if the repository has either i18next or typesafe-i18n installed in dependencies or devDependencies
-	// TODO
+	let plugin: SupportedLibrary = "json"
+	if (await vscode.workspace.findFiles("package.json")) {
+		// read package.json with vscode api
+		const packageJson = JSON.parse(
+			(
+				await vscode.workspace.fs.readFile(
+					vscode.Uri.joinPath(args.workspaceFolder.uri, "package.json"),
+				)
+			).toString(),
+		)
+		// Check if popular internationalization libraries are dependencies
+		plugin = getSupportedLibrary({ packageJson })
+
+		// Plugin specific logs
+		if (plugin === "@inlang/sdk-js") {
+			console.warn(
+				"📦 Using plugin: @inlang/sdk-js. You have to add a plugin which reads and writes resources e.g. the inlang-plugin-json. See: https://inlang.com/documentation/plugins/registry",
+			)
+		}
+	} else {
+		console.warn("📦 No package.json found in this directory. Using fallback plugin: json")
+		// Fallback, remove this someday
+		plugin = "json"
+	}
+
+	// Generate the config file content
+	const languageFolderPath = await getLanguageFolderPath(
+		args.workspaceFolder.uri.toString(),
+		vscodeFs,
+	)
+	const pathPatternRaw = languageFolderPath ? path.join(languageFolderPath, "{language}.json") : ""
+
+	// Windows: Replace backward slashes with forward slashes
+	const pathPattern = pathPatternRaw.replace(/\\/g, "/")
+
+	if (pathPattern === "") {
+		console.warn(
+			"Could not find a language folder in the project. You have to enter the path to your language files (pathPattern) manually.",
+		)
+	} else {
+		console.info(`🗂️  Found language folder path: ${italic(pathPattern)}`)
+		console.info(
+			`🗂️  Please adjust the ${`pathPattern`} in the inlang.config.js manually if it is not parsed correctly.`,
+		)
+	}
 
 	// Prompt the user to create a config file with the message like Improve your i18n experience with Inlang. Do you want to create a config file?
 	const createConfigFile = await vscode.window.showInformationMessage(
@@ -32,7 +81,15 @@ export const createInlangConfigFile = async ({
 	)
 
 	if (createConfigFile === "Accept") {
-		// TODO: create config file
+		const configContent = await getConfigContent({ plugin, pathPattern })
+		const configFilePath = vscode.Uri.joinPath(args.workspaceFolder.uri, "inlang.config.js")
+		try {
+			await vscode.workspace.fs.writeFile(configFilePath, Buffer.from(configContent))
+			console.info(`🎉 Created inlang.config.js file at ${italic(configFilePath.fsPath)}`)
+		} catch (error) {
+			console.error(`📄 Could not create inlang.config.js file at ${italic(configFilePath.fsPath)}`)
+			console.error(error)
+		}
 	} else {
 		// add git origin to disableConfigCreation setting
 		const gitOrigin = await getGitOrigin()
