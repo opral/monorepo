f20ed68eb7f7daea96dc23ddca0098ffd4685850
araknast <araknast@protonmail.com>
2023-06-26 12:23:39 -0700

    objectStoreFs: export `objectStoreFs` type

--- source-code-git/client/src/object-store-apis/index.ts
+++ source-code-git/client/src/object-store-apis/index.ts
@@ -1,1 +1,2 @@
 export { createObjectStoreFs } from "./objectStoreFs.js"
+export type { ObjectStoreFilesystem } from "./types.js"
---
+++ source-code-git/client/src/object-store-apis/types.ts
@@ -0,0 +1,6 @@
+export type FileData = string | Uint8Array
+export type ObjectStoreFilesystem = {
+	writeFile: (path: string, data: string | Uint8Array, options?: { mode: number }) => Promise<void>
+	readFile: (path: string, options?: { encoding: "utf-8" | "binary" }) => Promise<FileData>
+	readdir: (path: string) => Promise<string[]>
+}
