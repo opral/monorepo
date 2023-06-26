6d6c1a13abd21268593a040b29c20060870b295a
Nils Jacobsen <nils.jacobsen98@gmail.com>
2023-06-26 14:14:43 +0200

    feat: improve overview sdk page in docs

--- source-code/website/src/services/markdown/src/tags/QuickLinks.tsx
+++ source-code/website/src/services/markdown/src/tags/QuickLinks.tsx
@@ -1,5 +1,11 @@
 import { Icon, type AvailableIcon } from "@src/components/Icon.jsx"
-import type { JSXElement } from "solid-js"
+import Nextjs from "@src/pages/index/sections/02-integration/assets/nextjs.jsx"
+import Svelte from "@src/pages/index/sections/02-integration/assets/svelte.jsx"
+import Vuejs from "@src/pages/index/sections/02-integration/assets/vuejs.jsx"
+import Reactjs from "@src/pages/index/sections/02-integration/assets/reactjs.jsx"
+import { JSXElement, Show } from "solid-js"
+
+type BrandLogo = "next" | "vue" | "react" | "svelte"
 
 export function QuickLinks(props: { children: JSXElement[] }) {
 	return (
@@ -7,7 +13,8 @@ export function QuickLinks(props: { children: JSXElement[] }) {
 			class={
 				"not-prose my-12 grid grid-cols-1 gap-6 " +
 				((props.children as JSXElement[]).length === 2 && " sm:grid-cols-2 ") +
-				((props.children as JSXElement[]).length === 3 && " sm:grid-cols-3 ")
+				((props.children as JSXElement[]).length === 3 && " sm:grid-cols-3 ") +
+				((props.children as JSXElement[]).length === 4 && " sm:grid-cols-4 ")
 			}
 		>
 			{props.children}
@@ -19,13 +26,30 @@ export function QuickLink(props: {
 	title: string
 	description: string
 	href: string
-	icon: AvailableIcon
+	icon?: AvailableIcon
+	logo?: BrandLogo
 }) {
 	return (
 		<div class="group relative rounded-xl bg-surface-100 hover:bg-surface-200  hover:transition-colors duration-200">
 			<div class="relative overflow-hidden rounded-lg p-6">
 				<div class="bg-hover-info/10 w-fit p-2 rounded-lg">
-					<Icon name={props.icon} class="h-6 w-6 text-info" />
+					<Show when={props.icon}>
+						<Icon name={props.icon!} class="h-6 w-6 text-info" />
+					</Show>
+					<Show when={props.logo}>
+						<Show when={props.logo === "svelte"}>
+							<Svelte size={28} startColor="#434343" endColor="#434343" />
+						</Show>
+						<Show when={props.logo === "next"}>
+							<Nextjs size={28} startColor="#434343" endColor="#434343" />
+						</Show>
+						<Show when={props.logo === "vue"}>
+							<Vuejs size={28} startColor="#434343" endColor="#434343" />
+						</Show>
+						<Show when={props.logo === "react"}>
+							<Reactjs size={28} startColor="#434343" endColor="#434343" />
+						</Show>
+					</Show>
 				</div>
 
 				<h2 class="mt-4 text-base font-medium text-active-info">
@@ -34,7 +58,9 @@ export function QuickLink(props: {
 						{props.title}
 					</a>
 				</h2>
-				<p class="mt-1 text-sm">{props.description}</p>
+				<Show when={props.description}>
+					<p class="mt-1 text-sm">{props.description}</p>
+				</Show>
 			</div>
 		</div>
 	)
