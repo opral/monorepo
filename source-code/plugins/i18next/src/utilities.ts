aa438d267d67e38aef41f75e661c343589cf5798
Nils Jacobsen <nils.jacobsen98@gmail.com>
2023-06-28 14:53:25 +0200

    fix: replace replaceAll with regex function for older browsers

--- source-code/plugins/i18next/src/utilities.ts
+++ source-code/plugins/i18next/src/utilities.ts
@@ -44,3 +44,17 @@ export const detectJsonSpacing = (jsonString: string) => {
 	// No matching spacing configuration found
 	return undefined
 }
+
+/**
+ * replaceAll for old Browsers
+ *
+ * @example replaceAll("abs def abc", "abc", "o")
+ */
+
+export function replaceAll(str: string, find: string, replace: string) {
+	return str.replace(new RegExp(escapeRegExp(find), "g"), replace)
+}
+
+function escapeRegExp(string: string) {
+	return string.replace(/[.*+?^${}()|[\]\\]/g, "\\$&") // $& means the whole matched string
+}
