069876f2b885ab71d8a39204d3c45af60eac091b
Felix Haeberle <kontakt@felixhaeberle.de>
2023-07-04 16:02:21 +0200

    starters: update inlang.config.js

--- packages/starters/inlang-nextjs/inlang.config.js
+++ packages/starters/inlang-nextjs/inlang.config.js
@@ -3,7 +3,7 @@
  */
 export async function defineConfig(env) {
 	const { default: i18nextPlugin } = await env.$import(
-		"https://cdn.jsdelivr.net/gh/samuelstroschein/inlang-plugin-json@2/dist/index.js",
+		"https://cdn.jsdelivr.net/npm/@inlang/plugin-i18next@3/dist/index.js",
 	)
 
 	const { default: standardLintRules } = await env.$import(
@@ -14,7 +14,12 @@ export async function defineConfig(env) {
 		referenceLanguage: "en",
 		plugins: [
 			i18nextPlugin({
-				pathPattern: "./app/i18n/locales/{language}/*.json",
+				pathPattern: {
+					"client-page": "app/i18n/locales/{language}/client-page.json",
+					footer: "app/i18n/locales/{language}/footer.json",
+					"second-page": "app/i18n/locales/{language}/second-page.json",
+					translation: "app/i18n/locales/{language}/translation.json",
+				},
 			}),
 			standardLintRules(),
 		],
