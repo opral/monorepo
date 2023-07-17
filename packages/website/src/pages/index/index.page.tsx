f9597ca178fec5e989514d86aa8cd9ba9e7803c4
flooorianflo <florian.kiem@hfg.design>
2023-07-17 17:41:48 +0200

    feat: Product Hunt Badge

--- packages/website/src/pages/index/index.page.tsx
+++ packages/website/src/pages/index/index.page.tsx
@@ -25,6 +25,23 @@ export function Page() {
 					<ConfigPage />
 					<Pricing />
 					<GetStarted />
+
+					{/* Temporary Product Hunt Badge –– Will be removed after launch */}
+					<div class="max-w-screen-xl px-10 w-full sticky bottom-0 flex justify-end h-0 -translate-y-20 z-50 mx-auto pointer-events-none">
+						<a
+							href="https://www.producthunt.com/posts/inlang-vscode-extension-for-i18n-beta?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-inlang&#0045;vscode&#0045;extension&#0045;for&#0045;i18n&#0045;beta"
+							target="_blank"
+							class="pointer-events-auto"
+						>
+							<img
+								src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=404697&theme=neutral"
+								alt="inlang&#0032;VSCode&#0032;extension&#0032;for&#0032;i18n&#0032;&#0040;beta&#0041; - Extension&#0032;for&#0032;i18n&#0032;codebases&#0032;making&#0032;developers&#0032;life&#0039;s&#0032;easier | Product Hunt"
+								style={{ width: "250px", height: "54px" }}
+								width="250"
+								height="54"
+							/>
+						</a>
+					</div>
 				</div>
 			</RootLayout>
 		</>
