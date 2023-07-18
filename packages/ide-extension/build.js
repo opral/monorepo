ea1450129daad1d56fd09ddb7bf5d1c0c17bfd9c
Robin Bühler <git+r@obin.ch>
2023-07-18 15:38:32 +0000

    chore: set up vscode-test

--- packages/ide-extension/build.js
+++ packages/ide-extension/build.js
@@ -9,15 +9,24 @@ import { context } from "esbuild"
 
 // eslint-disable-next-line no-undef
 const isDev = process?.env?.DEV !== undefined
+// eslint-disable-next-line no-undef
+const isTest = process?.env?.TEST !== undefined
+
+const entryPoints = [{ in: "./src/main.ts", out: "./main" }]
+if (isTest) {
+	entryPoints.push({ in: "./test/test.ts", out: "./test" });
+	entryPoints.push({ in: "./test/suite.ts", out: "./suite" });
+}
 
 const ctx = await context({
-	entryPoints: ["./src/main.ts"],
-	outfile: "./dist/main.cjs",
+	entryPoints,
+	outdir: "./dist/",
+	outExtension: { '.js': '.cjs' },
 	bundle: true,
-	minify: isDev === false,
+	minify: !isDev && !isTest,
 	platform: "node",
-	sourcemap: isDev,
-	external: ["vscode"],
+	sourcemap: isDev || isTest,
+	external: ["vscode", "@vitest/ui", "@vitest/browser", "webdriverio", "safaridriver", "playwright"],
 })
 
 if (isDev) {
