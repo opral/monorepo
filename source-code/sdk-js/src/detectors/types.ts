e533a1caacccf54ff9bd6a50768f4ba1402ff33f
Ivan Hofer <ivan.hofer@outlook.com>
2023-07-19 08:54:58 +0200

    use [] instead of Array<>

--- source-code/sdk-js/src/detectors/types.ts
+++ source-code/sdk-js/src/detectors/types.ts
@@ -2,10 +2,10 @@ import type { BCP47LanguageTag } from '@inlang/core/languageTag'
 
 type MaybePromise<T> = T | Promise<T>
 
-export type Detector<Parameters extends Array<unknown> = Array<never>> = (
+export type Detector<Parameters extends unknown[] = never[]> = (
 	...parameters: Parameters
 ) => MaybePromise<BCP47LanguageTag[]>
 
-export type DetectorInitializer<Parameters extends Array<unknown> = Array<never>> = (
+export type DetectorInitializer<Parameters extends unknown[] = never[]> = (
 	...parameters: Parameters
 ) => Detector<Parameters>
