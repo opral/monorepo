1782dc416e5265c3d6b7667fc0a4507fbef6ccf4
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2023-07-21 13:44:42 +0200

    fix wrong merge. re-apply #1152

--- packages/core2/src/app/createInlang.ts
+++ packages/core2/src/app/createInlang.ts
@@ -1,10 +1,6 @@
-import type { InlangConfig } from "../config/schema.js"
 import type { InlangInstance } from "./api.js"
 import type { InlangInstanceEnv } from "./env/types.js"
 
-export function createInlang(args: {
-	config: InlangConfig
-	env: InlangInstanceEnv
-}): InlangInstance {
+export function createInlang(args: { configPath: string; env: InlangInstanceEnv }): InlangInstance {
 	return {} as any
 }
