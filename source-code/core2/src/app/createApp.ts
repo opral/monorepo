b73ad2357e056e776f39dc75f556222ae69248d1
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2023-07-21 12:16:12 +0200

    rename app to instance

--- source-code/core2/src/app/createApp.ts
+++
@@ -1,7 +0,0 @@
-import type { InlangConfig } from "../config/schema.js"
-import type { InlangApp } from "./api.js"
-import type { InlangAppEnvironment } from "./env/types.js"
-
-export function createApp(args: { config: InlangConfig; env: InlangAppEnvironment }): InlangApp {
-	return {} as any
-}
