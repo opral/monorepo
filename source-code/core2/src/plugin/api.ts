207db0d10e419ff11ebe2cc20489797f31410299
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2023-07-24 00:21:59 +0200

    improve: make meta json serializable

--- source-code/core2/src/plugin/api.ts
+++ source-code/core2/src/plugin/api.ts
@@ -3,24 +3,30 @@ import type { LanguageTag } from "../languageTag.js"
 import type { LintRule } from "../lint/api.js"
 import type { Message } from "../messages/schema.js"
 
+type JSONSerializable<T extends Record<string, string | string[]>> = T
+
 export type Plugin<
 	PluginOptions extends Record<string, string | string[]> = Record<string, never>,
 > = {
-	id: `${string}.${string}`
-	displayName: string
-	// TODO make translatable after https://github.com/inlang/inlang/pull/1155
-	description: string
-	keywords: string[]
+	// * Must be JSON serializable if we want an external plugin manifest in the future.
+	meta: JSONSerializable<{
+		id: `${string}.${string}`
+		displayName: string
+		// TODO make translatable after https://github.com/inlang/inlang/pull/1155
+		description: string
+		keywords: string[]
+	}>
 	/**
 	 * The setup function is the first function that is called when inlang loads the plugin.
 	 *
 	 * Use the setup function to initialize state, handle the options and more.
 	 */
-	setup: (args: { options: PluginOptions; inlang: InlangInstance }) => Promise<void> | void
-	extendLanguageTags?: () => LanguageTag[]
-	loadMessages?: () => Message[]
-	saveMessages?: (args: { messages: Message[] }) => void
-	addLintRules?: () => LintRule[]
+	setup: (args: { options: PluginOptions; inlang: InlangInstance }) => {
+		extendLanguageTags?: () => LanguageTag[]
+		loadMessages?: () => Message[]
+		saveMessages?: (args: { messages: Message[] }) => void
+		addLintRules?: () => LintRule[]
+	}
 }
 
 // ----------------- EXAMPLE PLUGIN -----------------
@@ -29,32 +35,30 @@ type PluginOptions = {
 	pathPattern: string
 }
 
-// this state is module scoped and can be used in all plugin functions
-let options: PluginOptions
-let inlang: InlangInstance
-
 export const examplePlugin: Plugin<PluginOptions> = {
-	id: "inlang.plugin-i18next",
-	displayName: "i18next",
-	description: "i18next plugin for inlang",
-	keywords: ["i18next", "react", "nextjs"],
-	setup: (args) => {
-		options = args.options
-		inlang = args.inlang
+	meta: {
+		id: "inlang.plugin-i18next",
+		displayName: "i18next",
+		description: "i18next plugin for inlang",
+		keywords: ["i18next", "react", "nextjs"],
+	},
+	setup: ({ options, inlang }) => {
 		if (options.pathPattern === undefined) {
 			throw Error("Path pattern is undefined")
 		}
-	},
-	extendLanguageTags: () => {
-		return ["en-US"]
-	},
-	loadMessages: () => {
-		for (const languageTag of inlang.config.languageTags) {
-			console.log(languageTag + options.pathPattern)
+		return {
+			extendLanguageTags: () => {
+				return ["en-US"]
+			},
+			loadMessages: () => {
+				for (const languageTag of inlang.config.languageTags) {
+					console.log(languageTag + options.pathPattern)
+				}
+				return []
+			},
+			saveMessages: ({ messages }) => {
+				console.log(messages)
+			},
 		}
-		return []
-	},
-	saveMessages: ({ messages }) => {
-		console.log(messages)
 	},
 }
