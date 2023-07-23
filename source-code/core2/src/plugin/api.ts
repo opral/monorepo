3bbe4fd0320133226849acde1109aae4a262c94f
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2023-07-23 17:50:53 +0200

    refactor: static access to plugin functions

--- source-code/core2/src/plugin/api.ts
+++ source-code/core2/src/plugin/api.ts
@@ -3,71 +3,72 @@ import type { LanguageTag } from "../languageTag.js"
 import type { LintRule } from "../lint/api.js"
 import type { Message } from "../messages/schema.js"
 
-/**
- * Proposal 1
- *
- * Setup function everything in one.
- *
- * + simple to use
- * - no access to static props.
- *   if a plugin crashes during setup -> complicated to show plugin with "id" crashed.
- */
-export type Plugin_Proposal_1<Options extends Record<string, string> = Record<string, never>> =
-	(args: { options: Options; inlang: InlangInstance }) => {
-		id: `${string}.${string}`
-		displayName: string
-		extendLanguageTags?: () => LanguageTag[]
-		loadMessages?: () => Message[]
-		saveMessages?: (args: { messages: Message[] }) => void
-		addLintRules?: () => LintRule[]
-	}
-
-export const example1: Plugin_Proposal_1<{ pathPattern: string }> = ({ options, inlang }) => {
-	return {
-		id: "inlang.myPlugin",
-		displayName: "My Plugin",
-		extendLanguageTags: () => {
-			return ["en-US"]
-		},
-		loadMessages: () => {
-			for (const languageTag of inlang.config.languageTags) {
-				console.log(languageTag + options.pathPattern)
-			}
-			return []
-		},
-		saveMessages: ({ messages }) => {
-			console.log(messages)
-		},
-	}
+export type Plugin<
+	PluginOptions extends Record<string, string | string[]> = Record<string, never>,
+> = {
+	id: `${string}.${string}`
+	displayName: string
+	// TODO make translatable after https://github.com/inlang/inlang/pull/1155
+	description: string
+	keywords: string[]
+	/**
+	 * The setup function is the first function that is called when inlang loads the plugin.
+	 *
+	 * Use the setup function to initialize state, handle the options and more.
+	 */
+	setup: (args: { options: PluginOptions; inlang: InlangInstance }) => Promise<void> | void
+	extendLanguageTags?: () => LanguageTag[]
+	loadMessages?: () => Message[]
+	saveMessages?: (args: { messages: Message[] }) => void
+	addLintRules?: () => LintRule[]
 }
 
-/**
- * Proposal 2
- *
- * Split static meta data from setup function.
- *
- * + access to static without crash chance
- * - arguably not so nice API.
- */
-export type Plugin_Proposal_2<Options extends Record<string, string> = Record<string, never>> = {
-	meta: {
-		id: `${string}.${string}`
-		displayName: string
-	}
-	setup: (args: { options: Options; inlang: InlangInstance }) => {
-		extendLanguageTags?: () => LanguageTag[]
-		loadMessages?: () => Message[]
-		saveMessages?: (args: { messages: Message[] }) => void
-		addLintRules?: () => LintRule[]
-	}
+// ----------------- EXAMPLE PLUGIN -----------------
+
+type PluginOptions = {
+	pathPattern: string
 }
 
-export const example2: Plugin_Proposal_2<{ pathPattern: string }> = {
-	meta: {
-		id: "inlang.myPlugin",
-		displayName: "My Plugin",
+// this state is module scoped and can be used in all plugin functions
+let options: PluginOptions
+let inlang: InlangInstance
+
+export const examplePlugin: Plugin<PluginOptions> = {
+	id: "inlang.plugin-i18next",
+	displayName: "i18next",
+	description: "i18next plugin for inlang",
+	keywords: ["i18next", "react", "nextjs"],
+	setup: (args) => {
+		options = args.options
+		inlang = args.inlang
+		if (options.pathPattern === undefined) {
+			throw Error("Path pattern is undefined")
+		}
 	},
-	setup: ({ inlang, options }) => {
+	extendLanguageTags: () => {
+		return ["en-US"]
+	},
+	loadMessages: () => {
+		for (const languageTag of inlang.config.languageTags) {
+			console.log(languageTag + options.pathPattern)
+		}
+		return []
+	},
+	saveMessages: ({ messages }) => {
+		console.log(messages)
+	},
+}
+
+export const examplePlugin2NotWorking: Plugin<PluginOptions> = {
+	id: "inlang.plugin-i18next",
+	displayName: "i18next",
+	description: "i18next plugin for inlang",
+	keywords: ["i18next", "react", "nextjs"],
+	setup: ({ options, inlang }) => {
+		//! The following line will crash any static access to plugin functions
+		if (options.pathPattern === undefined) {
+			throw Error("Path pattern is undefined")
+		}
 		return {
 			extendLanguageTags: () => {
 				return ["en-US"]
