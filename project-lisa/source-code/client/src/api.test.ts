c0d3f930dcbd146d7afa464b553df57a3c4b66f9
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2023-07-24 13:31:58 +0200

    .

--- project-lisa/source-code/client/src/api.test.ts
+++ project-lisa/source-code/client/src/api.test.ts
@@ -16,14 +16,14 @@ const localRepository = await load("/home/foo/bar.git", { fs: nodeFs })
 // -- loading a remote repository is possible
 //    - uses lisa.dev which acts as a proxy to github.com. Legacy git hosts don't support
 //      all features we need like lazy fetching, auth, etc.
-const repository = await load("https://fis.dev/github.com/foo/bar.git", {
+const repository = await load("https://lisa.dev/github.com/foo/bar.git", {
 	fs: memoryFs,
 	// todo auth is not yet implemented
 	auth: { username: "foo", password: "bar" },
 })
 
 // -- file is lazy fetched upon first access
-let file = await repository.fs.readFile("foo.txt")
+let file = await repository.fs.readFile("/foo.txt")
 
 // modifying the file
 file += "bar"
