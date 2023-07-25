28dae39cdcff1e64e97b32592f1cd0c9ffa1e07c
flooorianflo <florian.kiem@hfg.design>
2023-07-25 11:08:21 +0200

    fix: check loading of all imgs

--- source-code/website/src/pages/documentation/index.page.tsx
+++ source-code/website/src/pages/documentation/index.page.tsx
@@ -1,4 +1,4 @@
-import { For, Show, createRenderEffect, createSignal, createEffect, onMount } from "solid-js"
+import { For, Show, createRenderEffect, createSignal, onMount } from "solid-js"
 import { Layout as RootLayout } from "@src/pages/Layout.jsx"
 import { Markdown, parseMarkdown } from "@src/services/markdown/index.js"
 import type { ProcessedTableOfContents } from "./index.page.server.jsx"
@@ -158,16 +158,15 @@ function NavbarCommon(props: {
 			const targetElement = document.getElementById(
 				currentPageContext.urlParsed.hash.replace("#", ""),
 			)
-			if (targetElement) {
-				requestAnimationFrame(() => {
-					const elementRect = targetElement.getBoundingClientRect()
-					const offsetPosition = elementRect.top - 96 // The offset because of the fixed navbar
 
-					window.scrollBy({
-						top: offsetPosition,
-					})
+			checkLoadedImgs(() => {
+				const elementRect = targetElement!.getBoundingClientRect()
+				const offsetPosition = elementRect.top - 96 // The offset because of the fixed navbar
+
+				window.scrollBy({
+					top: offsetPosition,
 				})
-			}
+			})
 		}
 	})
 
@@ -246,4 +245,30 @@ function NavbarCommon(props: {
 			</For>
 		</ul>
 	)
+}
+
+function checkLoadedImgs(anchorScroll: () => void) {
+	let imgElementsLoaded = 0
+	const imgElements = document.querySelectorAll("img")
+	const imgElementsLength = imgElements.length
+
+	if (imgElementsLength === 0) {
+		anchorScroll()
+	} else {
+		for (const img of imgElements) {
+			if (img.complete) {
+				imgElementsLoaded++
+				if (imgElementsLoaded === imgElementsLength) {
+					anchorScroll()
+				}
+			} else {
+				img.addEventListener("load", () => {
+					imgElementsLoaded++
+					if (imgElementsLoaded === imgElementsLength) {
+						anchorScroll()
+					}
+				})
+			}
+		}
+	}
 }
