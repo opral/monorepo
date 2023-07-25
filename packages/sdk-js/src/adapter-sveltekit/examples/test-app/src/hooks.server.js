51be7f9d03421aa905806762049a91699e847c1b
Ivan Hofer <ivan.hofer@outlook.com>
2023-07-25 09:35:13 +0200

    allow to debug the applied transformations of a single file

--- packages/sdk-js/src/adapter-sveltekit/examples/test-app/src/hooks.server.js
+++ packages/sdk-js/src/adapter-sveltekit/examples/test-app/src/hooks.server.js
@@ -2,3 +2,4 @@
 It is needed in order to circumvent a current limitation of SvelteKit. See https://github.com/inlang/inlang/issues/647
 You can remove this comment and modify the file as you like. We just need to make sure it exists.
 Please do not delete it (inlang will recreate it if needed). */
+import "@inlang/sdk-js/debug"
