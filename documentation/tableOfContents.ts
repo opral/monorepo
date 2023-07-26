2c7772c24734d7ec898bb97af97a9f22d2340b12
flooorianflo <florian.kiem@hfg.design>
2023-07-26 15:06:09 +0200

    fix: tableOfContents not found

--- documentation/tableOfContents.ts
+++ documentation/tableOfContents.ts
@@ -1,5 +1,3 @@
-// Separate file: ../../../../../documentation/tableOfContents.js
-
 import { RequiredFrontmatter } from "@inlang/website/markdown"
 
 /**
@@ -7,63 +5,81 @@ import { RequiredFrontmatter } from "@inlang/website/markdown"
  */
 export const FrontmatterSchema = RequiredFrontmatter
 
-export const fileSources = {
-	"Getting started": [
-		"./getting-started/introduction.md?raw",
-		"./getting-started/quick-start.md?raw",
-	],
-	Apps: ["./apps/editor.md?raw", "./apps/ide-extension.md?raw", "../source-code/cli/README.md?raw"],
-	Plugins: ["./plugins/registry.md?raw", "./plugins/custom-plugins.md?raw"],
-	SDK: [
-		"./sdk/general.md?raw",
-		"./sdk/usage.md?raw",
-		"./sdk/configuration.md?raw",
-		"./sdk/sveltekit/overview.md?raw",
-		"./sdk/sveltekit/advanced.md?raw",
-		// "./sdk/custom.md?raw"
-	],
-	Guide: [
-		"./badge.md?raw",
-		// "./ci-cd.md?raw"
-		"./build-on-inlang.md?raw",
-	],
-	Core: [
-		"./ast.md?raw",
-		"../source-code/core/src/environment/README.md?raw",
-		"./query.md?raw",
-		"../source-code/core/src/lint/README.md?raw",
-	],
-	Community: ["../CONTRIBUTING.md?raw", "./code-organization.md?raw"],
-}
-
-interface TableOfContentsItem {
-	category: string
-	content: string
-}
-
 /**
- * The table of contents as an array of objects.
+ * The table of contents split by categories.
  */
-export async function buildTableOfContents(): Promise<TableOfContentsItem[]> {
-	const tableOfContents: TableOfContentsItem[] = []
-
-	for (const category in fileSources) {
-		const contents = await Promise.all(
-			fileSources[category].map(async (file: any) => {
-				const module = await import(
-					/* @vite-ignore */
-					file
-				)
-				return module.default
-			}),
-		)
-
-		for (const content of contents) {
-			tableOfContents.push({ category, content })
-		}
-	}
-
-	return tableOfContents
+export const tableOfContents: Record<string, { raw: string[]; import: string[] }> = {
+	"Getting started": {
+		raw: ["./getting-started/introduction.md?raw", "./getting-started/quick-start.md?raw"],
+		import: [
+			(await import("./getting-started/introduction.md?raw")).default,
+			(await import("./getting-started/quick-start.md?raw")).default,
+		],
+	},
+	Apps: {
+		raw: [
+			"./apps/editor.md?raw",
+			"./apps/ide-extension.md?raw",
+			"../source-code/cli/README.md?raw",
+		],
+		import: [
+			(await import("./apps/editor.md?raw")).default,
+			(await import("./apps/ide-extension.md?raw")).default,
+			(await import("../source-code/cli/README.md?raw")).default,
+		],
+	},
+	Plugins: {
+		raw: ["./plugins/registry.md?raw", "./plugins/custom-plugins.md?raw"],
+		import: [
+			(await import("./plugins/registry.md?raw")).default,
+			(await import("./plugins/custom-plugins.md?raw")).default,
+		],
+	},
+	SDK: {
+		raw: [
+			"./sdk/general.md?raw",
+			"./sdk/usage.md?raw",
+			"./sdk/configuration.md?raw",
+			"./sdk/sveltekit/overview.md?raw",
+			"./sdk/sveltekit/advanced.md?raw",
+			// "./sdk/custom.md?raw",
+		],
+		import: [
+			(await import("./sdk/general.md?raw")).default,
+			(await import("./sdk/usage.md?raw")).default,
+			(await import("./sdk/configuration.md?raw")).default,
+			(await import("./sdk/sveltekit/overview.md?raw")).default,
+			(await import("./sdk/sveltekit/advanced.md?raw")).default,
+			// (await import("./sdk/custom.md?raw")).default,
+		],
+	},
+	Guide: {
+		raw: ["./badge.md?raw", /* "./ci-cd.md?raw", */ "./build-on-inlang.md?raw"],
+		import: [
+			(await import("./badge.md?raw")).default,
+			// (await import("./ci-cd.md?raw")).default,
+			(await import("./build-on-inlang.md?raw")).default,
+		],
+	},
+	Core: {
+		raw: [
+			"./ast.md?raw",
+			"../source-code/core/src/environment/README.md?raw",
+			"./query.md?raw",
+			"../source-code/core/src/lint/README.md?raw",
+		],
+		import: [
+			(await import("./ast.md?raw")).default,
+			(await import("../source-code/core/src/environment/README.md?raw")).default,
+			(await import("./query.md?raw")).default,
+			(await import("../source-code/core/src/lint/README.md?raw")).default,
+		],
+	},
+	Community: {
+		raw: ["../CONTRIBUTING.md?raw", "./code-organization.md?raw"],
+		import: [
+			(await import("../CONTRIBUTING.md?raw")).default,
+			(await import("./code-organization.md?raw")).default,
+		],
+	},
 }
-
-export const tableOfContentsPromise = buildTableOfContents()
