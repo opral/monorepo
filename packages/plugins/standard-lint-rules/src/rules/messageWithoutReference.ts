7a38ca4d671e540de41d9579925518874a678578
Ivan Hofer <ivan.hofer@outlook.com>
2023-07-27 16:27:50 +0200

    migrate messageWithoutSource lint rule

--- packages/plugins/standard-lint-rules/src/rules/messageWithoutReference.ts
+++ packages/plugins/standard-lint-rules/src/rules/messageWithoutReference.ts
@@ -1,31 +1,27 @@
-import { createLintRule } from "@inlang/core/lint"
+import type { MessageLintRule } from '@inlang/lint-api'
 
 /**
  * Checks for likely outdated messages.
  *
- * A message with a missing reference is usually an indication that
- * the message (id) is no longer used in source code, but resources
+ * A message with a missing source is usually an indication that
+ * the message (id) is no longer used in source code, but messages
  * have not been updated accordingly.
  */
-export const messageWithoutReference = createLintRule({
-	id: "inlang.messageWithoutReference",
-	setup: ({ config, report }) => {
-		return {
-			visitors: {
-				Resource: ({ target }) => {
-					if (target && target.languageTag.name === config.sourceLanguageTag) {
-						return "skip"
-					}
-				},
-				Message: ({ target, reference }) => {
-					if (!reference && target) {
-						report({
-							node: target,
-							message: `Message with id '${target.id.name}' is specified, but missing in the reference.`,
-						})
-					}
-				},
-			},
+export const messageWithoutSource = () => ({
+	id: "inlang.messageWithoutSource",
+	displayName: {
+		en: 'Message Without Source'
+	},
+	defaultLevel: 'warn', // TODO: how to override level?
+	message: ({ message: { id, body }, config, report }) => {
+		if (Object.keys(body).length && !body[config.sourceLanguageTag]) {
+			report({
+				messageId: id,
+				languageTag: config.sourceLanguageTag,
+				body: {
+					en: `Message with id '${id}' is specified, but missing in the source.`, // TODO: simplify message as information is redundant
+				}
+			})
 		}
 	},
-})
+}) satisfies MessageLintRule
