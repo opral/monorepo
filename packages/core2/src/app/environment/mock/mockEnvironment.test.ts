20b9f55b8ac021e195a26935decca47d4c0ce7c3
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2023-07-27 08:47:22 +0200

    re-add mock environment

---
+++ packages/core2/src/app/environment/mock/mockEnvironment.test.ts
@@ -0,0 +1,77 @@
+import { expect, it } from "vitest"
+import { createMemoryFs, fromJson } from "@inlang-git/fs"
+import { createMockEnvironment } from "./mockEnvironment.js"
+import type { InlangInstanceEnvironment } from "../api.js"
+
+it("should copy a directory into the environment", async () => {
+	// to test with node (a real filesystem), outcomment this line and
+	// import fs from "node:fs/promises" above.
+	const fs = createMemoryFs() as InlangInstanceEnvironment["$fs"]
+	await fs.mkdir("./test")
+	await fs.writeFile("./test/file.txt", "Hello World!")
+	await fs.mkdir("./test/subdir")
+	await fs.writeFile("./test/subdir/file.txt", "Hello World!")
+
+	const env = await createMockEnvironment({ copyDirectory: { fs, paths: ["test"] } })
+	expect(await env.$fs.readFile("./test/file.txt", { encoding: "utf-8" })).toBe("Hello World!")
+	expect(await env.$fs.readFile("./test/subdir/file.txt", { encoding: "utf-8" })).toBe(
+		"Hello World!",
+	)
+})
+
+it("should copy multiple directories into the environment", async () => {
+	// to test with node (a real filesystem), outcomment this line and
+	// import fs from "node:fs/promises" above.
+	const fs = createMemoryFs() as InlangInstanceEnvironment["$fs"]
+	await fs.mkdir("./one")
+	await fs.writeFile("./one/file.txt", "Hello from one")
+	await fs.mkdir("./two/subdir", { recursive: true })
+	await fs.writeFile("./two/file.txt", "Hello from two")
+
+	const env = await createMockEnvironment({ copyDirectory: { fs, paths: ["one", "two"] } })
+	expect(await env.$fs.readFile("./one/file.txt", { encoding: "utf-8" })).toBe("Hello from one")
+	expect(await env.$fs.readFile("./two/file.txt", { encoding: "utf-8" })).toBe("Hello from two")
+})
+
+it("should be able to import JavaScript from the environment", async () => {
+	const fs = createMemoryFs() as InlangInstanceEnvironment["$fs"]
+	await fs.mkdir("./test")
+	await fs.writeFile("./test/file.js", "export const x = 'hello'")
+	const env = await createMockEnvironment({ copyDirectory: { fs, paths: ["./test"] } })
+	const { x } = await env.$import("./test/file.js")
+	expect(x).toBe("hello")
+})
+
+it("should give an error if the path does not exist (hinting at a current working directory problem)", async () => {
+	const fs = createMemoryFs() as InlangInstanceEnvironment["$fs"]
+	// relative imports are relative to the current working directory, not the file.
+	// thus, if you run the tests from the root of the project, the path will be wrong.
+	try {
+		await createMockEnvironment({ copyDirectory: { fs, paths: ["../test"] } })
+	} catch (error) {
+		expect(
+			(error as Error).message.includes(
+				`Make sure that the \`copyDirectory.path\` is relative to the current working directory`,
+			),
+		).toBe(true)
+	}
+})
+
+it("should work with filesystems created from volumes", async () => {
+	const fs = createMemoryFs()
+	await fromJson({
+		fs,
+		resolveFrom: "/",
+		json: {
+			"locales/en.json": btoa(JSON.stringify({ hello: "hello from en" })),
+			"locales/fr.json": btoa(JSON.stringify({ hello: "bonjour via fr" })),
+			"locales/de.json": btoa(JSON.stringify({ hello: "hallo von de" })),
+			"locales/utils.js": btoa(JSON.stringify("jibberish")),
+			"main.js": btoa("export function hello() { return 'hello' }"),
+		},
+	})
+	const env = await createMockEnvironment({ copyDirectory: { fs: fs, paths: ["/"] } })
+	expect(await env.$fs.readFile("./locales/en.json", { encoding: "utf-8" })).toBe(
+		JSON.stringify({ hello: "hello from en" }),
+	)
+})
