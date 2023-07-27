64e249253cde605ce06b993fc75d5298de16849e
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2023-07-27 10:15:39 +0200

    subdir for create app

--- packages/core2/src/app/createInlang.ts
+++
@@ -1,9 +0,0 @@
-import type { InlangInstance } from "./api.js"
-import type { InlangInstanceEnvironment } from "./environment/index.js"
-
-export function createInlang(args: {
-	configPath: string
-	env: InlangInstanceEnvironment
-}): InlangInstance {
-	return {} as any
-}
