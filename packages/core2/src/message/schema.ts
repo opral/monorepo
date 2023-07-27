7d631b37d55dcc32dc0183bf3a907176394e2004
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2023-07-27 10:14:05 +0200

    add zod types

--- packages/core2/src/message/schema.ts
+++ packages/core2/src/message/schema.ts
@@ -1,3 +1,4 @@
+import { z } from "zod"
 import type { LanguageTag } from "../types.js"
 
 export type Message = {
@@ -38,6 +39,50 @@ export type VariableReference = {
 	type: "VariableReference"
 	name: string
 }
+
+// export type FunctionReference = {
+// 	type: "function"
+// 	name: string
+// 	operand?: Text | VariableReference
+// 	options?: Option[]
+// }
+
+/**
+ * ---------- Zod Types ----------
+ *
+ * Not using z.infer to provide developers with a better
+ * hover experience over types (comments, etc.)
+ */
+
+export const VariableReference = z.object({
+	type: z.literal("VariableReference"),
+	name: z.string(),
+})
+
+export const Expression = z.object({
+	type: z.literal("Expression"),
+	// only variable reference for now, but will be extended in the future
+	body: VariableReference,
+})
+
+export const Text = z.object({
+	type: z.literal("Text"),
+	value: z.string(),
+})
+
+export const Pattern = z.array(z.union([Text, Expression]))
+
+export const Variant = z.object({
+	match: z.record(z.string()),
+	pattern: Pattern,
+})
+
+export const Message = z.object({
+	id: z.string(),
+	expressions: z.array(Expression),
+	selectors: z.array(Expression),
+	body: z.record(z.array(Variant)),
+})
 
 // export type FunctionReference = {
 // 	type: "function"
