7e3262bf717f8de10c58f2a1c521357e560beac9
Ivan Hofer <ivan.hofer@outlook.com>
2023-07-27 16:00:13 +0200

    migrate identicalPattern lint rule

--- packages/plugins/standard-lint-rules/src/rules/identicalPattern.ts
+++ packages/plugins/standard-lint-rules/src/rules/identicalPattern.ts
@@ -1,5 +1,9 @@
-import type { LanguageTag, Text } from "@inlang/core/ast"
-import { createLintRule } from "@inlang/core/lint"
+import type { MessageLintRule } from "@inlang/lint-api"
+import type { Variant } from '@inlang/messages'
+
+type IdenticalPatternRuleOptions = {
+	ignore: string[]
+}
 
 /**
  * Checks for identical patterns in different languages.
@@ -8,46 +12,35 @@ import { createLintRule } from "@inlang/core/lint"
  * that the translations are redundant or can be combined into a single
  * message to reduce translation effort.
  */
-export const identicalPattern = createLintRule({
+export const identicalPattern = ({ ignore = [] }: IdenticalPatternRuleOptions) => ({
 	id: "inlang.identicalPattern",
-	setup: ({ config, report }) => {
-		let targetMessageLanguage: LanguageTag["name"] | undefined
-		return {
-			visitors: {
-				Resource: ({ target, reference }) => {
-					targetMessageLanguage = target?.languageTag.name
-					if (
-						(target && target.languageTag.name === config.sourceLanguageTag) ||
-						!target?.body.find((element) => element.pattern) ||
-						!reference?.body.find((element) => element.pattern)
-					) {
-						return "skip"
-					}
-				},
-				Message: ({ target, reference }) => {
-					if (reference?.pattern === undefined || target?.pattern === undefined) {
-						return "skip"
-					}
+	displayName: {
+		en: 'Identical Pattern'
+	},
+	defaultLevel: 'warn', // TODO: how to override level?
+	message: ({ message: { id, body }, config, report }) => {
+		const referenceVariants = body[config.sourceLanguageTag]!
 
-					const referenceMessage = JSON.stringify(reference?.pattern)
-					const targetMessage = JSON.stringify(target?.pattern)
-
-					const numOfWords = reference.pattern.elements
-						.filter((element) => {
-							return element.type === "Text"
-						})
-						.map((element) => (element as Text).value)
-						.join(" ")
-						.split(" ").length
-
-					if (referenceMessage === targetMessage && numOfWords >= 3) {
-						report({
-							node: target,
-							message: `Identical message found in language '${targetMessageLanguage}' with message ID '${target.id.name}'.`,
-						})
+		const languageTags = Object.keys(body)
+		for (const languageTag of languageTags) {
+			const isMessageIdentical = messageBodyToString(referenceVariants) === messageBodyToString(body[languageTag]!)
+			const shouldBeIgnored = (referenceVariants || [])
+				.some(variant => ignore.includes(patternToString(variant.pattern)))
+			if (isMessageIdentical && !shouldBeIgnored) {
+				report({
+					messageId: id,
+					languageTag,
+					body: {
+						en: `Identical content found in language '${languageTag}' with message ID '${id}'.`,
 					}
-				},
-			},
+				})
+			}
 		}
 	},
-})
+}) satisfies MessageLintRule
+
+// TODO: use a generic toString function instead of JSON.stringify
+const messageBodyToString = (body: Variant[]) => JSON.stringify(body)
+
+// TODO: use a generic toString function instead of this custom code
+const patternToString = (pattern: Variant['pattern']) => JSON.stringify(pattern)
