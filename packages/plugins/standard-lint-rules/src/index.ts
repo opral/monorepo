74043ac0c7d8ead903725de50999d20b2a27978e
Ivan Hofer <ivan.hofer@outlook.com>
2023-07-31 15:12:33 +0200

    define module correctly

--- packages/plugins/standard-lint-rules/src/index.ts
+++ packages/plugins/standard-lint-rules/src/index.ts
@@ -1,1 +1,10 @@
-export { default } from "./module.js"
+// TODO: remove ignore comment
+// @ts-ignore
+import type { InlangModule } from "@inlang/module"
+import { identicalPatternRule } from "./rules/identicalPattern/identicalPattern.js"
+import { messageWithoutSourceRule } from "./rules/messageWithoutSource/messageWithoutSource.js"
+import { missingMessageRule } from "./rules/missingMessage/missingMessage.js"
+
+export default {
+	lintRules: [identicalPatternRule, messageWithoutSourceRule, missingMessageRule],
+} satisfies InlangModule
