83f4da17948ae4d65424dd6084eb279fffcfc921
Ivan Hofer <ivan.hofer@outlook.com>
2023-08-01 13:59:53 +0200

    add test cases

---
+++ source-code/core/lint/src/lintMessages.test.ts
@@ -0,0 +1,20 @@
+import { beforeEach, describe, expect, test, vi } from "vitest"
+import { lintMessages } from "./lintMessages.js"
+
+describe("lintMessages", async () => {
+	beforeEach(() => {
+		vi.resetAllMocks()
+	})
+
+	test("it should await all messages", async () => {
+
+	})
+
+	test("it should process all messages and rules in parallel", async () => {
+
+	})
+
+	test("it should not abort the linting process when errors occur", async () => {
+
+	})
+})
