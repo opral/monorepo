e3ae1cc1eef63fef1d3deb273bc8ec7023375e9a
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2023-08-01 18:38:27 +0200

    chore: add comment

--- packages/core/result/src/api.ts
+++ packages/core/result/src/api.ts
@@ -4,4 +4,26 @@ export type ErrorResult<Error> = { data?: never; error: Error }
 
 export type SuccessWithErrorResult<Data, Error> = { data: Data; error: Error }
 
-export type Result<Data, Error> = SuccessResult<Data> | ErrorResult<Error> | SuccessWithErrorResult<Data, Error>
+/**
+ * A result represents an either success or an error, but never both.
+ *
+ * Do not use Result to represent both success and error at the same time
+ * to not confuse the consumer of the result.
+ *
+ * @example
+ *  function doSomething(): Result<string, Error> {
+ *    return {
+ *      error: "Something went wrong"
+ *    }
+ *  }
+ *
+ *  const result = doSomething()
+ *  if (result.error) {
+ *    console.log(result.error)
+ *  }
+ *
+ */
+export type Result<Data, Error> =
+	| SuccessResult<Data>
+	| ErrorResult<Error>
+	| SuccessWithErrorResult<Data, Error>
