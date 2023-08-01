8e2022b698cac4b967279a89e8f61df3fd9b8bc0
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2023-08-01 10:58:35 +0200

    refactor: api

--- packages/core/app/src/api.ts
+++ packages/core/app/src/api.ts
@@ -1,9 +1,10 @@
 import type { InlangConfig } from "@inlang/config"
 import type { InlangEnvironment } from "@inlang/environment"
-import type { LintException, LintReport } from "@inlang/lint"
+import type { LintException, LintReport, LintRule } from "@inlang/lint"
 import type { MessageQueryApi } from "@inlang/messages"
 import type { Result } from "@inlang/result"
 import type { InvalidConfigError } from "./errors.js"
+import type { Plugin, ResolvedPluginsApi } from "@inlang/plugin"
 
 export type InlangInstance = {
 	config: {
@@ -14,13 +15,48 @@ export type InlangInstance = {
 		set: (config: InlangConfig) => Result<void, InvalidConfigError>
 	}
 	env: InlangEnvironment
-	query: {
-		messages: MessageQueryApi
-	}
 	lint: {
+		rules: Reactive<"onlyGetter", Array<Pick<LintRule, "meta"> & { module: string }>>
 		// for now, only simply array that can be improved in the future
 		// see https://github.com/inlang/inlang/issues/1098
-		reports: () => LintReport[]
-		exceptions: () => LintException[]
+		reports: ReactiveAsync<"onlyGetter", LintReport[]>
+		exceptions: ReactiveAsync<"onlyGetter", LintException[]>
+	}
+	messages: {
+		query: MessageQueryApi
+	}
+	plugin: {
+		list: Reactive<"onlyGetter", Array<Pick<Plugin, "meta"> & { module: string }>>
+		appSpecificApi: ResolvedPluginsApi["appSpecificApi"]
+		// loadMessages: () => Promise<void>
+		// saveMessages: () => Promise<void>
 	}
 }
+
+type Reactive<WithSetter extends "onlyGetter" | "withSetter", T> = {
+	get: () => T
+} & (WithSetter extends "withSetter"
+	? {
+			set: (value: T) => void
+	  }
+	: {})
+
+/**
+ * A reactive async value.
+ *
+ * @throws If the value is not yet initialized.
+ */
+type ReactiveAsync<WithSetter extends "onlyGetter" | "withSetter", T> = {
+	/**
+	 * Initialize the getter.
+	 */
+	init: () => Promise<void>
+	/**
+	 * @throws If the value is not yet initialized.
+	 */
+	get: () => T
+} & (WithSetter extends "withSetter"
+	? {
+			set: (value: T) => Promise<void>
+	  }
+	: {})
