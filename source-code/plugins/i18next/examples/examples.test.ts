c3bb0c55610045323bbcc556d641bff6df226805
Nils Jacobsen <nils.jacobsen98@gmail.com>
2023-08-02 16:49:46 +0200

    feat: update example test -> i18next
    - change js to json
    - update test file

--- source-code/plugins/i18next/examples/examples.test.ts
+++ source-code/plugins/i18next/examples/examples.test.ts
@@ -1,6 +1,6 @@
 import { expect } from "vitest"
-import { mockEnvironment } from "@inlang/core/test"
-import { setupConfig } from "@inlang/core/config"
+import { createMockEnvironment, InlangConfig } from "@inlang/plugin"
+import { plugin } from "./../src/plugin.js"
 import fs from "node:fs/promises"
 import { fileURLToPath } from "node:url"
 import { dirname } from "node:path"
@@ -17,17 +17,32 @@ describe("examples", () => {
 	for (const exampleDir of exampleDirectories) {
 		// setup config also validates the config with a few tests
 		it(`${exampleDir} inlang's setup config should pass`, async () => {
-			const env = await mockEnvironment({
+			const env = await createMockEnvironment({
 				copyDirectory: {
 					fs: fs,
 					paths: ["./dist", `./examples/${exampleDir}`],
 				},
 			})
 
-			const module = await import(`./${exampleDir}/inlang.config.js`)
-			const config = await setupConfig({ module, env })
+			let error = undefined
 
-			expect(config).toBeDefined()
+			try {
+				const config = JSON.parse(
+					await env.$fs.readFile(`./examples/${exampleDir}/inlang.config.json`, {
+						encoding: "utf-8",
+					}),
+				) as InlangConfig
+				const options = config.settings.plugins["inlang.plugin-i18next"].options
+				plugin.setup({ fs: env.$fs, options: options })
+				const messages = await plugin.loadMessages({
+					languageTags: config.languageTags,
+				})
+				await plugin.saveMessages({ messages: messages })
+			} catch (e) {
+				error = e
+			}
+
+			expect(error).toBe(undefined)
 		})
 	}
 })
