01c3ca83bf02237c12a0491a8bc056688d59bd0e
Robin Bühler <git+r@obin.ch>
2023-08-02 13:37:59 +0000

    chore: test with mocha

--- source-code/ide-extension/test/suite.ts
+++ source-code/ide-extension/test/suite.ts
@@ -1,6 +1,37 @@
 import * as path from 'node:path';
-import { run as nodeTestRun } from 'node:test';
+import Mocha from 'mocha';
+import * as glob from '/workspaces/inlang/source-code/ide-extension/node_modules/glob/dist/cjs/src/index.js';
 
-export function run() {
-  nodeTestRun({ files: [path.resolve('**/**.test.js')] }).pipe(process.stdout);
+export function run(): Promise<void> {
+  // Create the mocha test
+  const mocha = new Mocha({
+    ui: 'tdd'
+  });
+
+  const testsRoot = path.resolve(__dirname, '..');
+
+  return new Promise((c, e) => {
+    glob('**/**.test.js', { cwd: testsRoot }, (err, files) => {
+      if (err) {
+        return e(err);
+      }
+
+      // Add files to the test suite
+      for (const f of files) mocha.addFile(path.resolve(testsRoot, f));
+
+      try {
+        // Run the mocha test
+        mocha.run(failures => {
+          if (failures > 0) {
+            e(new Error(`${failures} tests failed.`));
+          } else {
+            c();
+          }
+        });
+      } catch (err) {
+        console.error(err);
+        e(err);
+      }
+    });
+  });
 }
