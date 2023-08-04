e752b3c2f3503068cca5a9f3c9b1d3990b074594
Ivan Hofer <ivan.hofer@outlook.com>
2023-08-04 06:59:16 +0200

    migrate sdk-js-plugin

--- packages/sdk-js-plugin/src/index.ts
+++ packages/sdk-js-plugin/src/index.ts
@@ -1,2 +1,7 @@
-export { sdkPlugin as default } from "./plugin.js"
 export { type SdkConfig, type SdkConfigInput, validateSdkConfig } from "./api.js"
+import type { InlangModule } from "@inlang/module"
+import { sdkPlugin } from './plugin.js'
+
+export default {
+	plugins: [sdkPlugin],
+} satisfies InlangModule['default']
--- packages/sdk-js-plugin/src/plugin.ts
+++ packages/sdk-js-plugin/src/plugin.ts
@@ -1,95 +1,22 @@
 import { type SdkConfigInput, validateSdkConfig } from "./api.js"
-import type { InlangConfig } from "@inlang/core/config"
-import { createPlugin } from "@inlang/core/plugin"
+import { Plugin } from "@inlang/plugin"
+import { ideExtensionDefaultConfig } from './ideExtension/config.js'
 
 // ------------------------------------------------------------------------------------------------
 
-export const sdkPlugin = createPlugin<SdkConfigInput>(({ settings }) => ({
-	id: "inlang.sdk-js",
-	config: async () => {
-		const parsedConfig = validateSdkConfig(settings)
+export const sdkPlugin = {
+	meta: {
+		id: "inlang.sdk-js",
+		displayName: { en: "Inlang SDK for JavaScript" },
+		description: { en: "Plugin for the Inlang SDK for JavaScript" },
+		keywords: ["inlang", "sdk", "runtime", "plugin", "tranform", "javascript", "typescript", "svelte", "sveltekit"],
+	},
+	addAppSpecificApi({ options }) {
+		const parsedConfig = validateSdkConfig(options)
 
 		return {
-			sdk: parsedConfig,
-			ideExtension: ideExtensionDefaultConfig,
+			"inlang.sdk-js": parsedConfig,
+			"inlang.ide-extension": ideExtensionDefaultConfig,
 		}
 	},
-}))
-
-const ideExtensionDefaultConfig: InlangConfig["ideExtension"] = {
-	messageReferenceMatchers: [
-		async (args) => {
-			const regex = /(?<!\w){?i\(['"](?<messageId>\S+)['"]\)}?/gm
-			const str = args.documentText
-			let match
-			const result = []
-
-			while ((match = regex.exec(str)) !== null) {
-				const startLine = (str.slice(0, Math.max(0, match.index)).match(/\n/g) || []).length + 1
-				const startPos = match.index - str.lastIndexOf("\n", match.index - 1)
-				const endPos =
-					match.index + match[0].length - str.lastIndexOf("\n", match.index + match[0].length - 1)
-				const endLine =
-					(str.slice(0, Math.max(0, match.index + match[0].length)).match(/\n/g) || []).length + 1
-
-				if (match.groups && "messageId" in match.groups) {
-					result.push({
-						messageId: match.groups["messageId"]!,
-						position: {
-							start: {
-								line: startLine,
-								character: startPos,
-							},
-							end: {
-								line: endLine,
-								character: endPos,
-							},
-						},
-					})
-				}
-			}
-			return result
-		},
-	],
-	extractMessageOptions: [
-		{
-			callback: (messageId) => `{i("${messageId}")}`,
-		},
-	],
-	documentSelectors: [
-		{
-			language: "javascript",
-		},
-		{
-			language: "typescript",
-		},
-		{
-			language: "svelte",
-		},
-	],
-}
-
-//* Prep for using chevrotain after build step issues have been resolved
-// const ideExtensionDefaultConfig: InlangConfig["ideExtension"] = {
-// 	messageReferenceMatchers: [
-// 		async (args) => {
-// 			return messageReferenceMatcher(args.documentText)
-// 		},
-// 	],
-// 	extractMessageOptions: [
-// 		{
-// 			callback: (messageId) => `{i("${messageId}")}`,
-// 		},
-// 	],
-// 	documentSelectors: [
-// 		{
-// 			language: "javascript",
-// 		},
-// 		{
-// 			language: "typescript",
-// 		},
-// 		{
-// 			language: "svelte",
-// 		},
-// 	],
-// }
+} satisfies Plugin<SdkConfigInput>
