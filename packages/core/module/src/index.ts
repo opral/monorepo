ad6eebb66c218553fb4891f212c242acd1145f14
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2023-08-04 10:48:26 +0200

    chore: clean up dependency graph
    - inlang module is now the entrypoint for plugins and lint rules
    - fixed various dependency graphs

--- packages/core/module/src/index.ts
+++ packages/core/module/src/index.ts
@@ -1,3 +1,12 @@
 export type { InlangModule } from "./api.js"
 export { moduleBuildConfig } from "./moduleBuildConfig.js"
 export { resolveModules } from "./resolveModules.js"
+
+/**
+ * -------- RE-EXPORTS --------
+ *
+ * See https://github.com/inlang/inlang/issues/1184
+ */
+
+export * from "@inlang/plugin"
+export * from "@inlang/lint"
