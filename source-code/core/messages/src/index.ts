c87e711584e00f64e5606e68b95b25fe11ff9629
Ivan Hofer <ivan.hofer@outlook.com>
2023-08-04 17:16:07 +0200

    expose `Pattern`

--- source-code/core/messages/src/index.ts
+++ source-code/core/messages/src/index.ts
@@ -1,3 +1,3 @@
-export { Message, VariableReference, Expression, Text, Variant } from "./schema.js"
+export { Message, VariableReference, Expression, Text, Variant, Pattern } from "./schema.js"
 export { getVariant, updateVariant, createVariant } from "./utilities.js"
 export { createQuery, type MessageQueryApi } from "./query.js"
