40e75b2c05d15ddc6b629b4fcf2fb6c5c9fc24b1
Nils Jacobsen <nils.jacobsen98@gmail.com>
2023-08-07 16:10:27 +0200

    refactoring: using message query in createInlang

--- packages/core/app/src/createInlang.test.ts
+++ packages/core/app/src/createInlang.test.ts
@@ -3,75 +3,113 @@ import { createInlang } from "./createInlang.js"
 // eslint-disable-next-line no-restricted-imports
 import fs from "node:fs/promises"
 import type { InlangConfig } from "@inlang/config"
-import type { Plugin } from "@inlang/plugin"
+import type { Message, Plugin } from "@inlang/plugin"
 import type { LintRule } from "@inlang/lint"
 import type { InlangModule } from "@inlang/module"
 
-const config: InlangConfig = {
-	sourceLanguageTag: "en",
-	languageTags: ["en"],
-	modules: ["./dist/index.js"],
-	settings: {
-		plugins: {
-			"inlang.plugin-i18next": {
-				options: {
-					pathPattern: "./examples/example01/{languageTag}.json",
-					variableReferencePattern: ["{", "}"],
-				},
-			},
-		},
-		lintRules: {
-			"inlang.missingMessage": {
-				level: "error",
-			},
-		},
-	},
-}
-
-const mockPlugin: Plugin = {
-	meta: {
-		id: "mock.plugin",
-		description: { en: "Mock plugin description" },
-		displayName: { en: "Mock Plugin" },
-		keywords: [],
-	},
-	loadMessages: () => undefined as any,
-	saveMessages: () => undefined as any,
-	addAppSpecificApi: () => ({
-		"inlang.ide-extension": {
-			messageReferenceMatcher: () => undefined as any,
-		},
-	}),
-}
-
-const mockLintRule: LintRule = {
-	meta: {
-		id: "mock.lint-rule",
-		description: { en: "Mock lint rule description" },
-		displayName: { en: "Mock Lint Rule" },
-	},
-	defaultLevel: "error",
-}
-
-const env = {
-	$fs: fs,
-	$import: async () =>
-		({
-			default: {
-				plugins: [mockPlugin],
-				lintRules: [mockLintRule],
-			},
-		} satisfies InlangModule),
-}
-
 describe("test", () => {
 	it("test", async () => {
-		await env.$fs.writeFile("./inlang.config.json", JSON.stringify(config))
+		const config: InlangConfig = {
+			sourceLanguageTag: "en",
+			languageTags: ["en"],
+			modules: ["./dist/index.js"],
+			settings: {
+				plugins: {
+					"inlang.plugin-i18next": {
+						options: {
+							pathPattern: "./examples/example01/{languageTag}.json",
+							variableReferencePattern: ["{", "}"],
+						},
+					},
+				},
+				lintRules: {
+					"inlang.missingMessage": {
+						level: "error",
+					},
+				},
+			},
+		}
+
+		const mockPlugin: Plugin = {
+			meta: {
+				id: "inlang.plugin-i18next",
+				description: { en: "Mock plugin description" },
+				displayName: { en: "Mock Plugin" },
+				keywords: [],
+			},
+			loadMessages: () => exampleMessages,
+			saveMessages: () => undefined as any,
+			addAppSpecificApi: () => ({
+				"inlang.ide-extension": {
+					messageReferenceMatcher: () => undefined as any,
+				},
+			}),
+		}
+
+		const exampleMessages: Message[] = [
+			{
+				id: "common:a..b",
+				selectors: [],
+				body: {
+					en: [
+						{
+							match: {},
+							pattern: [
+								{
+									type: "Text",
+									value: "test",
+								},
+							],
+						},
+					],
+				},
+			},
+			{
+				id: "common:c.",
+				selectors: [],
+				body: {
+					en: [
+						{
+							match: {},
+							pattern: [
+								{
+									type: "Text",
+									value: "test",
+								},
+							],
+						},
+					],
+				},
+			},
+		]
+
+		const mockLintRule: LintRule = {
+			meta: {
+				id: "inlang.missingMessage",
+				description: { en: "Mock lint rule description" },
+				displayName: { en: "Mock Lint Rule" },
+			},
+			defaultLevel: "error",
+		}
+
+		const $import = async () =>
+			({
+				default: {
+					plugins: [mockPlugin],
+					lintRules: [mockLintRule],
+				},
+			} satisfies InlangModule)
+
+		await fs.writeFile("./inlang.config.json", JSON.stringify(config))
 		const inlang = await createInlang({
 			configPath: "./inlang.config.json",
+			nodeishFs: fs,
+			_import: $import,
 		})
-		const reactiveConfig = instance.config.get()
-		instance.config.set({ ...reactiveConfig, languageTags: ["en", "de"] })
+		const reactiveConfig = inlang.config.get()
+		inlang.config.set({ ...reactiveConfig, languageTags: ["en", "de"] })
 		expect(reactiveConfig.languageTags.length).toBe(1)
+		const message = inlang.messages.query.get({ where: { id: "common:a..b" } })
+		expect(message?.id).toBe("common:a..b")
 	})
 })
--- packages/core/app/src/errors.ts
+++ packages/core/app/src/errors.ts
@@ -1,6 +1,6 @@
 export class InvalidConfigError extends Error {
-	constructor(message: string) {
-		super(message)
+	constructor(message: string, options: ErrorOptions) {
+		super(message, options)
 		this.name = "InvalidConfigError"
 	}
 }
