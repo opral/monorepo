60de6ee5cddabc50315fe7675a0a4c51d47d0f8d
Nils Jacobsen <nils.jacobsen98@gmail.com>
2023-08-07 16:10:27 +0200

    refactoring: using message query in createInlang

--- source-code/core/app/src/createInlang.ts
+++ source-code/core/app/src/createInlang.ts
@@ -1,9 +1,18 @@
-import { InlangConfig } from "@inlang/config"
 import type { InlangInstance } from "./api.js"
 import { ImportFunction, resolveModules } from "@inlang/module"
 // @ts-ignore
-import { createSignal, createRoot } from "solid-js/dist/solid.js"
-import type { NodeishFilesystemSubset } from "@inlang/plugin"
+import {
+	createSignal,
+	createRoot,
+	createMemo,
+	createEffect,
+	createResource,
+} from "solid-js/dist/solid.js"
+import { NodeishFilesystemSubset, InlangConfig, createQuery, ResolvedPlugins } from "@inlang/plugin"
+import { TypeCompiler } from "@sinclair/typebox/compiler"
+import { InvalidConfigError } from "./errors.js"
+
+const ConfigCompiler = TypeCompiler.Compile(InlangConfig)
 
 /**
  * Creates an inlang instance.
@@ -18,28 +27,68 @@ export async function createInlang(args: {
 	_import?: ImportFunction
 }): Promise<InlangInstance> {
 	return createRoot(async () => {
+		const errors: Array<InvalidConfigError> = []
+
 		// just for testing
 		if (!args.nodeishFs) return {} as any
 		// TODO #1182 the filesystem type is incorrect. manual type casting is required
 		const configFile = (await args.nodeishFs.readFile(args.configPath, {
 			encoding: "utf-8",
 		})) as string
-		const configJson = JSON.parse(configFile)
+		const parsedConfig = JSON.parse(configFile)
+		const typeErrors = [...ConfigCompiler.Errors(parsedConfig)]
 
-		const parsedConfig: InlangConfig = InlangConfig.passthrough().parse(configJson)
+		// -- USES INVALID SCHEMA --
+		if (typeErrors.length > 0) {
+			errors.push(
+				new InvalidConfigError(`The config is invalid according to the schema.`, {
+					cause: errors,
+				}),
+			)
+		}
+
+		// config reactivity
+		const [config, setConfig] = createSignal(parsedConfig as InlangConfig)
+
+		// resolve modules
+		const getResolvedModules = async (config: InlangConfig) => {
+			const resolvedModules = await resolveModules({
+				config,
+				nodeishFs: args.nodeishFs,
+				_import: args._import,
+			})
+			return resolvedModules
+		}
 
 		const resolvedModules = await resolveModules({
 			config: parsedConfig,
-			_import: args._import,
 			nodeishFs: args.nodeishFs,
+			_import: args._import,
 		})
 
-		const [config, setConfig] = createSignal<InlangConfig>(parsedConfig)
-		const [lintRules, setLintRules] = createSignal<Record<string, any>>({})
+		// access plugins
+		const plugins = resolvedModules.data.plugins.data
 
-		for (const rule of Object.values(resolvedModules.data.lintRules)) {
-			// console.log(rule)
+		// lint rules derived reactivity
+		const getLintRules = async () => {
+			if (config()) {
+				const resolvedModules = await resolveModules({
+					config: config(),
+					nodeishFs: args.nodeishFs,
+					_import: args._import,
+				})
+				return resolvedModules.data.lintRules.map((rule) => rule.meta)
+			} else {
+				return []
+			}
 		}
+
+		const messages = await plugins.loadMessages({
+			languageTags: ["en"],
+		})
+
+		// lint reports reactivity
+		const [lintReports] = createSignal({})
 
 		return {
 			config: {
@@ -47,12 +96,18 @@ export async function createInlang(args: {
 				set: setConfig,
 			},
 			lint: {
-				rules: {} as any,
-				reports: {} as any,
+				rules: {
+					get: {} as any,
+				},
+				reports: {
+					get: lintReports,
+				} as any,
 				exceptions: {} as any,
 			},
-			plugins: resolvedModules.data.plugins.data,
-			messages: {} as any,
-		}
+			plugins: {} as any,
+			messages: {
+				query: createQuery(messages),
+			},
+		} satisfies InlangInstance
 	})
 }
