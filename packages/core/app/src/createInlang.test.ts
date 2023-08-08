db4d85aaac0cff302ab1475a6458168982dd58ae
NiklasBuchfink <niklas.buchfink@web.de>
2023-08-08 16:54:37 +0200

    add: config tests for createInlang

--- packages/core/app/src/createInlang.test.ts
+++ packages/core/app/src/createInlang.test.ts
@@ -8,6 +8,135 @@ import type { LintRule } from "@inlang/lint"
 import type { InlangModule } from "@inlang/module"
 import { createRoot, createEffect } from "./solid.js"
 
+describe("initialization", () => {
+	describe("config", () => {
+		it.todo("should throw if config file is not found", async () => {
+			try {
+				await fs.readFile("./inlang.config.json", "utf-8")
+			}
+			catch (e) {
+				expect((e as Error).message).toBe("ENOENT: no such file or directory, open './inlang.config.json'")
+			}
+
+			try	{
+				await createInlang({
+					configPath: "./inlang.config.json",
+					nodeishFs: fs,
+					_import: $import,
+				})
+				throw new Error("Should not reach this")
+			}
+			catch (e) {
+				expect((e as Error).message).toBe("Config file not found")
+			}
+		})
+		
+		it.todo("should throw if config file is not a valid JSON", async () => {
+			await fs.writeFile("./inlang.config.json", "invalid json")
+			try	{
+				await createInlang({
+					configPath: "./inlang.config.json",
+					nodeishFs: fs,
+					_import: $import,
+				})
+				throw new Error("Should not reach this")
+			}
+			catch (e) {
+				expect((e as Error).message).toBe("Config file is not valid JSON")
+			}
+		})
+
+		it.todo("should throw if config file is does not match schema", async () => {
+			await fs.writeFile("./inlang.config.json", JSON.stringify({}))
+			try	{
+				await createInlang({
+					configPath: "./inlang.config.json",
+					nodeishFs: fs,
+					_import: $import,
+				})
+				throw new Error("Should not reach this")
+			}
+			catch (e) {
+				expect((e as Error).message).toBe("Config file does not match schema")
+			}
+		})
+
+		it("should return the parsed config", async () => {
+			await fs.writeFile("./inlang.config.json", JSON.stringify(config))
+			const inlang = await createInlang({
+				configPath: "./inlang.config.json",
+				nodeishFs: fs,
+				_import: $import,
+			})
+			expect(inlang.config.get()).toEqual(config)
+		})
+	})
+
+	describe("modules", () => {
+		it.todo("should throw if plugins contain errors ???")
+		it.todo("should throw if lintRules contain errors ???")
+		it.todo("should return meta data")
+		it.todo("should return plugins")
+		it.todo("should return lint rules")
+	})
+
+	describe("flow", () => {
+		it.todo("should not call functions multiple times")
+		it.todo("should load modules after config")
+		it.todo("should not load messages")
+		it.todo("should not call lint")
+	})
+
+	describe("instance object", () => {
+		it.todo("should contain all fields")
+	})
+})
+
+describe("reactivity", () => {
+	describe("config", () => {
+		it("should react to changes to config", async () => { 
+			createRoot( async () => {
+				await fs.writeFile("./inlang.config.json", JSON.stringify(config))
+				const inlang = await createInlang({
+					configPath: "./inlang.config.json",
+					nodeishFs: fs,
+					_import: $import,
+				})
+				const reactiveConfig = inlang.config.get
+				let counter = 0
+	
+				createEffect(() => {
+					// 2 times because effect creation + set
+					reactiveConfig()
+					counter += 1
+				})		
+	
+				inlang.config.set({ ...reactiveConfig(), languageTags: ["en", "de"] })
+				expect(counter).toBe(2)
+			})
+		})
+	})
+
+	describe("modules", () => {
+		it.todo("should react to changes to config")
+	})
+
+	describe("messages", () => {
+		it.todo("should react to changes to config")
+		it.todo("should react to changes to modules")
+	})
+
+	describe("lint", () => {
+		it.todo("should react to changes to config")
+		it.todo("should react to changes to modules")
+		it.todo("should react to changes to mesages")
+	})
+
+	describe("query", () => {
+		it.todo("should react to changes to mesages")
+	})
+})
+
 const config: InlangConfig = {
 	sourceLanguageTag: "en",
 	languageTags: ["en"],
@@ -56,7 +185,7 @@ const exampleMessages: Message[] = [
 					pattern: [
 						{
 							type: "Text",
-							value: "test",
+							value: "common a b",
 						},
 					],
 				},
@@ -73,7 +202,7 @@ const exampleMessages: Message[] = [
 					pattern: [
 						{
 							type: "Text",
-							value: "test",
+							value: "common c ",
 						},
 					],
 				},
@@ -98,112 +227,3 @@ const $import = async () =>
 		lintRules: [mockLintRule],
 	},
 } satisfies InlangModule)
-
-describe("config", () => {
-	it("should get the config", async () => {
-		await fs.writeFile("./inlang.config.json", JSON.stringify(config))
-		const inlang = await createInlang({
-			configPath: "./inlang.config.json",
-			nodeishFs: fs,
-			_import: $import,
-		})
-		expect(inlang.config.get()).toEqual(config)
-	})
-
-	// TODO: test that config set
-	it("should set the new config", async () => {
-		await fs.writeFile("./inlang.config.json", JSON.stringify(config))
-		const inlang = await createInlang({
-			configPath: "./inlang.config.json",
-			nodeishFs: fs,
-			_import: $import,
-		})
-		const newConfig = { ...config, languageTags: ["en", "de"] }
-		inlang.config.set(newConfig)
-		expect(inlang.config.get()).toEqual(newConfig)
-	})
-
-	it("should be reactive if the config changes", async () => {
-		createRoot(async () => {
-			await fs.writeFile("./inlang.config.json", JSON.stringify(config))
-			const inlang = await createInlang({
-				configPath: "./inlang.config.json",
-				nodeishFs: fs,
-				_import: $import,
-			})
-			const reactiveConfig = inlang.config.get
-			let counter = 0
-
-			createEffect(() => {
-				// 2 times because effect creation + set
-				reactiveConfig()
-				counter += 1
-			})
-
-			inlang.config.set({ ...reactiveConfig(), languageTags: ["en", "de"] })
-			expect(counter).toBe(2)
-		})
-	})
-})
-
-describe("query", () => {
-	it("get", async () => {
-		await fs.writeFile("./inlang.config.json", JSON.stringify(config))
-		const inlang = await createInlang({
-			configPath: "./inlang.config.json",
-			nodeishFs: fs,
-			_import: $import,
-		})
-		const message = inlang.messages.query.get({ where: { id: "common:a..b" } })
-		expect(message?.id).toBe("common:a..b")
-	})
-})
-
-describe("initialization", () => {
-	describe("config", () => {
-		it.todo("should throw if config file is not found")
-		it.todo("should throw if config file is not a valid JSON")
-		it.todo("should throw if config file is does not match schema")
-		it.todo("should return the parsed config")
-	})
-
-	describe("modules", () => {
-		it.todo("should throw if plugins contain errors ???")
-		it.todo("should throw if lintRules contain errors ???")
-		it.todo("should return meta data")
-		it.todo("should return plugins")
-		it.todo("should return lint rules")
-	})
-
-	describe("flow", () => {
-		it.todo("should not call functions multiple times")
-		it.todo("should load modules after config")
-		it.todo("should not load messages")
-		it.todo("should not call lint")
-	})
-
-	describe("instance object", () => {
-		it.todo("should contain all fields")
-	})
-})
-
-describe("reactivity", () => {
-	describe("modules", () => {
-		it.todo("should react to changes to config")
-	})
-
-	describe("messages", () => {
-		it.todo("should react to changes to config")
-		it.todo("should react to changes to modules")
-	})
-
-	describe("lint", () => {
-		it.todo("should react to changes to config")
-		it.todo("should react to changes to modules")
-		it.todo("should react to changes to mesages")
-	})
-
-	describe("query", () => {
-		it.todo("should react to changes to mesages")
-	})
-})
