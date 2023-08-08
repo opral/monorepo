182b1deac64a702e6951fb55e27004547bf09fb1
Ivan Hofer <ivan.hofer@outlook.com>
2023-08-08 11:24:42 +0200

    refactor

--- packages/core/app/src/createInlang.ts
+++ packages/core/app/src/createInlang.ts
@@ -1,6 +1,6 @@
 import type { InlangInstance } from "./api.js"
 import { ImportFunction, ResolveModulesFunction, resolveModules } from "@inlang/module"
-import { NodeishFilesystemSubset, InlangConfig, createQuery, ResolvedPlugins, Message } from "@inlang/plugin"
+import { NodeishFilesystemSubset, InlangConfig, createQuery, ResolvedPlugins, Message, tryCatch } from "@inlang/plugin"
 import { TypeCompiler } from "@sinclair/typebox/compiler"
 import { Value } from "@sinclair/typebox/value"
 import { InvalidConfigError } from "./errors.js"
@@ -16,151 +16,145 @@ const ConfigCompiler = TypeCompiler.Compile(InlangConfig)
  *   and supporting legacy modules such as CJS.
  *
  */
-export async function createInlang(args: {
+export const createInlang = async (args: {
 	configPath: string
 	nodeishFs: NodeishFilesystemSubset
 	_import?: ImportFunction
-}): Promise<InlangInstance> {
-	return createRoot(async () => {
-		const [initialized, markInitAsComplete] = createAwaitable()
+}): Promise<InlangInstance> => createRoot(async () => {
+	const [initialized, markInitAsComplete] = createAwaitable()
 
-		// -- config ------------------------------------------------------------
+	// -- config ------------------------------------------------------------
 
-		const [config, setConfig] = createSignal<InlangConfig>()
-		createEffect(() => {
-			loadConfig({ configPath: args.configPath, nodeishFs: args.nodeishFs }).then(setConfig)
-		})
-		// TODO: create FS watcher and update config on change
-
-		// -- modules -----------------------------------------------------------
-
-		const [modules, setModules] = createSignal<Awaited<ReturnType<ResolveModulesFunction>>>()
-		const [plugins, setPlugins] = createSignal<ResolvedPlugins>() // TODO: is `ResolvedPlugins` the correct naming?
-		const [lintRules, setLintRules] = createSignal<Pick<LintRule, 'meta'>[]>([])
-		createEffect(() => {
-			const conf = config()
-			if (!conf) return
-
-			loadModules({ config: conf, nodeishFs: args.nodeishFs, _import: args._import }).then((modules) => {
-				setModules(modules) // TODO: do we need a signal for this? Or just store modules.data.meta
-				// TODO: what to do with errors here?
-				setPlugins(modules.data.plugins.data)
-				setLintRules(modules.data.lintRules.data)
-			})
-		})
-
-		// -- messages ----------------------------------------------------------
-
-		const [messages, setMessages] = createSignal<Message[]>()
-		createEffect(() => {
-			const plugs = plugins()
-			if (!plugs) return
-
-			// TODO: should createInlang already load messages ? I don't think so
-			// TODO: call effect only with the same params as the app initialized it with
-			makeTrulyAsync(plugs.loadMessages({ languageTags: ["en"] }))  // TODO: why 'en' and why only 'en'?
-				.then((messages) => {
-					setMessages(messages)
-
-					markInitAsComplete()
-				})
-		})
-
-		// const query = createQuery(messages() || []) // TODO: make query reactive
-
-		// -- lint --------------------------------------------------------------
-
-		const [lintInitialized, setLintInitialized] = createSignal(false)
-		const [lintReportsInitialized, markLintReportsAsInitialized] = createAwaitable()
-		const initLint = () => {
-			setLintInitialized(true)
-			return lintReportsInitialized
-		}
-
-		const [lintReports, setLintReports] = createSignal<LintReport[]>()
-		const [lintExceptions, setLintExceptions] = createSignal<LintException[]>()
-		createEffect(() => {
-			const msgs = messages()
-			if (!msgs || !lintInitialized()) return
-
-			// TODO: only lint changed messages and update arrays selectively
-			lintMessages({ config: config() as InlangConfig, messages: msgs, query, rules: lintRules() as LintRule[] })
-				.then((report) => {
-					setLintReports(report.data)
-					setLintExceptions(report.errors) // TODO: errors or exceptions as name?
-
-					markLintReportsAsInitialized()
-				})
-		})
-
-		// -- app ---------------------------------------------------------------
-
-		await initialized
-
-		// TODO: remove workaround and init reactive query above
-		const query = createQuery(messages() || [])
-
-		return {
-			module: modules()!.data.module, // TODO: shouldn't this also be reactive?
-			config: {
-				get: config as () => InlangConfig,
-				set: setConfig,
-			},
-			lint: {
-				init: initLint,
-				rules: {
-					get: lintRules,
-				},
-				reports: {
-					get: () => {
-						const reports = lintReports()
-						// TODO: improve error
-						if (!reports) throw new Error('lint not initialized yet')
-						return reports
-					},
-				},
-				exceptions: {
-					get: () => {
-						const exceptions = lintExceptions()
-						// TODO: improve error
-						if (!exceptions) throw new Error('lint not initialized yet')
-						return exceptions
-					},
-				},
-			},
-			plugins: plugins() as ResolvedPlugins, // TODO: shouldn't this also be reactive?
-			messages: {
-				// init, // TODO: expose load messages to app (also save)
-				query,
-			},
-		} satisfies InlangInstance
+	const [config, setConfig] = createSignal<InlangConfig>()
+	createEffect(() => {
+		loadConfig({ configPath: args.configPath, nodeishFs: args.nodeishFs }).then(setConfig)
 	})
-}
+	// TODO: create FS watcher and update config on change
+
+	// -- modules -----------------------------------------------------------
+
+	const [modules, setModules] = createSignal<Awaited<ReturnType<ResolveModulesFunction>>>()
+	const [plugins, setPlugins] = createSignal<ResolvedPlugins>()
+	const [lintRules, setLintRules] = createSignal<Pick<LintRule, 'meta'>[]>([])
+	createEffect(() => {
+		const conf = config()
+		if (!conf) return
+
+		loadModules({ config: conf, nodeishFs: args.nodeishFs, _import: args._import }).then((modules) => {
+			setModules(modules)
+			setPlugins(modules.data.plugins.data)
+			setLintRules(modules.data.lintRules.data)
+
+			// TODO: handle `detectedLanguageTags`
+		})
+	})
+
+	// -- messages ----------------------------------------------------------
+
+	const [messages, setMessages] = createSignal<Message[]>()
+	createEffect(() => {
+		const plugs = plugins()
+		if (!plugs) return
+
+		makeTrulyAsync(plugs.loadMessages({ languageTags: config()!.languageTags }))
+			.then((messages) => {
+				setMessages(messages)
+
+				markInitAsComplete()
+			})
+	})
+
+	// const query = createQuery(messages() || []) // TODO: make query reactive
+
+	// -- lint --------------------------------------------------------------
+
+	const [lintInitialized, setLintInitialized] = createSignal(false)
+	const [lintReportsInitialized, markLintReportsAsInitialized] = createAwaitable()
+	const initLint = () => {
+		setLintInitialized(true)
+		return lintReportsInitialized
+	}
+
+	const [lintReports, setLintReports] = createSignal<LintReport[]>()
+	const [lintErrors, setLintErrors] = createSignal<LintException[]>()
+	createEffect(() => {
+		const msgs = messages()
+		if (!msgs || !lintInitialized()) return
+
+		// TODO: only lint changed messages and update arrays selectively
+		lintMessages({ config: config() as InlangConfig, messages: msgs, query, rules: lintRules() as LintRule[] })
+			.then((report) => {
+				setLintReports(report.data)
+				setLintErrors(report.errors)
+
+				markLintReportsAsInitialized()
+			})
+	})
+
+	// -- app ---------------------------------------------------------------
+
+	await initialized
+
+	// TODO: remove workaround and init reactive query above
+	const query = createQuery(messages() || [])
+
+	return {
+		meta: { // TODO: make reactive (using store)
+			modules: [], // TODO: get meta data
+			lintRules: [], // TODO: get meta data
+			plugins: [], // TODO: get meta data
+		},
+		errors: { // TODO: make reactive (using store)
+			module: modules()!.errors,
+			plugin: modules()!.data.plugins.errors,
+			lintRules: modules()!.data.lintRules.errors,
+		},
+		config: {
+			get: config as () => InlangConfig,
+			set: setConfig,
+		},
+		lint: {
+			init: initLint,
+			reports: {
+				get: () => {
+					const reports = lintReports()
+					// TODO: improve error
+					if (!reports) throw new Error('lint not initialized yet')
+					return reports
+				},
+			},
+		},
+		appSpecificApi: plugins()!.appSpecificApi, // TODO: make reactive (using store)
+		messages: {
+			query,
+		},
+	} satisfies InlangInstance
+})
 
 const loadConfig = async (args: {
 	configPath: string
 	nodeishFs: NodeishFilesystemSubset
 }) => {
 	// TODO #1182 the filesystem type is incorrect. manual type casting is required
-	const configFile = (await args.nodeishFs.readFile(args.configPath, { encoding: "utf-8" })) // TODO: this could throw!
-	const parsedConfig = JSON.parse(configFile) // TODO: this could throw!
+	const { data: configFile, error: configFileError } =
+		await tryCatch(async () => await args.nodeishFs.readFile(args.configPath, { encoding: "utf-8" }))
+	if (configFileError) throw configFileError // TODO: improve error
+
+	// TODO: fix tryCatch typing to get rid of !
+	const { data: parsedConfig, error: parseConfigError } = tryCatch(() => JSON.parse(configFile!))
+	if (parseConfigError) throw parseConfigError // TODO: improve error
+
 	const typeErrors = [...ConfigCompiler.Errors(parsedConfig)]
 
 	if (typeErrors.length > 0) {
-		const errors: Array<InvalidConfigError> = []
-
-		errors.push(
-			new InvalidConfigError(`The config is invalid according to the schema.`, {
-				cause: errors,
-			}),
-		)
-		// TODO: shouldn't we stop executing here?
+		throw new InvalidConfigError(`The config is invalid according to the schema.`, {
+			cause: typeErrors,
+		})
 	}
 
 	return Value.Cast(InlangConfig, parsedConfig)
 }
 
-// TODO: throw if errors occured?
 const loadModules = async (args: {
 	config: InlangConfig
 	nodeishFs: NodeishFilesystemSubset
--- packages/core/result/src/tryCatch.ts
+++ packages/core/result/src/tryCatch.ts
@@ -1,4 +1,4 @@
-import type { Result } from "./api.js"
+import type { ErrorResult, Result, SuccessResult } from "./api.js"
 
 /**
  * Try catch wrapper for functions that can throw errors.
@@ -14,11 +14,11 @@ export function tryCatch<Data>(callback: () => Promise<Data>): Promise<Result<Da
 export function tryCatch<Data>(callback: () => Data): Result<Data, unknown>
 export function tryCatch<Data>(
 	callback: () => Data | Promise<Data>,
-): Promise<Result<Data, unknown>> | Result<Data, unknown> {
+): PromiseLike<Result<Data, unknown>> | Result<Data, unknown> {
 	try {
 		const callbackResult = callback() as Data | Promise<Data>
-		if (callbackResult instanceof Promise) {
-			return callbackResult.then((data) => ({ data })).catch((error) => ({ error }))
+		if (callbackResult instanceof Promise) { // TODO: this is not the best way to check for a promise
+			return callbackResult.then((data) => ({ data } as SuccessResult<Data>)).catch((error) => ({ error } as unknown as ErrorResult<unknown>))
 		}
 		return { data: callbackResult }
 	} catch (e) {
