36d817a3709a396adcf074c70234d7c79f17c024
Robin Bühler <git+r@obin.ch>
2023-08-09 12:28:40 +0000

    chore: add test to check if extension is loaded

---
+++ source-code/ide-extension/src/main.e2e.test.ts
@@ -0,0 +1,8 @@
+import * as assert from 'node:assert';
+import * as vscode from 'vscode';
+
+suite('main.ts suite', () => {
+  test('if extension is loaded', () => {
+    assert.notEqual(vscode.extensions.getExtension('inlang.vs-code-extension'), undefined);
+  });
+});
