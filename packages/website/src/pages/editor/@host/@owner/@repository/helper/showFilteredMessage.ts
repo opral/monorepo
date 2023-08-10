6b6164845db582175812b5fbb6f37f196cf1bcb7
NiklasBuchfink <niklas.buchfink@web.de>
2023-08-10 18:33:24 +0200

    refactor: filtering in Editor

--- packages/website/src/pages/editor/@host/@owner/@repository/helper/showFilteredMessage.ts
+++ packages/website/src/pages/editor/@host/@owner/@repository/helper/showFilteredMessage.ts
@@ -2,68 +2,76 @@ import type { Message, LanguageTag } from "@inlang/app"
 import { handleMissingMessage } from "./handleMissingMessage.js"
 
 export const showFilteredMessage = (
-	message: Message[] | undefined,
+	message: Message | undefined,
 	filteredLanguageTags: LanguageTag[],
 	textSearch: string,
 	filteredLintRules: `${string}.${string}`[],
 	messageId: string,
 ) => {
 	// filteredByLanguage
-	const filteredByLanguage = Object.keys(messages)
-		.filter((key) => filteredLanguageTags.length === 0 || filteredLanguageTags.includes(key))
-		.reduce((filteredMessage, key) => {
-			filteredMessage[key] = messages[key]
-			return filteredMessage
-		}, {} as { [key: string]: LintedMessage | undefined })
+	const filteredByLanguage = {
+		...message,
+		body: Object.fromEntries(
+			Object.entries(message!.body).filter(([language]) => {
+				if (filteredLanguageTags.length === 0) {
+					return true
+				} else {
+					if (message !== undefined && filteredLanguageTags.includes(language)) {
+						return true
+					}
+				}
+				return false
+			}),
+		),
+	} as Message
 
-	// filteredById
-	const filteredById = Object.values(filteredByLanguage).filter((message) => {
-		if (messageId === "") {
-			return true
-		} else {
-			if (message !== undefined && message.id.name === messageId) {
-				return true
-			}
-		}
-		return false
-	})
+
+	// filteredById	
+	const filteredById = (messageId === "" || (message !== undefined && message.id === messageId)) 
+	? filteredByLanguage : undefined
 
 	// filteredBySearch
-	const filteredBySearch = filteredById.filter((message) => {
-		if (textSearch.length === 0) {
-			return true
-		} else {
-			if (
-				message !== undefined &&
-				textSearch.length > 0 &&
-				(message?.id.name.toLowerCase().includes(textSearch.toLowerCase()) ||
-					JSON.stringify(message).toLowerCase().includes(textSearch.toLowerCase()))
-			) {
-				return true
-			}
-		}
-		return false
-	})
+	const filteredBySearch = (
+		textSearch.length === 0 || 
+		(
+			message !== undefined &&
+			(
+				message?.id.toLowerCase().includes(textSearch.toLowerCase()) ||
+				Object.values(message!.body).some((value) => {
+						value[0]?.pattern.map((pattern) => {
+							if (pattern.type === "Text") {
+								return pattern.value.toLocaleLowerCase()
+							} else if (pattern.type === "VariableReference") {
+								return pattern.name.toLowerCase()
+							} else {
+								return false
+							}
+						}).join("").includes(textSearch.toLowerCase())
+					}
+				)
+			)
+		)
+	) ? filteredById : undefined
 
-	// filteredByLintRules
-	const filteredByLintRules = filteredBySearch.filter((message) => {
-		if (filteredLintRules.length === 0) {
-			return true
-		} else {
-			for (const report of getLintReports(message!)) {
-				if (
-					filteredLintRules.includes(report.id) &&
-					handleMissingMessage(report, filteredLanguageTags)
-				) {
-					return true
-				}
-				false
-				continue
-			}
-		}
-		return false
-	})
+	// // filteredByLintRules
+	// const filteredByLintRules = filteredBySearch.filter((message) => {
+	// 	if (filteredLintRules.length === 0) {
+	// 		return true
+	// 	} else {
+	// 		for (const report of getLintReports(message!)) {
+	// 			if (
+	// 				filteredLintRules.includes(report.id) &&
+	// 				handleMissingMessage(report, filteredLanguageTags)
+	// 			) {
+	// 				return true
+	// 			}
+	// 			false
+	// 			continue
+	// 		}
+	// 	}
+	// 	return false
+	// })
 
 	// return if matched
-	return filteredByLintRules
+	return filteredBySearch
 }
