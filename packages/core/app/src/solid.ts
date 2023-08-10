0f2510dc2dc33b5518d0d8450154105b7ec4d7ed
Nils Jacobsen <nils.jacobsen98@gmail.com>
2023-08-10 17:39:32 +0200

    refactor: createInlang return observables with solid wrapper

--- packages/core/app/src/solid.ts
+++ packages/core/app/src/solid.ts
@@ -1,12 +1,16 @@
-// @ts-ignore
-import { createSignal as _createSignal, createRoot as _createRoot, createEffect as _createEffect } from "solid-js/dist/solid.js"
+import {
+	createSignal as _createSignal,
+	createRoot as _createRoot,
+	createEffect as _createEffect,
+	observable as _observable,
+	from as _from,
+	// @ts-ignore
+} from "solid-js/dist/solid.js"
 
-const createSignal = _createSignal as typeof import('solid-js')['createSignal']
-const createRoot = _createRoot as typeof import('solid-js')['createRoot']
-const createEffect = _createEffect as typeof import('solid-js')['createEffect']
+const createSignal = _createSignal as typeof import("solid-js")["createSignal"]
+const createRoot = _createRoot as typeof import("solid-js")["createRoot"]
+const createEffect = _createEffect as typeof import("solid-js")["createEffect"]
+const observable = _observable as typeof import("solid-js")["observable"]
+const from = _from as typeof import("solid-js")["from"]
 
-export {
-	createSignal,
-	createRoot,
-	createEffect,
-}
+export { createSignal, createRoot, createEffect, observable, from }
