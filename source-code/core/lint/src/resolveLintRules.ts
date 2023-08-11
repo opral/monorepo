ec65764aeb21b660c073c06e75d63b02d5679340
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2023-08-11 13:49:49 +0200

    fix: remove dependency on module

--- source-code/core/lint/src/resolveLintRules.ts
+++ source-code/core/lint/src/resolveLintRules.ts
@@ -2,9 +2,7 @@ import { Value } from "@sinclair/typebox/value"
 import { LintRule } from "./api.js"
 import { InvalidLintRuleError } from "./errors.js"
 
-export const resolveLintRules = (args: {
-	lintRules: Array<LintRule & { meta: { module?: string } }>
-}) => {
+export const resolveLintRules = (args: { lintRules: Array<LintRule> }) => {
 	const result = {
 		data: [] as Array<LintRule>,
 		errors: [] as InvalidLintRuleError[],
@@ -12,14 +10,9 @@ export const resolveLintRules = (args: {
 	for (const rule of args.lintRules) {
 		if (Value.Check(LintRule, rule) === false) {
 			result.errors.push(
-				new InvalidLintRuleError(
-					`Couldn't parse lint rule "${rule.meta.id}"${
-						!rule.meta.module ? "" : ` from module  ${rule.meta.module}`
-					}"`,
-					{
-						module: "not implemented",
-					},
-				),
+				new InvalidLintRuleError(`Couldn't parse lint rule "${rule.meta.id}"`, {
+					module: "not implemented",
+				}),
 			)
 			continue
 		} else {
