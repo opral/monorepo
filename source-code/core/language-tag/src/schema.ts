a8df237eaf8da98dea7c46ceeb41872a20b0e819
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2023-08-14 16:08:58 +0200

    feat: enforce correct subset of language tags
    Closes enforce correct LanguageTag #1105

--- source-code/core/language-tag/src/schema.ts
+++ source-code/core/language-tag/src/schema.ts
@@ -1,14 +1,16 @@
 import { Type, Static, TSchema } from "@sinclair/typebox"
+import { languageTagRegex } from "./registry.js"
 
 /**
- * Follow the IETF BCP 47 language tag schema.
+ * Follows the IETF BCP 47 language tag schema.
+ *
+ * Contains only max 2 letter tags for now. Will be extended in the future.
  *
  * @see https://www.ietf.org/rfc/bcp/bcp47.txt
  * @see https://www.iana.org/assignments/language-subtag-registry/language-subtag-registry
  */
-// TODO improve typescript type to be a union of all valid language tags https://www.iana.org/assignments/language-subtag-registry/language-subtag-registry
 export type LanguageTag = Static<typeof LanguageTag>
-export const LanguageTag = Type.String()
+export const LanguageTag = Type.String({ pattern: languageTagRegex })
 
 /**
  * Content for a language tag.
@@ -17,7 +19,7 @@ export const LanguageTag = Type.String()
  */
 export type WithLanguageTags<T> = {
 	en: T
-	[languageTag: string]: T
+	[languageTag: LanguageTag]: T
 }
 export const WithLanguageTags = <T extends TSchema>(type: T) =>
 	Type.Intersect([Type.Object({ en: type }), Type.Record(LanguageTag, type)])
