9bea9bfa1b17c4441e950f5167420d0895f16540
Jan Johannes <jan@ntr.io>
2023-08-15 13:38:52 +0300

    make reactivity work as expected for testing

--- source-code/core/app/src/createReactiveQuery.test.ts
+++ source-code/core/app/src/createReactiveQuery.test.ts
@@ -5,7 +5,7 @@ import type { Message, Text } from '@inlang/plugin'
 
 describe("get", () => {
 	it.only("should react to `create`", async () => {
-		await createRoot(async () => {
+		createRoot(async () => {
 			const query = createReactiveQuery(() => [])
 
 			let message: Message | undefined
@@ -13,11 +13,11 @@ describe("get", () => {
 				message = query.get({ where: { id: "1" } })
 				console.log(1, message)
 			})
-			await new Promise((resolve) => setTimeout(resolve, 0))
+			await new Promise((resolve) => setTimeout(resolve, 100))
 			expect(message).toBeUndefined()
 
 			query.create({ data: { id: "1", selectors: [], body: {} } })
-			await new Promise((resolve) => setTimeout(resolve, 0))
+			await new Promise((resolve) => setTimeout(resolve, 100))
 
 			expect(message).toBeDefined()
 
