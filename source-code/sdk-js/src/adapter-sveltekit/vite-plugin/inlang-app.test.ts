03926b34908a61e78d8fdc1b46cfc40f3c364c2f
Ivan Hofer <ivan.hofer@outlook.com>
2023-08-16 11:32:50 +0200

    describe test cases

--- source-code/sdk-js/src/adapter-sveltekit/vite-plugin/inlang-app.test.ts
+++ source-code/sdk-js/src/adapter-sveltekit/vite-plugin/inlang-app.test.ts
@@ -77,3 +77,19 @@ describe('getSvelteKitVersion', async () => {
 		})
 	})
 })
+
+describe('shouldContentBePrerendered', () => {
+	describe('should return `false`', () => {
+		it.todo('if no file exists')
+		it.todo('if file does not have a `prerender` export')
+		it.todo('if file has a `prerender` export set to `false`')
+	})
+
+	describe('should return `true`', () => {
+		it.todo('if a file has a `prerender` export set to `true`')
+		it.todo('if a file has a `prerender` export set to `auto`')
+		it.todo('should work in `.js` files')
+		it.todo('should work in `.ts` files')
+		it.todo('should work with import statements')
+	})
+})
