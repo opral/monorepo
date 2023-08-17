85a936801bc0872b4255d902bfc2383b9236e3fe
Jan Johannes <jan@ntr.io>
2023-08-17 18:38:10 +0300

    fix lisa formatting and package

--- project-lisa/source-code/client/src/api.ts
+++ project-lisa/source-code/client/src/api.ts
@@ -3,7 +3,7 @@ import raw from "isomorphic-git"
 import http from "isomorphic-git/http/web/index.js"
 import type { NodeishFilesystem } from "@inlang-git/fs"
 import { withLazyFetching, transformRemote } from "./helpers.js"
-import type { Endpoints } from "@octokit/types";
+import type { Endpoints } from "@octokit/types"
 
 type Author = {
 	name?: string
@@ -14,27 +14,36 @@ type Author = {
 
 export type Repository = {
 	nodeishFs: NodeishFilesystem
-	commit: (args: { author: Author, message: string}) => Promise<Awaited<ReturnType<typeof raw.commit>> | undefined>
+	commit: (args: {
+		author: Author
+		message: string
+	}) => Promise<Awaited<ReturnType<typeof raw.commit>> | undefined>
 	push: () => Promise<Awaited<ReturnType<typeof raw.push>> | undefined>
-	pull: (args: { author: Author, fastForward: boolean, singleBranch: true }) => any
-	add: (args: { filepath: string }) =>  Promise<Awaited<ReturnType<typeof raw.add>>>
+	pull: (args: { author: Author; fastForward: boolean; singleBranch: true }) => any
+	add: (args: { filepath: string }) => Promise<Awaited<ReturnType<typeof raw.add>>>
 	listRemotes: () => Promise<Awaited<ReturnType<typeof raw.listRemotes>> | undefined>
-	log: (args?: { since?: Date, depth?: number }) =>  Promise<Awaited<ReturnType<typeof raw.log>>>
+	log: (args?: { since?: Date; depth?: number }) => Promise<Awaited<ReturnType<typeof raw.log>>>
 	statusMatrix: (args: { filter: any }) => Promise<Awaited<ReturnType<typeof raw.statusMatrix>>>
 	status: (args: { filepath: string }) => Promise<Awaited<ReturnType<typeof raw.status>>>
-	mergeUpstream: (args: { branch: string }) => Promise<Endpoints["POST /repos/{owner}/{repo}/merge-upstream"]["response"]["data"] | undefined>
+	mergeUpstream: (args: {
+		branch: string
+	}) => Promise<
+		Endpoints["POST /repos/{owner}/{repo}/merge-upstream"]["response"]["data"] | undefined
+	>
 	isCollaborator: (args: { username: string }) => Promise<boolean>
 	getOrigin: () => Promise<string>
 	getCurrentBranch: () => Promise<string | undefined>
 	getMeta: () => Promise<{
-		name: string,
-		isPrivate: boolean,
-		isFork: boolean,
-		owner: { name?: string, email?: string },
-		parent:  {
-			url: string,
-			fullName: string
-		} | undefined
+		name: string
+		isPrivate: boolean
+		isFork: boolean
+		owner: { name?: string; email?: string }
+		parent:
+			| {
+					url: string
+					fullName: string
+			  }
+			| undefined
 	}>
 
 	// TODO: implement these before publishing api, but not used in badge or editor, depends on strategy for statelessness
@@ -42,7 +51,15 @@ export type Repository = {
 	// changeBranch: () => unknown
 }
 
-export function open (url: string, args: { nodeishFs: NodeishFilesystem, workingDirectory?: string, corsProxy?: string, auth?: unknown }): Repository {
+export function open(
+	url: string,
+	args: {
+		nodeishFs: NodeishFilesystem
+		workingDirectory?: string
+		corsProxy?: string
+		auth?: unknown
+	},
+): Repository {
 	const rawFs = args.nodeishFs
 
 	// parse url in the format of github.com/inlang/example and split it to host, owner and repo
@@ -73,23 +90,26 @@ export function open (url: string, args: { nodeishFs: NodeishFilesystem, working
 	// the directory we use for all git operations
 	const dir = "/"
 
-	let pending: Promise<void> | undefined = raw.clone({
-		fs: withLazyFetching(rawFs, 'clone'),
-		http,
-		dir,
-		corsProxy: args?.corsProxy,
-		url: normalizedUrl,
-		singleBranch: true,
-		depth: 1,
-		noTags: true,
-	}).finally(() => {
-		pending = undefined
-	}).catch((err: any) => {
-		console.error('error cloning the repository', err)
-	})
+	let pending: Promise<void> | undefined = raw
+		.clone({
+			fs: withLazyFetching(rawFs, "clone"),
+			http,
+			dir,
+			corsProxy: args?.corsProxy,
+			url: normalizedUrl,
+			singleBranch: true,
+			depth: 1,
+			noTags: true,
+		})
+		.finally(() => {
+			pending = undefined
+		})
+		.catch((err: any) => {
+			console.error("error cloning the repository", err)
+		})
 
 	// delay all fs and repo operations until the repo clone and checkout have finished, this is preparation for the lazy feature
-	function delayedAction ({ execute }: { execute: () => any }) {
+	function delayedAction({ execute }: { execute: () => any }) {
 		if (pending) {
 			return pending.then(execute)
 		}
@@ -98,20 +118,20 @@ export function open (url: string, args: { nodeishFs: NodeishFilesystem, working
 	}
 
 	return {
-		nodeishFs: withLazyFetching(rawFs, 'app', delayedAction),
+		nodeishFs: withLazyFetching(rawFs, "app", delayedAction),
 
 		/**
 		 * Gets the git origin url of the current repository.
 		 *
 		 * @returns The git origin url or undefined if it could not be found.
 		 */
-		async listRemotes () {
+		async listRemotes() {
 			try {
-				const withLazyFetchingpedFS = withLazyFetching(rawFs, 'listRemotes', delayedAction)
+				const withLazyFetchingpedFS = withLazyFetching(rawFs, "listRemotes", delayedAction)
 
 				const remotes = await raw.listRemotes({
 					fs: withLazyFetchingpedFS,
-					dir
+					dir,
 				})
 
 				return remotes
@@ -120,72 +140,72 @@ export function open (url: string, args: { nodeishFs: NodeishFilesystem, working
 			}
 		},
 
-		status (cmdArgs) {
+		status(cmdArgs) {
 			return raw.status({
-				fs: withLazyFetching(rawFs, 'statusMatrix', delayedAction),
+				fs: withLazyFetching(rawFs, "statusMatrix", delayedAction),
 				dir,
-				filepath: cmdArgs.filepath
+				filepath: cmdArgs.filepath,
 			})
 		},
 
-		statusMatrix (cmdArgs) {
+		statusMatrix(cmdArgs) {
 			return raw.statusMatrix({
-				fs: withLazyFetching(rawFs, 'statusMatrix', delayedAction),
+				fs: withLazyFetching(rawFs, "statusMatrix", delayedAction),
 				dir,
-				filter: cmdArgs.filter
+				filter: cmdArgs.filter,
 			})
 		},
 
-		add (cmdArgs) {
+		add(cmdArgs) {
 			return raw.add({
-				fs: withLazyFetching(rawFs, 'add', delayedAction),
+				fs: withLazyFetching(rawFs, "add", delayedAction),
 				dir,
-				filepath: cmdArgs.filepath
+				filepath: cmdArgs.filepath,
 			})
 		},
 
-		commit (cmdArgs) {
+		commit(cmdArgs) {
 			return raw.commit({
-				fs: withLazyFetching(rawFs, 'commit', delayedAction),
+				fs: withLazyFetching(rawFs, "commit", delayedAction),
 				dir,
 				author: cmdArgs.author,
-				message: cmdArgs.message
+				message: cmdArgs.message,
 			})
 		},
 
-		push () {
+		push() {
 			return raw.push({
-				fs: withLazyFetching(rawFs, 'push', delayedAction),
+				fs: withLazyFetching(rawFs, "push", delayedAction),
 				url: normalizedUrl,
 				corsProxy: args?.corsProxy,
 				http,
-				dir
+				dir,
 			})
 		},
 
-		pull (cmdArgs) {
+		pull(cmdArgs) {
 			return raw.pull({
-				fs: withLazyFetching(rawFs, 'pull', delayedAction),
+				fs: withLazyFetching(rawFs, "pull", delayedAction),
 				url: normalizedUrl,
 				corsProxy: args?.corsProxy,
 				http,
 				dir,
 				fastForward: cmdArgs.fastForward,
 				singleBranch: cmdArgs.singleBranch,
-				author: cmdArgs.author
+				author: cmdArgs.author,
 			})
 		},
 
-		log (cmdArgs) {
+		log(cmdArgs) {
 			return raw.log({
-				fs: withLazyFetching(rawFs, 'log', delayedAction),
+				fs: withLazyFetching(rawFs, "log", delayedAction),
 				depth: cmdArgs?.depth,
 				dir,
-				since: cmdArgs?.since
+				since: cmdArgs?.since,
 			})
 		},
 
-		async mergeUpstream (cmdArgs) {
+		async mergeUpstream(cmdArgs) {
 			let response
 			try {
 				response = await github.request("POST /repos/{owner}/{repo}/merge-upstream", {
@@ -193,23 +213,28 @@ export function open (url: string, args: { nodeishFs: NodeishFilesystem, working
 					owner,
 					repo: repoName,
 				})
-			} catch (_err) { /* ignore */ }
+			} catch (_err) {
+				/* ignore */
+			}
 
 			return response?.data
 		},
 
-		async isCollaborator (cmdArgs) {
-			let response: Awaited<ReturnType<typeof github.request<"GET /repos/{owner}/{repo}/collaborators/{username}">>> | undefined
+		async isCollaborator(cmdArgs) {
+			let response:
+				| Awaited<
+						ReturnType<typeof github.request<"GET /repos/{owner}/{repo}/collaborators/{username}">>
+				  >
+				| undefined
 			try {
-				response = await github.request(
-					"GET /repos/{owner}/{repo}/collaborators/{username}",
-					{
-						owner,
-						repo: repoName,
-						username: cmdArgs.username,
-					},
-				)
-			} catch (_err) { /*  throws on non collaborator access */ }
+				response = await github.request("GET /repos/{owner}/{repo}/collaborators/{username}", {
+					owner,
+					repo: repoName,
+					username: cmdArgs.username,
+				})
+			} catch (_err) {
+				/*  throws on non collaborator access */
+			}
 
 			return response?.status === 204 ? true : false
 		},
@@ -219,7 +244,7 @@ export function open (url: string, args: { nodeishFs: NodeishFilesystem, working
 		 *
 		 * The function ensures that the same orgin is always returned for the same repository.
 		 */
-		async getOrigin (): Promise<string> {
+		async getOrigin(): Promise<string> {
 			const remotes: Array<{ remote: string; url: string }> | undefined = await this.listRemotes()
 
 			const origin = remotes?.find((elements) => elements.remote === "origin")
@@ -235,34 +260,40 @@ export function open (url: string, args: { nodeishFs: NodeishFilesystem, working
 			return transformRemote(result)
 		},
 
-		async getCurrentBranch () {
+		async getCurrentBranch() {
 			// TODO: make stateless
-			return await raw.currentBranch({
-				fs: withLazyFetching(rawFs, 'getCurrentBranch', delayedAction),
-				dir,
-			}) || undefined
+			return (
+				(await raw.currentBranch({
+					fs: withLazyFetching(rawFs, "getCurrentBranch", delayedAction),
+					dir,
+				})) || undefined
+			)
 		},
 
-		 /**
+		/**
 		 * Additional information about a repository provided by GitHub.
 		 */
-		 async getMeta () {
-			const { data: { name, private: isPrivate, fork: isFork, parent, owner: ownerMetaData }}: Awaited<ReturnType<typeof github.request<"GET /repos/{owner}/{repo}">>> = await github
-				.request("GET /repos/{owner}/{repo}", {
+		async getMeta() {
+			const {
+				data: { name, private: isPrivate, fork: isFork, parent, owner: ownerMetaData },
+			}: Awaited<ReturnType<typeof github.request<"GET /repos/{owner}/{repo}">>> =
+				await github.request("GET /repos/{owner}/{repo}", {
 					owner,
 					repo: repoName,
 				})
 
-				return {
-					name,
-					isPrivate,
-					isFork,
-					owner: { name: ownerMetaData.name || undefined, email: ownerMetaData.email || undefined},
-					parent: parent ? {
-						url: transformRemote(parent.git_url),
-						fullName: parent.full_name
-					} : undefined
-				}
+			return {
+				name,
+				isPrivate,
+				isFork,
+				owner: { name: ownerMetaData.name || undefined, email: ownerMetaData.email || undefined },
+				parent: parent
+					? {
+							url: transformRemote(parent.git_url),
+							fullName: parent.full_name,
+					  }
+					: undefined,
+			}
 		},
 	}
 }
--- project-lisa/source-code/client/tests/api.bench.ts
+++ project-lisa/source-code/client/tests/api.bench.ts
@@ -3,14 +3,22 @@ import { open, createNodeishMemoryFs } from "../src/index.js"
 
 describe("main workflow", () => {
 	let repository
-	bench('opens a repo url without error and without blocking i', () => {
-		repository = open("github.com/inlang/example.git", {
-			nodeishFs: createNodeishMemoryFs(),
-		})
-	}, { time: 50 })
+	bench(
+		"opens a repo url without error and without blocking i",
+		() => {
+			repository = open("github.com/inlang/example.git", {
+				nodeishFs: createNodeishMemoryFs(),
+			})
+		},
+		{ time: 50 },
+	)
 
 	let file
-	bench('file is lazy fetched upon first access', async () => {
-		file = await repository.nodeishFs.readFile("./inlang.config.js", { encoding: "utf-8" })
-	}, { time: 100 })
+	bench(
+		"file is lazy fetched upon first access",
+		async () => {
+			file = await repository.nodeishFs.readFile("./inlang.config.js", { encoding: "utf-8" })
+		},
+		{ time: 100 },
+	)
 })
