afcc36f4f7099c6e1b7012229d9723eebda6a8af
flooorianflo <florian.kiem@hfg.design>
2023-08-18 10:04:51 +0200

    feat: marketplace build step types generation

---
+++ packages/marketplace/src/schema.ts
@@ -0,0 +1,24 @@
+import { registry } from "./registry.js"
+
+/**
+ * Metadata that is specific to the marketplace.
+ */
+export interface MarketplaceMetadata {
+	icon: string
+	linkToReadme: Record<string, string>
+	keywords: string[]
+	publisherName: string
+	publisherIcon: string
+}
+
+/**
+ * General Metadata needed for the marketplace.
+ */
+export interface Metadata {
+	id: string
+	displayName: Record<string, string>
+	description: Record<string, string>
+	marketplace: MarketplaceMetadata
+}
+
+export const marketplaceItems: Metadata[] = registry
--- packages/website/src/pages/marketplace/index.page.tsx
+++ packages/website/src/pages/marketplace/index.page.tsx
@@ -1,6 +1,6 @@
 import { Layout } from "../Layout.jsx"
 import { Meta, Title } from "@solidjs/meta"
-import { registry as items } from "@inlang/marketplace/src/registry"
+import { marketplaceItems as items } from "@inlang/marketplace/src"
 import { For, Show, Accessor, createSignal, createEffect } from "solid-js"
 import { Chip } from "#src/components/Chip.jsx"
 import { SearchIcon } from "../editor/@host/@owner/@repository/components/SearchInput.jsx"
@@ -77,50 +77,46 @@ const Gallery = ({ tags }: { tags: Record<string, any>[] }) => {
 			<For each={items}>
 				{(item) => {
 					return (
-						<Show when={tags.some((tag) => tag.name === item.id.split(".")[1].toLowerCase())}>
-							<a
-								href={item.marketplace.linkToReadme.en}
-								target="_blanc"
-								class="relative no-underline"
-							>
-								<div class="flex flex-col relative justify-between gap-4 bg-surface-100 h-full hover:bg-surface-200 p-6 rounded-xl border border-surface-2 cursor-pointer">
-									<div class="flex flex-col gap-4">
-										<div class="flex items-center gap-4">
-											<img class="w-10 h-10 rounded-md m-0 shadow-lg" src={item.marketplace.icon} />
-											<p class="m-0 text-surface-900 font-semibold text-md">
-												{item.displayName.en}
-											</p>
-										</div>
-										<p class="m-0 font-normal leading-6 text-sm tracking-wide text-surface-500 line-clamp-3">
-											{item.description.en}
+						<a
+							href={item.marketplace.linkToReadme.en}
+							target="_blanc"
+							class="relative no-underline"
+						>
+							<div class="flex flex-col relative justify-between gap-4 bg-surface-100 h-full hover:bg-surface-200 p-6 rounded-xl border border-surface-2 cursor-pointer">
+								<div class="flex flex-col gap-4">
+									<div class="flex items-center gap-4">
+										<img class="w-10 h-10 rounded-md m-0 shadow-lg" src={item.marketplace.icon} />
+										<p class="m-0 text-surface-900 font-semibold text-md">{item.displayName.en}</p>
+									</div>
+									<p class="m-0 font-normal leading-6 text-sm tracking-wide text-surface-500 line-clamp-3">
+										{item.description.en}
+									</p>
+								</div>
+								<Show when={item.marketplace.publisherName && item.marketplace.publisherIcon}>
+									<div class="flex gap-2 items-center pt-6">
+										<img class="w-6 h-6 rounded-full m-0" src={item.marketplace.publisherIcon} />
+										<p class="m-0 text-surface-600 no-underline hover:text-surface-900 font-medium">
+											{item.marketplace.publisherName}
 										</p>
 									</div>
-									<Show when={item.marketplace.publisherName && item.marketplace.publisherIcon}>
-										<div class="flex gap-2 items-center pt-6">
-											<img class="w-6 h-6 rounded-full m-0" src={item.marketplace.publisherIcon} />
-											<p class="m-0 text-surface-600 no-underline hover:text-surface-900 font-medium">
-												{item.marketplace.publisherName}
-											</p>
-										</div>
-									</Show>
-									<Chip
-										text={
-											item.id.split(".")[1].toLowerCase() === "lintrule"
-												? "lint rule"
-												: item.id.split(".")[1].toLowerCase()
-										}
-										color={
-											item.id.split(".")[1].toLowerCase() === "app"
-												? "#BF7CE4"
-												: item.id.split(".")[1].toLowerCase() === "plugin"
-												? "#BF7CE4"
-												: "#06B6D4"
-										}
-										customClasses="absolute top-4"
-									/>
-								</div>
-							</a>
-						</Show>
+								</Show>
+								<Chip
+									text={
+										item.id.split(".")[1].toLowerCase() === "lintrule"
+											? "lint rule"
+											: item.id.split(".")[1].toLowerCase()
+									}
+									color={
+										item.id.split(".")[1].toLowerCase() === "app"
+											? "#BF7CE4"
+											: item.id.split(".")[1].toLowerCase() === "plugin"
+											? "#BF7CE4"
+											: "#06B6D4"
+									}
+									customClasses="absolute top-4"
+								/>
+							</div>
+						</a>
 					)
 				}}
 			</For>
