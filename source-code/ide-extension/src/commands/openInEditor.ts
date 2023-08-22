08764815fb9d7c3f5f2a026ab0b5eefd963e30d5
Felix Haeberle <kontakt@felixhaeberle.de>
2023-08-22 18:09:39 +0200

    refactor: IDE-EXTENSION update `openInEditor`

--- source-code/ide-extension/src/commands/openInEditor.ts
+++ source-code/ide-extension/src/commands/openInEditor.ts
@@ -1,19 +1,20 @@
 import { getGitOrigin } from "../services/telemetry/implementation.js"
 import * as vscode from "vscode"
+import type { Message } from "@inlang/app"
 
 const EDITOR_BASE_PATH = "https://inlang.com/editor/"
 
 export const openInEditorCommand = {
 	id: "inlang.openInEditor",
 	title: "Inlang: Open in editor",
-	callback: async function ({ messageId }: { messageId?: string }) {
+	callback: async function (args: { messageId: Message["id"] }) {
 		// TODO: Probably the origin should be configurable via the config.
 		const origin = (await getGitOrigin())?.replaceAll(".git", "")
-		const uri = messageId
-			? `${EDITOR_BASE_PATH}${origin}?id=${messageId}`
+		const uri = args.messageId
+			? `${EDITOR_BASE_PATH}${origin}?id=${args.messageId}`
 			: `${EDITOR_BASE_PATH}${origin}`
 
 		vscode.env.openExternal(vscode.Uri.parse(uri))
 		return undefined
 	},
-} as const
+}
