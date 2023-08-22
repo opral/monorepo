a9f7f04dd05cd88ac9940d205d44c12923942922
Felix Haeberle <kontakt@felixhaeberle.de>
2023-08-22 15:29:58 +0200

    refactor: IDE-EXTENSION update contextTooltip

--- packages/ide-extension/src/commands/editMessage.ts
+++ packages/ide-extension/src/commands/editMessage.ts
@@ -2,49 +2,10 @@ import { state } from "../state.js"
 import { msg } from "../utilities/message.js"
 import { EventEmitter, window } from "vscode"
 import type { LanguageTag, Message, Pattern } from "@inlang/app"
+import { getPatternFromString, getStringFromPattern } from "../utilities/query.js"
 
 const onDidEditMessageEmitter = new EventEmitter<void>()
 export const onDidEditMessage = onDidEditMessageEmitter.event
-
-// get string from pattern
-const getStringFromPattern = (args: {
-	pattern: Pattern
-	languageTag: LanguageTag
-	messageId: Message["id"]
-}): string => {
-	return args.pattern
-		.map((element) => {
-			if (element.type === "Text") {
-				return element.value
-			} else if (element.type === "VariableReference") {
-				return `{${element.name}}` // TODO: Use framework specific placeholder indication
-			} else {
-				return msg(
-					`Unknown pattern element type in message with id ${args.messageId} for languageTag ${args.languageTag}.`,
-				)
-			}
-		})
-		.join("")
-}
-
-// get Pattern from string
-const getPatternFromString = (args: { string: string }): Pattern => {
-	const patternElements = args.string.split(/({.*?})/g) // TODO: Use framework specific placeholder indication
-	const patternElementsWithTypes = patternElements.map((element) => {
-		if (element.startsWith("{") && element.endsWith("}")) {
-			return {
-				type: "VariableReference" as const,
-				name: element.slice(1, -1),
-			}
-		} else {
-			return {
-				type: "Text" as const,
-				value: element,
-			}
-		}
-	})
-	return patternElementsWithTypes
-}
 
 export const editMessageCommand = {
 	id: "inlang.editMessage",
