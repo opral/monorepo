ba85a292d4854d288a86b20dd9af08e1757b4441
Jan Johannes <jan.johannes@inlang.com>
2023-08-22 11:29:28 +0300

    implement cli test suite

--- packages/cli/src/utilities/getGitRemotes.ts
+++ packages/cli/src/utilities/getGitRemotes.ts
@@ -1,16 +1,25 @@
 import { raw } from "@inlang-git/client/raw"
 import fs from "node:fs"
+import type { NodeishFilesystem } from "@inlang-git/fs"
 
+// TODO: move to lix api when local repos supported
 /**
  * Gets the git origin url of the current repository.
  *
+ * @params args.filepath filepath override for injecting non cwd path for testing
+ * @params args.nodeishFs fs implementation override for injecting virtual fs for testing
  * @returns The git origin url or undefined if it could not be found.
  */
-export async function getGitRemotes() {
+export async function getGitRemotes(
+	args: { filepath?: string; nodeishFs?: NodeishFilesystem } = {},
+) {
 	try {
+		const usedFs = args.nodeishFs || fs
+		const root = await raw.findRoot({ fs: usedFs, filepath: args.filepath || process.cwd() })
+
 		const remotes = await raw.listRemotes({
-			fs,
-			dir: await raw.findRoot({ fs, filepath: process.cwd() }),
+			fs: usedFs,
+			dir: root,
 		})
 		return remotes
 	} catch (e) {
