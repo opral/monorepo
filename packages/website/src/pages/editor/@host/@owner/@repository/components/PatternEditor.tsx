dc80341e6fc88730d2025ee0fe92b504eea26c42
NiklasBuchfink <niklas.buchfink@web.de>
2023-08-25 13:23:27 +0200

    fix: save correct empty pattern

--- packages/website/src/pages/editor/@host/@owner/@repository/components/PatternEditor.tsx
+++ packages/website/src/pages/editor/@host/@owner/@repository/components/PatternEditor.tsx
@@ -135,7 +135,6 @@ export function PatternEditor(props: { languageTag: LanguageTag; message: Messag
 	createEffect(
 		on(currentJSON, () => {
 			if (
-				currentJSON().content[0].content !== undefined &&
 				JSON.stringify(currentJSON().content[0].content) !== JSON.stringify(previousContent())
 			) {
 				autoSave()
