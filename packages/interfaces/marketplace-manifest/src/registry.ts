fcbfa11c6e97ff1cc5397819be9deac7dec8a156
flooorianflo <florian.kiem@hfg.design>
2023-09-01 16:36:22 +0200

    feat: more manifests

--- packages/interfaces/marketplace-manifest/src/registry.ts
+++ packages/interfaces/marketplace-manifest/src/registry.ts
@@ -2,8 +2,76 @@
 
 export const registry = [
 	{
-		type: "plugin",
+		type: "app",
 		icon: "https://inlang.com/favicon/safari-pinned-tab.svg",
+		displayName: {
+			en: "CLI",
+		},
+		description: {
+			en: "Official inlang CLI",
+		},
+		linkToReadme: {
+			en: "https://github.com/inlang/inlang/blob/main/source-code/cli/README.md",
+		},
+		keywords: ["cli", "commands"],
+		publisherName: "inlang",
+		publisherIcon: "https://inlang.com/favicon/safari-pinned-tab.svg",
+		license: "Apache-2.0",
+	},
+	{
+		type: "app",
+		icon: "https://inlang.com/favicon/safari-pinned-tab.svg",
+		displayName: {
+			en: "Badge",
+		},
+		description: {
+			en: "Badge showing missing messages and more",
+		},
+		linkToReadme: {
+			en: "https://github.com/inlang/inlang/blob/main/source-code/badge/README.md",
+		},
+		keywords: ["badge"],
+		publisherName: "inlang",
+		publisherIcon: "https://inlang.com/favicon/safari-pinned-tab.svg",
+		license: "Apache-2.0",
+	},
+	{
+		type: "app",
+		icon: "https://inlang.com/favicon/safari-pinned-tab.svg",
+		displayName: {
+			en: "Web Editor",
+		},
+		description: {
+			en: "inlang's Web Editor",
+		},
+		linkToReadme: {
+			en: "https://github.com/inlang/inlang/blob/main/source-code/editor/README.md",
+		},
+		keywords: ["web", "editor", "visual"],
+		publisherName: "inlang",
+		publisherIcon: "https://inlang.com/favicon/safari-pinned-tab.svg",
+		license: "Apache-2.0",
+	},
+	{
+		type: "app",
+		icon: "https://inlang.com/favicon/safari-pinned-tab.svg",
+		displayName: {
+			en: "IDE Extension",
+		},
+		description: {
+			en: "IDE Extension for inlang projects",
+		},
+		linkToReadme: {
+			en: "https://github.com/inlang/inlang/blob/main/source-code/ide-extension/README.md",
+		},
+		keywords: ["ide", "extension", "vscode"],
+		publisherName: "inlang",
+		publisherIcon: "https://inlang.com/favicon/safari-pinned-tab.svg",
+		license: "Apache-2.0",
+	},
+	{
+		type: "plugin",
+		icon: "https://github.com/inlang/inlang/blob/main/source-code/plugins/json/assets/icon.png?raw=true",
 		displayName: {
 			en: "Plugin JSON",
 		},
@@ -18,5 +86,23 @@ export const registry = [
 		publisherIcon: "https://inlang.com/favicon/safari-pinned-tab.svg",
 		license: "Apache-2.0",
 		module: "../../plugins/json/dist/index.js",
+	},
+	{
+		type: "plugin",
+		icon: "https://github.com/inlang/inlang/blob/main/source-code/plugins/i18next/assets/icon.png?raw=true",
+		displayName: {
+			en: "i18next",
+		},
+		description: {
+			en: "i18next plugin for inlang",
+		},
+		linkToReadme: {
+			en: "https://github.com/inlang/inlang/blob/main/source-code/plugins/json/README.md",
+		},
+		keywords: ["i18next", "react", "nextjs"],
+		publisherName: "inlang",
+		publisherIcon: "https://inlang.com/favicon/safari-pinned-tab.svg",
+		license: "Apache-2.0",
+		module: "../../plugins/i18next/dist/index.js",
 	},
 ]
