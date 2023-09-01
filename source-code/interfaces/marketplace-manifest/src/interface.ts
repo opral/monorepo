9021b37f3b87c3e778b92b4e700991c4d8d08588
flooorianflo <florian.kiem@hfg.design>
2023-09-02 00:33:12 +0200

    feat: marketplace schema route

--- source-code/interfaces/marketplace-manifest/src/interface.ts
+++ source-code/interfaces/marketplace-manifest/src/interface.ts
@@ -33,7 +33,89 @@ const MarketplaceModule = Type.Object({
 	module: Type.String(),
 })
 
-export type MarketplaceSchema = Static<typeof MarketplaceSchema>
-export const MarketplaceSchema = Type.Union([MarketplaceApp, MarketplaceLibrary, MarketplaceModule])
+export type MarketplaceItem = Static<typeof MarketplaceItem>
+export const MarketplaceItem = Type.Union([MarketplaceApp, MarketplaceLibrary, MarketplaceModule])
 
-export const marketplaceItems = registry as unknown as MarketplaceSchema[]
+export const marketplaceItems = registry as unknown as MarketplaceItem[]
+
+/* JSON schema used in the manifest files */
+export const MarketplaceSchema = {
+	$schema: "https://json-schema.org/draft/2020-12/schema",
+	$id: "https://inlang.com/marketplace-manifest-schema.json",
+	title: "Inlang Marketplace Manifest",
+	description: "A manifest for inlang marketplace items",
+	type: "object",
+	properties: {
+		type: {
+			description: "The type of marketplace item",
+			type: "string",
+		},
+		icon: {
+			description: "A link to the icon of the marketplace item",
+			type: "string",
+		},
+		displayName: {
+			description: "The name of the marketplace item",
+			type: "object",
+			properties: {
+				en: {
+					description: "The english name of the marketplace item",
+					type: "string",
+				},
+			},
+			required: ["en"],
+		},
+		description: {
+			description: "The description of the marketplace item",
+			type: "object",
+			properties: {
+				en: {
+					description: "The english description of the marketplace item",
+					type: "string",
+				},
+			},
+			required: ["en"],
+		},
+		linkToReadme: {
+			description: "A link to the readme of the marketplace item",
+			type: "object",
+			properties: {
+				en: {
+					description: "A link to the english readme of the marketplace item",
+					type: "string",
+				},
+			},
+			required: ["en"],
+		},
+		keywords: {
+			description: "Keywords to help users find the marketplace item",
+			type: "array",
+			items: {
+				type: "string",
+			},
+			minItems: 1,
+			uniqueItems: true,
+		},
+		publisherName: {
+			description: "The name of the publisher of the marketplace item",
+			type: "string",
+		},
+		publisherIcon: {
+			description: "A link to the icon of the publisher of the marketplace item",
+			type: "string",
+		},
+		license: {
+			description: "The license of the marketplace item",
+			type: "string",
+		},
+	},
+	required: [
+		"type",
+		"displayName",
+		"description",
+		"linkToReadme",
+		"keywords",
+		"publisherName",
+		"license",
+	],
+}
