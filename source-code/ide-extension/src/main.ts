e0c780efec90bf3bf4b539511c9de25e967f15c7
jannesblobel <72493222+jannesblobel@users.noreply.github.com>
2023-09-05 14:52:57 +0200

    chore: adjust msg text

--- source-code/ide-extension/src/main.ts
+++ source-code/ide-extension/src/main.ts
@@ -45,7 +45,7 @@ export async function activate(context: vscode.ExtensionContext): Promise<void> 
 		})
 		// start the ide extension
 		await main({ context, gitOrigin })
-		msg("IDE extension activated", "info")
+		msg("inlang's extension activated", "info")
 	} catch (error) {
 		vscode.window.showErrorMessage((error as Error).message)
 		console.error(error)
