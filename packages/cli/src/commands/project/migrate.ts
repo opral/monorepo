cb6ecce4afa550de1188ccb9adbc491da374be0f
Jan Johannes <jan.johannes@inlang.com>
2023-09-05 13:44:31 +0300

    finalize initial project migrate version

---
+++ packages/cli/src/commands/project/migrate.ts
@@ -0,0 +1,46 @@
+import { Command } from "commander"
+import { log } from "../../utilities/log.js"
+import type { NodeishFilesystem } from "@lix-js/fs"
+import fs from "node:fs/promises"
+import { migrateProjectConfig } from "@inlang/create-project"
+import path from "node:path"
+
+export const migrate = new Command()
+	.command("migrate")
+	.description(
+		"Migrate a legacy inlang.config.js inlang configuration to the new project.inlang.json.",
+	)
+	.action(async () => {
+		await migrateCommandAction({ logger: log, nodeishFs: fs })
+	})
+
+export async function migrateCommandAction(args: {
+	nodeishFs: NodeishFilesystem
+	filepath?: string
+	logger: any
+}) {
+	const inlangConfigFilePath = "./project.inlang.json"
+
+	try {
+		const newProjFileStat = await fs.stat(inlangConfigFilePath)
+		if (newProjFileStat) {
+			args.logger.error("Aborting project intialization: Found existing inlang configuration.")
+		}
+
+		return
+	} catch (error: any) {
+		if (error.code !== "ENOENT") {
+			args.logger.error("unknown read error: " + error)
+			return
+		}
+	}
+
+	const { warnings } = await migrateProjectConfig({
+		nodeishFs: args.nodeishFs,
+		pathJoin: path.join,
+	})
+
+	warnings.forEach((warning) => args.logger.warn(warning))
+
+	args.logger.info(`✅ Successfully created your inlang configuration at: ${inlangConfigFilePath}`)
+}
