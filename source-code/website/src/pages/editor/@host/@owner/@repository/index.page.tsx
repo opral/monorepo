cae9e10d6003d79b33c2fe22751fea626115fcfe
flooorianflo <florian.kiem@hfg.design>
2023-09-06 12:29:59 +0200

    fix: error message styling

--- source-code/website/src/pages/editor/@host/@owner/@repository/index.page.tsx
+++ source-code/website/src/pages/editor/@host/@owner/@repository/index.page.tsx
@@ -140,18 +140,17 @@ function TheActualPage() {
 								{inlang()?.errors().length !== 0 && (
 									<For each={inlang()?.errors()}>
 										{(error) => {
-											console.log(error.cause)
 											return (
 												<li class="pt-2 md:w-[600px]">
 													<div class="bg-danger text-background p-4 rounded-md flex items-center gap-4 mb-8">
-														<Icon name="danger" class="w-7 h-7" />
+														<Icon name="danger" class="w-7 h-7 flex-shrink-0" />
 														<div>
 															<span class="font-semibold">{error.name}: </span>
 															<br />
 															{error?.message}
 														</div>
 													</div>
-													{error.cause.message && (
+													{error.cause && error.cause.message && (
 														<>
 															<p class="text-surface-500 text-sm mb-1">Error cause</p>
 															<div class="font-mono p-4 bg-surface-800 text-background rounded-md text-sm mb-8">
