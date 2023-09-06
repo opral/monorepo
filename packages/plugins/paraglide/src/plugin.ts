ecff7119e1e220b5b5ecb0a38e1b0217360df471
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2023-09-06 11:50:22 +0200

    refactor: remove paraglide plugin settings

--- packages/plugins/paraglide/src/plugin.ts
+++ packages/plugins/paraglide/src/plugin.ts
@@ -1,15 +1,16 @@
+import { validateSdkConfig } from "./api.js"
 import type { Plugin } from "@inlang/plugin"
 import { ideExtensionConfig } from "./ideExtension/config.js"
 
 // ------------------------------------------------------------------------------------------------
 
-export const plugin: Plugin = {
+export const plugin: Plugin<{}> = {
 	meta: {
 		id: "plugin.inlang.paraglide",
 		displayName: { en: "Inlang Paraglide Plugin" },
 		description: { en: "Inlang Paraglide Plugin" },
 	},
-	addCustomApi() {
+	addCustomApi({ settings }) {
 		return {
 			"app.inlang.ideExtension": ideExtensionConfig(),
 		}
