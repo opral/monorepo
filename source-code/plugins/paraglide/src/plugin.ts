ced0619fe43aa3ea5e6a79b0f07bfa8dbadb3f46
Felix Haeberle <kontakt@felixhaeberle.de>
2023-09-06 11:59:26 +0200

    add marketplace-manifest to plugin paraglide

--- source-code/plugins/paraglide/src/plugin.ts
+++ source-code/plugins/paraglide/src/plugin.ts
@@ -1,33 +1,16 @@
-import { type SdkConfigInput, validateSdkConfig } from "./api.js"
 import type { Plugin } from "@inlang/plugin"
 import { ideExtensionConfig } from "./ideExtension/config.js"
 
 // ------------------------------------------------------------------------------------------------
 
-export const plugin: Plugin<SdkConfigInput> = {
+export const plugin: Plugin = {
 	meta: {
 		id: "plugin.inlang.paraglide",
 		displayName: { en: "Inlang Paraglide Plugin" },
 		description: { en: "Inlang Paraglide Plugin" },
-		// marketplace: {
-		// 	keywords: [
-		// 		"inlang",
-		// 		"sdk",
-		// 		"runtime",
-		// 		"plugin",
-		// 		"transform",
-		// 		"javascript",
-		// 		"typescript",
-		// 		"svelte",
-		// 		"sveltekit",
-		// 	],
-		// },
 	},
-	addCustomApi({ settings }) {
-		const parsedConfig = validateSdkConfig(settings)
-
+	addCustomApi() {
 		return {
-			"app.inlang.paraglideJs": parsedConfig,
 			"app.inlang.ideExtension": ideExtensionConfig(),
 		}
 	},
