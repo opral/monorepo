118780770f0c14b28badf666582cba554b88a395
Jan Johannes <jan.johannes@inlang.com>
2023-09-08 18:34:42 +0300

    trigger ci

--- inlang/source-code/cli/src/commands/project/init.test.ts
+++ inlang/source-code/cli/src/commands/project/init.test.ts
@@ -3,6 +3,7 @@ import { describe, it } from "vitest"
 describe("init a new project configuration", () => {
 	it("fails on existing", () => {
 		// placeholder test
+
 	})
 
 	it("creates new inlang config", () => {
