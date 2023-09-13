0202925c8d8a015b24a8508ea5b0df76a20fd4d9
Felix Haeberle <kontakt@felixhaeberle.de>
2023-09-13 18:52:20 +0200

    update linter diagnostics

--- inlang/source-code/ide-extension/src/diagnostics/linterDiagnostics.ts
+++ inlang/source-code/ide-extension/src/diagnostics/linterDiagnostics.ts
@@ -1,11 +1,16 @@
 import * as vscode from "vscode"
 import { state } from "../state.js"
 import type { MessageLintReport } from "@inlang/sdk"
+import { onDidEditMessage } from "../commands/editMessage.js"
 
 export async function linterDiagnostics(args: { context: vscode.ExtensionContext }) {
 	const linterDiagnosticCollection = vscode.languages.createDiagnosticCollection("inlang-lint")
 
 	async function updateLintDiagnostics() {
+		// clean up the diagnostic collection
+		linterDiagnosticCollection.clear()
+
+		// get the active text editor
 		const activeTextEditor = vscode.window.activeTextEditor
 		if (!activeTextEditor) {
 			return
@@ -24,36 +29,45 @@ export async function linterDiagnostics(args: { context: vscode.ExtensionContext
 				documentText: activeTextEditor.document.getText(),
 			})
 			for (const message of messages) {
-				const matchingLintReports =
-					state().inlang.query.messageLintReports.get({
+				state().inlang.query.messageLintReports.get.subscribe(
+					{
 						where: {
 							messageId: message.messageId,
 						},
-					}) || []
+					},
+					(reports) => {
+						if (!reports) {
+							return
+						}
 
-				for (const report of matchingLintReports) {
-					const { level } = report
+						for (const report of reports) {
+							const { level } = report
 
-					const diagnosticRange = new vscode.Range(
-						new vscode.Position(
-							message.position.start.line - 1,
-							message.position.start.character - 1,
-						),
-						new vscode.Position(message.position.end.line - 1, message.position.end.character - 1),
-					)
+							const diagnosticRange = new vscode.Range(
+								new vscode.Position(
+									message.position.start.line - 1,
+									message.position.start.character - 1,
+								),
+								new vscode.Position(
+									message.position.end.line - 1,
+									message.position.end.character - 1,
+								),
+							)
 
-					// Get the lint message for the source language tag or fallback to "en"
+							// Get the lint message for the source language tag or fallback to "en"
 
-					const lintMessage = typeof report.body === "object" ? report.body.en : report.body
+							const lintMessage = typeof report.body === "object" ? report.body.en : report.body
 
-					const diagnostic = new vscode.Diagnostic(
-						diagnosticRange,
-						`[${message.messageId}] – ${lintMessage}`,
-						mapLintLevelToSeverity(level),
-					)
+							const diagnostic = new vscode.Diagnostic(
+								diagnosticRange,
+								`[${message.messageId}] – ${lintMessage}`,
+								mapLintLevelToSeverity(level),
+							)
 
-					diagnostics.push(diagnostic)
-				}
+							diagnostics.push(diagnostic)
+						}
+					},
+				)
 			}
 		})
 
@@ -74,6 +88,11 @@ export async function linterDiagnostics(args: { context: vscode.ExtensionContext
 
 	// in case the active text editor is already open, update lints
 	updateLintDiagnostics()
+
+	// update decorations, when message was edited
+	onDidEditMessage(() => {
+		updateLintDiagnostics()
+	})
 
 	// immediately update lints when the active text editor changes
 	vscode.window.onDidChangeActiveTextEditor(
