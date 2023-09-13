f9cc4b86f2017069846f20ccfdc56170199db04d
flooorianflo <florian.kiem@hfg.design>
2023-09-13 15:03:22 +0200

    fix: rename of displayImage and minor fixes

--- inlang/source-code/versioned-interfaces/marketplace-manifest/src/interface.ts
+++ inlang/source-code/versioned-interfaces/marketplace-manifest/src/interface.ts
@@ -10,7 +10,7 @@ import { ProjectConfig } from "@inlang/project-config"
 const MarketplaceManifestBase = Type.Object({
 	$schema: Type.Optional(Type.Literal("https://inlang.com/schema/marketplace-manifest")),
 	icon: Type.Optional(Type.String()),
-	displayImage: Type.Optional(
+	coverImage: Type.Optional(
 		Type.String({ description: "The cover displayed in the marketplace." }),
 	),
 	displayName: Translatable(
