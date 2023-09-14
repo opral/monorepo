1446ec041ed60fa9a89cfe9cc6d643f6b45206cf
NiklasBuchfink <niklas.buchfink@web.de>
2023-09-14 19:41:32 +0200

    fix: pr button in GitFloat

--- inlang/source-code/website/src/pages/editor/@host/@owner/@repository/components/Gitfloat.tsx
+++ inlang/source-code/website/src/pages/editor/@host/@owner/@repository/components/Gitfloat.tsx
@@ -140,8 +140,7 @@ export const Gitfloat = () => {
 	const pullrequestUrl = () => {
 		return `https://github.com/${
 			githubRepositoryInformation()?.parent?.fullName
-		}/compare/${currentBranch()}...${githubRepositoryInformation()?.name}:${
-			githubRepositoryInformation()?.owner.name
+			}/compare/${currentBranch()}...${localStorage.user?.username}:${githubRepositoryInformation()?.name
 		}:${currentBranch()}?expand=1;title=Update%20translations;body=Describe%20the%20changes%20you%20have%20conducted%20here%0A%0APreview%20the%20messages%20on%20https%3A%2F%2Finlang.com%2Fgithub.com%2F${
 			(currentPageContext.routeParams as EditorRouteParams).owner
 		}%2F${(currentPageContext.routeParams as EditorRouteParams).repository}%20.`
