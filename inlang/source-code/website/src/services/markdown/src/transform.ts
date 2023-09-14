a99c14eef106c86dffc85db72993ea51d24ebe6e
flooorianflo <florian.kiem@hfg.design>
2023-09-14 11:45:40 +0200

    feat: render github markdown

--- inlang/source-code/website/src/services/markdown/src/transform.ts
+++ inlang/source-code/website/src/services/markdown/src/transform.ts
@@ -1,7 +1,16 @@
-import { remark } from "remark"
+import { unified } from "unified"
+import remarkParse from "remark-parse"
 import remarkRehype from "remark-rehype"
+import remarkGfm from "remark-gfm"
+import rehypeStringify from "rehype-stringify"
 
 export async function convert(markdown: string): Promise<string> {
-	const content = await remark().use(remarkRehype).process(markdown)
-	return content.toString()
+	const content = await unified()
+		.use(remarkParse)
+		.use(remarkGfm)
+		.use(remarkRehype)
+		.use(rehypeStringify)
+		.process(markdown)
+
+	return String(content)
 }
