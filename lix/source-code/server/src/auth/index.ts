72da1fb5bc44a5349bb57c4279ea0b06eba1da2f
Jan Johannes <jan.johannes@inlang.com>
2023-09-19 17:07:06 +0300

    remove unused files

--- lix/source-code/server/src/auth/index.ts
+++
@@ -1,4 +0,0 @@
-export { SignInDialog } from "./src/components/SignInDialog.jsx"
-export { onSignOut } from "./src/onSignOut.js"
-// a telefunc automatically runs on the server, thus can be used on the client
-export { getUserInfo } from "./src/implementation.telefunc.js"
