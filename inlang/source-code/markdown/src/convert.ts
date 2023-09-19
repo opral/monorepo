fc58abec40026e8761ead1e8b9951b485c8380cc
flooorianflo <florian.kiem@hfg.design>
2023-09-19 11:21:55 +0200

    fix: styling and whitelabel

--- inlang/source-code/markdown/src/convert.ts
+++ inlang/source-code/markdown/src/convert.ts
@@ -3,13 +3,12 @@ import remarkParse from "remark-parse"
 import remarkRehype from "remark-rehype"
 import rehypeRaw from "rehype-raw"
 import rehypeStringify from "rehype-stringify"
-import rehypeSanitize from "rehype-sanitize"
+import rehypeSanitize, { defaultSchema } from "rehype-sanitize"
 import rehypeSlug from "rehype-slug"
 import rehypeHighlight from "rehype-highlight"
 import rehypeAutolinkHeadings from "rehype-autolink-headings"
 import addClasses from "rehype-class-names"
 import { rehypeAccessibleEmojis } from "rehype-accessible-emojis"
-import defaultTags from "./default-tags.json"
 
 /* Converts the markdown with remark and the html with rehype to be suitable for being rendered */
 export async function convert(markdown: string): Promise<string> {
@@ -21,30 +20,35 @@ export async function convert(markdown: string): Promise<string> {
 		/* @ts-ignore */
 		.use(rehypeRaw)
 		/* @ts-ignore */
-		// .use(rehypeSanitize, {
-		// 	tagNames: ["doc-figure", "quick-link", ...defaultTags],
-		// })
+		.use(rehypeSanitize, {
+			tagNames: ["doc-figure", "quick-link", "quick-links", ...defaultSchema.tagNames!],
+			attributes: {
+				"doc-figure": ["src", "alt", "caption"],
+				"quick-link": ["href", "description", "title", "icon"],
+				...defaultSchema.attributes,
+			},
+		})
 		.use(rehypeHighlight)
 		.use(rehypeSlug)
 		/* @ts-ignore */
 		.use(addClasses, {
-			"h1,h2,h3,h4,h5,h6": "font-semibold leading-relaxed my-5 cursor-pointer",
-			h1: "text-3xl",
-			h2: "text-2xl",
-			h3: "text-xl",
-			h4: "text-lg",
-			h5: "text-lg",
-			h6: "text-base",
-			p: "text-base text-surface-600 my-1 leading-relaxed",
-			a: "text-primary font-medium hover:text-hover-primary",
-			code: "p-1 bg-surface-100 rounded text-sm font-mono text-surface-700",
-			ul: "list-disc list-inside",
-			ol: "list-decimal list-inside",
-			li: "my-2",
-			table: "table-auto w-full my-6",
-			th: "p-2 rounded text-sm text-surface-700 font-medium",
-			td: "p-2 leading-7",
-			hr: "my-4 border-b border-surface-200",
+			"h1,h2,h3,h4,h5,h6": "im-font-semibold im-leading-relaxed im-my-5 im-cursor-pointer",
+			h1: "im-text-3xl",
+			h2: "im-text-2xl",
+			h3: "im-text-xl",
+			h4: "im-text-lg",
+			h5: "im-text-lg",
+			h6: "im-text-base",
+			p: "im-text-base im-text-surface-600 im-my-1 im-leading-relaxed",
+			a: "im-text-primary im-font-medium hover:im-text-hover-primary",
+			code: "im-p-1 im-bg-surface-100 im-rounded im-text-sm im-font-mono im-text-surface-700",
+			ul: "im-list-disc im-list-inside",
+			ol: "im-list-decimal im-list-inside",
+			li: "im-my-2",
+			table: "im-table-auto im-w-full im-my-6",
+			th: "im-p-2 im-rounded im-text-sm im-text-surface-700 im-font-medium",
+			td: "im-p-2 im-leading-7",
+			hr: "im-my-4 im-border-b im-border-surface-200",
 		})
 		.use(rehypeAutolinkHeadings)
 		.use(rehypeAccessibleEmojis)
--- inlang/source-code/markdown/tailwind.config.js
+++ inlang/source-code/markdown/tailwind.config.js
@@ -5,4 +5,5 @@ export default {
 		extend: {},
 	},
 	plugins: [],
+	prefix: "im-",
 }
