21de9a8fe89a232b598d824c188d269a31dcc637
NiklasBuchfink <niklas.buchfink@web.de>
2023-09-21 09:33:29 +0200

    fix: make plugin undefined for PluginsDoNotProvideLoadOrSaveMessagesError

--- inlang/source-code/sdk/src/resolve-modules/plugins/resolvePlugins.ts
+++ inlang/source-code/sdk/src/resolve-modules/plugins/resolvePlugins.ts
@@ -164,7 +164,7 @@ export const resolvePlugins: ResolvePluginsFunction = async (args) => {
 		result.errors.push(
 			new PluginsDoNotProvideLoadOrSaveMessagesError(
 				"It seems you did not install any plugin that handles messages. Please add one to make inlang work. See https://inlang.com/documentation/plugins/registry.",
-				{ plugin: "plugin.inlang.missing" },
+				{ plugin: undefined },
 			),
 		)
 	}
