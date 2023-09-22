9d977df501abfafd05e2d7191d9d15f1e671c4cb
Felix Haeberle <kontakt@felixhaeberle.de>
2023-09-22 17:52:21 +0200

    use other type format

--- inlang/source-code/versioned-interfaces/language-tag/src/interface.ts
+++ inlang/source-code/versioned-interfaces/language-tag/src/interface.ts
@@ -9,8 +9,10 @@ import { Type, type Static } from "@sinclair/typebox"
 export type LanguageTag = Static<typeof LanguageTag>
 /**
  * Follows the IETF BCP 47 language tag schema with modifications.
- * @see REAMDE.md file for more information.
+ * @see REAMDE.md file for more information on the validation.
  */
-export const LanguageTag = Type.RegExp(
-	/^((?<grandfathered>(en-GB-oed|i-ami|i-bnn|i-default|i-enochian|i-hak|i-klingon|i-lux|i-mingo|i-navajo|i-pwn|i-tao|i-tay|i-tsu|sgn-BE-FR|sgn-BE-NL|sgn-CH-DE)|(art-lojban|cel-gaulish|no-bok|no-nyn|zh-guoyu|zh-hakka|zh-min|zh-min-nan|zh-xiang))|((?<language>([A-Za-z]{2,3}(-(?<extlang>[A-Za-z]{3}(-[A-Za-z]{3}){0,2}))?))(-(?<script>[A-Za-z]{4}))?(-(?<region>[A-Za-z]{2}|[0-9]{3}))?(-(?<variant>[A-Za-z0-9]{5,8}|[0-9][A-Za-z0-9]{3}))*))$/,
-)
+export const LanguageTag = Type.String({
+	pattern:
+		"^((?<grandfathered>(en-GB-oed|i-ami|i-bnn|i-default|i-enochian|i-hak|i-klingon|i-lux|i-mingo|i-navajo|i-pwn|i-tao|i-tay|i-tsu|sgn-BE-FR|sgn-BE-NL|sgn-CH-DE)|(art-lojban|cel-gaulish|no-bok|no-nyn|zh-guoyu|zh-hakka|zh-min|zh-min-nan|zh-xiang))|((?<language>([A-Za-z]{2,3}(-(?<extlang>[A-Za-z]{3}(-[A-Za-z]{3}){0,2}))?))(-(?<script>[A-Za-z]{4}))?(-(?<region>[A-Za-z]{2}|[0-9]{3}))?(-(?<variant>[A-Za-z0-9]{5,8}|[0-9][A-Za-z0-9]{3}))*))$",
+	examples: ["en", "de", "en-US", "zh-Hans", "es-417"],
+})
