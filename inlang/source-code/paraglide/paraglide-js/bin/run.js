fafcac84e5d16c1cc082cecc2e12391c326b82cd
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2023-09-24 14:12:42 +0200

    add readme

---
+++ inlang/source-code/paraglide/paraglide-js/bin/run.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+
+// the comment above signals unix environments which interpreter
+// to use when executing this file (node)
+
+import "./dist/cli/main.js"
---
+++ inlang/source-code/paraglide/paraglide-js/example/src/main.js
@@ -0,0 +1,3 @@
+import { name } from "@inlang/paraglide-js"
+
+console.log(name)
