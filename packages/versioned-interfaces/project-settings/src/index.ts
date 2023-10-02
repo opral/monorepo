57661a0d77084d8c9f9d9aa8ca2250f8172183fd
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2023-10-02 18:20:38 +0200

    add: expose external project settings to ease type definitions

--- packages/versioned-interfaces/project-settings/src/index.ts
+++ packages/versioned-interfaces/project-settings/src/index.ts
@@ -1,5 +1,6 @@
 export {
 	ProjectSettings,
+	ExternalProjectSettings,
 	// the types beneath are exported to avoid circular dependencies.
 	// use the types from the corresponding package directly instead.
 	_MessageLintRuleId,
