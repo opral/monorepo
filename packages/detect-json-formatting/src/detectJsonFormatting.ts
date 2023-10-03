bd26da1fc5ab8dc9008857e9df25c859f574cdab
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2023-10-03 19:08:17 +0200

    add: detect spacing of array files
    Using a dependency was simpler than an own implementation. Unfortunately, the dependency does not append \n. Hence, our own package is still required.

--- packages/detect-json-formatting/src/detectJsonFormatting.ts
+++ packages/detect-json-formatting/src/detectJsonFormatting.ts
@@ -1,3 +1,5 @@
+import guessJsonSpacing from "guess-json-indent"
+
 /**
  * Detects the formatting of a JSON file and returns a function
  * that can be used to stringify JSON with the same formatting.
@@ -14,52 +16,8 @@ export const detectJsonFormatting = (
 	replacer?: Parameters<typeof JSON.stringify>[1]
 	// space is provided by the function
 ) => string) => {
-	let endsWithNewLine: boolean | undefined
-	let spacing: string | number | undefined
-
-	// check end with new line
-	if (file.endsWith("\n")) {
-		endsWithNewLine = true
-	}
-
-	//check spacing
-	const patterns = [
-		{
-			spacing: 1,
-			regex: /^{\n {1}[^ ]+.*$/m,
-		},
-		{
-			spacing: 2,
-			regex: /^{\n {2}[^ ]+.*$/m,
-		},
-		{
-			spacing: 3,
-			regex: /^{\n {3}[^ ]+.*$/m,
-		},
-		{
-			spacing: 4,
-
-			regex: /^{\n {4}[^ ]+.*$/m,
-		},
-		{
-			spacing: 6,
-			regex: /^{\n {6}[^ ]+.*$/m,
-		},
-		{
-			spacing: 8,
-			regex: /^{\n {8}[^ ]+.*$/m,
-		},
-		{
-			spacing: "\t",
-			regex: /^{\n\t[^ ]+.*$/m,
-		},
-	]
-
-	for (const value of patterns) {
-		if (value.regex.test(file)) {
-			spacing = value.spacing
-		}
-	}
+	const endsWithNewLine = file.endsWith("\n")
+	const spacing = guessJsonSpacing(file)
 
 	return (value, replacer) =>
 		JSON.stringify(value, replacer, spacing) + (endsWithNewLine ? "\n" : "")
