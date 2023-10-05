594406e1c1b412763d8ce0a0232b4401d201cd55
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2023-10-05 12:32:02 +0200

    first working version

--- inlang/source-code/paraglide/paraglide-js/example/src/main.js
+++ inlang/source-code/paraglide/paraglide-js/example/src/main.js
@@ -1,3 +1,12 @@
-import { name } from "@inlang/paraglide-js"
+/* eslint-disable no-undef */
+/* eslint-disable no-console */
+import * as m from "@inlang/paraglide-js/messages"
+import { setLanguageTag } from "@inlang/paraglide-js"
 
-console.log(name)
+setLanguageTag("en")
+
+console.log(m.greeting({ name: "Samuel", count: 5 }))
+
+setLanguageTag("de")
+
+console.log(m.greeting({ name: "Samuel", count: 5 }))
--- inlang/source-code/paraglide/paraglide-js/src/cli/cli.ts
+++ inlang/source-code/paraglide/paraglide-js/src/cli/cli.ts
@@ -1,47 +1,118 @@
 import consola from "consola"
 import minimist from "minimist"
-import { type compile } from "../compiler/compile.js"
+import { loadProject, type InlangProject } from "@inlang/sdk"
 import dedent from "dedent"
-import { loadProject } from "@inlang/sdk"
 import fs from "node:fs/promises"
+import type { CompileFunction } from "./types.js"
+import { fileURLToPath } from "node:url"
+
+/**
+ * The path this code is executed from.
+ */
+const metaUrlPath = fileURLToPath(import.meta.url)
+
+/**
+ * The path to the package directory.
+ *
+ * slices a path
+ * from '/Users/samuel/example/repository/node_modules/paraglide-js/dist/cli/main.js'
+ * to   '/Users/samuel/example/repository/node_modules/paraglide-js'
+ */
+const packageDirectory = metaUrlPath.slice(0, metaUrlPath.indexOf("/dist/"))
 
 /**
  * The CLI entry point.
  *
  * Pass the compile function for framework specific adjustments.
  */
-export const cli = async (args: { compile: typeof compile; name: string }) => {
+export const cli = async (args: { compile: CompileFunction; name: string }) => {
+	// ------------------- VALIDATE IF RUNNING FROM CORRECT FOLDER -------------------
+	// the CLI expects to be running from the dist folder of the specific paraglide package
+	// to compile the output in the correct directory
+	if (metaUrlPath.includes(`${args.name}/dist/`) === false) {
+		consola.error(
+			dedent`
+			The CLI is not running from the dist folder. 
+
+			This is likely an internal bug. Please file an issue at https://github.com/inlang/monorepo/issues.
+
+			Debug information: 
+
+			- name: ${args.name}
+			- metaUrlPath: ${metaUrlPath}
+			`
+		)
+		process.exit(1)
+	}
+
+	const usage = dedent`
+	Available commands: 
+
+	compile - Compiles an inlang project into the importable paraglide-js library. 
+	`
+
 	// the first two arguments are the node binary and the script path
 	const argv = minimist(process.argv.slice(2))
 
-	const usage = dedent`
-    Usage: ${args.name} compile <project-path>
-    Example: ${args.name} compile ./project.inlang.json
-  `
-
 	const commandName = argv._[0]
-	const projectPath = argv._[1]
 
-	if (commandName !== "compile") {
-		consola.error(`Unknown command "${commandName}"\n\n${usage}`)
-		process.exit(1)
-	} else if (projectPath === undefined) {
+	switch (commandName) {
+		case "compile":
+			return runCompileCommand({ compile: args.compile, name: args.name, argv })
+		default:
+			return (() => {
+				consola.error(`Unknown command "${commandName}"\n\n${usage}`)
+				process.exit(1)
+			})()
+	}
+}
+
+const runCompileCommand = async (args: {
+	argv: minimist.ParsedArgs
+	name: string
+	compile: CompileFunction
+}) => {
+	const usage = dedent`
+	Usage: ${args.name} compile <project-path>
+	Example: ${args.name} compile ./project.inlang.json
+	`
+
+	const projectPath = args.argv._[1]
+
+	if (projectPath === undefined) {
 		consola.error(`No project path specified.\n\n${usage}`)
 		process.exit(1)
 	}
 
 	consola.info(`Compiling inlang project at "${projectPath}".`)
 
-	const project = await loadProject({ settingsFilePath: projectPath, nodeishFs: fs })
+	const project = exitIfErrors(await loadProject({ settingsFilePath: projectPath, nodeishFs: fs }))
 
+	const output = args.compile({
+		messages: project.query.messages.getAll(),
+		settings: project.settings(),
+	})
+
+	for (const [fileName, fileContent] of Object.entries(output)) {
+		await fs.writeFile(`${packageDirectory}/dist/compiled-output/${fileName}`, fileContent, {
+			encoding: "utf-8",
+		})
+	}
+
+	consola.success("Successfully compiled the project.")
+}
+
+/**
+ * Utility function to exit when the project has errors.
+ */
+const exitIfErrors = (project: InlangProject) => {
 	if (project.errors().length > 0) {
 		consola.error(dedent`
-    The project "${argv.project}" has errors:
+    The project at has errors:
 
     ${project.errors().join("\n")}
     `)
 		process.exit(1)
 	}
-
-	consola.success("Successfully compiled the project.")
+	return project
 }
