7030abae59bd35cb4879c0f0d550f4042b409ac0
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2023-10-05 13:32:29 +0200

    chore: update test

--- packages/paraglide/paraglide-js/examples/js.test.ts
+++ packages/paraglide/paraglide-js/examples/js.test.ts
@@ -9,6 +9,6 @@ test("should log the messages in the correct language tag", async () => {
 	const consoleMock = vi.spyOn(console, "log").mockImplementation(() => {})
 	await import("./js/src/main.js")
 	expect(consoleMock).toHaveBeenCalledTimes(2)
-	expect(consoleMock.mock.calls[0][0]).toBe("Hello Samuel! You have 5 messages.")
+	expect(consoleMock.mock.calls[0][0]).toBe("Welcome Samuel! You have 5 messages.")
 	expect(consoleMock.mock.calls[1][0]).toBe("Hallo Samuel! Du hast 5 Nachrichten.")
 })
