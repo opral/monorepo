5fcbbe2b8bd23b1c14d9251d9b4d02aad473962b
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2023-10-05 14:19:36 +0200

    refactor: share messages across all examples for easier testing

--- packages/paraglide/paraglide-js/examples/js/src/main.js
+++ packages/paraglide/paraglide-js/examples/js/src/main.js
@@ -1,12 +1,14 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-console */
 import * as m from "@inlang/paraglide-js/messages"
-import { setLanguageTag } from "@inlang/paraglide-js"
+import { languageTag, setLanguageTag } from "@inlang/paraglide-js"
 
 setLanguageTag("en")
 
+console.log(m.currentLanguageTag({ languageTag: languageTag() }))
 console.log(m.greeting({ name: "Samuel", count: 5 }))
 
 setLanguageTag("de")
 
+console.log(m.currentLanguageTag({ languageTag: languageTag() }))
 console.log(m.greeting({ name: "Samuel", count: 5 }))
