d0c7a23b5e5b876bd2d0f3768a9dc5a0f4e6fcb3
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2023-10-05 18:42:58 +0200

    refactor: unbundle paraglide
    cc @felixhaeberle :)

--- inlang/source-code/paraglide/paraglide-js/examples/svelte/src/App.svelte
+++
@@ -1,6 +0,0 @@
-<script>
-  import { languageTag } from '@inlang/paraglide-js';
-</script>
-
-<button>Current language is {languageTag()}</button>
-
---
+++ inlang/source-code/paraglide/paraglide-svelte/bin/run.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+// the comment above signals unix environments which interpreter
+// to use when executing this file (node)
+
+import { cli } from "@inlang/paraglide-js"
+import { compile } from "../dist/compiler/compile.js"
+
+cli({ compile, name: "paraglide-js" })
