09c89379ea3908cef2aed60db1bffa862303f283
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2023-10-06 19:59:33 +0200

    add: first version of the provider

--- inlang/source-code/paraglide/paraglide-js-svelte/example/vite.config.js
+++ inlang/source-code/paraglide/paraglide-js-svelte/example/vite.config.js
@@ -3,4 +3,8 @@ import { svelte } from "@sveltejs/vite-plugin-svelte"
 
 export default defineConfig({
 	plugins: [svelte()],
+	build: {
+		// for easier debugging of e2e tests, don't minify
+		minify: false,
+	},
 })
