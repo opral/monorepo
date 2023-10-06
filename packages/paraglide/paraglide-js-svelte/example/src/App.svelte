638e39f98186d4df447122001565ed08a21e8767
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2023-10-06 15:18:14 +0200

    refactor: rename package to paraglide-js-svelte

---
+++ packages/paraglide/paraglide-js-svelte/example/src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+  import { changeLanguageTag, languageTag } from '@inlang/paraglide-js';
+  import * as m from "@inlang/paraglide-js/messages"
+</script>
+
+<p>{m.greeting({ name: "Samuel", count: 5 })}</p>
+
+<p>{m.currentLanguageTag({languageTag})}</p>
+
+<button on:click={() => changeLanguageTag("de")}>change language to "de"</button>
+<button on:click={() => changeLanguageTag("en")}>change language to "en"</button>
--- packages/paraglide/paraglide-svelte/bin/run.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-
-// the comment above signals unix environments which interpreter
-// to use when executing this file (node)
-
-import { cli } from "@inlang/paraglide-js"
-import { compile } from "../dist/compiler/compile.js"
-
-cli({ compile, name: "paraglide-svelte" })
