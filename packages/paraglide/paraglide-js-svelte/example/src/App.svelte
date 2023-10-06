27415551e0b6b1c253798e8b885b3ae6449a7266
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2023-10-06 19:59:33 +0200

    add: first version of the provider

--- packages/paraglide/paraglide-js-svelte/example/src/App.svelte
+++ packages/paraglide/paraglide-js-svelte/example/src/App.svelte
@@ -1,11 +1,14 @@
 <script>
   import { changeLanguageTag, languageTag } from '@inlang/paraglide-js';
   import * as m from "@inlang/paraglide-js/messages"
+	import Layout from './ParaglideJsProvider.svelte'
 </script>
 
-<p>{m.greeting({ name: "Samuel", count: 5 })}</p>
+<Layout>
 
-<p>{m.currentLanguageTag({languageTag})}</p>
+  <p>{m.greeting({ name: "Samuel", count: 5 })}</p>
+  <p>{m.currentLanguageTag({languageTag})}</p>
+  <button on:click={() => changeLanguageTag("de")}>change language to "de"</button>
+  <button on:click={() => changeLanguageTag("en")}>change language to "en"</button>
 
-<button on:click={() => changeLanguageTag("de")}>change language to "de"</button>
-<button on:click={() => changeLanguageTag("en")}>change language to "en"</button>
+</Layout>
---
+++ packages/paraglide/paraglide-js-svelte/example/src/ParaglideJsProvider.svelte
@@ -0,0 +1,14 @@
+<script>
+  import { languageTag, onChangeLanguageTag } from '@inlang/paraglide-js';
+
+  // initialize the language tag
+  $: _languageTag = languageTag;
+
+  onChangeLanguageTag((newLanguageTag) => {
+    _languageTag = newLanguageTag;
+  });
+</script>
+
+{#key _languageTag}
+  <slot></slot>
+{/key}
