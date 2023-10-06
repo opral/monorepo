259eb3942aaa7e52ced6cba942a89fc8c6cd2c26
flooorianflo <florian.kiem@hfg.design>
2023-10-06 17:18:56 +0200

    feat: implement marketplace section

--- inlang/source-code/website/src/pages/index/sections/02-marketplace/index.tsx
+++ inlang/source-code/website/src/pages/index/sections/02-marketplace/index.tsx
@@ -1,22 +1,13 @@
 import { Button } from "../../components/Button.jsx"
-import { CardTag } from "../../components/CardTag.jsx"
 import { SectionLayout } from "../../components/sectionLayout.jsx"
-import AppFlowy from "./assets/logos/appflowy.jsx"
-import Calcom from "./assets/logos/clacom.jsx"
-import Jitsi from "./assets/logos/jitsi.jsx"
-import Listmonk from "./assets/logos/listmonk.jsx"
-import OpenAssistant from "./assets/logos/openAssistant.jsx"
-import App from "./assets/categories/app.jsx"
-import Email from "./assets/categories/email.jsx"
-import Payments from "./assets/categories/payments.jsx"
-import Website from "./assets/categories/website.jsx"
-import Documents from "./assets/categories/documents.jsx"
+import ItemsList from "./assets/itemslist.jsx"
+import MissingPart from "./assets/missingpart.jsx"
 
 const Marketplace = () => {
 	return (
 		<>
 			<SectionLayout showLines={true}>
-				<div class="h-screen">
+				<div class="mb-24">
 					<div class="relative w-full pb-2">
 						<img src="/images/landingpage/marketplace_apps.png" class="w-full max-w-5xl mx-auto" />
 						<div class="absolute inset-0 z-10 bg-gradient-to-t from-surface-100/0 via-surface-100/0 via-70% to-surface-100/70 mix-blend-lighten" />
@@ -32,6 +23,46 @@ const Marketplace = () => {
 						<Button type="primary" href="/marketplace">
 							Browse Marketplace
 						</Button>
+					</div>
+				</div>
+				<div class="w-full flex pt-4 flex-col xl:flex-row mb-16">
+					<div class="w-full xl:w-1/2 flex flex-col gap-8 px-6 md:px-10 xl:py-16 pt-20 py-6">
+						<div class="flex flex-col items-start">
+							<div class="h-80 w-full rounded-xl bg-surface-100 mb-12 border border-surface-200 relative overflow-hidden">
+								<div class="absolute left-1/2 -translate-x-1/2 -bottom-1">
+									<ItemsList />
+								</div>
+							</div>
+							<div class="rounded-full text-sm bg-primary/20 text-primary px-3 py-0.5 mb-4">
+								Marketplace
+							</div>
+							<h2 class="text-xl font-semibold mb-4">Everything you need in one place</h2>
+							<p>
+								The marketplace provides you with the toolbox you need. With the wide range of
+								modules you can solve every globalization problem you have. Use libraries to build
+								on, plugins to enable features and rules to ensure and automate quality.
+							</p>
+						</div>
+					</div>
+					<div class="w-full xl:w-1/2 flex flex-col gap-8 px-6 md:px-10 xl:py-16 pt-20 py-6">
+						<div class="flex flex-col items-start">
+							<div class="h-80 w-full rounded-xl bg-surface-100 mb-12 border border-surface-200 relative overflow-hidden">
+								<div class="absolute left-1/2 -translate-x-1/2 -bottom-1 w-full">
+									<MissingPart />
+								</div>
+							</div>
+							<div class="rounded-full text-sm bg-primary/20 text-primary px-3 py-0.5 mb-4">
+								Custom modules
+							</div>
+							<h2 class="text-xl font-semibold mb-4">Haven’t found the missing part?</h2>
+							<p class="mb-6">
+								Extend the ecosystem by developing what's missing. You can develop your own app,
+								plugins or rules and publish them to the marketplace.
+							</p>
+							<Button type="text" href="/documentation" chevron>
+								inlang for Developers
+							</Button>
+						</div>
 					</div>
 				</div>
 			</SectionLayout>
