6789ce784f9bfae244f7198176fb42769d504acd
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2023-10-08 16:39:34 +0200

    chore: rename component

--- inlang/source-code/paraglide/paraglide-js-svelte/example/src/App.svelte
+++ inlang/source-code/paraglide/paraglide-js-svelte/example/src/App.svelte
@@ -1,14 +1,14 @@
 <script>
   import { setLanguageTag, languageTag } from '@inlang/paraglide-js';
   import * as m from "@inlang/paraglide-js/messages"
-	import Layout from './ParaglideJsProvider.svelte'
+	import ParaglideSvelte from './ParaglideJsProvider.svelte'
 </script>
 
-<Layout>
+<ParaglideSvelte>
 
   <p>{m.greeting({ name: "Samuel", count: 5 })}</p>
   <p>{m.currentLanguageTag({languageTag})}</p>
   <button on:click={() => setLanguageTag("de")}>change language to "de"</button>
   <button on:click={() => setLanguageTag("en")}>change language to "en"</button>
 
-</Layout>
+</ParaglideSvelte>
