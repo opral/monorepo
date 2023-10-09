c3961dcf0502ef0da2022214d3d9a46fa37e74c0
flooorianflo <florian.kiem@hfg.design>
2023-10-09 12:20:53 +0200

    feat: implement solid slider

--- inlang/source-code/website/src/pages/index/sections/01-hero/index.tsx
+++ inlang/source-code/website/src/pages/index/sections/01-hero/index.tsx
@@ -11,6 +11,7 @@ import Email from "./assets/categories/email.jsx"
 import Payments from "./assets/categories/payments.jsx"
 import Website from "./assets/categories/website.jsx"
 import Documents from "./assets/categories/documents.jsx"
+import CategorieSlider from "./Slider.jsx"
 
 const Hero = () => {
 	return (
@@ -79,73 +80,7 @@ const Hero = () => {
 						</div>
 					</div>
 				</div>
-				<div class="flex items-center justify-center gap-4 overflow-hidden relative pl-24">
-					<a
-						href="/documents"
-						class="w-96 h-80 bg-[#043855] rounded-3xl flex-shrink-0 relative group border border-primary/0 hover:border-background/20 transition-all"
-					>
-						<div class="absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 z-10 overflow-hidden">
-							<Documents />
-						</div>
-						{/* Used for making blurry hover effects possible  */}
-						<div class="absolute inset-0 blur-3xl transition-all opacity-0 group-hover:opacity-25">
-							<Documents />
-						</div>
-						<CardTag text="Documents" globalPrefix />
-					</a>
-					<a
-						href="/app"
-						class="w-[512px] h-80 bg-surface-800 rounded-3xl flex-shrink-0 relative overflow-hidden group border border-primary/0 hover:border-background/20 transition-all"
-					>
-						<div class="absolute -right-4 bottom-0 z-10">
-							<App />
-						</div>
-						{/* Used for making blurry hover effects possible  */}
-						<div class="absolute inset-0 transition-all blur-md opacity-0 group-hover:opacity-100">
-							<App />
-						</div>
-						<CardTag text="App" globalPrefix />
-					</a>
-					<a
-						href="/email"
-						class="w-96 h-80 bg-[#043855] rounded-3xl flex-shrink-0 relative overflow-hidden group border border-primary/0 hover:border-background/20 transition-all"
-					>
-						<div class="absolute right-0 bottom-0 z-10">
-							<Email />
-						</div>
-						{/* Used for making blurry hover effects possible  */}
-						<div class="absolute inset-0 transition-all blur-2xl opacity-0 group-hover:opacity-25">
-							<Email />
-						</div>
-						<CardTag text="Email" globalPrefix />
-					</a>
-					<a
-						href="/payments"
-						class="w-96 h-80 bg-surface-700 rounded-3xl flex-shrink-0 relative overflow-hidden group border border-primary/0 hover:border-background/20 transition-all"
-					>
-						<div class="absolute left-1/2 -translate-x-1/2 bottom-0 z-10">
-							<Payments />
-						</div>
-						{/* Used for making blurry hover effects possible  */}
-						<div class="absolute inset-0 transition-all blur-2xl opacity-0 group-hover:opacity-25">
-							<Payments />
-						</div>
-						<CardTag text="Payments" globalPrefix />
-					</a>
-					<a
-						href="/website"
-						class="w-[512px] h-80 bg-surface-800 rounded-3xl flex-shrink-0 relative group overflow-hidden border border-primary/0 hover:border-background/20 transition-all"
-					>
-						<div class="absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 z-10">
-							<Website />
-						</div>
-						{/* Used for making blurry hover effects possible  */}
-						<div class="absolute inset-0 blur-3xl transition-all opacity-0 group-hover:opacity-25">
-							<Website />
-						</div>
-						<CardTag text="Website" globalPrefix />
-					</a>
-				</div>
+				<CategorieSlider />
 			</div>
 			<SectionLayout showLines={true} type="dark">
 				<div class="w-full flex pt-4 md:pt-16 flex-col xl:flex-row py-32">
