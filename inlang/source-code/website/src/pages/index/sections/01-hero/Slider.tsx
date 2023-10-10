69572d26893bf8949d8a1041a6a28a188860a4c1
flooorianflo <florian.kiem@hfg.design>
2023-10-10 14:31:24 +0200

    chore: accessibility stylings

--- inlang/source-code/website/src/pages/index/sections/01-hero/Slider.tsx
+++ inlang/source-code/website/src/pages/index/sections/01-hero/Slider.tsx
@@ -48,11 +48,11 @@ export default function CategorieSlider() {
 
 	return (
 		<>
-			<div use:slider class="xl:pl-64">
+			<div use:slider class="xl:pl-64 cursor-grab active:cursor-grabbing">
 				<div class="flex justify-center items-center md:px-0 px-3">
 					<a
 						href="/documents"
-						class="max-w-sm w-full h-80 bg-[#043855] rounded-3xl flex-shrink-0 relative group border border-primary/0 hover:border-background/20 transition-all"
+						class="active:cursor-grabbing max-w-sm w-full h-80 bg-[#043855] rounded-3xl flex-shrink-0 relative group border border-primary/0 hover:border-background/20 transition-all"
 					>
 						<div class="absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 z-10 overflow-hidden">
 							<Documents />
@@ -67,7 +67,7 @@ export default function CategorieSlider() {
 				<div class="flex justify-center items-center md:px-0 px-3">
 					<a
 						href="/app"
-						class="lg:w-[512px] max-w-lg w-full h-80 bg-surface-800 rounded-3xl flex-shrink-0 relative overflow-hidden group border border-primary/0 hover:border-background/20 transition-all"
+						class="active:cursor-grabbing lg:w-[512px] max-w-lg w-full h-80 bg-surface-800 rounded-3xl flex-shrink-0 relative overflow-hidden group border border-primary/0 hover:border-background/20 transition-all"
 					>
 						<div class="absolute -right-4 bottom-0 z-10">
 							<App />
@@ -82,7 +82,7 @@ export default function CategorieSlider() {
 				<div class="flex justify-center items-center md:px-0 px-3">
 					<a
 						href="/email"
-						class="max-w-sm w-full h-80 bg-[#043855] rounded-3xl flex-shrink-0 relative overflow-hidden group border border-primary/0 hover:border-background/20 transition-all"
+						class="active:cursor-grabbing max-w-sm w-full h-80 bg-[#043855] rounded-3xl flex-shrink-0 relative overflow-hidden group border border-primary/0 hover:border-background/20 transition-all"
 					>
 						<div class="absolute right-0 bottom-0 z-10">
 							<Email />
@@ -97,7 +97,7 @@ export default function CategorieSlider() {
 				<div class="flex justify-center items-center md:px-0 px-3">
 					<a
 						href="/payments"
-						class="max-w-sm w-full h-80 bg-surface-700 rounded-3xl flex-shrink-0 relative overflow-hidden group border border-primary/0 hover:border-background/20 transition-all"
+						class="active:cursor-grabbing max-w-sm w-full h-80 bg-surface-700 rounded-3xl flex-shrink-0 relative overflow-hidden group border border-primary/0 hover:border-background/20 transition-all"
 					>
 						<div class="absolute left-1/2 -translate-x-1/2 bottom-0 z-10">
 							<Payments />
@@ -112,7 +112,7 @@ export default function CategorieSlider() {
 				<div class="flex justify-center items-center md:px-0 px-3">
 					<a
 						href="/website"
-						class="lg:w-[512px] max-w-lg w-full h-80 bg-surface-800 rounded-3xl flex-shrink-0 relative group overflow-hidden border border-primary/0 hover:border-background/20 transition-all"
+						class="active:cursor-grabbing lg:w-[512px] max-w-lg w-full h-80 bg-surface-800 rounded-3xl flex-shrink-0 relative group overflow-hidden border border-primary/0 hover:border-background/20 transition-all"
 					>
 						<div class="absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 z-10">
 							<Website />
