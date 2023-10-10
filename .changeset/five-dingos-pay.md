1502c0debd0b16641f2b37511a716465a9dabd92
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2023-10-10 19:02:23 +0200

    refactor: remove migrate script

---
+++ .changeset/five-dingos-pay.md
@@ -0,0 +1,10 @@
+---
+"@inlang/cli": minor
+---
+
+refactor: removed the project migrate command
+
+- migration command is not used anymore
+- removed the command to reduce complexity
+
+improve: the project init command forward to inlang.com/new
--- inlang/source-code/cli/src/commands/project/index.ts
+++ inlang/source-code/cli/src/commands/project/index.ts
@@ -1,6 +1,5 @@
 import { Command } from "commander"
 import { init } from "./init.js"
-import { migrate } from "./migrate.js"
 import { validate } from "./validate.js"
 
 export const project = new Command()
@@ -9,4 +8,3 @@ export const project = new Command()
 	.argument("[command]")
 	.addCommand(init)
 	.addCommand(validate)
-	.addCommand(migrate)
