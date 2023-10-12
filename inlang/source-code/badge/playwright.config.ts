0402a236886afb4a9ac91edc453142d76f7c2b17
Felix Haeberle <kontakt@felixhaeberle.de>
2023-10-12 15:39:57 +0200

    fix CI

--- inlang/source-code/badge/playwright.config.ts
+++ inlang/source-code/badge/playwright.config.ts
@@ -22,8 +22,6 @@ export default defineConfig({
 	/* Reporter to use. See https://playwright.dev/docs/test-reporters */
 	reporter: "null",
 	preserveOutput: "failures-only",
-	/* timeout */
-	globalTimeout: 120000, // 2 minutes
 	/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
 	use: {
 		/* Base URL to use in actions like `await page.goto('/')`. */
@@ -72,9 +70,9 @@ export default defineConfig({
 	],
 
 	/* Run your local dev server before starting the tests */
-	webServer: {
-		command: "npm run dev",
-		url: "http://localhost:3000",
-		reuseExistingServer: !process.env.CI,
-	},
+	// webServer: {
+	// 	command: "npm run dev",
+	// 	url: "http://localhost:3000",
+	// 	reuseExistingServer: !process.env.CI,
+	// },
 })
