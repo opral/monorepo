e1db6fe71a3c1f760a47108d4a50da5fc49d7e2f
Felix Haeberle <kontakt@felixhaeberle.de>
2023-10-12 14:07:10 +0200

    adust timeout

--- inlang/source-code/badge/playwright.config.ts
+++ inlang/source-code/badge/playwright.config.ts
@@ -22,6 +22,8 @@ export default defineConfig({
 	/* Reporter to use. See https://playwright.dev/docs/test-reporters */
 	reporter: "null",
 	preserveOutput: "failures-only",
+	/* timeout */
+	globalTimeout: 120000, // 2 minutes
 	/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
 	use: {
 		/* Base URL to use in actions like `await page.goto('/')`. */
