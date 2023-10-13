23161767995cc78109e2a5c617b91b1c80d39448
Nils Jacobsen <nils.jacobsen98@gmail.com>
2023-10-13 11:20:20 +0200

    add new nav

--- packages/website/src/pages/Layout.tsx
+++ packages/website/src/pages/Layout.tsx
@@ -16,6 +16,7 @@ import { SectionLayout } from "./index/components/sectionLayout.jsx"
 import { defaultLanguage, extractLocale } from "#src/renderer/_default.page.route.js"
 import { useI18n } from "@solid-primitives/i18n"
 import { NewsletterForm } from "#src/components/NewsletterForm.jsx"
+import MarketplaceBar from "./index/components/MarketplaceBar.jsx"
 
 /**
  * Ensure that all elements use the same margins.
@@ -30,7 +31,7 @@ const layoutMargins = "max-w-screen-xl w-full mx-auto px-4"
 // command-f this repo to find where the layout is called
 export function Layout(props: { children: JSXElement }) {
 	return (
-		<div class="flex flex-col">
+		<div class="relative flex flex-col">
 			<Header />
 			{/* the outer div is growing to occupy the entire height and thereby
 			push the footer to the bottom */}
@@ -89,7 +90,7 @@ const socialMediaLinks = [
 ]
 
 function Header(props: { landingpage?: boolean; darkmode?: boolean; transparent?: boolean }) {
-	const getLinks = () => {
+	const getMarketplaceLinks = () => {
 		return [
 			{
 				name: `All`,
@@ -121,15 +122,19 @@ function Header(props: { landingpage?: boolean; darkmode?: boolean; transparent?
 				href: "/website",
 				type: props.darkmode ? "textBackground" : ("text" as buttonType),
 			},
+		]
+	}
+
+	const getLinks = () => {
+		return [
 			{
 				name: `Developers`,
-				href: "/documentation",
+				href: "/marketplace",
 				type: props.darkmode ? "textBackground" : ("text" as buttonType),
 			},
 		]
 	}
 
-	const [localStorage] = useLocalStorage()
 	const [mobileMenuIsOpen, setMobileMenuIsOpen] = createSignal(false)
 	const [t, { locale }] = useI18n()
 
@@ -137,50 +142,172 @@ function Header(props: { landingpage?: boolean; darkmode?: boolean; transparent?
 		const language = locale() || defaultLanguage
 		return language !== defaultLanguage ? "/" + language : ""
 	}
-
 	return (
 		<>
-			<header
-				// bg-surface-1 is with fixed hex value to avoid transparency with dooms scrolling behaviour
-				class={
-					"sticky top-0 z-[9999] w-full border-b transition-colors " +
-					(!props.transparent && props.darkmode
-						? " bg-surface-900 border-b-surface-800"
-						: !props.transparent && !props.darkmode
-						? "bg-background border-surface-2"
-						: "" + props.transparent && props.darkmode
-						? "bg-transparent border-b-surface-900 bg-surface-900"
-						: "bg-transparent border-b-background bg-background")
-				}
+			<Show
+				when={!currentPageContext.urlParsed.pathname.includes("editor")}
+				fallback={<EditorHeader />}
 			>
-				<Show when={props.transparent}>
-					<div class="absolute left-1/2 -translate-x-1/2 h-full max-w-screen-xl w-full mx-auto">
-						<div class="invisible xl:visible absolute top-0 left-0 h-full w-full z-0 ">
-							<div class="flex w-full h-full justify-between mx-auto">
-								<div class="h-full w-[2px] bg-surface-400 opacity-[7%]" />
-								<div class="h-full w-[2px] bg-surface-400 opacity-[7%]" />
-								<div class="h-full w-[2px] bg-surface-400 opacity-[7%]" />
-								<div class="h-full w-[2px] bg-surface-400 opacity-[7%]" />
-								<div class="h-full w-[2px] bg-surface-400 opacity-[7%]" />
+				<header class="sticky top-0 w-full z-[100]">
+					<div
+						class={
+							"z-[80] w-full border-b transition-colors h-[76px] " +
+							(!props.transparent && props.darkmode
+								? " bg-surface-900 border-b-surface-800"
+								: !props.transparent && !props.darkmode
+								? "bg-background border-surface-2"
+								: "" + props.transparent && props.darkmode
+								? "bg-transparent border-b-surface-900 bg-surface-900"
+								: "bg-transparent border-b-background bg-background")
+						}
+					>
+						<Show when={props.transparent}>
+							<div class="absolute left-1/2 -translate-x-1/2 h-full max-w-screen-xl w-full mx-auto">
+								<div class="invisible xl:visible absolute top-0 left-0 h-full w-full z-0 ">
+									<div class="flex w-full h-full justify-between mx-auto">
+										<div class="h-full w-[2px] bg-surface-400 opacity-[7%]" />
+										<div class="h-full w-[2px] bg-surface-400 opacity-[7%]" />
+										<div class="h-full w-[2px] bg-surface-400 opacity-[7%]" />
+										<div class="h-full w-[2px] bg-surface-400 opacity-[7%]" />
+										<div class="h-full w-[2px] bg-surface-400 opacity-[7%]" />
+									</div>
+								</div>
 							</div>
+						</Show>
+						<div class={`w-full h-full relative z-10`}>
+							<nav class={"md:p-0 max-w-[1248px] w-full flex justify-center mx-auto h-full"}>
+								<div class={"md:py-4 md:px-4"}>
+									<MarketplaceBar
+										links={getMarketplaceLinks()}
+										type={props.darkmode ? "dark" : "light"}
+									/>
+								</div>
+
+								<Show when={mobileMenuIsOpen()}>
+									<ol class="pl-8 pb-8 space-y-3 relativ w-full pt-24 overflow text-surface-100 bg-background border border-surface-200 h-[480px]">
+										<For each={[getMarketplaceLinks(), getLinks()].flat()}>
+											{(link) => (
+												<>
+													<Show when={link.name === "Developers"}>
+														<div class="py-4">
+															<div class="w-24 h-[1px] bg-surface-200 ml-8" />
+														</div>
+													</Show>
+													<sl-tree>
+														<a
+															class={
+																(props.darkmode ? "text-surface-100" : "text-on-surface") +
+																" grow min-w-full w-full"
+															}
+															href={link.href}
+															onClick={() => setMobileMenuIsOpen(!mobileMenuIsOpen())}
+														>
+															<sl-tree-item>{link.name}</sl-tree-item>
+														</a>
+													</sl-tree>
+												</>
+											)}
+										</For>
+									</ol>
+								</Show>
+							</nav>
 						</div>
 					</div>
-				</Show>
-				<div class={`w-full h-full py-4 px-6 md:px-4 relative z-10 ${props.landingpage && "px-6"}`}>
-					<nav class={"max-w-[1248px] w-full mx-auto"}>
-						<div class="flex">
-							<a href={getLocale() + "/"} class="flex items-center w-fit">
+					<div class="pl-6 md:pl-4 xl:pl-0 absolute z-[90] top-0 h-[72px] left-0 w-full text-surface-200 pointer-events-none">
+						<div class="max-w-[1248px] w-full mx-auto">
+							<a
+								href={getLocale() + "/"}
+								class="flex items-center w-fit pt-[18px] pointer-events-auto"
+							>
 								<img
-									class={"h-9 w-9 " + (props.darkmode ? "filter invert" : "")}
+									class={
+										"h-9 w-9 " + (!mobileMenuIsOpen() && props.darkmode ? "filter invert" : "")
+									}
 									src="/favicon/safari-pinned-tab.svg"
 									alt="Company Logo"
 								/>
 								<span
 									class={
 										"self-center pl-2 text-left font-semibold " +
-										(props.darkmode ? "text-background" : "text-surface-900")
+										(!mobileMenuIsOpen() && props.darkmode ? "text-background" : "text-surface-900")
 									}
 								>
+									inlang
+								</span>
+							</a>
+						</div>
+					</div>
+					<div class="absolute pr-4 xl:pr-0 z-[90] top-0 h-[72px] left-0 w-full text-surface-200 pointer-events-none">
+						<div class="max-w-[1248px] w-full mx-auto justify-end hidden md:flex gap-8 items-center pt-[18px]">
+							<For each={getLinks()}>
+								{(link) => (
+									<>
+										<Button type={link.type} href={link.href}>
+											{link.name}
+										</Button>
+									</>
+								)}
+							</For>
+							<Show
+								when={
+									currentPageContext.urlParsed.pathname.includes("editor") === false &&
+									currentPageContext.urlParsed.pathname.includes("documentation") === false &&
+									currentPageContext.urlParsed.pathname.includes("blog") === false
+								}
+							>
+								<LanguagePicker darkmode={props.darkmode} />
+							</Show>
+							<Show when={currentPageContext.urlParsed.pathname.includes("editor") === false}>
+								<Button type={props.darkmode ? "primary" : "secondary"} href="/editor">
+									{t("header.openEditor")}
+								</Button>
+							</Show>
+						</div>
+						<div class="md:hidden flex items-center justify-end h-[76px] pr-4">
+							<button
+								onClick={() => setMobileMenuIsOpen(!mobileMenuIsOpen())}
+								type="button"
+								class="inline-flex items-center justify-center text-primary pointer-events-auto"
+							>
+								<span class="sr-only">{mobileMenuIsOpen() ? "Close menu" : "Open menu"}</span>
+								{mobileMenuIsOpen() ? <IconClose class="w-6 h-6" /> : <IconMenu class="w-6 h-6" />}
+							</button>
+						</div>
+					</div>
+				</header>
+			</Show>
+		</>
+	)
+}
+
+function EditorHeader() {
+	const getLinks = () => {
+		return [
+			{
+				name: `Editor Projects`,
+				href: "/editor",
+			},
+			{
+				name: `Apps`,
+				href: "/app",
+			},
+		]
+	}
+	const [, { locale }] = useI18n()
+	const getLocale = () => {
+		const language = locale() || defaultLanguage
+		return language !== defaultLanguage ? "/" + language : ""
+	}
+	const [mobileMenuIsOpen, setMobileMenuIsOpen] = createSignal(false)
+	return (
+		<>
+			<header class="sticky top-0 z-[9999] w-full border-b transition-colors bg-transparent border-b-background bg-background">
+				<div class="`w-full h-full py-4 px-6 md:px-4 relative z-10">
+					<nav class="max-w-[1248px] w-full mx-auto">
+						<div class="flex">
+							<a href={getLocale() + "/"} class="flex items-center w-fit">
+								<img class={"h-9 w-9"} src="/favicon/safari-pinned-tab.svg" alt="Company Logo" />
+								<span class={"self-center pl-2 text-left font-semibold text-surface-900"}>
 									inlang
 								</span>
 							</a>
@@ -189,35 +316,12 @@ function Header(props: { landingpage?: boolean; darkmode?: boolean; transparent?
 									<For each={getLinks()}>
 										{(link) => (
 											<>
-												<Show when={link.name === "Developers"}>
-													<div
-														class={
-															"w-[1px] h-6 " +
-															(props.darkmode ? "bg-surface-700" : "bg-surface-200")
-														}
-													/>
-												</Show>
-												<Button type={link.type} href={link.href}>
+												<Button type="text" href={link.href}>
 													{link.name}
 												</Button>
 											</>
 										)}
 									</For>
-									<Show
-										when={
-											currentPageContext.urlParsed.pathname.includes("editor") === false &&
-											currentPageContext.urlParsed.pathname.includes("documentation") === false &&
-											currentPageContext.urlParsed.pathname.includes("blog") === false
-										}
-									>
-										<LanguagePicker darkmode={props.darkmode} />
-									</Show>
-									<Show when={currentPageContext.urlParsed.pathname.includes("editor") === false}>
-										<Button type={props.darkmode ? "primary" : "secondary"} href="/editor">
-											{t("header.openEditor")}
-										</Button>
-									</Show>
-									{/* not overwhelming the user by only showing login button when not on landig page */}
 									<Show
 										when={
 											localStorage.user || currentPageContext.urlParsed.pathname.includes("editor")
@@ -303,21 +407,21 @@ const Footer = (props: { isLandingPage: boolean }) => {
 		return [...productsLinks]
 	}
 
-	const getDocLinks = () => {
-		return [
-			{
-				name: `${t("footer.docs.gettingStarted")}`,
-				href: "/documentation/quick-start",
-				type: "text" as buttonType,
-			},
-			{ name: `${t("footer.docs.whyInlang")}`, href: "/documentation", type: "text" as buttonType },
-			{
-				name: `${t("footer.docs.contribute")}`,
-				href: "/documentation/contributing",
-				type: "text" as buttonType,
-			},
-		]
-	}
+	// const getDocLinks = () => {
+	// 	return [
+	// 		{
+	// 			name: `${t("footer.docs.gettingStarted")}`,
+	// 			href: "/documentation/quick-start",
+	// 			type: "text" as buttonType,
+	// 		},
+	// 		{ name: `${t("footer.docs.whyInlang")}`, href: "/documentation", type: "text" as buttonType },
+	// 		{
+	// 			name: `${t("footer.docs.contribute")}`,
+	// 			href: "/documentation/contributing",
+	// 			type: "text" as buttonType,
+	// 		},
+	// 	]
+	// }
 	const getResourceLinks = () => {
 		return [
 			{
@@ -377,10 +481,7 @@ const Footer = (props: { isLandingPage: boolean }) => {
 								{(link) => (
 									<a
 										target="_blank"
-										class={
-											"link link-primary flex space-x-2 items-center " +
-											(props.darkmode ? "text-background" : "")
-										}
+										class={"link link-primary flex space-x-2 items-center"}
 										href={link.href}
 									>
 										<link.Icon class="w-5 h-5" />
@@ -587,57 +688,57 @@ function LanguagePicker(props: { darkmode?: boolean }) {
 /**
  * Language picker for the landing page.
  */
-function ProductDropdown(props: { darkmode?: boolean }) {
-	const languages = [
-		{
-			code: "en",
-			name: "English",
-		},
-		{
-			code: "de",
-			name: "Deutsch",
-		},
-		{
-			code: "zh",
-			name: "中文",
-		},
-		{
-			code: "sk",
-			name: "Slovak",
-		},
-		{
-			code: "pt_BR",
-			name: "Portuguese Brazil",
-		},
-	]
+// function ProductDropdown(props: { darkmode?: boolean }) {
+// 	const languages = [
+// 		{
+// 			code: "en",
+// 			name: "English",
+// 		},
+// 		{
+// 			code: "de",
+// 			name: "Deutsch",
+// 		},
+// 		{
+// 			code: "zh",
+// 			name: "中文",
+// 		},
+// 		{
+// 			code: "sk",
+// 			name: "Slovak",
+// 		},
+// 		{
+// 			code: "pt_BR",
+// 			name: "Portuguese Brazil",
+// 		},
+// 	]
 
-	return (
-		<div class="w-fit">
-			<sl-dropdown>
-				<div
-					slot="trigger"
-					class={
-						"cursor-pointer h-10 flex items-center font-medium text-sm " +
-						(props.darkmode
-							? "text-background hover:text-surface-300"
-							: "text-surface-700 hover:text-primary")
-					}
-				>
-					<p>Products</p>
-					<IconExpand class="w-5 h-5 opacity-50" />
-				</div>
-				<sl-menu>
-					<For each={productsLinks}>
-						{(product) => (
-							<sl-menu-item>
-								<a class="w-full block" href={product.href}>
-									{product.name.replace("Global ", "")}
-								</a>
-							</sl-menu-item>
-						)}
-					</For>
-				</sl-menu>
-			</sl-dropdown>
-		</div>
-	)
-}
+// 	return (
+// 		<div class="w-fit">
+// 			<sl-dropdown>
+// 				<div
+// 					slot="trigger"
+// 					class={
+// 						"cursor-pointer h-10 flex items-center font-medium text-sm " +
+// 						(props.darkmode
+// 							? "text-background hover:text-surface-300"
+// 							: "text-surface-700 hover:text-primary")
+// 					}
+// 				>
+// 					<p>Products</p>
+// 					<IconExpand class="w-5 h-5 opacity-50" />
+// 				</div>
+// 				<sl-menu>
+// 					<For each={productsLinks}>
+// 						{(product) => (
+// 							<sl-menu-item>
+// 								<a class="w-full block" href={product.href}>
+// 									{product.name.replace("Global ", "")}
+// 								</a>
+// 							</sl-menu-item>
+// 						)}
+// 					</For>
+// 				</sl-menu>
+// 			</sl-dropdown>
+// 		</div>
+// 	)
+// }
