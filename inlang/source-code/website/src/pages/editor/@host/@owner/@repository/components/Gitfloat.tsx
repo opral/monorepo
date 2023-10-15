0fbb685a57b964f2d2001aa72df34d59b57ce082
Jan Johannes <jan.johannes@inlang.com>
2023-10-16 01:10:09 +0300

    minor type fixes

--- inlang/source-code/website/src/pages/editor/@host/@owner/@repository/components/Gitfloat.tsx
+++ inlang/source-code/website/src/pages/editor/@host/@owner/@repository/components/Gitfloat.tsx
@@ -165,9 +165,13 @@ export const Gitfloat = () => {
 	}
 
 	const pullrequestUrl = () => {
-		return `https://github.com/${
-			githubRepositoryInformation()?.parent?.fullName
-		}/compare/${currentBranch()}...${routeParams().owner}:${
+		const repoInfo = githubRepositoryInformation()
+		if (typeof repoInfo === "undefined" || "error" in repoInfo) {
+			return
+		}
+		return `https://github.com/${repoInfo?.parent?.fullName}/compare/${currentBranch()}...${
+			routeParams().owner
+		}:${
 			routeParams().repository
 		}:${currentBranch()}?expand=1;title=Update%20translations;body=Describe%20the%20changes%20you%20have%20conducted%20here%0A%0APreview%20the%20messages%20on%20https%3A%2F%2Finlang.com%2Fgithub.com%2F${
 			(currentPageContext.routeParams as EditorRouteParams).owner
