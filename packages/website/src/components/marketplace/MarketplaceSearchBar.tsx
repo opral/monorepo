35c6f39ba9d7875773349a7a1b69bdb7df14e932
NiklasBuchfink <niklas.buchfink@web.de>
2023-10-16 19:07:34 +0200

    feat: search param to url

--- packages/website/src/components/marketplace/MarketplaceSearchBar.tsx
+++ packages/website/src/components/marketplace/MarketplaceSearchBar.tsx
@@ -1,21 +1,35 @@
 import IconSearch from "~icons/material-symbols/search-rounded"
 
 const MarketplaceSearchBar = () => {
-	const onSearch = () => {
-		// TODO
-		console.info("input")
+	const urlParams = new URLSearchParams(window.location.search)
+	const searchParam = urlParams.get("search")
+
+	const handleSubmit = () => {
+		const search = document.getElementById("search") as HTMLInputElement
+		const currentUrl = new URL(window.location.href)
+		const newUrl = new URL(currentUrl.protocol + "//" + currentUrl.host + search.value)
+		window.location.href = newUrl.href
+		window.history.pushState({}, "", newUrl)
 	}
 
 	return (
-		<div class="group flex justify-center gap-1 px-3 items-center border h-8 w-80 py-0.5 rounded-full transition-all bg-background border-surface-200 focus-within:border-primary">
+		<form
+			class="group flex justify-center gap-1 px-3 items-center border h-8 w-80 py-0.5 rounded-full transition-all bg-background border-surface-200 focus-within:border-primary"
+			onSubmit={() => handleSubmit()}
+		>
 			<input
 				type="text"
+				id="search"
 				name="search"
 				class="border-0 focus:ring-0 h-full w-full pl-0 text-sm"
-				style={{ "outline-width": "0" }}
-				onChange={onSearch} />
-			<IconSearch class="ml-1.5 group-focus-within:text-focus-primary" />
-		</div>
+				value={searchParam?.replace(/%20/g, " ")}
+			/>
+			<button
+				type="submit"
+			>
+				<IconSearch class="ml-1.5 group-focus-within:text-focus-primary" />
+			</button>
+		</form>
 	)
 }
 
