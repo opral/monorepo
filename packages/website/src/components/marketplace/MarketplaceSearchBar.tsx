712a9ff55aaf99ba8693e69c0fcc9b6203d5d51c
NiklasBuchfink <niklas.buchfink@web.de>
2023-10-16 18:20:04 +0200

    adjust search

--- packages/website/src/components/marketplace/MarketplaceSearchBar.tsx
+++ packages/website/src/components/marketplace/MarketplaceSearchBar.tsx
@@ -7,8 +7,8 @@ const MarketplaceSearchBar = () => {
 	}
 
 	return (
-		<div class="flex justify-center gap-1 px-3 items-center border py-0.5 rounded-full transition-all bg-background border-surface-200 focus-within:border-primary">
-			<input class="border-none focus:outline-none h-full" onChange={() => onSearch()} />
+		<div class="flex justify-center gap-1 px-4 items-center border py-0.5 rounded-full transition-all bg-background border-surface-200 focus-within:border-primary">
+			<input class="border-none focus:outline-none h-full pl-0" onChange={onSearch} />
 			<IconSearch class="ml-3" />
 		</div>
 	)
