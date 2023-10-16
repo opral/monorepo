6855fa9301ff5dafa89ccd11f8dd4be83808bf6b
NiklasBuchfink <niklas.buchfink@web.de>
2023-10-16 12:05:47 +0200

    init change

--- packages/website/src/pages/index/index.page.tsx
+++ packages/website/src/pages/index/index.page.tsx
@@ -1,9 +1,6 @@
 import { LandingPageLayout as RootLayout } from "../Layout.jsx"
 import { Meta, Title } from "@solidjs/meta"
 import { createSignal } from "solid-js"
-import Hero from "./sections/01-hero/index.jsx"
-import Marketplace from "./sections/03-marketplace/index.jsx"
-import Lix from "./sections/02-lix/index.jsx"
 
 export type PageProps = {
 	markdown: string
@@ -38,12 +35,7 @@ export function Page() {
 			/>
 			<Meta name="og:image" content="/images/inlang-social-image.jpg" />
 			<RootLayout landingpage darkmode={darkmode()} transparent={transparent()}>
-				<div>
-					<Hero />
-					<Lix />
-					<Marketplace />
-					{/* <QualityChecks /> */}
-				</div>
+				inlang Marketplace
 			</RootLayout>
 		</>
 	)
