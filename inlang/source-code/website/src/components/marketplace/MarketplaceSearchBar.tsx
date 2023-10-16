1a36769d210b621a1990f7143f0b63b57223ac4d
NiklasBuchfink <niklas.buchfink@web.de>
2023-10-16 18:59:38 +0200

    fix: searchbar styling

--- inlang/source-code/website/src/components/marketplace/MarketplaceSearchBar.tsx
+++ inlang/source-code/website/src/components/marketplace/MarketplaceSearchBar.tsx
@@ -7,9 +7,14 @@ const MarketplaceSearchBar = () => {
 	}
 
 	return (
-		<div class="flex justify-center gap-1 px-4 items-center border py-0.5 rounded-full transition-all bg-background border-surface-200 focus-within:border-primary">
-			<input class="border-none focus:outline-none h-full pl-0" onChange={onSearch} />
-			<IconSearch class="ml-3" />
+		<div class="group flex justify-center gap-1 px-3 items-center border h-8 w-80 py-0.5 rounded-full transition-all bg-background border-surface-200 focus-within:border-primary">
+			<input
+				type="text"
+				name="search"
+				class="border-0 focus:ring-0 h-full w-full pl-0 text-sm"
+				style={{ "outline-width": "0" }}
+				onChange={onSearch} />
+			<IconSearch class="ml-1.5 group-focus-within:text-focus-primary" />
 		</div>
 	)
 }
