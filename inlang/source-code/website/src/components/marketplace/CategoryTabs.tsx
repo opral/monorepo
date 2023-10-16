40a48ea6ae480016c35990377e503c5b541bcae1
Nils Jacobsen <nils.jacobsen98@gmail.com>
2023-10-16 17:56:03 +0200

    add subcategory nav

---
+++ inlang/source-code/website/src/components/marketplace/CategoryTabs.tsx
@@ -0,0 +1,56 @@
+import { For } from "solid-js"
+import { currentPageContext } from "#src/renderer/state.js"
+
+const CategoryTabs = () => {
+	const getCategories = () => {
+		return [
+			{
+				name: `Application`,
+				href: "/application",
+			},
+			{
+				name: `Website`,
+				href: "/website",
+			},
+			{
+				name: `Markdown`,
+				href: "/markdown",
+			},
+			{
+				name: `Missing something?`,
+				href: "https://github.com/orgs/inlang/discussions",
+			},
+		]
+	}
+	return (
+		<nav class="max-w-7xl mx-auto flex gap-4">
+			<For each={getCategories()}>
+				{(link) => (
+					<div
+						class={
+							(currentPageContext.urlParsed.pathname.includes(link.href)
+								? "border-hover-primary "
+								: "border-background ") +
+							" border-b-[2px] py-[4px] text-sm bg-transparent group content-box"
+						}
+					>
+						<a href={link.href}>
+							<div
+								class={
+									(currentPageContext.urlParsed.pathname.includes(link.href)
+										? "text-primary "
+										: "text-surface-600 ") +
+									" px-2 py-[6px] group-hover:bg-surface-100 rounded-md font-medium cursor-pointer "
+								}
+							>
+								{link.name}
+							</div>
+						</a>
+					</div>
+				)}
+			</For>
+		</nav>
+	)
+}
+
+export default CategoryTabs
--- inlang/source-code/website/src/pages/application/index.page.tsx
+++ inlang/source-code/website/src/pages/application/index.page.tsx
@@ -1,8 +1,32 @@
 import { Meta, Title } from "@solidjs/meta"
 import Marketplace from "#src/components/sections/marketplace/index.jsx"
 import MarketplaceLayout from "#src/components/marketplace/MarketplaceLayout.jsx"
+import SubcategoryPills from "#src/components/marketplace/SubcategoryPills.tsx.jsx"
 
 export function Page() {
+	const getSubCategies = [
+		{
+			name: "Svelte",
+			param: "svelte",
+		},
+		{
+			name: "React",
+			param: "react",
+		},
+		{
+			name: "Next.js",
+			param: "nextjs",
+		},
+		{
+			name: "Javascript",
+			param: "javascript",
+		},
+		{
+			name: "Flutter",
+			param: "flutter",
+		},
+	]
+
 	return (
 		<>
 			<Title>Global Application</Title>
@@ -10,6 +34,11 @@ export function Page() {
 			<Meta name="og:image" content="/images/inlang-marketplace-image.jpg" />
 			<div class="bg-surface-50">
 				<MarketplaceLayout>
+					<div class="pt-4 text-sm font-medium flex items-center gap-3">
+						<p class="pr-4 text-surface-400">Categories:</p>
+						<SubcategoryPills links={getSubCategies} />
+					</div>
+
 					<div class="pb-16 md:pb-20 min-h-screen relative">
 						<h2 class="text-2xl font-semibold pb-4 pt-8">All Modules</h2>
 						<Marketplace />
