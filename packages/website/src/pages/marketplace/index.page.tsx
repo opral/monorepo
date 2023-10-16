09ad061197924cefd7d317dcc414b7ae69e20b47
Nils Jacobsen <nils.jacobsen98@gmail.com>
2023-10-16 14:38:36 +0200

    added marketplace hero section

--- packages/website/src/pages/marketplace/index.page.tsx
+++ packages/website/src/pages/marketplace/index.page.tsx
@@ -4,12 +4,11 @@ import { registry } from "@inlang/marketplace-registry"
 import { For } from "solid-js"
 import type { MarketplaceManifest } from "@inlang/marketplace-manifest"
 import Marketplace from "#src/components/sections/marketplace/index.jsx"
+import { Button } from "../index/components/Button.jsx"
+
+const featuredArray = ["app.inlang.editor", "library.inlang.paraglideJsSveltekit", "app.inlang.cli"]
 
 export function Page() {
-	const icons = registry
-		.slice(registry.length - 9, registry.length)
-		.map((item: MarketplaceManifest) => item.icon)
-
 	return (
 		<>
 			<Title>inlang Marketplace</Title>
@@ -18,49 +17,45 @@ export function Page() {
 				content="Find apps, plugins and lint rules for inlang's ecosystem."
 			/>
 			<Meta name="og:image" content="/images/inlang-marketplace-image.jpg" />
-			<Layout>
-				<div class="pb-16 md:pb-20 min-h-screen relative">
-					<div class="grid xl:grid-cols-3 xl:gap-8 grid-flow-row-dense mb-9 min-h-[198px]">
-						<div class="relative z-20 xl:mt-0 -mt-12 pb-8 xl:pb-0">
-							<h1 class="xl:col-span-2 md:pt-20 pt-16 text-[40px] md:text-5xl font-bold text-left  leading-none md:leading-tight mb-3">
-								Explore the ecosystem
-							</h1>
+			<div class="bg-surface-50">
+				<Layout>
+					<div class="w-full flex gap-4 mt-12">
+						<div class="w-1/4 bg-background border border-surface-200 rounded-xl p-5">
+							<p class="pb-6 text-surface-400">Featured</p>
+							<ul class="flex flex-col gap-8">
+								<For each={featuredArray}>
+									{(feature) => {
+										const m = registry.find((m) => m.id === feature)
+										return (
+											<div class="flex flex-col gap-2">
+												<div class="flex items-center gap-2">
+													<img class="w-8 h-8 rounded" src={m?.publisherIcon} alt={m?.id} />
+													<h3 class="flex-1 text-lg text-surface-700 font-semibold whitespace-nowrap overflow-hidden">
+														{(m?.displayName as { en: string }).en}
+													</h3>
+												</div>
+												<p class="line-clamp-2">{(m?.description as { en: string }).en}</p>
+												<div class="w-fit">
+													<Button chevron={true} type="textPrimary" href={"/m/" + m?.id}>
+														See more
+													</Button>
+												</div>
+											</div>
+										)
+									}}
+								</For>
+							</ul>
 						</div>
-						<div class="xl:col-span-2 max-xl:row-start-1 relative max-xl:max-h-14 xl:blur-0 blur-xl pointer-events-none w-full flex xl:justify-end content-start items-start flex-wrap gap-4 pt-12 justify-start xl:ml-auto max-w-lg">
-							<For each={icons}>
-								{(icon) => (
-									<div
-										style={{
-											"background-image": `url(${icon})`,
-										}}
-										class="w-16 h-16 rounded-lg bg-surface-200 bg-cover bg-center opacity-30 xl:opacity-50 border border-surface-300"
-									/>
-								)}
-							</For>
-							<div class="absolute inset-0 xl:bg-gradient-to-tr bg-gradient-to-tl from-background/0 via-background/25 to-primary/25 blur-2xl z-10" />
-							<div class="absolute inset-0 xl:bg-gradient-to-tr bg-gradient-to-tl from-background/0 via-background/25 to-background" />
+						<div class="flex-1 h-[600px] bg-background border border-surface-200 rounded-xl p-5">
+							Welcome to inlang
 						</div>
 					</div>
-					<Marketplace
-						highlights={[
-							{
-								type: "card",
-								id: "app.inlang.editor",
-								slogan: "Translators choice",
-							},
-							{
-								type: "banner",
-								color: "#06B6D4",
-								title: "Develop with inlang",
-								slogan: "For Developers",
-								description: "Create your own module.",
-								link: "/documentation",
-								image: "/images/developers-artwork.jpg",
-							},
-						]}
-					/>
-				</div>
-			</Layout>
+					<div class="pb-16 md:pb-20 min-h-screen relative">
+						<h2 class="text-2xl font-semibold pb-4 pt-8">All Modules</h2>
+						<Marketplace />
+					</div>
+				</Layout>
+			</div>
 		</>
 	)
 }
