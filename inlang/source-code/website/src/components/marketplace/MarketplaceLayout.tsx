6bf76c554d44d552b4286cbbad47d43fff8fcad9
NiklasBuchfink <niklas.buchfink@web.de>
2023-10-17 15:26:40 +0200

    fix: marketplace layout padding

--- inlang/source-code/website/src/components/marketplace/MarketplaceLayout.tsx
+++ inlang/source-code/website/src/components/marketplace/MarketplaceLayout.tsx
@@ -4,9 +4,9 @@ import type { JSXElement } from "solid-js"
 
 const MarketplaceLayout = (props: { children: JSXElement }) => {
 	return (
-		<div class="px-4 bg-surface-50">
+		<div class="bg-surface-50">
 			<MarketplaceHeader />
-			<div class="max-w-7xl mx-auto">{props.children}</div>
+			<div class="max-w-7xl mx-auto px-4">{props.children}</div>
 			<Footer />
 		</div>
 	)
