15c92cd7cbf7680f1291e12f0229776d82302d38
NiklasBuchfink <niklas.buchfink@web.de>
2023-10-17 17:26:24 +0200

    add: simple search by text or category

--- inlang/source-code/website/src/pages/index/index.page.tsx
+++ inlang/source-code/website/src/pages/index/index.page.tsx
@@ -2,8 +2,10 @@ import { Meta, Title } from "@solidjs/meta"
 import Marketplace from "#src/components/sections/marketplace/index.jsx"
 import Hero from "./custom_section/Hero.jsx"
 import MarketplaceLayout from "#src/components/marketplace/MarketplaceLayout.jsx"
+import { Show } from "solid-js"
 
 export function Page() {
+	const urlParams = new URLSearchParams(window.location.search)
 	return (
 		<>
 			<Title>inlang Marketplace</Title>
@@ -14,7 +16,9 @@ export function Page() {
 			<Meta name="og:image" content="/images/inlang-marketplace-image.jpg" />
 			<div class="bg-surface-50">
 				<MarketplaceLayout>
-					<Hero />
+					<Show when={!urlParams.get("search")}>
+						<Hero />
+					</Show>
 					<div class="pb-16 md:pb-20 min-h-screen relative">
 						<h2 class="text-sm text-surface-500 pb-3 pl-2 pt-4">Products</h2>
 						<Marketplace />
