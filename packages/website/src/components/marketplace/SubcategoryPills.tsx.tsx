1858f912067a9c130474e9801777a9d7d22370d0
NiklasBuchfink <niklas.buchfink@web.de>
2023-10-17 10:59:59 +0200

    add: application category icons

--- packages/website/src/components/marketplace/SubcategoryPills.tsx.tsx
+++ packages/website/src/components/marketplace/SubcategoryPills.tsx.tsx
@@ -1,6 +1,6 @@
-import { For } from "solid-js"
+import { For, type JSX } from "solid-js"
 
-const SubcategoryPills = (props: { links: { name: string; param: string }[] }) => {
+const SubcategoryPills = (props: { links: { name: string; param: string; icon?: JSX.Element }[] }) => {
 	return (
 		<nav class="flex gap-[5px]">
 			<For each={props.links}>
@@ -15,9 +15,10 @@ const SubcategoryPills = (props: { links: { name: string; param: string }[] }) =
 								(window.location.search.includes(link.param)
 									? "bg-primary text-background "
 									: "bg-background text-surface-600 border border-surface-200 hover:border-surface-400") +
-								" px-3 py-[6px] font-medium cursor-pointer rounded-lg"
+								" px-3 py-[6px] font-medium cursor-pointer rounded-lg flex items-center gap-1"
 							}
 						>
+							{link.icon}
 							{link.name}
 						</div>
 					</a>
