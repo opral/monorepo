a5ab5ada33177e003d1139e2dc404964ce954e21
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2023-10-17 16:02:53 +0200

    fix: https://github.com/inlang/monorepo/issues/1478

--- inlang/source-code/paraglide/paraglide-js/CHANGELOG.md
+++ inlang/source-code/paraglide/paraglide-js/CHANGELOG.md
@@ -1,5 +1,9 @@
 # @inlang/paraglide-js
 
+## 	1.0.0-prerelease.2
+
+fix: https://github.com/inlang/monorepo/issues/1478
+
 ## 	1.0.0-prerelease.1
 
 ### fix: Jetbrains based editors not detecting `@inlang/paraglide-js/{namespace}/messages` imports
--- inlang/source-code/paraglide/paraglide-js/src/cli/main.ts
+++ inlang/source-code/paraglide/paraglide-js/src/cli/main.ts
@@ -1,46 +1,33 @@
 import consola from "consola"
 import dedent from "dedent"
-import { fileURLToPath } from "node:url"
 import { Command } from "commander"
 import { compileCommand } from "./commands/compile.js"
-
-/**
- * The path this file is executed from.
- *
- * Usually something like '/Users/samuel/example/repository/node_modules/paraglide-js/dist/cli/main.js'
- */
-const metaUrlPath = fileURLToPath(import.meta.url)
-
-/**
- * The absolute path to the paraglide directory.
- *
- * slices a path
- * from '/Users/samuel/example/repository/node_modules/paraglide-js/dist/cli/main.js'
- * to   '/Users/samuel/example/repository/node_modules/paraglide-js'
- */
-export const paraglideDirectory: string = metaUrlPath.slice(0, metaUrlPath.indexOf("/dist/"))
+import { paraglideDirectory } from "./state.js"
 
 export const cli = new Command()
 	.name("paraglide-js")
 	.addCommand(compileCommand)
-	.hook("preAction", () => {
+	.action(() => {
 		// ------------------- VALIDATE IF RUNNING FROM CORRECT FOLDER -------------------
 		// the CLI expects to be running from the dist folder of the specific paraglide package
 		// to compile the output in the correct directory
-		if (metaUrlPath.includes(`paraglide-js/dist/`) === false) {
+		if (paraglideDirectory.endsWith(`paraglide-js`) === false) {
 			consola.error(
 				dedent`
-			The CLI is not running from the dist folder. 
+			The CLI is not running from a paraglide-js directory. 
 
 			This is likely an internal bug. Please file an issue at https://github.com/inlang/monorepo/issues.
 
 			Debug information: 
 
-			- metaUrlPath: ${metaUrlPath}
+			- paraglideDirectory: ${paraglideDirectory}
 			`
 			)
-			process.exit(1)
+			return process.exit(1)
 		}
+		// show the help because no command is specified
+		return cli.help()
 	})
+
 
 cli.showHelpAfterError(true)
