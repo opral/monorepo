5e9d364b7644b6cb8c2983dd6513e572f07db13f
NiklasBuchfink <niklas.buchfink@web.de>
2023-10-18 16:24:34 +0200

    fix: search on landing page

---
+++ packages/website/src/pages/index/custom_section/Stack.tsx
@@ -0,0 +1,11 @@
+const Stack = () => {
+  return (
+    <div class="w-full flex gap-4 mt-4 md:mt-10 mb-8 flex-col-reverse md:flex-row">
+      <div class="w-full md:w-1/4 md:pr-8">
+        <p class="pb-2 text-surface-500 text-sm">Stack</p>
+      </div>
+    </div>
+  )
+}
+
+export default Stack
