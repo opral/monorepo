3d35944e556d69d35f011d3284431d71dcb476a9
jannesblobel <72493222+jannesblobel@users.noreply.github.com>
2023-10-18 20:41:18 +0200

    add pattern and a description to pathpattern

--- packages/plugins/json/src/settings.ts
+++ packages/plugins/json/src/settings.ts
@@ -3,7 +3,13 @@ import { Type, type Static } from "@sinclair/typebox"
 
 export type PluginSettings = Static<typeof PluginSettings>
 export const PluginSettings = Type.Object({
-	pathPattern: Type.Union([Type.String(), Type.Record(Type.String(), Type.String())]),
+	pathPattern: Type.Union([
+		Type.String({
+			pattern: ".*\\{languageTag\\}.*\\.json",
+			description: "The PluginSettings must contain `{languageTag}` and end with `.json`.",
+		}),
+		Type.Record(Type.String(), Type.String()),
+	]),
 	variableReferencePattern: Type.Optional(Type.Array(Type.String())),
 	ignore: Type.Optional(Type.Array(Type.String())),
 })
