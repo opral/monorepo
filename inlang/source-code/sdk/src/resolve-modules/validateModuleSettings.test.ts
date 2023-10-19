3856fea3ac50488a73653c1ed1f18324ba389a2d
jannesblobel <72493222+jannesblobel@users.noreply.github.com>
2023-10-19 22:24:50 +0200

    add validateModulesSetting test

---
+++ inlang/source-code/sdk/src/resolve-modules/validateModuleSettings.test.ts
@@ -0,0 +1,78 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import { expect, test } from "vitest"
+import { Plugin, MessageLintRule } from "@inlang/sdk"
+// import { createNodeishMemoryFs } from "@lix-js/fs"
+import { Type } from "@sinclair/typebox"
+import { validatedModuleSettings } from "./validatedModuleSettings.js"
+import { ModuleSettingsAreInvalidError } from "./errors.js"
+// TODO  validae tyebox and normal setting schema
+
+const mockPluginSchema: Plugin["settingsSchema"] = Type.Object({
+	pathPattern: Type.Union([
+		Type.String({
+			pattern: "^[^*]*\\{languageTag\\}[^*]*\\.json",
+			description: "The PluginSettings must contain `{languageTag}` and end with `.json`.",
+		}),
+		Type.Object(
+			Type.String({
+				pattern: "^[^*]*\\{languageTag\\}[^*]*\\.json",
+				description: "The PluginSettings must contain `{languageTag}` and end with `.json`.",
+			})
+		),
+	]),
+	variableReferencePattern: Type.Array(Type.String()),
+})
+
+const mockMessageLintRuleSchema: MessageLintRule["settingsSchema"] = Type.Object({
+	ignore: Type.Array(
+		Type.String({
+			pattern: '([^"]*)',
+			description: "All items in the array need quotaion marks at the end and beginning",
+		})
+	),
+})
+
+test("should throw if PluginSchema doesn't match with the Settings", async () => {
+	const isValid = await validatedModuleSettings({
+		settingsSchema: mockPluginSchema,
+		moduleSettings: {
+			pathPattern: "./examples/example01/{languageTag}.json",
+			variableReferencePattern: ["{", "}"],
+		},
+	})
+
+	expect(isValid).toBe("isValid")
+})
+
+test("should return an error if settings file is does not match plugin Schema", async () => {
+	const isValid = validatedModuleSettings({
+		settingsSchema: mockPluginSchema,
+		moduleSettings: {
+			pathPattern: "./examples/example01/{languageTag}.json",
+		},
+	})
+
+	expect(isValid).toBeInstanceOf(ModuleSettingsAreInvalidError)
+})
+
+test("should throw if MessageLintRuleSchema doesn't match with the Settiings", async () => {
+	const isValid = await validatedModuleSettings({
+		settingsSchema: mockMessageLintRuleSchema,
+		moduleSettings: {
+			ignore: ["example", "warning"],
+		},
+	})
+
+	expect(isValid).toBe("isValid")
+})
+
+test("should return an error if settings file is does not match messageLintRule Schema", async () => {
+	const isValid = validatedModuleSettings({
+		settingsSchema: mockMessageLintRuleSchema,
+		moduleSettings: {
+			ignore: "example",
+		},
+	})
+
+	expect(isValid).toBeInstanceOf(ModuleSettingsAreInvalidError)
+})
