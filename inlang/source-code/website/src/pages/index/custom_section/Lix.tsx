484196897de408e3312f14d612f70a7d727e3ce7
Nils Jacobsen <nils.jacobsen98@gmail.com>
2023-10-19 15:46:07 +0200

    fix: change img to SEO friendly format and size

--- inlang/source-code/website/src/pages/index/custom_section/Lix.tsx
+++ inlang/source-code/website/src/pages/index/custom_section/Lix.tsx
@@ -5,10 +5,10 @@ const Lix = () => {
 		<div class="w-full flex flex-col gap-4 md:py-0 bg-background rounded-2xl border border-surface-200 overflow-hidden lg:h-[330px]">
 			<img
 				class="flex-1 hidden lg:block w-full max-h-[260px] object-cover"
-				src="./images/lixCoverV01.png"
+				src="./images/lixCoverV01.webp"
 				alt="Lix Change Control"
 			/>
-			<img class="lg:hidden" src="./images/lixCoverV02.png" alt="Lix Change Control" />
+			<img class="lg:hidden" src="./images/lixCoverV02.webp" alt="Lix Change Control" />
 			<div class="flex flex-col md:flex-row items-start md:items-end px-8 pb-6 pt-3">
 				<div class="flex flex-col gap-2 flex-1">
 					<h1 class="text-md text-surface-700 font-semibold">Lix change control</h1>
