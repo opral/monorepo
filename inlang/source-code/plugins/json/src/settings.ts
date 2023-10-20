6a10f28e3cd8972b93ec9a0b50b984d4649f1efb
jannesblobel <72493222+jannesblobel@users.noreply.github.com>
2023-10-20 15:14:39 +0200

    add namespace regex

--- inlang/source-code/plugins/json/src/settings.ts
+++ inlang/source-code/plugins/json/src/settings.ts
@@ -1,17 +1,27 @@
 // pluginOptions for json plugin
 import { Type, type Static } from "@sinclair/typebox"
 const PathPattern = Type.String({
-	pattern: "^[^*]*\\{languageTag\\}[^*]*\\.json",
+	pattern: "^(\\./|\\../)[^*]*\\{languageTag\\}[^*]*\\.json",
 	description: "The pathPattern must contain `{languageTag}` and end with `.json`.",
 	examples: [
-		"{languageTag}/examplerfile.json",
-		"examplerFolder/{languageTag}/ExamplePath.json",
-		"examplerFolder/ExamplePath{languageTag}ExamplePath.json",
+		"./{languageTag}/file.json",
+		"../folder/{languageTag}/file.json",
+		"./{languageTag}.json",
 	],
 })
 export type PluginSettings = Static<typeof PluginSettings>
 export const PluginSettings = Type.Object({
-	pathPattern: Type.Union([PathPattern, Type.Record(Type.String(), PathPattern)]),
+	pathPattern: Type.Union([
+		PathPattern,
+		Type.Record(
+			Type.String({
+				pattern: "^[^.]+$",
+				description: "Dots are not allowd ",
+				examples: ["website", "app", "homepage"],
+			}),
+			PathPattern
+		),
+	]),
 	variableReferencePattern: Type.Optional(Type.Array(Type.String())),
 	ignore: Type.Optional(Type.Array(Type.String())),
 })
