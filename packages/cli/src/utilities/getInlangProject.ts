2481dceed0ba37bb84cb98bdefa3fa776dffa9e3
jannesblobel <72493222+jannesblobel@users.noreply.github.com>
2023-10-20 10:50:00 +0200

    refactor: simplify error handling

--- packages/cli/src/utilities/getInlangProject.ts
+++ packages/cli/src/utilities/getInlangProject.ts
@@ -2,14 +2,12 @@ import fs from "node:fs/promises"
 import { resolve } from "node:path"
 import { loadProject, type InlangProject } from "@inlang/sdk"
 import { telemetry } from "../services/telemetry/implementation.js"
-import { tryCatch, type Result } from "@inlang/result"
 
 // in case multiple commands run getInlang in the same process
 let cached: Awaited<ReturnType<typeof getInlangProject>> | undefined = undefined
 
-export async function getInlangProject(): Promise<Result<InlangProject, Error>> {
+export async function getInlangProject(): Promise<InlangProject> {
 	if (cached) return cached
-
 	const baseDirectory = process.cwd()
 	const settingsFilePath = resolve(baseDirectory, "project.inlang.json")
 
@@ -17,23 +15,21 @@ export async function getInlangProject(): Promise<Result<InlangProject, Error>> 
 		.access(settingsFilePath)
 		.then(() => true)
 		.catch(() => false)
-
-	if (configExists === false) {
-		return { error: new Error("No project.inlang.json file found in the repository.") }
+	if (configExists) {
+		throw new Error("No project.inlang.json file found in the repository.")
 	}
 
-	cached = await tryCatch(() =>
-		loadProject({
-			settingsFilePath,
-			nodeishFs: fs,
-			_capture(id, props) {
-				telemetry.capture({
-					// @ts-ignore the event types
-					event: id,
-					properties: props,
-				})
-			},
-		})
-	)
+	cached = await loadProject({
+		settingsFilePath,
+		nodeishFs: fs,
+		_capture(id, props) {
+			telemetry.capture({
+				// @ts-ignore the event types
+				event: id,
+				properties: props,
+			})
+		},
+	})
+
 	return cached
 }
