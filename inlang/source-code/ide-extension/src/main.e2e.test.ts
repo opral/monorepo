7448a7f580043ba1249a396ef6b3e04be0454bc2
Robin Bühler <git+r@obin.ch>
2023-10-24 10:17:08 +0000

    chore: open test document

--- inlang/source-code/ide-extension/src/main.e2e.test.ts
+++ inlang/source-code/ide-extension/src/main.e2e.test.ts
@@ -5,4 +5,16 @@ suite("main.ts suite", () => {
 	test("if extension is loaded", () => {
 		assert.notEqual(vscode.extensions.getExtension("inlang.vs-code-extension"), undefined)
 	})
+	test("if extension is initialized", async () => {
+		if (!vscode.workspace.workspaceFolders?.[0]?.uri) {
+			throw new Error("There is no workspace defined to run tests on.")
+		}
+		const document = await vscode.workspace.openTextDocument(
+			vscode.Uri.joinPath(
+				vscode.workspace.workspaceFolders[0].uri,
+				"/app/[lng]/second-page/page.js"
+			)
+		)
+		await vscode.window.showTextDocument(document)
+	})
 })
