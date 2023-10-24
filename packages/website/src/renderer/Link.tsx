6d047847613a7fec2c8d29ee36d955d04411c0cd
Nils Jacobsen <nils.jacobsen98@gmail.com>
2023-10-24 12:33:57 +0200

    no trailing /

--- packages/website/src/renderer/Link.tsx
+++ packages/website/src/renderer/Link.tsx
@@ -1,9 +1,15 @@
 import { languageTag, sourceLanguageTag } from "@inlang/paraglide-js/inlang-marketplace"
 
 const Link = (props: { href?: string; [key: string]: any }) => {
-	const modifiedHref =
+	let modifiedHref =
 		languageTag() === sourceLanguageTag ? props.href : "/" + languageTag() + props.href
 
+	if (modifiedHref?.endsWith("/")) {
+		modifiedHref = modifiedHref.slice(0, -1)
+	}
+	if (modifiedHref?.length === 0) {
+		modifiedHref = "/"
+	}
 	return <a {...props} href={modifiedHref} />
 }
 
