19a965dc1f54f3b9f4baf0da134f8882a1258170
NiklasBuchfink <niklas.buchfink@web.de>
2023-10-27 10:37:18 +0200

    fix: editor mobile layout

--- inlang/source-code/website/src/pages/editor/@host/@owner/@repository/components/Errors.tsx
+++ inlang/source-code/website/src/pages/editor/@host/@owner/@repository/components/Errors.tsx
@@ -7,8 +7,8 @@ export const Errors = (props: { errors: Array<any>; message: string; messagePlur
 	const [visibleError, setVisibleError] = createSignal(0)
 	return (
 		<div class="w-full h-full flex flex-col items-center justify-center gap-16">
-			<div class="pt-24 md:w-[600px] mx-auto">
-				<div class="flex md:w-[600px] items-center gap-4 justify-between pb-4">
+			<div class="pt-12 sm:pt-24 w-full sm:w-[600px] mx-auto">
+				<div class="flex sm:w-[600px] items-center gap-4 justify-between pb-4">
 					{props.errors.length === 1 ? (
 						<p class="text-lg font-medium">{props.message}</p>
 					) : (
@@ -50,37 +50,32 @@ export const Errors = (props: { errors: Array<any>; message: string; messagePlur
 					)}
 				</div>
 				{props.errors.length !== 0 && (
-					<div class="pt-2 md:w-[600px]">
+					<div class="pt-2 sm:w-[600px]">
 						<div class="bg-danger text-background p-4 rounded-md flex items-center gap-4 mb-8">
 							<Icon name="danger" class="w-7 h-7 flex-shrink-0" />
 							<div>
-								<span class="font-semibold">{props.errors[visibleError()]?.name}: </span>
+								<span class="font-semibold break-all">{props.errors[visibleError()]?.name}: </span>
 								<br />
-								{/* @ts-ignore */}
 								{props.errors[visibleError()]?.message}
 							</div>
 						</div>
-						{/* @ts-ignore */}
 						{props.errors[visibleError()]?.cause && props.errors[visibleError()]?.cause.message && (
 							<>
 								<p class="text-surface-500 text-sm mb-1">Error cause</p>
 								<div class="font-mono p-4 bg-surface-800 text-background rounded-md text-sm mb-8">
 									<p>
 										<span class="font-semibold text-hover-danger">{"> "}</span>
-										{/* @ts-ignore */}
 										{props.errors[visibleError()]?.cause.message}
 									</p>
 								</div>
 							</>
 						)}
-						{/* @ts-ignore */}
 						{props.errors[visibleError()]?.stack && (
 							<>
 								<p class="text-surface-500 text-sm mb-1">Stack trace</p>
 								<div class="font-mono p-4 bg-surface-800 text-background rounded-md text-sm mb-8 break-words">
 									<p>
 										<span class="font-semibold text-hover-danger">{"> "}</span>
-										{/* @ts-ignore */}
 										{props.errors[visibleError()]?.stack}
 									</p>
 								</div>
