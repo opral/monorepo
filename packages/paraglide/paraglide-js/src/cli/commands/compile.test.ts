4117c239394afdddaee24355212ce2991e730f37
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2023-10-31 17:16:54 -0400

    add compile command tests

--- packages/paraglide/paraglide-js/src/cli/commands/compile.test.ts
+++ packages/paraglide/paraglide-js/src/cli/commands/compile.test.ts
@@ -1,20 +1,13 @@
 import consola from "consola"
-import { beforeAll, vi, test, expect, beforeEach } from "vitest"
+import { vi, test, expect, beforeEach } from "vitest"
 import memfs from "memfs"
-import fs from "node:fs/promises"
+import mockedFs from "node:fs/promises"
+import fs from "node:fs"
 import { compileCommand } from "./compile.js"
 import type { ProjectSettings } from "@inlang/sdk"
+import { createMessage } from "@inlang/sdk/test-utilities"
 import { resolve } from "node:path"
-
-beforeAll(() => {
-	// spy on commonly used functions to prevent console output
-	// and allow expecations
-	vi.spyOn(consola, "log").mockImplementation(() => undefined as never)
-	vi.spyOn(consola, "info").mockImplementation(() => undefined as never)
-	vi.spyOn(consola, "success").mockImplementation(() => undefined as never)
-	vi.spyOn(consola, "error").mockImplementation(() => undefined as never)
-	vi.spyOn(consola, "warn").mockImplementation(() => undefined as never)
-})
+import { _setStateForTest } from "../state.js"
 
 beforeEach(() => {
 	vi.resetAllMocks()
@@ -24,12 +17,20 @@ beforeEach(() => {
 	// set the current working directory to some mock value to prevent
 	// the tests from failing when running in a different environment
 	process.cwd = () => "/"
+
+	// spy on commonly used functions to prevent console output
+	// and allow expecations
+	vi.spyOn(consola, "log").mockImplementation(() => undefined as never)
+	vi.spyOn(consola, "info").mockImplementation(() => undefined as never)
+	vi.spyOn(consola, "success").mockImplementation(() => undefined as never)
+	vi.spyOn(consola, "error").mockImplementation(() => undefined as never)
+	vi.spyOn(consola, "warn").mockImplementation(() => undefined as never)
+	vi.spyOn(process, "exit").mockImplementation(() => {
+		throw "PROCESS.EXIT()"
+	})
 })
 
 test("it should exit if the project has errors", async () => {
-	vi.spyOn(process, "exit").mockImplementation(() => {
-		throw "EXIT"
-	})
 	mockFs({
 		"/project.inlang.json": JSON.stringify({
 			// invalid source language tag
@@ -41,38 +42,50 @@ test("it should exit if the project has errors", async () => {
 
 	expect(
 		compileCommand.parseAsync(["--project", "./project.inlang.json", "--namespace", "frontend"])
-	).rejects.toEqual("EXIT")
+	).rejects.toEqual("PROCESS.EXIT()")
 })
 
-test("it should compile the project", async () => {
-	mockFs({
+test("it should compile a project with a namespace", async () => {
+	const name = "frontend"
+
+	_setStateForTest({ paraglideDirectory: "/node_modules/paraglide-js" })
+	const _fs = mockFs({
+		"/plugin.js": fs.readFileSync(
+			// using the inlang-message-format plugin
+			resolve(__dirname, "../../../../../plugins/inlang-message-format/dist/index.js"),
+			{ encoding: "utf-8" }
+		),
 		"/project.inlang.json": JSON.stringify({
 			sourceLanguageTag: "en",
-			languageTags: ["de"],
-			modules: [
-        "../"
-      ],
+			languageTags: ["de", "en"],
+			modules: ["/plugin.js"],
+			"plugin.inlang.messageFormat": {
+				filePath: "/messages.json",
+			},
 		} satisfies ProjectSettings),
-		"/frontend": {
-			"messages.inlang.json": JSON.stringify({
-				"test.message": {
-					en: "Hello",
-					de: "Hallo",
-				},
-			}),
-		},
+		"/messages.json": JSON.stringify({
+			$schema: "https://inlang.com/schema/inlang-message-format",
+			data: [
+				createMessage("loginButton", {
+					en: "Login",
+					de: "Anmelden",
+				}),
+			],
+		}),
 	})
-
-	await compileCommand.parseAsync(["--project", "./project.inlang.json", "--namespace", "frontend"])
+	await compileCommand.parseAsync(["--project", "./project.inlang.json", "--namespace", name])
+	expect(
+		_fs.existsSync(`/node_modules/paraglide-js/dist/compiled-output/${name}/messages.js`)
+	).toBe(true)
 })
 
-const mockFs = (files: memfs.NestedDirectoryJSON) => {
-	const _memfs = memfs.createFsFromVolume(memfs.Volume.fromNestedJSON(files))
-	for (const prop in fs) {
+const mockFs = (files: memfs.DirectoryJSON) => {
+	const _memfs = memfs.createFsFromVolume(memfs.Volume.fromJSON(files))
+	for (const prop in mockedFs) {
 		// @ts-ignore - memfs has the same interface as node:fs/promises
-		if (typeof fs[prop] !== "function") continue
+		if (typeof mockedFs[prop] !== "function") continue
 		// @ts-ignore - memfs has the same interface as node:fs/promises
-		vi.spyOn(fs, prop).mockImplementation(_memfs.promises[prop])
+		vi.spyOn(mockedFs, prop).mockImplementation(_memfs.promises[prop])
 	}
 	return _memfs
 }
