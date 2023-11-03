07ce7a5a1c23fe174403accfd8679830d7bf8342
NiklasBuchfink <niklas.buchfink@web.de>
2023-11-03 14:06:08 +0100

    fix: compile wrong paraglide namespace with editor watch mode

--- packages/editor/vite.config.ts
+++ packages/editor/vite.config.ts
@@ -33,8 +33,7 @@ export default defineConfig({
 		// markdownHotModuleReload(),
 		watch({
 			pattern: "static/messages.json",
-			command:
-				"paraglide-js compile --namespace inlang-marketplace --project ../../../project.inlang.json",
+			command: "paraglide-js compile --namespace editor --project ../../../project.inlang.json",
 		}),
 	],
 	resolve: {
