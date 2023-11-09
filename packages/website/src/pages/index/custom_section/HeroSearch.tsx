d157a905ba510ef679d41c68360ac8348dbe0d65
Nils Jacobsen <nils.jacobsen98@gmail.com>
2023-11-09 14:28:36 +0100

    add popular searches

--- packages/website/src/pages/index/custom_section/HeroSearch.tsx
+++ packages/website/src/pages/index/custom_section/HeroSearch.tsx
@@ -1,6 +1,6 @@
 import { handleNavigate, setSearchInput } from "#src/interface/components/SearchBar.jsx"
 import Link from "#src/renderer/Link.jsx"
-import { createSignal } from "solid-js"
+import { For, createSignal } from "solid-js"
 import Calcom from "../old_sections/01-hero/assets/logos/clacom.jsx"
 import AppFlowy from "../old_sections/01-hero/assets/logos/appflowy.jsx"
 import Jitsi from "../old_sections/01-hero/assets/logos/jitsi.jsx"
@@ -11,16 +11,46 @@ import * as m from "#src/paraglide/messages.js"
 const HeroSearch = () => {
 	const [input, setInput] = createSignal("")
 
+	const getPopularButtons = () => {
+		return [
+			{
+				name: "ParaglideJS",
+				href: "/m/gerre34r/library-inlang-paraglideJs",
+			},
+			{
+				name: "Plugins",
+				href: "/c/plugins",
+			},
+			{
+				name: "Lint Rules",
+				href: "/c/lint-rules",
+			},
+			{
+				name: "Web Editor",
+				href: "/m/tdozzpar/app-inlang-editor",
+			},
+		]
+	}
+
 	return (
 		<div class="flex flex-col gap-2 items-center">
-			<h1 class="text-4xl md:text-6xl text-surface-900 text-center font-semibold leading-snug tracking-tight mt-8 md:mt-20">
+			<div class="pt-8 md:pt-16 group">
+				<Link href="https://www.sveltesummit.com/2023/fall" target="_blanc">
+					<div class="flex gap-4 items-center text-surface-500 group-hover:text-active-primary">
+						<Play />
+						<p>{m.home_inlang_secondary_link()}</p>
+					</div>
+				</Link>
+			</div>
+			<h1 class="text-4xl max-w-[1000px] md:text-6xl text-surface-900 text-center font-bold leading-snug tracking-tight mt-6">
 				{m.home_inlang_title()}
 			</h1>
 			<p class="text-lg max-w-[700px] text-center text-surface-500 pt-4">
 				{m.home_inlang_description()}
 			</p>
+
 			<form
-				class="relative w-full md:w-[600px] flex items-center group mt-8"
+				class="relative w-full md:w-[600px] flex items-center group mt-8 md:mt-12"
 				onSubmit={(event) => {
 					event.preventDefault()
 					setSearchInput(input())
@@ -29,12 +59,12 @@ const HeroSearch = () => {
 			>
 				<div
 					class={
-						"pl-3 pr-2 gap-2 relative z-10 flex items-center w-full border border-surface-200 bg-background rounded-lg transition-all focus-within:border-primary"
+						"pl-6 pr-2 gap-2 relative z-10 flex items-center w-full border border-surface-200 bg-background rounded-lg transition-all focus-within:border-primary"
 					}
 				>
 					<input
 						class={
-							"active:outline-0 focus:outline-0 focus:ring-0 border-0 h-14 grow placeholder:text-surface-500 placeholder:font-normal placeholder:text-base text-surface-800"
+							"pl-0 active:outline-0 focus:outline-0 focus:ring-0 border-0 h-14 grow placeholder:text-surface-700 placeholder:font-normal placeholder:text-base text-surface-800"
 						}
 						placeholder={m.home_inlang_search_placeholder()}
 						onInput={(event) => {
@@ -80,12 +110,21 @@ const HeroSearch = () => {
 					class="absolute bg-on-background top-0 left-0 w-full h-full opacity-10 blur-sm group-hover:opacity-25 group-focus-within:opacity-25"
 				/>
 			</form>
-			<div class="pt-3">
-				<Link href="https://www.sveltesummit.com/2023/fall" target="_blanc">
-					<p class="hover:text-primary text-surface-500">{m.home_inlang_secondary_link()}</p>
-				</Link>
+			<div class="flex md:items-center gap-4 pt-2">
+				<p class="pt-2 md:pt-0 text-surface-600">{m.home_inlang_search_popular() + ":"}</p>
+				<div class="flex flex-wrap items-center gap-1.5">
+					<For each={getPopularButtons()}>
+						{(item) => (
+							<Link href={item.href}>
+								<p class="text-surface-500 text-sm md:text-md border border-surface-200 rounded-lg px-3 py-1.5 hover:border-surface-400 hover:bg-background transition-all">
+									{item.name}
+								</p>
+							</Link>
+						)}
+					</For>
+				</div>
 			</div>
-			<div class="w-full xl:w-3/4 flex flex-col gap-8 px-6 md:px-4 pb-14 md:pb-4 pt-14">
+			<div class="w-full xl:w-3/4 flex flex-col gap-8 px-6 md:px-4 pb-14 md:pb-4 pt-8 md:pt-20">
 				<div class="flex gap-4 justify-center md:gap-12 items-center w-full xl:justify-center text-surface-400 flex-wrap">
 					<Link class="transition-opacity hover:opacity-80" href="https://cal.com/" target="_blank">
 						<Calcom />
@@ -125,3 +164,14 @@ const HeroSearch = () => {
 }
 
 export default HeroSearch
+
+function Play() {
+	return (
+		<svg xmlns="http://www.w3.org/2000/svg" width="20" height="18" fill="none" viewBox="0 0 20 18">
+			<path
+				fill="currentColor"
+				d="M0 3.25A3.25 3.25 0 013.25 0h13.5A3.25 3.25 0 0120 3.25v11.5A3.25 3.25 0 0116.75 18H3.25A3.25 3.25 0 010 14.75V3.25zM3.25 1.5A1.75 1.75 0 001.5 3.25v11.5c0 .966.784 1.75 1.75 1.75h13.5a1.75 1.75 0 001.75-1.75V3.25a1.75 1.75 0 00-1.75-1.75H3.25zM7 6.25v5.5a1 1 0 001.482.876l5-2.75a1 1 0 000-1.752l-5-2.75A1 1 0 007 6.251V6.25z"
+			/>
+		</svg>
+	)
+}
