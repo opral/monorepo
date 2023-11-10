c94fb9ff459cf7b2944dda112fa3787a6ee5f759
flooorianflo <florian.kiem@hfg.design>
2023-11-10 11:48:53 +0100

    feat: adjust paraglideJS product page

---
+++ packages/markdown/src/custom-elements/doc-features.ts
@@ -0,0 +1,74 @@
+import { LitElement, css, html } from "lit"
+import { customElement, property } from "lit/decorators.js"
+
+@customElement("doc-feature")
+export class DocFeature extends LitElement {
+	static override styles = css`
+		.feature-card {
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+			justify-content: end;
+			gap: 3rem;
+			padding: 1rem;
+			border-radius: 0.5rem;
+			background-color: #e3e8ed;
+			height: 176px;
+		}
+		.feature-name {
+			font-weight: 500;
+			color: #0f172a;
+			margin: 0;
+		}
+	`
+
+	@property()
+	override title: string = ""
+	@property()
+	icon?: string = ""
+	@property()
+	image?: string = ""
+	@property()
+	color?: string = ""
+
+	override render() {
+		return html`<div
+			class="feature-card"
+			style="${this.color ? `background-color: ${this.color}` : ""}"
+		>
+			${this.icon && html`<iconify-icon height="64px" icon=${this.icon}></iconify-icon>`}
+			${this.image && html`<img src=${this.image} height="64px" />`}
+			<p class="feature-name">${this.title}</p>
+		</div>`
+	}
+}
+
+@customElement("doc-features")
+export class DocFeatures extends LitElement {
+	static override styles = css`
+		.doc-features-grid {
+			display: grid;
+			grid-template-columns: repeat(3, 1fr);
+			gap: 1rem;
+			width: 100%;
+		}
+
+		@media (max-width: 768px) {
+			.doc-features-grid {
+				grid-template-columns: repeat(2, 1fr);
+			}
+		}
+
+		@media (max-width: 480px) {
+			.doc-features-grid {
+				grid-template-columns: repeat(1, 1fr);
+			}
+		}
+	`
+
+	override render() {
+		return html`<div class="doc-features-grid">
+			<slot></slot>
+		</div>`
+	}
+}
