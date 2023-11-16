6702cb4cd5cb6c0de4c7c2dcb46c32042515422a
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2023-11-16 13:53:08 +0530

    add tests

--- inlang/source-code/cli/src/utilities/versioning.ts
+++ inlang/source-code/cli/src/utilities/versioning.ts
@@ -1,5 +1,5 @@
-import { execSync } from "node:child_process"
 import semver from "semver"
+import { execSync, spawnSync } from "node:child_process"
 
 export function isMajorVersionUpdate(currentVersion: string, latestVersion: string): boolean {
 	return semver.major(currentVersion) !== semver.major(latestVersion)
@@ -7,17 +7,23 @@ export function isMajorVersionUpdate(currentVersion: string, latestVersion: stri
 
 export function getCurrentVersion(): string | void {
 	try {
-		const output = execSync("npx @inlang/cli --version", { encoding: "utf-8", stdio: "pipe" })
-		if (output) return output.toString().trim()
+		const output = execSync("npm list -g @inlang/cli", { encoding: "utf8", stdio: "pipe" })
+		// If the package is installed, extract the version and return it
+		const versionMatch = output.match(/@inlang\/cli@(\d+\.\d+\.\d+)/)
+		if (versionMatch && versionMatch[1]) {
+			return versionMatch[1]
+		}
 	} catch (error) {
-		console.error(error instanceof Error ? error.message : error)
+		/* pass */
 	}
+	// If the package is not found in the global list, return "0.0.0"
+	return "0.0.0"
 }
 
 export function getLatestVersion(): string | void {
 	try {
-		const output = execSync("npm show @inlang/cli version", { encoding: "utf-8", stdio: "pipe" })
-		if (output) return output.toString().trim()
+		const output = spawnSync("npm", ["show", "@inlang/cli", "version"])
+		if (output) return output.stdout.toString().trim()
 	} catch (error) {
 		console.error(error instanceof Error ? error.message : error)
 	}
@@ -26,7 +32,7 @@ export function getLatestVersion(): string | void {
 export function updateToLatest(show: boolean = false): void {
 	try {
 		// Execute the update command in the background
-		execSync("npm i -g @inlang/cli@latest", { stdio: "ignore" })
+		spawnSync("npm", ["i", "-g", "@inlang/cli@latest"])
 		if (show) console.info("Updated @inlang/cli to the latest.")
 	} catch (error) {
 		if (show) {
