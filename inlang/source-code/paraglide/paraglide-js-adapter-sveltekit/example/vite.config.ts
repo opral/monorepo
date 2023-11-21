028977566668901b7620da4a4c20ed66415e657a
Loris Sigrist <sigrist@hasch.io>
2023-11-21 09:33:11 +0100

    Add vite & rollup plugins

--- inlang/source-code/paraglide/paraglide-js-adapter-sveltekit/example/vite.config.ts
+++ inlang/source-code/paraglide/paraglide-js-adapter-sveltekit/example/vite.config.ts
@@ -1,8 +1,15 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { paraglide } from "@inlang/paraglide-js-adapter-unplugin"
+import { defineConfig } from "vite"
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [
+		sveltekit(),
+		paraglide.vite({
+			project: "./project.inlang.json",
+			outdir: "./src/paraglide",
+		}),
+	],
 	// for easier debugging, don't minify
 	build: {
 		minify: false,
