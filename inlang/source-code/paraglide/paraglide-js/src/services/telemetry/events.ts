c17553f1f6523fb8e3165b52786f46b5cc58bd75
Loris Sigrist <sigrist@hasch.io>
2023-11-21 16:02:44 +0100

    Also track `paraglide-js init`

--- inlang/source-code/paraglide/paraglide-js/src/services/telemetry/events.ts
+++ inlang/source-code/paraglide/paraglide-js/src/services/telemetry/events.ts
@@ -3,4 +3,4 @@
  *
  * Exists to avoid typos/always set the correct event name and properties.
  */
-export type TelemetryEvents = "Paraglide compile executed" | "CLI started"
+export type TelemetryEvents = "Paraglide compile executed" | "Paraglide init executed"
