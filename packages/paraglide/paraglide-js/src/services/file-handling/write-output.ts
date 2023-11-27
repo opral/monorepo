5c172ed910e7de5e3e4445be30a8e4740ff081b8
Loris Sigrist <sigrist@hasch.io>
2023-11-27 11:27:31 +0100

    Expose programmatic compiler function from paraglide-js

---
+++ packages/paraglide/paraglide-js/src/services/file-handling/write-output.ts
@@ -0,0 +1,41 @@
+import type nodeFsPromises from "fs/promises"
+import path from "path"
+
+export async function writeOutput(
+	outputDirectory: string,
+	output: Record<string, string>,
+	fs: typeof nodeFsPromises
+) {
+	// create the output directory if it doesn't exist
+	await fs.access(outputDirectory).catch(async () => {
+		await fs.mkdir(outputDirectory, { recursive: true })
+	})
+
+	// clear the output directory
+	const files = await fs.readdir(outputDirectory)
+	await Promise.allSettled(
+		files.map(async (file) => {
+			await fs.unlink(outputDirectory + "/" + file)
+		})
+	)
+
+	//Create missing directories inside the output directory
+	await Promise.allSettled(
+		Object.keys(output).map(async (filePath) => {
+			const fullPath = path.resolve(outputDirectory, filePath)
+			const directory = path.dirname(fullPath)
+			await fs.mkdir(directory, { recursive: true })
+		})
+	)
+
+	//Write files
+	await Promise.allSettled(
+		Object.entries(output).map(async ([filePath, fileContent]) => {
+			const fullPath = path.resolve(outputDirectory, filePath)
+
+			await fs.writeFile(fullPath, fileContent, {
+				encoding: "utf-8",
+			})
+		})
+	)
+}
