d20d085d5985f76db477aca690e66b1ea85395ab
flooorianflo <florian.kiem@hfg.design>
2023-11-27 11:48:40 +0100

    feat: implement tailwind for inlang/manage
    easier to maintain

---
+++ inlang/source-code/manage/src/pages/IndexPage.ts
@@ -0,0 +1,18 @@
+// Components
+import type { TemplateResult } from "lit"
+import { html } from "lit"
+import { customElement } from "lit/decorators.js"
+import { TwLitElement } from "../common/TwLitElement"
+
+import "../components/HelloWorld"
+
+@customElement("x-index-page")
+export class IndexPage extends TwLitElement {
+	override render(): TemplateResult {
+		return html`
+			<div class="container">
+				<x-hello-world></x-hello-world>
+			</div>
+		`
+	}
+}
---
+++ inlang/source-code/manage/tailwind.config.cjs
@@ -0,0 +1,12 @@
+/* eslint-disable no-undef */
+
+module.exports = {
+	content: ["src/**/*.{ts,html}"],
+	theme: {
+		container: {
+			center: true,
+		},
+		extend: {},
+	},
+	plugins: [],
+}
