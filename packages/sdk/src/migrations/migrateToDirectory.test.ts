091ebd2589ff3909aba5a8a1e34cf855bc35ab58
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2023-11-27 16:13:34 -0500

    refactor: migrate to project directories

--- packages/sdk/src/migrations/migrateToDirectory.test.ts
+++ packages/sdk/src/migrations/migrateToDirectory.test.ts
@@ -1,31 +1,40 @@
 import { test, expect, vi } from "vitest"
-import { migrateToDirectory } from "./migrateToDirectory.js"
+import { maybeMigrateToDirectory } from "./migrateToDirectory.js"
 import { createNodeishMemoryFs } from "@lix-js/fs"
 import type { ProjectSettings } from "@inlang/project-settings"
 
 test("it should return if the settings file has an error (let loadProject handle it)", async () => {
-	const settingsFilePath = "./project.inlang.json"
+	const projectPath = "./project.inlang"
 	const mockFs = {
+		stat: vi.fn(() => {}),
 		readFile: vi.fn(() => {
 			throw Error()
 		}),
 	}
-	expect(() => migrateToDirectory({ fs: mockFs as any, settingsFilePath })).not.toThrow()
+	await maybeMigrateToDirectory({ nodeishFs: mockFs as any, projectPath })
+	// something goes wrong in readFile
 	expect(mockFs.readFile).toHaveBeenCalled()
 })
 
-test("it should create the project directory if it does not exist", async () => {
-	const settingsFilePath = "./project.inlang.json"
+test("it should create the project directory if it does not exist and a project settings file exists", async () => {
+	const projectPath = "./project.inlang"
 	const mockFs = {
-		readFile: vi.fn(() => "{}"),
+		readFile: vi.fn(
+			() => `{
+			"sourceLanguageTag": "en",
+			"languageTags": ["en", "de"],
+			"modules": []
+		}`
+		),
 		stat: vi.fn(() => {
 			throw Error()
 		}),
 		mkdir: vi.fn(),
 		writeFile: vi.fn(),
 	}
-	await migrateToDirectory({ fs: mockFs as any, settingsFilePath })
+	await maybeMigrateToDirectory({ nodeishFs: mockFs as any, projectPath })
 	expect(mockFs.mkdir).toHaveBeenCalled()
+	expect(mockFs.writeFile).toHaveBeenCalled()
 })
 
 test("it should write the settings file to the new path", async () => {
@@ -36,7 +45,7 @@ test("it should write the settings file to the new path", async () => {
 		modules: [],
 	}
 	await fs.writeFile("./project.inlang.json", JSON.stringify(mockSettings))
-	await migrateToDirectory({ fs, settingsFilePath: "./project.inlang.json" })
+	await maybeMigrateToDirectory({ nodeishFs: fs, projectPath: "./project.inlang" })
 	const migratedSettingsFile = await fs.readFile("./project.inlang/settings.json", {
 		encoding: "utf-8",
 	})
--- packages/sdk/src/migrations/migrateToDirectory.ts
+++ packages/sdk/src/migrations/migrateToDirectory.ts
@@ -5,26 +5,35 @@ import type { NodeishFilesystem } from "@lix-js/fs"
  * Migrates to the new project directory structure
  * https://github.com/inlang/monorepo/issues/1678
  */
-export const migrateToDirectory = async (args: {
-	fs: NodeishFilesystem
-	settingsFilePath: string
+export const maybeMigrateToDirectory = async (args: {
+	nodeishFs: NodeishFilesystem
+	projectPath: string
 }) => {
+	// the migration assumes that the projectPath ends with project.inlang
+	if (args.projectPath.endsWith("project.inlang") === false) {
+		return
+	}
+
+	// we assume that stat will throw when the directory does not exist
+	const projectDirectory = await tryCatch(() => args.nodeishFs.stat(args.projectPath))
+
+	// the migration has already been conducted.
+	if (projectDirectory.data) {
+		return
+	}
+
 	const settingsFile = await tryCatch(() =>
-		args.fs.readFile(args.settingsFilePath, { encoding: "utf-8" })
+		args.nodeishFs.readFile(args.projectPath + ".json", { encoding: "utf-8" })
 	)
+
 	// the settings file does not exist or something else is wrong, let loadProject handle it
 	if (settingsFile.error) {
 		return
 	}
-	// ./project.inlang.json -> ./project.inlang
-	const directoryPath = args.settingsFilePath.replace(/\.json$/, "")
-	// (Should be OK) naively assuming that the directory does not exist if stat throws
-	const { error: directoryDoesNotExist } = await tryCatch(() => args.fs.stat(directoryPath))
-	if (directoryDoesNotExist) {
-		await args.fs.mkdir(directoryPath)
-	}
-	await args.fs.writeFile(`${directoryPath}/settings.json`, settingsFile.data)
-	await args.fs.writeFile(args.settingsFilePath.replace(/\.json$/, ".README.md"), readme)
+
+	await args.nodeishFs.mkdir(args.projectPath)
+	await args.nodeishFs.writeFile(`${args.projectPath}/settings.json`, settingsFile.data)
+	await args.nodeishFs.writeFile(args.projectPath + ".README.md", readme)
 }
 
 const readme = `
@@ -41,7 +50,8 @@ The \`project.inlang.json\` file is now contained in a project directory e.g. \`
 
 ## Why is this happening?
 
-See this RFC https://docs.google.com/document/d/1OYyA1wYfQRbIJOIBDliYoWjiUlkFBNxH_U2R4WpVRZ4/edit#heading=h.pecv6xb7ial6.
+See this RFC https://docs.google.com/document/d/1OYyA1wYfQRbIJOIBDliYoWjiUlkFBNxH_U2R4WpVRZ4/edit#heading=h.pecv6xb7ial6 
+and the following GitHub issue for more information https://github.com/inlang/monorepo/issues/1678.
 
 - Monorepo support https://github.com/inlang/monorepo/discussions/258. 
 - Required for many other future features like caching, first class offline support, and more. 
