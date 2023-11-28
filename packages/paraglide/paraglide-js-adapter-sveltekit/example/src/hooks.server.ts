6ee4d79a16f37d7bfeb9ee77a48ebdc2f65f9928
Loris Sigrist <sigrist@hasch.io>
2023-11-28 18:11:48 +0100

    Make static export work

--- packages/paraglide/paraglide-js-adapter-sveltekit/example/src/hooks.server.ts
+++ packages/paraglide/paraglide-js-adapter-sveltekit/example/src/hooks.server.ts
@@ -1,10 +1,20 @@
 import { getTextDirection } from "$lib/i18n"
 import { sourceLanguageTag, type AvailableLanguageTag } from "$paraglide/runtime"
 
+/*
+	This whole file could be made redundant if svelte
+	introduces a `<svelte:html>` tag that allows us to
+	do this in the template.
+
+	`+layout.svelte` does the same thing on the client side.
+*/
+
+
 export async function handle({ event, resolve }) {
 	const lang: AvailableLanguageTag =
 		(event.params.lang as AvailableLanguageTag) ?? sourceLanguageTag
 	const textDirection = getTextDirection(lang)
+
 
 	return await resolve(event, {
 		transformPageChunk({ done, html }) {
--- packages/paraglide/paraglide-js-adapter-sveltekit/example/src/routes/[[lang=lang]]/+page.svelte
+++ packages/paraglide/paraglide-js-adapter-sveltekit/example/src/routes/[[lang=lang]]/+page.svelte
@@ -2,7 +2,7 @@
 	import { availableLanguageTags, languageTag } from "../../paraglide/runtime"
 	import { translatePath } from "$lib/i18n"
 	import { goto } from "$app/navigation"
-  import * as m from "../../paraglide/messages"
+	import * as m from "../../paraglide/messages"
 </script>
 
 <p>{m.greeting({ name: "Samuel", count: 5 })}</p>
@@ -10,12 +10,19 @@
 
 <h3>Switch Locales using Links</h3>
 
-{#each availableLanguageTags as lang}
-	<a href={translatePath("/", lang)} lang="de">change language to "{lang}"</a>
-{/each}
+<div role="radiogroup">
+	{#each availableLanguageTags as lang}
+		<a href={translatePath("/", lang)} {lang} hreflang={lang} aria-current={lang === languageTag()}
+			>{m.change_language_to({ languageTag: lang }, { languageTag: lang })}</a
+		>
+		<br />
+	{/each}
+</div>
 
-<h3>Switch Locales using Buttons</h3>
+<h3>Switch Locales Programatically</h3>
 
-{#each availableLanguageTags as lang}
-  <button on:click={() => goto(translatePath("/", lang))}>change language to "{lang}"</button>
-{/each}
+<select on:change={(e) => goto(translatePath("/", e.target.value))}>
+	{#each availableLanguageTags as lang}
+		<option value={lang} selected={lang === languageTag()}>{lang}</option>
+	{/each}
+</select>
