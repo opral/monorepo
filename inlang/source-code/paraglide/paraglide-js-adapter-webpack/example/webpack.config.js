7719ebded36dae65e27296dbc2c1f4d546605ed2
Loris Sigrist <sigrist@hasch.io>
2023-11-28 10:23:12 +0100

    Use named export for webpack plugin

--- inlang/source-code/paraglide/paraglide-js-adapter-webpack/example/webpack.config.js
+++ inlang/source-code/paraglide/paraglide-js-adapter-webpack/example/webpack.config.js
@@ -1,6 +1,6 @@
 import path from "path"
 import { fileURLToPath } from "url"
-import Paraglide from "@inlang/paraglide-js-adapter-webpack"
+import { paraglide } from "@inlang/paraglide-js-adapter-webpack"
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
@@ -18,7 +18,7 @@ export default {
 	},
 
 	plugins: [
-		Paraglide({
+		paraglide({
 			project: "./project.inlang.json",
 			outdir: "./src/paraglide",
 		}),
