a57954731a717164286a57e08100618c1ee72351
flooorianflo <florian.kiem@hfg.design>
2023-11-28 14:12:51 +0100

    feat: include manual install instructions

--- inlang/source-code/manage/src/components/InlangInstall.ts
+++ inlang/source-code/manage/src/components/InlangInstall.ts
@@ -13,15 +13,23 @@ export class InlangInstall extends TwLitElement {
 	@property({ type: String })
 	step: string = ""
 
+	@property({ type: String })
+	jsonURL: string = ""
+
 	@property({ type: Object })
 	url: Record<string, string> = {}
 
-	// @property({ type: String })
-	// url: string = ""
+	@property({ type: String })
+	module: string | undefined = ""
+
+	@property({ type: Boolean })
+	copied: boolean = false
 
 	override connectedCallback() {
 		super.connectedCallback()
-		console.log(this.url)
+		this.url = JSON.parse(this.jsonURL)
+		// @ts-ignore
+		if (this.url.module) this.module = registry.find((x) => x.id === this.url.module)?.module
 	}
 
 	override render(): TemplateResult {
@@ -54,8 +62,8 @@ export class InlangInstall extends TwLitElement {
 				</button>
 			</div>
 			${this.manual
-				? html`<div class="fixed inset-0 z-10 bg-black/10 flex items-center justify-center px-4">
-						<div class="max-w-lg w-full h-96 bg-slate-100 rounded-xl relative p-4">
+				? html`<div class="fixed inset-0 z-10 bg-black/25 flex items-center justify-center px-4">
+						<div class="max-w-lg w-full bg-slate-100 rounded-xl relative p-4">
 							<doc-icon
 								class="absolute top-2 right-2 bg-slate-200 rounded-full p-1 cursor-pointer transition-colors hover:bg-slate-300"
 								size="1.6em"
@@ -64,11 +72,85 @@ export class InlangInstall extends TwLitElement {
 									this.manual = false
 								}}
 							></doc-icon>
-							<div class="w-full border border-slate-200 rounded-lg p-4">
+							<div class="w-full border border-slate-200 rounded-lg p-4 mb-4">
 								<h3 class="text-lg font-semibold mb-2 leading-none">Copy the module</h3>
 								<p class="text-slate-500 mb-4">Copy the module you've selected:</p>
-								<code>npm install @inlang ${this.query.path}</code>
+								<div
+									class="flex gap-4 md:items-center w-full flex-col-reverse md:flex-row overflow-x-auto md:pb-2 pb-6 p-2 rounded-md bg-slate-200"
+								>
+									<code class="text-sm py-2">${this.module}</code>
+									<div
+										class="flex items-center gap-2 flex-shrink-0 ml-auto"
+										@click=${() => {
+											// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+											navigator.clipboard.writeText(this.module!)
+											this.copied = true
+											setTimeout(() => {
+												this.copied = false
+											}, 1000)
+										}}
+									>
+										${
+											this.copied
+												? html`<doc-icon
+														class="cursor-pointer"
+														size="1.4em"
+														icon="ic:check"
+												  ></doc-icon>`
+												: html` <doc-icon
+														class="cursor-pointer"
+														size="1.4em"
+														icon="ic:content-copy"
+												  ></doc-icon>`
+										}
+									</div>
+								</div>
 							</div>
+							<div class="w-full border border-slate-200 rounded-lg p-4 mb-8">
+								<h3 class="text-lg font-semibold mb-2 leading-none">
+									Paste the link into the project
+								</h3>
+								<p class="text-slate-500 mb-4">
+									Paste the copied link into the project.inlang.json file inside the modules
+									section:
+								</p>
+								<div
+									class="flex md:gap-4 md:items-center w-full flex-col md:flex-row relative md:pb-2 p-2 rounded-md bg-slate-200"
+								>
+									<pre class=" overflow-x-auto">
+									<code class="text-sm"
+										>${`
+{
+	...
+		"modules": [
+			"${this.module}"
+		],
+	...
+}
+									`}</code
+									>
+									</pre>
+									<div
+										class="flex items-center gap-2 flex-shrink-0 absolute top-3 right-2"
+										@click=${() => {
+											// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+											navigator.clipboard.writeText(this.module!)
+											this.copied = true
+											setTimeout(() => {
+												this.copied = false
+											}, 1000)
+										}}
+									></div>
+								</div>
+							</div>
+							<a
+								href="https://inlang.com/g/49fn9ggo/guide-niklasbuchfink-howToSetupInlang"
+								target="_blank"
+								class="text-[#098DAC] font-medium transition-colors hover:text-[#06b6d4]"									"
+							>
+								You don't have project file? Read how to get started
+								<doc-icon class="inline-block ml-1 translate-y-0.5" size="1.2em" icon="mdi:arrow-top-right"></doc-icon>
+							</a>
 						</div>
 				  </div>`
 				: ""} `
