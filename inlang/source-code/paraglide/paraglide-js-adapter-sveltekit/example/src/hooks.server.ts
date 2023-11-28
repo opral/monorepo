677bd843e90754406419c697494989d96a96a4ec
Loris Sigrist <sigrist@hasch.io>
2023-11-28 17:41:52 +0100

    Drastically simplify SvelteKit example

---
+++ inlang/source-code/paraglide/paraglide-js-adapter-sveltekit/example/src/hooks.server.ts
@@ -0,0 +1,16 @@
+import { getTextDirection } from "$lib/i18n"
+import { sourceLanguageTag, type AvailableLanguageTag } from "$paraglide/runtime"
+
+export async function handle({ event, resolve }) {
+	const lang: AvailableLanguageTag =
+		(event.params.lang as AvailableLanguageTag) ?? sourceLanguageTag
+	const textDirection = getTextDirection(lang)
+
+	return await resolve(event, {
+		transformPageChunk({ done, html }) {
+			if (done) {
+				return html.replace("%lang%", lang).replace("%textDir%", textDirection)
+			}
+		},
+	})
+}
