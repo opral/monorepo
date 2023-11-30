674590d8b27feaf0d5b07debdae6a951022bcde0
Loris Sigrist <sigrist@hasch.io>
2023-11-30 11:26:28 +0100

    save

--- packages/paraglide/paraglide-js-adapter-next/example-app/src/ClientsideLanguageProvider.tsx
+++ packages/paraglide/paraglide-js-adapter-next/example-app/src/ClientsideLanguageProvider.tsx
@@ -1,13 +1,7 @@
 "use client"
 import { AvailableLanguageTag, setLanguageTag } from "@/paraglide/runtime"
-import type { ReactNode } from "react"
 
-// This component sets the language tag on the client side.
-
-export function ClientSideLanguageProvider(props: {
-	language: AvailableLanguageTag
-	children?: ReactNode
-}) {
+export function ClientSideLanguageProvider(props: { language: AvailableLanguageTag }) {
 	setLanguageTag(props.language)
-	return <>{props.children}</>
+	return null
 }
