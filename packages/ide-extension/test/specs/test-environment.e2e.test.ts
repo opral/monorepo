0464cc8ef9bb23be3ef01887c7f977b77ee007a4
Robin Bühler <git+r@obin.ch>
2023-12-01 15:48:41 +0000

    test(e2e): vscode extension loadable

--- packages/ide-extension/test/specs/test-environment.e2e.test.ts
+++ packages/ide-extension/test/specs/test-environment.e2e.test.ts
@@ -1,4 +1,5 @@
 import { browser, expect } from "@wdio/globals"
+import vscode from "vscode"
 import {} from "wdio-vscode-service"
 
 describe("VSCode Extension E2E Testing Environment", () => {
@@ -7,5 +8,12 @@ describe("VSCode Extension E2E Testing Environment", () => {
 		const title = await workbench.getTitleBar().getTitle()
 		expect(title).toContain("[Extension Development Host]")
 		expect(title).toContain("inlang")
+	})
+
+	it("should load and install our VSCode Extension", async () => {
+		const extensions = await browser.executeWorkbench((vscodeApi: typeof vscode) => {
+			return vscodeApi.extensions.all
+		})
+		expect(extensions.some((extension) => extension.id === "inlang.vs-code-extension")).toBe(true)
 	})
 })
