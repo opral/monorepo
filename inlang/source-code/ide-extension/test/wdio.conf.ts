3781ea1e73502e862bb8d4ec59fa19356f8aa43c
Robin Bühler <git+r@obin.ch>
2023-12-01 15:18:41 +0000

    chore: enhance vscode e2e test setup

--- inlang/source-code/ide-extension/test/wdio.conf.ts
+++ inlang/source-code/ide-extension/test/wdio.conf.ts
@@ -69,7 +69,7 @@ export const config: Options.Testrunner = {
 			"wdio:vscodeOptions": {
 				// points to directory where extension package.json is located
 				extensionPath: path.join(__dirname, ".."),
-				workspacePath: path.join(__dirname, "../../../end-to-end-tests/inlang-nextjs"),
+				workspacePath: path.join(__dirname, "../../end-to-end-tests/inlang-nextjs"),
 				// optional VS Code settings
 				userSettings: {
 					"editor.fontSize": 14,
