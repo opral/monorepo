862197b9a2f5d4bc3d99e1a0f361f2a664e14f11
Loris Sigrist <sigrist@hasch.io>
2023-12-01 15:57:22 +0100

    Fix race condition

--- inlang/source-code/paraglide/paraglide-js-adapter-sveltekit/example/src/routes/[[lang=lang]]/+page.svelte
+++ inlang/source-code/paraglide/paraglide-js-adapter-sveltekit/example/src/routes/[[lang=lang]]/+page.svelte
@@ -6,18 +6,13 @@
 <p>{m.greeting({ name: "Samuel", count: 5 })}</p>
 <p>{m.currentLanguageTag({ languageTag: languageTag() })}</p>
 
-
-
-<a href="/not-translated" data-no-translate hreflang="en">Not translated</a>
 <a href="/about">{m.about()}</a>
-
 
 <h3>Switch Locales using Links</h3>
 
 {#each availableLanguageTags as lang}
 	<a href="/" hreflang={lang}>
 		{m.change_language_to({ languageTag: lang })}
-	</a
-	>
+	</a>
 	<br />
 {/each}
