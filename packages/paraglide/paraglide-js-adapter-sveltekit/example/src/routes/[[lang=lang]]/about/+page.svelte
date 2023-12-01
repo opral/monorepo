dd06a25e4da914536d51e3722eeeb844fbe80f12
Loris Sigrist <sigrist@hasch.io>
2023-12-01 10:03:55 +0100

    stash

--- packages/paraglide/paraglide-js-adapter-sveltekit/example/src/routes/[[lang=lang]]/about/+page.svelte
+++ packages/paraglide/paraglide-js-adapter-sveltekit/example/src/routes/[[lang=lang]]/about/+page.svelte
@@ -1,1 +1,3 @@
+<script></script>
+
 <h1>About</h1>
--- packages/paraglide/paraglide-js-adapter-sveltekit/example/vite.config.ts
+++ packages/paraglide/paraglide-js-adapter-sveltekit/example/vite.config.ts
@@ -1,5 +1,5 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { paraglide } from "@inlang/paraglide-js-adapter-vite"
+import { paraglide } from "@inlang/paraglide-js-adapter-sveltekit"
 import { defineConfig } from "vite"
 
 export default defineConfig({
