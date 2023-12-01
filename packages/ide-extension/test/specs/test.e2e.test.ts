d25ac4c8340cc01dbba2b97532faecb50bc262cd
Robin Bühler <git+r@obin.ch>
2023-12-01 15:18:41 +0000

    chore: enhance vscode e2e test setup

--- packages/ide-extension/test/specs/test.e2e.test.ts
+++ packages/ide-extension/test/specs/test.e2e.test.ts
@@ -1,10 +1,10 @@
 import { browser, expect } from '@wdio/globals'
+import {} from "wdio-vscode-service"
 
-describe('VS Code Extension Testing', () => {
-    it('should be able to load VSCode', async () => {
-        const workbench = await browser.getWorkbench()
-        expect(await workbench.getTitleBar().getTitle())
-            .toContain('[Extension Development Host]')
-    })
+describe("VS Code Extension Testing", () => {
+	it("should be able to load VSCode", async () => {
+		const workbench = await browser.getWorkbench()
+		expect(await workbench.getTitleBar().getTitle()).toContain("[Extension Development Host]")
+	})
 })
 
