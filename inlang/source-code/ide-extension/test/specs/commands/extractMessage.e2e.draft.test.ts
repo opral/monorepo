213a039b2574b2240b47541f6a7a4979686124b7
Robin Bühler <git+r@obin.ch>
2023-12-04 13:13:06 +0000

    test(e2e): extract message command

--- inlang/source-code/ide-extension/test/specs/commands/extractMessage.e2e.draft.test.ts
+++ inlang/source-code/ide-extension/test/specs/commands/extractMessage.e2e.draft.test.ts
@@ -4,14 +4,27 @@ import {} from "wdio-vscode-service"
 
 describe("Extract Message command", () => {
 	it("should be able to extract messages via Command Palette", async () => {
-		const testFile = "app/[lng]/page.js"
+		// remember that the slashes are different on windows
+		const testFile = "page.js"
+		const testFilePath = `app [lng] ${testFile}`
 		const testKey = "e2etest"
 		const testCommand = "inlang: extract message"
 		const workbench = await browser.getWorkbench()
+
+		// open test file
 		const prompt = await workbench.openCommandPrompt()
-		await prompt.setText(testFile)
+		await prompt.setText(testFilePath)
+		await prompt.wait(30000)
 		await prompt.confirm()
 
+		// wait until test file is opened
+		await browser.waitUntil(async () => {
+			const editorView = workbench.getEditorView()
+			const tabs = await editorView.getOpenTabs()
+			return tabs.some(async (tab) => (await tab.getTitle()) === testFile)
+		})
+
+		// select text
 		await browser.executeWorkbench(async (vscodeApi: typeof vscode) => {
 			const editor = vscodeApi.window.activeTextEditor
 			const search = "Translate me"
@@ -27,10 +40,16 @@ describe("Extract Message command", () => {
 			editor.selection = new vscodeApi.Selection(startPosition, endPosition)
 		})
 
+		await browser.pause(30000)
+
+		// execute extract message command
 		const command = await workbench.executeCommand(testCommand)
 		await command.setText(testKey)
+		await command.wait(30000)
 		await command.confirm()
+
 		const picks = await command.getQuickPicks()
+		await command.wait(30000)
 		const pick = picks.find(async (p) => {
 			const label = await p.getLabel()
 			return label.includes(testKey)
@@ -48,6 +67,7 @@ describe("Extract Message command", () => {
 
 			return editor.document.getText()
 		})
+		await browser.pause(30000)
 
 		expect(editorText).toContain(testKey)
 	})
