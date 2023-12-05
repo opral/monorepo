b0f8c15946cd2abe304fd7627c2117feafac5bcf
Loris Sigrist <sigrist@hasch.io>
2023-12-05 14:19:29 +0100

    Allow forcing the languageTag

--- packages/paraglide/paraglide-js-adapter-sveltekit/example/src/routes/+error.svelte
+++ packages/paraglide/paraglide-js-adapter-sveltekit/example/src/routes/+error.svelte
@@ -1,5 +1,9 @@
 <script>
     import * as m from "$paraglide/messages.js"
+    import { ParaglideJS } from "@inlang/paraglide-js-adapter-sveltekit"
 </script>
 
-<a href="/">{m.home()}</a>
+
+<ParaglideJS languageTag="en">
+    <a href="/">{m.home()}</a>
+</ParaglideJS>
