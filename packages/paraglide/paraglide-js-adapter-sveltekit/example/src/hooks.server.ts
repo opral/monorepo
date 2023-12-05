f1065a20cdabe4ad0b176deab7836d1433ae8d38
Loris Sigrist <sigrist@hasch.io>
2023-12-05 14:32:00 +0100

    Add hook for injecting language Tag

--- packages/paraglide/paraglide-js-adapter-sveltekit/example/src/hooks.server.ts
+++ packages/paraglide/paraglide-js-adapter-sveltekit/example/src/hooks.server.ts
@@ -1,19 +1,2 @@
-import type { AvailableLanguageTag } from "$paraglide/runtime"
-import getLanguage from "$paraglide-adapter-sveltekit:get-language"
-
-/*
-	We set the `lang` and `dir` attributes on the `<html>` element using a hook.
-	the `app.html` file contains placeholders for these attributes, which we just find and replace.
-*/
-
-export async function handle({ event, resolve }) {
-	const lang: AvailableLanguageTag = getLanguage(event.url)
-
-	return await resolve(event, {
-		transformPageChunk({ done, html }) {
-			if (done) {
-				return html.replace("%lang%", lang)
-			}
-		},
-	})
-}
+import { injectLangAttribute } from "@inlang/paraglide-js-adapter-sveltekit"
+export const handle = injectLangAttribute("%lang%")
--- packages/paraglide/paraglide-js-adapter-sveltekit/src/runtime/index.ts
+++ packages/paraglide/paraglide-js-adapter-sveltekit/src/runtime/index.ts
@@ -1,2 +1,3 @@
 export { default as ParaglideJS } from "./ParaglideJS.svelte"
+export { injectLangAttribute } from "./injectLangAttribute.js"
 export { goto } from "./goto.js"
