41c6a0c1db7c546cc697d9d13aed27d05ae169c9
Nils Jacobsen <nils.jacobsen98@gmail.com>
2023-12-05 08:27:29 +0100

    Add scroll container

--- packages/website/src/interface/sdkDocs/SdkDocsLayout.tsx
+++ packages/website/src/interface/sdkDocs/SdkDocsLayout.tsx
@@ -6,7 +6,7 @@ const SdkDocsLayout = (props: { children: JSXElement }) => {
 	return (
 		<div class="bg-surface-50 min-h-screen">
 			<SdkDocsHeader />
-			<div class="w-full px-4 md:px-12 xl:px-4 min-h-[calc(100vh_-_107px_-_480px)]">
+			<div class="w-full px-4 md:px-8 xl:px-4 min-h-[calc(100vh_-_107px_-_480px)]">
 				<div class="max-w-7xl mx-auto">{props.children}</div>
 			</div>
 			<Footer />
