09e64dc49cd85a0463a066736af7fa881ffb892a
NiklasBuchfink <niklas.buchfink@web.de>
2023-12-06 17:56:30 +0100

    chore: remove outdated guide from error page

--- packages/editor/src/pages/@host/@owner/@repository/components/Errors.tsx
+++ packages/editor/src/pages/@host/@owner/@repository/components/Errors.tsx
@@ -82,16 +82,16 @@ export const Errors = (props: { errors: Array<any>; message: string; messagePlur
 								</div>
 							</>
 						)}
-						<Show when={props.errors[visibleError()]?.name === "ProjectSettingsFileNotFoundError"}>
+						{/* <Show when={props.errors[visibleError()]?.name === "ProjectSettingsFileNotFoundError"}>
 							<sl-button
 								class="w-full"
 								prop:href="/g/49fn9ggo/guide-niklasbuchfink-howToSetupInlang"
 							>
-								{/* @ts-ignore */}
+
 								<MaterialSymbolsDeveloperGuide class="w-6 h-6" slot="prefix" />
 								Check the guide if you need help setting up inlang
 							</sl-button>
-						</Show>
+						</Show> */}
 					</div>
 				)}
 			</div>
