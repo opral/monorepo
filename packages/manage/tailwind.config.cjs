2675c37c1f7559596f3789d5e0712b59689fc39b
flooorianflo <florian.kiem@hfg.design>
2023-12-08 12:35:20 +0100

    feat: implement inter font

--- packages/manage/tailwind.config.cjs
+++ packages/manage/tailwind.config.cjs
@@ -1,10 +1,17 @@
 /* eslint-disable no-undef */
 
+import defaultTheme from "tailwindcss/defaultTheme"
+
 module.exports = {
 	content: ["src/**/*.{ts,html}"],
 	theme: {
 		container: {
 			center: true,
+		},
+		extends: {
+			fontFamily: {
+				sans: ["Inter var", "Inter", ...defaultTheme.fontFamily.sans],
+			},
 		},
 	},
 	plugins: [],
