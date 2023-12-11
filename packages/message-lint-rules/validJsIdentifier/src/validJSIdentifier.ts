d0b5557bba299b8ef7fcc48ae10ff0b719bee446
Loris Sigrist <sigrist@hasch.io>
2023-12-11 10:52:01 +0100

    Add copyright info

--- packages/message-lint-rules/validJsIdentifier/src/validJSIdentifier.ts
+++ packages/message-lint-rules/validJsIdentifier/src/validJSIdentifier.ts
@@ -23,6 +23,11 @@ export const validJsIdentifier: MessageLintRule = {
 	},
 }
 
+/*
+ * is-var-name | ISC (c) Shinnosuke Watanabe
+ * https://github.com/shinnn/is-var-name
+*/
+
 function isValidJsIdentifier(str: string): boolean {
 	if (str.trim() !== str) {
 		return false
