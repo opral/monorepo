f264c7cffe2adb0e005a83ce60a36fede906c84b
jannesblobel <72493222+jannesblobel@users.noreply.github.com>
2023-12-12 14:49:32 +0100

    add groupidentify

--- packages/paraglide/paraglide-js/src/cli/commands/compile.ts
+++ packages/paraglide/paraglide-js/src/cli/commands/compile.ts
@@ -6,6 +6,7 @@ import { Command } from "commander"
 import { telemetry } from "../../services/telemetry/implementation.js"
 import { writeOutput } from "../../services/file-handling/write-output.js"
 import { Logger } from "../../services/logger/index.js"
+import { gitOrigin } from "../../services/telemetry/implementation.js"
 
 export const compileCommand = new Command()
 	.name("compile")
@@ -23,6 +24,13 @@ export const compileCommand = new Command()
 		const outputDirectory = resolve(process.cwd(), options.outdir)
 
 		logger.info(`Compiling inlang project at "${options.project}".`)
+		telemetry.groupIdentify({
+			groupType: "repository",
+			groupKey: gitOrigin,
+			properties: {
+				name: gitOrigin,
+			},
+		})
 		const project = exitIfErrors(
 			await loadProject({
 				projectPath: path,
