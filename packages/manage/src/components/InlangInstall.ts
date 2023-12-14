248049fd6f0f4cf9207b3ff827ae6f9619aee452
flooorianflo <florian.kiem@hfg.design>
2023-12-14 11:46:49 +0100

    fix: improvements of install

--- packages/manage/src/components/InlangInstall.ts
+++ packages/manage/src/components/InlangInstall.ts
@@ -5,10 +5,11 @@ import { customElement, property } from "lit/decorators.js"
 import { TwLitElement } from "../common/TwLitElement.js"
 import { browserAuth, getUser } from "@lix-js/client/src/browser-auth.ts"
 import { registry } from "@inlang/marketplace-registry"
-import { ProjectSettings, loadProject } from "@inlang/sdk"
+import { ProjectSettings, loadProject, listProjects } from "@inlang/sdk"
 import { detectJsonFormatting } from "@inlang/detect-json-formatting"
 import { tryCatch } from "@inlang/result"
 import { publicEnv } from "@inlang/env-variables"
+import { z } from "zod"
 
 @customElement("inlang-install")
 export class InlangInstall extends TwLitElement {
@@ -61,6 +62,9 @@ export class InlangInstall extends TwLitElement {
 
 	@property()
 	error: string | string[] = "You've encountered an unknown error. Please try again later."
+
+	@property({ type: Array })
+	projects: Record<string, any>[] | undefined = undefined
 
 	/* This function uses lix to inject the module into the inlang project */
 	async install() {
@@ -241,6 +245,20 @@ export class InlangInstall extends TwLitElement {
 			this.step = "norepo"
 			this.loading = false
 		} else if (!this.url.project) {
+			const repo = await openRepository(
+				`${publicEnv.PUBLIC_GIT_PROXY_BASE_URL}/git/${this.url.repo}`,
+				{
+					nodeishFs: createNodeishMemoryFs(),
+				}
+			)
+
+			if (!repo) {
+				this.step = "error"
+				this.error = "Repository not found"
+			}
+
+			this.projects = await listProjects(repo.nodeishFs, "/")
+
 			this.step = "noproject"
 			this.loading = false
 		} else if (!this.optin) {
@@ -259,6 +277,12 @@ export class InlangInstall extends TwLitElement {
 	}
 
 	/* This function generates the install link for the user based on a repo url */
+	generateManageLink() {
+		const url = new URL(this.repoURL)
+		return `?repo=${url.host}${url.pathname.split("/").slice(0, 3).join("/")}`
+	}
+
+	/* This function generates the install link for the user based on a repo url */
 	generateInstallLink() {
 		const url = new URL(this.repoURL)
 		return `/install?repo=${url.host}${url.pathname.split("/").slice(0, 3).join("/")}&module=${
@@ -266,8 +290,16 @@ export class InlangInstall extends TwLitElement {
 		}`
 	}
 
+	/* Checks if the GitHub Repo Link is valid */
+	isValidUrl = () =>
+		z
+			.string()
+			.url()
+			.regex(/github/)
+			.safeParse(this.repoURL).success
+
 	override render(): TemplateResult {
-		return html`<div class="w-full h-full flex flex-col justify-center items-center">
+		return html`<div class="w-full h-full flex flex-col items-start justify-center">
 			${this.step === "nomodule"
 				? html`<div class="max-w-md w-full flex flex-col items-center gap-4">
 						<h1 class="font-bold text-4xl text-slate-900 text-center">No module selected</h1>
@@ -282,10 +314,12 @@ export class InlangInstall extends TwLitElement {
 						</a>
 				  </div>`
 				: this.step === "noauth"
-				? html`<div class="flex flex-col gap-2">
-							<h2 class="text-xl font-semibold">Please authorize to continue</h2>
-							<p class="text-slate-500 mb-8">
-								We need your authorization to install modules in your repository.
+				? html`<div class="flex flex-col gap-2 max-w-lg">
+							<h1 class="font-bold text-4xl text-slate-900 mb-2 text-center">Authorize inlang</h1>
+							<p
+								class="text-slate-600 w-full md:w-[400px] leading-relaxed text-center mx-auto mb-4"
+							>
+								We need your authorization to install modules in your repository and project.
 							</p>
 							<button
 								class=${"bg-slate-800 text-white text-center py-2 rounded-md font-medium hover:bg-slate-900 transition-colors " +
@@ -298,8 +332,8 @@ export class InlangInstall extends TwLitElement {
 								Authorize inlang
 							</button>
 							<div class="h-[1px] w-full bg-slate-200 my-4"></div>
-							<h2 class="text-xl font-semibold">Or install manually</h2>
-							<p class="text-slate-500 mb-4">
+							<h2 class="text-2xl font-semibold text-center">Or install manually</h2>
+							<p class="text-slate-500 mb-4 text-center">
 								In case you don't want to authorize inlang to install modules in your repository,
 								you can also install them manually.
 							</p>
@@ -319,7 +353,7 @@ export class InlangInstall extends TwLitElement {
 							this.manual = false
 						}}
 						>
-			<div class="max-w-lg w-full bg-slate-100 rounded-xl relative p-4"
+			<div class="max-w-xl w-full bg-slate-100 rounded-xl relative p-4"
 			@click=${(e: Event) => {
 				e.stopPropagation()
 			}}
@@ -412,86 +446,190 @@ export class InlangInstall extends TwLitElement {
 							: ""} `
 				: this.step === "norepo"
 				? html`<div class="flex flex-col gap-2">
-			<h2 class="text-xl font-semibold">Insert your repository link
-			</h2>
-					</h2>
-					<p class="text-slate-500 mb-4">
-					You can install modules into your repository
-					by providing the repository URL above.
-					</p>
-			</div>`
+						<h1 class="font-bold text-4xl text-slate-900 text-center">Open your Repo</h1>
+						<p class="text-slate-600 w-full md:w-[400px] leading-relaxed mb-4 text-center">
+							To access your projects, please enter the URL of your GitHub repository.
+						</p>
+						<div
+							disabled=${this.url.repo}
+							class=${`px-1 gap-2 relative max-w-lg z-10 flex items-center w-full border bg-white rounded-lg transition-all relative ${
+								this.url.repo ? "cursor-not-allowed" : ""
+							} ${
+								!this.isValidUrl() && this.repoURL.length > 0
+									? " border-red-500 mb-8"
+									: " focus-within:border-[#098DAC] border-slate-200"
+							}	
+`}
+						>
+							<input
+								id="repo-input"
+								.value=${this.url.repo ? this.url.repo : this.repoURL}
+								@input=${(e: InputEvent) => {
+									this.repoURL = (e.target as HTMLInputElement).value
+								}}
+								@keydown=${(e: KeyboardEvent) => {
+									if (e.key === "Enter" && this.isValidUrl()) {
+										window.location.href =
+											this.generateManageLink() +
+											(this.url.project ? `&project=${this.url.project}` : "") +
+											(this.url.module ? `&module=${this.url.module}` : "")
+									}
+								}}
+								class=${"active:outline-0 px-2 focus:outline-0 focus:ring-0 border-0 h-12 grow placeholder:text-slate-500 placeholder:font-normal placeholder:text-base " +
+								(this.url.repo ? "opacity-50 pointer-events-none " : " ") +
+								(!this.isValidUrl() && this.repoURL.length > 0 ? "text-red-500" : "text-slate-900")}
+								placeholder="https://github.com/user/example"
+							/>
+							<button
+								@click="${() => {
+									this.url.repo
+										? (window.location.href = "/")
+										: this.isValidUrl() &&
+										  (window.location.href =
+												this.generateManageLink() +
+												(this.url.project ? `&project=${this.url.project}` : "") +
+												(this.url.module ? `&module=${this.url.module}` : ""))
+								}}"
+								class="bg-white text-slate-600 border flex justify-center items-center h-10 relative rounded-md px-4 border-slate-200 transition-all duration-100 text-sm font-medium hover:bg-slate-100"
+							>
+								${this.url.repo ? "Edit" : "Confirm"}
+							</button>
+							${!this.isValidUrl() && this.repoURL.length > 0
+								? html`<p class="absolute text-red-500 -bottom-5 text-xs">
+										Please enter a valid GitHub repository URL.
+								  </p>`
+								: ""}
+						</div>
+				  </div>`
 				: this.step === "noproject"
 				? html`<div class="flex flex-col gap-2">
-			<h2 class="text-xl font-semibold">Select your project
-			</h2>
-					</h2>
-					<p class="text-slate-500 mb-4">
-					You can install modules into your project 
-					by selecting the project above.
-					</p>
-			</div>`
+						<h1 class="font-bold text-4xl text-slate-900 mb-4 text-center">Select your project</h1>
+						<p class="text-slate-600 w-full md:w-[400px] leading-relaxed text-center">
+							Please select the project you want to install to:
+						</p>
+						<div class="w-full relative max-w-sm">
+							<div
+								class="absolute pointer-events-none h-12 bg-gradient-to-b from-slate-50 to-transparent w-full top-0"
+							></div>
+							<div
+								class="absolute pointer-events-none h-12 bg-gradient-to-t from-slate-50 to-transparent w-full bottom-0"
+							></div>
+							<div class="w-full flex flex-col gap-1 max-h-96 overflow-y-scroll py-12">
+								${
+									// @ts-ignore
+									this.projects?.map(
+										(project: Record<string, any>) =>
+											html`<button
+												@click=${() => {
+													window.location.href =
+														`/install?repo=${this.url.repo}&project=${project.projectPath}` +
+														(this.url.module ? `&module=${this.url.module}` : "")
+												}}
+												class=${"flex gap-4 group items-center text-left p-2 text-md rounded-md text-slate-500 hover:bg-slate-100 hover:text-slate-600"}
+											>
+												${this.url.project === project.projectPath
+													? html`<inlang-folder size="3rem"></inlang-logo>`
+													: html`<inlang-folder size="3rem"></inlang-logo>`}
+												${project.projectPath}
+											</button>`
+									)
+								}
+							</div>
+						</div>
+				  </div>`
 				: this.step === "no-optin"
 				? html`<div class="flex flex-col gap-2">
-			<h2 class="text-xl font-semibold">Do you want to install this module?
-			</h2>
-					</h2>
-					<p class="text-slate-500 mb-4">
-					Please confirm that you want to install the following module in the project selected above:</p>
-					<p class="font-medium">${
-						// @ts-ignore
-						registry.find((x) => x.id === this.url.module)?.displayName.en
-					}</p>
-					<pre class="bg-slate-200 rounded-md p-2 text-sm overflow-x-scroll mb-8">${this.module}</pre>
-					<div class="flex gap-4">
-					<button
-							class="bg-slate-800 px-6 w-full text-white text-center py-2 rounded-md font-medium hover:bg-slate-900 transition-colors"
-							@click=${() => {
-								this.optin = true
-								this.step = "install"
-								this.install()
-							}}
-						>
-							Confirm 
-						</button>
+						<h1 class="font-bold text-4xl text-slate-900 mb-2 text-center">We need your consent</h1>
+						<p class="text-slate-600 w-full md:w-[400px] leading-relaxed text-center mx-auto mb-4">
+							Please confirm that you want to install the following module:
+						</p>
+						<div class="mx-auto max-w-lg mb-8">
+							<div
+								class="p-6 w-full bg-white border border-slate-200 rounded-xl flex flex-col justify-between gap-2"
+							>
+								<div>
+									<div class="w-full flex items-center justify-between mb-4">
+										<h2 class="font-semibold text-lg">
+											${
+												// @ts-ignore
+												registry.find((x) => x.id === this.url.module)?.displayName.en
+											}
+										</h2>
+									</div>
+									<p class="text-slate-500 line-clamp-2 text-sm mb-4">
+										${
+											// @ts-ignore
+											registry.find((x) => x.id === this.url.module)?.description.en
+										}
+									</p>
+									<a
+										target="_blank"
+										href=${`https://inlang.com/m/${
+											// @ts-ignore
+											registry.find((x) => x.id === this.url.module)?.uniqueID
+										}`}
+										class="text-[#098DAC] text-sm font-medium transition-colors hover:text-[#06b6d4]"
+									>
+										More information
+										<doc-icon
+											class="inline-block ml-0.5 translate-y-0.5"
+											size="1em"
+											icon="mdi:arrow-top-right"
+										></doc-icon>
+									</a>
+								</div>
+							</div>
+						</div>
+						<div class="flex gap-4">
+							<button
+								class="bg-slate-800 px-6 w-full text-white text-center py-2 rounded-md font-medium hover:bg-slate-900 transition-colors"
+								@click=${() => {
+									this.optin = true
+									this.step = "install"
+									this.install()
+								}}
+							>
+								Confirm
+							</button>
+							<button
+								class="bg-red-500/10 px-6 w-full text-red-500 text-center py-2 rounded-md font-medium hover:bg-red-500/20 transition-colors"
+								@click=${() => {
+									this.step = "abort"
+								}}
+							>
+								Cancel
+							</button>
+						</div>
+				  </div>`
+				: this.step === "install"
+				? html`<div class="flex flex-col gap-2">
+						<h1 class="font-bold text-4xl text-slate-900 mb-2 text-center">Installing module</h1>
+						<p class="text-slate-600 w-full md:w-[400px] leading-relaxed text-center mx-auto mb-4">
+							Modules are getting installed into your repository...
+						</p>
+						<div class="flex items-start gap-2 w-full h-2 bg-slate-200 rounded-full mb-12">
+							<div
+								class="bg-[#098DAC] h-2 rounded-lg w-full transition-all"
+								style="max-width: ${this.loadingProgress}%"
+							></div>
+						</div>
 						<button
-							class="bg-red-500/10 px-6 w-full text-red-500 text-center py-2 rounded-md font-medium hover:bg-red-500/20 transition-colors"
 							@click=${() => {
 								this.step = "abort"
 							}}
+							class="bg-red-500/10 text-red-500 text-center py-2 rounded-md font-medium hover:bg-red-500/20 transition-colors"
 						>
-							Cancel
+							Cancel installation
 						</button>
-					</div>
-			</div>`
-				: this.step === "install"
-				? html`<div class="flex flex-col gap-2">
-			<h2 class="text-xl font-semibold flex items-center gap-2">
-			Installing module
-			</h2>
-					</h2>
-					<p class="text-slate-500 mb-12">
-					Modules are getting installed into your repository...
-					</p>
-					<div class="flex items-start gap-2 w-full h-2 bg-slate-200 rounded-full mb-12">
-					<div class="bg-[#098DAC] h-2 rounded-lg w-full transition-all"
-					style="max-width: ${this.loadingProgress}%"
-					></div>
-					</div>
-					<button 
-					@click=${() => {
-						this.step = "abort"
-					}}
-					class="bg-red-500/10 text-red-500 text-center py-2 rounded-md font-medium hover:bg-red-500/20 transition-colors">
-					Cancel installation
-					</button>
-			</div>`
+				  </div>`
 				: this.step === "error"
-				? html`<div class="flex flex-col gap-2"><h2 class="text-xl font-semibold flex items-center gap-2">
-			An error occured
-			</h2>
+				? html`<div class="flex flex-col gap-2 max-w-lg">
 					</h2>
+					<h1 class="font-bold text-4xl text-slate-900 mb-2 text-center">
+					An error occured
+					</h1>
 					<div class="max-h-[256px] overflow-y-scroll">
-					<p class="text-slate-500 mb-8">
+					<p class="text-slate-500 mb-8 text-center">
 					${typeof this.error === "string" ? this.error : this.error.join("\n")}
 					</p></div>
 					<div class="flex gap-4">
@@ -522,7 +660,7 @@ export class InlangInstall extends TwLitElement {
 							this.manual = false
 						}}
 						>
-			<div class="max-w-lg w-full bg-slate-100 rounded-xl relative p-4"
+			<div class="max-w-xl w-full bg-slate-100 rounded-xl relative p-4"
 			@click=${(e: Event) => {
 				e.stopPropagation()
 			}}
@@ -615,29 +753,31 @@ export class InlangInstall extends TwLitElement {
 							: ""
 					} `
 				: this.step === "success"
-				? html`<div class="flex flex-col gap-2"><h2 class="text-xl font-semibold flex items-center gap-2">
-			Succesfully installed into your project: ${this.url.project}
-			</h2>
-					</h2>
-					<p class="text-slate-500 mb-8">
-					Your module was succesfully installed.
-					</p>
-					<div class="flex gap-4">
-					<a
-							class="bg-slate-800 px-6 w-full text-white text-center py-2 rounded-md font-medium hover:bg-slate-900 transition-colors"
-							href=${`/?repo=${this.url.repo + (this.url.project ? `&project=${this.url.project}` : "")}`}
-						>
-							To inlang Manage 
-						</a>
-						<a
-						class="bg-slate-200 truncate px-6 w-full text-slate-900 text-center py-2 rounded-md font-medium hover:bg-slate-300 transition-colors"
-						href=${`https://inlang.com/editor/${this.url.repo}`}
-						target="_blank"
-						>
-						Go to Fink - Editor
-						</a>
-					</div>
-					</div>`
+				? html`<div class="flex flex-col gap-2">
+						<h1 class="font-bold text-4xl text-slate-900 mb-4 text-center">
+							Succesfully installed into your project: ${this.url.project}
+						</h1>
+						<p class="text-slate-600 w-full md:w-[400px] leading-relaxed text-center">
+							Your module was succesfully installed.
+						</p>
+						<div class="flex gap-4">
+							<a
+								class="bg-slate-800 px-6 w-full text-white text-center py-2 rounded-md font-medium hover:bg-slate-900 transition-colors"
+								href=${`/?repo=${
+									this.url.repo + (this.url.project ? `&project=${this.url.project}` : "")
+								}`}
+							>
+								To inlang Manage
+							</a>
+							<a
+								class="bg-slate-200 truncate px-6 w-full text-slate-900 text-center py-2 rounded-md font-medium hover:bg-slate-300 transition-colors"
+								href=${`https://inlang.com/editor/${this.url.repo}`}
+								target="_blank"
+							>
+								Go to Fink - Editor
+							</a>
+						</div>
+				  </div>`
 				: this.step === "abort"
 				? html`<div class="flex flex-col gap-2"><h2 class="text-xl font-semibold flex items-center gap-2">
 			Installation aborted
