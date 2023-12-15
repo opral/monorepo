c3d828b026e2e2b7162159ef2c01c819b18554c4
Felix Haeberle <kontakt@felixhaeberle.de>
2023-12-15 16:46:28 +0100

    project selection in ide extension #1832

---
+++ inlang/source-code/ide-extension/src/utilities/project/findInlangProjectRecusively.ts
@@ -0,0 +1,35 @@
+import type { NodeishFilesystem } from "@lix-js/fs"
+import * as path from "node:path"
+
+export async function findInlangProjectRecursively(args: {
+	nodeishFs: NodeishFilesystem
+	rootPath: string
+}): Promise<string[]> {
+	const targetSuffix = ".inlang"
+	const foundFolders: string[] = []
+
+	async function searchForFolders(currentPath: string) {
+		try {
+			const items = await args.nodeishFs.readdir(currentPath)
+			for (const item of items) {
+				const itemPath = path.join(currentPath, item)
+				const stat = await args.nodeishFs.stat(itemPath)
+
+				if (stat.isDirectory() && item.endsWith(targetSuffix)) {
+					foundFolders.push(itemPath)
+				}
+
+				// Continue searching recursively
+				if (stat.isDirectory()) {
+					await searchForFolders(itemPath)
+				}
+			}
+		} catch (error) {
+			// Handle any errors here
+			console.error(`Error while processing ${currentPath}: ${error}`)
+		}
+	}
+
+	await searchForFolders(args.rootPath)
+	return foundFolders
+}
