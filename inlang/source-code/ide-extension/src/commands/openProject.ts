b53e5252a635879466e381dbd9dba05b0541c0d3
Felix Haeberle <kontakt@felixhaeberle.de>
2023-12-18 18:55:52 +0100

    add project view
    - list all available projects, found recursively
    - auto select the closest project
    - feature to swich projects in list view with a single click
    - add icon to jump to settings file for specific project
    - fix file system mapper
    - add new commands "openProject" & "openSettingsFile"
    
    good news cc @samuelstroschein we will have a lot of redundant code which will lead into approx 30% code reduction

---
+++ inlang/source-code/ide-extension/src/commands/openProject.ts
@@ -0,0 +1,12 @@
+import * as vscode from "vscode"
+import { handleTreeSelection, type ProjectNode } from "../utilities/project/project.js"
+import type { NodeishFilesystem } from "@lix-js/fs"
+
+export const openProjectCommand = {
+	command: "inlang.openProject",
+	title: "Inlang: Open project",
+	register: vscode.commands.registerCommand,
+	callback: async (node: ProjectNode, nodeishFs: NodeishFilesystem) => {
+		await handleTreeSelection(node, nodeishFs)
+	},
+}
