629e03e49511d29fdc22c2515b11984890178b9e
Loris Sigrist <sigrist@hasch.io>
2023-12-20 11:50:35 +0100

    Use tsup

--- inlang/source-code/paraglide/paraglide-js-adapter-next/src/LanguageProvider.tsx
+++ inlang/source-code/paraglide/paraglide-js-adapter-next/src/LanguageProvider.tsx
@@ -1,15 +1,18 @@
-import { languageTag, setLanguageTag } from "$paraglide-adapter-next-internal/runtime.js"
+import {
+	languageTag,
+	setLanguageTag,
+	isAvailableLanguageTag,
+} from "$paraglide-adapter-next-internal/runtime.js"
 import { headers } from "next/headers"
 import { ClientLanguageProvider } from "./ClientLanguageProvider"
 import React from "react"
 
-//This only needs to be called once, so it's fine to do it here
-setLanguageTag(() => {
-	const lang = headers().get("x-language-tag") as any
-	return lang
-})
-
 export default function LanguageProvider(props: { children: React.ReactNode }): React.ReactElement {
+	const langHeader = headers().get("x-language-tag")
+	if (isAvailableLanguageTag(langHeader)) {
+		setLanguageTag(langHeader)
+	}
+
 	//we make the client side language provider a sibling of the children
 	//That way the entire app isn't turned into a client component
 	return (
