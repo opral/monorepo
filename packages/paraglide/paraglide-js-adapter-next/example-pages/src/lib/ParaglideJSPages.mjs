19594a7a00777df9ab95d1a1b5463971b787e71f
Loris Sigrist <sigrist@hasch.io>
2023-12-20 15:13:45 +0100

    remove unnecessary source maps

---
+++ packages/paraglide/paraglide-js-adapter-next/example-pages/src/lib/ParaglideJSPages.mjs
@@ -0,0 +1,21 @@
+import { isAvailableLanguageTag, setLanguageTag, sourceLanguageTag } from '$paraglide-adapter-next-internal/runtime.js';
+import React from 'react';
+
+function ParaglideJS(props) {
+    if (isAvailableLanguageTag(props.language)) {
+        setLanguageTag(props.language);
+    }
+    else {
+        // dev only log
+        if (typeof window !== "undefined") {
+            if (process.env.NODE_ENV === "development") {
+                console.error(`[paraglide]: "${props.language}" is not one of the available language tags. Falling back to "${sourceLanguageTag}"`);
+            }
+        }
+        setLanguageTag(sourceLanguageTag);
+    }
+    return React.createElement(React.Fragment, null, props.children);
+}
+
+export { ParaglideJS as default };
+//# sourceMappingURL=ParaglideJSPages.mjs.map
