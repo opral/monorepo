386855d9cf8140397c008b0201405113ff04741f
Loris Sigrist <sigrist@hasch.io>
2023-12-20 15:06:58 +0100

    Sourcemaps

--- inlang/source-code/paraglide/paraglide-js-adapter-next/src/pages/ParaglideJSPages.tsx
+++ inlang/source-code/paraglide/paraglide-js-adapter-next/src/pages/ParaglideJSPages.tsx
@@ -5,7 +5,7 @@ import {
 } from "$paraglide-adapter-next-internal/runtime.js"
 import React from "react"
 
-export function ParaglideJS(props: {
+export default function ParaglideJS(props: {
 	language?: string
 	children: React.ReactNode
 }): React.ReactNode {
@@ -13,12 +13,10 @@ export function ParaglideJS(props: {
 		setLanguageTag(props.language as string)
 	} else {
 		// dev only log
-		if (typeof window !== "undefined") {
-			if (process.env.NODE_ENV === "development") {
-				console.error(
-					`[paraglide]: "${props.language}" is not one of the available language tags. Falling back to "${sourceLanguageTag}"`
-				)
-			}
+		if (process.env.NODE_ENV === "development") {
+			console.error(
+				`[paraglide]: "${props.language}" is not one of the available language tags. Falling back to "${sourceLanguageTag}"`
+			)
 		}
 		setLanguageTag(sourceLanguageTag)
 	}
