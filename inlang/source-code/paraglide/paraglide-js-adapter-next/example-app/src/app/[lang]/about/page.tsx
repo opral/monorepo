0e29c9eb6a04f65cda9348c54edfb3d2832f47e4
Loris Sigrist <sigrist@hasch.io>
2023-12-20 16:41:04 +0100

    Add translated link component

--- inlang/source-code/paraglide/paraglide-js-adapter-next/example-app/src/app/[lang]/about/page.tsx
+++ inlang/source-code/paraglide/paraglide-js-adapter-next/example-app/src/app/[lang]/about/page.tsx
@@ -1,14 +1,11 @@
-import Link from "next/link"
+import { Link } from "@inlang/paraglide-js-adapter-next"
 
 export default function About() {
 	return (
 		<>
 			<main>
 				<p>about page</p>
-				<a href="/de">Home</a>
-				<Link href="/" locale="de">
-					Home
-				</Link>
+				<Link href="/">Home</Link>
 			</main>
 		</>
 	)
