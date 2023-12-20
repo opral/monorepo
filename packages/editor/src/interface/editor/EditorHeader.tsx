184a0d9ef0c681eaed46243076f1c00f15bf6c53
Nils Jacobsen <nils.jacobsen98@gmail.com>
2023-12-20 16:38:35 +0100

    fix editor type errors

--- packages/editor/src/interface/editor/EditorHeader.tsx
+++ packages/editor/src/interface/editor/EditorHeader.tsx
@@ -1,15 +1,12 @@
-import { Button } from "#src/interface/components/Button.jsx"
-import { For, Show, createSignal } from "solid-js"
+import { Show } from "solid-js"
 import UserDropdown from "./UserDropdown.jsx"
-import IconClose from "~icons/material-symbols/close-rounded"
-import IconMenu from "~icons/material-symbols/menu-rounded"
 import Link from "#src/renderer/Link.jsx"
 import { useLocalStorage } from "#src/services/local-storage/index.js"
 
 function EditorHeader() {
-	const getLinks = () => {
-		return []
-	}
+	// const getLinks = () => {
+	// 	return []
+	// }
 
 	const [localStorage] = useLocalStorage()
 	const user = () => {
@@ -17,7 +14,7 @@ function EditorHeader() {
 		return localStorage.user
 	}
 
-	const [mobileMenuIsOpen, setMobileMenuIsOpen] = createSignal(false)
+	// const [mobileMenuIsOpen, setMobileMenuIsOpen] = createSignal(false)
 	return (
 		<>
 			<header class="sticky top-0 z-[9999] w-full bg-transparent bg-background py-3.5 px-4 border-b border-surface-200">
@@ -56,7 +53,7 @@ function EditorHeader() {
 							{/* <p class="self-center text-left font-regular text-surface-400 pl-3 pr-1">/</p> */}
 							<div class="w-full content-center">
 								<div class="hidden md:flex justify-end items-center gap-8">
-									<For each={getLinks()}>
+									{/* <For each={getLinks()}>
 										{(link) => (
 											<>
 												<Button type="text" href={link.href}>
@@ -64,14 +61,14 @@ function EditorHeader() {
 												</Button>
 											</>
 										)}
-									</For>
+									</For> */}
 									<Show when={user()?.isLoggedIn}>
 										<UserDropdown />
 									</Show>
 								</div>
 							</div>
 							{/* Controll the Dropdown/Navbar  if its open then Show MobileNavMenue */}
-							<div class="md:hidden flex items-center">
+							{/* <div class="md:hidden flex items-center">
 								<button
 									onClick={() => setMobileMenuIsOpen(!mobileMenuIsOpen())}
 									type="button"
@@ -84,10 +81,10 @@ function EditorHeader() {
 										<IconMenu class="w-6 h-6" />
 									)}
 								</button>
-							</div>
+							</div> */}
 						</div>
 						{/* MobileNavbar includes the Navigation for the Documentations sites  */}
-						<Show when={mobileMenuIsOpen()}>
+						{/* <Show when={mobileMenuIsOpen()}>
 							<ol class="space-y-1 relativ w-full min-h-full pt-3 pl-[5px] overflow">
 								<For each={getLinks()}>
 									{(link) => (
@@ -103,7 +100,7 @@ function EditorHeader() {
 									)}
 								</For>
 							</ol>
-						</Show>
+						</Show> */}
 					</nav>
 				</div>
 			</header>
