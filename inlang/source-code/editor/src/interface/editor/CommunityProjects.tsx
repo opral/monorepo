eab63e83274f087b904bc49ed554ffd2636e201c
flooorianflo <florian.kiem@hfg.design>
2023-12-21 10:34:01 +0100

    feat: implement redirections for editor

--- inlang/source-code/editor/src/interface/editor/CommunityProjects.tsx
+++ inlang/source-code/editor/src/interface/editor/CommunityProjects.tsx
@@ -118,7 +118,7 @@ export function RepositoryCard(props: {
 					? `/install?repo=github.com/${props.repository.owner}/${
 							props.repository.repository
 					  }&module=${props.modules?.join(",")}`
-					: `/editor/github.com/${props.repository.owner}/${props.repository.repository}`
+					: `/github.com/${props.repository.owner}/${props.repository.repository}`
 			}
 			class="rounded-xl bg-background transition-all border border-surface-200 hover:shadow-lg hover:shadow-surface-100 hover:border-surface-300 active:border-surface-400 p-6 flex flex-col justify-between gap-5"
 		>
