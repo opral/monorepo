40a720e0e06aadd9e1024a8ec75c44d05369c09b
Loris Sigrist <sigrist@hasch.io>
2023-12-21 10:20:40 +0100

    remove redundant languageSwitcher

--- packages/paraglide/paraglide-js-adapter-next/example-app/src/app/layout.tsx
+++ packages/paraglide/paraglide-js-adapter-next/example-app/src/app/layout.tsx
@@ -1,10 +1,6 @@
 import { LanguageProvider } from "@inlang/paraglide-js-adapter-next"
-import { availableLanguageTags, languageTag } from "@/paraglide/runtime"
+import { languageTag } from "@/paraglide/runtime"
 import { LanguageSwitcher } from "@/lib/LanguageSwitcher"
-
-export async function generateStaticParams() {
-	return availableLanguageTags.map((lang) => ({ lang }))
-}
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
