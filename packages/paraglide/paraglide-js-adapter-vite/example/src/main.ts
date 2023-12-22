4791478f6de5c22ad9a534d335dee51fd89ab55f
Loris Sigrist <sigrist@hasch.io>
2023-12-22 13:28:15 +0100

    Add /* @__NO_SIDE_EFFECTS__ */ to message functions

--- packages/paraglide/paraglide-js-adapter-vite/example/src/main.ts
+++ packages/paraglide/paraglide-js-adapter-vite/example/src/main.ts
@@ -1,5 +1,8 @@
 import * as m from "$paraglide/messages.js"
 
+//This should be removed by the compiler
+m.unused()
+
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
     <h1>${m.greeting({ name: "John Doe" })}</h1>
