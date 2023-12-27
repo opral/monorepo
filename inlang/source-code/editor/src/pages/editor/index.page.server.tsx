b4eb5df142e694a2bb9130f59ccb5861b8d7961f
flooorianflo <florian.kiem@hfg.design>
2023-12-27 10:36:41 +0100

    fix: fink redirect

--- inlang/source-code/editor/src/pages/editor/index.page.server.tsx
+++ inlang/source-code/editor/src/pages/editor/index.page.server.tsx
@@ -1,8 +1,5 @@
 import { redirect } from "vike/abort"
 
-const isProduction = process.env.NODE_ENV === "production"
-
 export async function onBeforeRender() {
-	if (!isProduction) throw redirect("http://localhost:4003/")
-	else throw redirect("https://fink.inlang.com/")
+	throw redirect("https://fink.inlang.com/")
 }
