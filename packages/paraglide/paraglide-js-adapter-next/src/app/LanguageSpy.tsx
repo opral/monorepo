5145e60c25ad8dac317799c8d8d1145b2d25b491
Loris Sigrist <sigrist@hasch.io>
2023-12-28 15:23:57 +0100

    Centralized routing Strategy

--- packages/paraglide/paraglide-js-adapter-next/src/app/LanguageSpy.tsx
+++ packages/paraglide/paraglide-js-adapter-next/src/app/LanguageSpy.tsx
@@ -1,19 +1,22 @@
 "use client"
 
 import {
-	isAvailableLanguageTag,
+	availableLanguageTags,
 	sourceLanguageTag,
 } from "$paraglide-adapter-next-internal/runtime.js"
 import { usePathname } from "next/navigation"
 import { useRouter } from "next/navigation"
 import { useEffect, useState } from "react"
+import { prefixStrategy } from "./navigation/utils"
+
+const { getLocaleFromPath } = prefixStrategy(availableLanguageTags, sourceLanguageTag)
 
 //Keep track of the language in the pathname, and reload the page when it changes
 export function LangaugeSpy() {
 	const router = useRouter()
 	const pathname = usePathname()
-	const maybeLanguage = pathname.split("/")[1]
-	const language = isAvailableLanguageTag(maybeLanguage) ? maybeLanguage : sourceLanguageTag
+
+	const language = getLocaleFromPath(pathname) ?? sourceLanguageTag
 	const [currentLanguage, setCurrentLanguage] = useState(language)
 
 	useEffect(() => {
