3dfda96f57095d32c5264e2c2cb724f232c69169
Martin Lysk <martin.lysk@gmail.com>
2024-01-08 10:13:29 +0100

    synchronize load and save messages

--- packages/sdk/src/loadProject.ts
+++ packages/sdk/src/loadProject.ts
@@ -33,13 +33,14 @@ import {
 	getMessageIdFromPath,
 	getPathFromMessageId,
 	parseMessage,
-	encodeMessage,
+	encodeMessage as stringifyMessage,
 } from "./storage/helper.js"
 
 import { humanIdHash } from "./storage/human-id/human-readable-id.js"
 
 import type { Repository } from "@lix-js/client"
 import { generateProjectId } from "./generateProjectId.js"
+import { createNodeishFsWithWatcher } from "./createNodeishFsWithWatcher.js"
 
 const settingsCompiler = TypeCompiler.Compile(ProjectSettings)
 
@@ -194,11 +195,12 @@ export const loadProject = async (args: {
 		const messageFolderPath = projectPath + "/messages" + "/v1"
 
 		createEffect(() => {
+			// wait for first effect excution until modules are resolved
 			const _resolvedModules = resolvedModules()
 			if (!_resolvedModules) return
 
+			// -- initial load of all messages found in the messages folder ----------
 			const loadAndSetMessages = async (fs: NodeishFilesystemSubset) => {
-				// load all messages
 				const loadedMessages: Message[] = []
 
 				try {
@@ -211,6 +213,7 @@ export const loadProject = async (args: {
 						}
 					}
 
+					// helper function that traverses recursivly through the tree
 					const readFilesFromFolderRecursive = async (
 						fileSystem: NodeishFilesystemSubset,
 						rootPath: string,
@@ -219,21 +222,25 @@ export const loadProject = async (args: {
 						let filePaths: string[] = []
 						const paths = await fileSystem.readdir(rootPath + pathToRead)
 						for (const path of paths) {
+							// TODO #1844 FILESYSTEM - what is inlangs best practice to handle other file systems atm?
 							const stat = await fileSystem.stat(rootPath + pathToRead + "/" + path)
 
 							if (stat.isDirectory()) {
 								const subfolderPaths = await readFilesFromFolderRecursive(
 									fileSystem,
 									rootPath,
+									// TODO #1844 FILESYSTEM - what is inlangs best practice to handle other file systems atm?
 									pathToRead + "/" + path
 								)
 								filePaths = filePaths.concat(subfolderPaths)
 							} else {
+								// TODO #1844 FILESYSTEM - what is inlangs best practice to handle other file systems atm?
 								filePaths.push(pathToRead + "/" + path)
 							}
 						}
 						return filePaths
 					}
+
 					const messageFilePaths = await readFilesFromFolderRecursive(fs, messageFolderPath, "")
 					for (const messageFilePath of messageFilePaths) {
 						const messageId = getMessageIdFromPath(messageFilePath)
@@ -248,15 +255,14 @@ export const loadProject = async (args: {
 
 							const message = parseMessage(messageFilePath, messageRaw) as Message
 
-							// message parsing was successfull remove entry in erros map if it exists
-
-							const _messageLoadErrors = { ... messageLoadErrors() }
+							// if we end up here - message parsing was successfull remove entry in erros map if it exists
+							const _messageLoadErrors = { ...messageLoadErrors() }
 							delete _messageLoadErrors[messageId]
 							setMessageLoadErrors(messageLoadErrors)
 
 							loadedMessages.push(message)
 						} catch (e) {
-							// TODO #1844 test errors being propagated
+							// TODO #1844 FINK - test errors being propagated - fink doesnt show errors other than lints at the moment...
 							// if reading of a single message fails we propagate the error to the project errors
 							messageLoadErrors()[messageId] = new LoadMessageError({
 								path: messageFilePath,
@@ -275,7 +281,7 @@ export const loadProject = async (args: {
 				}
 			}
 
-			// setup watchers on message files
+			// -- subsequencial upsers and delete of messages on file changes ------------
 			loadAndSetMessages(nodeishFs).then(() => {
 				// when initial message loading is done start watching on file changes in the message dir
 				;(async () => {
@@ -296,6 +302,7 @@ export const loadProject = async (args: {
 
 								const messageId = getMessageIdFromPath(event.filename)
 								if (!messageId) {
+									// ignore files not matching the expected id file path
 									continue
 								}
 
@@ -318,19 +325,20 @@ export const loadProject = async (args: {
 									try {
 										const message = parseMessage(event.filename, fileContent)
 
+										// if we end up here - message parsing was successfull remove entry in erros map if it exists
 										const _messageLoadErrors = messageLoadErrors()
 										delete _messageLoadErrors[messageId]
 										setMessageLoadErrors(_messageLoadErrors)
 
 										const currentMessage = messagesQuery.get({ where: { id: messageId } })
-										const currentMessageEncoded = encodeMessage(currentMessage)
-										if (currentMessage && currentMessageEncoded === fileContent) {
+										const currentMessageStringified = stringifyMessage(currentMessage)
+										if (currentMessage && currentMessageStringified === fileContent) {
 											continue
 										}
 
 										messagesQuery.upsert({ where: { id: messageId }, data: message })
 									} catch (e) {
-										// TODO #1844 test errors being propagated
+										// TODO #1844 FINK - test errors being propagated - fink doesnt show errors other than lints at the moment...
 										messageLoadErrors()[messageId] = new LoadMessageError({
 											path: messageFolderPath + "/" + event.filename,
 											messageId,
@@ -390,12 +398,20 @@ export const loadProject = async (args: {
 		const messagesQuery = createMessagesQuery(() => messages() || [])
 
 		const trackedMessages: Map<string, () => void> = new Map()
-		// subscribe to all messages and write to files on signal
+		// -- subscribe to all messages and write to files on signal -------------
 		createEffect(() => {
+			const _resolvedModules = resolvedModules()
+			if (!_resolvedModules) return
+
 			const currentMessageIds = messagesQuery.includedMessageIds()
 			const deletedMessageTrackedMessage = [...trackedMessages].filter(
 				(tracked) => !currentMessageIds.includes(tracked[0])
 			)
+
+			const saveMessagesPlugin = _resolvedModules.plugins.find(
+				(plugin) => plugin.saveMessages !== undefined
+			)
+
 			for (const messageId of currentMessageIds) {
 				if (!trackedMessages!.has(messageId!)) {
 					// to avoid to drop the effect after creation we need to create a new disposable root
@@ -422,8 +438,10 @@ export const loadProject = async (args: {
 										}
 									}
 
-									await fs.writeFile(path, encodeMessage(message))
-									debouncedSave(messagesQuery.getAll())
+									await fs.writeFile(path, stringifyMessage(message))
+
+									saveMessages(fs, messagesQuery, settings()!, saveMessagesPlugin)
+									// debouncedSave(messagesQuery.getAll())
 								}
 								const messageFilePath = messageFolderPath + "/" + getPathFromMessageId(message.id)
 								persistMessage(nodeishFs, messageFilePath, message)
@@ -455,7 +473,8 @@ export const loadProject = async (args: {
 				const messageFilePath = messageFolderPath + "/" + getPathFromMessageId(deletedMessage[0])
 				try {
 					nodeishFs.rm(messageFilePath)
-					debouncedSave(messagesQuery.getAll())
+					saveMessages(nodeishFs, messagesQuery, settings()!, saveMessagesPlugin)
+					// debouncedSave(messagesQuery.getAll())
 				} catch (e) {
 					if ((e as any).code !== "ENOENT") {
 						throw e
@@ -468,91 +487,29 @@ export const loadProject = async (args: {
 		})
 
 		// TODO #1844 CLEARIFY this was used to create a watcher on all files that the fs reads - shall we import on every change as well?
-		// const fsWithWatcher = createNodeishFsWithWatcher({
-		// 	nodeishFs: nodeishFs,
-		// 	updateMessages: () => {
-		// 		// TODO #1844 this is where the messages are loaded (all) when the message file changed
-		// 		// TODO #1844 do we still need to reload all messages when plugins change - guess not
-		// 		// loadAndSetMessages(nodeishFs)
-		// 	},
-		// })
+		// TODO #1844 synchronize imports with debouncedSave - wait for the import until the import took place and vice versa - synchronize import/export
+		// we need to do this since importers currently don't check the target state. if the importer works on two files en/de and a message is saved for both languages
+		// this would lead to a change in one language first – for example en – but the improter would load en and de. de is still in the old state and would override changes
+		const fsWithWatcher = createNodeishFsWithWatcher({
+			nodeishFs: nodeishFs,
+			updateMessages: () => {
+				// TODO #1844 this is where the messages are loaded (all) when the message file changed
+				// TODO #1844 do we still need to reload all messages when plugins change - guess not
+				// loadAndSetMessages(nodeishFs)
+			},
+		})
 
 		// run import
 		const _resolvedModules = resolvedModules()
-		// initial project setup finished - import all messages usign legacy load Messages
-		if (_resolvedModules?.resolvedPluginApi.loadMessages) {
-			// get plugin id by finding the plugin that provides loadMessages function
+		const _settings = settings()
+		// initial project setup finished - import all messages using legacy load Messages method
+		if (_resolvedModules?.resolvedPluginApi.loadMessages && _settings) {
+			// get plugin finding the plugin that provides loadMessages function
 			const loadMessagePlugin = _resolvedModules.plugins.find(
 				(plugin) => plugin.loadMessages !== undefined
 			)
-			const loadPluginId = loadMessagePlugin!.id
 
-			const importedMessages = await makeTrulyAsync(
-				_resolvedModules.resolvedPluginApi.loadMessages({
-					// @ts-ignore
-					settings: settingsValue,
-					nodeishFs: nodeishFs,
-				})
-			)
-
-			const messagesToImportSorted = importedMessages.sort((importMessageA, importMessageB) =>
-				importMessageA.id.localeCompare(importMessageB.id)
-			)
-			for (const importedMessage of messagesToImportSorted) {
-				const currentMessages = messagesQuery
-					.getAll()
-					// TODO #1585 here we match using the id to support legacy load message plugins - after we introduced import / export methods we will use importedMessage.alias
-					.filter((message) => message.alias[loadPluginId] === importedMessage.id)
-
-				if (currentMessages.length > 1) {
-					// TODO #1844 CLEARIFY how to handle the case that we find a dublicated alias during import? - change Error correspondingly
-					throw new Error("more than one message with the same alias found ")
-				} else if (currentMessages.length === 1) {
-					// update message in place - leave message id and alias untouched
-					importedMessage.alias = {} as any
-					// TODO #1585 we have to map the id of the importedMessage to the alias and fill the id property with the id of the existing message - change when import mesage provides importedMessage.alias
-					importedMessage.alias[loadPluginId] = importedMessage.id
-					importedMessage.alias["library.inlang.paraglideJs"] = importedMessage.id
-					importedMessage.id = currentMessages[0]!.id
-					const importedEnecoded = encodeMessage(importedMessage)
-					const currentMessageEncoded = encodeMessage(currentMessages[0]!)
-					if (importedEnecoded === currentMessageEncoded) {
-						continue
-					}
-					messagesQuery.update({ where: { id: importedMessage.id }, data: importedMessage })
-				} else {
-					// message with the given alias does not exist so far
-					importedMessage.alias = {} as any
-					// TODO #1585 we have to map the id of the importedMessage to the alias - change when import mesage provides importedMessage.alias
-					importedMessage.alias[loadPluginId] = importedMessage.id
-					importedMessage.alias["library.inlang.paraglideJs"] = importedMessage.id
-
-					let currentOffset = 0
-					let messsageId: string | undefined
-					do {
-						messsageId = humanIdHash(importedMessage.id, currentOffset)
-						const path = /* messageFolderPath + "/" +*/ getPathFromMessageId(messsageId)
-						try {
-							await nodeishFs.stat(path)
-						} catch (e) {
-							if ((e as any).code === "ENOENT") {
-								// keep the message id!
-								continue
-							}
-							throw e
-						}
-
-						currentOffset += 1
-						messsageId = undefined
-					} while (messsageId === undefined)
-
-					// create a humanId based on a hash of the alias
-					importedMessage.id = messsageId
-
-					// TODO #1844 CLEARIFY - we don't block fs here - we could have a situation where a file with the same message id is created in the meantime
-					messagesQuery.create({ data: importedMessage })
-				}
-			}
+			await loadMessages(fsWithWatcher, messagesQuery, _settings, loadMessagePlugin)
 		}
 
 		const lintReportsQuery = createMessageLintReportsQuery(
@@ -563,7 +520,8 @@ export const loadProject = async (args: {
 			hasWatcher
 		)
 
-		const debouncedSave = skipFirst(
+		// TODO #1844 i doubt this is needed
+		/* const debouncedSave = skipFirst(
 			debounce(
 				500,
 				async (newMessages) => {
@@ -595,7 +553,7 @@ export const loadProject = async (args: {
 				},
 				{ atBegin: false }
 			)
-		)
+		)*/
 
 		return {
 			installed: {
@@ -748,4 +706,202 @@ export function createSubscribable<T>(signal: () => T): Subscribable<T> {
 			})
 		},
 	})
+}
+
+let isSaving: boolean
+let currentSaveMessages: Promise<void> | undefined
+let sheduledSaveMessages:
+	| [awaitable: Promise<void>, resolve: () => void, reject: (e: unknown) => void]
+	| undefined
+
+let isLoading = false
+let sheduledLoadMessages:
+	| [awaitable: Promise<void>, resolve: () => void, reject: (e: unknown) => void]
+	| undefined
+
+/**
+ * Messsage that loads messages from a plugin - this method synchronizes with the saveMessage funciton.
+ * If a save is in progress loading will wait until saving is done. If another load kicks in during this load it will queue the
+ * load and execute it at the end of this load. subsequential loads will not be queued but the same promise will be reused
+ *
+ * - NOTE: this means that the parameters used to load like settingsValue and loadPlugin might not take into account. this has to be refactored
+ * with the loadProject restructuring
+ * @param fs
+ * @param messagesQuery
+ * @param settingsValue
+ * @param loadPlugin
+ * @returns void - updates the files and messages in of the project in place
+ */
+async function loadMessages(
+	fs: NodeishFilesystemSubset,
+	messagesQuery: any,
+	settingsValue: ProjectSettings,
+	loadPlugin: any
+) {
+	// let the current save process finish first
+	if (currentSaveMessages) {
+		await currentSaveMessages
+	}
+
+	// loading is an asynchronous process - check if another load is in progress - queue this call if so
+	if (isLoading) {
+		if (!sheduledLoadMessages) {
+			sheduledLoadMessages = createAwaitable()
+		}
+		// another load will take place right after the current one - its goingt to be idempotent form the current requested one - don't reschedule
+		return sheduledLoadMessages[0]
+	}
+
+	// set loading flag
+	isLoading = true
+
+	const loadPluginId = loadPlugin!.id
+
+	const importedMessages = await makeTrulyAsync(
+		loadPlugin.loadMessages({
+			// @ts-ignore
+			settings: settingsValue,
+			nodeishFs: fs,
+		})
+	)
+
+	for (const importedMessage of importedMessages) {
+		const currentMessages = messagesQuery
+			.getAll()
+			// TODO #1585 here we match using the id to support legacy load message plugins - after we introduced import / export methods we will use importedMessage.alias
+			.filter((message: any) => message.alias[loadPluginId] === importedMessage.id)
+
+		if (currentMessages.length > 1) {
+			// TODO #1844 CLEARIFY how to handle the case that we find a dublicated alias during import? - change Error correspondingly
+			throw new Error("more than one message with the same alias found ")
+		} else if (currentMessages.length === 1) {
+			// update message in place - leave message id and alias untouched
+			importedMessage.alias = {} as any
+			// TODO #1585 we have to map the id of the importedMessage to the alias and fill the id property with the id of the existing message - change when import mesage provides importedMessage.alias
+			importedMessage.alias[loadPluginId] = importedMessage.id
+			importedMessage.alias["library.inlang.paraglideJs"] = importedMessage.id
+			importedMessage.id = currentMessages[0]!.id
+			const importedEnecoded = stringifyMessage(importedMessage)
+			const currentMessageEncoded = stringifyMessage(currentMessages[0]!)
+			if (importedEnecoded === currentMessageEncoded) {
+				continue
+			}
+			messagesQuery.update({ where: { id: importedMessage.id }, data: importedMessage })
+		} else {
+			// message with the given alias does not exist so far
+			importedMessage.alias = {} as any
+			// TODO #1585 we have to map the id of the importedMessage to the alias - change when import mesage provides importedMessage.alias
+			importedMessage.alias[loadPluginId] = importedMessage.id
+			importedMessage.alias["library.inlang.paraglideJs"] = importedMessage.id
+
+			let currentOffset = 0
+			let messsageId: string | undefined
+			do {
+				messsageId = humanIdHash(importedMessage.id, currentOffset)
+				const path = /* messageFolderPath + "/" +*/ getPathFromMessageId(messsageId)
+				try {
+					await fs.stat(path)
+				} catch (e) {
+					if ((e as any).code === "ENOENT") {
+						// keep the message id!
+						continue
+					}
+					throw e
+				}
+
+				currentOffset += 1
+				messsageId = undefined
+			} while (messsageId === undefined)
+
+			// create a humanId based on a hash of the alias
+			importedMessage.id = messsageId
+
+			// TODO #1844 CLEARIFY - we don't block fs here - we could have a situation where a file with the same message id is created in the meantime
+			messagesQuery.create({ data: importedMessage })
+		}
+	}
+
+	isLoading = false
+
+	const executingScheduledMessages = sheduledLoadMessages
+	if (executingScheduledMessages) {
+		// a load has been requested during the load - executed it
+
+		// reset sheduling to except scheduling again
+		sheduledLoadMessages = undefined
+
+		// recall load unawaited to allow stack to pop
+		loadMessages(fs, messagesQuery, settingsValue, loadPlugin).then(
+			() => {
+				executingScheduledMessages[1]()
+			},
+			(e: Error) => {
+				executingScheduledMessages[2](e)
+			}
+		)
+	}
+}
+
+async function saveMessages(
+	fs: NodeishFilesystemSubset,
+	messagesQuery: any,
+	settingsValue: ProjectSettings,
+	savePlugin: any
+) {
+	// queue next save if we have a save ongoing
+	if (isSaving) {
+		if (!sheduledSaveMessages) {
+			sheduledSaveMessages = createAwaitable()
+		}
+
+		return sheduledSaveMessages[0]
+	}
+
+	// set isSavingFlag
+	isSaving = true
+
+	const savePluginId = savePlugin!.id
+
+	currentSaveMessages = (async function () {
+		try {
+			const currentMessages = messagesQuery.getAll()
+
+			const messagesToExport: Message[] = []
+			for (const message of currentMessages) {
+				const fixedExportMessage = { ...message }
+				// TODO #1585 here we match using the id to support legacy load message plugins - after we introduced import / export methods we will use importedMessage.alias
+				fixedExportMessage.id = fixedExportMessage.alias[savePluginId] ?? fixedExportMessage.id
+
+				messagesToExport.push(fixedExportMessage)
+			}
+
+			await savePlugin.saveMessages({
+				settings: settingsValue,
+				messages: messagesToExport,
+				nodeishFs: fs,
+			})
+		} catch (err) {
+			throw new PluginSaveMessagesError({
+				cause: err,
+			})
+		} finally {
+			isSaving = false
+		}
+	})()
+
+	await currentSaveMessages
+
+	if (sheduledSaveMessages) {
+		const executingSheduledSaveMessages = sheduledSaveMessages
+		sheduledSaveMessages = undefined
+
+		saveMessages(fs, messagesQuery, settingsValue, savePlugin).then(
+			() => {
+				executingSheduledSaveMessages[1]()
+			},
+			(e) => {
+				executingSheduledSaveMessages[2](e)
+			}
+		)
+	}
 }
