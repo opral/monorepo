7523958886f16bdff1c9335d18f60d5602487231
Jan Johannes <jan.johannes@inlang.com>
2024-01-09 18:51:19 +0200

    add local support for openRepo

--- lix/source-code/client/src/mockRepo.ts
+++ lix/source-code/client/src/mockRepo.ts
@@ -1,19 +1,23 @@
 import { openRepository } from "./openRepository.js"
-import { createNodeishMemoryFs, fromSnapshot } from "@lix-js/fs"
+import { createNodeishMemoryFs, fromSnapshot as loadSnapshot, type Snapshot } from "@lix-js/fs"
+import isoGit from "isomorphic-git"
 // @ts-ignore
-import repoDaata from "./ci-test-repo.js"
+// to load from json file JSON.parse(readFileSync("../mocks/ci-test-repo.json", { encoding: "utf-8" }))
 
-export async function mockRepo() {
+export async function mockRepo({ fromSnapshot }: { fromSnapshot?: Snapshot } = {}) {
 	const nodeishFs = createNodeishMemoryFs()
 
-	const snapshot = repoDaata
-	// JSON.parse(readFileSync("../mocks/ci-test-repo.json", { encoding: "utf-8" }))
-	fromSnapshot(nodeishFs, snapshot)
+	if (fromSnapshot) {
+		loadSnapshot(nodeishFs, fromSnapshot)
+	} else {
+		isoGit.init({ fs: nodeishFs, dir: "/" })
+	}
 
-	const repo = await openRepository("https://github.com/inlang/ci-test-repo", {
+	const repo = await openRepository("file://", {
 		nodeishFs,
 	})
 
+	// temporarily all test repos get same test metadata, this can be removed when repo ids are implemented
 	repo.getMeta = async () => {
 		return {
 			id: "34c48e4ba4c128582466b8dc1330feac0733880b35f467f4161e259070d24a31",
