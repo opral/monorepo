23757fa1b4508ac720c134d6658f35eb915f7c92
Loris Sigrist <sigrist@hasch.io>
2024-01-09 14:30:14 +0100

    Remove TS from example project

--- inlang/source-code/paraglide/paraglide-js-adapter-sveltekit/example/src/hooks.server.js
+++ inlang/source-code/paraglide/paraglide-js-adapter-sveltekit/example/src/hooks.server.js
@@ -1,3 +1,4 @@
 import { handleRedirects, injectLangAttribute } from "@inlang/paraglide-js-adapter-sveltekit"
 import { sequence } from "@sveltejs/kit/hooks"
+
 export const handle = sequence(injectLangAttribute("%lang%"), handleRedirects())
