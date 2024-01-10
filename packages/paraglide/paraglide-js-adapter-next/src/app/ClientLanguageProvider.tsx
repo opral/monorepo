b5c262cd2110179b2600074c0c57e202dcaecf5a
Loris Sigrist <sigrist@hasch.io>
2024-01-10 14:07:20 +0100

    Fix Build

--- packages/paraglide/paraglide-js-adapter-next/src/app/ClientLanguageProvider.tsx
+++ packages/paraglide/paraglide-js-adapter-next/src/app/ClientLanguageProvider.tsx
@@ -1,6 +1,11 @@
 "use client"
 import { setLanguageTag } from "$paraglide-adapter-next-internal/runtime.js"
 
+/**
+ * A client side component that sets the language tag on mount.
+ * @param props 
+ * @returns 
+ */
 export function ClientLanguageProvider(props: { language: string }) {
 	setLanguageTag(props.language)
 	return null
