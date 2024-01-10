c0a359853c5eea72ade08fc6451461e692ab9d2d
Loris Sigrist <sigrist@hasch.io>
2024-01-10 16:04:41 +0100

    Better Type Declaration for Pages Provider

--- packages/paraglide/paraglide-js-adapter-next/src/pages/ParaglideJSProvider.tsx
+++ packages/paraglide/paraglide-js-adapter-next/src/pages/ParaglideJSProvider.tsx
@@ -1,18 +1,26 @@
 "use client"
 import React from "react"
 import Header from "./Header"
-import { availableLanguageTags } from "$paraglide-adapter-next-internal/runtime.js"
 
-export default function ParaglideJS(props: {
-	runtime: typeof import("$paraglide-adapter-next-internal/runtime.js")
-	language?: string
+export interface Paraglide<T extends string> {
+	readonly setLanguageTag: (language_tag: T | (() => T)) => void
+	readonly languageTag: () => T
+	readonly onSetLanguageTag: (callback: (language_tag: T) => void) => void
+	readonly isAvailableLanguageTag: (thing: any) => thing is T
+	readonly availableLanguageTags: readonly T[]
+	readonly sourceLanguageTag: T
+}
+
+export default function ParaglideJS<T extends string>(props: {
+	runtime: Paraglide<T>
+	language?: T
 	children: React.ReactNode
 }): React.ReactNode {
 	const { isAvailableLanguageTag, setLanguageTag, sourceLanguageTag, availableLanguageTags } =
 		props.runtime
 
-	if (isAvailableLanguageTag(props.language as string)) {
-		setLanguageTag(props.language as string)
+	if (isAvailableLanguageTag(props.language)) {
+		setLanguageTag(props.language)
 	} else {
 		// dev only log
 		if (process.env.NODE_ENV === "development") {
