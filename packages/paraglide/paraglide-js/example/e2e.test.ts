43f51f028257c19fc7d1ecf39ff92ed541352361
Jan Johannes <jan.johannes@inlang.com>
2024-01-10 16:57:58 +0200

    add back import extension

--- packages/paraglide/paraglide-js/example/e2e.test.ts
+++ packages/paraglide/paraglide-js/example/e2e.test.ts
@@ -2,7 +2,7 @@ import { test, expect, vi } from "vitest"
 
 test("should log the messages in the correct language tag", async () => {
 	const consoleMock = vi.spyOn(console, "log").mockImplementation(() => {})
-	await import("./src/main")
+	await import("./src/main.js")
 	expect(consoleMock).toHaveBeenCalledTimes(6)
 
 	expect(consoleMock.mock.calls[0][0]).toBe('The current language tag is "en".')
