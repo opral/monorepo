6f68c1c76078014a881f575a312c46ef7ec69950
Jan Johannes <jan.johannes@inlang.com>
2024-01-11 12:10:37 +0200

    refactor project id for clarity

--- packages/sdk/src/generateProjectId.ts
+++ packages/sdk/src/generateProjectId.ts
@@ -1,5 +1,38 @@
 import type { Repository } from "@lix-js/client"
 import { hash } from "@lix-js/client"
+import { type NodeishFilesystem } from "@lix-js/fs"
+
+export async function maybeCreateProjectId({
+	nodeishFs,
+	projectPath,
+	repo,
+}: {
+	nodeishFs: any
+	projectPath: string
+	repo: Repository
+}) {
+	let projectId: string | undefined
+	const fs = nodeishFs as NodeishFilesystem
+
+	try {
+		projectId = await fs.readFile(projectPath + "/project_id", {
+			encoding: "utf-8",
+		})
+	} catch (error) {
+		// @ts-ignore
+		if (error.code === "ENOENT") {
+			if (repo) {
+				projectId = await generateProjectId(repo, projectPath)
+				if (projectId) {
+					await fs.writeFile(projectPath + "/project_id", projectId)
+				}
+			}
+		} else {
+			return { error }
+		}
+	}
+	return { projectId }
+}
 
 export async function generateProjectId(repo: Repository, projectPath: string) {
 	if (!repo || !projectPath) {
