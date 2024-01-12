b9d7807566feba30ca0bec9606becf9bc7a6e95a
Loris Sigrist <sigrist@hasch.io>
2024-01-12 10:55:13 +0100

    Standardize Path handling

--- inlang/source-code/paraglide/paraglide-js-adapter-sveltekit/example/src/hooks.js
+++ inlang/source-code/paraglide/paraglide-js-adapter-sveltekit/example/src/hooks.js
@@ -1,5 +1,6 @@
 import { reroute as reroute_ } from "@inlang/paraglide-js-adapter-sveltekit"
+import * as runtime from "$paraglide/runtime.js"
 import { pathTranslations } from "$lib/i18n"
 
 /** @type {import("@sveltejs/kit").Reroute} */
-export const reroute = reroute_(pathTranslations)
+export const reroute = reroute_(runtime, pathTranslations)
