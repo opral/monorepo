f43cb837e08a05b8213991b20f6390e17a0fffc8
Loris Sigrist <sigrist@hasch.io>
2024-01-12 09:21:44 +0100

    Properly handle Server-Side redirects

--- packages/paraglide/paraglide-js-adapter-sveltekit/example/src/hooks.server.js
+++ packages/paraglide/paraglide-js-adapter-sveltekit/example/src/hooks.server.js
@@ -1,4 +1,4 @@
-import { handleRedirects, handle as _handle } from "@inlang/paraglide-js-adapter-sveltekit"
+import { handle as _handle } from "@inlang/paraglide-js-adapter-sveltekit"
 import { sequence } from "@sveltejs/kit/hooks"
 
 export const handle = sequence(_handle({ langPlaceholder: "%lang%" }))
