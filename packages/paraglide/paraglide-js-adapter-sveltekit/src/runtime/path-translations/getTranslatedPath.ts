4877cbf988034dea6ba194fdab21b8acbbbc7776
Loris Sigrist <sigrist@hasch.io>
2024-01-12 14:31:11 +0100

    Enable slugs

--- packages/paraglide/paraglide-js-adapter-sveltekit/src/runtime/path-translations/getTranslatedPath.ts
+++ packages/paraglide/paraglide-js-adapter-sveltekit/src/runtime/path-translations/getTranslatedPath.ts
@@ -1,3 +1,5 @@
+import { matches } from "./matching/match.js"
+import { resolvePath } from "./matching/resolvePath.js"
 import type { PathTranslations } from "./types.js"
 
 export function getTranslatedPath(
@@ -5,10 +7,19 @@ export function getTranslatedPath(
 	lang: string,
 	translations: PathTranslations<string>
 ) {
-	const pathTranslations = translations[canonicalPath]
-	if (pathTranslations) {
-		const translatedPath = pathTranslations[lang]
-		if (translatedPath) return translatedPath
-	}
-	return canonicalPath
+	const pathTranslationsDefinition = Object.keys(translations).find((path) => {
+		return matches(canonicalPath, path).matches
+	})
+
+	if (!pathTranslationsDefinition) return canonicalPath
+	const translationsForPath = translations[pathTranslationsDefinition]
+	if (!translationsForPath) return canonicalPath
+
+	const translatedPath = translationsForPath[lang]
+	if (!translatedPath) return canonicalPath
+
+	const parseResult = matches(canonicalPath, pathTranslationsDefinition)
+	if (!parseResult.matches) return canonicalPath
+
+	return resolvePath(translatedPath, parseResult.params)
 }
