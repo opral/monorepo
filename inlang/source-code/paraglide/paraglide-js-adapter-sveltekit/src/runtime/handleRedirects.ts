a1954e69ce192e3adb067aec3fba1b8aefb93002
Loris Sigrist <sigrist@hasch.io>
2024-01-12 09:21:44 +0100

    Properly handle Server-Side redirects

--- inlang/source-code/paraglide/paraglide-js-adapter-sveltekit/src/runtime/handleRedirects.ts
+++ inlang/source-code/paraglide/paraglide-js-adapter-sveltekit/src/runtime/handleRedirects.ts
@@ -1,36 +1,40 @@
 import type { Handle } from "@sveltejs/kit"
 import type { Paraglide } from "./runtime.js"
+import type { PathTranslations } from "./translate-paths/path-translations.js"
+import { getTranslatedPath } from "./translate-paths/translate.js"
+import { isExternal } from "./utils/external.js"
+import { base } from "$app/paths"
 
 /**
  * This is a SvelteKit Server hook that rewrites redirects to internal pages to use the correct language.s
  * @param param0
  * @returns
  */
-export const handleRedirects: (runtime: Paraglide<string>) => Handle =
-	(runtime) =>
+export const handleRedirects: (
+	runtime: Paraglide<string>,
+	translations: PathTranslations<string>
+) => Handle =
+	(runtime, translations) =>
 	async ({ event, resolve }) => {
 		const response = await resolve(event)
 		if (!isRedirect(response)) return response
 
 		//If the redirect is to an internal page, we need to rewrite the url to include the language
 		const location = response.headers.get("location")
-		if (!location || !location.startsWith("/")) return response
+		if (!location) return response
 
-		const language = getLanguageFromURL(event.url) ?? runtime.sourceLanguageTag
-		const newLocation = translatePath(location, language)
+		const from = new URL(event.url)
+		const to = new URL(location, from)
 
-		response.headers.set("location", newLocation)
+		if (isExternal(to, from, base)) return response
+
+		//TODO Determin based on the URL
+		const lang = runtime.sourceLanguageTag
+
+		const translatedPath = getTranslatedPath(to.pathname, lang, translations)
+		response.headers.set("location", translatedPath)
 		return response
 	}
-
-function getLanguageFromURL(url: URL) {
-	const match = url.pathname.match(/^\/([a-z]{2})\//)
-	return match?.[1]
-}
-
-function translatePath(path: string, language: string) {
-	return path.replace(/^\/([a-z]{2})\//, `/${language}/`)
-}
 
 function isRedirect(response: Response) {
 	return response.status >= 300 && response.status < 400
