66b7a676b51188c40d15295d99eb7fac879644d3
Loris Sigrist <sigrist@hasch.io>
2024-01-15 13:17:46 +0100

    Automatically infer the language from the URL

---
+++ packages/paraglide/paraglide-js-adapter-sveltekit/example/src/routes/users/[id]/edit/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+	import { base } from '$app/paths'
+    import { page } from '$app/stores';
+    import * as m from '$paraglide/messages.js';
+	import { availableLanguageTags } from '$paraglide/runtime'
+</script>
+
+<h1>{$page.params.id}</h1>
+
+{#each availableLanguageTags as lang}
+    <a href="{base}/users/{$page.params.id}/edit" hreflang={lang}>
+    {m.change_language_to({ languageTag: lang })}
+    </a>
+    <br />
+{/each}
+
