1e69030201fd0b58c747a18144231b85ce143dae
Felix Haeberle <kontakt@felixhaeberle.de>
2024-01-16 17:20:28 +0100

    restructure code #1879

--- inlang/source-code/ide-extension/src/main.ts
+++ inlang/source-code/ide-extension/src/main.ts
@@ -15,7 +15,9 @@ import { messageView } from "./utilities/messages/messages.js"
 import { listProjects } from "@inlang/sdk"
 import { createFileSystemMapper } from "./utilities/fs/createFileSystemMapper.js"
 import fs from "node:fs/promises"
-import { normalizePath } from "@lix-js/fs"
+import { normalizePath, type NodeishFilesystem } from "@lix-js/fs"
+import { gettingStartedView } from "./utilities/getting-started/gettingStarted.js"
+import { closestInlangProject } from "./utilities/project/closestInlangProject.js"
 
 // Entry Point
 export async function activate(context: vscode.ExtensionContext): Promise<void> {
@@ -23,7 +25,25 @@ export async function activate(context: vscode.ExtensionContext): Promise<void> 
 		const gitOrigin = await getGitOrigin()
 		telemetryCapture("IDE-EXTENSION activated")
 
-		await main({ context, gitOrigin })
+		vscode.commands.executeCommand("setContext", "inlang:hasProjectInWorkspace", false)
+		const workspaceFolder = vscode.workspace.workspaceFolders?.[0]
+
+		if (!workspaceFolder) {
+			console.warn("No workspace folder found.")
+			return
+		}
+
+		const nodeishFs = createFileSystemMapper(normalizePath(workspaceFolder.uri.fsPath), fs)
+
+		try {
+			const projectsList = await listProjects(nodeishFs, normalizePath(workspaceFolder.uri.fsPath))
+			setState({ ...state(), projectsInWorkspace: projectsList })
+		} catch (error) {
+			handleError(error)
+			return
+		}
+
+		await main({ context, gitOrigin, workspaceFolder, nodeishFs })
 		msg("inlang's extension activated", "info")
 	} catch (error) {
 		handleError(error)
@@ -34,59 +54,71 @@ export async function activate(context: vscode.ExtensionContext): Promise<void> 
 async function main(args: {
 	context: vscode.ExtensionContext
 	gitOrigin: string | undefined
+	workspaceFolder: vscode.WorkspaceFolder
+	nodeishFs: NodeishFilesystem
 }): Promise<void> {
-	vscode.commands.executeCommand("setContext", "inlang:hasProjectInWorkspace", false)
-	const workspaceFolder = vscode.workspace.workspaceFolders?.[0]
-
-	if (!workspaceFolder) {
-		console.warn("No workspace folder found.")
-		return
-	}
-
-	const nodeishFs = createFileSystemMapper(normalizePath(workspaceFolder.uri.fsPath), fs)
-
-	try {
-		const projectsList = await listProjects(nodeishFs, normalizePath(workspaceFolder.uri.fsPath))
-		setState({ ...state(), projectsInWorkspace: projectsList })
-	} catch (error) {
-		handleError(error)
-		return
-	}
-
 	if (state().projectsInWorkspace.length > 0) {
 		vscode.commands.executeCommand("setContext", "inlang:hasProjectInWorkspace", true)
+
+		// find the closest project to the workspace
+		const closestProjectToWorkspace = await closestInlangProject({
+			workingDirectory: normalizePath(args.workspaceFolder.uri.fsPath),
+			projects: state().projectsInWorkspace,
+		})
+
+		setState({
+			...state(),
+			selectedProjectPath:
+				closestProjectToWorkspace?.projectPath || state().projectsInWorkspace[0]?.projectPath || "",
+		})
+
+		await projectView({ ...args })
+		await messageView({ ...args })
+		await errorView({ ...args })
+
+		registerExtensionComponents(args)
+		// TODO: Replace by reactive settings API?
+		setupFileSystemWatcher(args)
+		handleInlangErrors()
+	} else {
+		await gettingStartedView()
 	}
-
-	await projectView({ ...args, workspaceFolder, nodeishFs })
-	await messageView({ ...args })
-	await errorView({ ...args })
-
-	setupFileSystemWatcher(workspaceFolder, args)
-	registerExtensionComponents(workspaceFolder, args)
-	handleInlangErrors()
 }
 
-function setupFileSystemWatcher(
-	workspaceFolder: vscode.WorkspaceFolder,
-	args: { context: vscode.ExtensionContext; gitOrigin: string | undefined }
-) {
+function setupFileSystemWatcher(args: {
+	context: vscode.ExtensionContext
+	workspaceFolder: vscode.WorkspaceFolder
+	nodeishFs: NodeishFilesystem
+	gitOrigin: string | undefined
+}) {
 	const watcher = vscode.workspace.createFileSystemWatcher(
-		new vscode.RelativePattern(workspaceFolder, CONFIGURATION.FILES.PROJECT)
+		new vscode.RelativePattern(
+			args.workspaceFolder,
+			state().selectedProjectPath || CONFIGURATION.FILES.PROJECT
+		)
 	)
 
-	watcher.onDidChange(() => main(args))
+	watcher.onDidChange(async () => {
+		// reload project
+		await main({
+			context: args.context,
+			gitOrigin: args.gitOrigin,
+			workspaceFolder: args.workspaceFolder,
+			nodeishFs: args.nodeishFs,
+		})
+	})
 }
 
-function registerExtensionComponents(
-	workspaceFolder: vscode.WorkspaceFolder,
-	args: { context: vscode.ExtensionContext; gitOrigin: string | undefined }
-) {
+function registerExtensionComponents(args: {
+	context: vscode.ExtensionContext
+	workspaceFolder: vscode.WorkspaceFolder
+}) {
 	args.context.subscriptions.push(
 		...Object.values(CONFIGURATION.COMMANDS).map((c) => c.register(c.command, c.callback as any))
 	)
 
 	const additionalSelectors =
-		state().project?.customApi()["app.inlang.ideExtension"]?.documentSelectors || []
+		state().project.customApi()["app.inlang.ideExtension"]?.documentSelectors || []
 
 	const documentSelectors: vscode.DocumentSelector = [
 		{ language: "javascript", pattern: `!${CONFIGURATION.FILES.PROJECT}` },
@@ -101,12 +133,12 @@ function registerExtensionComponents(
 
 	messagePreview(args)
 	propertiesMissingPreview()
-	recommendation({ workspaceFolder })
+	recommendation(args)
 	linterDiagnostics(args)
 }
 
 function handleInlangErrors() {
-	const inlangErrors = state().project?.errors() || []
+	const inlangErrors = state().project.errors() || []
 	if (inlangErrors.length > 0) {
 		console.error("Inlang VSCode Extension errors:", inlangErrors)
 	}
@@ -116,3 +148,5 @@ function handleInlangErrors() {
 export function getActiveTextEditor(): vscode.TextEditor | undefined {
 	return vscode.window.activeTextEditor
 }
+
+
