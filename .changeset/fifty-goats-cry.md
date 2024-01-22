870143a220b092162536f3e1d296703c9dbb2159
Felix Haeberle <kontakt@felixhaeberle.de>
2024-01-22 22:51:35 +0100

    update to import with workspaceFolder

---
+++ .changeset/fifty-goats-cry.md
@@ -0,0 +1,5 @@
+---
+"vs-code-extension": patch
+---
+
+update import with workspaceFolder
--- inlang/source-code/ide-extension/src/commands/openProject.ts
+++ inlang/source-code/ide-extension/src/commands/openProject.ts
@@ -6,7 +6,11 @@ export const openProjectCommand = {
 	command: "inlang.openProject",
 	title: "Inlang: Open project",
 	register: vscode.commands.registerCommand,
-	callback: async (node: ProjectViewNode, nodeishFs: NodeishFilesystem) => {
-		await handleTreeSelection({ selectedNode: node, nodeishFs })
+	callback: async (
+		node: ProjectViewNode,
+		nodeishFs: NodeishFilesystem,
+		workspaceFolder: vscode.WorkspaceFolder
+	) => {
+		await handleTreeSelection({ selectedNode: node, nodeishFs, workspaceFolder })
 	},
 }
