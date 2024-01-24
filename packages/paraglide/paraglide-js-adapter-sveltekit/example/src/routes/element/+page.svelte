ad81a07c031519e6961417c19cde7847c2117cb7
Loris Sigrist <sigrist@hasch.io>
2024-01-24 11:52:15 +0100

    .

---
+++ packages/paraglide/paraglide-js-adapter-sveltekit/example/src/routes/element/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+    let isButton = false;
+</script>
+
+<input type="checkbox" bind:checked={isButton} />
+
+<svelte:element this={isButton ? 'button' : 'a'} href="/">
+    {isButton ? 'Button' : 'Link'}
+</svelte:element>
