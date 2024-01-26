2794301bb808e895b40a2387134874f74c835419
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2024-01-26 12:32:52 +0100

    improve: rename "stack" to "library"
    - stack is unclear
    - people search for i18n libaries

--- inlang/source-code/website/src/interface/marketplace/categoryHeaders/cards/astro.tsx
+++ inlang/source-code/website/src/interface/marketplace/categoryHeaders/cards/astro.tsx
@@ -9,7 +9,7 @@ const AstroHeader = () => {
 				<div class="relative z-30 flex-1 flex flex-col items-start gap-4 px-6 py-3 md:min-w-[380px]">
 					<div class="flex flex-col gap-2 pt-2 md:pt-6">
 						<p class="text-sm text-[#E23788] font-medium">For the Astro Community</p>
-						<h2 class="font-medium text-xl">Recommended i18n stack</h2>
+						<h2 class="font-medium text-xl">Recommended i18n library</h2>
 					</div>
 					<p class="text-sm text-surface-500 pr-20">
 						A small collection of tooling to get you going to build your global Astro app with
