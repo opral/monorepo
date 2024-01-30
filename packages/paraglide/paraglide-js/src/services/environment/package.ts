53c173cae28a5f401756032506597fc1e8080391
Loris Sigrist <sigrist@hasch.io>
2024-01-30 14:57:57 +0100

    Formalize `pathExists` function

--- packages/paraglide/paraglide-js/src/services/environment/package.ts
+++ packages/paraglide/paraglide-js/src/services/environment/package.ts
@@ -1,18 +1,17 @@
 import type { NodeishFilesystem } from "@lix-js/fs"
+import path from "node:path"
+import { pathExists } from "../file-handling/exists.js"
 
 /**
  * Attempts to find the package.json file that's closest to the current working directory.
  *
  * @param fs The filesystem to use.
- * @param path The path to start searching from.
  * @returns The path to the package.json file, or undefined if none was found.
  */
-export async function findPackageJson(fs: NodeishFilesystem, path: string): string | undefined {
-	try {
-	} catch (error) {
-		if (error.code === "ENOENT") {
-			return undefined
-		}
-		throw error
-	}
+export async function findPackageJson(fs: NodeishFilesystem): Promise<string | undefined> {
+	const potentialPackageJsonPath = path.resolve(process.cwd(), "package.json")
+
+	const packageJsonExists = await pathExists(potentialPackageJsonPath, fs)
+	if (packageJsonExists) return potentialPackageJsonPath
+	return undefined
 }
