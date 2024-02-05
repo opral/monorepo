1fa776ad5c0aed346b37915e625a853648b53593
Loris Sigrist <sigrist@hasch.io>
2024-02-05 11:39:56 +0100

    Make installable via astro add

--- inlang/source-code/paraglide/paraglide-js-adapter-astro/src/ambient.d.ts
+++ inlang/source-code/paraglide/paraglide-js-adapter-astro/src/ambient.d.ts
@@ -11,6 +11,11 @@ declare module "paraglide-js-adapter-astro:runtime" {
 	export const sourceLanguageTag: string
 }
 
+declare module "astro:i18n" {
+	export const getLocaleByPath: (path: string) => string
+	export const getPathByLocale: (locale: string) => string
+}
+
 declare namespace App {
 	interface Locals {
 		paraglide: {
