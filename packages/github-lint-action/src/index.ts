63d277bb22efe708d225ae934f92d014e55949f6
NiklasBuchfink <niklas.buchfink@web.de>
2024-02-08 15:38:26 +0100

    try different module resolution

--- packages/github-lint-action/src/index.ts
+++ packages/github-lint-action/src/index.ts
@@ -1,7 +1,7 @@
 /**
  * The entrypoint for the action.
  */
-import { run } from "./main.js"
+import { run } from "./main.mjs"
 
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
 //console.log("before run")
