23e75f2f945ec8d28e836daf0ee246b765290a9b
NiklasBuchfink <niklas.buchfink@web.de>
2024-02-08 19:24:21 +0100

    Fix savePath calculation in createImport function

--- packages/github-lint-action/import/_import.ts
+++ packages/github-lint-action/import/_import.ts
@@ -21,7 +21,12 @@ const createImport = async (uri: string, basePath: string) => {
 		? await(await fetch(uri)).text()
 		: await fs.readFile(uri, { encoding: "utf-8" })
 
-	const savePath = basePath + "/" + uri.slice(2)
+	const savePath = basePath + "/" + uri.slice(4)
+	console.log(uri.slice(1))
+	console.log(uri.slice(2))
+	console.log(uri.slice(3))
+	console.log(uri.slice(4))
+	console.log(uri.slice(5))
 	console.log("Saving to", savePath)
 	await fs.writeFile(savePath, moduleAsText).catch((e) => {
 		console.error("Error while saving file", e)
