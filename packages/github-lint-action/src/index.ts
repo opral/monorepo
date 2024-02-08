5819f0064e0c153e910cf50c51dd507593b10844
NiklasBuchfink <niklas.buchfink@web.de>
2024-02-08 16:18:39 +0100

    revert module resolution

--- packages/github-lint-action/src/index.ts
+++ packages/github-lint-action/src/index.ts
@@ -1,7 +1,7 @@
 /**
  * The entrypoint for the action.
  */
-import { run } from "./main.mjs"
+import { run } from "./main.js"
 
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
 //console.log("before run")
