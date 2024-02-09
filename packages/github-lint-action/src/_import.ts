50c4140c4c0abd87ea266fe7da37b1a32386d9ce
NiklasBuchfink <niklas.buchfink@web.de>
2024-02-09 13:18:35 +0100

    Remove unnecessary devDependencies and import statements

--- packages/github-lint-action/src/_import.ts
+++ packages/github-lint-action/src/_import.ts
@@ -1,8 +1,6 @@
 import { normalizePath } from "@lix-js/fs"
 import type { ImportFunction } from "@inlang/sdk"
 import fs from "node:fs/promises"
-import ts from "typescript"
-import requireFromString from "require-from-string"
 
 /**
  * Wraps the import function to inject the base path.
@@ -29,29 +27,5 @@ const createImport = async (uri: string, basePath: string) => {
 	await fs.writeFile(savePath, moduleAsText).catch((e) => {
 		console.error("Error while saving file", e)
 	})
-	try {
-		const asCjs = transpileToCjs(moduleAsText)
-		const module = requireFromString(asCjs)
-		// for whatever reason, the default export is a function
-		// that we need to call to get the property
-		// if (module.default) {
-		// 	module.default = module.default()
-		// }
-		return module
-	} catch (error) {
-		throw new Error(
-			`Error while importing ${uri}: ${(error as Error)?.message ?? "Unknown error"}`,
-			{ cause: error }
-		)
-	}
-}
-
-function transpileToCjs(code: string): string {
-	const compilerOptions: ts.CompilerOptions = {
-		module: ts.ModuleKind.CommonJS,
-		target: ts.ScriptTarget.ES2020,
-	}
-
-	const result = ts.transpileModule(code, { compilerOptions })
-	return result.outputText
+	return moduleAsText
 }
