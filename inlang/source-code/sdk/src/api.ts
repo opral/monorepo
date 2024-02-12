acfffc07e092e1b0851bdb7c35bf434335e9755e
jldec <jurgen.leschner@inlang.com>
2024-02-12 23:30:19 +0000

    subscribable getByDefaultAlias() with test

--- inlang/source-code/sdk/src/api.ts
+++ inlang/source-code/sdk/src/api.ts
@@ -73,7 +73,9 @@ export type MessageQueryApi = {
 		) => void
 	}
 	// use getByDefaultAlias() to resolve a message from its alias, not subscribable
-	getByDefaultAlias: (alias: Message["alias"]["default"]) => Readonly<Message>
+	getByDefaultAlias: ((alias: Message["alias"]["default"]) => Readonly<Message>) & {
+		subscribe: (alias: Message["alias"]["default"], callback: (message: Message) => void) => void
+	}
 	includedMessageIds: Subscribable<Message["id"][]>
 	/*
 	 * getAll is depricated do not use it
