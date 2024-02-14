2cecc1231b46c775d40d2c1d8883a47331bfc55c
NiklasBuchfink <niklas.buchfink@web.de>
2024-02-14 18:16:55 +0100

    Update package.json and import statements

--- inlang/source-code/github-lint-action/src/_import.ts
+++ inlang/source-code/github-lint-action/src/_import.ts
@@ -1,6 +1,6 @@
-import path from "node:path"
-import crypto from "node:crypto"
-import fs from "node:fs"
+import * as path from "node:path"
+import * as crypto from "node:crypto"
+import * as fs from "node:fs"
 import { normalizePath } from "@lix-js/fs"
 import type { ImportFunction } from "@inlang/sdk"
 
