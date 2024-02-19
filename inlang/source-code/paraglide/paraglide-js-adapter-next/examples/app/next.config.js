9c8967544c7bc15ae36a697947d49562bed6726a
Loris Sigrist <loris@sigrist.dev>
2024-02-19 11:06:32 +0100

    stash

--- inlang/source-code/paraglide/paraglide-js-adapter-next/examples/app/next.config.js
+++ inlang/source-code/paraglide/paraglide-js-adapter-next/examples/app/next.config.js
@@ -1,7 +1,7 @@
-const { withParaglide } = require("@inlang/paraglide-js-adapter-next/plugin")
+const { paraglide } = require("@inlang/paraglide-js-adapter-next/plugin")
 
 /** @type {import('next').NextConfig} */
-module.exports = withParaglide({
+module.exports = paraglide({
 	paraglide: {
 		project: "./project.inlang",
 		outdir: "./src/paraglide",
