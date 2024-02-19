9ffd48b8a278fd5897957ffa51b53d26b963493c
Loris Sigrist <loris@sigrist.dev>
2024-02-19 13:27:42 +0100

    resolveRoute ignore path if it doesn't start with the base

--- inlang/source-code/paraglide/paraglide-js-adapter-sveltekit/src/runtime/adapter.test.ts
+++ inlang/source-code/paraglide/paraglide-js-adapter-sveltekit/src/runtime/adapter.test.ts
@@ -5,18 +5,66 @@ import { base } from "$app/paths"
 import * as runtime from "$paraglide/runtime.js"
 
 describe("createI18n", () => {
-	it("resolves routes", () => {
-		const i18n = createI18n(runtime)
+	describe("resolveRoute", () => {
+		it("resolves routes", () => {
+			const i18n = createI18n(runtime)
 
-		//don't prefix the default language
-		expect(i18n.resolveRoute(base + "/about", "en")).toBe(base + "/about")
-		expect(i18n.resolveRoute(base + "/about", "de")).toBe(base + "/de/about")
+			//don't prefix the default language
+			expect(i18n.resolveRoute(base + "/about", "en")).toBe(base + "/about")
+			expect(i18n.resolveRoute(base + "/about", "de")).toBe(base + "/de/about")
+		})
+
+		it("uses pathnames", () => {
+			const i18n = createI18n(runtime, {
+				pathnames: {
+					"/about": {
+						en: "/about",
+						de: "/ueber-uns",
+					},
+				},
+			})
+
+			expect(i18n.resolveRoute(base + "/about", "en")).toBe(base + "/about")
+			expect(i18n.resolveRoute(base + "/about", "de")).toBe(base + "/de/ueber-uns")
+		})
+
+		it.skipIf(base === "")("leaves the path as is if it doen't start with the base path", () => {
+			const i18n = createI18n(runtime)
+
+			expect(i18n.resolveRoute("/about", "en")).toBe("/about")
+			expect(i18n.resolveRoute("/about", "de")).toBe("/about")
+		})
+
+		it("leaves the path as is if it's excluded", () => {
+			const i18n = createI18n(runtime, {
+				exclude: [base + "/about"],
+			})
+
+			expect(i18n.resolveRoute(base + "/about", "en")).toBe(base + "/about")
+			expect(i18n.resolveRoute(base + "/about", "de")).toBe(base + "/about")
+		})
 	})
 
-	it("get's the canonical route from a localised path", () => {
-		const i18n = createI18n(runtime)
+	describe("route", () => {
+		it("get's the canonical route from a localised path", () => {
+			const i18n = createI18n(runtime)
 
-		expect(i18n.route(base + "/about")).toBe(base + "/about")
-		expect(i18n.route(base + "/de/about")).toBe(base + "/about")
+			expect(i18n.route(base + "/about")).toBe(base + "/about")
+			expect(i18n.route(base + "/de/about")).toBe(base + "/about")
+		})
+
+		it("uses pathnames", () => {
+			const i18n = createI18n(runtime, {
+				pathnames: {
+					"/about": {
+						en: "/about",
+						de: "/ueber-uns",
+					},
+				},
+			})
+
+			expect(i18n.route(base + "/about")).toBe(base + "/about")
+			expect(i18n.route(base + "/de/ueber-uns")).toBe(base + "/about")
+		})
 	})
 })
