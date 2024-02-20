0a7af7f5e11d4913277cb0c8a6c07a5a4dff00ee
LorisSigrist <loris@sigrist.dev>
2024-02-20 16:03:58 +0100

    Fully test `<Link>` component

--- inlang/source-code/paraglide/paraglide-js-adapter-next/src/app/navigation/Link.test.tsx
+++ inlang/source-code/paraglide/paraglide-js-adapter-next/src/app/navigation/Link.test.tsx
@@ -1,9 +1,33 @@
 import React from "react"
-import { expect, test } from "vitest"
-import { render, screen } from "@testing-library/react"
+import { describe, it, expect, beforeEach, afterEach } from "vitest"
+import { render, screen, cleanup } from "@testing-library/react"
 import { Link } from "./Link"
+import { setLanguageTag, sourceLanguageTag } from "$paraglide/runtime.js"
 
-test("<Link>", () => {
-	render(<Link href="/about" locale="de" data-testid="link" />)
-	expect(screen.getByTestId("link").getAttribute("href")).toEqual("/de/about")
+describe("<Link>", () => {
+	beforeEach(() => {
+		setLanguageTag(sourceLanguageTag)
+	})
+
+	afterEach(cleanup)
+	it("renders a link with the correct localised href", () => {
+		render(
+			<>
+				<Link href="/about" locale="de" data-testid="german-link" />
+				<Link href="/about" locale="en" data-testid="english-link" />
+			</>
+		)
+		expect(screen.getByTestId("english-link").getAttribute("href")).toEqual("/about")
+		expect(screen.getByTestId("german-link").getAttribute("href")).toEqual("/de/about")
+	})
+
+	it("renders a link with the current language if no locale is provided", () => {
+		setLanguageTag("de")
+		render(<Link href="/about" data-testid="german-link" />)
+		expect(screen.getByTestId("german-link").getAttribute("href")).toEqual("/de/about")
+
+		setLanguageTag("en")
+		render(<Link href="/about" data-testid="english-link" />)
+		expect(screen.getByTestId("english-link").getAttribute("href")).toEqual("/about")
+	})
 })
