b2f951b8ee0e610cd876584bdd66ed65a3b4f179
Nils Jacobsen <nils.jacobsen98@gmail.com>
2024-02-20 16:26:26 +0100

    update: add storybook and discover setup

---
+++ inlang/source-code/shared-ui/src/stories/ui-button.ts
@@ -0,0 +1,26 @@
+import { html, LitElement } from "lit"
+import { customElement, property } from "lit/decorators.js"
+import "@shoelace-style/shoelace/dist/components/button/button.js"
+
+@customElement("ui-button")
+export class UiButton extends LitElement {
+	@property()
+	size: "small" | "medium" | "large" = "small"
+
+	@property()
+	variant: "default" | "primary" | "success" | "neutral" | "warning" | "danger" | "text" = "default"
+
+	@property()
+	label: string = "Button"
+
+	override render() {
+		return html` <sl-button size=${this.size} variant=${this.variant}>${this.label}</sl-button> `
+	}
+}
+
+// add types
+declare global {
+	interface HTMLElementTagNameMap {
+		"ui-button": UiButton
+	}
+}
