a81c37f149e1cd90a4c029f346c3c57366ffbb39
jldec <jurgen.leschner@inlang.com>
2024-02-20 23:00:00 +0000

    small error handling improvements for cli translate

--- inlang/source-code/cli/src/utilities/log.ts
+++ inlang/source-code/cli/src/utilities/log.ts
@@ -9,3 +9,17 @@ import consola from "consola"
  *   log.success("Success")
  */
 export const log = consola
+
+export function logError(error: any) {
+	log.error(causeString(error), error)
+}
+
+// Convert error.cause into a string for logging
+export function causeString(error: any) {
+	if (typeof error === "object" && error.cause) {
+		if (error.cause.errors?.length) return error.cause.errors.join(", ")
+		if (error.cause.code) return "" + error.cause.code
+		return JSON.stringify(error.cause)
+	}
+	return ""
+}
