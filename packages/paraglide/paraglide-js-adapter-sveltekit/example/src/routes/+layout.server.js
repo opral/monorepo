3528c596e013afc136bdd37a13603855888ebb4c
LorisSigrist <loris@sigrist.dev>
2024-02-20 15:10:44 +0100

    Clean up

--- packages/paraglide/paraglide-js-adapter-sveltekit/example/src/routes/+layout.server.js
+++ packages/paraglide/paraglide-js-adapter-sveltekit/example/src/routes/+layout.server.js
@@ -1,13 +1,11 @@
 export const prerender = true
 
-let i = 0
-
 /**
  * @type { import("./$types").LayoutServerLoad}
  */
 export function load({ locals, depends }) {
+	// This tells SvelteKit to re-run this load function when the language changes
 	depends("paraglide:lang")
-	console.log("Server layout load", i++)
 
 	return {
 		serverLang: `The language on the server is ${locals.paraglide.lang}`,
