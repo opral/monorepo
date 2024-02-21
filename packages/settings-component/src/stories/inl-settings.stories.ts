aab9beef9ee0812a06fe81498289b76ce8a513dc
Nils Jacobsen <nils.jacobsen98@gmail.com>
2024-02-21 14:53:34 +0100

    add settings component

---
+++ packages/settings-component/src/stories/inl-settings.stories.ts
@@ -0,0 +1,14 @@
+import "./inl-settings.ts"
+import type { Meta, StoryObj } from "@storybook/web-components"
+
+const meta: Meta = {
+	component: "inl-settings",
+	title: "Example/inl-settings",
+	tags: ["autodocs"],
+}
+
+export default meta
+
+export const Default: StoryObj = {
+	args: {},
+}
