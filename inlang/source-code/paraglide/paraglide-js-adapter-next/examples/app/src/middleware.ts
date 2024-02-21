070a652047902b0387925c5bb68f0de34abac2a3
LorisSigrist <loris@sigrist.dev>
2024-02-21 11:00:04 +0100

    .

--- inlang/source-code/paraglide/paraglide-js-adapter-next/examples/app/src/middleware.ts
+++ inlang/source-code/paraglide/paraglide-js-adapter-next/examples/app/src/middleware.ts
@@ -2,5 +2,5 @@ export { middleware } from "@inlang/paraglide-js-adapter-next"
 
 export const config = {
 	//Must be hardcoded string
-	matcher: ["/", `/(de|en|de-CH)/:path*`],
+	matcher: ["/", `/:path*`],
 }
