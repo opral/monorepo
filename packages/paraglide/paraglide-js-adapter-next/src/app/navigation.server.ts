85fa9345b2dd96e2c697a2e6999982e6dfc9f6e9
LorisSigrist <loris@sigrist.dev>
2024-02-22 17:36:09 +0100

    Use strategy in hooks

--- packages/paraglide/paraglide-js-adapter-next/src/app/navigation.server.ts
+++ packages/paraglide/paraglide-js-adapter-next/src/app/navigation.server.ts
@@ -1,4 +1,13 @@
 import { createNavigation } from "./navigation.base"
 import { getLanguage } from "./getLanguage.server"
+import { availableLanguageTags, sourceLanguageTag } from "$paraglide/runtime.js"
+import { prefixStrategy } from "./routing/prefix"
 
-export const { useRouter, redirect, permanentRedirect, usePathname } = createNavigation(getLanguage)
+export const { useRouter, redirect, permanentRedirect, usePathname } = createNavigation(
+	getLanguage,
+	prefixStrategy({
+		availableLanguageTags,
+		sourceLanguageTag,
+		exclude: [],
+	})
+)
