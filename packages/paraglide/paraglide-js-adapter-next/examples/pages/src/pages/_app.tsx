08249aaf70b0f9c141f223c4c8bb72fb99d41806
LorisSigrist <loris@sigrist.dev>
2024-02-26 14:41:15 +0100

    Remove unnecessary props passed to `<ParaglideJS>` component

--- packages/paraglide/paraglide-js-adapter-next/examples/pages/src/pages/_app.tsx
+++ packages/paraglide/paraglide-js-adapter-next/examples/pages/src/pages/_app.tsx
@@ -1,10 +1,9 @@
 import type { AppProps } from "next/app"
 import { ParaglideJS } from "@inlang/paraglide-js-adapter-next/pages"
-import * as runtime from "@/paraglide/runtime.js"
 
-export default function App({ Component, pageProps, router }: AppProps) {
+export default function App({ Component, pageProps }: AppProps) {
 	return (
-		<ParaglideJS runtime={runtime} language={router.locale}>
+		<ParaglideJS>
 			<Component {...pageProps} />
 		</ParaglideJS>
 	)
