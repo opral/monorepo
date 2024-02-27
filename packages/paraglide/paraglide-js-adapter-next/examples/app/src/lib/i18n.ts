aad7f33d8db6c457255699b5458fea811dc0e59b
LorisSigrist <loris@sigrist.dev>
2024-02-27 11:30:32 +0100

    Fix path validation

--- packages/paraglide/paraglide-js-adapter-next/examples/app/src/lib/i18n.ts
+++ packages/paraglide/paraglide-js-adapter-next/examples/app/src/lib/i18n.ts
@@ -1,15 +1,12 @@
 import { AvailableLanguageTag } from "@/paraglide/runtime"
 import { createI18n } from "@inlang/paraglide-js-adapter-next"
+import * as m from "@/paraglide/messages"
 
 export const { Link, middleware, useRouter, usePathname, redirect, permanentRedirect } =
 	createI18n<AvailableLanguageTag>({
 		exclude: ["/not-translated"],
 		pathnames: {
-			"/about": {
-				en: "/about",
-				de: "/ueber-uns",
-				"de-CH": "/ueber-uns",
-			},
+			"/about": m.about_path,
 			"/form": {
 				en: "/form",
 				de: "/formular",
