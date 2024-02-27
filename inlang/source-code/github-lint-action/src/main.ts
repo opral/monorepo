366175150e22ce3e14031cc6868110ff67627376
NiklasBuchfink <niklas.buchfink@web.de>
2024-02-27 18:29:15 +0100

    Retrigger action

--- inlang/source-code/github-lint-action/src/main.ts
+++ inlang/source-code/github-lint-action/src/main.ts
@@ -11,6 +11,7 @@ import { exec } from "node:child_process"
  */
 export async function run(): Promise<void> {
 	core.debug("Running the action")
+	console.debug("Running the action")
 
 	try {
 		const token = process.env.GITHUB_TOKEN
