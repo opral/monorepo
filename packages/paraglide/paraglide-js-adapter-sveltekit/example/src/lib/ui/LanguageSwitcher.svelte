179f7163f0d73b275f67096a2976051602941d12
LorisSigrist <loris@sigrist.dev>
2024-02-29 13:41:05 +0100

    Update SvelteKit example

---
+++ packages/paraglide/paraglide-js-adapter-sveltekit/example/src/lib/ui/LanguageSwitcher.svelte
@@ -0,0 +1,35 @@
+<script>
+    import { availableLanguageTags, languageTag } from "$paraglide/runtime";
+    import { i18n } from "$lib/i18n";
+	import { goto } from "$app/navigation"
+    import { page } from "$app/stores";
+	import { get } from "svelte/store"
+
+    /**
+     * @param { import("$paraglide/runtime").AvailableLanguageTag } newLanguage
+     */
+    function switchToLanguage(newLanguage) {
+        const canonicalPath = i18n.route(get(page).url.pathname)
+        const localisedPath = i18n.resolveRoute(canonicalPath, newLanguage)
+        goto(localisedPath)
+    }
+
+    /**
+     * @type {Record<import("$paraglide/runtime").AvailableLanguageTag, string>}
+     */
+    const labels = {
+        en: "🇬🇧 English",
+        de: "🇩🇪 Deutsch",
+        fr: "🇫🇷 Français",
+        ru: "🇷🇺 Русский",
+    }
+</script>
+
+<select on:change={e => switchToLanguage(/** @type {any} */ (e).target.value)}>
+    {#each availableLanguageTags as langTag}
+        <option 
+            value={langTag}
+            selected={languageTag() === langTag}
+            >{labels[langTag]}</option>
+    {/each}
+</select>
