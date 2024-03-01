edb1a9dd10664bcfb2c7ba5c25a60d85d53d8bae
LorisSigrist <loris@sigrist.dev>
2024-03-01 14:23:10 +0100

    Use fully qualified hrefs in `Link` headers

--- inlang/source-code/paraglide/paraglide-js-adapter-next/examples/app/src/lib/i18n.ts
+++ inlang/source-code/paraglide/paraglide-js-adapter-next/examples/app/src/lib/i18n.ts
@@ -2,11 +2,18 @@ import { AvailableLanguageTag } from "@/paraglide/runtime"
 import { createI18n } from "@inlang/paraglide-js-adapter-next"
 import * as m from "@/paraglide/messages"
 
-export const { Link, middleware, useRouter, usePathname, redirect, permanentRedirect } =
-	createI18n<AvailableLanguageTag>({
-		pathnames: {
-			"/about": m.about_path,
-		},
+export const {
+	Link,
+	middleware,
+	useRouter,
+	usePathname,
+	redirect,
+	permanentRedirect,
+	localizePath,
+} = createI18n<AvailableLanguageTag>({
+	pathnames: {
+		"/about": m.about_path,
+	},
 
-		exclude: ["/not-translated"], //makes sure that the /not-translated page is not translated
-	})
+	exclude: ["/not-translated"], //makes sure that the /not-translated page is not translated
+})
