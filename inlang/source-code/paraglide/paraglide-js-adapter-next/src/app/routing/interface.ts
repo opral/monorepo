42ecffcd885bdbfc20364ee263d21b51aa693199
LorisSigrist <loris@sigrist.dev>
2024-03-07 11:02:03 +0100

    stash

--- inlang/source-code/paraglide/paraglide-js-adapter-next/src/app/routing/interface.ts
+++ inlang/source-code/paraglide/paraglide-js-adapter-next/src/app/routing/interface.ts
@@ -2,8 +2,8 @@ import { NextRequest } from "next/server"
 
 export interface RoutingStragey<T extends string> {
 	getLocalisedPath(canonicalPath: string, locale: T): string
-	getCanonicalPath(localisedPath: string): string
+	getCanonicalPath(localisedPath: string, locale: T): string
 	resolveLanguage(request: NextRequest): T
-	translatePath(localisedPath: string, newLocale: T): string
+	translatePath(localisedPath: string, currentLocale: T, newLocale: T): string
 	defaultLanguage: T
 }
