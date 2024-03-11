5ac3ab3aeb87cd735c6f1cc17ccdb0e488271ffb
Felix Haeberle <kontakt@felixhaeberle.de>
2024-03-11 15:48:21 +0100

    add typebox for type validation

--- packages/cross-sell/cross-sell-sherlock/src/recommendation/utils/types.ts
+++ packages/cross-sell/cross-sell-sherlock/src/recommendation/utils/types.ts
@@ -1,3 +1,7 @@
-export type ExtensionsJson = {
-	recommendations: string[]
-}
+import { Type, type Static } from "@sinclair/typebox"
+
+export const ExtensionsJson = Type.Object({
+	recommendations: Type.Array(Type.String()),
+})
+
+export type ExtensionsJson = Static<typeof ExtensionsJson>
