37aa16184e3788b79ba467f76c46fcdc2f4b15e4
LorisSigrist <loris@sigrist.dev>
2024-03-12 13:58:48 +0100

    Remove unused languages

--- packages/paraglide/paraglide-js-adapter-sveltekit/example/src/lib/i18n.js
+++ packages/paraglide/paraglide-js-adapter-sveltekit/example/src/lib/i18n.js
@@ -8,26 +8,18 @@ export const i18n = createI18n(runtime, {
 		"/users": {
 			en: "/users",
 			de: "/benutzer",
-			fr: "/utilisateurs",
-			ru: "/пользователи",
 		},
 		"/users/[id]": {
 			en: "/users/[id]",
 			de: "/benutzer/[id]",
-			fr: "/utilisateurs/[id]",
-			ru: "/пользователи/[id]",
 		},
 		"/users/[id]/edit": {
 			en: "/users/[id]/edit",
 			de: "/benutzer/[id]/bearbeiten",
-			fr: "/utilisateurs/[id]/modifier",
-			ru: "/пользователи/[id]/редактировать",
 		},
 		"/some-subpage": {
 			en: "/some-subpage",
 			de: "/irgendeine-unterseite",
-			fr: "/quelque-sous-page",
-			ru: "/какая-то-подстраница",
 		},
 	},
 	seo: {
@@ -37,7 +29,5 @@ export const i18n = createI18n(runtime, {
 	textDirection: {
 		en: "ltr",
 		de: "ltr",
-		fr: "ltr",
-		ru: "ltr",
 	},
 })
