c82dc231c9ac08a5ccb1d1b20963a4bb8f0db159
Nils Jacobsen <nils.jacobsen98@gmail.com>
2024-03-13 11:31:35 +0100

    fix: put sl imports into preview
    - no overwrites if app uses sl-components too

--- inlang/source-code/settings-component/.storybook/preview.js
+++ inlang/source-code/settings-component/.storybook/preview.js
@@ -1,4 +1,17 @@
 /** @type { import('@storybook/web-components').Preview } */
+
+import { setBasePath } from "@shoelace-style/shoelace/dist/utilities/base-path.js"
+setBasePath("./../../node_modules/@shoelace-style/shoelace/dist")
+
+import "@shoelace-style/shoelace/dist/components/input/input.js"
+import "@shoelace-style/shoelace/dist/components/button/button.js"
+import "@shoelace-style/shoelace/dist/components/tag/tag.js"
+import "@shoelace-style/shoelace/dist/components/icon/icon.js"
+import "@shoelace-style/shoelace/dist/components/icon-button/icon-button.js"
+import "@shoelace-style/shoelace/dist/components/select/select.js"
+import "@shoelace-style/shoelace/dist/components/option/option.js"
+import "@shoelace-style/shoelace/dist/components/checkbox/checkbox.js"
+
 const preview = {
 	parameters: {
 		actions: { argTypesRegex: "^on[A-Z].*" },
