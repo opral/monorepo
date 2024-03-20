8f292a69264c8a87485ab42971994833f470ef30
LorisSigrist <loris@sigrist.dev>
2024-03-20 14:46:26 +0100

    Simplify

--- packages/paraglide/paraglide-js-adapter-astro/src/middleware.ts
+++ packages/paraglide/paraglide-js-adapter-astro/src/middleware.ts
@@ -5,16 +5,9 @@ import {
 } from "paraglide-js-adapter-astro:runtime"
 import type { MiddlewareHandler, Locales } from "astro"
 
-export const onRequest: MiddlewareHandler = async (
-	{ url, locals, currentLocale, preferredLocales },
-	next
-) => {
+export const onRequest: MiddlewareHandler = async ({ url, locals, currentLocale }, next) => {
 	const locale = currentLocale ?? getLangFromPath(url.pathname)
 	const dir = guessTextDirection(locale)
-
-	const astroI18n = await maybeGetAstroI18n()
-
-	console.log(astroI18n)
 
 	locals.paraglide = {
 		lang: locale,
@@ -53,48 +46,4 @@ function guessTextDirection(lang: string): "ltr" | "rtl" {
 	} catch (e) {
 		return "ltr"
 	}
-}
-
-async function maybeGetAstroI18n(): Promise<typeof import("astro:i18n") | undefined> {
-	try {
-		return await import("astro:i18n")
-	} catch (e) {
-		return undefined
-	}
-}
-
-/**
- * Copied from Astro's built-in language negotiation.
- *
- * @see https://github.com/withastro/astro/blob/17b4991cffb2f16fb3deba5881c54fa67c9ee9ce/packages/astro/src/i18n/utils.ts#L155
- */
-function computeCurrentLocale(pathname: string, locales: Locales): undefined | string {
-	for (const segment of pathname.split("/")) {
-		for (const locale of locales) {
-			if (typeof locale === "string") {
-				// we skip a locale that isn't present in the current segment
-
-				if (!segment.includes(locale)) continue
-				if (normalizeLocale(locale) === normalizeLocale(segment)) {
-					return locale
-				}
-			} else {
-				if (locale.path === segment) {
-					return locale.codes.at(0)
-				} else {
-					for (const code of locale.codes) {
-						if (normalizeLocale(code) === normalizeLocale(segment)) {
-							return code
-						}
-					}
-				}
-			}
-		}
-	}
-
-	return undefined
-}
-
-function normalizeLocale(locale: string): string {
-	return locale.replaceAll("_", "-").toLowerCase()
 }
