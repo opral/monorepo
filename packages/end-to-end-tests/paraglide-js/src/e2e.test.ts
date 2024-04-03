5940f2ea172937af45f4bfab059ad9fe61bd3a0d
LorisSigrist <loris@sigrist.dev>
2024-04-03 15:37:28 +0200

    Add first e2e test

--- packages/end-to-end-tests/paraglide-js/src/e2e.test.ts
+++ packages/end-to-end-tests/paraglide-js/src/e2e.test.ts
@@ -1,5 +1,10 @@
-import { describe, it, expect } from "vitest"
+import { describe, it, expect, beforeAll, beforeEach } from "vitest"
+import fs from "node:fs/promises"
+import path from "node:path"
 import child_process from "node:child_process"
+import { respondToPrompts, sleep } from "./utils.js"
+
+const tmpDir = path.resolve(process.cwd(), ".tmp")
 
 describe("paraglide-js", () => {
 	it("prints it's version when run with --version", () => {
@@ -8,5 +13,51 @@ describe("paraglide-js", () => {
 		expect(semverRegex.test(stdout)).toBe(true)
 	})
 
-	describe("init", () => {})
+	describe("init", () => {
+		beforeAll(async () => {
+			//remove tmp dir if it exists
+			try {
+				await fs.stat(tmpDir)
+				await fs.rmdir(tmpDir, { recursive: true })
+			} catch (e) {
+				//do nothing
+			}
+		})
+		beforeEach(async () => {
+			//create a tmp dir
+			await fs.mkdir(tmpDir, { recursive: true })
+			return async () => {
+				await fs.rmdir(tmpDir, { recursive: true })
+			}
+		})
+
+		it(
+			"initializes paraglide if no project is present",
+			async () => {
+				await fs.stat(tmpDir)
+				//create empty package.json
+				await fs.writeFile(path.join(tmpDir, "package.json"), "{}", {
+					encoding: "utf-8",
+				})
+
+				const process = child_process.spawn("npx", ["paraglide-js", "init"], {
+					cwd: tmpDir,
+				})
+
+				const responses: Record<string, string> = {
+					"without committing your current changes?": "y",
+					"Which languages do you want to support?": "en, de \r\n",
+					"Which tech stack are you using?": "\r\n",
+				}
+
+				await respondToPrompts(process, responses)
+				await sleep(500)
+				process.kill()
+
+				//expect project.inlang/settings.json to be created
+				expect(await fs.stat(path.join(tmpDir, "project.inlang/settings.json"))).toBeDefined()
+			},
+			{ timeout: 30000 }
+		)
+	})
 })
