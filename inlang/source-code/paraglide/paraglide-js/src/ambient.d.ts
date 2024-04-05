54b96343f2681fdba47e3472e5d49965d5b64370
LorisSigrist <loris@sigrist.dev>
2024-04-05 11:28:06 +0200

    use vite instead of esbuild for building paraglide

---
+++ inlang/source-code/paraglide/paraglide-js/src/ambient.d.ts
@@ -0,0 +1,7 @@
+/**
+ * The Package version from package.json
+ *
+ * This value is inlined by vite during build to avoid having to read the package.json
+ * at runtime
+ */
+declare const PACKAGE_VERSION: string
