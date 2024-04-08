72b10a194f5dee849271af34da962101b455bf40
LorisSigrist <loris@sigrist.dev>
2024-04-08 11:11:08 +0200

    Vendor in SvelteKit's path parser

---
+++ inlang/source-code/paraglide/paraglide-js-adapter-sveltekit/src/runtime/path-translations/matching/routeId.test.ts
@@ -0,0 +1,37 @@
+import { describe, it, expect } from "vitest"
+import { parse_route_id } from "./routeId"
+
+describe("parse_route_id", () => {
+	it("should parse a route id", () => {
+		expect(parse_route_id("/foo/bar")).toEqual({
+			params: [],
+			pattern: /^\/foo\/bar\/?$/,
+		})
+
+		expect(parse_route_id("/[lang]/test")).toEqual({
+			params: [
+				{
+					chained: false,
+					matcher: undefined,
+					name: "lang",
+					optional: false,
+					rest: false,
+				},
+			],
+			pattern: /^\/([^/]+?)\/test\/?$/,
+		})
+
+		expect(parse_route_id("/[[lang]]/test")).toEqual({
+			params: [
+				{
+					chained: true,
+					matcher: undefined,
+					name: "lang",
+					optional: true,
+					rest: false,
+				},
+			],
+			pattern: /^(?:\/([^/]+))?\/test\/?$/,
+		})
+	})
+})
