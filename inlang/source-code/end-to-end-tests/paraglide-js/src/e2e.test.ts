547af9ccc66ff00a3ffa205d3fc59469c9f08a19
LorisSigrist <loris@sigrist.dev>
2024-04-08 17:14:39 +0200

    Make sure `projectPath` is a relative path

--- inlang/source-code/end-to-end-tests/paraglide-js/src/e2e.test.ts
+++ inlang/source-code/end-to-end-tests/paraglide-js/src/e2e.test.ts
@@ -2,6 +2,7 @@ import { describe, it, expect } from "vitest"
 import { prepareEnvironment } from "@gmrchk/cli-testing-library"
 import path from "node:path"
 import child_process from "node:child_process"
+import type { ProjectSettings } from "@inlang/sdk"
 
 const ParaglideLocation = child_process.execSync("which paraglide-js").toString().trim()
 
@@ -19,6 +20,80 @@ describe("paraglide-js", () => {
 				const { spawn, writeFile, readFile, cleanup, path: workingDir } = await prepareEnvironment()
 				await writeFile(path.resolve(workingDir, "package.json"), "{}")
 				process.env.TERM_PROGRAM = "not-vscode"
+				const { wait, waitForText, writeText, debug, pressKey } = await spawn(
+					ParaglideLocation,
+					"init"
+				)
+
+				debug()
+
+				await waitForText("Which languages do you want to support?")
+				await wait(200)
+				await writeText("en, de")
+				await wait(200)
+				await pressKey("enter")
+
+				await waitForText("Where should the compiled files be placed?")
+				await wait(200)
+				await pressKey("enter") // use default value of ./src/paraglide
+
+				await waitForText("Are you using Visual Studio Code?")
+				await wait(200)
+				await writeText("y")
+				await wait(200)
+				await pressKey("enter")
+
+				await waitForText("Which tech stack are you using?")
+				await wait(200)
+				await pressKey("enter")
+				await wait(1000)
+
+				//check that the settings.json file exists
+				const fileContent = await readFile(path.resolve(workingDir, "project.inlang/settings.json"))
+				const settings = JSON.parse(fileContent)
+				expect(settings.languageTags).toEqual(["en", "de"])
+				expect(settings.sourceLanguageTag).toEqual("en")
+
+				//Check that the messages/en.json and messages/de.json files exist
+				expect(await readFile(path.resolve(workingDir, "messages/en.json"))).toBeTruthy()
+				expect(await readFile(path.resolve(workingDir, "messages/de.json"))).toBeTruthy()
+
+				//Check that the compiler ran and generated the files
+				expect(await readFile(path.resolve(workingDir, "src/paraglide/runtime.js"))).toBeTruthy()
+				expect(await readFile(path.resolve(workingDir, "src/paraglide/messages.js"))).toBeTruthy()
+				expect(await readFile(path.resolve(workingDir, "src/paraglide/messages/en.js"))).includes(
+					"export {}"
+				)
+				expect(await readFile(path.resolve(workingDir, "src/paraglide/messages/de.js"))).includes(
+					"export {}"
+				)
+
+				const packageJson = JSON.parse(await readFile(path.resolve(workingDir, "package.json")))
+
+				const expectedVersion = child_process.execSync(ParaglideLocation + " --version").toString()
+				expect(packageJson.devDependencies["@inlang/paraglide-js"].trim()).toEqual(
+					expectedVersion.trim()
+				)
+				await cleanup()
+			},
+			{ timeout: 30_000 }
+		)
+
+		it(
+			"initializes paraglide if another project is present",
+			async () => {
+				const { spawn, writeFile, readFile, cleanup, path: workingDir } = await prepareEnvironment()
+				await writeFile(path.resolve(workingDir, "package.json"), "{}")
+				process.env.TERM_PROGRAM = "not-vscode"
+
+				const existingProject: ProjectSettings = {
+					modules: [],
+					sourceLanguageTag: "en",
+					languageTags: ["en"],
+				}
+
+				writeFile(path.resolve(workingDir, "project.inlang/settings.json"), "{}")
+
 				const { wait, waitForText, writeText, debug, pressKey } = await spawn(
 					ParaglideLocation,
 					"init"
