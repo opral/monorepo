1c9e65c163de17052a4e687f887b7db3976e1873
jldec <jurgen.leschner@opral.com>
2024-04-10 01:08:28 +0100

    sdk/multi-project-test with MOCK_TRANSLATE_LOCAL

---
+++ inlang/source-code/sdk/multi-project-test/README.md
@@ -0,0 +1,6 @@
+# multi-project-test
+- project 1 in ./project.inlang (test root)
+- project 2 in ./project2-dir/project.inlang (test non-root)
+- project 3 in ./project3-dir/project.inlang.json (test auto-migrate to directory)
+
+
---
+++ inlang/source-code/sdk/multi-project-test/multi-project.test.ts
@@ -0,0 +1,71 @@
+/* eslint-disable no-console */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { describe, it, expect, vi } from "vitest"
+import { promisify } from "node:util"
+import { fileURLToPath } from "node:url"
+import childProcess from "node:child_process"
+import { dirname, join } from "node:path"
+import fs from "node:fs/promises"
+
+const exec = promisify(childProcess.exec)
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+it("pwd", async () => {
+	const { stdout } = await exec("pwd", { cwd: __dirname })
+	console.log(stdout)
+	expect(stdout).toBe(`${__dirname}\n`)
+})
+
+describe.concurrent("translate multiple projects in different directories", () => {
+	it(
+		"project1 in root",
+		async () => {
+			await exec("pnpm  translate1", { cwd: __dirname })
+			const result = await fs.readFile(join(__dirname, "locales", "de.json"), "utf8")
+			expect(result).toEqual(`{
+	"$schema": "https://inlang.com/schema/inlang-message-format",
+	"project1_message_key_1": "Mock translate local en to de: Generated message (1)",
+	"project1_message_key_2": "Mock translate local en to de: Generated message (2)",
+	"project1_message_key_3": "Mock translate local en to de: Generated message (3)"
+}`)
+		},
+		{ timeout: 10000 }
+	)
+
+	it(
+		"project2 in project2-dir",
+		async () => {
+			await exec("pnpm  translate2", { cwd: __dirname })
+			const result = await fs.readFile(
+				join(__dirname, "project2-dir", "locales", "de.json"),
+				"utf8"
+			)
+			expect(result).toEqual(`{
+	"$schema": "https://inlang.com/schema/inlang-message-format",
+	"project2_message_key_1": "Mock translate local en to de: Generated message (1)",
+	"project2_message_key_2": "Mock translate local en to de: Generated message (2)",
+	"project2_message_key_3": "Mock translate local en to de: Generated message (3)"
+}`)
+		},
+		{ timeout: 10000 }
+	)
+
+	it(
+		"project3 in project3-dir",
+		async () => {
+			await exec("pnpm  translate3", { cwd: __dirname })
+			const result = await fs.readFile(
+				join(__dirname, "project3-dir", "locales", "de.json"),
+				"utf8"
+			)
+			expect(result).toEqual(`{
+	"$schema": "https://inlang.com/schema/inlang-message-format",
+	"project3_message_key_1": "Mock translate local en to de: Generated message (1)",
+	"project3_message_key_2": "Mock translate local en to de: Generated message (2)",
+	"project3_message_key_3": "Mock translate local en to de: Generated message (3)"
+}`)
+		},
+		{ timeout: 10000 }
+	)
+})
