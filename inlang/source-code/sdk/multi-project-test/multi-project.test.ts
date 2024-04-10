6f4e55abe44fe9cb5a2d086d76b03aa831509f69
jldec <jurgen.leschner@opral.com>
2024-04-10 18:36:00 +0100

    cli should not wrap console, spawn with stdio inherit

--- inlang/source-code/sdk/multi-project-test/multi-project.test.ts
+++ inlang/source-code/sdk/multi-project-test/multi-project.test.ts
@@ -1,6 +1,6 @@
 /* eslint-disable no-console */
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { describe, it, expect, vi } from "vitest"
+import { describe, it, expect } from "vitest"
 import { promisify } from "node:util"
 import { fileURLToPath } from "node:url"
 import childProcess from "node:child_process"
@@ -15,29 +15,29 @@ function sleep(ms: number) {
 	return new Promise((resolve) => setTimeout(resolve, ms))
 }
 
-describe.concurrent("sanity check exec behavior", () => {
+describe.concurrent("sanity check run behavior", () => {
 	it("pwd", async () => {
-		const { stdout } = await exec("pwd", { cwd: __dirname })
-		console.log(stdout)
-		expect(stdout).toBe(`${__dirname}\n`)
+		const code = await run("pwd")
+		expect(code === 0).toBeTruthy()
 	})
 
 	it("ls ./no_such_directory_here/ has non-zeru exit code", async () => {
 		try {
-			await exec("ls ./no_such_directory_here/", { cwd: __dirname })
+			await run("ls ./no_such_directory_here/")
 			throw new Error("should not reach this")
 		} catch (e) {
 			// @ts-ignore
-			expect(e.code !== 0)
+			console.error(e)
+			expect(e.code !== 0).toBeTruthy()
 		}
 	})
 })
 
 describe.concurrent("translate multiple projects in different directories", () => {
-	it(
+	it.skip(
 		"project1 in root",
 		async () => {
-			await exec("pnpm translate1", { cwd: __dirname })
+			await run("pnpm translate1")
 			const result = await fs.readFile(join(__dirname, "locales", "de.json"), "utf8")
 			expect(result).toEqual(`{
 	"$schema": "https://inlang.com/schema/inlang-message-format",
@@ -49,10 +49,10 @@ describe.concurrent("translate multiple projects in different directories", () =
 		{ timeout: 10000 }
 	)
 
-	it(
+	it.skip(
 		"project2 in project2-dir",
 		async () => {
-			await exec("pnpm translate2", { cwd: __dirname })
+			await run("pnpm translate2")
 			const result = await fs.readFile(
 				join(__dirname, "project2-dir", "locales", "de.json"),
 				"utf8"
@@ -70,23 +70,34 @@ describe.concurrent("translate multiple projects in different directories", () =
 	it(
 		"project3 in project3-dir",
 		async () => {
-			try {
-				await exec("pnpm translate3", { cwd: __dirname })
-				const result = await fs.readFile(
-					join(__dirname, "project3-dir", "locales", "de.json"),
-					"utf8"
-				)
-				expect(result).toEqual(`{
+			await run("pnpm translate3")
+			const result = await fs.readFile(
+				join(__dirname, "project3-dir", "locales", "de.json"),
+				"utf8"
+			)
+			expect(result).toEqual(`{
 	"$schema": "https://inlang.com/schema/inlang-message-format",
 	"project3_message_key_1": "Mock translate local en to de: Generated message (1)",
 	"project3_message_key_2": "Mock translate local en to de: Generated message (2)",
 	"project3_message_key_3": "Mock translate local en to de: Generated message (3)"
 }`)
-			} catch (e) {
-				console.error(e)
-				throw e
-			}
 		},
-		{ timeout: 10000 }
+		{ timeout: 30000 }
 	)
 })
+
+// run command in __dirname
+// returns 0 or rejects promise with error
+// inherits stdio so that vitest shows output
+async function run(command: string): Promise<number> {
+	return new Promise((resolve, reject) => {
+		const p = childProcess.spawn(command, {
+			cwd: __dirname,
+			stdio: "inherit",
+			shell: true,
+			detached: false,
+		})
+		p.on("close", () => resolve(0))
+		p.on("error", (err) => reject(err))
+	})
+}
