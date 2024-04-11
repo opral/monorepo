6a636fb31d1b858ab8e3358e4dd981af369a4a86
LorisSigrist <loris@sigrist.dev>
2024-04-11 16:30:50 +0200

    Use path validation from core

--- packages/paraglide/paraglide-js-adapter-sveltekit/src/runtime/adapter.ts
+++ packages/paraglide/paraglide-js-adapter-sveltekit/src/runtime/adapter.ts
@@ -12,7 +12,7 @@ import { normaliseBase as canonicalNormaliseBase } from "./utils/normaliseBase.j
 import { createExclude, type ExcludeConfig } from "./exclude.js"
 import { guessTextDirMap } from "./utils/text-dir.js"
 import { resolvePathTranslations } from "./config/resolvePathTranslations.js"
-import { validatePathTranslations } from "./config/validatePathTranslations.js"
+import { validatePathTranslations } from "@inlang/paraglide-js/internal/adapter-utils"
 import type { ParamMatcher } from "@sveltejs/kit"
 import type { PathTranslations, UserPathTranslations } from "./config/pathTranslations.js"
 import type { Paraglide } from "./runtime.js"
