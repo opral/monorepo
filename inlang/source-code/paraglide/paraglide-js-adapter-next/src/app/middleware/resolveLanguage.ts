96a666cd2b441a5d6878dbb88609d97c3c8cade5
LorisSigrist <loris@sigrist.dev>
2024-04-11 15:37:28 +0200

    Pass lint

--- inlang/source-code/paraglide/paraglide-js-adapter-next/src/app/middleware/resolveLanguage.ts
+++ inlang/source-code/paraglide/paraglide-js-adapter-next/src/app/middleware/resolveLanguage.ts
@@ -3,10 +3,7 @@ import { RoutingStragey } from "../routing/interface"
 import { ResolvedI18nConfig } from "../config"
 import { HeaderNames, LANG_COOKIE } from "../constants"
 import { isAvailableLanguageTag } from "$paraglide/runtime.js"
-import {
-	negotiateLanguagePreferences,
-	detectLanguageFromPath,
-} from "@inlang/paraglide-js/internal/adapter-utils"
+import { negotiateLanguagePreferences } from "@inlang/paraglide-js/internal/adapter-utils"
 
 /**
  * Returns the language that should be used for this request
