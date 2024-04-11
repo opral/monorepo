c79c9de1e1ff5c0c66b92485312eca1f1ad1d1fd
LorisSigrist <loris@sigrist.dev>
2024-04-11 10:16:13 +0200

    Use adapter-utils langauge negotiator in Next Adapter

--- inlang/source-code/paraglide/paraglide-js/src/adapter-utils/negotiate-language.test.ts
+++ inlang/source-code/paraglide/paraglide-js/src/adapter-utils/negotiate-language.test.ts
@@ -12,12 +12,30 @@ describe("preferredLanguages", () => {
 
 	it("should sort the matches by quality", () => {
 		const headerValue = "fr-CH, fr;q=0.6, en;q=0.8, de;q=0.7, *;q=0.5"
-		expect(negotiateLanguagePreferences(headerValue, ["fr-CH", "fr", "en", "de", "*"])).toEqual([
+		expect(negotiateLanguagePreferences(headerValue, ["fr-CH", "fr", "en", "de"])).toEqual([
 			"fr-CH",
 			"en",
 			"de",
 			"fr",
-			"*",
 		])
 	})
+
+	it("should return the available languages if the accept header is missing", () => {
+		expect(negotiateLanguagePreferences(undefined, ["en-US", "en-GB", "de"])).toEqual([
+			"en-US",
+			"en-GB",
+			"de",
+		])
+	})
+
+	it("should use the index of the language as a tie-breaker if the quality is the same", () => {
+		const headerValue = "fr-CH, fr;q=0.8, en;q=0.8, de;q=0.7, *;q=0.5"
+		expect(negotiateLanguagePreferences(headerValue, ["fr-CH", "fr", "en", "de"])).toEqual([
+			"fr-CH",
+			"fr",
+			"en",
+			"de",
+		])
+	})
+
 })
