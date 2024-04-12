c29f0fb7ecdd8b18bae7463bf9704ff6186c8824
LorisSigrist <loris@sigrist.dev>
2024-04-12 12:00:12 +0200

    Fix build

--- packages/paraglide/paraglide-js-adapter-next/src/app/getLanguage.server.ts
+++ packages/paraglide/paraglide-js-adapter-next/src/app/getLanguage.server.ts
@@ -1,6 +1,6 @@
 import { headers } from "next/headers"
 import { sourceLanguageTag, isAvailableLanguageTag } from "$paraglide/runtime.js"
-import { HeaderNames } from "./constants"
+import { PARAGLIDE_LANGUAGE_HEADER_NAME } from "./constants"
 
 /**
  * Returns the current language tag.
@@ -9,7 +9,7 @@ import { HeaderNames } from "./constants"
  * TODO: Replace with languageTag() from paraglide/runtime.js - Needs reliable way to call setLanguageTag() from middleware.tsx
  */
 export function getLanguage<T extends string>(): T {
-	const langHeader = headers().get(HeaderNames.ParaglideLanguage)
+	const langHeader = headers().get(PARAGLIDE_LANGUAGE_HEADER_NAME)
 	const lang = isAvailableLanguageTag(langHeader) ? langHeader : sourceLanguageTag
 	return lang as T
 }
