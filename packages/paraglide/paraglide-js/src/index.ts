72cef52d3e86393830e50cf3d423931c7728b91a
LorisSigrist <loris@sigrist.dev>
2024-04-12 09:53:26 +0200

    Better resolveUserPathDefinitions

--- packages/paraglide/paraglide-js/src/index.ts
+++ packages/paraglide/paraglide-js/src/index.ts
@@ -4,8 +4,8 @@ export { writeOutput } from "./services/file-handling/write-output.js"
 export { Logger, type LoggerOptions } from "./services/logger/index.js"
 
 export type MessageIndexFunction<T extends string> = (
-	params?: Record<string, never>,
-	options?: { languageTag: T }
+	params: Record<string, never>,
+	options: { languageTag: T }
 ) => string
 
 export type MessageFunction = (params?: Record<string, never>) => string
