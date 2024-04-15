bf64f81a16d293678bc8e555096e15c8ffb16b64
LorisSigrist <loris@sigrist.dev>
2024-04-15 15:22:12 +0200

    Support relative links

--- inlang/source-code/paraglide/paraglide-js-adapter-next/src/app/localiseHref.ts
+++ inlang/source-code/paraglide/paraglide-js-adapter-next/src/app/localiseHref.ts
@@ -1,39 +1,37 @@
 import type { RoutingStragey } from "./routing/interface"
 import type { LinkProps } from "next/link"
+import { isExternal as isStringHrefExternal } from "./utils/href"
 
-const getPathname = (href: string, currentHref: string): string =>
-	new URL(href, currentHref).pathname
+const getPathname = (href: string, currentPath: string): string => {
+	const base = new URL(currentPath, "http://n")
+	const resolved = new URL(href, base)
+	return resolved.pathname
+}
 
 export function createLocaliseHref<T extends string>(
 	strategy: RoutingStragey<T>
 ): <P extends LinkProps["href"]>(
 	canonicalHref: P,
 	lang: T,
-	currentHref: string,
+	currentPath: string,
 	isLanugageSwitch: boolean
 ) => P {
 	return <P extends LinkProps["href"]>(
 		canonicalHref: P,
 		lang: T,
-		currentHref: string,
+		currentPath: string,
 		isLanugageSwitch: boolean
 	): P => {
 		//don't translate external links
 		if (isExternal(canonicalHref)) return canonicalHref
-
-		// don't touch relative links
-		if (typeof canonicalHref === "string" && !canonicalHref.startsWith("/")) return canonicalHref
 
 		//guard against empty pathnames on object hrefs
 		if (typeof canonicalHref === "object" && !canonicalHref.pathname) return canonicalHref
 
 		const canonicalPathname: string =
 			typeof canonicalHref === "object"
-				? canonicalHref.pathname ?? ""
-				: getPathname(canonicalHref, currentHref)
-
-		//dont' touch relative links
-		if (!canonicalPathname.startsWith("/")) return canonicalHref
+				? getPathname(canonicalHref.pathname ?? "", currentPath)
+				: getPathname(canonicalHref, currentPath)
 
 		const translatedUrl = strategy.getLocalisedUrl(canonicalPathname, lang, isLanugageSwitch)
 
@@ -47,21 +45,8 @@ export function createLocaliseHref<T extends string>(
  * Returns true if the href explicitly includes the origin, even if it's the current origin
  */
 export function isExternal(href: LinkProps["href"]) {
-	//Make sure none of the telltales for external links are set
-	if (typeof href === "object") {
-		return Boolean(href.protocol || href.auth || href.port || href.hostname || href.host)
-	}
-
-	//Make sure the href isn't a full url
-	const [, ...rest] = href.split(":")
-	if (rest.length === 0) return false
-
-	const schemeRegex = /^[a-z][a-z0-9+\-.]*:/i
-	return (
-		// href must not start with a url scheme
-		// see: https://datatracker.ietf.org/doc/html/rfc3986#section-3.1
-		schemeRegex.test(href) ||
-		//If the href starts with // it's a protocol relative url -> must include the host -> external
-		href.startsWith("//")
-	)
+	return typeof href === "object"
+		? //Make sure none of the telltales for external links are set
+		  Boolean(href.protocol || href.auth || href.port || href.hostname || href.host)
+		: isStringHrefExternal(href)
 }
--- inlang/source-code/paraglide/paraglide-js-adapter-next/src/app/navigation.ts
+++ inlang/source-code/paraglide/paraglide-js-adapter-next/src/app/navigation.ts
@@ -63,7 +63,13 @@ export const createNavigation = <T extends string>(
 			options?: (NavigateOptions & OptionalLanguageOption) | undefined
 		) => {
 			const locale = options?.locale ?? languageTag()
-			const localisedPath = localiseHref(canonicalPath, locale, locale !== languageTag())
+			const currentPathname = usePathname()
+			const localisedPath = localiseHref(
+				canonicalPath,
+				locale,
+				currentPathname,
+				locale !== languageTag()
+			)
 
 			// If the current and new canonical paths are the same, but the language is different,
 			// we need to do a native reload to make sure the new language is used
@@ -111,7 +117,13 @@ export const createNavigation = <T extends string>(
 			options?: (NavigateOptions & OptionalLanguageOption) | undefined
 		) => {
 			const locale = options?.locale ?? languageTag()
-			const localisedPath = localiseHref(canonicalPath, locale, locale !== languageTag())
+			const currentPathname = usePathname()
+			const localisedPath = localiseHref(
+				canonicalPath,
+				locale,
+				currentPathname,
+				locale !== languageTag()
+			)
 
 			// If the current and new canonical paths are the same, but the language is different,
 			// we need to do a native reload to make sure the new language is used
@@ -156,7 +168,13 @@ export const createNavigation = <T extends string>(
 		 */
 		const prefetch = (canonicalPath: string, options: PrefetchOptions & OptionalLanguageOption) => {
 			const locale = options?.locale ?? languageTag()
-			const localisedPath = localiseHref(canonicalPath, locale, locale !== languageTag())
+			const currentPathname = usePathname()
+			const localisedPath = localiseHref(
+				canonicalPath,
+				locale,
+				currentPathname,
+				locale !== languageTag()
+			)
 			return nextRouter.prefetch(localisedPath, options)
 		}
 
@@ -201,7 +219,13 @@ export function createRedirects<T extends string>(
 	 *  @param url the url to redirect to
 	 */
 	const redirect: NextRedirect = (...args) => {
-		args[0] = localiseHref(args[0], languageTag(), false)
+		const href = args[0]
+
+		if (process.env.NODE_ENV === "development" && !href.startsWith("/")) {
+			throw new Error("The href passed to redirect cannot be relative")
+		}
+
+		args[0] = localiseHref(href, languageTag(), "/", false)
 		NextNavigation.redirect(...args)
 	}
 
@@ -214,7 +238,12 @@ export function createRedirects<T extends string>(
 	 * @param url the url to redirect to
 	 */
 	const permanentRedirect: NextPermanentRedirect = (...args) => {
-		args[0] = localiseHref(args[0], languageTag(), false)
+		const href = args[0]
+		if (process.env.NODE_ENV === "development" && !href.startsWith("/")) {
+			throw new Error("The href passed to permanentRedirect cannot be relative")
+		}
+
+		args[0] = localiseHref(href, languageTag(), "/", false)
 		NextNavigation.permanentRedirect(...args)
 	}
 
