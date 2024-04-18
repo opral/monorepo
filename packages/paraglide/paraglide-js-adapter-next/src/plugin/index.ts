3c1ffbed3ecb1538892b7e0a52dfb61ff584c8bc
LorisSigrist <loris@sigrist.dev>
2024-04-18 09:38:43 +0200

    Make paraglide compiler silenceable

--- packages/paraglide/paraglide-js-adapter-next/src/plugin/index.ts
+++ packages/paraglide/paraglide-js-adapter-next/src/plugin/index.ts
@@ -4,13 +4,36 @@ import { once } from "./utils"
 import { useCompiler } from "./useCompiler"
 
 type ParaglideConfig = {
+	/**
+	 * Where the Inlang Project that defines the languages
+	 * and messages is located.
+	 *
+	 * This should be a relative path starting from the project root.
+	 *
+	 * @example "./project.inlang"
+	 */
 	project: string
+
+	/**
+	 * Where the paraglide output files should be placed. This is usually
+	 * inside a `src/paraglide` folder.
+	 *
+	 * This should be a relative path starting from the project root.
+	 *
+	 * @example "./src/paraglide"
+	 */
 	outdir: string
+
+	/**
+	 * If true, the paraglide compiler will only log errors to the console
+	 *
+	 * @default false
+	 */
+	silent?: boolean
 }
 
 type Config = NextConfig & {
 	paraglide: ParaglideConfig
-	paths?: Record<string, Record<string, string>>
 }
 
 /**
@@ -32,6 +55,7 @@ export function paraglide(config: Config): NextConfig {
 			project: config.paraglide.project,
 			outdir: config.paraglide.outdir,
 			watch: process.env.NODE_ENV === "development",
+			silent: config.paraglide.silent ?? false,
 		})
 	})
 
