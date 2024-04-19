c2eecd6419f396d2d915062240ab733e3e69b20e
LorisSigrist <loris@sigrist.dev>
2024-04-19 16:20:48 +0200

    FInd & replace next/link and next/navigation imports

--- packages/paraglide/paraglide-next/src/cli/index.ts
+++ packages/paraglide/paraglide-next/src/cli/index.ts
@@ -1,4 +1,8 @@
 import { Command } from "commander"
-import { InitCommand } from "./init-command"
+import { InitCommand } from "./commands/init.js"
+import { i18nCommand } from "./commands/i18n.js"
 
-export const cli = new Command().version(PARAGLIDE_NEXT_VERSION).addCommand(InitCommand)
+export const cli = new Command()
+	.version(PARAGLIDE_NEXT_VERSION)
+	.addCommand(InitCommand)
+	.addCommand(i18nCommand)
