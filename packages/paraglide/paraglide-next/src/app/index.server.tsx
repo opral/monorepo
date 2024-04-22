8e81faac5364daa8976a0186d8c5aae367833fef
LorisSigrist <loris@sigrist.dev>
2024-04-22 14:06:43 +0200

    Export createMiddleware and createNavigation

--- packages/paraglide/paraglide-next/src/app/index.server.tsx
+++ packages/paraglide/paraglide-next/src/app/index.server.tsx
@@ -1,2 +1,5 @@
 export { default as LanguageProvider } from "./providers/LanguageProvider.js"
 export { createI18n } from "./createI18n.server.js"
+
+export { createMiddleware } from "./middleware"
+export { createNavigation } from "./navigation"
