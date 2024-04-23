ab9db663bab64847b6f3680874baeec3dd0c66dc
LorisSigrist <loris@sigrist.dev>
2024-04-23 09:50:41 +0200

    Add `initializeLanguage` function

--- inlang/source-code/paraglide/paraglide-next/src/app/index.client.tsx
+++ inlang/source-code/paraglide/paraglide-next/src/app/index.client.tsx
@@ -1,1 +1,2 @@
 export { createI18n } from "./createI18n.client.js"
+export { initializeLanguage } from "./initializeLanguage.client.js"
--- inlang/source-code/paraglide/paraglide-next/src/app/index.server.tsx
+++ inlang/source-code/paraglide/paraglide-next/src/app/index.server.tsx
@@ -1,2 +1,3 @@
 export { default as LanguageProvider } from "./providers/LanguageProvider.js"
 export { createI18n } from "./createI18n.server.js"
+export { initializeLanguage } from "./initializeLanguage.server.js"
