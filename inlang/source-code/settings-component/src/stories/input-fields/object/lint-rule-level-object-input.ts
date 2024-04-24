b8a1114a9451a171e3869deb613d7f6f6a69322b
Nils Jacobsen <nils.jacobsen98@gmail.com>
2024-04-24 10:55:01 +0200

    feat: add error icon to lint levels

--- inlang/source-code/settings-component/src/stories/input-fields/object/lint-rule-level-object-input.ts
+++ inlang/source-code/settings-component/src/stories/input-fields/object/lint-rule-level-object-input.ts
@@ -30,7 +30,7 @@ export class LintRuleLevelObjectInput extends LitElement {
 				flex-wrap: wrap;
 			}
 			.select {
-				max-width: 120px;
+				max-width: 140px;
 				min-width: 100px;
 			}
 			.title-container {
@@ -45,6 +45,14 @@ export class LintRuleLevelObjectInput extends LitElement {
 			}
 			sl-select::part(base):hover {
 				border: var(--sl-input-placeholder-color);
+			}
+			.level-icon {
+				color: var(--sl-color-neutral-400);
+				margin-top: 1px;
+				margin-right: 6px;
+			}
+			.level-icon.danger {
+				color: var(--sl-color-danger-600);
 			}
 		`,
 	]
@@ -157,6 +165,31 @@ export class LintRuleLevelObjectInput extends LitElement {
 												)
 											}}
 										>
+											${(this.value as any)[module.id] === "error"
+												? html`<svg
+														class="level-icon danger"
+														slot="prefix"
+														width="20"
+														height="20"
+														viewBox="0 0 24 24"
+												  >
+														<path
+															fill="currentColor"
+															d="M12 17q.425 0 .713-.288T13 16t-.288-.712T12 15t-.712.288T11 16t.288.713T12 17m0-4q.425 0 .713-.288T13 12V8q0-.425-.288-.712T12 7t-.712.288T11 8v4q0 .425.288.713T12 13m0 9q-2.075 0-3.9-.788t-3.175-2.137T2.788 15.9T2 12t.788-3.9t2.137-3.175T8.1 2.788T12 2t3.9.788t3.175 2.137T21.213 8.1T22 12t-.788 3.9t-2.137 3.175t-3.175 2.138T12 22m0-2q3.35 0 5.675-2.325T20 12t-2.325-5.675T12 4T6.325 6.325T4 12t2.325 5.675T12 20m0-8"
+														/>
+												  </svg>`
+												: html`<svg
+														class="level-icon"
+														slot="prefix"
+														width="20"
+														height="20"
+														viewBox="0 0 24 24"
+												  >
+														<path
+															fill="currentColor"
+															d="M12 17q.425 0 .713-.288T13 16t-.288-.712T12 15t-.712.288T11 16t.288.713T12 17m0-4q.425 0 .713-.288T13 12V8q0-.425-.288-.712T12 7t-.712.288T11 8v4q0 .425.288.713T12 13m0 9q-2.075 0-3.9-.788t-3.175-2.137T2.788 15.9T2 12t.788-3.9t2.137-3.175T8.1 2.788T12 2t3.9.788t3.175 2.137T21.213 8.1T22 12t-.788 3.9t-2.137 3.175t-3.175 2.138T12 22m0-2q3.35 0 5.675-2.325T20 12t-2.325-5.675T12 4T6.325 6.325T4 12t2.325 5.675T12 20m0-8"
+														/>
+												  </svg>`}
 											${this._valueOptions?.map((option) => {
 												return html`<sl-option
 													exportparts="base:option"
