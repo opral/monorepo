0367f1841cb6a70fd1c3ea63d31591d16a6d7aa3
Nils Jacobsen <nils.jacobsen98@gmail.com>
2024-04-26 09:53:59 +0200

    feat: add doc-layout component package

---
+++ packages/doc-layout-component/src/stories/inlang-doc-layout.stories.ts
@@ -0,0 +1,18 @@
+import "./inlang-doc-layout.ts"
+import type { Meta, StoryObj } from "@storybook/web-components"
+import { html } from "lit"
+
+const meta: Meta = {
+	component: "inlang-doc-layout",
+	title: "Public/inlang-doc-layout",
+}
+
+export default meta
+
+export const Props: StoryObj = {
+	render: () => html`<inlang-doc-layout></inlang-doc-layout> `,
+}
+
+export const Attributes: StoryObj = {
+	render: () => html` <inlang-doc-layout></inlang-doc-layout> `,
+}
