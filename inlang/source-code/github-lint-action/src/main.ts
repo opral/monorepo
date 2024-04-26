d2f2a354c53f2443be756a8f1c726f21868f6dc5
NiklasBuchfink <niklas.buchfink@web.de>
2024-04-26 20:11:56 +0200

    chore: add space

--- inlang/source-code/github-lint-action/src/main.ts
+++ inlang/source-code/github-lint-action/src/main.ts
@@ -329,7 +329,7 @@ ${lintSummary
 				error.message.includes("'target'")
 			) {
 				core.setFailed(
-					"Your workflow file needs to be updated. Please copy the new snippet of the ninja_i18n.yml file from the README(https://github.com/opral/ninja-i18n-action)."
+					"Your workflow file needs to be updated. Please copy the new snippet of the ninja_i18n.yml file from the README (https://github.com/opral/ninja-i18n-action)."
 				)
 				return
 			}
