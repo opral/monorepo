343715f445be19c3b2e6620f953056fce84a5d07
LorisSigrist <loris@sigrist.dev>
2024-05-03 15:16:02 +0200

    Add routing Strategies

--- packages/paraglide/paraglide-next/src/app/index.client.tsx
+++ packages/paraglide/paraglide-next/src/app/index.client.tsx
@@ -4,8 +4,7 @@ export { createNavigation } from "./navigation/navigation.client.js"
 export { initializeLanguage } from "./initializeLanguage.client.js"
 
 // Routing Strategies
-export { PrefixStrategy } from "./routing-strategy/prefixStrategy"
-export { DomainStrategy } from "./routing-strategy/domainStrategy"
+export * from "./routing-strategy/strategies"
 
 // Legacy
 export { createI18n } from "./legacy/createI18n.client.js"
