0ee31a523387b972bacbdd24dda330ddd31de625
LorisSigrist <loris@sigrist.dev>
2024-05-03 14:43:41 +0200

    Update `init` CLI

--- inlang/source-code/paraglide/paraglide-next/examples/app/src/lib/i18n.ts
+++ inlang/source-code/paraglide/paraglide-next/examples/app/src/lib/i18n.ts
@@ -1,13 +1,8 @@
-import {
-	createI18n,
-	createMiddleware,
-	PrefixStrategy,
-	createNavigation,
-} from "@inlang/paraglide-next"
+import { createMiddleware, PrefixStrategy, createNavigation } from "@inlang/paraglide-next"
 import type { AvailableLanguageTag } from "@/paraglide/runtime"
 import * as m from "@/paraglide/messages"
 
-const strategy = PrefixStrategy<AvailableLanguageTag>({
+export const routingStrategy = PrefixStrategy<AvailableLanguageTag>({
 	pathnames: {
 		"/about": m.about_path,
 		"/admin/[...rest]": {
@@ -16,23 +11,11 @@ const strategy = PrefixStrategy<AvailableLanguageTag>({
 			"de-CH": "/administrator/[...rest]",
 		},
 	},
-	prefix: "except-default",
+	prefixDefault: "never",
 	exclude: () => false,
 })
 
-export const middleware = createMiddleware({ strategy })
+export const middleware = createMiddleware({ strategy: routingStrategy })
 export const { Link, useRouter, usePathname, redirect, permanentRedirect } = createNavigation({
-	strategy,
-})
-
-export const { localizePath } = createI18n<AvailableLanguageTag>({
-	pathnames: {
-		"/about": m.about_path,
-		"/admin/[...rest]": {
-			en: "/admin/[...rest]",
-			de: "/administrator/[...rest]",
-			"de-CH": "/administrator/[...rest]",
-		},
-	},
-	exclude: ["/not-translated"], //makes sure that the /not-translated page is not translated
+	strategy: routingStrategy,
 })
