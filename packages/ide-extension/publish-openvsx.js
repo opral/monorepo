9c43b08bfbdbb7e80bac944698ddc8caafa3e169
Felix Haeberle <kontakt@felixhaeberle.de>
2024-05-07 17:53:06 +0200

    chore: Add continue-on-error flag to VS Code extension publishing workflow

--- packages/ide-extension/publish-openvsx.js
+++ packages/ide-extension/publish-openvsx.js
@@ -2,7 +2,7 @@
 
 import { execSync } from "node:child_process"
 import path from "node:path"
-import packageJson from "../package.json"
+import packageJson from "./package.json"
 
 // Ensure environment variables are checked properly
 if (!process.env.OPEN_VSX_TOKEN) {
