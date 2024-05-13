2c1103d2ee615a825d3362dd19e8d087bb9dd1c8
Felix Haeberle <kontakt@felixhaeberle.de>
2024-05-13 14:03:03 +0200

    refactor: Update openInFink command to include branch in Fink URL

--- packages/ide-extension/src/commands/openInFink.ts
+++ packages/ide-extension/src/commands/openInFink.ts
@@ -3,19 +3,20 @@ import { telemetry } from "../services/telemetry/implementation.js"
 import type { Message } from "@inlang/sdk"
 import { CONFIGURATION } from "../configuration.js"
 import { getGitOrigin } from "../utilities/settings/getGitOrigin.js"
+import { getCurrentBranch } from "../utilities/settings/getCurrentBranch.js"
 
 export const openInFinkCommand = {
 	command: "sherlock.openInFink",
-	title: "Sherlock: Open in Editor",
+	title: "Sherlock: Open in Fink",
 	register: commands.registerCommand,
 	callback: async function (args: { messageId: Message["id"]; selectedProjectPath: string }) {
-		// TODO: Probably the origin should be configurable via the config.
 		const origin = (await getGitOrigin())?.replaceAll(".git", "")
+		const branch = (await getCurrentBranch()) || "main"
 		const uri = args.messageId
 			? `${CONFIGURATION.STRINGS.FINK_BASE_URL}${origin}?project=${encodeURIComponent(
 					args.selectedProjectPath
-			  )}&id=${encodeURIComponent(args.messageId)}`
-			: `${CONFIGURATION.STRINGS.FINK_BASE_URL}${origin}`
+			  )}&branch=${encodeURIComponent(branch)}&id=${encodeURIComponent(args.messageId)}`
+			: `${CONFIGURATION.STRINGS.FINK_BASE_URL}${origin}?branch=${encodeURIComponent(branch)}`
 
 		env.openExternal(Uri.parse(uri))
 
