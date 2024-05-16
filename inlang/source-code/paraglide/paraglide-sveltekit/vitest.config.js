196c0ea6d5d98241e9b1fc4e9233546accda6228
LorisSigrist <loris@sigrist.dev>
2024-05-16 10:45:23 +0200

    Fix tests

--- inlang/source-code/paraglide/paraglide-sveltekit/vitest.config.js
+++ inlang/source-code/paraglide/paraglide-sveltekit/vitest.config.js
@@ -5,15 +5,15 @@ import { defineConfig } from "vitest/config"
 const __filename = resolve(fileURLToPath(import.meta.url))
 const __dirname = dirname(__filename)
 
-// eslint-disable-next-line no-undef
 const BASE_PATH = process.env.BASE_PATH ?? ""
 
-export default defineConfig(() => {
+export default defineConfig((env) => {
 	return {
 		plugins: [
 			virtualModules({
 				"$app/paths": getPathsModule({ base: BASE_PATH }),
 				"$app/environment": getEnvironmentModule({ dev: false, browser: false }),
+				"$app/stores": getStoresModule({}),
 			}),
 		],
 		test: {
@@ -64,5 +64,13 @@ function getEnvironmentModule(options) {
 	for (const [key, value] of Object.entries(options)) {
 		code += `export const ${key} = ${JSON.stringify(value)}\n`
 	}
+	return code
+}
+
+/**
+ * @param {{}} options
+ */
+function getStoresModule(options) {
+	let code = ``
 	return code
 }
