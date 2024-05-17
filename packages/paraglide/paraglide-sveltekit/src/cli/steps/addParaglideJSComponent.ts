218e61add2de3a76512afcc546d74a3cf199fc44
LorisSigrist <loris@sigrist.dev>
2024-05-17 15:45:36 +0200

    Start updating the code if needed

--- packages/paraglide/paraglide-sveltekit/src/cli/steps/addParaglideJSComponent.ts
+++ packages/paraglide/paraglide-sveltekit/src/cli/steps/addParaglideJSComponent.ts
@@ -40,6 +40,33 @@ export const addParaglideJSComponent: CliStep<
 	}
 }
 
+// assumption: The <script> element is at the top (if present) and the <style> at the bottom (if present)
 function updateLayoutFile(content: string): string {
-	// assumption: The <script> element is at the top (if present) and the <style> at the bottom (if present)
+	const scriptStart = content.indexOf("<script")
+	const scriptEnd = content.indexOf("</script>")
+
+	if (scriptStart !== -1 && scriptEnd !== -1) {
+		// if there is a script present, add the imports to it
+	} else {
+		// if there isn't a script present, add one with the imports
+		content =
+			`<script>
+    import { ParaglideJS } from "@inlang/paraglide-sveltekit"
+    import { i18n } from "$lib/i18n"
+</script>
+
+<ParaglideJS {i18n}>\n` + content
+	}
+
+	const styleStart = content.indexOf("<style")
+	if (styleStart !== -1 && styleStart > scriptStart) {
+		content = content + "\n</ParaglideJS>"
+	} else {
+		const before = content.slice(0, Math.max(0, scriptStart))
+		const after = content.slice(Math.max(0, scriptEnd))
+
+		content = before + "\n</ParaglideJS>\n" + after
+	}
+
+	return content
 }
