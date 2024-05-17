d7e6d694aa64b5d1a3a5c262ce9bb88afe2a5e62
jldec <jurgen.leschner@opral.com>
2024-05-16 22:44:06 -0400

    remove parsePattern - createMessage with no placeholders only

--- inlang/source-code/sdk/src/v2/createMessageBundle.ts
+++ inlang/source-code/sdk/src/v2/createMessageBundle.ts
@@ -1,20 +1,12 @@
-import {
-	LanguageTag,
-	Declaration,
-	MessageBundle,
-	Message,
-	Pattern,
-	Text,
-	Expression,
-} from "./types.js"
+import { LanguageTag, MessageBundle, Message, Text } from "./types.js"
 
 /**
  * create v2 MessageBundle
  * @example createMessageBundle({
  * 	 id: "greeting",
  *   messages: [
- * 		 createMessage({locale: "en", pattern: "Hello {name}!"})
- * 		 createMessage({locale: "de", pattern: "Hallo {name}!"})
+ * 		 createMessage({locale: "en", text: "Hello world!"})
+ * 		 createMessage({locale: "de", text: "Hallo Welt!"})
  *   ]
  * })
  */
@@ -31,34 +23,16 @@ export function createMessageBundle(args: {
 }
 
 /**
- * create v2 Messsage AST with either text or pattern
- * @example createMessage({locale: "en", pattern: "Hello {name}!"})
+ * create v2 Messsage AST with text-only pattern
+ * @example createMessage({locale: "en", text: "Hello world"})
  */
-export function createMessage(args: {
-	locale: LanguageTag
-	text?: string
-	pattern?: string
-	variableReferencePattern?: string[]
-}): Message {
-	const patternAst: Pattern = args.pattern
-		? parsePattern(args.pattern, ["{", "}"])
-		: [toTextElement(args.text ?? "")]
+export function createMessage(args: { locale: LanguageTag; text: string }): Message {
 	return {
 		locale: args.locale,
-		declarations: inferInputDeclarations(patternAst),
+		declarations: [],
 		selectors: [],
-		variants: [{ match: [], pattern: patternAst }],
+		variants: [{ match: [], pattern: [toTextElement(args.text ?? "")] }],
 	}
-}
-
-function inferInputDeclarations(pattern: Pattern) {
-	const inputMap: Record<string, boolean> = {}
-	for (const element of pattern) {
-		if (element.type === "expression" && element.arg.type === "variable") {
-			inputMap[element.arg.name] = true
-		}
-	}
-	return Object.keys(inputMap).map(toInputDeclaration)
 }
 
 function toTextElement(text: string): Text {
@@ -66,61 +40,4 @@ function toTextElement(text: string): Text {
 		type: "text",
 		value: text,
 	}
-}
-
-function toInputDeclaration(name: string): Declaration {
-	return {
-		type: "input",
-		name,
-		value: toVariableExpression(name),
-	}
-}
-
-function toVariableExpression(name: string): Expression {
-	return {
-		type: "expression",
-		arg: {
-			type: "variable",
-			name,
-		},
-	}
-}
-
-/**
- * Parses a pattern.
- * based on inlang/source-code/plugins/i18next/src/plugin.ts
- * returns v2 Pattern with variableExpression or Text elements
- *
- * @example parsePattern("Hello {{name}}!", ["{{", "}}"])
- */
-function parsePattern(text: string, variableReferencePattern: string[]): Pattern {
-	// dependent on the variableReferencePattern, different regex
-	// expressions are used for matching
-
-	const expression = variableReferencePattern[1]
-		? new RegExp(
-				`(\\${variableReferencePattern[0]}[^\\${variableReferencePattern[1]}]+\\${variableReferencePattern[1]})`,
-				"g"
-		  )
-		: new RegExp(`(${variableReferencePattern}\\w+)`, "g")
-
-	const pattern: Pattern = text
-		.split(expression)
-		.filter((element) => element !== "")
-		.map((element) => {
-			if (expression.test(element) && variableReferencePattern[0]) {
-				return toVariableExpression(
-					variableReferencePattern[1]
-						? element.slice(
-								variableReferencePattern[0].length,
-								// negative index, removing the trailing pattern
-								-variableReferencePattern[1].length
-						  )
-						: element.slice(variableReferencePattern[0].length)
-				)
-			} else {
-				return toTextElement(element)
-			}
-		})
-	return pattern
 }
