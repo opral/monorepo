47783f6f3cd4ec9b9a11e3ab7d14f44104176157
LorisSigrist <loris@sigrist.dev>
2024-05-21 10:43:13 +0200

    Properly update existing layout files

---
+++ packages/paraglide/paraglide-sveltekit/src/cli/steps/addParaglideJSComponent.test.ts
@@ -0,0 +1,58 @@
+import { describe, expect, it } from "vitest"
+import { updateLayoutFile } from "./addParaglideJSComponent"
+
+describe("updateLayoutFile", () => {
+	it("should update an empty layout file", () => {
+		expect(updateLayoutFile("")).toMatchInlineSnapshot(`
+			"<script>
+				import { ParaglideJS } from '@inlang/paraglide-sveltekit'
+				import { i18n } from '$lib/i18n'
+			</script>
+			<ParaglideJS {i18n}>
+
+
+			<slot/>
+			</ParaglideJS>"
+		`)
+	})
+
+	it("should update a layout file with just a slot", () => {
+		expect(updateLayoutFile("<slot/>")).toMatchInlineSnapshot(`
+			"<script>
+				import { ParaglideJS } from '@inlang/paraglide-sveltekit'
+				import { i18n } from '$lib/i18n'
+			</script>
+			<ParaglideJS {i18n}>
+
+			<slot/>
+			</ParaglideJS>"
+		`)
+	})
+
+	it("should update a layout file with a script", () => {
+		expect(updateLayoutFile(`<script>const a = 5;</script>`)).toMatchInlineSnapshot(`
+			"<script>
+				import { ParaglideJS } from '@inlang/paraglide-sveltekit'
+				import { i18n } from '$lib/i18n'
+			const a = 5;</script>
+			<ParaglideJS {i18n}>
+
+			<slot/>
+			</ParaglideJS>"
+		`)
+	})
+
+	it("should update a layout file with just a style", () => {
+		expect(updateLayoutFile(`<style>a { color: inherit; }</style>`)).toMatchInlineSnapshot(`
+			"<script>
+				import { ParaglideJS } from '@inlang/paraglide-sveltekit'
+				import { i18n } from '$lib/i18n'
+			</script>
+			<ParaglideJS {i18n}>
+
+
+			<slot/>
+			</ParaglideJS><style>a { color: inherit; }</style>"
+		`)
+	})
+})
