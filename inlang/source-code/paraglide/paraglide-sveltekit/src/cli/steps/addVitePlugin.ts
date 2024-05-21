3beb02702b1146e14b027bf8a7f4902b861a0148
LorisSigrist <loris@sigrist.dev>
2024-05-21 11:17:20 +0200

    Properly update vite config

--- inlang/source-code/paraglide/paraglide-sveltekit/src/cli/steps/addVitePlugin.ts
+++ inlang/source-code/paraglide/paraglide-sveltekit/src/cli/steps/addVitePlugin.ts
@@ -15,35 +15,21 @@ export const addParaglideSvelteKitVitePlugin: CliStep<
 			encoding: "utf-8",
 		})
 
-		if (fileContent.includes("@inlang/paraglide-sveltekit/vite")) {
-			console.warn("@inlang/paraglide-sveltekit/vite vite-plugin already added - Skipping step")
+		const result = maybeUpdateViteConfig(fileContent)
+		if (result.ok) {
+			await ctx.repo.nodeishFs.writeFile(ctx.viteConfigPath, result.updated)
+			ctx.logger.success("Added @inlang/paraglide-sveltekit/vite vite-plugin to vite config")
+		} else {
+			const msg = [
+				"Failed to add the `@inlang/paraglide-sveltekit/vite` vite-plugin in vite.config.js.",
+				"Reason: " + result.reason,
+				"",
+				"Please add the plugin manually.",
+			].join("\n")
+
+			ctx.logger.warn(msg)
 			return ctx
 		}
-
-		const PLUGINS_REGEX = /plugins\s*:\s*\[/g
-		const match = PLUGINS_REGEX.exec(fileContent)
-
-		if (!match) {
-			ctx.logger.warn(
-				"Failed to add the @inlang/paraglide-sveltekit/vite vite-plugin. You will need to do it manually"
-			)
-			return ctx
-		}
-
-		const endIndex = match.index + match.length
-
-		const before = fileContent.slice(0, match.index)
-		const after = fileContent.slice(endIndex)
-
-		const newFileContent =
-			"import { paraglide } from '@inlang/paraglide-sveltekit/vite'\n" +
-			before +
-			`paraglide({ project: './project.inlang', outdir: './src/lib/paraglide' }),` +
-			after
-
-		await ctx.repo.nodeishFs.writeFile(ctx.viteConfigPath, newFileContent)
-
-		ctx.logger.success("Added @inlang/paraglide-sveltekit/vite vite-plugin to vite config")
 	} catch (e) {
 		ctx.logger.error(
 			`Failed to add @inlang/paraglide-sveltekit/vite vite-plugin to ${ctx.viteConfigPath}.`
@@ -52,4 +38,55 @@ export const addParaglideSvelteKitVitePlugin: CliStep<
 	}
 
 	return ctx
+}
+
+type UpdateResult =
+	| {
+			ok: true
+			updated: string
+			reason: undefined
+	  }
+	| {
+			ok: false
+			reason: string
+			updated: undefined
+	  }
+
+/**
+ * @private Only exported for testings
+ */
+export function maybeUpdateViteConfig(config: string): UpdateResult {
+	if (config.includes("@inlang/paraglide-sveltekit/vite")) {
+		return {
+			ok: false,
+			reason: "Already present",
+		}
+	}
+
+	const PLUGINS_REGEX = /plugins\s*:\s*\[/g
+	const match = PLUGINS_REGEX.exec(config)
+
+	if (!match) {
+		return {
+			ok: false,
+			updated: undefined,
+			reason: "Could not find the plugins array",
+		}
+	}
+
+	const endIndex = match.index + match[0].length
+	const before = config.slice(0, endIndex)
+	const after = config.slice(endIndex)
+
+	const updatedConfig =
+		"import { paraglide } from '@inlang/paraglide-sveltekit/vite'\n" +
+		before +
+		`paraglide({ project: './project.inlang', outdir: './src/lib/paraglide' }),` +
+		after
+
+	return {
+		ok: true,
+		updated: updatedConfig,
+		reason: undefined,
+	}
 }
