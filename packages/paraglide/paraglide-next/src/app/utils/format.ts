d042ae7f18c6a718490104e8621a87ef62ef5a0f
LorisSigrist <loris@sigrist.dev>
2024-05-23 15:54:08 +0200

    Minifiy format file

--- packages/paraglide/paraglide-next/src/app/utils/format.ts
+++ packages/paraglide/paraglide-next/src/app/utils/format.ts
@@ -2,23 +2,10 @@ import type { UrlObject } from "node:url"
 import querystring from "qs"
 
 // polyfill for node:url's format as it is not available in Next-production mode
-
-const slashedProtocols = ["http:", "https:", "ftp:", "gopher:", "file:"] as const
+const slashedProtocols: readonly string[] = ["http:", "https:", "ftp:", "gopher:", "file:"]
 
 export function format(url: UrlObject) {
-	const auth = url.auth ? encodeURIComponent(url.auth).replace(/%3A/i, ":") + "@" : ""
-
-	let pathname = url.pathname || ""
-	let host: string | false = false
-
-	if (url.host) {
-		host = auth + url.host
-	} else if (url.hostname) {
-		host = auth + (!url.hostname.includes(":") ? url.hostname : "[" + url.hostname + "]")
-		if (url.port) {
-			host += ":" + url.port
-		}
-	}
+	const auth = url.auth ? `${encodeURIComponent(url.auth).replace(/%3A/i, ":")}@` : ""
 
 	let query = ""
 	if (url.query && typeof url.query === "object" && Object.keys(url.query).length) {
@@ -28,39 +15,48 @@ export function format(url: UrlObject) {
 		})
 	}
 
-	let protocol = url.protocol || ""
-	if (protocol && protocol.slice(-1) !== ":") {
-		protocol += ":"
-	}
+	const protocol = url.protocol
+		? ((url.protocol.endsWith(":") ? url.protocol : url.protocol + ":") as `${string}:`)
+		: ""
 
-	const isSlashedProtocol =
-		slashedProtocols.includes(protocol as (typeof slashedProtocols)[number]) ||
-		slashedProtocols.includes((protocol + ":") as (typeof slashedProtocols)[number])
+	const isSlashedProtocol = slashedProtocols.includes(protocol)
+
+	let host: string | false = false
+	let pathname = url.pathname || ""
+
+	if (url.host) {
+		host = auth + url.host
+	} else if (url.hostname) {
+		host =
+			auth +
+			(!url.hostname.includes(":") ? url.hostname : `[${url.hostname}]`) +
+			(url.host ? `:${url.port}` : "")
+	}
 
 	/*
 	 * only the slashedProtocols get the //.  Not mailto:, xmpp:, etc.
 	 * unless they had them to begin with.
 	 */
 	if (url.slashes || ((!protocol || isSlashedProtocol) && host !== false)) {
-		host = "//" + (host || "")
-		if (pathname && pathname.charAt(0) !== "/") {
-			pathname = "/" + pathname
-		}
-	} else if (!host) {
-		host = ""
-	}
-	pathname = pathname.replace(/[?#]/g, encodeURIComponent)
-
-	let hash: string = url.hash || ""
-	if (hash && !hash.startsWith("#")) {
-		hash = "#" + hash
+		host = `//${host}`
+		if (pathname && !pathname.startsWith("/")) pathname = `/${pathname}`
 	}
 
-	let search = url.search || query ? `?${query}` : ""
-	if (search && !search.startsWith("?")) {
-		search = "?" + search
-	}
-	search = search.replace("#", "%23")
+	const resolvedHost = host || ""
 
-	return protocol + host + pathname + search + hash
+	const escapedPathname = pathname.replace(/[?#]/g, encodeURIComponent)
+	const hash = url.hash ? (url.hash.startsWith("#") ? url.hash : `#${url.hash}`) : ""
+	const search =
+		// if there is a search, use it
+		url.search
+			? url.search.startsWith("?")
+				? url.search
+				: `?${url.search}`
+			: // if there is instead a query string, use it
+			query
+			? `?${query}`
+			: // otherwise fall back to nothing
+			  ""
+	const escapedSearch = search.replace("#", "%23")
+	return protocol + resolvedHost + escapedPathname + escapedSearch + hash
 }
