9634ea99236efec0926d811f38b1fb5ae4bb88af
LorisSigrist <loris@sigrist.dev>
2024-05-28 10:54:52 +0200

    minify

--- inlang/source-code/paraglide/paraglide-next/src/app/utils/href.ts
+++ inlang/source-code/paraglide/paraglide-next/src/app/utils/href.ts
@@ -1,12 +1,9 @@
 /**
  * Check if a string-href points to an external page
  */
-export function isExternal(href: string): boolean {
-	return (
-		// If the href starts with a url scheme
-		// see: https://datatracker.ietf.org/doc/html/rfc3986#section-3.1
-		/^[a-z][a-z0-9+\-.]*:/i.test(href) ||
-		//If the href starts with // it's a protocol relative url -> must include the host -> external
-		/^\/\//.test(href)
-	)
-}
+export const isExternal = (href: string): boolean =>
+	// If the href starts with a url scheme
+	// see: https://datatracker.ietf.org/doc/html/rfc3986#section-3.1
+	/^[a-z][a-z0-9+\-.]*:/i.test(href) ||
+	//If the href starts with // it's a protocol relative url -> must include the host -> external
+	/^\/\//.test(href)
