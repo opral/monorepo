65de249b14830f8e548f12a592e68a40f7145da6
LorisSigrist <loris@sigrist.dev>
2024-05-28 11:18:05 +0200

    Use locale Cache

---
+++ inlang/source-code/paraglide/paraglide-next/src/app/cache/localeCache.ts
@@ -0,0 +1,30 @@
+// rsc only
+
+import { isAvailableLanguageTag, sourceLanguageTag } from "$paraglide/runtime.js"
+import { cache } from "react"
+import { DEV } from "../env"
+
+/**
+ * A request-scoped cache with the current langauge tag (if set)
+ */
+export const localeCache = cache<() => { current: string | undefined }>(() => ({
+	current: undefined,
+}))
+
+/**
+ * Call this function on before using `languageTag()`
+ */
+export function initializeLocaleCache(languageTag: string) {
+	if (!isAvailableLanguageTag(languageTag)) {
+		if (DEV) console.warn("Invalid language tag:", languageTag)
+	} else {
+		localeCache().current = languageTag
+	}
+}
+
+export function getLanguageFromCache() {
+	const cachedLanguageTag = localeCache().current
+	if (isAvailableLanguageTag(cachedLanguageTag)) return cachedLanguageTag
+	if (DEV) console.warn("Falling back to source language tag")
+	return sourceLanguageTag
+}
