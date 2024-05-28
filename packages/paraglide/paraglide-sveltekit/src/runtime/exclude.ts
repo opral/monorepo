b353e9b8e13e863364eb5625f1dcdac08b3db46c
LorisSigrist <loris@sigrist.dev>
2024-05-28 15:23:41 +0200

    minify

--- packages/paraglide/paraglide-sveltekit/src/runtime/exclude.ts
+++ packages/paraglide/paraglide-sveltekit/src/runtime/exclude.ts
@@ -3,15 +3,9 @@ import { normalize } from "./utils/path.js"
 export type ExcludeConfig = (string | RegExp)[]
 
 export function createExclude(excludeConfig: ExcludeConfig): (path: string) => boolean {
-	const checks: ((path: string) => boolean)[] = []
-
-	for (const exclude of excludeConfig) {
-		if (typeof exclude === "string") {
-			checks.push((path) => path === exclude)
-		} else {
-			checks.push((path) => exclude.test(path))
-		}
-	}
+	const checks: ((path: string) => boolean)[] = excludeConfig.map((exclude) =>
+		typeof exclude === "string" ? (path) => path === exclude : (path) => exclude.test(path)
+	)
 
 	return (path) => {
 		path = normalize(path)
