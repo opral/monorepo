cc42f130ffade0d21cca8344a4b4ffce629a7644
LorisSigrist <loris@sigrist.dev>
2024-05-30 15:50:56 +0200

    Make sure to only catch `header()` related errors

--- packages/paraglide/paraglide-next/src/app/getLanguage.server.ts
+++ packages/paraglide/paraglide-next/src/app/getLanguage.server.ts
@@ -15,15 +15,22 @@ export function getLanguage<T extends string>(): T {
 		const lang = isAvailableLanguageTag(langHeader) ? langHeader : sourceLanguageTag
 		return lang as T
 	} catch (e) {
-		const msg = [
-			"Tried to access the languageTag when no request was available.",
-			"A common cause for this is using messages in a top-level variable.",
-			"If you need to use a message in a top-level variable, make the variable a getter function instead.",
-			"",
-			"See https://github.com/opral/inlang-paraglide-js/issues/132 for more information.",
-		].join("\n")
-		throw Error(msg, {
-			cause: e,
-		})
+		if (
+			e instanceof Error &&
+			e.message.includes("Invariant: headers() expects to have requestAsyncStorage, none available")
+		) {
+			const msg = [
+				"Tried to access the languageTag when no request was available.",
+				"A common cause for this is using messages in a top-level variable.",
+				"If you need to use a message in a top-level variable, make the variable a getter function instead.",
+				"",
+				"See https://github.com/opral/inlang-paraglide-js/issues/132 for more information.",
+			].join("\n")
+			throw Error(msg, {
+				cause: e,
+			})
+		}
+
+		throw e
 	}
 }
