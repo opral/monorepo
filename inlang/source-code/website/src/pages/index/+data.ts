ddfca85516a5cf8a9f7fe9577dc2bcf9b88bc4dc
Nils Jacobsen <nils.jacobsen98@gmail.com>
2024-05-31 15:24:16 +0200

    feat: improve posthog query caching

--- inlang/source-code/website/src/pages/index/+data.ts
+++ inlang/source-code/website/src/pages/index/+data.ts
@@ -3,104 +3,124 @@ import { privateEnv } from "@inlang/env-variables"
 export { data }
 export type Data = Awaited<ReturnType<typeof data>>
 
+let cachedProjectCount: string | undefined = undefined
+let cacheLastSet: number | undefined = undefined
+
 async function data() {
-	try {
-		// prettier-ignore
-		const raw = JSON.stringify({
-			"refresh": true,
-			"query": {
-				"kind": "InsightVizNode",
-				"source": {
-				"kind": "TrendsQuery",
-				"properties": {
-					"type": "AND",
-					"values": [
-					{
+	if (
+		cachedProjectCount &&
+		cacheLastSet &&
+		Date.now() - cacheLastSet < 24 * 60 * 60 * 1000 &&
+		cachedProjectCount !== "10000+"
+	) {
+		return {
+			projectCount: cachedProjectCount,
+		}
+	} else {
+		let projectCount: string | undefined = undefined
+		try {
+			// prettier-ignore
+			const raw = JSON.stringify({
+				"refresh": true,
+				"query": {
+					"kind": "InsightVizNode",
+					"source": {
+					"kind": "TrendsQuery",
+					"properties": {
 						"type": "AND",
-						"values": []
-					}
-					]
-				},
-				"filterTestAccounts": true,
-				"dateRange": {
-					"date_to": null,
-					"date_from": "-180d"
-				},
-				"series": [
-					{
-					"kind": "EventsNode",
-					"event": "IDE-EXTENSION activated",
-					"name": "IDE-EXTENSION activated",
-					"custom_name": "Entire ecosystem",
-					"math": "dau"
-					},
-					{
-					"kind": "EventsNode",
-					"event": "EDITOR clicked in field",
-					"name": "EDITOR clicked in field",
-					"math": "dau"
-					},
-					{
-					"kind": "EventsNode",
-					"event": "SDK loaded project",
-					"name": "SDK loaded project",
-					"properties": [
+						"values": [
 						{
-						"key": "appId",
-						"type": "event",
-						"value": [
-							"library.inlang.paraglideJs"
+							"type": "AND",
+							"values": []
+						}
+						]
+					},
+					"filterTestAccounts": true,
+					"dateRange": {
+						"date_to": null,
+						"date_from": "-180d"
+					},
+					"series": [
+						{
+						"kind": "EventsNode",
+						"event": "IDE-EXTENSION activated",
+						"name": "IDE-EXTENSION activated",
+						"custom_name": "Entire ecosystem",
+						"math": "dau"
+						},
+						{
+						"kind": "EventsNode",
+						"event": "EDITOR clicked in field",
+						"name": "EDITOR clicked in field",
+						"math": "dau"
+						},
+						{
+						"kind": "EventsNode",
+						"event": "SDK loaded project",
+						"name": "SDK loaded project",
+						"properties": [
+							{
+							"key": "appId",
+							"type": "event",
+							"value": [
+								"library.inlang.paraglideJs"
+							],
+							"operator": "exact"
+							}
 						],
-						"operator": "exact"
+						"math": "unique_group",
+						"math_group_type_index": 1
 						}
 					],
-					"math": "unique_group",
-					"math_group_type_index": 1
+					"interval": "month",
+					"breakdownFilter": {
+						"breakdown_group_type_index": 1
+					},
+					"trendsFilter": {
+						"showLegend": false,
+						"formula": "A + B + C",
+						"display": "ActionsLineGraphCumulative",
+						"showValuesOnSeries": true
 					}
-				],
-				"interval": "month",
-				"breakdownFilter": {
-					"breakdown_group_type_index": 1
-				},
-				"trendsFilter": {
-					"showLegend": false,
-					"formula": "A + B + C",
-					"display": "ActionsLineGraphCumulative",
-					"showValuesOnSeries": true
+					},
+					"full": true
 				}
-				},
-				"full": true
-			}
-		})
+			})
 
-		const response = await fetch(
-			`https://eu.posthog.com/api/projects/${privateEnv.PUBLIC_POSTHOG_PROJECT_ID}/query`,
-			{
-				method: "POST",
-				headers: {
-					Authorization: "Bearer " + privateEnv.POSTHOG_API_KEY,
-					"Content-Type": "application/json",
-				},
-				body: raw,
-				redirect: "follow",
-				cache: "no-cache",
-			}
-		)
-		const json = await response.json()
-		const number = json.results[0].data.at(-1)
+			const response = await fetch(
+				`https://eu.posthog.com/api/projects/${privateEnv.PUBLIC_POSTHOG_PROJECT_ID}/query`,
+				{
+					method: "POST",
+					headers: {
+						Authorization: "Bearer " + privateEnv.POSTHOG_API_KEY,
+						"Content-Type": "application/json",
+					},
+					body: raw,
+					redirect: "follow",
+					cache: "no-cache",
+				}
+			)
 
-		if (number) {
+			const json = await response.json()
+			projectCount = json.results[0].data.at(-1)
+		} catch (error) {
+			if (cachedProjectCount) {
+				projectCount = cachedProjectCount
+			} else {
+				projectCount = "10000+"
+			}
+		}
+
+		if (projectCount) {
+			cachedProjectCount = projectCount
+			cacheLastSet = Date.now()
 			return {
-				projectCount: number,
+				projectCount,
 			}
 		} else {
 			return {
 				projectCount: "10000+",
 			}
-		}
-	} catch (error) {
-		return {
-			projectCount: "10000+",
 		}
 	}
 }
