380583aa0fbfb4cefa40eb59d5326a5ea4fe24a9
LorisSigrist <loris@sigrist.dev>
2024-06-03 11:11:13 +0200

    getLanguageFromRequest function

--- inlang/source-code/paraglide/paraglide-next/src/app/getLanguage.server.ts
+++ inlang/source-code/paraglide/paraglide-next/src/app/getLanguage.server.ts
@@ -1,33 +1,9 @@
-import { headers } from "next/headers"
-import { sourceLanguageTag, isAvailableLanguageTag } from "$paraglide/runtime.js"
-import { PARAGLIDE_LANGUAGE_HEADER_NAME } from "./constants"
 import { cache } from "react"
+import { getLanugageFromRequest } from "./middleware/getLanguageFromRequest"
 
-const cached = cache<() => { languageTag: string }>(() => {
-	try {
-		const langHeader = headers().get(PARAGLIDE_LANGUAGE_HEADER_NAME)
-		const lang = isAvailableLanguageTag(langHeader) ? langHeader : sourceLanguageTag
-		return { languageTag: lang }
-	} catch (e) {
-		if (
-			e instanceof Error &&
-			e.message.includes("Invariant: headers() expects to have requestAsyncStorage, none available")
-		) {
-			const msg = [
-				"Tried to access the languageTag when no request was available.",
-				"A common cause for this is using messages in a top-level variable.",
-				"If you need to use a message in a top-level variable, make the variable a getter function instead.",
-				"",
-				"See https://github.com/opral/inlang-paraglide-js/issues/132 for more information.",
-			].join("\n")
-			throw Error(msg, {
-				cause: e,
-			})
-		}
-
-		throw e
-	}
-})
+const cached = cache<() => { languageTag: string }>(() => ({
+	languageTag: getLanugageFromRequest(),
+}))
 
 /**
  * Returns the current language tag.
