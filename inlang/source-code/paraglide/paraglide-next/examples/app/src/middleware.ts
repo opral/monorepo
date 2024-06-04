b6e7906c7c41e84506b5b3ad7f96b39950874f6d
LorisSigrist <loris@sigrist.dev>
2024-06-04 10:07:42 +0200

    Update middleware

--- inlang/source-code/paraglide/paraglide-next/examples/app/src/middleware.ts
+++ inlang/source-code/paraglide/paraglide-next/examples/app/src/middleware.ts
@@ -1,1 +1,7 @@
-export { middleware } from "@/lib/i18n"
+import { middleware as i18nMiddleware } from "@/lib/i18n"
+
+export async function middleware(request: any) {
+	const lang = i18nMiddleware.detectLanguage(request)
+	const response = i18nMiddleware.getResponse(request, lang)
+	return response
+}
