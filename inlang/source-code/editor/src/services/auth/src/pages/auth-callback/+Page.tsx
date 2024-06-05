de33ce0a4ef16b74062b35699b3171b8f54835db
NiklasBuchfink <niklas.buchfink@web.de>
2024-06-05 18:15:56 +0200

    update auth-callback pages

--- inlang/source-code/editor/src/services/auth/src/pages/auth-callback/+Page.tsx
+++ inlang/source-code/editor/src/services/auth/src/pages/auth-callback/+Page.tsx
@@ -47,18 +47,6 @@ export default function Page() {
 							<MaterialSymbolsCheckCircleRounded class="text-success w-16 h-16 self-center" />
 							<h2 class="text-xl font-medium self-center pt-2">Successfully logged in</h2>
 							<p class="self-center">You can close this window.</p>
-							<sl-button
-								class="pt-6"
-								prop:variant="primary"
-								onClick={() => {
-									// this pattern will break if the last opened window is not the login window
-									window.close()
-								}}
-							>
-								{/* @ts-ignore */}
-								<MaterialSymbolsArrowBackRounded slot="prefix" />
-								Return to app
-							</sl-button>
 						</div>
 					</div>
 				</Match>
