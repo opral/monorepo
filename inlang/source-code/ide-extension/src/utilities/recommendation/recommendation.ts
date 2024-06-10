3bbcb209641b28176e7b5103b696c6554b94d3c1
Felix Haeberle <kontakt@felixhaeberle.de>
2024-06-10 16:23:13 +0200

    chore: Update recommendation.ts to use Sherlock for workspace recommendation

--- inlang/source-code/ide-extension/src/utilities/recommendation/recommendation.ts
+++ inlang/source-code/ide-extension/src/utilities/recommendation/recommendation.ts
@@ -1,40 +1,9 @@
 import * as vscode from "vscode"
-import type { CommentJSONValue } from "comment-json/index.js"
-import { parse, stringify } from "comment-json"
-import * as path from "node:path"
-import * as fs from "node:fs"
 import { telemetry } from "../../services/telemetry/implementation.js"
 import { getGitOrigin } from "../settings/getGitOrigin.js"
 import { getSetting, updateSetting } from "../settings/index.js"
-
-/**
- * Defines the structure of the extensions.json file.
- */
-type ExtensionsJson = {
-	recommendations: string[]
-}
-
-export async function isInWorkspaceRecommendation(args: {
-	workspaceFolder: vscode.WorkspaceFolder
-}) {
-	const vscodeFolderPath = path.join(args.workspaceFolder.uri.fsPath, ".vscode")
-	const extensionsJsonPath = path.join(vscodeFolderPath, "extensions.json")
-
-	let extensions: CommentJSONValue | undefined
-	// Read the extensions.json file
-	if (fs.existsSync(extensionsJsonPath) && fs.existsSync(vscodeFolderPath)) {
-		extensions = parse(fs.readFileSync(extensionsJsonPath, "utf8"))
-	}
-	const extensionsResult =
-		// @ts-expect-error
-		extensions?.recommendations?.includes("inlang.vs-code-extension") || false
-
-	if (extensionsResult === true) {
-		return true
-	} else {
-		return false
-	}
-}
+import * as Sherlock from "@inlang/cross-sell-sherlock"
+import type { NodeishFilesystem } from "@lix-js/fs"
 
 export const isDisabledRecommendation = async (): Promise<boolean> => {
 	const disabledRecommendations = await getSetting("disableRecommendation")
@@ -49,7 +18,10 @@ export const updateDisabledRecommendation = async (): Promise<void> => {
 	])
 }
 
-export function createRecommendationBanner(args: { workspaceFolder: vscode.WorkspaceFolder }) {
+export function createRecommendationBanner(args: {
+	workspaceFolder: vscode.WorkspaceFolder
+	fs: NodeishFilesystem
+}) {
 	return {
 		resolveWebviewView(webviewView: vscode.WebviewView) {
 			webviewView.webview.options = {
@@ -57,34 +29,13 @@ export function createRecommendationBanner(args: { workspaceFolder: vscode.Works
 			}
 
 			webviewView.webview.onDidReceiveMessage(async (message) => {
-				const vscodeFolderPath = path.join(args.workspaceFolder.uri.fsPath, ".vscode")
-				const extensionsJsonPath = path.join(vscodeFolderPath, "extensions.json")
-
 				switch (message.command) {
 					case "addRecommendation":
 						if (args.workspaceFolder) {
-							// Check if the .vscode folder exists
-							if (!fs.existsSync(vscodeFolderPath)) {
-								fs.mkdirSync(vscodeFolderPath)
-							}
-
-							// Check if the extensions.json file exists
-							if (!fs.existsSync(extensionsJsonPath)) {
-								// Create a new extensions.json file with an empty recommendations array
-								fs.writeFileSync(
-									extensionsJsonPath,
-									JSON.stringify({ recommendations: [] }, undefined, 2)
-								)
-							}
-
-							// Add the Inlang extension to the recommendations object
-							const newExtensions: ExtensionsJson = parse(
-								fs.readFileSync(extensionsJsonPath, "utf8")
-							) as any
-							newExtensions.recommendations.push("inlang.vs-code-extension")
-
-							// Write the updated extensions.json file
-							fs.writeFileSync(extensionsJsonPath, stringify(newExtensions, undefined, 2))
+							Sherlock.add({
+								fs: args.fs,
+								workingDirectory: args.workspaceFolder.uri.fsPath,
+							})
 
 							// Hide the banner
 							vscode.commands.executeCommand(
@@ -210,10 +161,16 @@ export function getRecommendationBannerHtml(args: { webview: vscode.Webview }): 
         </html>`
 }
 
-export async function recommendationBannerView(args: { workspaceFolder: vscode.WorkspaceFolder }) {
+export async function recommendationBannerView(args: {
+	workspaceFolder: vscode.WorkspaceFolder
+	nodeishFs: NodeishFilesystem
+}) {
 	if (
 		(await isDisabledRecommendation()) ||
-		(await isInWorkspaceRecommendation({ workspaceFolder: args.workspaceFolder }))
+		(await Sherlock.isAdopted({
+			fs: args.nodeishFs,
+			workingDirectory: args.workspaceFolder.uri.fsPath,
+		}))
 	) {
 		return
 	} else {
@@ -223,6 +180,7 @@ export async function recommendationBannerView(args: { workspaceFolder: vscode.W
 	vscode.window.registerWebviewViewProvider(
 		"recommendationBanner",
 		createRecommendationBanner({
+			fs: args.nodeishFs,
 			workspaceFolder: args.workspaceFolder,
 		})
 	)
