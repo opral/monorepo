fb53eb19af3d7e67d87872eda9dac65e157370e7
NiklasBuchfink <niklas.buchfink@web.de>
2024-06-10 16:54:45 +0200

    chore: update wording for permission request from repo owner

--- inlang/source-code/editor/src/pages/@host/@owner/@repository/+Page.tsx
+++ inlang/source-code/editor/src/pages/@host/@owner/@repository/+Page.tsx
@@ -3,6 +3,7 @@ import MaterialSymbolsUnknownDocumentOutlineRounded from "~icons/material-symbol
 import MaterialSymbolsArrowOutwardRounded from "~icons/material-symbols/arrow-outward-rounded"
 import IconClose from "~icons/material-symbols/close"
 import IconLightbulb from "~icons/material-symbols/lightbulb-outline"
+import IconGithub from "~icons/cib/github"
 import { EditorStateProvider, useEditorState } from "./State.jsx"
 import NoMatchPlaceholder from "./components/NoMatchPlaceholder.jsx"
 import { ListHeader } from "./components/Listheader.jsx"
@@ -327,17 +328,18 @@ function RepositoryDoesNotExistOrNotAuthorizedCard(args: {
 					{args.userIsLoggedIn ? (
 						<>
 							<li class="pt-2">
-								If this is a <span class="font-bold">private repository</span> you need need to add
-								it to the GitHub app permissions by clicking the button below
+								If this is a <span class="font-bold">private repository</span> you need to add it to
+								the GitHub app permissions by clicking the button below
 							</li>
 							<li class="pt-2">
-								If you are <span class="font-bold">not a owner</span> of the repository, ask the
-								owner to:
-								<ul class="list-disc pl-4">
-									<li>install the inlang GitHub app</li>
-									<li>add the repository to the app permissions</li>
-									<li>add you as a collaborator</li>
-								</ul>
+								If you are <span class="font-bold">not a owner</span> of the repository:
+								<ol class="list-decimal pl-5">
+									<li>ask the owner to add you as a collaborator</li>
+									<li>
+										request the owner to install the inlang GitHub app and add the repository to the
+										app permissions by clicking the button below
+									</li>
+								</ol>
 							</li>
 						</>
 					) : (
@@ -368,15 +370,9 @@ function RepositoryDoesNotExistOrNotAuthorizedCard(args: {
 							location.reload()
 						}}
 					>
-						Add GitHub app permissions{" "}
-						<div slot="suffix">
-							<svg viewBox="0 0 32 32" width="1.2em" height="1.2em">
-								<path
-									fill="currentColor"
-									d="M16 .396c-8.839 0-16 7.167-16 16c0 7.073 4.584 13.068 10.937 15.183c.803.151 1.093-.344 1.093-.772c0-.38-.009-1.385-.015-2.719c-4.453.964-5.391-2.151-5.391-2.151c-.729-1.844-1.781-2.339-1.781-2.339c-1.448-.989.115-.968.115-.968c1.604.109 2.448 1.645 2.448 1.645c1.427 2.448 3.744 1.74 4.661 1.328c.14-1.031.557-1.74 1.011-2.135c-3.552-.401-7.287-1.776-7.287-7.907c0-1.751.62-3.177 1.645-4.297c-.177-.401-.719-2.031.141-4.235c0 0 1.339-.427 4.4 1.641a15.436 15.436 0 0 1 4-.541c1.36.009 2.719.187 4 .541c3.043-2.068 4.381-1.641 4.381-1.641c.859 2.204.317 3.833.161 4.235c1.015 1.12 1.635 2.547 1.635 4.297c0 6.145-3.74 7.5-7.296 7.891c.556.479 1.077 1.464 1.077 2.959c0 2.14-.02 3.864-.02 4.385c0 .416.28.916 1.104.755c6.4-2.093 10.979-8.093 10.979-15.156c0-8.833-7.161-16-16-16z"
-								/>
-							</svg>
-						</div>
+						Add GitHub app permissions
+						{/* @ts-ignore */}
+						<IconGithub slot="suffix" class="-ml-1" />
 					</sl-button>
 				) : (
 					""
