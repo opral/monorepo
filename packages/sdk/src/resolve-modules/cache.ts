d1faf655e1ad04235768bf4941b737a2c8f8a4a6
LorisSigrist <loris@sigrist.dev>
2024-06-17 17:06:38 +0200

    Use lix's hash function instead

--- packages/sdk/src/resolve-modules/cache.ts
+++ packages/sdk/src/resolve-modules/cache.ts
@@ -1,38 +1,13 @@
 import type { NodeishFilesystemSubset } from "@inlang/plugin"
+import { hash } from "@lix-js/client"
 import { type Result, tryCatch } from "@inlang/result"
-
-/**
- * Hashes a module URI to only include characters that can be used in filenames.
- *
- * THIS FUNCTION ALWAYS FAILS IN TEST BECAUSE VITEST HAS A BUG
- * https://github.com/vitest-dev/vitest/issues/4043#issuecomment-1742028595
- *
- * @param moduleURI A JSDelivr URI
- */
-async function cacheModuleUri(moduleURI: string) {
-	if ("process" in globalThis && process?.env?.TEST === "true") {
-		return moduleURI.replaceAll(/[^a-zA-Z0-9]/g, "")
-	}
-	const buffer = toArrayBuffer(moduleURI)
-
-	const hash_bytes = await crypto.subtle.digest("SHA-1", buffer as ArrayBuffer)
-	return [...new Uint8Array(hash_bytes)].map((x) => x.toString(16).padStart(2, "0")).join("")
-}
-
-function toArrayBuffer(str: string) {
-	const bytes = new Uint8Array(str.length)
-	for (let i = 0, strLen = str.length; i < strLen; i++) {
-		bytes[i] = str.charCodeAt(i)
-	}
-	return bytes.buffer.slice(bytes.byteOffset, bytes.byteLength + bytes.byteOffset)
-}
 
 async function readModuleFromCache(
 	moduleURI: string,
 	projectPath: string,
 	readFile: NodeishFilesystemSubset["readFile"]
 ): Promise<Result<string, Error>> {
-	const moduleHash = await cacheModuleUri(moduleURI)
+	const moduleHash = await hash(moduleURI)
 	const filePath = projectPath + `/cache/${moduleHash}.js`
 
 	return await tryCatch(async () => await readFile(filePath, { encoding: "utf-8" }))
@@ -44,7 +19,7 @@ async function writeModuleToCache(
 	projectPath: string,
 	writeFile: NodeishFilesystemSubset["writeFile"]
 ): Promise<void> {
-	const moduleHash = await cacheModuleUri(moduleURI)
+	const moduleHash = await hash(moduleURI)
 	const filePath = projectPath + `/cache/${moduleHash}.js`
 	await writeFile(filePath, moduleContent)
 }
