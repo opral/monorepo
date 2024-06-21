7a6952c3d6f9b6aab1ec013383b5542df2905840
Felix Haeberle <kontakt@felixhaeberle.de>
2024-06-21 12:48:53 +0200

    feat: Add new files and configuration for @inlang/recommend-sherlock package

---
+++ inlang/source-code/recommendations/recommend-sherlock/src/recommendation/utils/exists.ts
@@ -0,0 +1,16 @@
+import type { NodeishFilesystem } from "@lix-js/fs"
+
+/**
+ * Returns true if the path exists (file or directory), false otherwise.
+ *
+ * @param nodeishFs
+ * @returns
+ */
+export async function pathExists(filePath: string, nodeishFs: NodeishFilesystem) {
+	try {
+		await nodeishFs.stat(filePath)
+		return true
+	} catch (error) {
+		return false
+	}
+}
---
+++ inlang/source-code/recommendations/recommend-sherlock/src/recommendation/utils/types.ts
@@ -0,0 +1,7 @@
+import { Type, type Static } from "@sinclair/typebox"
+
+export const ExtensionsJson = Type.Object({
+	recommendations: Type.Array(Type.String()),
+})
+
+export type ExtensionsJson = Static<typeof ExtensionsJson>
