b07566a55cc84d034c6ea5065d3409bf3d109e31
LorisSigrist <loris@sigrist.dev>
2024-06-24 10:35:18 +0200

    Add "prefixes" option

--- packages/paraglide/paraglide-next/src/app/routing-strategy/strats/prefixStrategy.ts
+++ packages/paraglide/paraglide-next/src/app/routing-strategy/strats/prefixStrategy.ts
@@ -8,7 +8,6 @@ import {
 	resolveUserPathDefinitions,
 } from "@inlang/paraglide-js/internal/adapter-utils"
 import type { RoutingStrategy } from "../interface"
-import { usePrefixDetection } from "../../middleware/detection/prefixDetection"
 import { DEV } from "../../env"
 import { rsc } from "rsc-env"
 import { availableLanguageTags, sourceLanguageTag } from "$paraglide/runtime.js"
@@ -44,16 +43,46 @@ export function PrefixStrategy<T extends string>({
 
 	/**
 	 * Custom language prefixes
+	 * Do not include slashes in the prefixes
+	 *
+	 * @example
+	 * ```ts
+	 * prefixes: {
+	 *   "de-CH": "ch",
+	 *   "en-US": "us",
+	 * }
+	 * ```
 	 */
-	prefixes?: Record<T, string>
+	prefixes?: {
+		[key in T]?: string
+	}
 } = {}): RoutingStrategy<T> {
 	const resolvedPathnames = /** @__PURE__ */ userPathnames
 		? resolveUserPathDefinitions(userPathnames, availableLanguageTags)
 		: {}
 
-	const resolvedPrefixes: Record<T, string> = {
-		...Object.fromEntries(availableLanguageTags.map((lang) => [lang, "/" + lang])),
+	const resolvedPrefixes = {
+		...(Object.fromEntries(availableLanguageTags.map((lang) => [lang, lang])) as Record<T, string>),
 		...prefixes,
+	}
+
+	if (DEV) {
+		const withSlashes = Object.values(resolvedPrefixes).filter(
+			(val) => typeof val === "string" && val.includes("/")
+		)
+		if (withSlashes.length) {
+			throw new Error(
+				["[parglide-sveltekit:dev-only] Prefixes on cannot include slashes.", ...withSlashes].join(
+					"\n"
+				)
+			)
+		}
+
+		const paths = Object.values(resolvedPathnames)
+		const hasDuplicates = new Set(paths).size !== paths.length
+		if (hasDuplicates) {
+			throw new Error("[parglide-sveltekit:dev-only] `prefixes` must be unique.")
+		}
 	}
 
 	prefixDefault ??= "never"
@@ -71,8 +100,11 @@ export function PrefixStrategy<T extends string>({
 	}
 
 	function getCanonicalPath(localisedPath: `/${string}`, locale: T): `/${string}` {
-		const pathWithoutLocale: `/${string}` = localisedPath.startsWith(`/${locale}`)
-			? ((localisedPath.replace(`/${locale}`, "") || "/") as `/${string}`)
+		// get the prefix for this language
+		const prefix = resolvedPrefixes[locale]
+
+		const pathWithoutLocale: `/${string}` = localisedPath.startsWith(`/${prefix}`)
+			? ((localisedPath.replace(`/${prefix}`, "") || "/") as `/${string}`)
 			: localisedPath
 
 		for (const [canonicalPathDefinition, translationsForPath] of Object.entries(
@@ -119,8 +151,10 @@ export function PrefixStrategy<T extends string>({
 			const translatedPath = getTranslatedPath(canonicalPath, targetLanguage, resolvedPathnames)
 			const shouldAddPrefix = targetLanguage !== sourceLanguageTag || prefixDefault === "always"
 
+			const prefix = resolvedPrefixes[targetLanguage]
+
 			const localisedPath: `/${string}` = shouldAddPrefix
-				? `/${targetLanguage}${translatedPath == "/" ? "" : translatedPath}`
+				? `/${prefix}${translatedPath == "/" ? "" : translatedPath}`
 				: translatedPath
 			return {
 				pathname: localisedPath,
@@ -129,8 +163,11 @@ export function PrefixStrategy<T extends string>({
 		getCanonicalPath,
 
 		resolveLocale(request) {
-			const detect = usePrefixDetection({ availableLanguageTags })
-			const detected = detect(request) as T | undefined
+			const pathWithoutBase = request.nextUrl.pathname
+
+			const entries = Object.entries(resolvedPrefixes) as [T, string][]
+			const entry = entries.find(([, prefix]) => pathWithoutBase.startsWith(`/${prefix}`))
+			const detected = entry?.[0]
 
 			// If no prefix is detected and prefixDefault is "never" -> use default language
 			// Otherwise leave it ambiguous
