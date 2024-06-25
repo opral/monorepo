aee5c59c6ff3db43b185dc404afaac010eab6f7b
Felix Haeberle <kontakt@felixhaeberle.de>
2024-06-25 15:48:38 +0200

    chore: Update Ninja recommendation logic to use recommendNinja function

--- packages/ide-extension/src/diagnostics/linterDiagnostics.ts
+++ packages/ide-extension/src/diagnostics/linterDiagnostics.ts
@@ -1,7 +1,7 @@
 import * as vscode from "vscode"
 import { state } from "../utilities/state.js"
 import type { MessageLintReport } from "@inlang/sdk"
-import { crossSellNinja } from "../utilities/cross-sell/ninja/ninja.js"
+import { recommendNinja } from "../utilities/cross-sell/ninja/ninja.js"
 import type { NodeishFilesystem } from "@lix-js/fs"
 
 export async function linterDiagnostics(args: {
@@ -89,7 +89,7 @@ export async function linterDiagnostics(args: {
 							)
 
 							if (diagnostics.length > 0) {
-								crossSellNinja({ fs: args.nodeishFs })
+								recommendNinja({ fs: args.nodeishFs })
 							}
 						}
 					)
