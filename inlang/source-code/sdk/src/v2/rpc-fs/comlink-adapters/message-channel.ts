48424a787c1faa60d58cee8c8e5569456165fdc8
LorisSigrist <loris@sigrist.dev>
2024-07-04 10:10:20 +0200

    stash

---
+++ inlang/source-code/sdk/src/v2/rpc-fs/comlink-adapters/message-channel.ts
@@ -0,0 +1,156 @@
+import * as Comlink from "comlink"
+
+type StructuredCloneEndpoint = Omit<Comlink.Endpoint, "postMessage"> & {
+	postMessage(message: any): void
+}
+
+type EventListenerOrEventListenerObject = EventListener | EventListenerObject
+
+const portMessageSymbol = Symbol("inlang.sdk.rpc-fs.port-message")
+const comlinkMessageSymbol = Symbol("inlang.sdk.rpc-fs.comlink-message")
+
+type PortMessage = {
+	[portMessageSymbol]: true
+	portId: string
+	value: any
+}
+const isPortMessage = (thing: unknown): thing is PortMessage =>
+	!!thing && typeof thing === "object" && portMessageSymbol in thing
+
+type ComlinkMessage = {
+	[comlinkMessageSymbol]: true
+	value: any
+}
+const isComlinkMessage = (thing: unknown): thing is ComlinkMessage =>
+	!!thing && typeof thing === "object" && comlinkMessageSymbol in thing
+
+type Message = PortMessage | ComlinkMessage
+
+/**
+ * Wraps a comlink endpoint that does not support `transferables` on `postMessage` &
+ * allows it to at least transfer MessagePorts
+ */
+export const StructuredCloneAdapter = (ep: Comlink.Endpoint): Comlink.Endpoint => {
+	const proxiedPorts = new Map<string, MessagePort>()
+
+	return {
+		postMessage(message, transfer) {
+			if (transfer) {
+				const ports = transfer.filter(isMessagePort)
+				const allArePorts = ports.length === transfer.length
+				if (!allArePorts)
+					throw new Error(
+						"A StructuredCloneAdapter can only `transfer` MessagePorts via postMessage"
+					)
+
+				// proxy the ports
+				for (const port of ports) {
+					const id = generateUUID()
+				}
+			}
+		},
+
+		addEventListener(type, listener) {
+			ep.addEventListener("message", (ev: MessageEvent) => {})
+		},
+
+		removeEventListener(type, listener) {
+			ep.removeEventListener(type, listener)
+		},
+		start: ep.start,
+	}
+}
+
+function isMessagePort(thing: unknown): thing is MessagePort {
+	return thing instanceof MessagePort
+}
+
+const wrap = (smc: StructuredCloneEndpoint, id: string | undefined = undefined): MessagePort => {
+	const { port1, port2 } = new MessageChannel()
+	hookup(port2, smc, id)
+	return port1
+}
+
+function hookup(
+	/**
+	 * The port that get's exposed publicly
+	 */
+	internalPort: MessagePort,
+
+	/**
+	 * The postmessage channel to
+	 */
+	sce: StructuredCloneEndpoint,
+
+	/**
+	 * A unique identifier for this MessageChannel
+	 */
+	id: string | undefined = undefined
+): void {
+	internalPort.onmessage = (event: MessageEvent<Message>) => {
+		if (!id) id = generateUID()
+		const msg = event.data
+		const messageChannels = [...findMessageChannels(event.data)]
+		for (const messageChannel of messageChannels) {
+			const id = generateUID()
+			const channel = replaceProperty(msg, messageChannel, id)
+			hookup(channel, sce, id)
+		}
+		sce.postMessage({ id, msg, messageChannels })
+	}
+
+	sce.addEventListener("message", (event) => {
+		if ("") const data = event.data as Message
+		if (!id) id = data.id
+		if (id !== data.id) return
+
+		const mcs = data.messageChannels.map((messageChannel) => {
+			const id = messageChannel.reduce((obj, key) => obj[key], data.msg)
+			const port = wrap(sce, id)
+			replaceProperty(data.msg, messageChannel, port)
+			return port
+		})
+
+		internalPort.postMessage(data.msg, mcs)
+	})
+}
+
+function replaceProperty(obj: any, path: string[], newVal: any): any {
+	for (const key of path.slice(0, -1)) obj = obj[key]
+	const key = path.at(-1)
+	const orig = obj[key]
+	obj[key] = newVal
+	return orig
+}
+
+function* findMessageChannels(obj: any, path: string[] = []): Iterable<string[]> {
+	if (!obj) return
+	if (typeof obj === "string") return
+	if (obj instanceof MessagePort) {
+		yield [...path]
+		return
+	}
+	for (const key of Object.keys(obj)) {
+		path.push(key)
+		yield* findMessageChannels(obj[key], path)
+		path.pop()
+	}
+}
+
+function hex4(): string {
+	return Math.floor((1 + Math.random()) * 0x10000)
+		.toString(16)
+		.slice(1)
+}
+
+const bits = 128
+function generateUID(): string {
+	return new Array(bits / 16).fill(0).map(hex4).join("")
+}
+
+function generateUUID(): string {
+	return new Array(4)
+		.fill(0)
+		.map(() => Math.floor(Math.random() * Number.MAX_SAFE_INTEGER).toString(16))
+		.join("-")
+}
