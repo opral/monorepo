648d5aa4fd48eec6091e04e16b1376901cb7949c
LorisSigrist <loris@sigrist.dev>
2024-07-08 10:01:39 +0200

    Remove unused tests

--- inlang/source-code/sdk/src/v2/rpc-fs/tests/workers/read-write.ts
+++
@@ -1,6 +0,0 @@
-import { endpoint } from "comlink-node/worker"
-import { getFs } from "../../client.js"
-
-const fs = getFs(endpoint)
-const content = await fs.readFile("/innn.txt", { encoding: "utf-8" })
-await fs.writeFile("/out.txt", content)
