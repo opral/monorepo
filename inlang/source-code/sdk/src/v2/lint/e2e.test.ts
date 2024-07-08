3e319d760084759566fda3ddb7ed75df03693e84
LorisSigrist <loris@sigrist.dev>
2024-07-08 10:38:16 +0200

    check that linter initializes properly

--- inlang/source-code/sdk/src/v2/lint/e2e.test.ts
+++ inlang/source-code/sdk/src/v2/lint/e2e.test.ts
@@ -2,12 +2,17 @@ import { describe, it, expect } from "vitest"
 import { connectToLinter } from "./host.js"
 import { defaultProjectSettings } from "../defaultProjectSettings.js"
 import { createNodeishMemoryFs } from "@lix-js/fs"
+import { makeLinterAvailableTo } from "./linter.js"
 
 describe("Lint Worker", () => {
 	it("initializes successfully", async () => {
 		const { port1, port2 } = new MessageChannel()
-
 		const fs = createNodeishMemoryFs()
+
+		makeLinterAvailableTo(port2)
 		const linter = await connectToLinter("/porject.inlang", defaultProjectSettings, fs, port1)
+
+		expect(linter.fix).toBeDefined()
+		expect(linter.lint).toBeDefined()
 	})
 })
