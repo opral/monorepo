b7bfea2097163c99bd8c224fb37af30affc12819
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2024-07-24 17:15:03 -0400

    fix: include css

--- lix/website/src/renderer/+onRenderClient.ts
+++ lix/website/src/renderer/+onRenderClient.ts
@@ -1,5 +1,6 @@
 import { hydrate } from "@lit-labs/ssr-client"
 import "@lit-labs/ssr-client/lit-element-hydrate-support.js"
+import "../style.css"
 
 export async function onRenderClient(pageContext) {
 	const page = pageContext.Page()
--- lix/website/src/renderer/+onRenderHtml.ts
+++ lix/website/src/renderer/+onRenderHtml.ts
@@ -2,6 +2,7 @@ import { dangerouslySkipEscape, escapeInject } from "vike/server"
 import { PageContextServer } from "vike/types"
 import { collectResult } from "@lit-labs/ssr/lib/render-result.js"
 import { render } from "@lit-labs/ssr"
+import "../style.css"
 
 // https://vike.dev/onRenderHtml
 export async function onRenderHtml(pageContext: PageContextServer) {
@@ -13,7 +14,6 @@ export async function onRenderHtml(pageContext: PageContextServer) {
     <html>
       <head>
         <title>Lix - Change Control System</title>
-        <link rel="stylesheet" href="./src/style.css">
         <link rel="icon" type="image/x-icon" href="/favicon.svg">
       </head>
       <body>
