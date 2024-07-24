45bbe136962de70606856d42d7fc184f4f105ec9
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2024-07-24 16:45:12 -0400

    change to templates

--- lix/website/src/renderer/+onRenderClient.ts
+++ lix/website/src/renderer/+onRenderClient.ts
@@ -1,9 +1,8 @@
+import { hydrate } from "@lit-labs/ssr-client"
 import "@lit-labs/ssr-client/lit-element-hydrate-support.js"
-import { kebabCasePageId } from "./kebabCasePageId.ts"
 
 export async function onRenderClient(pageContext) {
-	const pageId = kebabCasePageId(pageContext._pageId)
-	if (customElements.get(pageId) === undefined) {
-		customElements.define(pageId, pageContext.Page)
-	}
+	const page = pageContext.Page()
+	page.renderRoot = document.body
+	hydrate(page, document.body)
 }
--- lix/website/src/renderer/+onRenderHtml.ts
+++ lix/website/src/renderer/+onRenderHtml.ts
@@ -1,20 +1,13 @@
-import { html } from "lit"
 import { dangerouslySkipEscape, escapeInject } from "vike/server"
 import { PageContextServer } from "vike/types"
 import { collectResult } from "@lit-labs/ssr/lib/render-result.js"
 import { render } from "@lit-labs/ssr"
-import { kebabCasePageId } from "./kebabCasePageId.ts"
-import { unsafeHTML } from "lit/directives/unsafe-html.js"
 
 // https://vike.dev/onRenderHtml
 export async function onRenderHtml(pageContext: PageContextServer) {
 	// @ts-expect-error - internal type of vike
-	const pageId = kebabCasePageId(pageContext._pageId)
-	if (customElements.get(pageId) === undefined) {
-		// @ts-expect-error - type is unknown
-		customElements.define(pageId, pageContext.Page)
-	}
-	const content = await collectResult(render(html`${unsafeHTML(`<${pageId}></${pageId}>`)}`))
+	const template = pageContext.Page()
+	const content = await collectResult(render(template))
 
 	return escapeInject`<!DOCTYPE html>
     <html>
