bfc4448599eb4cb2ea43df98b0f371ce73047ad3
Nils Jacobsen <nils.jacobsen98@gmail.com>
2024-07-25 16:04:49 +0200

    fix font

--- lix/website/src/renderer/+onRenderHtml.ts
+++ lix/website/src/renderer/+onRenderHtml.ts
@@ -15,6 +15,9 @@ export async function onRenderHtml(pageContext: PageContextServer) {
       <head>
         <title>Lix - Change Control System</title>
         <link rel="icon" type="image/x-icon" href="/favicon.svg">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1, user-scalable=0">
       </head>
       <body>
