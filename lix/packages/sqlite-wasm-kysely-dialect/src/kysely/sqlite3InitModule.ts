118528cbe52515aede34618baa256d99a8e96a56
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2024-08-01 18:16:13 -0400

    fix: import for node

--- lix/packages/sqlite-wasm-kysely-dialect/src/kysely/sqlite3InitModule.ts
+++ lix/packages/sqlite-wasm-kysely-dialect/src/kysely/sqlite3InitModule.ts
@@ -1,3 +1,20 @@
 import sqlite3InitModule from "@sqlite.org/sqlite-wasm";
 
-export const sqliteModule = await sqlite3InitModule();
+
+let wasm;
+
+// detect if we are in node
+// TODO remove this monkey pathing
+// @ts-expect-error - process is a global variable from node
+if (typeof process !== "undefined") {
+  // @ts-expect-error - node specific
+  const fs = await import("node:fs");
+  wasm = fs.readFileSync(
+    "./node_modules/@sqlite.org/sqlite-wasm/sqlite-wasm/jswasm/sqlite3.wasm",
+  );
+}
+
+export const sqliteModule = await sqlite3InitModule({
+  // @ts-expect-error
+  wasmBinary: wasm,
+});
