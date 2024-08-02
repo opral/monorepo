a9b2d27e6eac64707049e6ea0b3537b76c48ca72
NiklasBuchfink <niklas.buchfink@web.de>
2024-08-02 15:36:33 +0200

    setup getNestedBundleById

--- packages/fink2/src/components/InlangBundle.tsx
+++ packages/fink2/src/components/InlangBundle.tsx
@@ -2,11 +2,10 @@ import { createComponent } from "@lit/react";
 import { InlangBundle as LitInlangBundle } from "@inlang/bundle-component"
 import React, { useEffect, useState } from "react";
 import { useAtom } from "jotai";
-import { jsonArrayFrom } from "kysely/helpers/postgres";
 import { projectAtom } from "../state.ts";
 import { poll } from "../poll.ts";
-import { Database, NestedBundle } from "@inlang/sdk2";
-import { SelectQueryBuilder } from "kysely";
+import { NestedBundle } from "@inlang/sdk2";
+import { getNestedBundleById } from "../helper/getNestedBundleById.ts";
 
 export const ReactBundle = createComponent({
   tagName: "inlang-bundle",
@@ -28,60 +27,15 @@ const InlangBundle = (props: { bundleId: string }) => {
   const [project] = useAtom(projectAtom);
   const [bundle, setBundle] = useState<NestedBundle | undefined>(undefined);
 
-  const populateMessages = (bundleSelect: SelectQueryBuilder<Database, "bundle", object>) => {
-    return bundleSelect.select((eb) => [
-      // select all columns from bundle
-      "id",
-      "alias",
-      // select all columns from messages as "messages"
-      jsonArrayFrom(
-        populateVariants(
-          eb
-            .selectFrom("message"))
-          .whereRef("message.bundleId", "=", "bundle.id" as any)
-      ).as("messages"),
-    ])
-  }
-
-  const populateVariants = (
-    messageSelect: SelectQueryBuilder<Database, "message", object>
-  ) => {
-    return messageSelect.select((eb) => [
-      // select all columns from message
-      "id",
-      "bundleId",
-      "locale",
-      "declarations",
-      "selectors",
-      // select all columns from variants as "variants"
-      jsonArrayFrom(
-        eb
-          .selectFrom("variant")
-          .select(["id", "messageId", "match", "pattern"])
-          .whereRef("variant.messageId", "=", "message.id")
-      ).as("variants"),
-    ])
-  }
-
-  const getBundleById = async (bundleId: string) => {
-    // query the bundle by id and set it to the state
-    if (!project) return
-    populateMessages(project?.db.selectFrom("bundle"))
-      .where("bundle.id", "=", bundleId)
-      .execute()
-      .then((bundle: any) => {
-        setBundle(bundle);
-        console.log("loadedBundle:", bundle);
-      })
-  }
-
   useEffect(() => {
     if (!project) return
     poll({
       every: 2000,
       fn: async () => {
         console.log("poll: " + props.bundleId)
-        getBundleById(props.bundleId);
+        const bundle = await getNestedBundleById(project.db.selectFrom("bundle"), props.bundleId);
+        console.log("bundle", bundle)
+        setBundle(bundle);
       }
     })
   }, [])
