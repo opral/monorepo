61f49b41cb116b878e2268f64b1840cbf33f72ca
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2024-08-02 18:35:18 -0400

    refactor: clean up types
    The runtime types differ from the database types. hence, separate them

--- packages/sdk2/src/schema/schema.ts
+++ packages/sdk2/src/schema/schema.ts
@@ -1,117 +1,72 @@
 import { Type, type Static } from "@sinclair/typebox"
-import type { Insertable, Selectable, Updateable } from "kysely"
 
+export type VariableReference = Static<typeof VariableReference>
 export const VariableReference = Type.Object({
 	type: Type.Literal("variable"),
 	name: Type.String(),
 })
 
+export type Literal = Static<typeof Literal>
 export const Literal = Type.Object({
 	type: Type.Literal("literal"),
 	name: Type.String(),
 })
 
+export type Option = Static<typeof Option>
 export const Option = Type.Object({
 	name: Type.String(),
 	value: Type.Union([Literal, VariableReference]),
 })
 
-const FunctionAnnotation = Type.Object({
+export type FunctionAnnotation = Static<typeof FunctionAnnotation>
+export const FunctionAnnotation = Type.Object({
 	type: Type.Literal("function"),
 	name: Type.String(),
 	options: Type.Array(Option),
 })
 
+export type Expression = Static<typeof Expression>
 export const Expression = Type.Object({
 	type: Type.Literal("expression"),
 	arg: Type.Union([VariableReference, Literal]),
 	annotation: Type.Optional(FunctionAnnotation),
 })
 
+export type Text = Static<typeof Text>
 export const Text = Type.Object({
 	type: Type.Literal("text"),
 	value: Type.String(),
 })
 
+export type Declaration = Static<typeof Declaration>
 export const Declaration = Type.Object({
 	type: Type.Literal("input"),
 	name: Type.String(),
 	value: Expression,
 })
 
+export type Pattern = Static<typeof Pattern>
 export const Pattern = Type.Array(Type.Union([Text, Expression]))
 
-export const BundleTable = Type.Object({
+export type Bundle = Static<typeof Bundle>
+export const Bundle = Type.Object({
 	id: Type.String(),
-	alias: Type.Any(), // TODO: Use appropriate Typebox type for JSONColumnType<AliasMap>
+	alias: Type.Record(Type.String(), Type.String()),
 })
 
-export const MessageTable = Type.Object({
+export type Message = Static<typeof Message>
+export const Message = Type.Object({
 	id: Type.String(),
 	bundleId: Type.String(),
 	locale: Type.String(),
-	declarations: Type.Any(), // TODO: Use appropriate Typebox type for JSONColumnType<Declaration[]>
-	selectors: Type.Any(), // TODO: Use appropriate Typebox type for JSONColumnType<Expression[]>
+	declarations: Type.Array(Declaration),
+	selectors: Type.Array(Expression),
 })
 
-export const VariantTable = Type.Object({
+export type Variant = Static<typeof Variant>
+export const Variant = Type.Object({
 	id: Type.String(),
 	messageId: Type.String(),
 	match: Type.Any(), // TODO: Use appropriate Typebox type for JSONColumnType<Record<Expression["arg"]["name"], string>>
-	pattern: Type.Any(), // TODO: Use appropriate Typebox type for JSONColumnType<Pattern>
+	pattern: Pattern,
 })
-
-export const NestedMessage = Type.Object({
-	id: Type.String(),
-	locale: Type.String(),
-	declarations: Type.Any(), // TODO: Use appropriate Typebox type for JSONColumnType<Declaration[]>
-	selectors: Type.Any(), // TODO: Use appropriate Typebox type for JSONColumnType<Expression[]>
-	variants: Type.Array(VariantTable),
-})
-
-export const NestedBundle = Type.Object({
-	id: Type.String(),
-	alias: Type.Any(), // TODO: Use appropriate Typebox type for JSONColumnType<AliasMap>
-	messages: Type.Array(NestedMessage),
-})
-
-// Export types
-export type BundleTable = Static<typeof BundleTable>
-export type MessageTable = Static<typeof MessageTable>
-export type VariantTable = Static<typeof VariantTable>
-
-export type Bundle = Selectable<BundleTable>
-
-export type NestedMessage = Selectable<MessageTable> & {
-	variants: Selectable<VariantTable>[]
-}
-
-export type NestedBundle = Selectable<BundleTable> & {
-	messages: NestedMessage[]
-}
-
-export type NewBundle = Insertable<BundleTable>
-export type UpdatedBundle = Updateable<BundleTable>
-
-export type Message = Selectable<MessageTable>
-export type NewMessage = Insertable<MessageTable>
-export type UpdatedMessage = Updateable<MessageTable>
-
-export type Variant = Selectable<VariantTable>
-export type NewVariant = Insertable<VariantTable>
-export type UpdatedVariant = Updateable<VariantTable>
-
-export type Pattern = Static<typeof Pattern>
-export type Text = Static<typeof Text>
-export type VariableReference = Static<typeof VariableReference>
-export type Literal = Static<typeof Literal>
-export type Declaration = Static<typeof Declaration>
-export type Expression = Static<typeof Expression>
-export type FunctionAnnotation = Static<typeof FunctionAnnotation>
-export type Option = Static<typeof Option>
-
-export type Database = {
-	bundle: Static<typeof BundleTable>
-	message: Static<typeof MessageTable>
-	variant: Static<typeof VariantTable>
-}
