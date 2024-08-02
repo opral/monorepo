322207a17d9b39ef0958142d1fc66877cc30a63d
NiklasBuchfink <niklas.buchfink@web.de>
2024-08-02 12:32:20 +0200

    insert demo bundle on empty project load

--- inlang/source-code/fink2/src/helper/insertNestedBundle.ts
+++ inlang/source-code/fink2/src/helper/insertNestedBundle.ts
@@ -1,9 +1,12 @@
 import { InlangProject, NestedBundle } from "@inlang/sdk2";
 
 export const insertNestedBundle = async (
-	project: InlangProject,
+	project: InlangProject | undefined,
 	bundle: NestedBundle
 ): Promise<void> => {
+	if (project === undefined) {
+		throw new Error("Project is undefined");
+	}
 	const x = await project.db
 		.insertInto("bundle")
 		.values({
