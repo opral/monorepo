c036ed25b2f53ee2223aa8789fd989a0a00c021f
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2024-08-02 18:34:49 -0400

    refactor: bundle sqlite wasm

--- lix/packages/sqlite-wasm-kysely/src/util/createInMemoryDatabase.ts
+++ lix/packages/sqlite-wasm-kysely/src/util/createInMemoryDatabase.ts
@@ -1,5 +1,6 @@
 import sqlite3InitModule from "@eliaspourquoi/sqlite-node-wasm";
 import { setSqliteModule, sqliteModule } from "../kysely/sqliteModule.js";
+import { wasmBinary } from "./sqliteWasmBinary.js";
 
 export const createInMemoryDatabase = async ({
   readOnly = false,
@@ -18,23 +19,11 @@ export const createInMemoryDatabase = async ({
 };
 
 async function initSqlite() {
-  let wasm;
-
-  // detect if we are in node
-  // TODO remove this monkey pathing
-  // @ts-expect-error - process is a global variable from node
-  if (typeof process !== "undefined") {
-    // @ts-expect-error - node specific
-    const fs = await import("node:fs");
-    wasm = fs.readFileSync(
-      "./node_modules/@eliaspourquoi/sqlite-node-wasm/sqlite-wasm/jswasm/sqlite3.wasm",
-    );
-  }
-
   setSqliteModule(
     await sqlite3InitModule({
       // @ts-expect-error
-      wasmBinary: wasm,
+      wasmBinary: wasmBinary,
     }),
   );
 }
+
