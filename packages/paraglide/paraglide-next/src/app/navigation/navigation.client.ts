514c0324204ee90a54d4124cfc815b57f2f6a270
LorisSigrist <loris@sigrist.dev>
2024-08-02 11:32:26 +0200

    Support `typedRoutes` on `useRouter`

--- packages/paraglide/paraglide-next/src/app/navigation/navigation.client.ts
+++ packages/paraglide/paraglide-next/src/app/navigation/navigation.client.ts
@@ -48,129 +48,127 @@ function createUsePathname<T extends string>(strategy: RoutingStrategy<T>): Loca
 	}
 }
 
-type LocalizedUseRouter<T extends string> = ReturnType<typeof createUseRouter<T>>
+type AppRouterInstance = ReturnType<typeof import("next/navigation").useRouter>
+type LocalizedUseRouter<T extends string> = () => Omit<
+	AppRouterInstance,
+	"push" | "replace" | "prefetch"
+> & {
+	push: (
+		canonicalDestinationPath: Parameters<AppRouterInstance["push"]>[0], // using the parameter is important for typedRoutes
+		options?: (Parameters<AppRouterInstance["push"]>[1] & { locale?: T }) | undefined
+	) => ReturnType<AppRouterInstance["push"]>
+	replace: (
+		canonicalDestinationPath: Parameters<AppRouterInstance["replace"]>[0], // using the parameter is important for typedRoutes
+		options?: (Parameters<AppRouterInstance["replace"]>[1] & { locale?: T }) | undefined
+	) => ReturnType<AppRouterInstance["replace"]>
+	prefetch: (
+		canonicalDestinationPath: Parameters<AppRouterInstance["prefetch"]>[0], // using the parameter is important for typedRoutes
+		options?: (Parameters<AppRouterInstance["prefetch"]>[1] & { locale?: T }) | undefined
+	) => ReturnType<AppRouterInstance["prefetch"]>
+}
+
 function createUseRouter<T extends string>(
 	strategy: RoutingStrategy<T>,
 	usePathname: LocalizedUsePathname
-) {
+): LocalizedUseRouter<T> {
 	return () => {
 		const localiseHref = createLocaliseHref(strategy)
 		const nextRouter = NextNavigation.useRouter()
 		const searchParams = NextNavigation.useSearchParams()
 		const canonicalCurrentPathname = usePathname()
 
-		type NavigateOptions = Parameters<(typeof nextRouter)["push"]>[1]
-		type PrefetchOptions = Parameters<(typeof nextRouter)["prefetch"]>[1]
-
-		type OptionalLanguageOption = { locale?: T }
-
-		/**
-		 * Navigate to the provided href. Pushes a new history entry.
-		 */
-		const push = (
-			canonicalDestinationPath: string,
-			options?: (NavigateOptions & OptionalLanguageOption) | undefined
-		) => {
-			const locale = options?.locale ?? (languageTag() as T)
-			const isLanguageSwitch = locale !== languageTag()
-
-			const localisedPath = localiseHref(
-				canonicalDestinationPath,
-				locale,
-				canonicalCurrentPathname,
-				isLanguageSwitch
-			)
-
-			// If the current and new canonical paths are the same, but the language is different,
-			// we need to do a native reload to make sure the new language is used
-			if (canonicalCurrentPathname === canonicalDestinationPath && isLanguageSwitch) {
-				let destination = addBasePath(localisedPath, true)
-				const searchParamString = searchParams.toString()
-				if (searchParamString) {
-					destination += `?${searchParamString}`
-				}
-				history.pushState({}, "", destination)
-
-				document.cookie = serializeCookie({
-					...LANG_COOKIE,
-					value: locale,
-					Path: basePath ?? "/",
-				})
-
-				window.location.reload()
-				return
-			}
-
-			if (options?.locale) {
-				//Make sure to render new client components with the new language
-				setLanguageTag(options.locale)
-			}
-
-			return nextRouter.push(localisedPath, options)
-		}
-
-		/**
-		 * Navigate to the provided href. Replaces the current history entry.
-		 */
-		const replace = (
-			canonicalDestinationPath: string,
-			options?: (NavigateOptions & OptionalLanguageOption) | undefined
-		) => {
-			const locale = options?.locale ?? (languageTag() as T)
-			const isLanguageSwitch = locale !== languageTag()
-			const localisedPath = localiseHref(
-				canonicalDestinationPath,
-				locale,
-				canonicalCurrentPathname,
-				isLanguageSwitch
-			)
-
-			// If the current and new canonical paths are the same, but the language is different,
-			// we need to do a native reload to make sure the new language is used
-			if (canonicalCurrentPathname === canonicalDestinationPath && isLanguageSwitch) {
-				let destination = addBasePath(localisedPath, true)
-				const searchParamString = searchParams.toString()
-				if (searchParamString) {
-					destination += `?${searchParamString}`
-				}
-				history.replaceState({}, "", destination)
-
-				document.cookie = serializeCookie({
-					...LANG_COOKIE,
-					value: locale,
-					Path: basePath ?? "/",
-				})
-
-				window.location.reload()
-				return
-			}
-
-			if (options?.locale) {
-				//Make sure to render new client components with the new language
-				setLanguageTag(options.locale)
-			}
-
-			return nextRouter.replace(localisedPath, options)
-		}
-
-		/**
-		 * Prefetch the provided href.
-		 */
-		const prefetch = (
-			canonicalDestinationPath: string,
-			options: PrefetchOptions & OptionalLanguageOption
-		) => {
-			const locale = options?.locale ?? (languageTag() as T)
-			const isLanguageSwitch = locale !== languageTag()
-			const localisedPath = localiseHref(canonicalDestinationPath, locale, "/", isLanguageSwitch)
-			return nextRouter.prefetch(localisedPath, options)
-		}
-
 		return {
 			...nextRouter,
-			push,
-			replace,
-			prefetch,
+			/**
+			 * Navigate to the provided href. Pushes a new history entry.
+			 */
+			push: (canonicalDestinationPath, options) => {
+				const locale = options?.locale ?? (languageTag() as T)
+				const isLanguageSwitch = locale !== languageTag()
+
+				const localisedPath = localiseHref(
+					canonicalDestinationPath,
+					locale,
+					canonicalCurrentPathname,
+					isLanguageSwitch
+				)
+
+				// If the current and new canonical paths are the same, but the language is different,
+				// we need to do a native reload to make sure the new language is used
+				if (canonicalCurrentPathname === canonicalDestinationPath && isLanguageSwitch) {
+					let destination = addBasePath(localisedPath, true)
+					const searchParamString = searchParams.toString()
+					if (searchParamString) {
+						destination += `?${searchParamString}`
+					}
+					history.pushState({}, "", destination)
+
+					document.cookie = serializeCookie({
+						...LANG_COOKIE,
+						value: locale,
+						Path: basePath ?? "/",
+					})
+
+					window.location.reload()
+					return
+				}
+
+				if (options?.locale) {
+					//Make sure to render new client components with the new language
+					setLanguageTag(options.locale)
+				}
+
+				return nextRouter.push(localisedPath, options)
+			},
+			/**
+			 * Navigate to the provided href. Replaces the current history entry.
+			 */
+			replace: (canonicalDestinationPath, options) => {
+				const locale = options?.locale ?? (languageTag() as T)
+				const isLanguageSwitch = locale !== languageTag()
+				const localisedPath = localiseHref(
+					canonicalDestinationPath,
+					locale,
+					canonicalCurrentPathname,
+					isLanguageSwitch
+				)
+
+				// If the current and new canonical paths are the same, but the language is different,
+				// we need to do a native reload to make sure the new language is used
+				if (canonicalCurrentPathname === canonicalDestinationPath && isLanguageSwitch) {
+					let destination = addBasePath(localisedPath, true)
+					const searchParamString = searchParams.toString()
+					if (searchParamString) {
+						destination += `?${searchParamString}`
+					}
+					history.replaceState({}, "", destination)
+
+					document.cookie = serializeCookie({
+						...LANG_COOKIE,
+						value: locale,
+						Path: basePath ?? "/",
+					})
+
+					window.location.reload()
+					return
+				}
+
+				if (options?.locale) {
+					//Make sure to render new client components with the new language
+					setLanguageTag(options.locale)
+				}
+
+				return nextRouter.replace(localisedPath, options)
+			},
+			/**
+			 * Prefetch the provided href.
+			 */
+			prefetch: (canonicalDestinationPath, options) => {
+				const locale = options?.locale ?? (languageTag() as T)
+				const isLanguageSwitch = locale !== languageTag()
+				const localisedPath = localiseHref(canonicalDestinationPath, locale, "/", isLanguageSwitch)
+				return nextRouter.prefetch(localisedPath, options)
+			},
 		}
 	}
 }
