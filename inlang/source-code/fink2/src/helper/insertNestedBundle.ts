f9140ba5d00ce37186db6b724bc704078df359b4
Nils Jacobsen <nils.jacobsen98@gmail.com>
2024-08-02 14:39:54 +0200

    feat: add insert messages and variants

--- inlang/source-code/fink2/src/helper/insertNestedBundle.ts
+++ inlang/source-code/fink2/src/helper/insertNestedBundle.ts
@@ -15,5 +15,30 @@ export const insertNestedBundle = async (
 		})
 		.returning("id")
 		.execute();
+
+	bundle.messages.forEach(async (message) => {
+		await project.db
+			.insertInto("message")
+			.values({
+				id: message.id,
+				bundleId: bundle.id,
+				locale: message.locale,
+				declarations: JSON.stringify(message.declarations),
+				selectors: JSON.stringify(message.selectors),
+			})
+			.execute();
+
+		message.variants.forEach(async (variant) => {
+			await project.db
+				.insertInto("variant")
+				.values({
+					id: variant.id,
+					messageId: message.id,
+					match: JSON.stringify(variant.match),
+					pattern: JSON.stringify(variant.pattern),
+				})
+				.execute();
+		});
+	});
 	console.log({ x });
 };
