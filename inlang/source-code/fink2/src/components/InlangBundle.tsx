d79228ee86e67fa40529a8b4d6891cf0b1c66037
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2024-08-03 17:51:55 -0400

    add: selectBundleNested helper

--- inlang/source-code/fink2/src/components/InlangBundle.tsx
+++ inlang/source-code/fink2/src/components/InlangBundle.tsx
@@ -3,50 +3,45 @@ import { InlangBundle as LitInlangBundle } from "@inlang/bundle-component"
 import React, { useEffect, useState } from "react";
 import { useAtom } from "jotai";
 import { projectAtom } from "../state.ts";
-import { BundleNested } from "@inlang/sdk2";
-import { getNestedBundleById } from "../helper/getNestedBundleById.ts";
+import { BundleNested, selectBundleNested } from "@inlang/sdk2";
 
 export const ReactBundle = createComponent({
-  tagName: "inlang-bundle",
-  elementClass: LitInlangBundle,
-  react: React,
-  events: {
-    changeMessageBundle: "change-message-bundle",
-    insertMessage: "insert-message",
-    updateMessage: "update-message",
+	tagName: "inlang-bundle",
+	elementClass: LitInlangBundle,
+	react: React,
+	events: {
+		changeMessageBundle: "change-message-bundle",
+		insertMessage: "insert-message",
+		updateMessage: "update-message",
 
-    insertVariant: "insert-variant",
-    updateVariant: "update-variant",
-    deleteVariant: "delete-variant",
-    fixLint: "fix-lint",
-  },
-})
+		insertVariant: "insert-variant",
+		updateVariant: "update-variant",
+		deleteVariant: "delete-variant",
+		fixLint: "fix-lint",
+	},
+});
 
 const InlangBundle = (props: { bundleId: string }) => {
-  const [project] = useAtom(projectAtom);
-  const [bundle, setBundle] = useState<BundleNested | undefined>(undefined);
+	const [project] = useAtom(projectAtom);
+	const [bundle, setBundle] = useState<BundleNested | undefined>(undefined);
 
-  useEffect(() => {
-    if (!project) return
-    setTimeout(async () => {
-			const bundle = await getNestedBundleById(
-				project.db.selectFrom("bundle"),
-				props.bundleId
-			);
+	useEffect(() => {
+		if (!project) return;
+		setTimeout(async () => {
+			const bundle = await selectBundleNested(project.db)
+				.where("id", "=", props.bundleId)
+				.executeTakeFirst();
 			setBundle(bundle);
 		}, 2000);
-  }, [])
+	}, []);
 
-  return (
-    <>
-      {bundle && (
-        <ReactBundle
-          bundle={bundle}
-          settings={project?.settings.get()}
-        />
-      )}
-    </>
-  )
-}
+	return (
+		<>
+			{bundle && (
+				<ReactBundle bundle={bundle} settings={project?.settings.get()} />
+			)}
+		</>
+	);
+};
 
 export default InlangBundle;
