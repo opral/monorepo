eceb0da7f92069b094b12941736b5fbb12649fa9
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2024-08-03 16:56:15 -0400

    test: get and set settings

--- inlang/source-code/sdk2/src/project/loadProjectInMemory.test.ts
+++ inlang/source-code/sdk2/src/project/loadProjectInMemory.test.ts
@@ -2,7 +2,6 @@ import { expect, test } from "vitest"
 import { newProject } from "./newProject.js"
 import { loadProjectInMemory } from "./loadProjectInMemory.js"
 import { generateBundleId } from "../bundle-id/bundle-id.js"
-import { uuidv4 } from "@lix-js/sdk"
 
 test("roundtrip should succeed", async () => {
 	const file1 = await newProject()
@@ -28,54 +27,4 @@ test("roundtrip should succeed", async () => {
 	const bundles = await project2.db.selectFrom("bundle").select("id").execute()
 	expect(bundles.length).toBe(1)
 	expect(bundles[0]?.id).toBe(insertedBundle.id)
-})
-
-test("it should persist changes of bundles, messages, and variants to lix ", async () => {
-	const file1 = await newProject()
-	const project1 = await loadProjectInMemory({ blob: file1 })
-	const bundle = await project1.db
-		.insertInto("bundle")
-		.values({
-			id: generateBundleId(),
-			// @ts-expect-error - manual stringification
-			alias: JSON.stringify({ default: "bundle1" }),
-		})
-		.returning("id")
-		.executeTakeFirstOrThrow()
-
-	const message = await project1.db
-		.insertInto("message")
-		.values({
-			id: uuidv4(),
-			bundle_id: bundle.id,
-			locale: "en",
-			// @ts-expect-error - manual stringification
-			declarations: "[]",
-			// @ts-expect-error - manual stringification
-			selectors: "[]",
-		})
-		.returning("id")
-		.executeTakeFirstOrThrow()
-
-	await project1.db
-		.insertInto("variant")
-		.values({
-			id: uuidv4(),
-			message_id: message.id,
-			match: "exact",
-			// @ts-expect-error - manual stringification
-			pattern: "[]",
-		})
-		.execute()
-
-	const file1AfterUpdates = await project1.toBlob()
-	await project1.close()
-
-	const project2 = await loadProjectInMemory({ blob: file1AfterUpdates })
-	const bundles = await project2.db.selectFrom("bundle").select("id").execute()
-	const messages = await project2.db.selectFrom("message").select("id").execute()
-	const variants = await project2.db.selectFrom("variant").select("id").execute()
-	expect(bundles.length).toBe(1)
-	expect(messages.length).toBe(1)
-	expect(variants.length).toBe(1)
 })
