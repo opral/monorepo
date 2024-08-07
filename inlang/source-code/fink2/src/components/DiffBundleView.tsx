fa4f107e86fe9c79e89db4fdd7b8c53775e7873a
Nils Jacobsen <nils.jacobsen98@gmail.com>
2024-08-07 17:02:55 +0200

    feat: add history

--- inlang/source-code/fink2/src/components/DiffBundleView.tsx
+++ inlang/source-code/fink2/src/components/DiffBundleView.tsx
@@ -3,9 +3,8 @@ import { projectAtom } from "../state.ts";
 import { useAtom } from "jotai";
 import { BundleNested, selectBundleNested } from "@inlang/sdk2";
 import { ReactBundle } from "./InlangBundle.tsx";
-import { jsonArrayFrom } from "kysely/helpers/sqlite";
 
-const DiffBundleView = (props: { change: any }) => {
+const DiffBundleView = (props: { changes: any[]; bundleId: string }) => {
 	const [project] = useAtom(projectAtom);
 	const [bundle, setBundle] = useState<BundleNested | undefined>(undefined);
 	const [oldBundle, setOldBundle] = useState<BundleNested | undefined>(
@@ -15,67 +14,97 @@ const DiffBundleView = (props: { change: any }) => {
 	useEffect(() => {
 		if (!project) return;
 		queryNewBundle();
-		queryOldBundle();
 		setInterval(async () => {
 			queryNewBundle();
-			queryOldBundle();
 		}, 2000);
 	}, []);
 
 	const queryNewBundle = async () => {
 		if (!project) return;
 		const bundle = await selectBundleNested(project.db)
-			.where((eb) =>
-				eb.exists(
-					project.db
-						.selectFrom("message")
-						.selectAll()
-						.where((em) =>
-							em.exists(
-								project.db
-									.selectFrom("variant")
-									.selectAll()
-									.where("id", "is", props.change.value.id)
-							)
-						)
-				)
-			)
+			.where("bundle.id", "=", props.bundleId)
 			.executeTakeFirst();
-		setBundle(bundle);
+
+		if (bundle) {
+			setBundle(bundle);
+			const oldBundle = structuredClone(bundle);
+			for (const change of props.changes) {
+				const latestCommitedChange = await getLatestCommitedChange(change);
+				if (
+					latestCommitedChange &&
+					latestCommitedChange.value &&
+					latestCommitedChange.type === "variant"
+				) {
+					const message = oldBundle.messages.find(
+						(message) => message.id === latestCommitedChange.value!.messageId
+					);
+					if (message) {
+						const variant = message.variants.find(
+							(variant) => variant.id === latestCommitedChange.value!.id
+						);
+						if (variant) {
+							variant.match = latestCommitedChange.value.match;
+							variant.pattern = latestCommitedChange.value.pattern;
+						}
+					}
+				}
+			}
+			setOldBundle(oldBundle);
+		}
 	};
 
-	const queryOldBundle = async () => {
+	const getLatestCommitedChange = async (change: any) => {
 		if (!project) return;
-		const result = await project.lix.db
+		const latestCommitedChange = await project.lix.db
 			.selectFrom("change")
-			.select((ec) => [
-				"commit_id",
-				"value",
-				jsonArrayFrom(
-					ec
-						.selectFrom("commit")
-						.select(["commit.zoned_date_time"])
-						.whereRef("commit_id", "is", "commit.id")
-				).as("commit"),
-			])
-			.execute();
+			.selectAll()
+			.where("commit_id", "is not", null)
+			.where("type", "=", "variant")
+			.where((eb) => eb.ref("value", "->>").key("id"), "=", change.value.id)
+			.innerJoin("commit", "commit.id", "change.commit_id")
+			.orderBy("commit.zoned_date_time desc")
+			.executeTakeFirst();
+
+		if (latestCommitedChange) {
+			return latestCommitedChange;
+		} else {
+			return;
+		}
 	};
 
 	return (
 		<div className="bg-zinc-100 rounded p-4 mt-4">
-			<h3 className="pb-2">
-				{props.change.value.id} | {props.change.type} changed
-			</h3>
-			<p className="text-sm">{JSON.stringify(props.change)}</p>
-			<p className="font-medium pb-2 pt-4">New:</p>
-			{bundle && (
-				<ReactBundle
-					style={{ pointerEvents: "none" }}
-					bundle={bundle}
-					settings={project?.settings.get()}
-				/>
-			)}
-			<p className="font-medium pb-2 pt-4">Old:</p>
+			<h3 className="font-medium text-[16px] pb-4">{props.bundleId}</h3>
+			{props.changes.map((change) => (
+				<p className="text-zinc-600" key={change.id}>
+					<span className="font-bold text-zinc-950">You</span> changed the{" "}
+					<span className="font-bold text-zinc-950">{change.type}</span> with
+					the id{" "}
+					<span className="font-bold text-zinc-950">{change.value.id}</span>
+				</p>
+			))}
+			<div className="flex gap-8">
+				<div className="flex-1 opacity-50">
+					<p className="font-medium pb-2 pt-4">Old:</p>
+					{oldBundle && (
+						<ReactBundle
+							style={{ pointerEvents: "none" }}
+							bundle={oldBundle}
+							settings={project?.settings.get()}
+						/>
+					)}
+				</div>
+				<div className="flex-1">
+					<p className="font-medium pb-2 pt-4">New:</p>
+					{bundle && (
+						<ReactBundle
+							style={{ pointerEvents: "none" }}
+							bundle={bundle}
+							settings={project?.settings.get()}
+						/>
+					)}
+				</div>
+			</div>
 		</div>
 	);
 };
--- inlang/source-code/fink2/src/routes/changes/Page.tsx
+++ inlang/source-code/fink2/src/routes/changes/Page.tsx
@@ -8,32 +8,80 @@ import {
 	SlInput,
 } from "@shoelace-style/shoelace/dist/react";
 import DiffBundleView from "../../components/DiffBundleView.tsx";
+import { BundleNested, selectBundleNested } from "@inlang/sdk2";
 
 export default function App() {
 	const [project] = useAtom(projectAtom);
 	const [uncommittedChanges, setUncommittedChanges] = useState<Array<any>>([]);
+	const [numCommits, setNumCommits] = useState<number>(0);
+	const [bundlesWithChanges, setBundlesWithChanges] = useState<Array<any>>([]);
 	const [commitAuthor, setCommitAuthor] = useState<string>("");
 	const [commitDescription, setCommitDescription] = useState<string>("");
 	const [showDialog, setShowDialog] = useState(false);
+	const [commitedChanges, setCommitedChanges] = useState<Array<any>>([]);
 
 	const setNumbers = async () => {
-		const newUncommittedChanges = await project?.lix.db
+		if (!project) return;
+		const newUncommittedChanges = await project.lix.db
 			.selectFrom("change")
 			.selectAll()
 			.where("commit_id", "is", null)
 			.execute();
 
-		if (
-			newUncommittedChanges &&
-			JSON.stringify(uncommittedChanges) !==
-				JSON.stringify(newUncommittedChanges)
-		) {
+		if (newUncommittedChanges) {
 			setUncommittedChanges(newUncommittedChanges);
+
+			const atomsWithChanges = newUncommittedChanges
+				.map((change) => change.value?.id)
+				.filter((id) => id !== undefined) as string[];
+
+			const bundlesWithChanges = await selectBundleNested(project.db)
+				.where(({ eb, selectFrom }) =>
+					eb.exists(
+						selectFrom("message")
+							.selectAll()
+							.whereRef("message.bundleId", "=", "bundle.id")
+							.where(({ eb, selectFrom }) =>
+								eb.exists(
+									selectFrom("variant")
+										.selectAll()
+										.whereRef("variant.messageId", "=", "message.id")
+										.where("variant.id", "in", atomsWithChanges)
+								)
+							)
+					)
+				)
+				.execute();
+
+			if (bundlesWithChanges) {
+				setBundlesWithChanges(bundlesWithChanges);
+			}
+		}
+
+		const numCommits = await project.lix.db
+			.selectFrom("commit")
+			.selectAll()
+			.execute();
+
+		if (numCommits) {
+			setNumCommits(numCommits.length);
+		}
+
+		const commitedChanges = await project.lix.db
+			.selectFrom("change")
+			.selectAll()
+			.where("commit_id", "is not", null)
+			.innerJoin("commit", "commit.id", "change.commit_id")
+			.orderBy("commit.zoned_date_time desc")
+			.execute();
+
+		if (commitedChanges) {
+			setCommitedChanges(commitedChanges);
 		}
 	};
 
 	const handleCommit = async () => {
-		console.log("executing commit");
+		//console.log("executing commit");
 		await project?.lix.commit({
 			userId: commitAuthor,
 			description: commitDescription,
@@ -41,12 +89,23 @@ export default function App() {
 		setShowDialog(false);
 	};
 
+	const getScopedChangesByBundle = (bundle: BundleNested) => {
+		const uncommittedChangesForBundle = uncommittedChanges.filter((change) => {
+			const variantId = change.value?.id;
+			if (!variantId) return false;
+			return bundle.messages.some((message) =>
+				message.variants.some((variant) => variant.id === variantId)
+			);
+		});
+		return uncommittedChangesForBundle;
+	};
+
 	useEffect(() => {
 		setNumbers();
 		setInterval(async () => {
 			setNumbers();
 		}, 1500);
-	});
+	}, []);
 
 	return (
 		<>
@@ -54,7 +113,7 @@ export default function App() {
 				<div className="flex gap-4 items-center bg-zinc-100 rounded px-4 py-2 justify-between">
 					<div className="flex items-center gap-2">
 						<p className="text-sm font-medium">
-							{uncommittedChanges.length} Outstanding change[s]
+							{uncommittedChanges.length} Pending change[s]
 						</p>
 						{/* <p>{numCommittedChanges} Committed changes </p>
 						<p>{numCommits} Commits</p> */}
@@ -94,9 +153,37 @@ export default function App() {
 						</SlButton>
 					</SlDialog>
 				</div>
-				{uncommittedChanges.map((change) => (
-					<DiffBundleView change={change} key={change.id} />
+				{bundlesWithChanges.map((bundle) => (
+					<DiffBundleView
+						changes={getScopedChangesByBundle(bundle)}
+						bundleId={bundle.id}
+						key={bundle.id}
+					/>
 				))}
+				<div className="flex gap-4 items-center bg-zinc-100 rounded px-4 py-2 justify-between mt-8 h-[46px]">
+					<div className="flex items-center gap-2 justify-between w-full">
+						<p className="text-sm font-medium">History</p>
+						<div className="text-xs! text-zinc-700 bg-zinc-300 h-5 rounded flex items-center px-2 font-medium">
+							{numCommits} commits
+						</div>
+					</div>
+				</div>
+				<div className="flex flex-col gap-2 py-4">
+					{commitedChanges.map((change) => (
+						<p
+							key={change.id}
+							className="text-zinc-600 h-[46px] bg-zinc-50 px-4 py-3 rounded"
+						>
+							<span className="font-bold text-zinc-950">You</span>
+							{" changed the "}
+							<span className="font-bold text-zinc-950">
+								{change.type}
+							</span>{" "}
+							with the id{" "}
+							<span className="font-bold text-zinc-950">{change.value.id}</span>
+						</p>
+					))}
+				</div>
 			</Layout>
 		</>
 	);
