1946af9a464f66d858576b3ad842afa49f751905
Nils Jacobsen <nils.jacobsen98@gmail.com>
2024-08-08 11:22:26 +0200

    feat: show old and neu in split diff view

---
+++ inlang/source-code/fink2/src/components/SingleDiffBundle.tsx
@@ -0,0 +1,88 @@
+import { BundleNested, MessageNested, ProjectSettings } from "@inlang/sdk2";
+import { createComponent } from "@lit/react";
+import {
+	InlangBundleHeader as LitInlangBundleHeader,
+	InlangMessage as LitInlangMessage,
+	InlangVariant as LitInlangVariant,
+	InlangPatternEditor as LitInlangPatternEditor,
+} from "@inlang/bundle-component";
+import React from "react";
+import clsx from "clsx";
+
+export const InlangBundleHeader = createComponent({
+	tagName: "inlang-bundle-header",
+	elementClass: LitInlangBundleHeader,
+	react: React,
+});
+export const InlangMessage = createComponent({
+	tagName: "inlang-message",
+	elementClass: LitInlangMessage,
+	react: React,
+});
+export const InlangVariant = createComponent({
+	tagName: "inlang-variant",
+	elementClass: LitInlangVariant,
+	react: React,
+});
+export const InlangPatternEditor = createComponent({
+	tagName: "inlang-pattern-editor",
+	elementClass: LitInlangPatternEditor,
+	react: React,
+});
+
+const SingleDiffBundle = (props: {
+	bundle: BundleNested;
+	oldBundle: BundleNested;
+	settings: ProjectSettings;
+	changes: any[];
+	type: "neu" | "old";
+}) => {
+	return (
+		<div className="pointer-events-none">
+			<InlangBundleHeader bundle={props.bundle} settings={props.settings} />
+			{props.bundle.messages.map((message) => {
+				return (
+					<InlangMessage
+						key={message.id}
+						message={message}
+						locale={message.locale}
+						settings={props.settings}
+					>
+						{message.variants.map((variant) => {
+							const change = props.changes.find(
+								(change) => change.value.id === variant.id
+							);
+
+							return (
+								<InlangVariant
+									slot="variant"
+									key={variant.id}
+									bundleId={props.bundle.id}
+									message={message}
+									locale={message.locale}
+									variant={variant}
+									className={clsx(!change ? "opacity-30" : "")}
+								>
+									<InlangPatternEditor
+										slot="pattern-editor"
+										pattern={variant.pattern}
+										className={clsx(
+											change &&
+												clsx(
+													props.type === "neu"
+														? "inlang-pattern-editor-neu"
+														: "inlang-pattern-editor-old"
+												)
+										)}
+									></InlangPatternEditor>
+								</InlangVariant>
+							);
+						})}
+					</InlangMessage>
+				);
+			})}
+		</div>
+	);
+};
+
+export default SingleDiffBundle;
