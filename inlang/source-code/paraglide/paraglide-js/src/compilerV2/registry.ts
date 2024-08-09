9cac02295b5042d8f41e17d208e5e3bce427a0d8
LorisSigrist <loris@sigrist.dev>
2024-08-09 15:50:55 +0200

    stash

---
+++ inlang/source-code/paraglide/paraglide-js/src/compilerV2/registry.ts
@@ -0,0 +1,14 @@
+export function createRegistry() {
+	return `/* eslint-disable */
+
+/**
+ * @param {import("./runtime.js").AvailableLanguageTags} lang
+ * @param {number} input
+ * @param {{ type: "cardinal" | "ordinal" }} options
+ * @returns {string}
+ */
+export function plural(lang, input, options) {
+    const formatter = new Intl.PluralRules(lang, options)
+    return formatter.select(input)
+}`
+}
