496ddd50462f3aade0f5e4ee153cf31cf8ed49d5
LorisSigrist <loris@sigrist.dev>
2024-08-09 15:51:33 +0200

    use `loadProjectFromDirectory`

---
+++ inlang/source-code/paraglide/paraglide-js/src/cli2/commands/compile/command.ts
@@ -0,0 +1,75 @@
+import nodeFsPromises from "node:fs/promises"
+import { loadProjectFromDirectoryInMemory } from "@inlang/sdk2"
+import { Command } from "commander"
+import { resolve } from "node:path"
+import { Logger } from "~/services/logger/index.js"
+import { openRepository, findRepoRoot } from "@lix-js/client"
+import { runCompiler } from "~/cli/steps/run-compiler.js"
+import { DEFAULT_OUTDIR } from "~/cli/defaults.js"
+
+export const compileCommand = new Command()
+	.name("compile")
+	.summary("Compiles inlang Paraglide-JS.")
+	.requiredOption("--project <path>", 'The path to the inlang project. Example: "./project.inlang"')
+	.requiredOption(
+		"--outdir <path>",
+		'The path to the output directory. Example: "./src/paraglide"',
+		DEFAULT_OUTDIR
+	)
+	.requiredOption("--silent", "Only log errors to the console", false)
+	.requiredOption("--watch", "Watch for changes and recompile.", false)
+	.action(async (options: { silent: boolean; project: string; outdir: string; watch: boolean }) => {
+		const logger = new Logger({ silent: options.silent, prefix: true })
+		const path = resolve(process.cwd(), options.project)
+
+		logger.info(`Compiling inlang project at "${options.project}".`)
+
+		const repoRoot = await findRepoRoot({ nodeishFs: nodeFsPromises, path })
+		const repo = await openRepository(repoRoot || "file://" + process.cwd(), {
+			nodeishFs: nodeFsPromises,
+		})
+
+		if (!repoRoot) {
+			logger.warn(`Could not find repository root for path ${path}`)
+		}
+
+		const project = await loadProjectFromDirectoryInMemory({
+			path,
+			fs: nodeFsPromises,
+		})
+
+		await runCompiler({
+			project,
+			repo,
+			outdir: options.outdir,
+		})
+
+		if (options.watch) {
+			process.on("SIGINT", () => {
+				//start with a new line, since the ^C is on the current line
+				process.exit(0)
+			})
+
+			let numChanges = 0
+			project.query.messages.getAll.subscribe(async (messages) => {
+				numChanges++
+				if (messages.length === 0) return //messages probably haven't loaded yet
+				if (numChanges === 1) return //don't recompile on the first run
+
+				logger.info("Messages changed. Recompiling...")
+				await runCompiler({
+					project,
+					repo,
+					outdir: options.outdir,
+				})
+			})
+
+			/* eslint-disable no-constant-condition */
+			while (true) {
+				// Keep the process alive
+				await new Promise((resolve) => setTimeout(resolve, 10_000))
+			}
+		}
+
+		logger.info("Successfully compiled the project.")
+	})
