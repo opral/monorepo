c3e5a085a615387304a2a3bec5bc96559fcc3b95
LorisSigrist <loris@sigrist.dev>
2024-08-12 11:30:27 +0200

    update registry

--- packages/paraglide/paraglide-js/src/compilerV2/registry.ts
+++ packages/paraglide/paraglide-js/src/compilerV2/registry.ts
@@ -2,14 +2,11 @@ export function createRegistry() {
 	return `/* eslint-disable */
 
 /**
- * @param {import("./runtime.js").AvailableLanguageTag} lang
+ * @param {import("./runtime.js").AvailableLanguageTag} locale
  * @param {number} input
  * @param {object} [options]
  * @param {"cardinal" | "ordinal"} [options.type="cardinal"] 
- * @returns {"zero" | "one" | "two" | "few" | "many" | "other"}
+ * @returns {string}
  */
-export function plural(lang, input, options) {
-    const formatter = new Intl.PluralRules(lang, options)
-    return formatter.select(input)
-}`
+export const plural = (locale, input, options) =>  new Intl.PluralRules(locale, options).select(input)`
 }
