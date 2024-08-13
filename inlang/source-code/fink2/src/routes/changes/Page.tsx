0af10c482b2e47320cbc5f56cfe1596997497a2d
Nils Jacobsen <nils.jacobsen98@gmail.com>
2024-08-13 17:10:16 +0200

    feat: add commit line

--- inlang/source-code/fink2/src/routes/changes/Page.tsx
+++ inlang/source-code/fink2/src/routes/changes/Page.tsx
@@ -68,81 +68,109 @@ export default function App() {
 	return (
 		<>
 			<Layout>
-				<div className="flex gap-4 items-center bg-zinc-100 rounded px-4 py-2 justify-between mb-4">
-					<div className="flex items-center gap-2">
-						<p className="text-sm font-medium">
-							<span className="text-zinc-800 bg-zinc-300 px-[6px] py-[2px] rounded mr-2">
-								{pendingChanges.length}
-							</span>
-							{pendingChanges.length === 1
-								? "Pending change"
-								: "Pending changes"}
-						</p>
-						{/* <p>{numCommittedChanges} Committed changes </p>
-						<p>{numCommits} Commits</p> */}
-					</div>
-					<SlButton
-						size="small"
-						variant="primary"
-						disabled={pendingChanges.length === 0}
-						onClick={() => {
-							setShowDialog(true);
-						}}
-					>
-						Commit changes
-					</SlButton>
-					<SlDialog
-						label="Add commit details"
-						open={showDialog}
-						onSlRequestClose={() => setShowDialog(false)}
-					>
-						<div className="flex flex-col gap-4">
-							<SlInput
-								label="Author"
-								helpText="Enter your name"
-								placeholder="Max Mustermann"
-								// eslint-disable-next-line @typescript-eslint/no-explicit-any
-								onInput={(e: any) => setCommitAuthor(e.target.value)}
-							></SlInput>
-							<SlInput
-								label="Description"
-								helpText="Add a description of the changes"
-								placeholder="I updated the german translations"
-								// eslint-disable-next-line @typescript-eslint/no-explicit-any
-								onInput={(e: any) => setCommitDescription(e.target.value)}
-							></SlInput>
+				<div className="relative pb-10">
+					{commits.length > 0 && (
+						<div className="w-[1px] h-full absolute pt-6 translate-x-[9.3px] -z-10">
+							<div className="w-full h-[calc(100%_+_10px)] border-r-[2px] border-zinc-400 border-dashed" />
 						</div>
-						<SlButton variant="primary" slot="footer" onClick={handleCommit}>
-							Commit changes
-						</SlButton>
-					</SlDialog>
+					)}
+
+					<div className="w-full flex gap-3 items-center">
+						{pendingChanges.length > 0 ? (
+							<div className="w-5 h-5 bg-blue-100 flex items-center justify-center rounded-full">
+								<div className="w-2 h-2 bg-blue-600 rounded-full" />
+							</div>
+						) : (
+							<div className="w-5 h-5 bg-zinc-100 flex items-center justify-center rounded-full">
+								<div className="w-2 h-2 bg-zinc-600 rounded-full" />
+							</div>
+						)}
+
+						<div className="flex-1 flex gap-4 items-center bg-zinc-100 rounded px-4 py-2 justify-between">
+							<div className="flex items-center gap-2">
+								<p className="text-sm font-medium">
+									<span className="text-zinc-800 bg-zinc-300 px-[6px] py-[2px] rounded mr-2">
+										{pendingChanges.length}
+									</span>
+									{pendingChanges.length === 1
+										? "Pending change"
+										: "Pending changes"}
+								</p>
+								{/* <p>{numCommittedChanges} Committed changes </p>
+						<p>{numCommits} Commits</p> */}
+							</div>
+							<SlButton
+								size="small"
+								variant="primary"
+								disabled={pendingChanges.length === 0}
+								onClick={() => {
+									setShowDialog(true);
+								}}
+							>
+								Commit changes
+							</SlButton>
+							<SlDialog
+								label="Add commit details"
+								open={showDialog}
+								onSlRequestClose={() => setShowDialog(false)}
+							>
+								<div className="flex flex-col gap-4">
+									<SlInput
+										label="Author"
+										helpText="Enter your name"
+										placeholder="Max Mustermann"
+										// eslint-disable-next-line @typescript-eslint/no-explicit-any
+										onInput={(e: any) => setCommitAuthor(e.target.value)}
+									></SlInput>
+									<SlInput
+										label="Description"
+										helpText="Add a description of the changes"
+										placeholder="I updated the german translations"
+										// eslint-disable-next-line @typescript-eslint/no-explicit-any
+										onInput={(e: any) => setCommitDescription(e.target.value)}
+									></SlInput>
+								</div>
+								<SlButton
+									variant="primary"
+									slot="footer"
+									onClick={handleCommit}
+								>
+									Commit changes
+								</SlButton>
+							</SlDialog>
+						</div>
+					</div>
+
+					<div className="pl-[30px]">
+						{bundlesWithChanges.map((bundle) => (
+							<DiffBundleView
+								changes={getScopedChangesByBundle(bundle)}
+								bundleId={bundle.id}
+								key={bundle.id}
+							/>
+						))}
+					</div>
 				</div>
-				{bundlesWithChanges.map((bundle) => (
-					<DiffBundleView
-						changes={getScopedChangesByBundle(bundle)}
-						bundleId={bundle.id}
-						key={bundle.id}
-					/>
-				))}
-				<div className="flex gap-4 items-center bg-zinc-100 rounded px-4 py-2 justify-between mt-8 h-[46px]">
+
+				{/* <div className="flex gap-4 items-center bg-zinc-100 rounded px-4 py-2 justify-between mt-8 h-[46px]">
 					<div className="flex items-center gap-2 justify-between w-full">
 						<p className="text-sm font-medium">Commit history</p>
 						<div className="text-xs! text-zinc-700 bg-zinc-300 h-5 rounded flex items-center px-2 font-medium">
 							{commits.length} commits
 						</div>
 					</div>
-				</div>
-				<div className="flex flex-col gap-2 my-4 relative">
-					<div className="w-[1px] h-full absolute py-6 translate-x-[11.5px] -z-10">
-						<div className="w-full h-full border-r-[2px] border-zinc-300 border-dashed " />
+				</div> */}
+				<div className="flex flex-col gap-2 relative">
+					<div className="w-[1px] h-full absolute py-6 translate-x-[9.3px] -z-10">
+						<div className="w-full h-full border-r-[2px] border-zinc-400" />
 					</div>
 					{commits.map((commit) => (
 						<div
 							key={commit.id + Math.random()}
 							className="flex gap-3 items-center"
 						>
-							<div className="w-6 h-6 bg-zinc-200 flex items-center justify-center rounded-full">
-								<div className="w-2.5 h-2.5 bg-zinc-700 rounded-full"></div>
+							<div className="w-5 h-5 bg-white flex items-center justify-center rounded-full">
+								<div className="w-2 h-2 bg-zinc-700 rounded-full"></div>
 							</div>
 							<div className="flex-1 flex gap-2 items-center justify-between bg-zinc-50 px-4 py-3 rounded h-[46px]">
 								<div className="flex gap-2 items-center">
