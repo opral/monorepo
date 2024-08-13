b6b0855a5f6e7c3c270c1c74ab513e700145ee38
NiklasBuchfink <niklas.buchfink@web.de>
2024-08-13 11:09:51 +0200

    chore: invalid dom properties

--- packages/fink2/src/components/NoProjectView.tsx
+++ packages/fink2/src/components/NoProjectView.tsx
@@ -10,13 +10,13 @@ const NoProjectView = () => {
 				fill="none"
 				xmlns="http://www.w3.org/2000/svg"
 			>
-				<g clip-path="url(#clip0_1090_10128)">
+				<g clipPath="url(#clip0_1090_10128)">
 					<path
 						d="M7.85993 15.7171V4.71708C7.85993 3.88354 8.19105 3.08414 8.78045 2.49474C9.36985 1.90534 10.1693 1.57422 11.0028 1.57422H31.4314L42.4314 12.5742V39.2885C42.4314 40.122 42.1002 40.9214 41.5108 41.5108C40.9214 42.1002 40.122 42.4314 39.2885 42.4314H23.5742M11.0028 23.5742V42.4314M1.57422 33.0028H20.4314"
 						stroke="currentColor"
-						stroke-width="2.51429"
-						stroke-linecap="round"
-						stroke-linejoin="round"
+						strokeWidth="2.51429"
+						strokeLinecap="round"
+						strokeLinejoin="round"
 					/>
 				</g>
 				<defs>
