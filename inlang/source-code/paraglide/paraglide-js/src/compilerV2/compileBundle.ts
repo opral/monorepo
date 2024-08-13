1e86fbc27486f3ffb26b6052670b17da0d83884b
LorisSigrist <loris@sigrist.dev>
2024-08-13 10:10:22 +0200

    Support a wider array of strings

--- inlang/source-code/paraglide/paraglide-js/src/compilerV2/compileBundle.ts
+++ inlang/source-code/paraglide/paraglide-js/src/compilerV2/compileBundle.ts
@@ -1,5 +1,4 @@
 import type { BundleNested, MessageNested } from "@inlang/sdk2"
-import { isValidJSIdentifier } from "../services/valid-js-identifier/index.js"
 import { bundleIndexFunction } from "./bundleIndex.js"
 import { compileMessage } from "./compileMessage.js"
 import { mergeTypeRestrictions, type Compilation } from "./types.js"
@@ -20,12 +19,6 @@ export const compileBundle = (
 	bundle: BundleNested,
 	fallbackMap: Record<string, string | undefined>
 ): Resource => {
-	if (!isValidJSIdentifier(bundle.id)) {
-		throw new Error(
-			`Cannot compile message with ID "${bundle.id}".\n\nThe message is not a valid JavaScript variable name. Please choose a different ID.\n\nTo detect this issue during linting, use the valid-js-identifier lint rule: https://inlang.com/m/teldgniy/messageLintRule-inlang-validJsIdentifier`
-		)
-	}
-
 	const compiledMessages: Record<string, Compilation<MessageNested>> = {}
 
 	let typeRestrictions = {}
