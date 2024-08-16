b79c3f26c7a1827569baef8198653eb2e3b82d3a
LorisSigrist <loris@sigrist.dev>
2024-08-16 09:57:05 +0200

    update types

--- inlang/source-code/sdk2/src/plugin/schema.ts
+++ inlang/source-code/sdk2/src/plugin/schema.ts
@@ -1,4 +1,4 @@
-import type { TObject, Static, TSchema, TUndefined } from "@sinclair/typebox";
+import type { TObject } from "@sinclair/typebox";
 import type { BundleNested } from "../schema/schemaV2.js";
 import type { MessageV1 } from "../schema/schemaV1.js";
 import type { ProjectSettings } from "../schema/settings.js";
@@ -7,18 +7,17 @@ import type fs from "node:fs/promises";
 import type { ResourceFile } from "../project/api.js";
 
 export type InlangPlugin<
-	ID extends string = NoInfer<string>,
-	SettingsSchema extends TSchema = TUndefined
+	ExternalSettings extends Record<string, any> | unknown = unknown
 > = {
 	/**
 	 * @deprecated Use `key` instead.
 	 */
-	id?: NoInfer<ID>;
+	id?: string;
 	/**
 	 * The key of the plugin.
 	 */
-	key: ID;
-	settingsSchema?: SettingsSchema;
+	key: string;
+	settingsSchema?: TObject;
 	/**
 	 * @deprecated Use `importFiles` instead.
 	 */
@@ -39,10 +38,7 @@ export type InlangPlugin<
 	 * see https://linear.app/opral/issue/MESDK-157/sdk-v2-release-on-sqlite
 	 */
 	toBeImportedFiles?: (args: {
-		settings: ProjectSettings &
-			(SettingsSchema extends TObject
-				? Record<ID, Static<SettingsSchema>>
-				: never);
+		settings: ProjectSettings & ExternalSettings;
 		nodeFs: typeof fs;
 	}) => Promise<Array<ResourceFile>> | Array<ResourceFile>;
 	importFiles?: (args: { files: Array<ResourceFile> }) => {
@@ -50,10 +46,7 @@ export type InlangPlugin<
 	};
 	exportFiles?: (args: {
 		bundles: BundleNested;
-		settings: ProjectSettings &
-			(SettingsSchema extends TObject
-				? Record<ID, Static<SettingsSchema>>
-				: never);
+		settings: ProjectSettings & ExternalSettings;
 	}) => Array<ResourceFile>;
 	/**
 	 * Define app specific APIs.
@@ -66,9 +59,6 @@ export type InlangPlugin<
 	 *  })
 	 */
 	addCustomApi?: (args: {
-		settings: ProjectSettings &
-			(SettingsSchema extends TObject
-				? Record<ID, Static<SettingsSchema>>
-				: never);
+		settings: ProjectSettings & ExternalSettings;
 	}) => Record<string, unknown>;
 };
