ab15ef08127df00bf032c7c3b94f57905119064a
LorisSigrist <loris@sigrist.dev>
2024-08-16 11:21:00 +0200

    Demo project

---
+++ inlang/development-projects/sdk-importer/index.js
@@ -0,0 +1,16 @@
+import { loadProjectFromDirectoryInMemory, selectBundleNested } from "@inlang/sdk2"
+import icu1Importer from "@inlang/icu1-importer"
+import fs from "node:fs/promises"
+import path from "node:path"
+
+const project = await loadProjectFromDirectoryInMemory({
+	_mockPlugins: {
+		"local:@inlang/icu1-importer": icu1Importer,
+	},
+	path: path.join(process.cwd(), "project.inlang"),
+	fs: fs,
+})
+
+// get bundles
+const bundles = await selectBundleNested(project.db).selectAll().execute()
+console.log(JSON.stringify(bundles, undefined, 2))
