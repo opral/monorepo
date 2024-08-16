98cdb2133b49218202df92a023161655a5e80fd5
LorisSigrist <loris@sigrist.dev>
2024-08-16 09:16:52 +0200

    Make sure that tags are treated as text

--- inlang/source-code/plugins/icu1-importer/src/settings.ts
+++ inlang/source-code/plugins/icu1-importer/src/settings.ts
@@ -1,13 +1,12 @@
-import { Type, type Static, } from "@sinclair/typebox"
+import { Type, type Static } from "@sinclair/typebox"
 
 export type PluginSettings = Static<typeof PluginSettings>
-export const PluginSettings = 
-	Type.Object({
-		messagesFolderPath:Type.String({
-			pattern: ".*",
-			examples: ["./messages", "./i18n"],
-			title: "Path to folder containing language files",
-			description:
-				"The relative path to the folder containing the language files in your repository. It must contain files named `{locale}.json`.",
-		}),
-)
+export const PluginSettings = Type.Object({
+	messagesFolderPath: Type.String({
+		pattern: ".*",
+		examples: ["./messages", "./i18n"],
+		title: "Path to folder containing language files",
+		description:
+			"The relative path to the folder containing the language files in your repository. It must contain files named `{locale}.json`.",
+	}),
+})
