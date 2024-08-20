d09c94bbe32e3f7b86aad901a410d3c5e2961b72
NiklasBuchfink <niklas.buchfink@web.de>
2024-08-20 19:03:07 +0200

    feat: display conflict with DiffBundleView

--- packages/fink2/src/routes/conflicts/Page.tsx
+++ packages/fink2/src/routes/conflicts/Page.tsx
@@ -1,11 +1,13 @@
 import { useAtom } from "jotai";
 import Layout from "../../layout.tsx";
-import { conflictsAtom, projectAtom } from "../../state.ts";
+import { bundlesNestedAtom, conflictsAtom, projectAtom } from "../../state.ts";
 import { useEffect, useState } from "react";
+import DiffBundleView from "../../components/DiffBundleView.tsx";
 
 export default function Page() {
 	const [project] = useAtom(projectAtom);
 	const [conflicts] = useAtom(conflictsAtom);
+	const [bundlesNested] = useAtom(bundlesNestedAtom);
 	const [conflictingChanges, setConflictingChanges] = useState({});
 
 	const getConflictingChanges = async () => {
@@ -21,9 +23,17 @@ export default function Page() {
 				.selectAll()
 				.where("id", "=", conflict.conflicting_change_id)
 				.executeTakeFirstOrThrow();
-
+			// get the bundleId of the change
+			const bundleWithConflict = bundlesNested.filter((bundle) =>
+				bundle.messages.find((message) =>
+					message.variants.find((variant) =>
+						variant.id === change.value.id
+					)
+				)
+			);
 			result[conflicting.id] = conflicting;
 			result[change.id] = change;
+			result[change.id].bundleId = bundleWithConflict[0].id;
 		}
 		setConflictingChanges(result);
 	};
@@ -38,22 +48,31 @@ export default function Page() {
 			{conflicts.length === 0 ? (
 				<p>No Conflicts</p>
 			) : (
-				conflicts.map((c) => {
-					const change = conflictingChanges[c.change_id];
-					const conflicting = conflictingChanges[c.conflicting_change_id];
-					return (
-						<div className="space-y-2">
-							<h2 className="font-bold text-xl">This {change?.type} change </h2>
-							<p>{JSON.stringify(change?.value, undefined, 2)}</p>
-							<h2 className="font-bold text-xl">
-								Conflicts with this {conflicting?.type} change
-							</h2>
-							<p>{JSON.stringify(conflicting?.value, undefined, 2)}</p>
-							<h2 className="font-bold text-xl">Because</h2>
-							<p>{c.reason}</p>
-						</div>
-					);
-				})
+					conflicts.map((c) => {
+						const bundleId = conflictingChanges[c.change_id]?.bundleId;
+						const change = conflictingChanges[c.change_id];
+						const conflicting = conflictingChanges[c.conflicting_change_id];
+						return (
+							<div className="space-y-2">
+								<h2 className="font-bold text-xl">This {change?.type} change </h2>
+								<pre>{JSON.stringify(change?.value, undefined, 2)}</pre>
+								<h2 className="font-bold text-xl">
+									Conflicts with this {conflicting?.type} change
+								</h2>
+								<pre>{JSON.stringify(conflicting?.value, undefined, 2)}</pre>
+								<h2 className="font-bold text-xl">Because</h2>
+								<p>{c.reason}</p>
+								{change && conflicting && (
+									<>
+										<DiffBundleView
+											bundleId={bundleId}
+											changes={[change, conflicting]}
+										/>
+									</>
+								)}
+							</div>
+						);
+					})
 			)}
 		</Layout>
 	);
