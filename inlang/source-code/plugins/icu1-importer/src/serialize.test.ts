1b84493daf13e525ccb2056adfa8e5bccf46d68a
LorisSigrist <loris@sigrist.dev>
2024-08-21 09:46:59 +0200

    Properly serialize ICU1 AST

---
+++ inlang/source-code/plugins/icu1-importer/src/serialize.test.ts
@@ -0,0 +1,20 @@
+import { describe, it, expect } from "vitest"
+import { serializeICU1Message } from "./serialize.js"
+import { parse as parseICU1 } from "@formatjs/icu-messageformat-parser"
+
+describe("serializeICU1Message", () => {
+	it("serializes text", () => {
+		const ast = parseICU1("Hello, {name}!")
+		expect(serializeICU1Message(ast)).toBe("Hello, {name}!")
+	})
+
+	it("serializes select", () => {
+		const ast = parseICU1(
+			"It's, {season, select, spring {spring} summer {summer} fall {fall} winter {winter}}!",
+			{ requiresOtherClause: false }
+		)
+		expect(serializeICU1Message(ast)).toBe(
+			"It's, {season, select, spring {spring} summer {summer} fall {fall} winter {winter}}!"
+		)
+	})
+})
