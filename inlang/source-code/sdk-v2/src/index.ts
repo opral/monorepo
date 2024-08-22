a1bf6c995b8be085e0fee5198a7ca6e60129c261
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2024-08-22 15:16:47 -0400

    chore: delete old sdk v2 package

--- inlang/source-code/sdk-v2/src/index.ts
+++
@@ -1,9 +0,0 @@
-export * from "./loadProjectOpfs.js"
-export * from "./newProjectOpfs.js"
-export * from "./types/index.js"
-export * from "./helper.js"
-export * from "./mock/index.js"
-
-// export * from "./mock/mockhelper";
-// export { newProject } from "./newProject";
-// export * from "./helper/index";
--- inlang/source-code/sdk-v2/src/mock/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./lintRports/mockCreate.js"
-export * from "./mockhelper.js"
-export * from "./plural/bundle.js"
