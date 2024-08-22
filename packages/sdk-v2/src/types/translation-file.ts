a45681ee0fbd19f4efed6dd304eec8ca13640fc0
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2024-08-22 15:16:47 -0400

    chore: delete old sdk v2 package

--- packages/sdk-v2/src/types/translation-file.ts
+++
@@ -1,9 +0,0 @@
-import { Type, type Static } from "@sinclair/typebox"
-
-export const TranslationFile = Type.Object({
-	path: Type.String(),
-	content: Type.String(),
-	pluginKey: Type.String(),
-})
-
-export type TranslationFile = Static<typeof TranslationFile>
