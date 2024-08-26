91566bf27055a6204c0634a0e87a5a2501b7ef5f
LorisSigrist <loris@sigrist.dev>
2024-08-26 09:43:46 +0200

    Add proper registry, number and datetime functions

--- inlang/source-code/paraglide/paraglide-js/src/compilerV2/registry.ts
+++ inlang/source-code/paraglide/paraglide-js/src/compilerV2/registry.ts
@@ -1,3 +1,42 @@
+export type Registry = Record<
+	string,
+	{
+		/** The Type-Restriction that should be added to the argument of the function */
+		typeRestriction?: string
+		options: Record<
+			string,
+			{
+				/** If a variable is used to provide a value for this option, it should be of this type */
+				typeRestriction?: string
+			}
+		>
+	}
+>
+
+export const REGISTRY: Registry = {
+	plural: {
+		typeRestriction: "number",
+		options: {
+			type: { typeRestriction: "string" },
+		},
+	},
+
+	number: {
+		typeRestriction: "number",
+		options: {
+			type: { typeRestriction: "string" },
+		},
+	},
+
+	datetime: {
+		typeRestriction: "Date",
+		options: {},
+	},
+}
+
+/**
+ * Creates the Registry implementation file
+ */
 export function createRegistry() {
 	return `/* eslint-disable */
 
@@ -8,5 +47,28 @@ export function createRegistry() {
  * @param {"cardinal" | "ordinal"} [options.type="cardinal"] 
  * @returns {string}
  */
-export const plural = (locale, input, options) =>  new Intl.PluralRules(locale, options).select(input)`
+export function plural(locale, input, options) { 
+	return new Intl.PluralRules(locale, options).select(input)
+}
+
+/**
+ * @param {import("./runtime.js").AvailableLanguageTag} locale
+ * @param {number} input
+ * @param {object} [options]
+ * @returns {string}
+ */
+export function number(locale, input, options) {
+	return new Intl.NumberFormat(locale, options).format(input)
+}
+
+/**
+ * @param {import("./runtime.js").AvailableLanguageTag} locale
+ * @param {Date} input
+ * @param {object} [options]
+ * @returns {string}
+ */
+export function datetime(locale, input, options) {
+	return new Intl.DateTimeFormat(locale, options).format(input)
+}
+`
 }
