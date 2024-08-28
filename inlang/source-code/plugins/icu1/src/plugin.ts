c1d985c90fdcc5c4e1be4567226e9742e63af499
LorisSigrist <loris@sigrist.dev>
2024-08-28 11:26:06 +0200

    Import from `.experimental.json`

--- inlang/source-code/plugins/icu1/src/plugin.ts
+++ inlang/source-code/plugins/icu1/src/plugin.ts
@@ -12,7 +12,9 @@ export const plugin = {
 		const files: ResourceFile[] = []
 		const pathPattern =
 			settings["plugin.inlang.icu-messageformat-1"].pathPattern ??
-			settings["plugin.inlang.messageFormat"]?.pathPattern?.replace("{languageTag}", "{locale}")
+			settings["plugin.inlang.messageFormat"]?.pathPattern
+				?.replace("{languageTag}", "{locale}")
+				.replace(".json", ".experimental.json")
 
 		for (const locale of settings.locales) {
 			const path = pathPattern.replace("{locale}", locale)
