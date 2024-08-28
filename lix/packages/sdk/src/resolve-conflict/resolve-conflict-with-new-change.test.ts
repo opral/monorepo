c0997f3886572799f2f2992c0d834f9082387f8f
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2024-08-28 14:24:06 -0400

    refactor: break up `resolveConflict` into multiple functions
    closes https://github.com/opral/inlang-sdk/issues/149

--- lix/packages/sdk/src/resolve-conflict/resolve-conflict-with-new-change.test.ts
+++ lix/packages/sdk/src/resolve-conflict/resolve-conflict-with-new-change.test.ts
@@ -3,38 +3,34 @@
 import { test, expect, vi } from "vitest";
 import { openLixInMemory } from "../open/openLixInMemory.js";
 import { newLixFile } from "../newLix.js";
-import { resolveConflict } from "./resolve-conflict.js";
-import type { Change } from "../database/schema.js";
+import { resolveConflictWithNewChange } from "./resolve-conflict-with-new-change.js";
+import type { NewChange } from "../database/schema.js";
 import type { LixPlugin } from "../plugin.js";
 import {
+	ChangeAlreadyExistsError,
 	ChangeDoesNotBelongToFileError,
-	ChangeHasBeenMutatedError,
 	ChangeNotDirectChildOfConflictError,
 } from "./errors.js";
 
-test("it should resolve a conflict by applying the change and mark the conflict as resolved with the applied change", async () => {
-	const mockChanges: Change[] = [
+test("it should throw if the to be resolved with change already exists", async () => {
+	const mockChanges: NewChange[] = [
 		{
-			id: "change1",
 			operation: "create",
 			plugin_key: "plugin1",
 			type: "mock",
 			file_id: "mock",
-			// @ts-expect-error - manual stringification
-			value: JSON.stringify({
-				key: "value1",
-			}),
+			value: {
+				id: "value1",
+			},
 		},
 		{
-			id: "change2",
 			operation: "create",
-			plugin_key: "plugin1",
 			file_id: "mock",
+			plugin_key: "plugin1",
 			type: "mock",
-			// @ts-expect-error - manual stringification
-			value: JSON.stringify({
-				key: "value2",
-			}),
+			value: {
+				id: "value2",
+			},
 		},
 	];
 
@@ -42,98 +38,7 @@ test("it should resolve a conflict by applying the change and mark the conflict 
 		key: "plugin1",
 		glob: "*",
 		applyChanges: vi.fn().mockResolvedValue({
-			fileData: new TextEncoder().encode(
-				mockChanges[0]?.value as unknown as string,
-			),
-		}),
-		diff: {
-			file: vi.fn(),
-		},
-	};
-
-	const lix = await openLixInMemory({
-		blob: await newLixFile(),
-		providePlugins: [mockPlugin],
-	});
-
-	await lix.db
-		.insertInto("file")
-		.values({ id: "mock", path: "mock", data: new Uint8Array() })
-		.execute();
-
-	const changes = await lix.db
-		.insertInto("change")
-		.values(mockChanges)
-		.returningAll()
-		.execute();
-
-	const conflict = await lix.db
-		.insertInto("conflict")
-		.values({
-			change_id: changes[0]!.id,
-			conflicting_change_id: changes[1]!.id,
-		})
-		.returningAll()
-		.executeTakeFirstOrThrow();
-
-	await resolveConflict({
-		lix: lix,
-		conflict: conflict,
-		resolveWithChange: changes[0]!,
-	});
-
-	const resolvedConflict = await lix.db
-		.selectFrom("conflict")
-		.selectAll()
-		.where("change_id", "=", conflict.change_id)
-		.where("conflicting_change_id", "=", conflict.conflicting_change_id)
-		.executeTakeFirstOrThrow();
-
-	const fileAfterResolve = await lix.db
-		.selectFrom("file")
-		.selectAll()
-		.where("id", "=", changes[0]!.file_id)
-		.executeTakeFirstOrThrow();
-
-	const parsed = new TextDecoder().decode(fileAfterResolve.data);
-
-	expect(parsed).toBe(mockChanges[0]!.value);
-	expect(resolvedConflict.resolved_with_change_id).toBe(changes[0]!.id);
-});
-
-test("it should throw if the to be resolved with change already exists in the database but is not equal (change immutability is violated)", async () => {
-	const mockChanges: Change[] = [
-		{
-			id: "change1",
-			operation: "create",
-			plugin_key: "plugin1",
-			type: "mock",
-			file_id: "mock",
-			// @ts-expect-error - manual stringification
-			value: JSON.stringify({
-				key: "value1",
-			}),
-		},
-		{
-			id: "change2",
-			operation: "create",
-			file_id: "mock",
-			plugin_key: "plugin1",
-			type: "mock",
-			// @ts-expect-error - manual stringification
-			value: JSON.stringify({
-				key: "value2",
-			}),
-		},
-	];
-
-	const mockPlugin: LixPlugin = {
-		key: "plugin1",
-		glob: "*",
-		applyChanges: vi.fn().mockResolvedValue({
-			fileData: new TextEncoder().encode(
-				mockChanges[0]?.value as unknown as string,
-			),
+			fileData: new TextEncoder().encode(JSON.stringify(mockChanges[0]?.value)),
 		}),
 		diff: {
 			file: vi.fn(),
@@ -166,44 +71,37 @@ test("it should throw if the to be resolved with change already exists in the da
 		.executeTakeFirstOrThrow();
 
 	await expect(
-		resolveConflict({
+		resolveConflictWithNewChange({
 			lix: lix,
 			conflict: conflict,
-			resolveWithChange: {
+			newChange: {
 				...changes[0]!,
-				// @ts-expect-error - manual stringification
-				value: JSON.stringify({
-					key: "mutated-value",
-				}),
+				parent_id: changes[0]!.id,
 			},
 		}),
-	).rejects.toThrowError(new ChangeHasBeenMutatedError());
+	).rejects.toThrowError(ChangeAlreadyExistsError);
 });
 
 // the sequence of changes will be broken otherwise
 test("resolving a conflict should throw if the to be resolved with change is not a direct child of the conflicting changes", async () => {
-	const mockChanges: Change[] = [
+	const mockChanges: NewChange[] = [
 		{
-			id: "change1",
 			operation: "create",
 			plugin_key: "plugin1",
 			type: "mock",
 			file_id: "mock",
-			// @ts-expect-error - manual stringification
-			value: JSON.stringify({
-				key: "value1",
-			}),
+			value: {
+				id: "value1",
+			},
 		},
 		{
-			id: "change2",
 			operation: "create",
 			file_id: "mock",
 			plugin_key: "plugin1",
 			type: "mock",
-			// @ts-expect-error - manual stringification
-			value: JSON.stringify({
-				key: "value2",
-			}),
+			value: {
+				id: "value2",
+			},
 		},
 	];
 
@@ -211,9 +109,7 @@ test("resolving a conflict should throw if the to be resolved with change is not
 		key: "plugin1",
 		glob: "*",
 		applyChanges: vi.fn().mockResolvedValue({
-			fileData: new TextEncoder().encode(
-				mockChanges[0]?.value as unknown as string,
-			),
+			fileData: new TextEncoder().encode(JSON.stringify(mockChanges[0]?.value)),
 		}),
 		diff: {
 			file: vi.fn(),
@@ -246,49 +142,43 @@ test("resolving a conflict should throw if the to be resolved with change is not
 		.executeTakeFirstOrThrow();
 
 	await expect(
-		resolveConflict({
+		resolveConflictWithNewChange({
 			lix: lix,
 			conflict: conflict,
-			resolveWithChange: {
-				id: "new-change-3",
+			newChange: {
 				operation: "create",
 				file_id: "mock",
 				parent_id: undefined,
 				plugin_key: "plugin1",
 				type: "mock",
-				// @ts-expect-error - manual stringification
-				value: JSON.stringify({
-					key: "value3",
-				}),
+				value: {
+					id: "value3",
+				},
 			},
 		}),
-	).rejects.toThrowError(new ChangeNotDirectChildOfConflictError());
+	).rejects.toThrowError(ChangeNotDirectChildOfConflictError);
 });
 
 // the sequence of changes will be broken otherwise
 test("resolving a conflict should throw if the change to resolve with does not belong to the same file as the conflicting changes", async () => {
-	const mockChanges: Change[] = [
+	const mockChanges: NewChange[] = [
 		{
-			id: "change1",
 			operation: "create",
 			plugin_key: "plugin1",
 			type: "mock",
 			file_id: "mock",
-			// @ts-expect-error - manual stringification
-			value: JSON.stringify({
-				key: "value1",
-			}),
+			value: {
+				id: "value1",
+			},
 		},
 		{
-			id: "change2",
 			operation: "create",
 			file_id: "mock",
 			plugin_key: "plugin1",
 			type: "mock",
-			// @ts-expect-error - manual stringification
-			value: JSON.stringify({
-				key: "value2",
-			}),
+			value: {
+				id: "value2",
+			},
 		},
 	];
 
@@ -296,9 +186,7 @@ test("resolving a conflict should throw if the change to resolve with does not b
 		key: "plugin1",
 		glob: "*",
 		applyChanges: vi.fn().mockResolvedValue({
-			fileData: new TextEncoder().encode(
-				mockChanges[0]?.value as unknown as string,
-			),
+			fileData: new TextEncoder().encode(JSON.stringify(mockChanges[0]?.value)),
 		}),
 		diff: {
 			file: vi.fn(),
@@ -331,48 +219,42 @@ test("resolving a conflict should throw if the change to resolve with does not b
 		.executeTakeFirstOrThrow();
 
 	await expect(
-		resolveConflict({
+		resolveConflictWithNewChange({
 			lix: lix,
 			conflict: conflict,
-			resolveWithChange: {
-				id: "new-change-3",
+			newChange: {
 				operation: "create",
 				file_id: "other-mock-file",
 				parent_id: changes[0]!.id,
 				plugin_key: "plugin1",
 				type: "mock",
-				// @ts-expect-error - manual stringification
-				value: JSON.stringify({
-					key: "value3",
-				}),
+				value: {
+					id: "value3",
+				},
 			},
 		}),
-	).rejects.toThrowError(new ChangeDoesNotBelongToFileError());
+	).rejects.toThrowError(ChangeDoesNotBelongToFileError);
 });
 
-test("resolving a conflict with a new change (likely the result of a merge resolution) should insert the change and mark the conflict as resolved with the new change", async () => {
-	const mockChanges: Change[] = [
+test("resolving a conflict with a new change should insert the change and mark the conflict as resolved with the new change", async () => {
+	const mockChanges: NewChange[] = [
 		{
-			id: "change1",
 			operation: "create",
 			plugin_key: "plugin1",
 			type: "mock",
 			file_id: "mock",
-			// @ts-expect-error - manual stringification
-			value: JSON.stringify({
-				key: "value1",
-			}),
+			value: {
+				id: "value1",
+			},
 		},
 		{
-			id: "change2",
 			operation: "create",
 			file_id: "mock",
 			plugin_key: "plugin1",
 			type: "mock",
-			// @ts-expect-error - manual stringification
-			value: JSON.stringify({
-				key: "value2",
-			}),
+			value: {
+				id: "value2",
+			},
 		},
 	];
 
@@ -380,7 +262,7 @@ test("resolving a conflict with a new change (likely the result of a merge resol
 		key: "plugin1",
 		glob: "*",
 		applyChanges: vi.fn().mockResolvedValue({
-			fileData: new TextEncoder().encode('{"key":"value3"}'),
+			fileData: new TextEncoder().encode('{"id":"value3"}'),
 		}),
 		diff: {
 			file: vi.fn(),
@@ -412,22 +294,17 @@ test("resolving a conflict with a new change (likely the result of a merge resol
 		.returningAll()
 		.executeTakeFirstOrThrow();
 
-	await resolveConflict({
+	await resolveConflictWithNewChange({
 		lix: lix,
 		conflict: conflict,
-		resolveWithChange: {
-			id: "new-change-3",
+		newChange: {
 			operation: "create",
 			file_id: "mock",
 			parent_id: changes[0]!.id,
 			plugin_key: "plugin1",
 			type: "mock",
-			meta: {},
-			author: undefined,
-			commit_id: "mock",
 			value: {
-				id: "mock",
-				key: "value3",
+				id: "value3",
 			},
 		},
 	});
@@ -455,10 +332,9 @@ test("resolving a conflict with a new change (likely the result of a merge resol
 		changesAfterResolve[2]?.id,
 	);
 	expect(changesAfterResolve[2]!.value).toStrictEqual({
-		id: "mock",
-		key: "value3",
+		id: "value3",
 	});
 	expect(new TextDecoder().decode(fileAfterResolve.data)).toBe(
-		'{"key":"value3"}',
+		'{"id":"value3"}',
 	);
 });
