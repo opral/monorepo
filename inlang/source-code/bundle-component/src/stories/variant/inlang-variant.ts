d162d6a4ba9acc6a8dc70a19b2bd6425e546026e
Nils Jacobsen <nils.jacobsen98@gmail.com>
2024-08-30 11:51:45 +0200

    feat: improve edit variant actions

--- inlang/source-code/bundle-component/src/stories/variant/inlang-variant.ts
+++ inlang/source-code/bundle-component/src/stories/variant/inlang-variant.ts
@@ -93,25 +93,6 @@ export default class InlangVariant extends LitElement {
 				background-color: var(--sl-input-background-color-hover);
 				border: 1px solid var(--sl-input-border-color-hover);
 			}
-			.dynamic-actions {
-				display: flex;
-				align-items: center;
-				gap: 4px;
-				z-index: 3;
-			}
-			.hide-dynamic-actions {
-				display: none;
-			}
-			.variant:hover .dynamic-actions {
-				display: flex;
-			}
-			.dropdown-open.dynamic-actions {
-				display: flex;
-			}
-			sl-tooltip::part(base) {
-				background-color: var(--sl-tooltip-background-color);
-				color: var(--sl-tooltip-color);
-			}
 			.history-button {
 				position: relative;
 				z-index: 0;
@@ -121,18 +102,6 @@ export default class InlangVariant extends LitElement {
 
 	@property()
 	variant: Variant | undefined
-
-	private _delete = () => {
-		if (this.variant) {
-			this.dispatchEvent(
-				createChangeEvent({
-					type: "Variant",
-					operation: "delete",
-					newData: this.variant,
-				})
-			)
-		}
-	}
 
 	private _updateMatch = (selectorName: string, value: string) => {
 		//TODO improve this function
@@ -188,32 +157,7 @@ export default class InlangVariant extends LitElement {
 						: undefined}
 					<slot name="pattern-editor" class="pattern-editor"></slot>
 					<div class="actions">
-						<div class="dynamic-actions hide-dynamic-actions">
-							<slot name="variant-action"></slot>
-							${this.variant.pattern
-								? html`<sl-tooltip content="Delete"
-										><sl-button size="small" @click=${() => this._delete()}
-											><svg
-												xmlns="http://www.w3.org/2000/svg"
-												width="18px"
-												height="18px"
-												viewBox="0 0 24 24"
-												slot="prefix"
-												style="margin-right: -2px; margin-left: -2px"
-											>
-												<g fill="none">
-													<path
-														d="M24 0v24H0V0zM12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035q-.016-.005-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427q-.004-.016-.017-.018m.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093q.019.005.029-.008l.004-.014l-.034-.614q-.005-.019-.02-.022m-.715.002a.02.02 0 0 0-.027.006l-.006.014l-.034.614q.001.018.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01z"
-													/>
-													<path
-														fill="currentColor"
-														d="M20 5a1 1 0 1 1 0 2h-1l-.003.071l-.933 13.071A2 2 0 0 1 16.069 22H7.93a2 2 0 0 1-1.995-1.858l-.933-13.07L5 7H4a1 1 0 0 1 0-2zm-3.003 2H7.003l.928 13h8.138zM14 2a1 1 0 1 1 0 2h-4a1 1 0 0 1 0-2z"
-													/>
-												</g></svg></sl-button
-								  ></sl-tooltip>`
-								: ``}
-						</div>
-						<slot name="edit-status"></slot>
+						<slot name="variant-action"></slot>
 					</div>
 			  </div>`
 			: undefined
--- inlang/source-code/fink2/src/components/InlangBundle.tsx
+++ inlang/source-code/fink2/src/components/InlangBundle.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import { createComponent } from "@lit/react";
 
 import { InlangBundle as LitInlangBundle } from "@inlang/bundle-component";
@@ -6,7 +7,7 @@ import { InlangVariant as LitInlangVariant } from "@inlang/bundle-component";
 import { InlangPatternEditor as LitInlangPatternEditor } from "@inlang/bundle-component";
 import { InlangAddSelector as LitInlangAddSelector } from "@inlang/bundle-component";
 
-import React from "react";
+import React, { useState } from "react";
 import { useAtom } from "jotai";
 import { pendingChangesAtom, projectAtom, settingsAtom } from "../state.ts";
 import {
@@ -15,9 +16,19 @@ import {
 	createVariant,
 	Message,
 	MessageNested,
+	ProjectSettings,
 	Variant,
 } from "@inlang/sdk2";
 import queryHelper from "../helper/queryHelper.ts";
+import {
+	SlDialog,
+	SlDropdown,
+	SlMenu,
+	SlMenuItem,
+	SlTooltip,
+} from "@shoelace-style/shoelace/dist/react";
+import clsx from "clsx";
+import VariantHistory from "./VariantHistory.tsx";
 
 const ReactInlangBundle = createComponent({
 	tagName: "inlang-bundle",
@@ -44,12 +55,20 @@ const ReactInlangPatternEditor = createComponent({
 	tagName: "inlang-pattern-editor",
 	elementClass: LitInlangPatternEditor,
 	react: React,
+	events: {
+		onPatternEditorFocus: "pattern-editor-focus",
+		onPatternEditorBlur: "pattern-editor-blur",
+	},
 });
 
 const ReactInlangAddSelector = createComponent({
 	tagName: "inlang-add-selector",
 	elementClass: LitInlangAddSelector,
 	react: React,
+	events: {
+		change: "change",
+		onSubmit: "submit",
+	},
 });
 
 const InlangBundle = (props: {
@@ -59,6 +78,13 @@ const InlangBundle = (props: {
 	const [project] = useAtom(projectAtom);
 	const [pendingChanges] = useAtom(pendingChangesAtom);
 	const [settings] = useAtom(settingsAtom);
+
+	const [patternEditorFocused, setPatternEditorFocused] = useState<
+		string | undefined
+	>(undefined);
+	const [patternEditorHovered, setPatternEditorHovered] = useState<
+		string | undefined
+	>(undefined);
 
 	const onMesageInsert = async (message: Message) => {
 		if (project) {
@@ -118,120 +144,258 @@ const InlangBundle = (props: {
 						messages={props.bundle.messages}
 						change={handleChange}
 					>
-						{settings.locales.map((locale) => {
-							const message = props.bundle.messages.find(
-								(message) => message.locale === locale
-							);
-							if (message) {
-								return (
-									<ReactInlangMessage
-										slot="message"
-										key={message.id}
-										message={message}
-										settings={settings}
-									>
-										{message.variants.map((variant) => {
-											const change = pendingChanges.find(
-												// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-												// @ts-ignore
-												(change) => (change.value as Variant).id === variant.id
-											);
+						{settings.locales.map(
+							(locale: ProjectSettings["locales"][number]) => {
+								const message = props.bundle.messages.find(
+									(message) => message.locale === locale
+								);
+								if (message) {
+									return (
+										<ReactInlangMessage
+											slot="message"
+											key={message.id}
+											message={message}
+											settings={settings}
+										>
+											{message.variants.map((variant) => {
+												const change = pendingChanges.find(
+													// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+													// @ts-ignore
+													(change) =>
+														(change.value as Variant).id === variant.id
+												);
 
-											return (
-												<ReactInlangVariant
-													slot="variant"
-													key={variant.id}
-													variant={variant}
-												>
-													<ReactInlangPatternEditor
-														slot="pattern-editor"
+												return (
+													<ReactInlangVariant
+														slot="variant"
+														key={variant.id}
 														variant={variant}
-													/>
-
-													{((message.selectors.length === 0 &&
-														message.variants.length <= 1) ||
-														!message.selectors) && (
-														<ReactInlangAddSelector
-															slot="variant-action"
-															message={message}
-															// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-															// @ts-ignore
-															messages={
-																props.bundle.messages as MessageNested[]
+														onMouseEnter={() =>
+															setTimeout(() => {
+																setPatternEditorHovered(variant.id);
+															}, 1)
+														}
+														onMouseLeave={() =>
+															setPatternEditorHovered(undefined)
+														}
+														className={clsx(
+															(variant.id === patternEditorFocused ||
+																variant.id === patternEditorHovered) &&
+																"relative z-20"
+														)}
+													>
+														<ReactInlangPatternEditor
+															slot="pattern-editor"
+															variant={variant}
+															onPatternEditorFocus={() =>
+																setTimeout(() => {
+																	setPatternEditorFocused(variant.id);
+																}, 1)
+															}
+															onPatternEditorBlur={() =>
+																setPatternEditorFocused(undefined)
 															}
 														/>
-													)}
-													<div
-														slot="edit-status"
-														className="pl-2 text-[14px]! text-zinc-500"
-													>
-														By Nils, 10 hours ago
-													</div>
-													{change && (
-														<div slot="edit-status" className="pl-2">
-															<div className="w-2.5 h-2.5 rounded-full bg-blue-400"></div>
-														</div>
-													)}
-												</ReactInlangVariant>
-											);
-										})}
-										<ReactInlangAddSelector
-											slot="selector-button"
-											message={message}
-											// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-											// @ts-ignore
-											messages={props.bundle.messages as MessageNested[]}
-										/>
-									</ReactInlangMessage>
-								);
-							} else {
-								const message = createMessage({
-									bundleId: props.bundle.id,
-									locale: locale,
-									text: "",
-								});
-								return (
-									<ReactInlangMessage
-										slot="message"
-										message={message}
-										key={`${props.bundle.id}-${locale}-empty`}
-									>
-										<p
-											className="min-h-[44px] bg-white hover:bg-zinc-50 flex items-center px-2 text-[14px] text-zinc-500 hover:text-zinc-950 gap-[4px] cursor-pointer w-full"
-											slot="variant"
-											onClick={async () => {
-												if (project) {
-													await queryHelper.message
-														.insert(project.db, message)
-														.execute();
+														{(variant.id === patternEditorFocused ||
+															variant.id === patternEditorHovered) && (
+															<SlTooltip
+																content="Show history"
+																slot="variant-action"
+															>
+																<div
+																	className="animate-blendIn px-3 flex items-center h-8 text-[14px]! rounded text-zinc-400 hover:bg-zinc-100 cursor-pointer"
+																	onClick={() =>
+																		props.setShowHistory(variant.id)
+																	}
+																>
+																	{change ? (
+																		<p>By You, draft</p>
+																	) : (
+																		<VariantHistory variantId={variant.id} />
+																	)}
+																</div>
+															</SlTooltip>
+														)}
 
-													await queryHelper.variant
-														.insert(
-															project.db,
-															createVariant({ messageId: message.id })
-														)
-														.execute();
-												}
-											}}
-										>
-											<svg
-												viewBox="0 0 24 24"
-												width="18"
-												height="18"
-												className="w-5 h-5"
+														{(variant.id === patternEditorFocused ||
+															variant.id === patternEditorHovered) && (
+															<SlDropdown
+																slot="variant-action"
+																className="relative z-10 animate-blendIn"
+																distance={4}
+															>
+																<div
+																	slot="trigger"
+																	className="px-2 h-8 border border-zinc-300 bg-white rounded text-zinc-600 flex items-center justify-center hover:bg-zinc-100 hover:border-zinc-400 cursor-pointer"
+																>
+																	<svg
+																		xmlns="http://www.w3.org/2000/svg"
+																		width="20"
+																		height="20"
+																		viewBox="0 0 24 24"
+																		className="-mx-[2px]"
+																	>
+																		<path
+																			fill="currentColor"
+																			d="M7 12a2 2 0 1 1-4 0a2 2 0 0 1 4 0m7 0a2 2 0 1 1-4 0a2 2 0 0 1 4 0m7 0a2 2 0 1 1-4 0a2 2 0 0 1 4 0"
+																		/>
+																	</svg>
+																</div>
+																<SlMenu className="border border-zinc-300 rounded-md">
+																	<SlMenuItem
+																		onClick={() =>
+																			props.setShowHistory(variant.id)
+																		}
+																	>
+																		History
+																	</SlMenuItem>
+																	{message.variants &&
+																		message.variants.length > 1 && (
+																			<SlMenuItem
+																				onClick={() => {
+																					onVariantDelete(variant as Variant);
+																				}}
+																			>
+																				Delete
+																			</SlMenuItem>
+																		)}
+																	<SlMenuItem
+																		onClick={() => {
+																			const dialog = document.getElementById(
+																				`selector-button-dialog-${props.bundle.id}`
+																				// @ts-expect-error
+																			) as SlDialog;
+																			if (dialog) {
+																				const child = dialog
+																					.children[0] as LitInlangAddSelector;
+																				if (child) {
+																					child.message = message;
+																				}
+																				setTimeout(() => {
+																					dialog.show();
+																				});
+																			}
+																		}}
+																	>
+																		Add Selector
+																	</SlMenuItem>
+																</SlMenu>
+															</SlDropdown>
+														)}
+
+														{change &&
+															variant.id !== patternEditorFocused &&
+															variant.id !== patternEditorHovered && (
+																<div slot="variant-action" className="pl-2">
+																	<div className="w-2.5 h-2.5 rounded-full bg-blue-400"></div>
+																</div>
+															)}
+													</ReactInlangVariant>
+												);
+											})}
+
+											<div
+												slot="selector-button"
+												className="px-2 h-8 mt-[6px] ml-[1px] border border-zinc-300 bg-white rounded text-zinc-600 flex items-center justify-center hover:bg-zinc-100 hover:border-zinc-400 cursor-pointer"
+												onClick={() => {
+													const dialog = document.getElementById(
+														`selector-button-dialog-${props.bundle.id}`
+														// @ts-expect-error
+													) as SlDialog;
+													if (dialog) {
+														const child = dialog
+															.children[0] as LitInlangAddSelector;
+														if (child) {
+															child.message = message;
+														}
+														setTimeout(() => {
+															dialog.show();
+														});
+													}
+												}}
 											>
-												<path
-													fill="currentColor"
-													d="M11 13H5v-2h6V5h2v6h6v2h-6v6h-2z"
-												></path>
-											</svg>
-											{`Add ${locale}`}
-										</p>
-									</ReactInlangMessage>
-								);
+												<svg
+													xmlns="http://www.w3.org/2000/svg"
+													width="20"
+													height="20"
+													viewBox="0 0 24 24"
+													className="-mx-[3px]"
+												>
+													<path
+														fill="currentColor"
+														d="M19 12.998h-6v6h-2v-6H5v-2h6v-6h2v6h6z"
+													/>
+												</svg>
+											</div>
+										</ReactInlangMessage>
+									);
+								} else {
+									const message = createMessage({
+										bundleId: props.bundle.id,
+										locale: locale,
+										text: "",
+									});
+									return (
+										<ReactInlangMessage
+											slot="message"
+											message={message}
+											key={`${props.bundle.id}-${locale}-empty`}
+										>
+											<p
+												className="min-h-[44px] bg-white hover:bg-zinc-50 flex items-center px-2 text-[14px] text-zinc-500 hover:text-zinc-950 gap-[4px] cursor-pointer w-full"
+												slot="variant"
+												onClick={async () => {
+													if (project) {
+														await queryHelper.message
+															.insert(project.db, message)
+															.execute();
+
+														await queryHelper.variant
+															.insert(
+																project.db,
+																createVariant({ messageId: message.id })
+															)
+															.execute();
+													}
+												}}
+											>
+												<svg
+													viewBox="0 0 24 24"
+													width="18"
+													height="18"
+													className="w-5 h-5"
+												>
+													<path
+														fill="currentColor"
+														d="M11 13H5v-2h6V5h2v6h6v2h-6v6h-2z"
+													></path>
+												</svg>
+												{`Add ${locale}`}
+											</p>
+										</ReactInlangMessage>
+									);
+								}
 							}
-						})}
+						)}
 					</ReactInlangBundle>
+					<SlDialog
+						id={`selector-button-dialog-${props.bundle.id}`}
+						className="add-selector-dialog"
+						label="Add selector"
+					>
+						<ReactInlangAddSelector
+							change={handleChange}
+							onSubmit={() => {
+								const dialog = document.getElementById(
+									`selector-button-dialog-${props.bundle.id}`
+									// @ts-expect-error
+								) as SlDialog;
+								dialog.hide();
+							}}
+							// @ts-ignore
+							messages={props.bundle.messages as MessageNested[]}
+						/>
+					</SlDialog>
 				</div>
 			)}
 		</>
