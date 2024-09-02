f19d37a49c2a1c53fc4549d413e873356ebde734
Felix Haeberle <kontakt@felixhaeberle.de>
2024-09-02 20:44:20 +0200

    try to fix extractMessage

--- inlang/source-code/ide-extension/src/commands/extractMessage.ts
+++ inlang/source-code/ide-extension/src/commands/extractMessage.ts
@@ -20,104 +20,127 @@ export const extractMessageCommand = {
 	title: "Sherlock: Extract Message",
 	register: commands.registerTextEditorCommand,
 	callback: async function (textEditor: TextEditor | undefined) {
-		const ideExtension = (await state().project.plugins.get()).find(
-			(plugin) => plugin?.meta?.["app.inlang.ideExtension"]
-		)?.meta?.["app.inlang.ideExtension"] as IdeExtensionConfig | undefined
-
-		const baseLocale = (await state().project.settings.get()).baseLocale
-
-		// guards
-		if (!ideExtension) {
-			return msg(
-				"There is no `plugin` configuration for the Visual Studio Code extension (Sherlock). One of the `modules` should expose a `plugin` which has `customApi` containing `app.inlang.ideExtension`",
-				"warn",
-				"notification"
-			)
-		}
-		if (ideExtension.extractMessageOptions === undefined) {
-			return msg(
-				"The `extractMessageOptions` are not defined in `app.inlang.ideExtension` but required to extract a message.",
-				"warn",
-				"notification"
-			)
-		}
-
-		if (textEditor === undefined) {
-			return msg(
-				"No active text editor found. Please open a file in the editor to extract a message.",
-				"warn",
-				"notification"
-			)
-		}
-
-		if (textEditor.selection.isEmpty) {
-			return msg("Please select a text to extract in your text editor.", "warn", "notification")
-		}
-
-		// create random message id as default value
-		const autoHumanId = await getSetting("extract.autoHumanId.enabled").catch(() => true)
-
-		const bundleId = await window.showInputBox({
-			title: "Enter the ID:",
-			value: autoHumanId ? generateBundleId() : "",
-			prompt:
-				autoHumanId &&
-				"Tip: It's best practice to use random names for your messages. Read this [guide](https://inlang.com/documentation/concept/message#idhuman-readable) for more information.",
-		})
-		if (bundleId === undefined) {
-			return
-		}
-
-		const messageValue = textEditor.document.getText(textEditor.selection)
-
-		const preparedExtractOptions = ideExtension.extractMessageOptions.reduce(
-			(acc, option) => {
-				const formattedSelection = isQuoted(messageValue) ? stripQuotes(messageValue) : messageValue
-				const formattedOption = option.callback({
-					bundleId,
-					selection: formattedSelection,
-				})
-
-				if (acc.includes(formattedOption)) {
-					return acc
-				}
-				return [...acc, formattedOption]
-			},
-			[] as { bundleId: string; messageReplacement: string }[]
-		)
-
-		const messageReplacements = preparedExtractOptions.map(
-			({ messageReplacement }) => messageReplacement
-		)
-
-		const preparedExtractOption = await window.showQuickPick(messageReplacements, {
-			title: "Replace highlighted text with:",
-		})
-		if (preparedExtractOption === undefined) {
-			return
-		}
-
-		const selectedExtractOption = preparedExtractOptions.find(
-			({ messageReplacement }) => messageReplacement === preparedExtractOption
-		)
-
-		if (selectedExtractOption === undefined) {
-			return msg("Couldn't find choosen extract option.", "warn", "notification")
-		}
-
-		const message = createMessage({
-			bundleId: selectedExtractOption.bundleId,
-			locale: baseLocale,
-			text: isQuoted(messageValue) ? stripQuotes(messageValue) : messageValue,
-		})
-
-		const bundle = createBundle({
-			id: bundleId,
-			messages: [message],
-		})
-
 		try {
-			await state()
+			console.log("extractMessageCommand callback triggered")
+
+			// Simulating an error to test the catch block
+			// throw new Error("Forced error for testing");
+
+			const ideExtension = (await state().project.plugins.get()).find(
+				(plugin) => plugin?.meta?.["app.inlang.ideExtension"]
+			)?.meta?.["app.inlang.ideExtension"] as IdeExtensionConfig | undefined
+
+			console.log("ideExtension:", ideExtension)
+
+			const baseLocale = (await state().project.settings.get()).baseLocale
+
+			console.log("baseLocale:", baseLocale)
+
+			if (!ideExtension) {
+				console.log("ideExtension is undefined, sending warning")
+				return msg(
+					"There is no `plugin` configuration for the Visual Studio Code extension (Sherlock). One of the `modules` should expose a `plugin` which has `customApi` containing `app.inlang.ideExtension`",
+					"warn",
+					"notification"
+				)
+			}
+
+			if (ideExtension.extractMessageOptions === undefined) {
+				console.log("extractMessageOptions are undefined, sending warning")
+				return msg(
+					"The `extractMessageOptions` are not defined in `app.inlang.ideExtension` but required to extract a message.",
+					"warn",
+					"notification"
+				)
+			}
+
+			if (textEditor === undefined) {
+				console.log("textEditor is undefined, sending warning")
+				return msg(
+					"No active text editor found. Please open a file in the editor to extract a message.",
+					"warn",
+					"notification"
+				)
+			}
+
+			if (textEditor.selection.isEmpty) {
+				console.log("No text selected, sending warning")
+				return msg("Please select a text to extract in your text editor.", "warn", "notification")
+			}
+
+			console.log("Proceeding with message extraction")
+
+			const autoHumanId = await getSetting("extract.autoHumanId.enabled").catch(() => true)
+			const bundleId = await window.showInputBox({
+				title: "Enter the ID:",
+				value: autoHumanId ? generateBundleId() : "",
+				prompt:
+					autoHumanId &&
+					"Tip: It's best practice to use random names for your messages. Read this [guide](https://inlang.com/documentation/concept/message#idhuman-readable) for more information.",
+			})
+			if (bundleId === undefined) {
+				console.log("bundleId is undefined, operation cancelled")
+				return
+			}
+
+			const messageValue = textEditor.document.getText(textEditor.selection)
+
+			const preparedExtractOptions = ideExtension.extractMessageOptions.reduce(
+				(acc, option) => {
+					const formattedSelection = isQuoted(messageValue)
+						? stripQuotes(messageValue)
+						: messageValue
+					const formattedOption = option.callback({
+						bundleId,
+						selection: formattedSelection,
+					})
+
+					if (acc.includes(formattedOption)) {
+						return acc
+					}
+					return [...acc, formattedOption]
+				},
+				[] as { bundleId: string; messageReplacement: string }[]
+			)
+
+			const messageReplacements = preparedExtractOptions.map(
+				({ messageReplacement }) => messageReplacement
+			)
+
+			const preparedExtractOption = await window.showQuickPick(messageReplacements, {
+				title: "Replace highlighted text with:",
+			})
+
+			console.log("preparedExtractOption:", preparedExtractOption)
+
+			if (preparedExtractOption === undefined) {
+				console.log("No extract option selected, sending warning")
+				return msg("Couldn't find choosen extract option.", "warn", "notification")
+			}
+
+			console.log("Proceeding to create message")
+
+			const selectedExtractOption = preparedExtractOptions.find(
+				({ messageReplacement }) => messageReplacement === preparedExtractOption
+			)
+
+			if (selectedExtractOption === undefined) {
+				return msg("Couldn't find choosen extract option.", "warn", "notification")
+			}
+
+			const message = createMessage({
+				bundleId: selectedExtractOption.bundleId,
+				locale: baseLocale,
+				text: isQuoted(messageValue) ? stripQuotes(messageValue) : messageValue,
+			})
+
+			const bundle = createBundle({
+				id: bundleId,
+				messages: [message],
+			})
+
+			console.log("Before executing transaction")
+			const transaction = await state()
 				.project.db.transaction()
 				.execute(async (trx) => {
 					await trx
@@ -141,11 +164,14 @@ export const extractMessageCommand = {
 						.execute()
 				})
 
+			console.log("transaction:", transaction)
+
+			console.log("After executing transaction")
+
 			await textEditor.edit((editor) => {
 				editor.replace(textEditor.selection, preparedExtractOption)
 			})
 
-			// Emit event to notify that a message was extracted
 			CONFIGURATION.EVENTS.ON_DID_EXTRACT_MESSAGE.fire()
 
 			telemetry.capture({
@@ -154,7 +180,8 @@ export const extractMessageCommand = {
 
 			return msg("Message extracted.")
 		} catch (e) {
-			return window.showErrorMessage(`Couldn't upsert new message with id ${bundleId}.`)
+			console.log("An error occurred:", e)
+			return window.showErrorMessage(`Couldn't extract new message with id ${bundleId}. ${e}`)
 		}
 	},
 }
