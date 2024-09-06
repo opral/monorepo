19b3f6396f5f66412aac59eba6c5dedc435fc5a6
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2024-09-05 21:29:00 -0400

    add: caching of plugins

--- packages/sdk2/src/plugin/importPlugins.test.ts
+++ packages/sdk2/src/plugin/importPlugins.test.ts
@@ -1,6 +1,7 @@
 import { test, expect, vi } from "vitest";
 import { importPlugins } from "./importPlugins.js";
 import { PluginImportError } from "./errors.js";
+import { newLixFile, openLixInMemory } from "@lix-js/sdk";
 
 test("it should preprocess a plugin", async () => {
 	global.fetch = vi.fn().mockResolvedValue({
@@ -8,7 +9,9 @@ test("it should preprocess a plugin", async () => {
 		text: vi.fn().mockResolvedValue("export default { key: 'mock' }"),
 	});
 
+	const lix = await openLixInMemory({ blob: await newLixFile() });
 	const result = await importPlugins({
+		lix,
 		settings: {
 			baseLocale: "en",
 			locales: ["en"],
@@ -30,8 +33,10 @@ test("if a fetch fails, a plugin import error is expected", async () => {
 		ok: false,
 		statusText: "HTTP 404",
 	});
+	const lix = await openLixInMemory({ blob: await newLixFile() });
 
 	const result = await importPlugins({
+		lix,
 		settings: {
 			baseLocale: "en",
 			locales: ["en"],
@@ -45,7 +50,6 @@ test("if a fetch fails, a plugin import error is expected", async () => {
 	expect(result.errors[0]).toBeInstanceOf(PluginImportError);
 });
 
-
 test("it should filter message lint rules for legacy reasons", async () => {
 	global.fetch = vi.fn().mockResolvedValue({
 		ok: true,
@@ -54,7 +58,10 @@ test("it should filter message lint rules for legacy reasons", async () => {
 			.mockResolvedValue("export default { id: 'messageLintRule.something' }"),
 	});
 
+	const lix = await openLixInMemory({ blob: await newLixFile() });
+
 	const result = await importPlugins({
+		lix,
 		settings: {
 			baseLocale: "en",
 			locales: ["en"],
@@ -64,4 +71,37 @@ test("it should filter message lint rules for legacy reasons", async () => {
 
 	expect(result.plugins.length).toBe(0);
 	expect(result.errors.length).toBe(0);
+});
+
+// more tests are found in cache.test.ts
+test("cache should work", async () => {
+	global.fetch = vi.fn().mockResolvedValue({
+		ok: false,
+	});
+
+	const mockModulePath = "https://mock.com/module.js";
+	const mockModuleCachePath = "/cache/plugins/31i1etp0l413h";
+
+	const lix = await openLixInMemory({ blob: await newLixFile() });
+
+	await lix.db
+		.insertInto("file")
+		.values({
+			path: mockModuleCachePath,
+			data: new TextEncoder().encode("export default { key: 'mock' }"),
+		})
+		.execute();
+
+	const result = await importPlugins({
+		lix,
+		settings: {
+			baseLocale: "en",
+			locales: ["en"],
+			modules: [mockModulePath],
+		},
+	});
+
+	expect(result.errors).lengthOf(0);
+	expect(result.plugins).lengthOf(1);
+	expect(result.plugins[0]?.key).toBe("mock");
 });
