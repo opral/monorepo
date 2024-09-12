5c030cd0cae6dd066acb494ef5ec425381a5f1c0
Felix Haeberle <kontakt@felixhaeberle.de>
2024-09-12 12:36:58 +0200

    export bundleIdOrAliasIs from sdk2

--- inlang/source-code/sdk2/src/query-utilities/index.ts
+++ inlang/source-code/sdk2/src/query-utilities/index.ts
@@ -1,4 +1,5 @@
 export { selectBundleNested } from "./selectBundleNested.js";
 export { insertBundleNested } from "./insertBundleNested.js";
 export { updateBundleNested } from "./updateBundleNested.js";
+export { bundleIdOrAliasIs } from "./bundleIdOrAliasIs.js";
 export { pollQuery } from "./pollQuery.js";
