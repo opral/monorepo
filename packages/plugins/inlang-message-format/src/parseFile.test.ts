e2af81e0a10c33509910ab0f6df3f04b4afd934a
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2024-09-16 11:17:29 -0400

    work in progress

--- packages/plugins/inlang-message-format/src/parseFile.test.ts
+++ packages/plugins/inlang-message-format/src/parseFile.test.ts
@@ -1,8 +1,9 @@
 import type { MessageNested } from "@inlang/sdk2"
 import { expect, test } from "vitest"
 import { parseFile } from "./parseFile.js"
+import type { FileSchema } from "./fileSchema.js"
 
-test("it parse a file with one variant", async () => {
+test("it parses a file with one variant", async () => {
 	const mockFile = {
 		some_happy_cat: "Read more about Lix",
 	}
@@ -31,9 +32,82 @@ test("it parse a file with one variant", async () => {
 	] satisfies MessageNested[])
 })
 
-test("it parse a file with one variant which uses variables", async () => {
+test("it parses a file with one variant which uses variables", async () => {
 	const mockFile = {
 		some_happy_cat: "Used by {count} devs, {numDesigners} designers and translators",
+		multi_variant: {
+			"plural(count)=one": "You have one photo.",
+			"plural(count)=other": "You have {count} photos.",
+		},
+		multi_variant2: {
+			declaration: {
+				count: { type: "input", expression: "plural($count)" },
+				countTimesTwo: { type: "Variable", expression: "double($count)" },
+			},
+			selectors: {
+				count: "plural($count)",
+			},
+			match: {
+				"count=one": "You have one photo.",
+				"count=other": "You have {$doubleCount} photos.",
+			},
+		},
+	}
+
+	const result = parseFile({
+		locale: "en",
+		fileContent: new TextEncoder().encode(JSON.stringify(mockFile)),
+	})
+
+	expect(result).toStrictEqual([
+		{
+			id: "some_happy_cat_en",
+			bundleId: "some_happy_cat",
+			locale: "en",
+			selectors: [],
+			declarations: [],
+			variants: [
+				{
+					id: "some_happy_cat_en",
+					match: {},
+					messageId: "some_happy_cat_en",
+					pattern: [
+						{ type: "text", value: "Used by " },
+						{
+							type: "expression",
+							arg: { type: "variable", name: "count" },
+						},
+						{
+							type: "text",
+							value: " devs, ",
+						},
+						{ type: "expression", arg: { type: "variable", name: "numDesigners" } },
+						{
+							type: "text",
+							value: " designers and translators",
+						},
+					],
+				},
+			],
+		},
+	] satisfies MessageNested[])
+})
+
+test("it parses multi variants", async () => {
+	const mockFile: FileSchema = {
+		some_happy_cat: {
+			selectors: {
+				count: "plural(count)",
+			},
+			match: {
+				"count=one": "You have one photo.",
+				"count=other": "You have {count} photos.",
+			},
+		},
+		multi_selector: {
+			"plural(photoCount)=other, plural(likeCount)=one": "You have one photo and one like.",
+			"plural(photoCount)=one, plural(likeCount)=one": "You have one photo and one like.",
+		},
 	}
 
 	const result = parseFile({
