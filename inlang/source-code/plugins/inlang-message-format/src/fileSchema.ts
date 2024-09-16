4d717cc128d69011114e2ac4e866bbc68422e5bb
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2024-09-16 11:17:29 -0400

    work in progress

--- inlang/source-code/plugins/inlang-message-format/src/fileSchema.ts
+++ inlang/source-code/plugins/inlang-message-format/src/fileSchema.ts
@@ -12,26 +12,19 @@ const Messages = Type.Record(
 			"The message key. Hypens (-) and dots (.) are not allowed to increase compatibility with libraries.",
 		examples: ["helloWorld", "hello_world", "helloWorld123", "hello_world_123"],
 	}),
-	Type.String(),
+	Type.Union([Type.String(), Type.Record(Type.String(), Type.String())]),
+
 	{ additionalProperties: false }
 )
 
 /**
- * 1. The storage schema is defined as object to enable the $schema
- *    property. The $schema property enables typesafety in most IDEs.
+ * The storage schema is defined as object to enable the $schema
+ * property. The $schema property enables typesafety in most IDEs.
  *
- *
- * 2. For simplicity, the storage schema `data` property is
- *    identical to the I/O of the plugin functions.
- *
- *    Pros:
- *      - No need to transform the data (time complexity).
- *      - No need to maintain a separate data structure (space complexity).
- *      - No need for plugin authors to deal with optimizations (ecosystem complexity).
- *
- *    Cons:
- *      - No optimizations but they can be introduced in a non-breaking change manner
- *        in the future, IF REQUIRED.
+ * ! DO NOT USE THE SCHEMA TO PERFORM VALIDATION.
+ * ! The schema disallows additional properties. If an old version
+ * ! tries to parse a newer version that has new properties, the
+ * ! validation will fail.
  */
 export type FileSchema = Static<typeof FileSchema>
 export const FileSchema = Type.Union([InternalProperties, Messages])
