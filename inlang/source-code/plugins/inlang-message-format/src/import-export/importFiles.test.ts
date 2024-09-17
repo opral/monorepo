f8bc031ed6168369b78a3ce92978b4c09dd44bb5
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2024-09-17 18:49:52 -0400

    add multi variant import

--- inlang/source-code/plugins/inlang-message-format/src/import-export/importFiles.test.ts
+++ inlang/source-code/plugins/inlang-message-format/src/import-export/importFiles.test.ts
@@ -221,6 +221,108 @@ test("it assigns the correct locales to messages", async () => {
 	})
 })
 
+test("it handles multi variant messages", async () => {
+	const mockSettings: ProjectSettings = {
+		baseLocale: "en",
+		locales: ["en", "de"],
+	}
+
+	const mockEnFile = new TextEncoder().encode(
+		JSON.stringify({
+			some_happy_cat: {
+				match: {
+					"platform=android, userGender=male":
+						"{username} has to download the app on his phone from the Google Play Store.",
+					"platform=ios, userGender=female":
+						"{username} has to download the app on her iPhone from the App Store.",
+					"platform=*, userGender=*": "The person has to download the app.",
+				},
+			},
+		})
+	)
+
+	const result = await importFiles({
+		settings: mockSettings,
+		files: [
+			{
+				locale: "en",
+				content: mockEnFile,
+				path: "mock/en.json",
+			},
+		],
+	})
+
+	expect(result).toStrictEqual({
+		bundles: [
+			{
+				id: "some_happy_cat",
+				alias: {},
+				messages: [
+					{
+						id: "some_happy_cat_en",
+						bundleId: "some_happy_cat",
+						locale: "en",
+						selectors: [],
+						declarations: [],
+						variants: [
+							{
+								id: "some_happy_cat_en;platform=android,userGender=male",
+								match: {
+									platform: "android",
+									userGender: "male",
+								},
+								messageId: "some_happy_cat_en",
+								pattern: [
+									{
+										type: "expression",
+										arg: { type: "variable", name: "username" },
+									},
+									{
+										type: "text",
+										value: " has to download the app on his phone from the Google Play Store.",
+									},
+								],
+							},
+							{
+								id: "some_happy_cat_en;platform=ios,userGender=female",
+								match: {
+									platform: "ios",
+									userGender: "female",
+								},
+								messageId: "some_happy_cat_en",
+								pattern: [
+									{
+										type: "expression",
+										arg: { type: "variable", name: "username" },
+									},
+									{
+										type: "text",
+										value: " has to download the app on her iPhone from the App Store.",
+									},
+								],
+							},
+							{
+								id: "some_happy_cat_en;platform=*,userGender=*",
+								match: {
+									platform: "*",
+									userGender: "*",
+								},
+								messageId: "some_happy_cat_en",
+								pattern: [
+									{
+										type: "text",
+										value: "The person has to download the app.",
+									},
+								],
+							},
+						],
+					},
+				],
+			},
+		] satisfies BundleNested[],
+	})
+})
+
 // test.todo("it parses multi variants", async () => {
 // 	const mockFile: FileSchema = {
 // 		some_happy_cat: {
