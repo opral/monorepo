30f00c2e9c2ca044f3c6a927bbb77c0e101c59ef
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2024-09-18 17:48:45 -0400

    chore: comment out old apis until https://github.com/opral/inlang-sdk/issues/201 is resolved

--- inlang/source-code/plugins/inlang-message-format/src/plugin.ts
+++ inlang/source-code/plugins/inlang-message-format/src/plugin.ts
@@ -15,8 +15,8 @@ export const plugin: InlangPlugin<{
 	// given that most people don't have a major version
 	// pinning in their settings
 	id: pluginV2.id,
-	loadMessages: pluginV2.loadMessages,
-	saveMessages: pluginV2.saveMessages,
+	// loadMessages: pluginV2.loadMessages,
+	// saveMessages: pluginV2.saveMessages,
 	settingsSchema: PluginSettings,
 	toBeImportedFiles,
 	importFiles,
