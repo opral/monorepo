af81e392c0af920e72509903d12445eee731e041
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2024-09-18 18:55:46 -0400

    fix: machine translate

--- packages/cli/src/commands/machine/translate.ts
+++ packages/cli/src/commands/machine/translate.ts
@@ -3,15 +3,19 @@ import { Command } from "commander"
 import { rpc } from "@inlang/rpc"
 import { getInlangProject } from "../../utilities/getInlangProject.js"
 import { log, logError } from "../../utilities/log.js"
-import { selectBundleNested, type InlangProject } from "@inlang/sdk2"
-import prompts from "prompts"
+import {
+	saveProjectToDirectory,
+	selectBundleNested,
+	upsertBundleNested,
+	type InlangProject,
+} from "@inlang/sdk2"
 import { projectOption } from "../../utilities/globalFlags.js"
 import progessBar from "cli-progress"
+import fs from "node:fs/promises"
 
 export const translate = new Command()
 	.command("translate")
 	.requiredOption(projectOption.flags, projectOption.description)
-	.option("-f, --force", "Force machine translation and skip the confirmation prompt.", false)
 	.option("-q, --quiet", "don't log every tranlation.", false)
 	.option("--locale <source>", "Locales for translation.")
 	.option("--targetLocales <targets...>", "Comma separated list of target locales for translation.")
@@ -19,47 +23,32 @@ export const translate = new Command()
 	.description("Machine translate bundles.")
 	.action(async (args: { force: boolean; project: string }) => {
 		try {
-			// Prompt the user to confirm
-			if (!args.force) {
-				log.warn(
-					"Human translations are better than machine translations. \n\nWe advise to use machine translations in the build step without commiting them to the repo. By using machine translate in the build step, you avoid missing translations in production while still flagging to human translators that transaltions are missing. You can use the force flag (-f, --force) to skip this prompt warning."
-				)
-				const response = await prompts({
-					type: "confirm",
-					name: "value",
-					message: "Are you sure you want to machine translate?",
-				})
-				if (!response.value) {
-					log.warn("Aborting machine translation.")
-					return
-				}
-			}
-
 			const project = await getInlangProject({ projectPath: args.project })
 			await translateCommandAction({ project })
+			await saveProjectToDirectory({ fs, path: args.project, project })
 		} catch (error) {
 			logError(error)
 		}
 	})
 
 export async function translateCommandAction(args: { project: InlangProject }) {
+	const options = translate.opts()
+
+	const bar = options.nobar
+		? undefined
+		: new progessBar.SingleBar(
+				{
+					clearOnComplete: true,
+					format: `🤖 Machine translating bundles | {bar} | {percentage}% | {value}/{total} Bundles`,
+				},
+				progessBar.Presets.shades_grey
+		  )
 	try {
-		const options = translate.opts()
 		const settings = await args.project.settings.get()
 
 		const targetLocales: string[] = options.targetLocales
 			? options.targetLocales[0]?.split(",")
 			: settings.locales
-
-		const bar = options.nobar
-			? undefined
-			: new progessBar.SingleBar(
-					{
-						clearOnComplete: true,
-						format: `🤖 Machine translating bundles | {bar} | {percentage}% | {value}/{total} Bundles`,
-					},
-					progessBar.Presets.shades_grey
-			  )
 
 		const bundles = await selectBundleNested(args.project.db).selectAll().execute()
 
@@ -78,15 +67,21 @@ export async function translateCommandAction(args: { project: InlangProject }) {
 					})
 					.then((result) => {
 						bar?.increment()
-						if (result.error) {
-							errors.push(result.error)
-						}
 						return result
 					})
 			)
 		}
 
-		await Promise.all(promises)
+		const updatedBundles = await Promise.all(promises)
+
+		for (const bundle of updatedBundles) {
+			if (bundle.error) {
+				errors.push(bundle.error)
+				continue
+			} else if (bundle.data) {
+				await upsertBundleNested(args.project.db, bundle.data)
+			}
+		}
 
 		log.success("Machine translate complete.")
 		if (errors.length > 0) {
@@ -95,5 +90,7 @@ export async function translateCommandAction(args: { project: InlangProject }) {
 		}
 	} catch (error) {
 		logError(error)
+	} finally {
+		bar?.stop()
 	}
 }
