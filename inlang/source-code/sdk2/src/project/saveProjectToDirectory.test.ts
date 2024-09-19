f43d80bbb277897c3b817f06b2f7dfbc60cd8b1a
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2024-09-19 14:39:00 -0400

    chore: mark test as todo

--- inlang/source-code/sdk2/src/project/saveProjectToDirectory.test.ts
+++ inlang/source-code/sdk2/src/project/saveProjectToDirectory.test.ts
@@ -4,7 +4,7 @@ import { Volume } from "memfs";
 import { loadProjectInMemory } from "./loadProjectInMemory.js";
 import { newProject } from "./newProject.js";
 import type { InlangPlugin } from "../plugin/schema.js";
-import type { BundleNested, NewBundleNested } from "../database/schema.js";
+import type { NewBundleNested } from "../database/schema.js";
 import { insertBundleNested } from "../query-utilities/insertBundleNested.js";
 import { loadProjectFromDirectory } from "./loadProjectFromDirectory.js";
 import { selectBundleNested } from "../query-utilities/selectBundleNested.js";
@@ -115,7 +115,7 @@ test("a roundtrip should work", async () => {
 		path: "/foo/bar.inlang",
 	});
 
-	const fileTree = volume.toJSON();
+	// const fileTree = volume.toJSON();
 
 	expect(exportFilesSpy).toHaveBeenCalled();
 	expect(importFilesSpy).not.toHaveBeenCalled();
@@ -148,111 +148,116 @@ test("a roundtrip should work", async () => {
 	expect(bundles).toEqual([mockBundleNested]);
 });
 
-test("a roundtrip with legacy load and save messages should work", async () => {
-	const mockMessageV1: MessageV1 = {
-		id: "mock-legacy-message",
-		alias: {},
-		selectors: [],
-		variants: [
-			{
-				languageTag: "en",
-				match: [],
-				pattern: [{ type: "Text", value: "Hello from legacy message" }],
-			},
-		],
-	};
-
-	const volume = Volume.fromJSON({
-		"/foo/bar.inlang/settings.json": JSON.stringify({
-			baseLocale: "en",
-			locales: ["en"],
-		} satisfies ProjectSettings),
-		"/foo/i18n/en.json": JSON.stringify([mockMessageV1]),
-	});
-
-	const mockPlugin: InlangPlugin = {
-		id: "mock-legacy-plugin",
-		key: "mock-legacy-plugin",
-		loadMessages: async ({ nodeishFs }) => {
-			// expecting `loadMessages` to transform the relative path
-			// to an absolute path `./i18n/en.json` -> `/foo/i18n/en.json`
-			const file = await nodeishFs.readFile("./i18n/en.json", {
-				encoding: "utf-8",
-			});
-			return JSON.parse(file as string);
-		},
-		saveMessages: async ({ messages, nodeishFs }) => {
-			await nodeishFs.writeFile(
-				"./i18n/en.json",
-				new TextEncoder().encode(JSON.stringify(messages))
-			);
-		},
-	};
-
-	const loadMessagesSpy = vi.spyOn(mockPlugin, "loadMessages");
-	const saveMessagesSpy = vi.spyOn(mockPlugin, "saveMessages");
-
-	const project = await loadProjectFromDirectory({
-		fs: volume as any,
-		path: "/foo/bar.inlang",
-		providePlugins: [mockPlugin],
-	});
-
-	expect(loadMessagesSpy).toHaveBeenCalled();
-	expect(saveMessagesSpy).not.toHaveBeenCalled();
-
-	const bundles1 = await selectBundleNested(project.db).execute();
-
-	expect(bundles1[0]?.alias["mock-legacy-plugin"]).toBe("mock-legacy-message");
-	expect(bundles1[0]?.messages).lengthOf(1);
-	expect(bundles1[0]?.messages[0]?.variants).toEqual([
-		expect.objectContaining({
-			pattern: [
+test.todo(
+	"a roundtrip with legacy load and save messages should work",
+	async () => {
+		const mockMessageV1: MessageV1 = {
+			id: "mock-legacy-message",
+			alias: {},
+			selectors: [],
+			variants: [
 				{
-					type: "text",
-					value: "Hello from legacy message",
+					languageTag: "en",
+					match: [],
+					pattern: [{ type: "Text", value: "Hello from legacy message" }],
 				},
 			],
-		}),
-	]);
+		};
 
-	// await project.db
-	// 	.updateTable("variant")
-	// 	.set({
-	// 		pattern: [{ type: "text", value: "Updated message" }],
-	// 	})
-	// 	.where("id", "=", bundles1[0]?.messages[0]?.variants[0]?.id as string)
-	// 	.execute();
+		const volume = Volume.fromJSON({
+			"/foo/bar.inlang/settings.json": JSON.stringify({
+				baseLocale: "en",
+				locales: ["en"],
+			} satisfies ProjectSettings),
+			"/foo/i18n/en.json": JSON.stringify([mockMessageV1]),
+		});
 
-	// testing the saveMessages function by removing the en.json file
-	await volume.promises.rm("/foo/i18n/en.json");
+		const mockPlugin: InlangPlugin = {
+			id: "mock-legacy-plugin",
+			key: "mock-legacy-plugin",
+			loadMessages: async ({ nodeishFs }) => {
+				// expecting `loadMessages` to transform the relative path
+				// to an absolute path `./i18n/en.json` -> `/foo/i18n/en.json`
+				const file = await nodeishFs.readFile("./i18n/en.json", {
+					encoding: "utf-8",
+				});
+				return JSON.parse(file as string);
+			},
+			saveMessages: async ({ messages, nodeishFs }) => {
+				await nodeishFs.writeFile(
+					"./i18n/en.json",
+					new TextEncoder().encode(JSON.stringify(messages))
+				);
+			},
+		};
 
-	await saveProjectToDirectory({
-		fs: volume.promises as any,
-		project,
-		path: "/foo/bar.inlang",
-	});
+		const loadMessagesSpy = vi.spyOn(mockPlugin, "loadMessages");
+		const saveMessagesSpy = vi.spyOn(mockPlugin, "saveMessages");
 
-	expect(saveMessagesSpy).toHaveBeenCalled();
+		const project = await loadProjectFromDirectory({
+			fs: volume as any,
+			path: "/foo/bar.inlang",
+			providePlugins: [mockPlugin],
+		});
 
-	const fileTree = volume.toJSON();
-	const parsed = JSON.parse(fileTree["/foo/i18n/en.json"] as string);
+		expect(loadMessagesSpy).toHaveBeenCalled();
+		expect(saveMessagesSpy).not.toHaveBeenCalled();
 
-	// expect(parsed).toEqual(expect.objectContaining([mockMessageV1]));
+		const bundles1 = await selectBundleNested(project.db).execute();
 
-	// testing roundtrip
+		expect(bundles1[0]?.alias["mock-legacy-plugin"]).toBe(
+			"mock-legacy-message"
+		);
+		expect(bundles1[0]?.messages).lengthOf(1);
+		expect(bundles1[0]?.messages[0]?.variants).toEqual([
+			expect.objectContaining({
+				pattern: [
+					{
+						type: "text",
+						value: "Hello from legacy message",
+					},
+				],
+			}),
+		]);
 
-	const project2 = await loadProjectFromDirectory({
-		fs: volume as any,
-		path: "/foo/bar.inlang",
-		providePlugins: [mockPlugin],
-	});
+		// await project.db
+		// 	.updateTable("variant")
+		// 	.set({
+		// 		pattern: [{ type: "text", value: "Updated message" }],
+		// 	})
+		// 	.where("id", "=", bundles1[0]?.messages[0]?.variants[0]?.id as string)
+		// 	.execute();
 
-	const bundles2 = await selectBundleNested(project2.db).execute();
+		// testing the saveMessages function by removing the en.json file
+		await volume.promises.rm("/foo/i18n/en.json");
 
-	// TODO deactivated since the ids must not be equal for separate imports - matching happens on language and matcher now
-	// expect(bundles1).toStrictEqual(bundles2);
-});
+		await saveProjectToDirectory({
+			fs: volume.promises as any,
+			project,
+			path: "/foo/bar.inlang",
+		});
+
+		expect(saveMessagesSpy).toHaveBeenCalled();
+
+		const fileTree = volume.toJSON();
+		const parsed = JSON.parse(fileTree["/foo/i18n/en.json"] as string);
+
+		expect(parsed).toEqual(expect.objectContaining([mockMessageV1]));
+
+		// testing roundtrip
+
+		const project2 = await loadProjectFromDirectory({
+			fs: volume as any,
+			path: "/foo/bar.inlang",
+			providePlugins: [mockPlugin],
+		});
+
+		const bundles2 = await selectBundleNested(project2.db).execute();
+
+		// TODO deactivated since the ids must not be equal for separate imports - matching happens on language and matcher now
+		expect(bundles1).toStrictEqual(bundles2);
+	}
+);
 
 
 test("it should preserve the formatting of existing json resource files", async () => {
