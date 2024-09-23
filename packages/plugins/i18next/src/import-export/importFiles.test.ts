3fadc1738497832f9b2b024b51453c93c5870430
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2024-09-23 18:49:53 -0400

    add: import of files

--- packages/plugins/i18next/src/import-export/importFiles.test.ts
+++ packages/plugins/i18next/src/import-export/importFiles.test.ts
@@ -1,6 +1,7 @@
 import { describe, expect, test } from "vitest"
 import { importFiles } from "./importFiles.js"
 import { PLUGIN_KEY } from "../plugin.js"
+import type { LiteralMatch, Pattern, Variant } from "@inlang/sdk2"
 
 // https://www.i18next.com/misc/json-format#i18next-json-v4
 describe("it should be able to import the i18next JSON v4", async () => {
@@ -49,11 +50,230 @@ describe("it should be able to import the i18next JSON v4", async () => {
 
 	test("single key value", () => {
 		const bundle = result.bundles.find((bundle) => bundle.id === "key")
-		expect(bundle).toStrictEqual(expect.objectContaining({ id: "key" }))
+		expect(bundle?.messages[0]?.variants[0]?.pattern).toStrictEqual([
+			{ type: "text", value: "value" },
+		])
 	})
 
 	test("key deep", () => {
 		const bundle = result.bundles.find((bundle) => bundle.id === "keyDeep.inner")
-		expect(bundle).toStrictEqual(expect.objectContaining({ id: "keyDeep.inner" }))
+		expect(bundle?.messages[0]?.variants[0]?.pattern).toStrictEqual([
+			{ type: "text", value: "value" },
+		])
+	})
+
+	test("keyInterpolate", () => {
+		const bundle = result.bundles.find((bundle) => bundle.id === "keyInterpolate")
+
+		expect(bundle?.declarations).toStrictEqual([
+			{
+				type: "input-variable",
+				name: "value",
+			},
+		])
+
+		expect(bundle?.messages[0]?.variants[0]?.pattern).toStrictEqual([
+			{ type: "text", value: "replace this " },
+			{
+				type: "expression",
+				arg: {
+					type: "variable-reference",
+					name: "value",
+				},
+			},
+		] satisfies Pattern)
+	})
+
+	test("keyInterpolateUnescaped", () => {
+		const bundle = result.bundles.find((bundle) => bundle.id === "keyInterpolateUnescaped")
+		expect(bundle?.declarations).toStrictEqual([
+			{
+				type: "input-variable",
+				name: "- value",
+			},
+		])
+		expect(bundle?.messages[0]?.variants[0]?.pattern).toStrictEqual([
+			{ type: "text", value: "replace this " },
+			{
+				type: "expression",
+				arg: {
+					type: "variable-reference",
+					name: "- value",
+				},
+			},
+		] satisfies Pattern)
+	})
+
+	test("keyInterpolateWithFormatting", () => {
+		const bundle = result.bundles.find((bundle) => bundle.id === "keyInterpolateWithFormatting")
+		expect(bundle?.messages[0]?.variants[0]?.pattern).toStrictEqual([
+			{ type: "text", value: "replace this " },
+			{
+				type: "expression",
+				arg: {
+					type: "variable-reference",
+					name: "value",
+				},
+				annotation: {
+					type: "function-reference",
+					name: "format",
+					options: [],
+				},
+			},
+		] satisfies Pattern)
+	})
+
+	test("keyContext", () => {
+		const bundle = result.bundles.find((bundle) => bundle.id === "keyContext")
+
+		expect(bundle?.declarations).toStrictEqual([
+			{
+				type: "input-variable",
+				name: "context",
+			},
+		])
+		expect(bundle?.messages[0]?.variants[0]).toStrictEqual(
+			expect.objectContaining({
+				matches: [
+					{
+						type: "literal-match",
+						key: "context",
+						value: "male",
+					},
+				],
+				pattern: [{ type: "text", value: "the male variant" }],
+			} satisfies Partial<Variant>)
+		)
+		expect(bundle?.messages[0]?.variants[1]).toStrictEqual(
+			expect.objectContaining({
+				matches: [
+					{
+						type: "literal-match",
+						key: "context",
+						value: "female",
+					},
+				],
+				pattern: [{ type: "text", value: "the female variant" }],
+			} satisfies Partial<Variant>)
+		)
+	})
+
+	test("keyPluralSimple", () => {
+		const bundle = result.bundles.find((bundle) => bundle.id === "keyPluralSimple")
+
+		expect(bundle?.declarations).toStrictEqual(
+			expect.arrayContaining([
+				{
+					type: "input-variable",
+					name: "count",
+				},
+				expect.objectContaining({
+					type: "local-variable",
+					name: "countPlural",
+					value: {
+						type: "expression",
+						arg: {
+							type: "variable-reference",
+							name: "count",
+						},
+						annotation: {
+							type: "function-reference",
+							name: "plural",
+							options: [],
+						},
+					},
+				}),
+			])
+		)
+
+		expect(bundle?.messages[0]?.variants[0]).toStrictEqual(
+			expect.objectContaining({
+				matches: [
+					{
+						type: "literal-match",
+						key: "countPlural",
+						value: "one",
+					},
+				],
+				pattern: [{ type: "text", value: "the singular" }],
+			} satisfies Partial<Variant>)
+		)
+
+		expect(bundle?.messages[0]?.variants[1]).toStrictEqual(
+			expect.objectContaining({
+				matches: [
+					{
+						type: "literal-match",
+						key: "countPlural",
+						value: "other",
+					},
+				],
+				pattern: [{ type: "text", value: "the plural" }],
+			} satisfies Partial<Variant>)
+		)
+	})
+
+	test("keyPluralMultipleEgArabic", () => {
+		const bundle = result.bundles.find((bundle) => bundle.id === "keyPluralMultipleEgArabic")
+
+		expect(bundle?.declarations).toStrictEqual(
+			expect.arrayContaining([
+				{
+					type: "input-variable",
+					name: "count",
+				},
+				expect.objectContaining({
+					type: "local-variable",
+					name: "countPlural",
+					value: {
+						type: "expression",
+						arg: {
+							type: "variable-reference",
+							name: "count",
+						},
+						annotation: {
+							type: "function-reference",
+							name: "plural",
+							options: [],
+						},
+					},
+				}),
+			])
+		)
+
+		const matches = bundle?.messages[0]?.variants.map(
+			(variant) => (variant.matches?.[0] as LiteralMatch).value
+		)
+		expect(matches).toStrictEqual(["zero", "one", "two", "few", "many", "other"])
+		expect(bundle?.messages[0]?.variants[0]?.pattern).toStrictEqual([
+			{ type: "text", value: "the plural form 0" },
+		])
+		expect(bundle?.messages[0]?.variants[1]?.pattern).toStrictEqual([
+			{ type: "text", value: "the plural form 1" },
+		])
+		expect(bundle?.messages[0]?.variants[2]?.pattern).toStrictEqual([
+			{ type: "text", value: "the plural form 2" },
+		])
+		expect(bundle?.messages[0]?.variants[3]?.pattern).toStrictEqual([
+			{ type: "text", value: "the plural form 3" },
+		])
+		expect(bundle?.messages[0]?.variants[4]?.pattern).toStrictEqual([
+			{ type: "text", value: "the plural form 4" },
+		])
+		expect(bundle?.messages[0]?.variants[5]?.pattern).toStrictEqual([
+			{ type: "text", value: "the plural form 5" },
+		])
+	})
+
+	test("keyWithObjectValue", () => {
+		const valueA = result.bundles.find((bundle) => bundle.id === "keyWithObjectValue.valueA")
+		const valueB = result.bundles.find((bundle) => bundle.id === "keyWithObjectValue.valueB")
+
+		expect(valueA?.messages[0]?.variants[0]?.pattern).toStrictEqual([
+			{ type: "text", value: "return this with valueB" },
+		] satisfies Pattern)
+		expect(valueB?.messages[0]?.variants[0]?.pattern).toStrictEqual([
+			{ type: "text", value: "more text" },
+		] satisfies Pattern)
 	})
 })
--- packages/plugins/i18next/src/import-export/importFiles.ts
+++ packages/plugins/i18next/src/import-export/importFiles.ts
@@ -1,5 +1,12 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
-import type { Bundle, BundleNested, Message, Variant } from "@inlang/sdk2"
+import type {
+	Bundle,
+	BundleNested,
+	Message,
+	Pattern,
+	VariableReference,
+	Variant,
+} from "@inlang/sdk2"
 import { type plugin } from "../plugin.js"
 import { flatten } from "flat"
 
@@ -20,7 +27,7 @@ export const importFiles: NonNullable<(typeof plugin)["importFiles"]> = async ({
 	const uniqueBundleIds = [...new Set(bundles.map((bundle) => bundle.id))]
 	const uniqueBundles: Bundle[] = uniqueBundleIds.map((id) => {
 		const _bundles = bundles.filter((bundle) => bundle.id === id)
-		const declarations = [...new Set(_bundles.flatMap((bundle) => bundle.declarations))]
+		const declarations = removeDuplicates(_bundles.flatMap((bundle) => bundle.declarations))
 		return { id, declarations }
 	})
 
@@ -70,34 +77,106 @@ function parseMessage(args: {
 	locale: string
 	resource: Record<string, any>
 }): { bundle: Bundle; message: Message; variant: Variant } {
+	const pattern = parsePattern(args.value)
+
+	// i18next suffixes keys with context or plurals
+	// "friend_female_one" -> "friend"
+	const keyWithoutContextOrPlurals = args.key.split("_")[0]!
+
 	const bundle: Bundle = {
-		id: args.key,
-		declarations: [],
+		id: keyWithoutContextOrPlurals,
+		declarations: pattern.variableReferences.map((variableReference) => ({
+			type: "input-variable",
+			name: variableReference.name,
+		})),
 	}
 
 	const message: Message = {
-		id: args.key,
-		bundleId: args.key,
+		id: keyWithoutContextOrPlurals,
+		bundleId: keyWithoutContextOrPlurals,
 		selectors: [],
 		locale: args.locale,
 	}
 
 	const variant: Variant = {
-		id: args.key,
-		messageId: args.key,
+		id: keyWithoutContextOrPlurals,
+		messageId: keyWithoutContextOrPlurals,
 		matches: [],
-		pattern: parsePattern(args.value),
+		pattern: pattern.result,
 	}
 
-	// plural, see https://www.i18next.com/misc/json-format#i18next-json-v4
-	if (
+	// plurals, see https://www.i18next.com/misc/json-format#i18next-json-v4
+	const hasPlurals =
 		args.key.endsWith("_zero") ||
 		args.key.endsWith("_one") ||
 		args.key.endsWith("_two") ||
 		args.key.endsWith("_few") ||
 		args.key.endsWith("_many") ||
 		args.key.endsWith("_other")
-	) {
+	// context is used see https://www.i18next.com/translation-function/context
+	const hasContext = hasPlurals
+		? args.key.split("_").length === 3
+		: args.key.split("_").length === 2
+
+	if (hasContext && hasPlurals === false) {
+		// "friend_male" -> ["friend", "male"]
+		const [, context] = args.key.split("_")
+		bundle.declarations.push({
+			type: "input-variable",
+			name: "context",
+		})
+		variant.matches = [
+			{
+				type: "literal-match",
+				// i18next always uses "context" as the key
+				key: "context",
+				value: context!,
+			},
+		]
+	} else if (hasContext && hasPlurals) {
+		// "friend_female_one": "A girlfriend" -> ["friend", "female", "one"]
+		const [, context, plural] = args.key.split("_")
+		bundle.declarations.push({
+			type: "input-variable",
+			name: "context",
+		})
+		bundle.declarations.push({
+			type: "input-variable",
+			name: "count",
+		})
+		bundle.declarations.push({
+			type: "local-variable",
+			name: "countPlural",
+			value: {
+				type: "expression",
+				arg: {
+					type: "variable-reference",
+					name: "count",
+				},
+				annotation: {
+					type: "function-reference",
+					name: "plural",
+					options: [],
+				},
+			},
+		})
+		variant.matches = [
+			{
+				type: "literal-match",
+				key: "context",
+				value: context!,
+			},
+			{
+				type: "literal-match",
+				// i18next only allows matching against a count variable.
+				// suffixing plural here because the inlang sdk v2 purposefully
+				// did not allow using a variable with a function like `plural`
+				// without declaring a new variable
+				key: "countPlural",
+				value: plural!,
+			},
+		]
+	} else if (hasPlurals) {
 		variant.matches = [
 			{
 				// i18next only allows matching against a count variable
@@ -109,6 +188,10 @@ function parseMessage(args: {
 				value: args.key.split("_").at(-1)!,
 			},
 		]
+		bundle.declarations.push({
+			type: "input-variable",
+			name: "count",
+		})
 		bundle.declarations.push({
 			type: "local-variable",
 			name: "countPlural",
@@ -127,11 +210,17 @@ function parseMessage(args: {
 		})
 	}
 
+	bundle.declarations = removeDuplicates(bundle.declarations)
+
 	return { bundle, message, variant }
 }
 
-function parsePattern(value: string | string[]): Variant["pattern"] {
-	const pattern: Variant["pattern"] = []
+function parsePattern(value: string | string[]): {
+	variableReferences: VariableReference[]
+	result: Pattern
+} {
+	const result: Variant["pattern"] = []
+	const variableReferences: VariableReference[] = []
 
 	if (Array.isArray(value)) {
 		// i18next allows arrays as values
@@ -145,19 +234,44 @@ function parsePattern(value: string | string[]): Variant["pattern"] {
 
 	// splits a pattern like "Hello {{name}}!" into an array of parts
 	// "hello {{name}}, how are you?" -> ["hello ", "{{name}}", ", how are you?"]
-	const parts = value.split(/(\{{.*?\}})/).filter((part) => part !== "")
+	const parts = value.split(/({{.*?}})/).filter((part) => part !== "")
 
 	for (const part of parts) {
 		// it's text
-		if ((part.startsWith("{{") && part.endsWith("}")) === false) {
-			pattern.push({ type: "text", value: part })
+		if ((part.startsWith("{{") && part.endsWith("}}")) === false) {
+			result.push({ type: "text", value: part })
 		}
-		// it's an expression (only supporting variables for now)
+		// it's an expression
 		else {
-			const variableName = part.slice(1, -1)
-			pattern.push({ type: "expression", arg: { type: "variable-reference", name: variableName } })
+			// i18next allows for annotations like `{{name, uppercase}}`
+			const subparts = part.slice(2, -2).split(",")
+
+			const arg = subparts[0]?.trim()
+			const annotation = subparts[1]?.trim()
+
+			if (arg === undefined) {
+				throw new Error("Expected an argument in the expression but received undefined.")
+			}
+
+			const variableReference: VariableReference = { type: "variable-reference", name: arg }
+
+			variableReferences.push(variableReference)
+
+			result.push({
+				type: "expression",
+				arg: variableReference,
+				...(annotation && {
+					annotation: {
+						type: "function-reference",
+						name: annotation,
+						options: [],
+					},
+				}),
+			})
 		}
 	}
 
-	return pattern
+	return { variableReferences, result }
 }
+const removeDuplicates = <T extends any[]>(arr: T) =>
+	[...new Set(arr.map((item) => JSON.stringify(item)))].map((item) => JSON.parse(item))
