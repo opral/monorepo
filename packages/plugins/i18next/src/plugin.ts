ceaf08b1871a6d2bdc4b9fedef0f7ab25e1c2892
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2024-09-25 10:28:28 -0400

    fix: add api to plugin

--- packages/plugins/i18next/src/plugin.ts
+++ packages/plugins/i18next/src/plugin.ts
@@ -2,6 +2,9 @@ import type { InlangPlugin } from "@inlang/sdk2"
 import type { PluginSettings } from "./settings.js"
 import { config } from "./ideExtension/config.js"
 import { pluginV4 } from "./legacy/plugin.v4.js"
+import { importFiles } from "./import-export/importFiles.js"
+import { exportFiles } from "./import-export/exportFiles.js"
+import { toBeImportedFiles } from "./import-export/toBeImportedFiles.js"
 
 export const PLUGIN_KEY = "plugin.inlang.i18next"
 
@@ -13,6 +16,9 @@ export const plugin: InlangPlugin<{
 	addCustomApi: pluginV4.addCustomApi,
 	loadMessages: pluginV4.loadMessages,
 	saveMessages: pluginV4.saveMessages,
+	importFiles,
+	exportFiles,
+	toBeImportedFiles,
 	meta: {
 		"app.inlang.ide-extension": config,
 	},
