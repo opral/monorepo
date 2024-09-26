2024be9e7f91840a5c9bbccfbf13c9f9e430ad9d
NiklasBuchfink <niklas.buchfink@web.de>
2024-09-26 12:39:04 +0200

    fix: input locale name during import

--- inlang/source-code/fink2/src/components/layout/ProjectMenu.tsx
+++ inlang/source-code/fink2/src/components/layout/ProjectMenu.tsx
@@ -1,8 +1,9 @@
 import { SlDropdown, SlMenu, SlMenuItem, SlBadge } from "@shoelace-style/shoelace/dist/react";
-import { exportToJSON, handleDownload, handleMerge, handleOpenProject, importFromJSON } from "../../helper/utils.ts";
+import { exportToJSON, handleDownload, handleMerge, handleOpenProject, selectImportFile } from "../../helper/utils.ts";
 import { forceReloadProjectAtom, projectAtom, selectedProjectPathAtom } from "../../state.ts";
 import { useAtom } from "jotai";
 import CreateProjectDialog from "./CreateProjectDialog.tsx";
+import ImportLocaleDialog from "./ImportLocaleDialog.tsx";
 import { useState } from "react";
 
 const ProjectMenu = () => {
@@ -10,15 +11,8 @@ const ProjectMenu = () => {
   const [selectedProjectPath, setSelectedProjectPath] = useAtom(selectedProjectPathAtom);
   const [, setForceReloadProject] = useAtom(forceReloadProjectAtom);
   const [showNewProjectDialog, setShowNewProjectDialog] = useState(false);
-
-  const menuPoints = [
-    { value: "new", label: "New project", function: () => setShowNewProjectDialog(true) },
-    { value: "open", label: "Open project", function: () => handleOpenProject(setSelectedProjectPath) },
-    { value: "download", label: "Download file", function: () => handleDownload(project, selectedProjectPath) },
-    { value: "merge", label: "Merge file", function: () => handleMerge(project, selectedProjectPath, setForceReloadProject) },
-    { value: "import", label: "Import from JSON", pill: "i18next", function: () => importFromJSON(project) },
-    { value: "export", label: "Export to JSON", pill: "i18next", function: () => exportToJSON(project) },
-  ]
+  const [importedFile, setImportedFile] = useState<File | undefined>(undefined);
+  const [importPlugin, setImportPlugin] = useState<"plugin.inlang.i18next" | "plugin.inlang.messageFormat" | undefined>(undefined);
 
   return (
     <div className="flex items-center gap-1">
@@ -40,26 +34,53 @@ const ProjectMenu = () => {
           </svg>
         </div>
         <SlMenu>
-          {menuPoints.map((point) => (
-            <>
-              <SlMenuItem key={point.value} value={point.value} onClick={point.function}>
-                {point.label}
-                {point.pill && (
-                  <SlBadge pill variant="neutral" className="ml-2">
-                    {point.pill}
-                  </SlBadge>
-                )}
+          <SlMenuItem value="new" onClick={() => setShowNewProjectDialog(true)}>New project</SlMenuItem>
+          <SlMenuItem value="open" onClick={() => handleOpenProject(setSelectedProjectPath)}>Open project</SlMenuItem>
+          <div className="w-full border-b border-zinc-200 my-1" />
+          <SlMenuItem value="download" onClick={() => handleDownload(project, selectedProjectPath)}>Download file</SlMenuItem>
+          <SlMenuItem value="merge" onClick={() => handleMerge(project, selectedProjectPath, setForceReloadProject)}>Merge file</SlMenuItem>
+          <div className="w-full border-b border-zinc-200 my-1" />
+          <SlMenuItem>
+            Import
+            <SlMenu slot="submenu">
+              <SlMenuItem value="import JSON" onClick={async () => {
+                setImportedFile(await selectImportFile(".json"))
+                setImportPlugin("plugin.inlang.i18next")
+              }}>
+                JSON
+                <SlBadge pill variant="neutral" className="ml-2">i18next</SlBadge>
               </SlMenuItem>
-              {(point.value === "merge" || point.value === "open") && (
-                <div className="w-full border-b border-zinc-200 my-1" />
-              )}
-            </>
-          ))}
+              {/* <SlMenuItem value="import JSON" onClick={async () => {
+                setImportedFile(await selectImportFile(".json"))
+                setImportPlugin("plugin.inlang.messageFormat")
+              }}>
+                Message format
+              </SlMenuItem> */}
+            </SlMenu>
+          </SlMenuItem>
+          <SlMenuItem>
+            Export
+            <SlMenu slot="submenu">
+              <SlMenuItem value="export JSON" onClick={() => exportToJSON(project)}>
+                JSON
+                <SlBadge pill variant="neutral" className="ml-2">i18next</SlBadge>
+              </SlMenuItem>
+              {/* <SlMenuItem value="export message format" onClick={() => exportToJSON(project)}>
+                Message format
+              </SlMenuItem> */}
+            </SlMenu>
+          </SlMenuItem>
         </SlMenu>
       </SlDropdown>
       <CreateProjectDialog
         showNewProjectDialog={showNewProjectDialog}
         setShowNewProjectDialog={setShowNewProjectDialog}
+      />
+      <ImportLocaleDialog
+        importedFile={importedFile}
+        setImportedFile={setImportedFile}
+        importPlugin={importPlugin}
+        setImportPlugin={setImportPlugin}
       />
     </div>
   )
