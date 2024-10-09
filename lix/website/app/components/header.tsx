e0a3ac3a55136ee31726e59471344d3e0851a6ca
NiklasBuchfink <niklas.buchfink@web.de>
2024-10-09 16:37:36 +0200

    feat: add header

--- lix/website/app/components/header.tsx
+++ lix/website/app/components/header.tsx
@@ -1,12 +1,48 @@
+import IconDiscord from "./icons/discord";
+import IconGitHub from "./icons/github";
 import IconLix from "./icons/lix";
+import IconX from "./icons/x";
+
+const socialLinks = [
+  {
+    href: "https://github.com/opral/monorepo",
+    icon: <IconGitHub />,
+  },
+  {
+    href: "https://discord.gg/gdMPPWy57R",
+    icon: <IconDiscord />,
+  },
+  {
+    href: "https://x.com/lixCCS",
+    icon: <IconX />,
+  },
+];
 
 const Header = () => {
   return (
-    <header>
-      <div className="w-full max-w-5xl px-4 mx-auto my-2 flex items-center justify-between gap-4">
+    <header className="w-full max-w-5xl px-4 py-3 mx-auto flex items-center justify-between gap-4">
+      <div className="flex items-center gap-4">
         <IconLix />
+        <a
+          className="px-2 py-1 font-medium text-zinc-500 hover:text-cyan-600 bg-white"
+          href="https://opral.substack.com">
+          Blog
+        </a>
       </div>
-    </header>
+      <div className="flex items-center gap-4">
+        {socialLinks.map((socialLink, index) => (
+          <a
+            key={index}
+            className="p-2 text-black hover:text-cyan-600"
+            href={socialLink.href}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {socialLink.icon}
+          </a>
+        ))}
+      </div>
+    </header >
   );
 }
 
--- lix/website/app/routes/_index.tsx
+++ lix/website/app/routes/_index.tsx
@@ -2,64 +2,12 @@ import type { MetaFunction } from "@remix-run/node";
 import Footer from "../components/footer";
 import Header from "../components/header";
 
-type UseCase = {
-  title: string
-  year: number
-  link: string
-}
-
-type BlogPost = {
-  title: string
-  link: string
-}
-
 export const meta: MetaFunction = () => {
   return [
     { title: "Lix - Change Control System" },
     { name: "description", content: "The lix change control system allows storing, tracking, querying, and reviewing changes in different file formats, e.g. .xlsx, .sqlite, or .inlang." },
   ];
 };
-
-const useCases: Array<UseCase> = [
-  {
-    title: "Fink - Change Control in Translation Management Systems",
-    year: 2024,
-    link: "https://opral.substack.com/cp/146431448",
-  },
-  {
-    title: "Collaborative Markdown with Lix Change Control",
-    year: 2024,
-    link: "https://opral.substack.com/p/collaborative-markdown-with-lix-change",
-  },
-]
-
-
-const blogPosts: Array<BlogPost> = [
-  {
-    title: "July 2024 update",
-    link: "https://opral.substack.com/p/july-2024-update",
-  },
-  {
-    title: "Accelerate by years part IV - The prototype",
-    link: "https://opral.substack.com/p/accelerate-by-years-iv-the-prototype",
-  },
-  {
-    title: "Accelerate by years part III - Lix on SQLite",
-    link: "https://opral.substack.com/p/accelerate-by-years-part-iii-lix",
-  },
-  {
-    title: "Accelerate by years part II - Self-contained inlang files",
-    link: "https://opral.substack.com/p/accelerate-by-years-part-ii-self",
-  },
-  {
-    title: "Accelerate by years part I - Inlang directories as lix repositories",
-    link: "https://opral.substack.com/p/accelerate-by-years-part-i-inlang",
-  },
-  {
-    title: "June 2024 update",
-    link: "https://opral.substack.com/p/june-2024-update",
-  },
-]
 
 export default function Index() {
   return (
