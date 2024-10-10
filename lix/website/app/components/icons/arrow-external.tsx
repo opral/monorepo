8f17d8823e73539aa926c85e7a0da4fd1e286418
NiklasBuchfink <niklas.buchfink@web.de>
2024-10-10 15:10:32 +0200

    feat: setup faq and quote section

--- lix/website/app/components/icons/arrow-external.tsx
+++ lix/website/app/components/icons/arrow-external.tsx
@@ -8,9 +8,9 @@ const IconArrowExternal = () => {
     >
       <path
         fill="currentColor"
-        fill-rule="evenodd"
+        fillRule="evenodd"
         d="M3.646 11.354a.5.5 0 0 1 0-.707L10.293 4H6a.5.5 0 0 1 0-1h5.5a.5.5 0 0 1 .5.5V9a.5.5 0 0 1-1 0V4.707l-6.646 6.647a.5.5 0 0 1-.708 0"
-        clip-rule="evenodd"
+        clipRule="evenodd"
       />
     </svg>
   )
--- lix/website/app/routes/_index.tsx
+++ lix/website/app/routes/_index.tsx
@@ -1,5 +1,5 @@
 import type { MetaFunction } from "@remix-run/node";
-import Header from "../components/header";
+import Header, { socialLinks } from "../components/header";
 import Footer from "../components/footer";
 import Check from "~/components/ui/check";
 import IconArrowExternal from "~/components/icons/arrow-external";
@@ -70,11 +70,30 @@ const appsBuiltOnTopOfLix = [
   }
 ];
 
+const faq = [
+  {
+    question: "What is the difference between a Lix app and a conventional file in a Lix system?",
+    answer: "Changes in conventional files are only tracked when saved to the Lix file system. Therefore, features are only available via the Lix file system, while editing happens only in conventional apps. Lix apps are built on the system and define their file format. This allows them to use all the functions in the app to improve the editing process and collaboration workflow. If conventional apps have a plug-in interface, it could be used to integrate Lix features."
+  },
+  {
+    question: "How is it different from my current file-sharing solution?",
+    answer: "Your current file-sharing solution may show which of your colleagues made the last change to a file, but you don't know what changed, what the previous version was and what the context of the changes was."
+  },
+  {
+    question: "How does it compare to versioning I know from other apps?",
+    answer: "There are apps with versioning, but in many cases, they only save versions of the entire project at specific points in time. Lix tracks and understands the context of every change in a file, giving you more context and allowing you to set automations. Furthermore, Lix provides a generalized system that allows all files and apps to work together."
+  },
+  {
+    question: "Is lix replacing git?",
+    answer: "No. Lix is designed to change control non-text files and build apps, not version source code."
+  },
+]
+
 export default function Index() {
   return (
     <div className="min-h-screen">
       <Header />
-      <main className="w-full max-w-5xl px-4 mx-auto space-y-16">
+      <main className="w-full max-w-5xl px-4 mx-auto space-y-20">
         <div className="grid grid-cols-2 gap-24 mt-12 mb-12">
           <div>
             <div>
@@ -148,12 +167,14 @@ export default function Index() {
               <a
                 key={index}
                 href={app.link}
-                className="relative card font-semibold gap-4 w-full"
+                className="relative card font-semibold gap-4 w-full group"
               >
-                <div className="absolute top-4 right-4 flex justify-center items-center w-10 h-10 rounded-full bg-white text-slate-500 ring ring-1 ring-slate-200">
-                  <IconArrowExternal />
+                <div className="absolute top-6 right-6 flex justify-center items-center w-10 h-10 rounded-full bg-white text-slate-500 ring ring-1 ring-slate-200 group-hover:text-cyan-500  group-hover:ring-cyan-500">
+                  <div className="transition-transform group-hover:translate-x-0.5 group-hover:-translate-y-0.5">
+                    <IconArrowExternal />
+                  </div>
                 </div>
-                <div className="mt-10">{app.title}</div>
+                <div className="mt-12">{app.title}</div>
               </a>
             )}
           </div>
@@ -165,7 +186,39 @@ export default function Index() {
           </div>
         </div>
 
+        <div className="grid grid-cols-3 gap-24">
+          <div>
+            <h2 className="text-3xl font-semibold">Open questions?</h2>
+            <div className="mt-4 flex gap-4">
+              {socialLinks.map((socialLink, index) =>
+                <a
+                  key={index}
+                  href={socialLink.href}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="px-4 py-2 border border-1 border-slate-20 rounded-md text-slate-500 hover:text-cyan-600"
+                >
+                  {socialLink.text}
+                </a>
+              ).slice(0, 2)}
+            </div>
+          </div>
+          <div className="col-span-2 space-y-4">
+            {faq.map((question, index) => (
+              <div key={index} className="card">
+                <h3 className="font-semibold">{question.question}</h3>
+                <p>{question.answer}</p>
+              </div>
+            ))}
+          </div>
+        </div>
 
+        <div className="flex flex-col gap-4 max-w-xl mx-auto pl-20 border-l border-surface-200">
+          <p className="italic">
+            "Every work that we create, every time we collaborate, everything we automate, it revolves around changes. A system, that can understand changes and inform you about that these changes happened, means that you have a system to collaborate, validate, automate and create."
+          </p>
+          <p>- Samuel Stroschein, Founder of Opral (lix & inlang)</p>
+        </div>
       </main>
       <Footer />
     </div>
