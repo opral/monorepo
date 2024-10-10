605890a5aa56c315aa6e93e4eeaf6a79d59f7aed
NiklasBuchfink <niklas.buchfink@web.de>
2024-10-10 17:06:23 +0200

    chore: reapply button

--- lix/website/app/routes/_index.tsx
+++ lix/website/app/routes/_index.tsx
@@ -114,13 +114,22 @@ export default function Index() {
               The lix change control system allows storing, tracking, querying, and reviewing
               changes in different file formats, e.g. .xlsx, .sqlite, or .inlang.
             </p>
-            <a
-              href="https://opral.substack.com/"
-              target="_blanc"
-              className="w-fit px-4 py-2 text-white bg-cyan-600 hover:bg-cyan-700 rounded-md font-semibold flex items-center gap-2"
-            >
-              Get updates
-            </a>
+            <div className="flex items-center gap-2">
+              <a
+                href="https://opral.substack.com/"
+                target="_blanc"
+                className="w-fit px-4 py-2 text-white bg-cyan-600 hover:bg-cyan-700 rounded-md font-semibold flex items-center gap-2 transition-all"
+              >
+                Get updates
+              </a>
+              <a
+                href="https://x.com/FabianHiller/status/1839301632888131814"
+                target="_blanc"
+                className="w-fit px-4 py-2 text-slate-500 bg-white hover:bg-slate-100 rounded-md font-medium flex items-center gap-2 border border-slate-300 hover:border-slate-400 transition-all"
+              >
+                Lix talk at Web Dev Meetup NY
+              </a>
+            </div>
           </div>
           <div>
             {coreFeatures.map((feature, index) => (
