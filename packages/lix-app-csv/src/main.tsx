7a86f86861f8ba5837e4efbb80a75730a62e45a5
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2024-10-21 15:30:31 +0200

    fix: suspense annoyance

--- packages/lix-app-csv/src/main.tsx
+++ packages/lix-app-csv/src/main.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import "./style.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
@@ -10,7 +10,15 @@ setBasePath(
 	"https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.16.0/cdn/"
 );
 
-const router = createBrowserRouter(routes);
+const router = createBrowserRouter(
+	routes.map((route) => ({
+		...route,
+		// Wrapping the route in a Suspense to avoid React Router
+		// errors that "A component is async but wasn't wrapped in Suspense"
+		// The error is irrelevant for the demo. A 1ms white screen is OK.
+		element: <Suspense fallback={null}>{route.element}</Suspense>,
+	}))
+);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
