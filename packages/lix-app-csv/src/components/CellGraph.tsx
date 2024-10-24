1a4d77d27586de2b14789a3d68ad33aabbb21fd1
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2024-10-24 16:55:57 +0200

    add mock discussion

--- packages/lix-app-csv/src/components/CellGraph.tsx
+++ packages/lix-app-csv/src/components/CellGraph.tsx
@@ -1,6 +1,13 @@
 import timeAgo from "../helper/timeAgo.ts";
 import { ChangeWithSnapshot } from "@lix-js/sdk";
 
+// mock
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function hasDiscussion(change: any) {
+	// change
+	return false;
+}
+
 /**
  * Renders change control information for a row.
  */
@@ -13,20 +20,6 @@ export default function CellGraph(props: {
 			<div className="w-full flex items-center min-h-[54px] gap-1 border-b border-zinc-200">
 				<div className=" w-full flex justify-between items-center text-zinc-950 h-9 rounded-lg px-3">
 					<div className="flex items-center gap-2">
-						<div className="flex rotate-90 md:rotate-0 bg-zinc-100 hover:bg-zinc-200 cursor-pointer w-7 h-7 items-center justify-center rounded">
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								width="18"
-								height="18"
-								viewBox="0 0 24 24"
-							>
-								<path
-									fill="currentColor"
-									d="M10 6L8.59 7.41L13.17 12l-4.58 4.59L10 18l6-6z"
-								/>
-							</svg>
-						</div>
-
 						<h1 className="font-medium pl-1">Change Graph</h1>
 					</div>
 				</div>
@@ -52,20 +45,22 @@ export default function CellGraph(props: {
 									</div> */}
 									<p>{timeAgo(change.created_at)}</p>
 								</div>
-								<div className="flex items-center gap-2">
-									<svg
+								{hasDiscussion(change) && (
+									<div className="flex items-center gap-2">
+										{/* <svg
 										xmlns="http://www.w3.org/2000/svg"
 										width="1em"
 										height="1em"
 										viewBox="0 0 24 24"
-									>
+										>
 										<path
-											fill="currentColor"
-											d="M5.763 17H20V5H4v13.385zm.692 2L2 22.5V4a1 1 0 0 1 1-1h18a1 1 0 0 1 1 1v14a1 1 0 0 1-1 1z"
+										fill="currentColor"
+										d="M5.763 17H20V5H4v13.385zm.692 2L2 22.5V4a1 1 0 0 1 1-1h18a1 1 0 0 1 1 1v14a1 1 0 0 1-1 1z"
 										/>
-									</svg>
-									{/* <p className="text-sm">{change.description}</p> */}
-								</div>
+										</svg> */}
+										{/* <p className="text-sm">{change.description}</p> */}
+									</div>
+								)}
 								<div className="mt-2 px-3 py-1.5 bg-zinc-100 border border-zinc-200 rounded">
 									{change.content!.text.split(",")![props.activeCell.col]}
 								</div>
