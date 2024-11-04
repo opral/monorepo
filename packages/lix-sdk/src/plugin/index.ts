8ea4ec89bbb58a07186a20cae8d1bbbb14872970
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2024-11-04 15:47:55 -0500

    add missing export

--- packages/lix-sdk/src/plugin/index.ts
+++ packages/lix-sdk/src/plugin/index.ts
@@ -1,2 +1,7 @@
-export type { LixPlugin } from "./lix-plugin.js";
+export type {
+	LixPlugin,
+	DetectedChange,
+	DetectedConflict,
+	LixReadonly,
+} from "./lix-plugin.js";
 export { applyChanges } from "./apply-changes.js";
