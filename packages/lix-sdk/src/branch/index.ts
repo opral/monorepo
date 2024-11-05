c66bf78fc1a43fe2b1250c8ce2aa17ed34831e41
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2024-11-05 18:09:52 -0500

    add missing export

--- packages/lix-sdk/src/branch/index.ts
+++ packages/lix-sdk/src/branch/index.ts
@@ -5,3 +5,4 @@
 export { createBranch } from "./create-branch.js";
 export { switchBranch } from "./switch-branch.js";
 export { updateBranchPointers } from "./update-branch-pointers.js";
+export { mergeBranch } from "./merge-branch.js";
