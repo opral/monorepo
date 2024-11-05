5e923b4930d17bb29e42851aa6c34306ed315c9d
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2024-11-04 21:59:55 -0500

    remove isInSimulatedCurrentBranch
    real branches now exist

--- packages/lix-sdk/src/query-utilities/index.ts
+++ packages/lix-sdk/src/query-utilities/index.ts
@@ -1,6 +1,5 @@
 export { getLowestCommonAncestor } from "./get-lowest-common-ancestor.js";
 export { getLeafChangesOnlyInSource } from "./get-leaf-changes-only-in-source.js";
-export { isInSimulatedCurrentBranch } from "./is-in-simulated-branch.js";
 export { mockJsonSnapshot } from "./mock-json-snapshot.js";
 export { changeHasLabel } from "./change-has-label.js";
 export { changeIsLeafChange } from "./change-is-leaf-change.js";
