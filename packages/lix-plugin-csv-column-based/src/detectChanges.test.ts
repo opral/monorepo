0bf0e8d245df8317f0a3646d98285f6f668f0b00
Martin Lysk <martin.lysk@gmail.com>
2024-11-08 13:25:23 +0100

    WIP detect changes works

--- packages/lix-plugin-csv-column-based/src/detectChanges.test.ts
+++ packages/lix-plugin-csv-column-based/src/detectChanges.test.ts
@@ -1,6 +1,10 @@
 import { expect, test } from "vitest";
-import { detectChanges } from "./detectChanges.js";
-import type { DetectedChange } from "@lix-js/sdk";
+import {
+	detectChanges,
+	type DetectedCellChange,
+	type DetectedRowChange,
+	type DetectedSchemaDefinitionChange,
+} from "./detectChanges.js";
 
 test("it should not detect changes if the csv did not update", async () => {
 	const before = new TextEncoder().encode("Name,Age\nAnna,20\nPeter,50");
@@ -34,16 +38,24 @@ test("it should detect an insert", async () => {
 
 	expect(detectedChanges).toEqual([
 		{
+			entity_id: "Name|John",
+			type: "csv-v2-row",
+			snapshot: {
+				rowIndex: 2,
+				rowEntities: ["Name|John|Name", "Name|John|Age"].sort(),
+			},
+		},
+		{
 			entity_id: "Name|John|Name",
-			type: "cell",
+			type: "csv-v2-cell",
 			snapshot: { text: "John" },
 		},
 		{
 			entity_id: "Name|John|Age",
-			type: "cell",
+			type: "csv-v2-cell",
 			snapshot: { text: "30" },
 		},
-	] satisfies DetectedChange[]);
+	] satisfies (DetectedRowChange | DetectedCellChange)[]);
 });
 
 test("it should detect updates", async () => {
@@ -59,11 +71,11 @@ test("it should detect updates", async () => {
 
 	expect(detectedChanges).toEqual([
 		{
-			type: "cell",
+			type: "csv-v2-cell",
 			entity_id: "Name|Anna|Age",
 			snapshot: { text: "21" },
 		},
-	] satisfies DetectedChange[]);
+	] satisfies (DetectedRowChange | DetectedCellChange)[]);
 });
 
 test("it should detect a deletion", async () => {
@@ -78,9 +90,14 @@ test("it should detect a deletion", async () => {
 	});
 
 	expect(detectedChanges).toEqual([
-		{ entity_id: "Name|Peter|Name", type: "cell", snapshot: undefined },
-		{ entity_id: "Name|Peter|Age", type: "cell", snapshot: undefined },
-	] satisfies DetectedChange[]);
+		{
+			entity_id: "Name|Peter",
+			type: "csv-v2-row",
+			snapshot: null,
+		},
+		{ entity_id: "Name|Peter|Name", type: "csv-v2-cell", snapshot: null },
+		{ entity_id: "Name|Peter|Age", type: "csv-v2-cell", snapshot: null },
+	] satisfies (DetectedRowChange | DetectedCellChange)[]);
 });
 
 // throwing an error leads to abysmal UX on potentially every save
@@ -117,18 +134,118 @@ test("changing the unique column should lead to a new entity_id to avoid bugs", 
 
 	expect(detectedChanges).toEqual(
 		expect.arrayContaining([
-			// detect the deletion of the old unique column
-			{ entity_id: "Name|Anna|Name", type: "cell", snapshot: undefined },
-			{ entity_id: "Name|Anna|Age", type: "cell", snapshot: undefined },
-			{ entity_id: "Name|Peter|Name", type: "cell", snapshot: undefined },
-			{ entity_id: "Name|Peter|Age", type: "cell", snapshot: undefined },
+			// detect the deletion of old rows
+			{ entity_id: "Name|Anna", type: "csv-v2-row", snapshot: null },
+			{ entity_id: "Name|Peter", type: "csv-v2-row", snapshot: null },
+
 			// detect the insertion of the new unique column
+			{
+				entity_id: "Age|20",
+				type: "csv-v2-row",
+				snapshot: {
+					rowIndex: 0,
+					rowEntities: ["Age|20|Age", "Age|20|Name"].sort(),
+				},
+			},
+			{
+				entity_id: "Age|50",
+				type: "csv-v2-row",
+				snapshot: {
+					rowIndex: 1,
+					rowEntities: ["Age|50|Age", "Age|50|Name"].sort(),
+				},
+			},
+			// detect the deletion of the old unique column
+			{ entity_id: "Name|Anna|Name", type: "csv-v2-cell", snapshot: null },
+			{ entity_id: "Name|Anna|Age", type: "csv-v2-cell", snapshot: null },
+			{
+				entity_id: "Name|Peter|Name",
+				type: "csv-v2-cell",
+				snapshot: null,
+			},
+			{ entity_id: "Name|Peter|Age", type: "csv-v2-cell", snapshot: null },
 
-			{ entity_id: "Age|50|Name", type: "cell", snapshot: { text: "Peter" } },
-			{ entity_id: "Age|50|Age", type: "cell", snapshot: { text: "50" } },
+			{
+				entity_id: "Age|50|Name",
+				type: "csv-v2-cell",
+				snapshot: { text: "Peter" },
+			},
+			{
+				entity_id: "Age|50|Age",
+				type: "csv-v2-cell",
+				snapshot: { text: "50" },
+			},
 
-			{ entity_id: "Age|20|Name", type: "cell", snapshot: { text: "Anna" } },
-			{ entity_id: "Age|20|Age", type: "cell", snapshot: { text: "20" } },
-		]),
+			{
+				entity_id: "Age|20|Name",
+				type: "csv-v2-cell",
+				snapshot: { text: "Anna" },
+			},
+			{
+				entity_id: "Age|20|Age",
+				type: "csv-v2-cell",
+				snapshot: { text: "20" },
+			},
+		] satisfies (DetectedRowChange | DetectedCellChange)[]),
 	);
+});
+
+// 1. if the entity id would remain identical, the change graph would be messed up
+// 2. if no new changes are reported after a column change, the initial state
+//    is not tracked
+test("changing the column order should just result in a schame change", async () => {
+	const before = new TextEncoder().encode("Name,Age\nAnna,20\nPeter,50");
+	const after = new TextEncoder().encode("Age,Name\n20,Anna\n50,Peter");
+
+	const meta = { unique_column: "Name" };
+
+	const detectedChanges = await detectChanges?.({
+		before: { id: "random", path: "x.csv", data: before, metadata: meta },
+		after: { id: "random", path: "x.csv", data: after, metadata: meta },
+	});
+
+	expect(detectedChanges).toEqual([
+		{
+			entity_id: "schema",
+			type: "csv-v2-schema",
+			snapshot: {
+				columnNames: ["Age", "Name"],
+			},
+		},
+	] satisfies (
+		| DetectedRowChange
+		| DetectedCellChange
+		| DetectedSchemaDefinitionChange
+	)[]);
+});
+
+test("changing the row order should just result in a row change", async () => {
+	const before = new TextEncoder().encode("Name,Age\nAnna,20\nPeter,50");
+	const after = new TextEncoder().encode("Name,Age\nPeter,50\nAnna,20");
+
+	const meta = { unique_column: "Name" };
+
+	const detectedChanges = await detectChanges?.({
+		before: { id: "random", path: "x.csv", data: before, metadata: meta },
+		after: { id: "random", path: "x.csv", data: after, metadata: meta },
+	});
+
+	expect(detectedChanges).toEqual([
+		{
+			entity_id: "Name|Anna",
+			type: "csv-v2-row",
+			snapshot: {
+				rowIndex: 1,
+				rowEntities: ["Name|Anna|Name", "Name|Anna|Age"].sort(),
+			},
+		},
+		{
+			entity_id: "Name|Peter",
+			type: "csv-v2-row",
+			snapshot: {
+				rowIndex: 0,
+				rowEntities: ["Name|Peter|Name", "Name|Peter|Age"].sort(),
+			},
+		},
+	] satisfies (DetectedRowChange | DetectedCellChange)[]);
 });
