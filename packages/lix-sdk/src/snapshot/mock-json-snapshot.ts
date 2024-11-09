d898b8e5763e503e0afa69fb650304cf35879a0e
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2024-11-09 12:44:36 -0500

    improve explicit return type for inference

--- packages/lix-sdk/src/snapshot/mock-json-snapshot.ts
+++ packages/lix-sdk/src/snapshot/mock-json-snapshot.ts
@@ -1,13 +1,15 @@
 import { sha256 } from "js-sha256";
+import type { Snapshot } from "../database/schema.js";
 
 /**
  *
  * Util function for tests that creates a snapshot that looks like one you got returned from the database after inserting
- * @param content
- * @returns
+ *
  */
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const mockJsonSnapshot = function (content: Record<string, any>) {
+export const mockJsonSnapshot = function (
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	content: Record<string, any>,
+): Snapshot {
 	return {
 		id: sha256(JSON.stringify(content)),
 		content: content,
