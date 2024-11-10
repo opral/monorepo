91d619155b18fefd385509a7da7488530f473963
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2024-11-10 18:30:44 -0500

    add test for cascade

--- packages/lix-sdk/src/change-conflict/schema.test.ts
+++ packages/lix-sdk/src/change-conflict/schema.test.ts
@@ -2,6 +2,36 @@ import { test, expect } from "vitest";
 import { openLixInMemory } from "../lix/open-lix-in-memory.js";
 import { LIX_DIVERGING_ENTITY_CONFLICT_KEY } from "./detect-diverging-entity-conflict.js";
 
+// skipping because foreign keys are not activated yet
+test.skip("change conflict deletions should cascade to change conflict elements", async () => {
+	const lix = await openLixInMemory({});
+
+	const changeConflict = await lix.db
+		.insertInto("change_conflict")
+		.values({ key: "foo" })
+		.returningAll()
+		.executeTakeFirstOrThrow();
+
+	await lix.db
+		.insertInto("change_conflict_element")
+		.values({ change_conflict_id: changeConflict.id, change_id: "bar" })
+		.returningAll()
+		.executeTakeFirstOrThrow();
+
+	await lix.db
+		.deleteFrom("change_conflict")
+		.where("id", "=", changeConflict.id)
+		.execute();
+
+	expect(
+		lix.db
+			.selectFrom("change_conflict_element")
+			.where("change_conflict_id", "=", changeConflict.id)
+			.selectAll()
+			.execute(),
+	).resolves.toEqual([]);
+});
+
 test.skip("it should throw if a change_conflict.key starts with 'lix-' and is not whitelisted", async () => {
 	const lix = await openLixInMemory({});
 
