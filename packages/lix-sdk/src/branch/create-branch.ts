affb74aefc71883377a24b3c6cdc3d3c60c78506
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2024-11-11 18:13:04 -0500

    fix: branch target creation

--- packages/lix-sdk/src/branch/create-branch.ts
+++ packages/lix-sdk/src/branch/create-branch.ts
@@ -68,21 +68,8 @@ export async function createBranch(args: {
 			await trx
 				.insertInto("branch_target")
 				.values({
-					// before merging this branch into the parent branch,
-					// the source branch needs to be mergable into this branch
-					// hence, this branch is the target branch
-					//
-					// yes, that requires a bit of mental gymnastics
-					//
-					// the parent branch has no intention (and probably never will)
-					// to merge into this branch. but this branch has the intention
-					// to merge into the parent branch. to make this branch mergable
-					// into the parent branch, no conflicts between the parent branch
-					// and this branch should exist.
-					source_branch_id: args.parent.id,
-					target_branch_id: branch.id,
-					// source_branch_id: branch.id,
-					// target_branch_id: args.parent.id,
+					source_branch_id: branch.id,
+					target_branch_id: args.parent.id,
 				})
 				// ignore if the merge intent already exists
 				.onConflict((oc) => oc.doNothing())
