f109c78b552faa73a8d851467c58af1d2fe0b1d2
NiklasBuchfink <niklas.buchfink@web.de>
2024-11-12 16:00:46 +0100

    move header, footer and main to layout

--- lix/website/app/root.tsx
+++ lix/website/app/root.tsx
@@ -3,6 +3,9 @@ import type { LinksFunction } from "@remix-run/node"
 import { useEffect } from "react"
 import { posthog } from "posthog-js"
 import "./tailwind.css"
+import Header from "./components/header"
+import Footer from "./components/footer"
+import IconArrowExternal from "./components/icons/arrow-external"
 
 export async function loader() {
 	return { PUBLIC_POSTHOG_TOKEN: process.env.PUBLIC_POSTHOG_TOKEN }
@@ -53,7 +56,24 @@ export function Layout({ children }: { children: React.ReactNode }) {
 					<Links />
 				</head>
 				<body>
-					{children}
+					<div className="min-h-screen">
+						<div className="flex justify-center items-center h-[50px] text-[16px] px-3 bg-slate-100 border-b border-slate-200 text-black font-medium">
+							Public preview launching, Dec 16
+							<a
+								className="group text-cyan-600 hover:text-black border border-slate-200 mx-6 flex gap-2 items-center py-1 px-3 rounded-md bg-white"
+								target="_blank"
+								href="https://forms.gle/cR3iDsUB7DEygJaZ8"
+							>
+								Get notified
+								<IconArrowExternal />
+							</a>
+						</div>
+						<Header />
+						<main className="w-full max-w-5xl px-4 mx-auto space-y-16 md:space-y-24">
+							{children}
+						</main>
+						<Footer />
+					</div>
 					<ScrollRestoration />
 					<Scripts />
 				</body>
--- lix/website/app/routes/file-manager.tsx
+++ lix/website/app/routes/file-manager.tsx
@@ -1,23 +1,52 @@
-import Footer from "~/components/footer"
-import Header from "~/components/header"
-import IconArrowExternal from "~/components/icons/arrow-external"
+import { socialLinks } from "~/components/header"
+import Details from "~/components/ui/details"
+
+const faq = [
+	{
+		question: "What is that in the air?",
+		answer: "It's superman.",
+	},
+	{
+		question: "Is it a plane?",
+		answer: "It's superman.",
+	},
+	{
+		question: "Is it a plane?",
+		answer: "It's superman.",
+	},
+]
 
 export default function FileManager() {
 	return (
-		<div className="min-h-screen">
-			<div className="flex justify-center items-center h-[50px] text-[16px] px-3 bg-slate-100 border-b border-slate-200 text-black font-medium">
-				Public preview launching, Dec 16
-				<a
-					className="group text-cyan-600 hover:text-black border border-slate-200 mx-6 flex gap-2 items-center py-1 px-3 rounded-md bg-white"
-					target="_blank"
-					href="https://forms.gle/cR3iDsUB7DEygJaZ8"
-				>
-					Get notified
-					<IconArrowExternal />
-				</a>
+		<div className="grid md:grid-cols-3 gap-8 md:gap-4">
+			<div className="col-span-2 md:col-span-1">
+				<h2>Open questions?</h2>
+				<div className="mt-4 flex gap-2">
+					{socialLinks
+						.map((socialLink, index) => (
+							<a
+								key={index}
+								href={socialLink.href}
+								target="_blank"
+								rel="noopener noreferrer"
+								className="transition-all w-fit px-4 py-2 text-slate-500 bg-white hover:bg-slate-100 rounded-md font-medium flex items-center gap-2 border border-slate-300 hover:border-slate-400"
+							>
+								{socialLink.text}
+							</a>
+						))
+						.slice(0, 2)}
+				</div>
 			</div>
-			<Header />
-			<Footer />
+			<div className="md:mt-3 col-span-2 space-y-3 md:space-y-6">
+				{faq.map((question, index) => (
+					<div key={index}>
+						<Details summary={question.question} content={question.answer} />
+						{faq.length - 1 !== index && (
+							<div className="mt-3 md:mt-6 border-b border-slate-200"></div>
+						)}
+					</div>
+				))}
+			</div>
 		</div>
 	)
 }
