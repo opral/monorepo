473761cda2ab1e0a839033de9b25cd5c814afe10
NiklasBuchfink <niklas.buchfink@web.de>
2024-11-14 13:36:44 +0100

    feat: make banner responsive

--- lix/website/app/components/ui/banner.tsx
+++ lix/website/app/components/ui/banner.tsx
@@ -2,10 +2,10 @@ import IconArrowExternal from "../icons/arrow-external"
 
 const Banner = () => {
 	return (
-		<div className="flex justify-center items-center h-[50px] text-[16px] px-3 bg-slate-100 border-b border-slate-200 text-black font-medium">
+		<div className="flex justify-center items-center gap-2 h-16 md:h-[50px] text-[16px] px-3 bg-slate-100 border-b border-slate-200 text-black font-medium">
 			Public preview launching, Dec 16
 			<a
-				className="group text-cyan-600 hover:text-black border border-slate-200 mx-6 flex gap-2 items-center py-1 px-3 rounded-md bg-white"
+				className="group text-cyan-600 hover:text-black border border-slate-200 md:mx-6 flex gap-2 items-center py-1 px-3 rounded-md bg-white whitespace-nowrap"
 				target="_blank"
 				href="https://forms.gle/cR3iDsUB7DEygJaZ8"
 			>
