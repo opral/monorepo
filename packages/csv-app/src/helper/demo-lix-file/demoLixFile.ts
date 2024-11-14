e75504c8e269e00b6cda32d364c886c59dd2ae1d
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2024-11-14 13:52:27 -0500

    fix: re-enable cap table example

--- packages/csv-app/src/helper/demo-lix-file/demoLixFile.ts
+++ packages/csv-app/src/helper/demo-lix-file/demoLixFile.ts
@@ -1,6 +1,7 @@
 import { newLixFile, openLixInMemory } from "@lix-js/sdk";
 import { plugin as pluginV2 } from "@lix-js/plugin-csv-column-based";
 import emailNewsletterCsv from "./email-newsletter.csv?raw";
+import captableCsv from "./cap-table.csv?raw";
 
 export const DEMO_CAP_TABLE_CSV_FILE_ID = "29jas9j-2sk2-cap";
 export const DEMO_EMAIL_NEWSLETTER_CSV_FILE_ID = "oj20a1-40ss-email";
@@ -15,20 +16,22 @@ export async function lixCsvDemoFile(): Promise<Blob> {
 		providePlugins: [pluginV2],
 	});
 
-	// await lix.db
-	// 	.insertInto("file")
-	// 	.values({
-	// 		id: DEMO_CAP_TABLE_CSV_FILE_ID,
-	// 		path: "/cap-table-example.csv",
-	// 		data: new TextEncoder().encode(capTableCsv),
-	// 		// @ts-expect-error - insert expects stringified json
-	// 		metadata: JSON.stringify({
-	// 			unique_column: "Stakeholder",
-	// 		}),
-	// 	})
-	// 	.execute();
-
-	// await lix.settled();
+	// only in production have the demo files been inserted
+	// to reduce the number of changes when debugging the app
+	if (import.meta.env.PROD) {
+		await lix.db
+			.insertInto("file")
+			.values({
+				id: DEMO_CAP_TABLE_CSV_FILE_ID,
+				path: "/cap-table-example.csv",
+				data: new TextEncoder().encode(captableCsv),
+				// @ts-expect-error - insert expects stringified json
+				metadata: JSON.stringify({
+					unique_column: "Stakeholder",
+				}),
+			})
+			.execute();
+	}
 
 	await lix.db
 		.insertInto("file")
