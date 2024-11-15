cb3e35a899bc55ca7dd26462ed3efad5f27a68e6
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2024-11-14 22:56:57 -0500

    only have minimal csv in dev to ease debugging

--- packages/csv-app/src/helper/demo-lix-file/demoLixFile.ts
+++ packages/csv-app/src/helper/demo-lix-file/demoLixFile.ts
@@ -2,6 +2,7 @@ import { newLixFile, openLixInMemory } from "@lix-js/sdk";
 import { plugin as pluginV2 } from "@lix-js/plugin-csv-column-based";
 import emailNewsletterCsv from "./email-newsletter.csv?raw";
 import captableCsv from "./cap-table.csv?raw";
+import minimalCsv from "./minimal.csv?raw";
 
 export const DEMO_CAP_TABLE_CSV_FILE_ID = "29jas9j-2sk2-cap";
 export const DEMO_EMAIL_NEWSLETTER_CSV_FILE_ID = "oj20a1-40ss-email";
@@ -16,9 +17,21 @@ export async function lixCsvDemoFile(): Promise<Blob> {
 		providePlugins: [pluginV2],
 	});
 
-	// only in production have the demo files been inserted
-	// to reduce the number of changes when debugging the app
-	if (import.meta.env.PROD) {
+	if (!import.meta.env.PROD) {
+		await lix.db
+			.insertInto("file")
+			.values({
+				id: "sjd9a9-2j2j-minimal",
+				metadata: {
+					unique_column: "email",
+				},
+				path: "/minimal.csv",
+				data: new TextEncoder().encode(minimalCsv),
+			})
+			.execute();
+	} else {
+		// only in production have the demo files been inserted
+		// to reduce the number of changes when debugging the app
 		await lix.db
 			.insertInto("file")
 			.values({
@@ -31,20 +44,20 @@ export async function lixCsvDemoFile(): Promise<Blob> {
 				}),
 			})
 			.execute();
-	}
 
-	await lix.db
-		.insertInto("file")
-		.values({
-			id: DEMO_EMAIL_NEWSLETTER_CSV_FILE_ID,
-			path: "/email-newsletter.csv",
-			data: new TextEncoder().encode(emailNewsletterCsv),
-			// @ts-expect-error - insert expects stringified json
-			metadata: JSON.stringify({
-				unique_column: "email",
-			}),
-		})
-		.execute();
+		await lix.db
+			.insertInto("file")
+			.values({
+				id: DEMO_EMAIL_NEWSLETTER_CSV_FILE_ID,
+				path: "/email-newsletter.csv",
+				data: new TextEncoder().encode(emailNewsletterCsv),
+				// @ts-expect-error - insert expects stringified json
+				metadata: JSON.stringify({
+					unique_column: "email",
+				}),
+			})
+			.execute();
+	}
 
 	await lix.settled();
 
