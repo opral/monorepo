26ef73095e73871ae51a6c14dbd0c549be9fa4df
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2024-11-17 20:13:40 -0500

    copy over tests

--- packages/lix-plugin-csv/src/detectChanges.test.ts
+++ packages/lix-plugin-csv/src/detectChanges.test.ts
@@ -2,6 +2,8 @@ import { expect, test } from "vitest";
 import { detectChanges } from "./detectChanges.js";
 import type { DetectedChange } from "@lix-js/sdk";
 import { CellSchema } from "./schemas/cell.js";
+import { HeaderSchema } from "./schemas/header.js";
+import { RowSchema } from "./schemas/row.js";
 
 test("it should not detect changes if the csv did not update", async () => {
 	const before = new TextEncoder().encode("Name,Age\nAnna,20\nPeter,50");
@@ -140,4 +142,62 @@ test("changing the unique column should lead to a new entity_id to avoid bugs", 
 			{ entity_id: "Age|20|Age", schema: CellSchema, snapshot: { text: "20" } },
 		] satisfies DetectedChange<typeof CellSchema>[]),
 	);
+});
+
+// 1. if the entity id would remain identical, the change graph would be messed up
+// 2. if no new changes are reported after a column change, the initial state
+//    is not tracked
+test("changing the header order should result in a change", async () => {
+	const before = new TextEncoder().encode("Name,Age\nAnna,20\nPeter,50");
+	const after = new TextEncoder().encode("Age,Name\n20,Anna\n50,Peter");
+
+	const meta = { unique_column: "Name" };
+
+	const detectedChanges = await detectChanges?.({
+		before: { id: "random", path: "x.csv", data: before, metadata: meta },
+		after: { id: "random", path: "x.csv", data: after, metadata: meta },
+	});
+
+	expect(detectedChanges).toEqual([
+		{
+			entity_id: "header",
+			schema: HeaderSchema,
+			snapshot: {
+				columnNames: ["Age", "Name"],
+			},
+		},
+	] satisfies DetectedChange<typeof HeaderSchema>[]);
+});
+
+test("row order changes should be detected", async () => {
+	// the row `Anna,20` is moved to the end
+	// while `Peter,50` is moved to the beginning
+	const before = new TextEncoder().encode("Name,Age\nAnna,20\nPeter,50");
+	const after = new TextEncoder().encode("Name,Age\nPeter,50\nAnna,20");
+
+	const meta = { unique_column: "Name" };
+
+	const detectedChanges = await detectChanges?.({
+		before: { id: "random", path: "x.csv", data: before, metadata: meta },
+		after: { id: "random", path: "x.csv", data: after, metadata: meta },
+	});
+
+	expect(detectedChanges).toEqual([
+		{
+			entity_id: "Name|Anna",
+			schema: RowSchema,
+			snapshot: {
+				rowIndex: 1,
+				rowEntities: ["Name|Anna|Name", "Name|Anna|Age"].sort(),
+			},
+		},
+		{
+			entity_id: "Name|Peter",
+			schema: RowSchema,
+			snapshot: {
+				rowIndex: 0,
+				rowEntities: ["Name|Peter|Name", "Name|Peter|Age"].sort(),
+			},
+		},
+	] satisfies DetectedChange<typeof RowSchema>[]);
 });
