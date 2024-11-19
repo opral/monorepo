3711dbf8d38807a15552de151b8f55b092e5f425
Nils Jacobsen <nils.jacobsen98@gmail.com>
2024-11-19 15:36:20 +0100

    fix: tailwind config

--- packages/lix-file-manager/src/layouts/AppLayout.tsx
+++ packages/lix-file-manager/src/layouts/AppLayout.tsx
@@ -1,7 +1,19 @@
+import { useEffect } from "react";
 import { SidebarProvider } from "../../components/ui/sidebar.tsx";
 import { AppSidebar } from "./Sidebar.tsx";
+import { useAtom } from "jotai";
+import { withPollingAtom } from "./../state.ts";
 
 export function App({ children }: { children: React.ReactNode }) {
+	const [, setPolling] = useAtom(withPollingAtom);
+
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setPolling(Date.now());
+		}, 1000);
+		return () => clearInterval(interval);
+	}, []);
+
 	return (
 		<SidebarProvider>
 			<div className="flex h-screen w-full">
