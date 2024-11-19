b72388c56c9798df56a2be2e4dfc9b03b779b83b
NiklasBuchfink <niklas.buchfink@web.de>
2024-11-19 14:28:15 +0100

    chore: replace backarrow

--- packages/lix-file-manager/src/components/SectionHeader.tsx
+++ packages/lix-file-manager/src/components/SectionHeader.tsx
@@ -1,4 +1,5 @@
 import { Button } from "./Button.tsx";
+import IconArrowLeft from "./icons/IconArrowLeft.tsx";
 
 interface SectionHeaderProps {
   title: string;
@@ -12,19 +13,7 @@ const SectionHeader = ({ title, backaction, children }: SectionHeaderProps) => {
       <div className="flex items-center gap-3">
         {backaction && (
           <Button variant="ghost" size="icon" onClick={backaction}>
-            <svg
-              xmlns="http://www.w3.org/
-              2000/svg"
-              className="h-5 w-5"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-            >
-              <path
-                fillRule="evenodd"
-                d="M7.293 8.293a1 1 0 011.414 0L12 11.586V7a1 1 0 112 0v5a1 1 0 01-1 1H7a1 1 0 010-2h3.586l-3.293-3.293a1 1 0 010-1.414z"
-                clipRule="evenodd"
-              />
-            </svg>
+            <IconArrowLeft />
           </Button>
         )}
         <h2 className="text-lg font-medium text-foreground">{title}</h2>
