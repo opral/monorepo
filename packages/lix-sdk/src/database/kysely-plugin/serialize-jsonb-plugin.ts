59b9b6daddc8992562f8fe393a02f592a745dc97
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2024-11-20 22:17:24 -0500

    add tests

--- packages/lix-sdk/src/database/kysely-plugin/serialize-jsonb-plugin.ts
+++ packages/lix-sdk/src/database/kysely-plugin/serialize-jsonb-plugin.ts
@@ -1,45 +1,35 @@
-import { type KyselyPlugin } from "kysely";
-import { createInMemoryDatabase } from "sqlite-wasm-kysely";
+import { ValuesNode, type KyselyPlugin } from "kysely";
 
-export async function ParseJsonBPluginV1(
+export function SerializeJsonBPlugin(
 	jsonbColumns: Record<string, string[]>,
-): Promise<KyselyPlugin> {
+): KyselyPlugin {
 	const jsonColumnNames = Object.keys(jsonbColumns).flatMap(
 		(key) => jsonbColumns[key]!,
 	);
 
-	const sqlite = await createInMemoryDatabase({});
-
 	return {
-		transformResult: async (args) => {
-			for (const row of args.result.rows) {
-				for (const col of jsonColumnNames) {
-					if (!row[col]) {
-						// result does not have a whitelisted json column
-						// ! this is a heursitic. ideally, we invest into
-						// ! building ParseJsonBPluginV2 that detects
-						// ! json columns from the query itself
-						continue;
-					}
-					try {
-						// sqlite has it's own jsonb format
-						// hence, need to query sqlite to convert
-						// to json
-						const json = sqlite.exec("SELECT json(?)", {
-							bind: [row[col] as Uint8Array],
-							returnValue: "resultRows",
-						})[0]![0];
+		transformResult: async (args) => args.result,
+		transformQuery: (args) => {
+			const query = args.node;
 
-						row[col] = JSON.parse(json as string);
-					} catch {
-						// it's not a json binary
-						continue;
-					}
-				}
+			if (query.kind !== "InsertQueryNode") {
+				return query;
 			}
 
-			return args.result;
+			const updatedValues = [];
+
+			for (const [i, value] of (
+				(query.values as ValuesNode).values[0]?.values ?? []
+			).entries()) {
+				if (!jsonColumnNames.includes(query.columns?.[i]?.column?.name ?? "")) {
+					updatedValues.push(value);
+					continue;
+				}
+				updatedValues.push(JSON.stringify(value));
+			}
+
+			// Return the updated query node
+			return query;
 		},
-		transformQuery: (args) => args.node,
 	};
 }
