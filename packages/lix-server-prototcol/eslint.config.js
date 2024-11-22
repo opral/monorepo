79ef3c0cf6c49401d8567e363d28d7f1a2eb9bfc
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2024-11-22 12:51:34 -0500

    add package

---
+++ packages/lix-server-prototcol/eslint.config.js
@@ -0,0 +1,28 @@
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default [
+	pluginJs.configs.recommended,
+	...tseslint.configs.recommended,
+	{
+		files: ["**/*.{js,ts}"],
+		rules: {
+			"@typescript-eslint/no-explicit-any": "off",
+			"no-restricted-imports": [
+				"error",
+				{
+					name: "@lix-js/sdk",
+					message:
+						"Importing from the compiled dist is not allowed (and you likely did this by accident). Import from source directly instead e.g. `./file.js`",
+				},
+			],
+		},
+	},
+	{
+		files: ["**/*.test.ts"],
+		rules: {
+			// any makes testing sometimes easier
+			"@typescript-eslint/no-explicit-any": "off",
+		},
+	},
+];
