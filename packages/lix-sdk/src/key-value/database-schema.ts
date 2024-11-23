741859e31955de335e0103c15db9e5769aa22a2b
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2024-11-23 15:36:51 -0500

    add autocomplete

--- packages/lix-sdk/src/key-value/database-schema.ts
+++ packages/lix-sdk/src/key-value/database-schema.ts
@@ -16,6 +16,31 @@ export type KeyValue = Selectable<KeyValueTable>;
 export type NewKeyValue = Insertable<KeyValueTable>;
 export type KeyValueUpdate = Updateable<KeyValueTable>;
 export type KeyValueTable = {
-	key: string;
+	/**
+	 * The key of the key-value pair.
+	 *
+	 * Lix prefixes its keys with "lix-" to avoid conflicts with user-defined keys.
+	 *
+	 * @example
+	 *   "namespace-cool-key"
+	 */
+	key: KeyValueKeys;
+	/**
+	 * The value of the key-value pair.
+	 *
+	 * Must be a string. A JSON is a string too ;)
+	 *
+	 * @example
+	 *   "some value"
+	 *   "{ "foo": "bar" }"
+	 *
+	 */
 	value: string;
 };
+
+
+type PredefinedKeys = `lix-${string}`;
+// The string & {} ensures TypeScript recognizes KeyValueKeys
+// as a superset of string, preventing conflicts when using other string values.
+type KeyType = string & {};
+type KeyValueKeys = PredefinedKeys | KeyType;
