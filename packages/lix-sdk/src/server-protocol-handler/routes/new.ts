949bad4bdf147e45cb9422d46e7c0b7e0dde076c
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2024-11-24 19:47:22 -0500

    store lix files by id

--- packages/lix-sdk/src/server-protocol-handler/routes/new.ts
+++ packages/lix-sdk/src/server-protocol-handler/routes/new.ts
@@ -1,19 +1,40 @@
+import { openLixInMemory } from "../../lix/open-lix-in-memory.js";
+import type { Lix } from "../../lix/open-lix.js";
 import type { LspRouteHandler } from "../create-lsp-handler.js";
 
 export const route: LspRouteHandler = async (context) => {
-	const body = await context.request.blob();
+	const blob = await context.request.blob();
 
-	context.storage.set("new", body);
+	let lix: Lix;
 
-	return new Response("hello world", {
-		status: 200,
+	try {
+		lix = await openLixInMemory({ blob });
+	} catch {
+		return new Response(null, {
+			status: 400,
+		});
+	}
+
+	const { value: id } = await lix.db
+		.selectFrom("key_value")
+		.where("key", "=", "lix-id")
+		.selectAll()
+		.executeTakeFirstOrThrow();
+
+	const exists = await context.storage.has(`lix-file-${id}`);
+
+	if (exists) {
+		return new Response(null, {
+			status: 409,
+		});
+	}
+
+	await context.storage.set(`lix-file-${id}`, blob);
+
+	return new Response(JSON.stringify({ id }), {
+		status: 201,
+		headers: {
+			"Content-Type": "application/json",
+		},
 	});
-
-	// if (!body) {
-	// 	return new Response(null, {
-	// 		status: 400,
-	// 	});
-	// }
-
-	// throw new Error("Not implemented");
 };
