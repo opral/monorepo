1eb032e4875e3976bd9c107f2ed7097a7ebe9937
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2024-11-27 18:59:29 -0500

    implement pull from server

--- packages/lix-sdk/src/sync/pull-from-server.ts
+++ packages/lix-sdk/src/sync/pull-from-server.ts
@@ -1,64 +1,44 @@
 import type { Lix } from "../lix/open-lix.js";
-import type { LixDatabaseSchema, LixFile } from "../database/schema.js";
-import * as LixServerProtocol from "@lix-js/server-api-schema";
+import type { LixDatabaseSchema } from "../database/schema.js";
+import * as LixServerApi from "@lix-js/server-api-schema";
 
 export async function pullFromServer(args: {
 	id: string;
 	lix: Lix;
 	serverUrl: string;
-	tableNames: Array<keyof LixDatabaseSchema>;
 }): Promise<void> {
-	for (const name of args.tableNames) {
-		let sqlQuery = `SELECT * FROM ${name}`;
-
-		if (name === "snapshot") {
-			sqlQuery = `SELECT json(content) as content FROM snapshot`;
-		} else if (name === "file") {
-			// don't sync the data column
-			sqlQuery = `SELECT id, path, metadata FROM file`;
-		}
-
-		const response = await fetch(
-			new Request(`${args.serverUrl}/lsa/lix/${args.id}/query`, {
-				method: "POST",
-				body: JSON.stringify({
-					sql: sqlQuery,
-				}),
-				headers: {
-					"Content-Type": "application/json",
+	const response = await fetch(
+		new Request(`${args.serverUrl}/lsa/sync/pull-v1`, {
+			method: "POST",
+			body: JSON.stringify({
+				lix_id: args.id,
+				vector_clock: {
+					session: "123e4567-e",
+					time: 123456789,
 				},
-			})
-		);
-		if (!response.ok) {
-			throw new Error(`Failed to pull from server: ${response.statusText}`);
-		}
-		const data =
-			(await response.json()) as LixServerProtocol.paths["/lsa/lix/{id}/query"]["post"]["responses"]["200"]["content"]["application/json"];
-
-		if (name === "file") {
-			// await sql`
-			// 	INSERT INTO file (id, path, data, metadata)
-			// 	VALUES ('file0', '/hello.txt', X'${data.rows[0].data}', NULL)
-			// 	ON CONFLICT (id) DO NOTHING;
-			// `.execute(args.lix.db);
-			await args.lix.db
-				.insertInto("file")
-				.values(
-					(data.rows as LixFile[])?.map((row) => ({
-						id: row.id,
-						metadata: row.metadata,
-						path: row.path,
-						data: new Uint8Array([]),
-					})) ?? []
-				)
-				.onConflict((oc) => oc.doNothing())
-				.execute();
-		} else {
-			await args.lix.db
-				.insertInto(name)
-				.values(data.rows ?? [])
-				.onConflict((oc) => oc.doNothing())
-				.execute();
-		}
+			} satisfies LixServerApi.paths["/lsa/sync/pull-v1"]["post"]["requestBody"]["content"]["application/json"]),
+			headers: {
+				"Content-Type": "application/json",
+			},
+		})
+	);
+	const body = await response.json();
+	if (response.ok === false) {
+		throw new Error(`Failed to pull from server: ${body.code} ${body.message}`);
 	}
-}
+	const data = (
+		body as LixServerApi.paths["/lsa/sync/pull-v1"]["post"]["responses"]["200"]["content"]["application/json"]
+	).data;
+
+	await args.lix.db.transaction().execute(async (trx) => {
+		for (const { table_name, rows } of data) {
+			if (rows.length > 0) {
+				await trx
+					.insertInto(table_name as keyof LixDatabaseSchema)
+					.values(rows)
+					.onConflict((oc) => oc.doNothing())
+					.execute();
+			}
+		}
+	});
+} 
