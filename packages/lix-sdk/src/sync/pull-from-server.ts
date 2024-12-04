d473112c8fe2e1d783c33b84e7cafbd575061fb8
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2024-12-04 18:15:07 -0500

    fix: only apply changes of version

--- packages/lix-sdk/src/sync/pull-from-server.ts
+++ packages/lix-sdk/src/sync/pull-from-server.ts
@@ -1,10 +1,12 @@
 import type { Lix } from "../lix/open-lix.js";
 import * as LixServerApi from "@lix-js/server-api-schema";
 import { mergeTheirState, type VectorClock } from "./merge-state.js";
+import { applyChanges } from "../plugin/apply-changes.js";
+import type { Change, VersionChange } from "../database/schema.js";
 
 export async function pullFromServer(args: {
 	id: string;
-	lix: Pick<Lix, "db">;
+	lix: Pick<Lix, "db" | "plugin">;
 	serverUrl: string;
 }): Promise<VectorClock> {
 	// 1. get the current vector clock on the client "sessionStatesKnownByTheClient" and send it to the server
@@ -44,10 +46,35 @@ export async function pullFromServer(args: {
 		body as LixServerApi.paths["/lsa/pull-v1"]["post"]["responses"]["200"]["content"]["application/json"]
 	).vector_clock;
 
-	await mergeTheirState({
-		lix: args.lix,
-		sourceVectorClock: sessionStateServer,
-		sourceData: data,
+	const changes = (data["change"] ?? []) as Change[];
+
+	await args.lix.db.transaction().execute(async (trx) => {
+		const currentVersion = await trx
+			.selectFrom("current_version")
+			.select("id")
+			.executeTakeFirstOrThrow();
+
+		const currentVersionChanges = (
+			(data["version_change"] ?? []) as VersionChange[]
+		).filter((vc) => vc.version_id === currentVersion.id);
+
+		// only apply changes that are part of the current version
+		const changesToApply = changes.filter((c) =>
+			currentVersionChanges.some((vc) => vc.change_id === c.id)
+		);
+
+		await mergeTheirState({
+			lix: { ...args.lix, db: trx },
+			sourceVectorClock: sessionStateServer,
+			sourceData: data,
+		});
+
+		if (changesToApply.length > 0) {
+			await applyChanges({
+				lix: { ...args.lix, db: trx },
+				changes: changesToApply,
+			});
+		}
 	});
 
 	return sessionStateServer;
