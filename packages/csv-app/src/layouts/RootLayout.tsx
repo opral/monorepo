a96c74fa6e21fef8f1b89a8a69773e32f5d09d95
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2024-12-04 18:43:42 -0500

    re-arrange buttons

--- packages/csv-app/src/layouts/RootLayout.tsx
+++ packages/csv-app/src/layouts/RootLayout.tsx
@@ -49,6 +49,23 @@ export default function RootLayout(props: { children: JSX.Element }) {
 					<img src="/lix.svg" alt="logo" className="w-8 h-8" />
 					{/* </a> */}
 					<h1 className="font-medium">CSV Demo</h1>
+				</div>
+				<div className="flex gap-3 items-center">
+					<a href="https://github.com/opral/monorepo" target="_blank">
+						<img src="/github-icon.svg" alt="logo" className="w-5 h-5" />
+					</a>
+					<a href="https://discord.gg/gdMPPWy57R" target="_blank">
+						<img
+							src="/discord-icon.svg"
+							alt="logo"
+							className="w-6 h-6 filter grayscale"
+						/>
+					</a>
+					<a href="https://x.com/lixCCS" target="_blank">
+						<img src="/x-icon.svg" alt="logo" className="w-5 h-5" />
+					</a>
+				</div>
+				<div className="flex gap-2 items-center">
 					<SlDropdown>
 						<SlButton slot="trigger" caret size="small">
 							Options
@@ -94,17 +111,6 @@ export default function RootLayout(props: { children: JSX.Element }) {
 							</SlMenuItem>
 						</SlMenu>
 					</SlDropdown>
-				</div>
-				<div className="flex gap-3 items-center">
-					<a href="https://discord.gg/gdMPPWy57R" target="_blank">
-						<img src="/discord-icon.svg" alt="logo" className="w-6 h-6" />
-					</a>
-					<a href="https://x.com/lixCCS" target="_blank">
-						<img src="/x-icon.svg" alt="logo" className="w-5 h-5" />
-					</a>
-					<a href="https://github.com/opral/monorepo" target="_blank">
-						<img src="/github-icon.svg" alt="logo" className="w-5 h-5" />
-					</a>
 				</div>
 			</div>
 			{props.children}
