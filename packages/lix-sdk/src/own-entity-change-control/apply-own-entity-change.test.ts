45f49eaba3b31151e37916dc35662e8f4dac3539
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2024-12-09 22:45:46 -0500

    apply own changes in foreign key order

--- packages/lix-sdk/src/own-entity-change-control/apply-own-entity-change.test.ts
+++ packages/lix-sdk/src/own-entity-change-control/apply-own-entity-change.test.ts
@@ -1,6 +1,10 @@
 import { test, expect } from "vitest";
 import { openLixInMemory } from "../lix/open-lix-in-memory.js";
-import type { Change } from "../database/schema.js";
+import type {
+	Change,
+	ChangeSet,
+	ChangeSetElement,
+} from "../database/schema.js";
 import { applyOwnEntityChanges } from "./apply-own-entity-change.js";
 import { mockJsonSnapshot } from "../snapshot/mock-json-snapshot.js";
 
@@ -15,7 +19,7 @@ test("it should apply insert changes correctly", async () => {
 	const change: Change = {
 		id: "change1",
 		entity_id: "key1",
-		schema_key: "lix_key_value",
+		schema_key: "lix_key_value_table",
 		plugin_key: "lix_own_entity",
 		file_id: "null",
 		snapshot_id: snapshot.id,
@@ -53,7 +57,7 @@ test("it should apply update changes correctly", async () => {
 
 	const change: Change = {
 		id: "change1",
-		schema_key: "lix_key_value",
+		schema_key: "lix_key_value_table",
 		entity_id: "key1",
 		file_id: "null",
 		created_at: "2021-01-01T00:00:00.000Z",
@@ -89,7 +93,7 @@ test("it should apply delete changes correctly", async () => {
 
 	const change: Change = {
 		id: "change1",
-		schema_key: "lix_key_value",
+		schema_key: "lix_key_value_table",
 		file_id: "null",
 		created_at: "2021-01-01T00:00:00.000Z",
 		entity_id: "key1",
@@ -113,7 +117,7 @@ test("it should throw an error for invalid plugin key", async () => {
 
 	const change: Change = {
 		id: "change1",
-		schema_key: "lix_key_value",
+		schema_key: "lix_key_value_table",
 		entity_id: "key1",
 		file_id: "null",
 		created_at: "2021-01-01T00:00:00.000Z",
@@ -139,7 +143,6 @@ test("it should throw an error for invalid plugin key", async () => {
 		"Expected 'lix_own_entity' as plugin key but received invalid-plugin"
 	);
 });
-
 
 test("file.data is not changed by applyOwnEntityChanges", async () => {
 	const lix = await openLixInMemory({});
@@ -205,4 +208,119 @@ test("file.data is not changed by applyOwnEntityChanges", async () => {
 		path: "/test.txt",
 		metadata: { foo: "baz" },
 	});
+});
+
+test("foreign key constraints are deferred to make the order of applying changes irrelevant", async () => {
+	const lix = await openLixInMemory({});
+
+	const snapshots = [
+		mockJsonSnapshot({
+			id: "change-set-1",
+		} satisfies ChangeSet),
+		mockJsonSnapshot({
+			change_id: "change0",
+			change_set_id: "change-set-1",
+		} satisfies ChangeSetElement),
+	] as const;
+
+	const mockChanges: Change[] = [
+		// applying changes in reverse order
+		{
+			id: "change2",
+			entity_id: "change-set-1,change0",
+			schema_key: "lix_change_set_element_table",
+			plugin_key: "lix_own_entity",
+			file_id: "null",
+			snapshot_id: snapshots[1].id,
+			created_at: "2021-01-01T00:00:00.000Z",
+		},
+		{
+			id: "change1",
+			entity_id: "change-set-1",
+			schema_key: "lix_change_set_table",
+			plugin_key: "lix_own_entity",
+			file_id: "null",
+			snapshot_id: snapshots[0].id,
+			created_at: "2021-01-01T00:00:00.000Z",
+		},
+	];
+
+	for (const snapshot of snapshots) {
+		// @ts-expect-error - 'cannot' insert into generated column error
+		delete snapshot.id;
+	}
+
+	await lix.db
+		.insertInto("snapshot")
+		.values(snapshots.map((s) => ({ content: s.content })))
+		.execute();
+
+	// insert change that the change set element references
+	await lix.db
+		.insertInto("change")
+		.values({
+			id: "change0",
+			plugin_key: "mock",
+			file_id: "null",
+			entity_id: "mock",
+			schema_key: "mock",
+			snapshot_id: "no-content",
+		})
+		.execute();
+
+	await expect(
+		applyOwnEntityChanges({ lix, changes: mockChanges })
+	).resolves.toBeUndefined();
+});
+
+test("foreign key constraints are obeyed", async () => {
+	const lix = await openLixInMemory({});
+
+	const snapshots = [
+		mockJsonSnapshot({
+			// both change 0 and the change set are missing
+			change_id: "change0",
+			change_set_id: "change-set-1",
+		} satisfies ChangeSetElement),
+	] as const;
+
+	const mockChanges: Change[] = [
+		{
+			id: "change2",
+			// the change set for this change does not exist
+			entity_id: "change-set-1,change0",
+			schema_key: "lix_change_set_element_table",
+			plugin_key: "lix_own_entity",
+			file_id: "null",
+			snapshot_id: snapshots[0].id,
+			created_at: "2021-01-01T00:00:00.000Z",
+		},
+	];
+
+	for (const snapshot of snapshots) {
+		// @ts-expect-error - 'cannot' insert into generated column error
+		delete snapshot.id;
+	}
+
+	await lix.db
+		.insertInto("snapshot")
+		.values(snapshots.map((s) => ({ content: s.content })))
+		.execute();
+
+	// insert change that the change set element references
+	await lix.db
+		.insertInto("change")
+		.values({
+			id: "change0",
+			plugin_key: "mock",
+			file_id: "null",
+			entity_id: "mock",
+			schema_key: "mock",
+			snapshot_id: "no-content",
+		})
+		.execute();
+
+	expect(
+		applyOwnEntityChanges({ lix, changes: mockChanges })
+	).rejects.toThrow();
 });
