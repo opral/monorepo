270c47777ecc6a4b2f951cf19cb8cd7462dacb46
Loris Sigrist <loris@sigrist.dev>
2024-12-11 22:14:06 +0100

    Fix CI

--- inlang/source-code/paraglide/paraglide-unplugin/vitest.config.ts
+++ inlang/source-code/paraglide/paraglide-unplugin/vitest.config.ts
@@ -1,10 +1,6 @@
 import { defineConfig } from "vitest/config"
-import { createVitePlugin } from "unplugin"
 import { paraglide } from "./src/index"
-import { virtual } from "./src/virtual"
 import path from "path"
-
-const virtualPlugin = createVitePlugin(virtual)
 
 export default defineConfig({
 	test: {
@@ -17,16 +13,6 @@ export default defineConfig({
 		},
 	},
 	plugins: [
-		virtualPlugin({
-			name: "$virtual",
-			getModule(path) {
-				const files = {
-					"file1.js": "export { default } from './file2.js'",
-					"file2.js": "export default true",
-				}
-				return files[path]
-			},
-		}),
 		// set up the vite plugin
 		// this is used to test the virtual modules in `e2e.test.ts`
 
