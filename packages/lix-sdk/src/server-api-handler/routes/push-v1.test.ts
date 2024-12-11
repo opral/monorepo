7768c05db097576820f2d18eb4c89312eb655ecf
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2024-12-10 23:07:51 -0500

    detects conflicts in push

--- packages/lix-sdk/src/server-api-handler/routes/push-v1.test.ts
+++ packages/lix-sdk/src/server-api-handler/routes/push-v1.test.ts
@@ -1,10 +1,14 @@
-import { test, expect } from "vitest";
+import { test, expect, vi } from "vitest";
 import * as LixServerApi from "@lix-js/server-api-schema";
 import { createServerApiMemoryStorage } from "../storage/create-memory-storage.js";
 import { openLixInMemory } from "../../lix/open-lix-in-memory.js";
 import { createServerApiHandler } from "../create-server-api-handler.js";
 import type { Change } from "../../database/schema.js";
 import { mockChange } from "../../change/mock-change.js";
+import { getDiffingRows } from "../../sync/get-diffing-rows.js";
+import { createVersion } from "../../version/create-version.js";
+import { switchVersion } from "../../version/switch-version.js";
+import { pullFromServer } from "../../sync/pull-from-server.js";
 
 test("it should push data successfully", async () => {
 	const lix = await openLixInMemory({});
@@ -147,4 +151,93 @@ test("it should return 400 for a failed insert operation", async () => {
 	const responseJson = await response.json();
 	expect(responseJson.code).toBe("FAILED_TO_INSERT_DATA");
 	expect(responseJson.message).toBeDefined();
+});
+
+test("it should detect conflicts", async () => {
+	let lixOnServer = await openLixInMemory({});
+
+	// ensure that both client and server create
+	// changes in the same version
+	const version0 = await createVersion({
+		lix: lixOnServer,
+	});
+	await switchVersion({ lix: lixOnServer, to: version0 });
+
+	// initialize client
+	const lixOnClient = await openLixInMemory({
+		blob: await lixOnServer.toBlob(),
+	});
+
+	const lixId = await lixOnServer.db
+		.selectFrom("key_value")
+		.where("key", "=", "lix_id")
+		.selectAll()
+		.executeTakeFirstOrThrow();
+
+	const storage = createServerApiMemoryStorage();
+
+	const lsaHandler = await createServerApiHandler({ storage });
+
+	global.fetch = vi.fn((request) => lsaHandler(request));
+
+	// server has/creates value0 for mock_key
+	await lixOnServer.db
+		.insertInto("key_value")
+		.values({ key: "mock_key", value: "value0" })
+		.execute();
+
+	await storage.set(`lix-file-${lixId.value}`, await lixOnServer.toBlob());
+
+	// client has/creates value1 for mock_key
+	await lixOnClient.db
+		.insertInto("key_value")
+		.values({ key: "mock_key", value: "value1" })
+		.execute();
+
+	const { upsertedRows: tableRowsToPush, state } = await getDiffingRows({
+		lix: lixOnClient,
+		targetVectorClock: [],
+	});
+
+	const response = await lsaHandler(
+		new Request("http://localhost:3000/lsa/push-v1", {
+			method: "POST",
+			body: JSON.stringify({
+				lix_id: lixId.value,
+				vector_clock: state,
+				data: tableRowsToPush,
+			} satisfies LixServerApi.paths["/lsa/push-v1"]["post"]["requestBody"]["content"]["application/json"]),
+			headers: {
+				"Content-Type": "application/json",
+			},
+		})
+	);
+
+	expect(response.status).toBe(201);
+
+	lixOnServer = await openLixInMemory({
+		blob: await storage.get(`lix-file-${lixId.value}`),
+	});
+
+	const conflictsOnServer = await lixOnServer.db
+		.selectFrom("version_change_conflict")
+		.where("version_id", "=", version0.id)
+		.selectAll()
+		.execute();
+
+	expect(conflictsOnServer).not.toHaveLength(0);
+
+	await pullFromServer({
+		id: lixId.value,
+		lix: lixOnClient,
+		serverUrl: "http://localhost:3000",
+	});
+
+	const conflictsOnClient = await lixOnClient.db
+		.selectFrom("version_change_conflict")
+		.where("version_id", "=", version0.id)
+		.selectAll()
+		.execute();
+
+	expect(conflictsOnClient).toEqual(conflictsOnServer);
 });
--- packages/lix-sdk/src/server-api-handler/routes/push-v1.ts
+++ packages/lix-sdk/src/server-api-handler/routes/push-v1.ts
@@ -3,6 +3,10 @@ import type { LixServerApiHandlerRoute } from "../create-server-api-handler.js";
 import { openLixInMemory } from "../../lix/open-lix-in-memory.js";
 import type { Lix } from "../../lix/open-lix.js";
 import { mergeTheirState } from "../../sync/merge-state.js";
+import type { Change } from "../../database/schema.js";
+import { detectChangeConflicts } from "../../change-conflict/detect-change-conflicts.js";
+import { createChangeConflict } from "../../change-conflict/create-change-conflict.js";
+import { CompiledQuery } from "kysely";
 
 type RequestBody =
 	LixServerApi.paths["/lsa/push-v1"]["post"]["requestBody"]["content"]["application/json"];
@@ -34,40 +38,119 @@ export const route: LixServerApiHandlerRoute = async (context) => {
 	}
 
 	try {
-		console.log(
-			"----------- PROCESSING PUSH FROM CLIENT  --s----------- with state:",
-			body.vector_clock
-		);
-		await mergeTheirState({
-			lix,
-			sourceVectorClock: body.vector_clock,
-			sourceData: body.data,
+		// console.log(
+		// 	"----------- PROCESSING PUSH FROM CLIENT  --s----------- with state:",
+		// 	body.vector_clock
+		// );
+
+		await lix.db.transaction().execute(async (trx) => {
+			await trx.executeQuery(
+				CompiledQuery.raw("PRAGMA defer_foreign_keys = ON;")
+			);
+
+			await mergeTheirState({
+				lix: { ...lix, db: trx },
+				sourceVectorClock: body.vector_clock,
+				sourceData: body.data,
+			});
+
+			const allIncomingChanges = (body.data.change ?? []) as Change[];
+
+			// retrieve the version change updates from the changes
+			const incomingVersionChanges: Record<string, Change[]> = {};
+
+			for (const change of (body.data.change ?? []) as Change[]) {
+				if (change.schema_key !== "lix_version_change_table") {
+					continue;
+				}
+				const [versionId] = change.entity_id.split(",");
+				if (versionId === undefined) {
+					throw new Error("Expected versionId to be defined");
+				}
+				// safe assignment
+				if (!incomingVersionChanges[versionId]) {
+					incomingVersionChanges[versionId] = [];
+				}
+				incomingVersionChanges[versionId].push(change);
+			}
+
+			for (const [versionId, versionChanges] of Object.entries(
+				incomingVersionChanges
+			)) {
+				const incomingChanges = versionChanges.map((change) => {
+					const [, changeId] = change.entity_id.split(",");
+					const foundChange = allIncomingChanges.find((c) => c.id === changeId);
+					if (foundChange === undefined) {
+						throw new Error(
+							"Change not found. Expected to find a change for the version change in the incoming changes."
+						);
+					}
+					return foundChange;
+				});
+				const existingChanges: Change[] = [];
+
+				// manually constructing the symmetric difference
+				// between the incoming version and existing version
+				await Promise.all(
+					incomingChanges.map(async (change) => {
+						const existingChange = await trx
+							.selectFrom("version_change")
+							.innerJoin("change", "version_change.change_id", "change.id")
+							.where("version_change.version_id", "=", versionId)
+							.where("change.file_id", "=", change.file_id)
+							.where("change.schema_key", "=", change.schema_key)
+							.where("change.entity_id", "=", change.entity_id)
+							// only select the change if it differs from the incoming change
+							.where("change.id", "!=", change.id)
+							.selectAll("change")
+							.executeTakeFirst();
+						if (existingChange) {
+							existingChanges.push(existingChange);
+						}
+					})
+				);
+
+				const detectedConflicts = await detectChangeConflicts({
+					lix: { ...lix, db: trx },
+					changes: [...incomingChanges, ...existingChanges],
+				});
+
+				for (const conflict of detectedConflicts) {
+					await createChangeConflict({
+						lix: { ...lix, db: trx },
+						key: conflict.key,
+						version: { id: versionId },
+						conflictingChangeIds: new Set(conflict.conflictingChangeIds),
+					});
+				}
+			}
+
+			// const allmlog = await trx
+			// 	.selectFrom("mutation_log")
+			// 	.selectAll()
+			// 	// .select(({ fn }) => {
+			// 	// 	return ["session", fn.max<number>("session_time").as("time")];
+			// 	// })
+			// 	// .groupBy("session")
+			// 	.execute();
+
+			// const vcAfterPush = await trx
+			// 	.selectFrom("mutation_log")
+			// 	.select(({ fn }) => {
+			// 		return ["session", fn.max<number>("session_time").as("time")];
+			// 	})
+			// 	.groupBy("session")
+			// 	.execute();
+
+			// console.log(
+			// 	"----------- DONE PROCESSING PUSH FROM CLIENT  ---- ---------",
+			// 	vcAfterPush,
+			// 	allmlog.length
+			// );
 		});
-
-		const allmlog = await lix.db
-			.selectFrom("mutation_log")
-			.selectAll()
-			// .select(({ fn }) => {
-			// 	return ["session", fn.max<number>("session_time").as("time")];
-			// })
-			// .groupBy("session")
-			.execute();
-
-		const vcAfterPush = await lix.db
-			.selectFrom("mutation_log")
-			.select(({ fn }) => {
-				return ["session", fn.max<number>("session_time").as("time")];
-			})
-			.groupBy("session")
-			.execute();
 
 		await context.storage.set(`lix-file-${body.lix_id}`, await lix.toBlob());
 
-		console.log(
-			"----------- DONE PROCESSING PUSH FROM CLIENT  ---- ---------",
-			vcAfterPush,
-			allmlog.length
-		);
 		return new Response(null, {
 			status: 201,
 		});
