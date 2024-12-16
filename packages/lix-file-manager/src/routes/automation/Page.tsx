fd65de02204b543b8a321bff0c116c791cf3eb35
NiklasBuchfink <niklas.buchfink@web.de>
2024-12-16 14:44:50 +0100

    fix: automation layout

--- packages/lix-file-manager/src/routes/automation/Page.tsx
+++ packages/lix-file-manager/src/routes/automation/Page.tsx
@@ -151,7 +151,7 @@ export default function Page() {
 					<div className="absolute right-0 top-0 bottom-0 w-1 bg-slate-200 opacity-0 group-hover:opacity-100" />
 				</div>
 			</div>
-			<Separator orientation="vertical" className="h-full" />
+			<Separator orientation="vertical" className="h-[calc(100vh_-_36px)]" />
 			<div className="flex-1 flex flex-col">
 				<SectionHeader title={dummyAutomations[activeAutomation].name}>
 					<div className="flex gap-2 items-center">
