82d076a799587f85759bcf6d10d6ab0249aaed42
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2024-12-19 12:59:47 -0500

    fix: queries against closed database
    closes https://github.com/opral/lix-sdk/issues/226

--- packages/lix-sdk/src/file-queue/file-queue-process.ts
+++ packages/lix-sdk/src/file-queue/file-queue-process.ts
@@ -27,6 +27,12 @@ export async function initFileQueueProcess(args: {
 	let hasMoreEntriesSince: number | undefined = undefined;
 
 	async function queueWorker(trail = false) {
+
+		if (args.lix.sqlite.isOpen() === false) {
+			console.log("sqlite is closed");
+			return;
+		}
+
 		try {
 			if (pending && !trail) {
 				hasMoreEntriesSince = runNumber;
