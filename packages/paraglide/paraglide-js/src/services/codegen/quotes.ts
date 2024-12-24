993af0def74eca5cddddef3d9ce4e46e6d4308a4
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2024-12-24 16:57:36 +0100

    move to packages

--- packages/paraglide/paraglide-js/src/services/codegen/quotes.ts
+++
@@ -1,4 +0,0 @@
-/** Wrap the given string in double quotes */
-export const doubleQuote = (str: string) => `"${str.replace(/"/g, '\\"')}"`;
-/** Wrap the given string in backticks */
-export const backtick = (str: string) => `\`${str}\``;
