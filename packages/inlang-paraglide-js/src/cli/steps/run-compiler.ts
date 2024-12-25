5928ad4385cabef7bf45632208ec0516714e914e
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2024-12-25 17:48:04 +0100

    refactor: use `compileProject()`

--- packages/inlang-paraglide-js/src/cli/steps/run-compiler.ts
+++ packages/inlang-paraglide-js/src/cli/steps/run-compiler.ts
@@ -1,9 +1,9 @@
-import { selectBundleNested, type InlangProject } from "@inlang/sdk";
+import { type InlangProject } from "@inlang/sdk";
 import type { CliStep } from "../utils.js";
 import path from "node:path";
-import { compile } from "../../compiler/compile.js";
 import { writeOutput } from "../../services/file-handling/write-output.js";
 import fs from "node:fs";
+import { compileProject } from "../../compiler/compileProject.js";
 
 export const runCompiler: CliStep<
 	{
@@ -14,14 +14,11 @@ export const runCompiler: CliStep<
 	unknown
 > = async (ctx) => {
 	const absoluteOutdir = path.resolve(process.cwd(), ctx.outdir);
-	const bundles = await selectBundleNested(ctx.project.db).execute();
-	const settings = await ctx.project.settings.get();
 
-	const output = await compile({
-		bundles,
-		settings,
+	const output = await compileProject({
+		project: ctx.project,
 	});
 
 	await writeOutput(absoluteOutdir, output, ctx.fs);
-	return { ...ctx, bundles };
+	return { ...ctx };
 };
