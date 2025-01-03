b3fe6bbe798722c549170a0b29a409bd8361b01f
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2025-01-03 12:04:41 -0500

    Vendors legacy v1 `machineTranslateMessage` function to enable legacy v1 apps to machine translate https://github.com/opral/inlang-cli/issues/29.

--- packages/rpc/src/functions/index.ts
+++ packages/rpc/src/functions/index.ts
@@ -1,9 +1,11 @@
 import { subscribeNewsletter } from "./subscribeNewsletter.js";
 import { subscribeCategory } from "./subscribeCategory.js";
 import { machineTranslateBundle } from "./machineTranslateBundle.js";
+import { machineTranslateMessage } from "./machineTranslateMessage.js";
 
 export const allRpcs = {
 	machineTranslateBundle,
+	machineTranslateMessage,
 	subscribeNewsletter,
 	subscribeCategory,
 };
