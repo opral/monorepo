7916b7688c44fb0603c7fa84f19b6aa818ef7afb
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2025-01-08 15:08:27 -0500

    add react example

--- inlang/packages/paraglide-js/examples/react/vite.config.js
+++ inlang/packages/paraglide-js/examples/react/vite.config.js
@@ -1,19 +1,14 @@
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import { paraglideVitePlugin } from "@inlang/paraglide-js";
 
+// https://vite.dev/config/
 export default defineConfig({
 	plugins: [
+		react(),
 		paraglideVitePlugin({
 			project: "./project.inlang",
 			outdir: "./src/paraglide",
-			options: {
-				outputStructure: "locale-modules",
-			},
 		}),
 	],
-	build: {
-		// eases debugging
-		minify: false,
-	},
 });
-
