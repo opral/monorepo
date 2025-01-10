13ec7cb987d4c0bd98698cd7fe4ee6cfa3252b2b
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2025-01-10 09:10:40 -0500

    add old load and save messages function

--- inlang/packages/plugins/inlang-message-format/src/plugin.ts
+++ inlang/packages/plugins/inlang-message-format/src/plugin.ts
@@ -8,17 +8,17 @@ import { exportFiles } from "./import-export/exportFiles.js";
 export const PLUGIN_KEY = "plugin.inlang.messageFormat";
 
 export const plugin: InlangPlugin<{
-  [PLUGIN_KEY]?: PluginSettings;
+	[PLUGIN_KEY]?: PluginSettings;
 }> = {
-  key: PLUGIN_KEY,
-  // legacy v2 stuff for backwards compatibility
-  // given that most people don't have a major version
-  // pinning in their settings
-  id: pluginV2.id,
-  // loadMessages: pluginV2.loadMessages,
-  // saveMessages: pluginV2.saveMessages,
-  settingsSchema: PluginSettings,
-  toBeImportedFiles,
-  importFiles,
-  exportFiles,
+	key: PLUGIN_KEY,
+	// legacy v2 stuff for backwards compatibility
+	// given that most people don't have a major version
+	// pinning in their settings
+	id: pluginV2.id,
+	loadMessages: pluginV2.loadMessages,
+	saveMessages: pluginV2.saveMessages,
+	settingsSchema: PluginSettings,
+	toBeImportedFiles,
+	importFiles,
+	exportFiles,
 };
