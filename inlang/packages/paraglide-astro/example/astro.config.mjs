08c169915051f103260ed8405381f0b6b884bc81
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2025-01-14 17:07:49 -0500

    update dependencies

--- inlang/packages/paraglide-astro/example/astro.config.mjs
+++ inlang/packages/paraglide-astro/example/astro.config.mjs
@@ -1,46 +1,44 @@
 import paraglide from "@inlang/paraglide-astro"
-import { defineConfig } from "astro/config"
-import mdx from "@astrojs/mdx"
+import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap"
 import svelte, { vitePreprocess } from "@astrojs/svelte"
 import node from "@astrojs/node"
 
 // https://astro.build/config
 export default defineConfig({
-	//configure this to your domain name
-	//site: "https://acme.com",
+  //configure this to your domain name
+  //site: "https://acme.com",
 
-	i18n: {
-		defaultLocale: "en",
-		locales: [
-			"en",
-			{
-				codes: ["de"],
-				path: "deutsch",
-			},
-		],
-	},
-	integrations: [
-		mdx(),
-		sitemap({
-			i18n: {
-				defaultLocale: "en",
-				locales: {
-					en: "en-US",
-					de: "de-CH",
-				},
-			},
-		}),
-		svelte({
-			preprocess: [vitePreprocess()],
-		}),
-		paraglide({
-			project: "./project.inlang",
-			outdir: "./src/paraglide",
-		}),
-	],
-	output: "server",
-	adapter: node({
-		mode: "standalone",
-	}),
-})
+  i18n: {
+    defaultLocale: "en",
+    locales: [
+      "en",
+      {
+        codes: ["de"],
+        path: "deutsch",
+      },
+    ],
+  },
+  integrations: [
+    sitemap({
+      i18n: {
+        defaultLocale: "en",
+        locales: {
+          en: "en-US",
+          de: "de-CH",
+        },
+      },
+    }),
+    svelte({
+      preprocess: [vitePreprocess()],
+    }),
+    paraglide({
+      project: "./project.inlang",
+      outdir: "./src/paraglide",
+    }),
+  ],
+  output: "server",
+  adapter: node({
+    mode: "standalone",
+  }),
+});
