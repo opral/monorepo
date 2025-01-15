ec969ce08529339aac12ea1e49b794fbf23273aa
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2025-01-15 18:44:38 -0500

    example

--- packages/paraglide-js/examples/astro/astro.config.mjs
+++ packages/paraglide-js/examples/astro/astro.config.mjs
@@ -1,9 +1,15 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import { paraglideVitePlugin } from "@inlang/paraglide-js";
 
 // https://astro.build/config
 export default defineConfig({
+	i18n: {
+		defaultLocale: "en",
+		locales: ["en", "de"],
+		routing: {
+			prefixDefaultLocale: false,
+		},
+	},
 	vite: {
 		plugins: [
 			paraglideVitePlugin({
