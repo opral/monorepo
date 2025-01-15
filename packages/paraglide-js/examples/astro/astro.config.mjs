559d775f2319aa2e6f9e6068bf5ed10796add242
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2025-01-15 16:46:06 -0500

    add vite

--- packages/paraglide-js/examples/astro/astro.config.mjs
+++ packages/paraglide-js/examples/astro/astro.config.mjs
@@ -1,5 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import { paraglideVitePlugin } from "@inlang/paraglide-js";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	vite: {
+		plugins: [
+			paraglideVitePlugin({
+				project: "./project.inlang",
+				outdir: "./src/paraglide",
+			}),
+		],
+	},
+});
