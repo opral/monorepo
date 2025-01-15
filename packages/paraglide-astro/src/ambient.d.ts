a318f2d699d6172db4f0f03b9f4cc9304f242b0e
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2025-01-15 15:24:30 -0500

    format

--- packages/paraglide-astro/src/ambient.d.ts
+++ packages/paraglide-astro/src/ambient.d.ts
@@ -4,23 +4,23 @@
  */
 declare module "virtual:paraglide-astro:runtime" {
 	export const defineGetLocale: (fn: () => string) => void;
-  export const defineSetLocale: (fn: (locale: string) => void) => void;
-  export const getLocale: () => string;
-  export const isAvailableLocale: (locale: unknown) => locale is string;
-  export const locales: readonly string[];
-  export const baseLocale: string;
+	export const defineSetLocale: (fn: (locale: string) => void) => void;
+	export const getLocale: () => string;
+	export const isAvailableLocale: (locale: unknown) => locale is string;
+	export const locales: readonly string[];
+	export const baseLocale: string;
 }
 
 declare module "astro:i18n" {
-	export const getLocaleByPath: (path: string) => string
-	export const getPathByLocale: (locale: string) => string
+	export const getLocaleByPath: (path: string) => string;
+	export const getPathByLocale: (locale: string) => string;
 }
 
 declare namespace App {
 	interface Locals {
 		paraglide: {
-			lang: string
-			dir: "ltr" | "rtl"
-		}
+			lang: string;
+			dir: "ltr" | "rtl";
+		};
 	}
 }
--- packages/paraglide-astro/src/middleware.ts
+++ packages/paraglide-astro/src/middleware.ts
@@ -1,8 +1,8 @@
 import {
-  baseLocale,
-  defineGetLocale,
-  defineSetLocale,
-  isAvailableLocale,
+	baseLocale,
+	defineGetLocale,
+	defineSetLocale,
+	isAvailableLocale,
 } from "virtual:paraglide-astro:runtime";
 import { type MiddlewareHandler } from "astro";
 import { AsyncLocalStorage } from "node:async_hooks";
@@ -10,87 +10,87 @@ import { AsyncLocalStorage } from "node:async_hooks";
 type NormalizedBase = `/${string}` | "";
 
 declare global {
-  interface ImportMeta {
-    env: {
-      /**
-       * The base URL set in the astro.config.
-       *
-       * May include a trailing slash.
-       * If no base is set, it defaults to "/".
-       */
-      BASE_URL: string;
-    };
-  }
+	interface ImportMeta {
+		env: {
+			/**
+			 * The base URL set in the astro.config.
+			 *
+			 * May include a trailing slash.
+			 * If no base is set, it defaults to "/".
+			 */
+			BASE_URL: string;
+		};
+	}
 }
 
 const asyncStorage = new AsyncLocalStorage<string>();
 
 defineGetLocale(() => {
-  const maybeLocale = asyncStorage.getStore();
-  return maybeLocale ?? baseLocale;
+	const maybeLocale = asyncStorage.getStore();
+	return maybeLocale ?? baseLocale;
 });
 
 defineSetLocale(() => {
-  // do nothing on the server
+	// do nothing on the server
 });
 
 export const onRequest: MiddlewareHandler = async (
-  { url, locals, currentLocale },
-  next
+	{ url, locals, currentLocale },
+	next,
 ) => {
-  const normalizedBase = normalizeBase(import.meta.env.BASE_URL);
+	const normalizedBase = normalizeBase(import.meta.env.BASE_URL);
 
-  const locale =
-    currentLocale ??
-    getLocaleFromPath(url.pathname, normalizedBase) ??
-    baseLocale;
+	const locale =
+		currentLocale ??
+		getLocaleFromPath(url.pathname, normalizedBase) ??
+		baseLocale;
 
-  locals.paraglide = {
-    lang: locale,
-    dir: guessTextDirection(locale),
-  };
+	locals.paraglide = {
+		lang: locale,
+		dir: guessTextDirection(locale),
+	};
 
-  return await asyncStorage.run(locale, next);
+	return await asyncStorage.run(locale, next);
 };
 
 function normalizeBase(rawBase: string): NormalizedBase {
-  if (rawBase === "/") return "";
-  // if there is a trailing slash, remove it
-  return (
-    rawBase.endsWith("/") ? rawBase.slice(0, -1) : rawBase
-  ) as NormalizedBase;
+	if (rawBase === "/") return "";
+	// if there is a trailing slash, remove it
+	return (
+		rawBase.endsWith("/") ? rawBase.slice(0, -1) : rawBase
+	) as NormalizedBase;
 }
 
 function getLocaleFromPath(
-  path: string,
-  base: NormalizedBase
+	path: string,
+	base: NormalizedBase,
 ): string | undefined {
-  if (!path.startsWith(base)) return undefined;
+	if (!path.startsWith(base)) return undefined;
 
-  const withoutBasePath = path.replace(base, "");
-  const maybeLocale = withoutBasePath.split("/").find(Boolean); // get the first segment
-  return isAvailableLocale(maybeLocale) ? maybeLocale : undefined;
+	const withoutBasePath = path.replace(base, "");
+	const maybeLocale = withoutBasePath.split("/").find(Boolean); // get the first segment
+	return isAvailableLocale(maybeLocale) ? maybeLocale : undefined;
 }
 
 function guessTextDirection(lang: string): "ltr" | "rtl" {
 	try {
-		const locale = new Intl.Locale(lang)
+		const locale = new Intl.Locale(lang);
 
 		// Node
 		if ("textInfo" in locale) {
 			// @ts-ignore
-			return locale.textInfo.direction
+			return locale.textInfo.direction;
 		}
 
 		// Spec compliant (future proofing)
 		if ("getTextInfo" in locale) {
 			// @ts-ignore
-			return locale.getTextInfo().direction
+			return locale.getTextInfo().direction;
 		}
 
 		// Fallback
-		return "ltr"
+		return "ltr";
 	} catch (e) {
-		return "ltr"
+		return "ltr";
 	}
 }
