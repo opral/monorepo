168c2acbfba31a62af47d0a6d4c34ac90d48fa09
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2025-01-16 13:29:12 -0500

    use compiler args

--- inlang/packages/paraglide-js/src/compiler/compile.ts
+++ inlang/packages/paraglide-js/src/compiler/compile.ts
@@ -1,15 +1,39 @@
 import { loadProjectFromDirectory } from "@inlang/sdk";
 import path from "node:path";
 import { ENV_VARIABLES } from "../services/env-variables/index.js";
-import {
-	compileProject,
-	type ParaglideCompilerOptions,
-} from "./compileProject.js";
+import { compileProject, type CompilerOptions } from "./compileProject.js";
 import { writeOutput } from "../services/file-handling/write-output.js";
 import {
 	getLocalAccount,
 	saveLocalAccount,
 } from "../services/account/index.js";
+
+export type CompilerArgs = {
+	/**
+	 * The path to the project to compile.
+	 *
+	 * @example
+	 *   './project.inlang'
+	 */
+	project: string;
+	/**
+	 * The path to the directory to write the output to.
+	 *
+	 * @example
+	 *   './src/paraglide'
+	 */
+	outdir: string;
+	/**
+	 * Additional compiler options.
+	 */
+	compilerOptions?: CompilerOptions;
+	/**
+	 * The file system to use. Defaults to `await import('node:fs')`.
+	 *
+	 * Useful for testing the paraglide compiler by mocking the fs.
+	 */
+	fs?: typeof import("node:fs");
+};
 
 /**
  * Loads, compiles, and writes the output to disk.
@@ -24,12 +48,7 @@ import {
  *     outdir: 'path/to/output',
  *   })
  */
-export async function compile(args: {
-	project: string;
-	outdir: string;
-	fs?: typeof import("node:fs");
-	compilerOptions?: ParaglideCompilerOptions;
-}): Promise<void> {
+export async function compile(args: CompilerArgs): Promise<void> {
 	const fs = args.fs ?? (await import("node:fs"));
 	const absoluteOutdir = path.resolve(process.cwd(), args.outdir);
 
--- inlang/packages/paraglide-js/src/index.ts
+++ inlang/packages/paraglide-js/src/index.ts
@@ -1,9 +1,2 @@
-export type MessageIndexFunction<T extends string> = (
-	params: Record<string, never>,
-	options: { languageTag: T }
-) => string;
-
-export type MessageFunction = (params?: Record<string, never>) => string;
-
-export { paraglideVitePlugin } from "./bundler-plugins/vite.js";
-export { paraglideRollupPlugin } from "./bundler-plugins/rollup.js";
+export * from "./bundler-plugins/index.js";
+export * from "./compiler/index.js";
