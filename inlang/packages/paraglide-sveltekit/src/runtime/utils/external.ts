5f5b0e55386de2cadaba4c115a017fcaad2783d5
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2025-01-18 18:37:16 -0500

    remove sveltekit adapter

--- inlang/packages/paraglide-sveltekit/src/runtime/utils/external.ts
+++
@@ -1,4 +0,0 @@
-export function isExternal(url: URL, currentUrl: URL, base: string) {
-	const absoluteBase = new URL(base ?? "/", currentUrl).pathname
-	return url.origin !== currentUrl.origin || !url.pathname.startsWith(absoluteBase)
-}
