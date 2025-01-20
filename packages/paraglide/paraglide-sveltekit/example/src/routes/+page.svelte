da21d5f1f8eb30f57a292158edd1da195bd4eba3
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2025-01-19 19:36:29 -0500

    init inlang project

--- packages/paraglide/paraglide-sveltekit/example/src/routes/+page.svelte
+++ packages/paraglide/paraglide-sveltekit/example/src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+  import * as m from "$lib/paraglide/messages"
+</script>
+
+<h1>{m.welcome()}</h1>
--- packages/paraglide/paraglide-sveltekit/example/vite.config.ts
+++ packages/paraglide/paraglide-sveltekit/example/vite.config.ts
@@ -1,6 +1,13 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { paraglideVitePlugin } from '@inlang/paraglide-js';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [
+		sveltekit(),
+		paraglideVitePlugin({
+			project: './project.inlang',
+			outdir: './src/lib/paraglide'
+		})
+	]
 });
