1ba14abdb320452c5b7fac6657dee0cca31c5a8e
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2025-01-20 14:51:05 -0500

    basic i18n routing

--- packages/paraglide/paraglide-sveltekit/example/src/hooks.ts
+++ packages/paraglide/paraglide-sveltekit/example/src/hooks.ts
@@ -1,9 +1,13 @@
-import type { Reroute } from "@sveltejs/kit";
+import type { Reroute } from '@sveltejs/kit';
 
 export const reroute: Reroute = (request) => {
+	if (request.url.pathname === '/de') {
+		return '/';
+	}
+
 	return stripLocaleFromPath(request.url.pathname);
 };
 
 function stripLocaleFromPath(path: string) {
-	return path.replace(/^\/[a-z]{2}\//, "/");
+	return path.replace(/^\/[a-z]{2}\//, '/');
 }
