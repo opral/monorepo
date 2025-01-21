0586d49fdf6e480e02cd370694a5d76a0314c71d
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2025-01-21 17:11:38 -0500

    remove webpack plugin
    can't get a basic example working. webpack has issues with path resolving. the API is so legacy that it's not worth an investment

--- packages/paraglide/paraglide-js/src/bundler-plugins/index.ts
+++ packages/paraglide/paraglide-js/src/bundler-plugins/index.ts
@@ -1,3 +1,2 @@
 export { paraglideVitePlugin } from "./vite.js";
 export { paraglideRollupPlugin } from "./rollup.js";
-export { paraglideWebpackPlugin } from "./webpack.js";
--- packages/paraglide/paraglide-js/src/bundler-plugins/webpack.ts
+++
@@ -1,4 +0,0 @@
-import { createWebpackPlugin } from "unplugin";
-import { unpluginFactory } from "./unplugin.js";
-
-export const paraglideWebpackPlugin = createWebpackPlugin(unpluginFactory);
