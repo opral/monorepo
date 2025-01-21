ee2bc26b42d6f50d60d8f1958269b50dedd61add
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2025-01-21 13:24:04 -0500

    add readme

--- packages/paraglide/paraglide-sveltekit/example/src/hooks.server.ts
+++ packages/paraglide/paraglide-sveltekit/example/src/hooks.server.ts
@@ -1,4 +1,4 @@
-import type { Handle, Reroute } from '@sveltejs/kit';
+import type { Handle } from '@sveltejs/kit';
 import * as paraglideAdapter from '$lib/paraglide/adapter';
 
 export const handle: Handle = paraglideAdapter.handle;
