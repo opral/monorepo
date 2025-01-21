89ba0eafdaf7758ff26d2e1683970de585155be5
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2025-01-21 17:11:38 -0500

    remove webpack plugin
    can't get a basic example working. webpack has issues with path resolving. the API is so legacy that it's not worth an investment

--- inlang/packages/paraglide/paraglide-next/example/next.config.ts
+++ inlang/packages/paraglide/paraglide-next/example/next.config.ts
@@ -1,18 +1,5 @@
 import type { NextConfig } from "next";
-import { paraglideWebpackPlugin } from "@inlang/paraglide-js";
 
-const nextConfig: NextConfig = {
-	webpack: (config, { isServer }) => {
-		if (isServer) {
-			config.plugins?.push(
-				paraglideWebpackPlugin({
-					project: "./project.inlang",
-					outdir: "./src/app/paraglide",
-				})
-			);
-		}
-		return config;
-	},
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
