4c5a168bdc90604d6c344ab5f57b945f40147855
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2025-01-21 15:41:39 -0500

    init

---
+++ packages/paraglide/paraglide-next/src/runtime.d.ts
@@ -0,0 +1,13 @@
+import type { Runtime } from "@inlang/paraglide-js";
+
+export const baseLocale: Runtime["baseLocale"];
+export const locales: Runtime["locales"];
+export const assertIsLocale: Runtime["assertIsLocale"];
+export const getLocale: Runtime["getLocale"];
+export const setLocale: Runtime["setLocale"];
+export const defineGetLocale: Runtime["defineGetLocale"];
+export const defineSetLocale: Runtime["defineSetLocale"];
+export const isLocale: Runtime["isLocale"];
+export const getLocaleFromPath: Runtime["getLocaleFromPath"];
+export const localizedPath: Runtime["localizedPath"];
+export const deLocalizedPath: Runtime["deLocalizedPath"];
