ec6df2aee500335305a04c237992f98fdb868e44
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2025-01-21 21:24:51 -0500

    remove unused variable

--- inlang/packages/paraglide/paraglide-next/example/next.config.js
+++ inlang/packages/paraglide/paraglide-next/example/next.config.js
@@ -9,7 +9,7 @@ const nextConfig = {
 		// your project has ESLint errors.
 		ignoreDuringBuilds: true,
 	},
-	webpack: (config, { isServer }) => {
+	webpack: (config) => {
 		config.plugins.push(
 			paraglideWebpackPlugin({
 				project: "./project.inlang",
