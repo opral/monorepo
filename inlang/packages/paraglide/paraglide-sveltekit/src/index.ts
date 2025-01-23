6e03f58b3fb0713fc155c2bf5fdb7e04593a4206
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2025-01-23 18:45:14 -0500

    refactor: top level paraglide sveltekit function
    part of https://github.com/opral/inlang-paraglide-js/issues/338

--- inlang/packages/paraglide/paraglide-sveltekit/src/index.ts
+++ inlang/packages/paraglide/paraglide-sveltekit/src/index.ts
@@ -1,5 +1,5 @@
-import type { Adapter } from "@inlang/paraglide-js";
 import fs from "node:fs";
+import { paraglideVitePlugin, type CompilerArgs } from "@inlang/paraglide-js";
 
 /**
  * Reads a file from the file system and returns it as a string.
@@ -8,10 +8,22 @@ const file = (path: string) => ({
 	[path]: fs.readFileSync(new URL(path, import.meta.url), "utf-8"),
 });
 
-export const ParaglideSveltekitAdapter: () => Adapter = () => {
-	const files = {
-		...file("adapter.provider.svelte"),
-		...file("adapter.js"),
-	};
-	return { files };
-};
+/**
+ * A Vite plugin that compiles the inlang project and emits output with
+ * additional files for SvelteKit.
+ *
+ * @example
+ *   paraglideSvelteKit({
+ *      project: './project.inlang',
+ *      outdir: './src/lib/paraglide',
+ *   })
+ */
+export const paraglideSvelteKit = (args: CompilerArgs) =>
+	paraglideVitePlugin({
+		...args,
+		additionalFiles: {
+			...file("adapter.js"),
+			...file("adapter.provider.svelte"),
+			...args.additionalFiles,
+		},
+	});
