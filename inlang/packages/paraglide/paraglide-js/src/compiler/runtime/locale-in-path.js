1d17bad2bb51e4bdbd10e9e2125652735d2248d0
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2025-01-24 17:36:11 -0500

    clean up

--- inlang/packages/paraglide/paraglide-js/src/compiler/runtime/locale-in-path.js
+++ inlang/packages/paraglide/paraglide-js/src/compiler/runtime/locale-in-path.js
@@ -3,13 +3,13 @@
  *
  * @example
  *   const path = '/en/home';
- *   const locale = getLocaleFromPath(path);
+ *   const locale = localeInPath(path);
  *   console.log(locale); // 'en'
  *
  * @param {string} path - The path from which to extract the locale.
  * @returns {Locale|undefined} The extracted locale, or undefined if no locale is found.
  */
-export function getLocaleFromPath(path) {
+export function localeInPath(path) {
 	const [, maybeLocale] = path.split("/");
 	if (isLocale(maybeLocale)) {
 		return maybeLocale;
