33aaf6fe020079c5f87f1314669a7de2b8c06d65
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2025-01-27 16:48:43 -0500

    cookie strategy

--- inlang/packages/paraglide/paraglide-js/examples/react/vite.config.js
+++ inlang/packages/paraglide/paraglide-js/examples/react/vite.config.js
@@ -4,11 +4,20 @@ import { paraglideVitePlugin } from "@inlang/paraglide-js";
 
 // https://vite.dev/config/
 export default defineConfig({
+	build: {
+		// eases debugging
+		minify: false,
+		target: "es2022",
+	},
 	plugins: [
 		react(),
 		paraglideVitePlugin({
 			project: "./project.inlang",
 			outdir: "./src/paraglide",
+			strategy: {
+				type: "cookie",
+				cookieName: "PARAGLIDE_LOCALE",
+			},
 		}),
 	],
 });
