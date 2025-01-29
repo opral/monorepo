39889cf6afc9fc3b9c9a8a9ba828808a96df5a86
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2025-01-29 10:29:15 -0500

    remove count stuff

--- inlang/packages/paraglide/paraglide-sveltekit/example/src/routes/+page.svelte
+++ inlang/packages/paraglide/paraglide-sveltekit/example/src/routes/+page.svelte
@@ -2,13 +2,9 @@
 	import * as m from '$lib/paraglide/messages';
 	import { setLocale } from '$lib/paraglide/runtime';
 
-	let count = $state(0)
 </script>
 
 <h1>{m.welcome()}</h1>
 
 <button onclick={() => setLocale('en')}>Set locale to "en"</button>
 <button onclick={() => setLocale('de')}>Set locale to "de"</button>
-
-<p>Count is {count}</p>
-<button onclick={() => count += 1}>Increment count</button>
