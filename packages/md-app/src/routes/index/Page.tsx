8146b453864f08bace50e045913bc6ef507b19a1
NiklasBuchfink <niklas.buchfink@web.de>
2025-02-06 17:33:07 +0100

    fix: avoid rendering error if activeFile is undefined

--- packages/md-app/src/routes/index/Page.tsx
+++ packages/md-app/src/routes/index/Page.tsx
@@ -2,8 +2,11 @@ import { Toaster } from 'sonner';
 import { PlateEditor } from '@/components/editor/plate-editor';
 import { SettingsProvider } from '@/components/editor/settings';
 import LixMenuDropdown from "@/components/LixMenuDropdown";
+import { activeFileAtom } from '@/state-active-file';
+import { useAtom } from 'jotai/react';
 
 export default function Page() {
+	const [activeFile] = useAtom(activeFileAtom)
 	return (
 		<>
 			<div className="w-full p-2 flex justify-between items-center">
@@ -11,12 +14,14 @@ export default function Page() {
 				<LixMenuDropdown />
 			</div>
 			<div className="w-full border-t-[1px] border-[hsl(var(--border))]"></div>
-			<div className="h-screen w-full" data-registry="plate">
-				<SettingsProvider>
-					<PlateEditor />
-				</SettingsProvider>
-				<Toaster />
-			</div>
+			{activeFile &&
+				<div className="h-screen w-full" data-registry="plate">
+					<SettingsProvider>
+						<PlateEditor />
+					</SettingsProvider>
+					<Toaster />
+				</div>
+			}
 		</>
 	);
 }
