21e67e774e940a1bf5d0c7222d51e696ce92fd4b
Felix Haeberle <kontakt@felixhaeberle.de>
2025-02-12 13:47:50 +0100

    add tailwind

--- packages/sherlock/src/utilities/editor/sherlock-editor-app/vite.config.ts
+++ packages/sherlock/src/utilities/editor/sherlock-editor-app/vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
+import tailwindcss from "@tailwindcss/vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [react(), tailwindcss()],
 	build: {
 		outDir: "build",
 		rollupOptions: {
