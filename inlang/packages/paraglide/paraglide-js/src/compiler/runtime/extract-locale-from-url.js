a71a16148938709beeb7d3cb302d359a59b862e8
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2025-02-13 13:31:36 -0500

    add extract-locale-from-url

--- inlang/packages/paraglide/paraglide-js/src/compiler/runtime/extract-locale-from-url.js
+++ inlang/packages/paraglide/paraglide-js/src/compiler/runtime/extract-locale-from-url.js
@@ -2,24 +2,52 @@ import { assertIsLocale } from "./assert-is-locale.js";
 import { urlPatterns } from "./variables.js";
 
 /**
- * Extracts the locale from a given pathname.
+ * Extracts the locale from a given url.
  *
- * @example
- *   const pathname = '/en/home';
- *   const locale = extractLocaleFromUrl(pathname);
- *   console.log(locale); // 'en'
- *
- * @param {string} url - The pathname from which to extract the locale.
+ * @param {string} url - The full URL from which to extract the locale.
  * @returns {Locale|undefined} The extracted locale, or undefined if no locale is found.
  */
 export function extractLocaleFromUrl(url) {
-	let currentOrigin = window.location.origin;
-	const urlObj = new URL(url, currentOrigin);
+	const urlObj = new URL(url);
 
 	for (const element of urlPatterns) {
 		const match = pathToRegexp.match(element.pattern)(urlObj.href);
+
 		if (match) {
-			return assertIsLocale(element.locale);
+			for (const [locale, overrideParams] of Object.entries(
+				element.localizedParams
+			)) {
+				let allMatch = true;
+
+				for (const [key, val] of Object.entries(overrideParams)) {
+					// overwritten parameter like { locale: null }
+					if (val === null) {
+						if (match.params[key] != null) {
+							allMatch = false;
+							break;
+						}
+					}
+					// wildcard match like `/*domain` where the parameter is an array
+					else if (Array.isArray(val) && Array.isArray(match.params[key])) {
+						for (let i = 0; i < match.params[key].length; i++) {
+							if (match.params[key][i] !== val[i]) {
+								allMatch = false;
+								break;
+							}
+						}
+					}
+					// regular parameter match like `/:domain`
+					else if (match.params[key] !== val) {
+						allMatch = false;
+						break;
+					}
+				}
+
+				if (allMatch) {
+					// e.g. if overrideParams = { locale: 'fr' }, we found locale 'fr'
+					return assertIsLocale(locale);
+				}
+			}
 		}
 	}
 
---
+++ inlang/packages/paraglide/paraglide-js/src/compiler/runtime/extract-locale-from-url.test.ts
@@ -0,0 +1,85 @@
+import { test, expect } from "vitest";
+import { createRuntimeForTesting } from "./create-runtime.js";
+
+test("parameters", async () => {
+	const runtime = await createRuntimeForTesting({
+		baseLocale: "en",
+		locales: ["en", "de"],
+		compilerOptions: {
+			urlPatterns: [
+				{
+					pattern: "http{s}\\://:domain/:bookstore/:item/:id",
+					localizedParams: {
+						de: { bookstore: "buchladen", item: "artikel" },
+						en: { bookstore: "bookstore", item: "item" },
+					},
+				},
+			],
+		},
+	});
+
+	expect(
+		runtime.extractLocaleFromUrl(`https://example.com/bookstore/item/123`)
+	).toBe("en");
+
+	expect(
+		runtime.extractLocaleFromUrl(`https://example.com/buchladen/artikel/123`)
+	).toBe("de");
+
+	expect(
+		runtime.extractLocaleFromUrl(`https://example.com/something/else`)
+	).toBe(undefined);
+});
+
+test("wildcards", async () => {
+	const runtime = await createRuntimeForTesting({
+		baseLocale: "en",
+		locales: ["en", "de"],
+		compilerOptions: {
+			urlPatterns: [
+				{
+					pattern: "http{s}\\://:domain/*path",
+					localizedParams: {
+						de: { domain: "de.example.com", path: ["startseite", "ueber-uns"] },
+						en: { domain: "example.com", path: ["home", "about-us"] },
+					},
+				},
+			],
+		},
+	});
+
+	expect(
+		runtime.extractLocaleFromUrl(`https://de.example.com/startseite/ueber-uns`)
+	).toBe("de");
+
+	expect(
+		runtime.extractLocaleFromUrl(`https://example.com/home/about-us`)
+	).toBe("en");
+});
+
+test("optional parameters", async () => {
+	const runtime = await createRuntimeForTesting({
+		baseLocale: "en",
+		locales: ["en", "de"],
+		compilerOptions: {
+			urlPatterns: [
+				{
+					pattern: "https\\://example.com/:locale{/optional-subpage}",
+					localizedParams: {
+						de: { locale: "de" },
+						en: { locale: "en" },
+					},
+				},
+			],
+		},
+	});
+
+	expect(runtime.extractLocaleFromUrl(`https://example.com/de`)).toBe("de");
+	expect(runtime.extractLocaleFromUrl(`https://example.com/en`)).toBe("en");
+	expect(
+		runtime.extractLocaleFromUrl(`https://example.com/de/optional-subpage`)
+	).toBe("de");
+	expect(
+		runtime.extractLocaleFromUrl(`https://example.com/en/optional-subpage`)
+	).toBe("en");
+});
