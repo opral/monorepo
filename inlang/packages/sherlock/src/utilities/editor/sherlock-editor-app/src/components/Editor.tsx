fd3dfc7285e88809021a6327e2dd8a4d71388610
Felix Haeberle <kontakt@felixhaeberle.de>
2025-02-13 22:08:09 +0100

    apply theming

--- inlang/packages/sherlock/src/utilities/editor/sherlock-editor-app/src/components/Editor.tsx
+++ inlang/packages/sherlock/src/utilities/editor/sherlock-editor-app/src/components/Editor.tsx
@@ -84,56 +84,79 @@ const Editor: React.FC<{
 	}
 
 	return (
-		<ReactInlangBundle bundle={bundle} change={handleChangeEvent}>
-			<ReactInlangBundleAction
-				slot="bundle-action"
-				actionTitle="Delete"
-				onClick={() => handleDelete({ command: "delete-bundle", message: { id: bundle.id } })}
-			/>
-			{bundle.messages.map((message) => (
-				<ReactInlangMessage
-					key={message.id}
-					slot="message"
-					message={message}
-					variants={message.variants}
-					settings={settings}
-				>
-					{message.variants.map((variant) => (
-						<ReactInlangVariant key={variant.id} slot="variant" variant={variant}>
-							<ReactInlangPatternEditor slot="pattern-editor" variant={variant} />
-							<SlDropdown slot="variant-action">
-								<div slot="trigger" className="dropdown-trigger">
-									...
-								</div>
-								<SlMenu>
-									<SlMenuItem onClick={() => setIsDialogOpen(true)}>Add Selector</SlMenuItem>
-									{message.variants.length > 1 && (
-										<SlMenuItem
-											onClick={() =>
-												handleDelete({ command: "delete-variant", message: { id: variant.id } })
-											}
+		<div className="relative">
+			<ReactInlangBundle bundle={bundle} change={handleChangeEvent}>
+				<ReactInlangBundleAction
+					slot="bundle-action"
+					actionTitle="Delete"
+					onClick={() => handleDelete({ command: "delete-bundle", message: { id: bundle.id } })}
+				/>
+				{bundle.messages.map((message) => (
+					<ReactInlangMessage
+						key={message.id}
+						slot="message"
+						message={message}
+						variants={message.variants}
+						settings={settings}
+					>
+						{message.variants.map((variant) => (
+							<ReactInlangVariant key={variant.id} slot="variant" variant={variant}>
+								<ReactInlangPatternEditor slot="pattern-editor" variant={variant} />
+								<SlDropdown
+									slot="variant-action"
+									className="animate-blendIn"
+									distance={4}
+									hoist
+									onSlShow={(e) => {
+										setTimeout(() => {
+											e.target?.querySelector("sl-menu-item")?.focus()
+										})
+									}}
+								>
+									<div slot="trigger" className="px-2 cursor-pointer">
+										<svg
+											xmlns="http://www.w3.org/2000/svg"
+											width="20"
+											height="20"
+											viewBox="0 0 24 24"
+											className="-mx-[2px]"
 										>
-											Delete
-										</SlMenuItem>
-									)}
-								</SlMenu>
-							</SlDropdown>
-						</ReactInlangVariant>
-					))}
-					<SlDialog slot="selector-button" label="Add Selector" open={isDialogOpen}>
-						<ReactInlangAddSelector
-							bundle={bundle}
-							message={message}
-							variants={message.variants}
-							change={(e) => {
-								handleChangeEvent(e)
-								setIsDialogOpen(false)
-							}}
-						/>
-					</SlDialog>
-				</ReactInlangMessage>
-			))}
-		</ReactInlangBundle>
+											<path
+												fill="currentColor"
+												d="M7 12a2 2 0 1 1-4 0a2 2 0 0 1 4 0m7 0a2 2 0 1 1-4 0a2 2 0 0 1 4 0m7 0a2 2 0 1 1-4 0a2 2 0 0 1 4 0"
+											/>
+										</svg>
+									</div>
+									<SlMenu>
+										<SlMenuItem onClick={() => setIsDialogOpen(true)}>Add Selector</SlMenuItem>
+										{message.variants.length > 1 && (
+											<SlMenuItem
+												onClick={() =>
+													handleDelete({ command: "delete-variant", message: { id: variant.id } })
+												}
+											>
+												Delete
+											</SlMenuItem>
+										)}
+									</SlMenu>
+								</SlDropdown>
+							</ReactInlangVariant>
+						))}
+						<SlDialog slot="selector-button" label="Add Selector" open={isDialogOpen}>
+							<ReactInlangAddSelector
+								bundle={bundle}
+								message={message}
+								variants={message.variants}
+								change={(e) => {
+									handleChangeEvent(e)
+									setIsDialogOpen(false)
+								}}
+							/>
+						</SlDialog>
+					</ReactInlangMessage>
+				))}
+			</ReactInlangBundle>
+		</div>
 	)
 }
 
