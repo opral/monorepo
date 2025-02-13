8a5414af22f15b183c9f1832f0f92cc0bb8f32a1
miika <miika.kokkonen.mk@gmail.com>
2025-02-14 01:47:55 +0200

    fix: plugin imports on Bun

--- packages/sdk/src/plugin/importPlugins.ts
+++ packages/sdk/src/plugin/importPlugins.ts
@@ -29,10 +29,16 @@ export async function importPlugins(args: {
 			if (args.preprocessPluginBeforeImport) {
 				moduleAsText = await args.preprocessPluginBeforeImport(moduleAsText);
 			}
-			const moduleWithMimeType =
-				"data:application/javascript," + encodeURIComponent(moduleAsText);
+			let moduleAsURL;
+			if (process.versions.bun) {
+				// In bun we need to do dynamic imports differently
+				moduleAsURL = URL.createObjectURL(new Blob([moduleAsText], { type: 'text/javascript' }));
+			} else {
+				// node and others
+				moduleAsURL = 'data:text/javascript;base64,' + btoa(moduleAsText);
+			}
 			const { default: module } = await import(
-				/* @vite-ignore */ moduleWithMimeType
+				/* @vite-ignore */ moduleAsURL
 			);
 			// old legacy message lint rules are not supported
 			// and ingored for backwards compatibility
