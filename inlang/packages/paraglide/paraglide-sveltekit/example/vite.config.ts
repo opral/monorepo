2b1ed4363587b380fe59bbcbd18dd5b7e79d0a9a
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2025-02-16 21:00:17 -0500

    remove custom url patterns in favor of default

--- inlang/packages/paraglide/paraglide-sveltekit/example/vite.config.ts
+++ inlang/packages/paraglide/paraglide-sveltekit/example/vite.config.ts
@@ -11,25 +11,7 @@ export default defineConfig({
 		paraglideSveltekit({
 			project: './project.inlang',
 			outdir: './src/lib/paraglide',
-			strategy: ['urlPattern', 'cookie', 'baseLocale'],
-			urlPatterns: [
-				{
-					pattern: ':protocol://localhost::port?/:locale(de)?/:path(.*)?',
-					localizedNamedGroups: {
-						en: { locale: null },
-						de: { locale: 'de' }
-					},
-					deLocalizedNamedGroups: { locale: null }
-				},
-				{
-					pattern: ':protocol://:domain(.*)::port?/:locale(de)?/:path(.*)?',
-					localizedNamedGroups: {
-						en: { locale: null },
-						de: { domain: 'de.example.com', locale: 'de' }
-					},
-					deLocalizedNamedGroups: { locale: null }
-				}
-			]
+			strategy: ['url']
 		})
 	]
 });
