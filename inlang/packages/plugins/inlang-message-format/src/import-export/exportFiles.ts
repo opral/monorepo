33befe53e48dec98d8d1588e0be23a6854c11a0b
Felix Haeberle <kontakt@felixhaeberle.de>
2025-02-18 17:26:40 +0100

    update exportFiles to emit sorted output to have less diff noise

--- inlang/packages/plugins/inlang-message-format/src/import-export/exportFiles.ts
+++ inlang/packages/plugins/inlang-message-format/src/import-export/exportFiles.ts
@@ -67,60 +67,63 @@ function serializeMessage(
 }
 
 function serializeVariants(
-  bundle: Bundle,
-  message: Message,
-  variants: Variant[],
+	bundle: Bundle,
+	message: Message,
+	variants: Variant[]
 ): string | Record<string, any> {
-  // single variant
-  // todo add logic for handling if a variant has a match even if it's
-  // the only variant
-  if (variants.length === 1) {
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    return serializePattern(variants[0]!.pattern);
-  }
-  const entries = [];
-  for (const variant of variants) {
-    const match = serializeMatcher(variant.matches);
-    const pattern = serializePattern(variant.pattern);
-    entries.push([match, pattern]);
-  }
+	// single variant
+	// todo add logic for handling if a variant has a match even if it's
+	// the only variant
+	if (variants.length === 1) {
+		// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+		return serializePattern(variants[0]!.pattern);
+	}
+	const entries = [];
+	for (const variant of variants) {
+		const match = serializeMatcher(variant.matches);
+		const pattern = serializePattern(variant.pattern);
+		entries.push([match, pattern]);
+	}
 
-  return {
-    // naively adding all declarations, even if unused in the variants
-    // can be optimized later.
-    declarations: bundle.declarations.map(serializeDeclaration),
-    selectors: message.selectors.map((s) => s.name),
-    match: Object.fromEntries(entries),
-  };
+	return {
+		// naively adding all declarations, even if unused in the variants
+		// can be optimized later.
+		declarations: bundle.declarations
+			.sort((a, b) => a.name.localeCompare(b.name))
+			.map(serializeDeclaration)
+			.sort(),
+		selectors: message.selectors.map((s) => s.name).sort(),
+		match: Object.fromEntries(entries),
+	};
 }
 
 function serializePattern(pattern: Variant["pattern"]): string {
-  let result = "";
+	let result = "";
 
-  for (const part of pattern) {
-    if (part.type === "text") {
-      result += part.value;
-    } else if (part.arg.type === "variable-reference") {
-      result += `{${part.arg.name}}`;
-    } else {
-      throw new Error("Unsupported expression type");
-    }
-  }
-  return result;
+	for (const part of pattern) {
+		if (part.type === "text") {
+			result += part.value;
+		} else if (part.arg.type === "variable-reference") {
+			result += `{${part.arg.name}}`;
+		} else {
+			throw new Error("Unsupported expression type");
+		}
+	}
+	return result;
 }
 
 // input: { platform: "android", userGender: "male" }
 // output: `platform=android,userGender=male`
 function serializeMatcher(matches: Match[]): string {
-  const parts = [];
-  for (const match of matches) {
-    if (match.type === "literal-match") {
-      parts.push(`${match.key}=${match.value}`);
-    } else {
-      parts.push(`${match.key}=*`);
-    }
-  }
-  return parts.join(", ");
+	const parts = matches
+		.sort((a, b) => a.key.localeCompare(b.key))
+		.map((match) =>
+			match.type === "literal-match"
+				? `${match.key}=${match.value}`
+				: `${match.key}=*`
+		);
+
+	return parts.join(", ");
 }
 
 function serializeDeclaration(declaration: Declaration): string {
