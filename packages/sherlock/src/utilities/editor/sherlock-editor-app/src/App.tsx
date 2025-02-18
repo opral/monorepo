56f0ed666f34c9b1cd5d117ff0ced61bab750246
Felix Haeberle <kontakt@felixhaeberle.de>
2025-02-18 15:08:17 +0100

    remove unused hook in `App`

--- packages/sherlock/src/utilities/editor/sherlock-editor-app/src/App.tsx
+++ packages/sherlock/src/utilities/editor/sherlock-editor-app/src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css"
-import { useEffect, useState } from "react"
+import { useState } from "react"
 import { vscode } from "./utils/vscode.js"
 import Editor from "./components/Editor.js"
 import { BundleNested, ProjectSettings } from "@inlang/sdk"
@@ -16,15 +16,12 @@ function App() {
 	const [bundle, setBundle] = useState<BundleNested | undefined>(undefined)
 	const [settings, setSettings] = useState<ProjectSettings | undefined>(undefined)
 
-	useEffect(() => {
-		// Listen for messages from the extension
-		vscode.onMessage((message: unknown) => {
-			const typedMessage = message as VscodeMessage
+	vscode.onMessage((message: unknown) => {
+		const typedMessage = message as VscodeMessage
 
-			setBundle(typedMessage.data.bundle)
-			setSettings(typedMessage.data.settings)
-		})
-	}, [])
+		setBundle(typedMessage.data.bundle)
+		setSettings(typedMessage.data.settings)
+	})
 
 	return (
 		<>
