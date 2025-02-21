83ad59010a28f55696f06b1c581e06c1dedd4850
Felix Haeberle <kontakt@felixhaeberle.de>
2025-02-21 20:47:22 +0100

    fix settings component

--- inlang/packages/ui-components/settings-component/src/stories/field-header.ts
+++ inlang/packages/ui-components/settings-component/src/stories/field-header.ts
@@ -1,91 +1,98 @@
-import { css, html, LitElement } from "lit"
-import { customElement, property } from "lit/decorators.js"
-//import { baseStyling } from "../../../styling/base.js"
+import { css, html, LitElement, nothing } from "lit";
+import { customElement, property } from "lit/decorators.js";
 
 @customElement("field-header")
 export class FieldHeader extends LitElement {
-	static override styles = [
-		//baseStyling,
-		css`
-			.header {
-				display: flex;
-				flex-direction: column;
-				gap: 4px;
-			}
-			h3 {
-				margin: 0;
-				font-size: 14px;
-				font-weight: 800;
-				line-height: 1.5;
-			}
-			.help-text {
-				font-size: 14px;
-				color: var(--sl-input-help-text-color);
-				margin: 0;
-				line-height: 1.5;
-			}
-			.optinal {
-				font-size: 14px;
-				font-style: italic;
-				font-weight: 500;
-				color: var(--sl-input-help-text-color);
-			}
-			.example-container {
-				display: flex;
-				flex-wrap: wrap;
-				gap: 4px;
-			}
-			.example {
-				background-color: var(--sl-input-background-color-disabled);
-				width: fit-content;
-				padding: 0px 6px;
-				border-radius: 2px;
-				font-size: 14px;
-				display: flex;
-				align-items: center;
-				justify-content: center;
-				color: var(--sl-input-color-disabled);
-				margin: 0;
-				line-height: 1.5;
-			}
-		`,
-	]
+	static override styles = css`
+		.header {
+			display: flex;
+			flex-direction: column;
+			gap: 4px;
+		}
+		h3 {
+			margin: 0;
+			font-size: 14px;
+			font-weight: 800;
+			line-height: 1.5;
+		}
+		.help-text {
+			font-size: 14px;
+			color: var(--sl-input-help-text-color);
+			margin: 0;
+			line-height: 1.5;
+		}
+		.optional {
+			font-size: 14px;
+			font-style: italic;
+			font-weight: 500;
+			color: var(--sl-input-help-text-color);
+		}
+		.example-container {
+			display: flex;
+			flex-wrap: wrap;
+			gap: 4px;
+		}
+		.example {
+			background-color: var(--sl-input-background-color-disabled);
+			width: fit-content;
+			padding: 0px 6px;
+			border-radius: 2px;
+			font-size: 14px;
+			display: flex;
+			align-items: center;
+			justify-content: center;
+			color: var(--sl-input-color-disabled);
+			margin: 0;
+			line-height: 1.5;
+		}
+	`;
 
 	@property()
-	fieldTitle?: string
+	fieldTitle?: string;
 
 	@property()
-	description?: string
+	description?: string;
 
 	@property({ type: Array })
-	examples?: Array<string>
+	examples?: string[];
 
 	@property({ type: Boolean })
-	optional?: boolean = false
+	optional?: boolean = false;
 
 	override render() {
-		return html` <div class="header">
-			${this.fieldTitle &&
-			html`<h3 part="property-title">
-				${this.fieldTitle}${this.optional
-					? html`<span class="optinal">${" " + "(optional)"}</span>`
-					: ""}
-			</h3>`}
-			${this.description &&
-			html`<p part="property-paragraph" class="help-text">${this.description}</p>`}
-			${this.examples
-				? html`<div class="example-container">
-						<p class="help-text">Examples:</p>
-						${this.examples.map((example) => html`<p class="example">${example}</p>`)}
-				  </div>`
-				: ``}
-		</div>`
+		return html`
+			<div class="header">
+				${this.fieldTitle
+					? html`
+							<h3 part="property-title">
+								${this.fieldTitle}${this.optional
+									? html`<span class="optional">${" (optional)"}</span>`
+									: nothing}
+							</h3>
+						`
+					: nothing}
+				${this.description
+					? html`<p part="property-paragraph" class="help-text">
+							${this.description}
+						</p>`
+					: nothing}
+				${Array.isArray(this.examples) && this.examples.length > 0
+					? html`
+							<div class="example-container">
+								<p class="help-text">Examples:</p>
+								${this.examples.map(
+									(example) => html`<p class="example">${example}</p>`
+								)}
+							</div>
+						`
+					: nothing}
+			</div>
+		`;
 	}
 }
 
-// add types
 declare global {
 	interface HTMLElementTagNameMap {
-		"field-header": FieldHeader
+		"field-header": FieldHeader;
 	}
 }
