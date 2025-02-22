c7ab8b480b9d6089fbec59f79314b2574d9fa496
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2025-02-22 11:22:09 -0500

    remove old localizePath apis

--- packages/paraglide/paraglide-js/src/compiler/runtime/localize-href.js
+++ packages/paraglide/paraglide-js/src/compiler/runtime/localize-href.js
@@ -74,19 +74,3 @@ export function deLocalizeHref(href) {
 
 	return deLocalized.href;
 }
-
-/**
- * @deprecated use `localizeHref` instead and give feedback on here https://github.com/opral/inlang-paraglide-js/issues/380
- * @type {(href: string, options?: { locale?: string }) => string}
- */
-export function localizePath(href, options) {
-	return localizeHref(href, options);
-}
-
-/**
- * @deprecated use `deLocalizeHref` instead and give feedback on here https://github.com/opral/inlang-paraglide-js/issues/380
- * @type {(href: string) => string}
- */
-export function deLocalizePath(href) {
-	return deLocalizeHref(href);
-}
