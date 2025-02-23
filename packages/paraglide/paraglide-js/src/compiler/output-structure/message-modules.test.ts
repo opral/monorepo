9e2c53018f6f52bb6e1a50cc42256714d4268ec3
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2025-02-23 17:10:07 -0500

    add test case

--- packages/paraglide/paraglide-js/src/compiler/output-structure/message-modules.test.ts
+++ packages/paraglide/paraglide-js/src/compiler/output-structure/message-modules.test.ts
@@ -49,3 +49,68 @@ test("should emit per locale message files", () => {
 	expect(output).toHaveProperty("messages/happy_elephant/de.js");
 	expect(output).toHaveProperty("messages/happy_elephant/index.js");
 });
+
+test("handles case senstivity by creating directories and files only in lowercase", () => {
+	const resources: CompiledBundleWithMessages[] = [
+		{
+			bundle: {
+				code: "export const HappyElephant = () => en.HappyElephant",
+				node: {
+					id: "HappyElephant",
+				} as unknown as Bundle,
+			},
+			messages: {
+				en: {
+					code: 'export const HappyElephant = () => "HappyElephant0"',
+					node: {} as unknown as Message,
+				},
+			},
+		},
+		{
+			bundle: {
+				code: "export const happyelephant = () => en.happyelephant",
+				node: {
+					id: "happyelephant",
+				} as unknown as Bundle,
+			},
+			messages: {
+				en: {
+					code: 'export const happyelephant = () => "happyelephant1"',
+					node: {} as unknown as Message,
+				},
+			},
+		},
+	];
+	const settings: Pick<ProjectSettings, "locales" | "baseLocale"> = {
+		locales: ["en", "de"],
+		baseLocale: "en",
+	};
+	const output = generateMessageModules(
+		resources,
+		settings,
+		{},
+		defaultCompilerOptions
+	);
+
+	// expecting only lowercase directories and files
+	expect(output).toHaveProperty("messages/happyelephant/en.js");
+	expect(output).toHaveProperty("messages/happyelephant/de.js");
+	expect(output).toHaveProperty("messages/happyelephant/index.js");
+	expect(output).not.toHaveProperty("messages/HappyElephant/en.js");
+	expect(output).not.toHaveProperty("messages/HappyElephant/de.js");
+	expect(output).not.toHaveProperty("messages/HappyElephant/index.js");
+
+	// expecting both bundles to be merged into the "happyelephant" module
+	expect(output["messages/happyelephant/index.js"]).toMatchInlineSnapshot(
+		[
+			`"export const happyelephant = () => en.happyelephant"`,
+			`"export const HappyElephant = () => en.HappyElephant"`,
+		].join("\n")
+	);
+	expect(output["messages/happyelephant/en.js"]).toMatchInlineSnapshot(
+		[
+			`"export const HappyElephant = () => "HappyElephant0"`,
+			`"export const happyelephant = () => "happyelephant1"`,
+		].join("\n")
+	);
+});
