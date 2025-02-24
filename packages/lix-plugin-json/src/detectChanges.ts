f8a1129c9d09b426f0915bb7fd01f57b0cd99649
Martin Lysk <martin.lysk@gmail.com>
2025-02-24 12:36:01 +0100

    Adds the json plugin

---
+++ packages/lix-plugin-json/src/detectChanges.ts
@@ -0,0 +1,62 @@
+import type { DetectedChange, LixPlugin } from "@lix-js/sdk";
+import { flatten } from "flat";
+import { JSONPropertySchema } from "./schemas/JSONPropertySchema.js";
+
+// @ts-expect-error - possibly too recursive inference
+export const detectChanges: NonNullable<LixPlugin["detectChanges"]> = async ({
+	before,
+	after,
+}) => {
+	const detectedChanges: DetectedChange<typeof JSONPropertySchema>[] = [];
+
+	const beforeParsed = before?.data
+		? JSON.parse(new TextDecoder().decode(before?.data))
+		: {};
+	const afterParsed = after?.data
+		? JSON.parse(new TextDecoder().decode(after?.data))
+		: {};
+
+	const flattenedBefore = flatten(beforeParsed, {
+		safe: true
+	}) as Record<string, unknown>;
+	const flattenedAfter = flatten(afterParsed, {
+		safe: true
+	}) as Record<string, unknown>;
+
+	for (const key in flattenedBefore) {
+		if (!(key in flattenedAfter)) {
+			detectedChanges.push({
+				schema: JSONPropertySchema,
+				entity_id: key,
+				snapshot: undefined,
+			});
+		} else if (
+			JSON.stringify(flattenedBefore[key]) !==
+			JSON.stringify(flattenedAfter[key])
+		) {
+			detectedChanges.push({
+				schema: JSONPropertySchema,
+				entity_id: key,
+				snapshot: {
+					property: key,
+					value: flattenedAfter[key],
+				},
+			});
+		}
+	}
+
+	for (const key in flattenedAfter) {
+		if (!(key in flattenedBefore)) {
+			detectedChanges.push({
+				schema: JSONPropertySchema,
+				entity_id: key,
+				snapshot: {
+					property: key,
+					value: flattenedAfter[key],
+				},
+			});
+		}
+	}
+
+	return detectedChanges;
+};
