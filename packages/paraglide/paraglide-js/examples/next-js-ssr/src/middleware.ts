0265039ac2f478892b1ad490dad66d87eb36373b
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2025-02-24 19:47:18 -0500

    fix nextjs example

--- packages/paraglide/paraglide-js/examples/next-js-ssr/src/middleware.ts
+++ packages/paraglide/paraglide-js/examples/next-js-ssr/src/middleware.ts
@@ -4,6 +4,7 @@ import { serverMiddleware } from "./paraglide/runtime";
 export function middleware(request: NextRequest) {
 	return serverMiddleware(request, ({ request, locale }) => {
 		request.headers.set("x-paraglide-locale", locale);
+		request.headers.set("x-paraglide-request-url", request.url);
 		return NextResponse.rewrite(request.url, request);
 	});
 }
