08c11635fc7da129664a373cda81bbd2a529fda2
NiklasBuchfink <niklas.buchfink@web.de>
2025-02-28 18:48:11 +0100

    fix: use selectedNodes to serialize the correct content

--- packages/md-app/src/components/copy-markdown-plugin.ts
+++ packages/md-app/src/components/copy-markdown-plugin.ts
@@ -1,3 +1,4 @@
+import { NodeApi } from "@udecode/plate";
 import { createPlatePlugin } from "@udecode/plate-core/react";
 import { ClipboardEvent } from "react";
 
@@ -10,9 +11,12 @@ export const CreateCopyMarkdownPlugin = createPlatePlugin({
 			const editor = ctx.editor;
 			if (!editor || !editor.selection) return;
 
+			const selectedNodes = NodeApi.fragment(editor, editor.selection);
+
 			// @ts-expect-error - markdown is not in the types
-			const markdown = editor.api.markdown.serialize();
-			console.log(markdown);
+			const markdown = editor.api.markdown.serialize({
+				nodes: selectedNodes,
+			});
 
 			window.navigator.clipboard.writeText(markdown);
 			event.preventDefault();
