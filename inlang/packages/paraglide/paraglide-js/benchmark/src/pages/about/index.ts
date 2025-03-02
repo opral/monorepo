3e54308cbee134c40740c3fae7f493cc94339e93
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2025-03-02 11:01:36 -0500

    client side build works

--- inlang/packages/paraglide/paraglide-js/benchmark/src/pages/about/index.ts
+++ inlang/packages/paraglide/paraglide-js/benchmark/src/pages/about/index.ts
@@ -2,7 +2,7 @@ import { m } from "../../paraglide/messages.js";
 
 export function Page() {
 	return `
-    <p>${m.message0()}</p>
+    <p>${m.message1()}</p>
     <a href="/">Go back to the homepage</a>
   `;
 }
