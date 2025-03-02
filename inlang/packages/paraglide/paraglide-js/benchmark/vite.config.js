20cfe67d18d140825ba73d7a77fb3cbd3fb43056
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2025-03-01 21:14:19 -0500

    init benchmark

---
+++ inlang/packages/paraglide/paraglide-js/benchmark/vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from "vite";
+import { paraglideVitePlugin } from "@inlang/paraglide-js";
+
+export default defineConfig({
+	plugins: [
+		paraglideVitePlugin({
+			project: "./project.inlang",
+			outdir: "./src/paraglide",
+		}),
+	],
+	build: {
+		// eases debugging
+		minify: false,
+	},
+});
