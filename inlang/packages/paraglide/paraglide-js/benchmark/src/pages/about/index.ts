36fbba52ed3d5b7562dbd1177cb86b6815643672
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2025-03-02 17:08:23 -0500

    delete pages

--- inlang/packages/paraglide/paraglide-js/benchmark/src/pages/about/index.ts
+++ inlang/packages/paraglide/paraglide-js/benchmark/src/pages/about/index.ts
@@ -1,7 +1,5 @@
-import { m } from "../../paraglide/messages.js";
-
 export function Page() {
 	return `
-    <p>${m.message1()}</p>
+    <p>mock</p>
   `;
 }
--- inlang/packages/paraglide/paraglide-js/benchmark/src/pages/index.ts
+++ inlang/packages/paraglide/paraglide-js/benchmark/src/pages/index.ts
@@ -1,8 +1,5 @@
-import { m } from "../paraglide/messages.js";
-import { t } from "../i18n/i18next.ts";
-
 export function Page() {
 	return `
-    <p>${m.message0()}</p>
+    <p>mock</p>
   `;
 }
