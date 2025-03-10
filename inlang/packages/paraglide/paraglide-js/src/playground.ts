cf55e19ad87dee38efe5298ecb74c2d28a8a20b8
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2025-03-09 19:47:20 -0700

    chore: remove unused files

--- inlang/packages/paraglide/paraglide-js/src/playground.ts
+++
@@ -1,9 +0,0 @@
-import "urlpattern-polyfill";
-
-const pattern = new URLPattern("/*", "https://example.com");
-
-console.log(pattern.test("/", "https://example.com"));
-
-console.log(pattern.test("/page", "https://example.com"));
-
-console.log(pattern.test("/page/blabla", "https://example.com"));
