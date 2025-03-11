4c1ea406cf09d142b4d0f3760c6b78e524022b2d
NiklasBuchfink <niklas.buchfink@web.de>
2025-03-11 16:17:24 +0100

    refactor: trigger "polling" after every saveLixToOpfs

--- packages/md-app/src/helper/saveLixToOpfs.ts
+++ packages/md-app/src/helper/saveLixToOpfs.ts
@@ -1,7 +1,11 @@
 import { Lix, toBlob } from "@lix-js/sdk";
 import { getOriginPrivateDirectory } from "native-file-system-adapter";
+import { getDefaultStore } from "jotai";
+import { withPollingAtom } from "@/state";
 
 export async function saveLixToOpfs(args: { lix: Lix }) {
+	const store = getDefaultStore();
+
 	const lixId = await args.lix.db
 		.selectFrom("key_value")
 		.where("key", "=", "lix_id")
@@ -16,5 +20,8 @@ export async function saveLixToOpfs(args: { lix: Lix }) {
 	const file = await toBlob({ lix: args.lix });
 	await writable.write(file);
 	await writable.close();
+
+	store.set(withPollingAtom, Date.now());
+
 	console.log(`done saving lix ${lixId.value} to opfs`);
 }
