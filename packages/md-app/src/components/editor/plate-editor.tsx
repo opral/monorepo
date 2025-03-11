a0749d3b6eb0590ba46f91a9d40972dd4fe541fe
NiklasBuchfink <niklas.buchfink@web.de>
2025-03-11 15:29:30 +0100

    fix: "polling" only on saveToOPFS and reset

--- packages/md-app/src/components/editor/plate-editor.tsx
+++ packages/md-app/src/components/editor/plate-editor.tsx
@@ -11,7 +11,7 @@ import { useCreateEditor } from "@/components/editor/use-create-editor";
 import { Editor, EditorContainer } from "@/components/plate-ui/editor";
 import { debounce } from "lodash-es";
 import { useAtom } from "jotai";
-import { lixAtom } from "@/state";
+import { lixAtom, withPollingAtom } from "@/state";
 import { activeFileAtom, loadedMdAtom } from "@/state-active-file";
 import { saveLixToOpfs } from "@/helper/saveLixToOpfs";
 import { MarkdownPlugin } from "@udecode/plate-markdown";
@@ -23,6 +23,7 @@ export function PlateEditor() {
 	const [loadedMd] = useAtom(loadedMdAtom);
 
 	const editor = useCreateEditor();
+	const [, setPolling] = useAtom(withPollingAtom);
 
 	// Set the initial value of the editor-
 	useEffect(() => {
@@ -64,6 +65,9 @@ export function PlateEditor() {
 			// needed because lix is not writing to OPFS yet
 			await saveLixToOpfs({ lix });
 			console.log("saved to lix db");
+
+			// trigger "polling" to update the lix state
+			setPolling(Date.now());
 		}, 500),
 		[]
 	);
