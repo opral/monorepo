68ce22d0ce36ff1322556ff6ebcc56b640275b24
NiklasBuchfink <niklas.buchfink@web.de>
2025-03-11 17:38:44 +0100

    fix: plugin-txt build

--- packages/lix-plugin-txt/src/index.ts
+++ packages/lix-plugin-txt/src/index.ts
@@ -2,7 +2,6 @@ import { type LixPlugin } from "@lix-js/sdk";
 import { detectChanges } from "./detectChanges.js";
 import { DiffComponent } from "./diff.js";
 
-
 export const plugin: LixPlugin = {
 	key: "lix_plugin_txt",
 	// we use this for markdown now
