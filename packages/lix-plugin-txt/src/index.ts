faebb687c3302e74f30655886b41f38041983710
Martin Lysk <martin.lysk@gmail.com>
2025-03-11 16:07:03 +0100

    removes unused import

--- packages/lix-plugin-txt/src/index.ts
+++ packages/lix-plugin-txt/src/index.ts
@@ -1,7 +1,7 @@
 import { type LixPlugin } from "@lix-js/sdk";
 import { detectChanges } from "./detectChanges.js";
 import { DiffComponent } from "./diff.js";
-import { parseMdBlocks } from "./utilities/parseMdBlocks.js";
+
 
 export const plugin: LixPlugin = {
 	key: "lix_plugin_txt",
@@ -12,4 +12,3 @@ export const plugin: LixPlugin = {
 };
 
 export { TextSchemaV1 } from "./schemas/text.js";
-export { parseMdBlocks };
