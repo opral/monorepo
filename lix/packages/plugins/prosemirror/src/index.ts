0d71b34ee3943b85056ad9d482369e68fd7d3a5e
Felix Haeberle <kontakt@felixhaeberle.de>
2025-03-17 15:59:31 +0100

    export functions to use in UI and update scripts

--- lix/packages/plugins/prosemirror/src/index.ts
+++ lix/packages/plugins/prosemirror/src/index.ts
@@ -1,6 +1,7 @@
 import { type LixPlugin } from "@lix-js/sdk";
 import { applyChanges } from "./applyChanges.js";
 import { detectChanges } from "./detectChanges.js";
+import type { ProsemirrorNode } from "./detectChanges.js";
 
 export const plugin: LixPlugin = {
 	key: "plugin_prosemirror",
@@ -8,4 +9,8 @@ export const plugin: LixPlugin = {
 	detectChanges,
 	applyChanges,
 };
+
+// Export the main functions for easier consumption by the UI
+export { detectChanges, applyChanges };
+export type { ProsemirrorNode };
 
