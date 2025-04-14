c287a62ba023132b0fda124d929c0a2dcfda07dd
NiklasBuchfink <niklas.buchfink@web.de>
2025-04-14 11:49:02 +0200

    chore: disable image resizing

--- packages/md-app/src/components/plate-ui/image-element.tsx
+++ packages/md-app/src/components/plate-ui/image-element.tsx
@@ -5,85 +5,56 @@ import React from 'react';
 import { cn, withRef } from '@udecode/cn';
 import { useDraggable } from '@udecode/plate-dnd';
 import { ImagePlugin, useMediaState } from '@udecode/plate-media/react';
-import { ResizableProvider, useResizableValue } from '@udecode/plate-resizable';
-import { PlateElement, withHOC } from '@udecode/plate/react';
+import { PlateElement } from '@udecode/plate/react';
 
 import { Caption, CaptionTextarea } from './caption';
 import { LixImage } from './lix-image';
 import { MediaPopover } from './media-popover';
-import {
-  mediaResizeHandleVariants,
-  Resizable,
-  ResizeHandle,
-} from './resizable';
 
-export const ImageElement = withHOC(
-  ResizableProvider,
-  withRef<typeof PlateElement>(
-    ({ children, className, nodeProps, ...props }, ref) => {
-      const { align = 'center', focused, readOnly, selected } = useMediaState();
+export const ImageElement = withRef<typeof PlateElement>(
+  ({ children, className, nodeProps, ...props }, ref) => {
+    const { align = 'center', focused, readOnly, selected } = useMediaState();
 
-      const width = useResizableValue('width');
+    const { isDragging, handleRef } = useDraggable({
+      element: props.element,
+    });
 
-      const { isDragging, handleRef } = useDraggable({
-        element: props.element,
-      });
+    return (
+      <MediaPopover plugin={ImagePlugin}>
+        <PlateElement
+          ref={ref}
+          className={cn(className, 'py-2.5')}
+          {...props}
+        >
+          <figure className="group relative m-0" contentEditable={false}>
+            <div ref={handleRef} style={{ width: '100%' }}>
+              <LixImage
+                className={cn(
+                  'block w-full max-w-full cursor-pointer object-cover px-0',
+                  'rounded-sm',
+                  focused && selected && 'ring-2 ring-ring ring-offset-2',
+                  isDragging && 'opacity-50'
+                )}
+                alt={props.element.alt as string || ''}
+                src={props.element.url as string || ''}
+                {...nodeProps}
+              />
+            </div>
 
-      return (
-        <MediaPopover plugin={ImagePlugin}>
-          <PlateElement
-            ref={ref}
-            className={cn(className, 'py-2.5')}
-            {...props}
-          >
-            <figure className="group relative m-0" contentEditable={false}>
-              <Resizable
-                align={align}
-                options={{
-                  align,
-                  readOnly,
+            <Caption align={align}>
+              <CaptionTextarea
+                readOnly={readOnly}
+                onFocus={(e) => {
+                  e.preventDefault();
                 }}
-              >
-                <ResizeHandle
-                  className={mediaResizeHandleVariants({ direction: 'left' })}
-                  options={{ direction: 'left' }}
-                />
-                <div ref={handleRef}>
-                  <LixImage
-                    className={cn(
-                      'block w-full max-w-full cursor-pointer object-cover px-0',
-                      'rounded-sm',
-                      focused && selected && 'ring-2 ring-ring ring-offset-2',
-                      isDragging && 'opacity-50'
-                    )}
-                    alt={props.element.alt as string || ''}
-                    src={props.element.url as string || ''}
-                    {...nodeProps}
-                  />
-                </div>
-                <ResizeHandle
-                  className={mediaResizeHandleVariants({
-                    direction: 'right',
-                  })}
-                  options={{ direction: 'right' }}
-                />
-              </Resizable>
+                placeholder="Write a caption..."
+              />
+            </Caption>
+          </figure>
 
-              <Caption style={{ width }} align={align}>
-                <CaptionTextarea
-                  readOnly={readOnly}
-                  onFocus={(e) => {
-                    e.preventDefault();
-                  }}
-                  placeholder="Write a caption..."
-                />
-              </Caption>
-            </figure>
-
-            {children}
-          </PlateElement>
-        </MediaPopover>
-      );
-    }
-  )
+          {children}
+        </PlateElement>
+      </MediaPopover>
+    );
+  }
 );
