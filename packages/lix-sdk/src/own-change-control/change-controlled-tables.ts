bcc415a846bf593b86f54cd8f0c00e081a4a59b8
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2025-04-15 00:04:17 -0400

    remove version v2 interim from change control to avoid checkpoint bug

--- packages/lix-sdk/src/own-change-control/change-controlled-tables.ts
+++ packages/lix-sdk/src/own-change-control/change-controlled-tables.ts
@@ -15,7 +15,7 @@ export const changeControlledTableIds = {
 	label: ["id"],
 	key_value: ["key"],
 	version: ["id"],
-	version_v2: ["id"],
+	// version_v2: ["id"],
 	// version_change: ["version_id", "change_id"],
 } as const;
 
--- packages/lix-sdk/src/version/switch-version.ts
+++ packages/lix-sdk/src/version/switch-version.ts
@@ -74,7 +74,16 @@ export async function switchVersion(args: {
 				}
 				// need to remove the entity when switching the version
 				else {
-					thi
+					if (
+						change.plugin_key === "lix_own_change_control" &&
+						(change.schema_key === "lix_account_table" ||
+							change.schema_key === "lix_version_table")
+					) {
+						// deleting accounts and versions when switching is
+						// not desired. a version should be able to jump to a
+						// different version and the accounts are not affected
+						continue;
+					}
 					// the entity does not exist in the switched to version
 					toBeAppliedChanges.set(
 						`${change.file_id},${change.entity_id},${change.schema_key}`,
