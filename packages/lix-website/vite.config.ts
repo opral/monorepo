cf7b7410f9716f69094706001757af9b835c93dd
NiklasBuchfink <niklas.buchfink@web.de>
2025-04-15 14:04:38 +0200

    chore: clean up unnecessary code

--- packages/lix-website/vite.config.ts
+++ packages/lix-website/vite.config.ts
@@ -3,8 +3,8 @@ import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command, mode }) => {
-  const config = {
+export default defineConfig(() => {
+  return {
     envPrefix: "PUBLIC_",
     plugins: [
       react(),
@@ -18,35 +18,19 @@ export default defineConfig(({ command, mode }) => {
     preview: {
       port: 3006,
     },
-    base: "/",
-    publicDir: "public",
+    base: "/", // Use root-relative paths for URLs
+    publicDir: "public", // Copy files from public directory
     build: {
       minify: true,
       target: "esnext",
-      ssrManifest: !(command === "build" && mode === "ssr"),
       outDir: "dist",
       emptyOutDir: true,
-      assetsInlineLimit: 0,
-      rollupOptions: {},
+      assetsInlineLimit: 0, // Don't inline assets
+      rollupOptions: {
+        input: {
+          main: "index.html",
+        },
+      },
     },
   };
-
-  // Specific configurations for SSR build
-  if (command === "build" && mode === "ssr") {
-    // SSR specific config adjustments
-    config.build.rollupOptions = {
-      output: {
-        format: "es",
-      },
-    };
-  } else {
-    // Client build specific settings
-    config.build.rollupOptions = {
-      input: {
-        main: "index.html",
-      },
-    };
-  }
-
-  return config;
 });
