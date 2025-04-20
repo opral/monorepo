a75dda471e9ffc1bdccd156bf26b5f4c0025e34b
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2025-04-20 11:58:08 -0400

    export generateKey

--- packages/zettel/zettel-ast/src/index.ts
+++ packages/zettel/zettel-ast/src/index.ts
@@ -2,3 +2,4 @@ export * from "./schema.js";
 export * from "./builder.js";
 export { serializeToText } from "./serialize-to-text.js";
 export { validate } from "./validate.js";
+export { nanoid as generateKey } from "./utils/nano-id.js";
