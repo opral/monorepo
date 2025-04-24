333d5b5934aecbb002a3be12f8cf3766a4457698
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2025-04-24 13:46:27 -0400

    remove unused package

--- packages/zettel/zettel-spec/src/index.ts
+++
@@ -1,4 +0,0 @@
-export { toPlainText, fromPlainText } from "./plain-text.js";
-export { validateDoc } from "./validate-doc.js";
-export { nanoid as generateKey } from "./utils/nano-id.js";
-export type { ValidationResult, SerializableError } from "./validate-doc.js";
