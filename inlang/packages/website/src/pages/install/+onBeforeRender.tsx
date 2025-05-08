dc8de8bdb2ed435d735ed8a4d5ec822678f01627
NiklasBuchfink <niklas.buchfink@web.de>
2025-05-08 17:38:08 +0200

    fix: remove deprecated links and redirect routes of manage.inlang.com and badge.inlang.com

--- inlang/packages/website/src/pages/install/+onBeforeRender.tsx
+++
@@ -1,8 +0,0 @@
-import type { PageContext } from "#src/renderer/types.js";
-import { redirect } from "vike/abort";
-
-export default async function onBeforeRender(pageContext: PageContext) {
-	throw redirect(
-		`https://manage.inlang.com/install${pageContext.urlParsed.searchOriginal}`
-	);
-}
