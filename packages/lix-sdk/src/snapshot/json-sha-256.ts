9df5c18607311d1d85e69e0baa26fd6304e3a924
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2025-05-08 18:42:17 -0400

    add test cases

--- packages/lix-sdk/src/snapshot/json-sha-256.ts
+++ packages/lix-sdk/src/snapshot/json-sha-256.ts
@@ -1,4 +1,5 @@
 import { sha256 } from "js-sha256";
+import type { JSONType } from "../change/schema.js";
 
 /**
  * Creates a sha256 hash from a json object.
@@ -6,26 +7,24 @@ import { sha256 } from "js-sha256";
  * Sorts the object properties to increase consistency,
  * and de-duplication of snapshots.
  */
-export function jsonSha256(content: Record<string, unknown>): string {
+export function jsonSha256(content: JSONType): string {
 	const sortedContent = deepSortObject(content);
 
 	return sha256(JSON.stringify(sortedContent));
 }
 
 // naive deep sorting to receive consistent hash
-function deepSortObject(obj: Record<string, unknown>): any {
+function deepSortObject(obj: JSONType): any {
 	if (Array.isArray(obj)) {
 		return obj.map((item) =>
-			typeof item === "object" && item !== null
-				? deepSortObject(item as Record<string, unknown>)
-				: item
+			typeof item === "object" && item !== null ? deepSortObject(item) : item
 		);
 	} else if (typeof obj === "object" && obj !== null) {
 		return Object.keys(obj)
 			.sort()
 			.reduce(
 				(acc, key) => {
-					acc[key] = deepSortObject(obj[key] as Record<string, unknown>);
+					acc[key] = deepSortObject(obj[key] as JSONType);
 					return acc;
 				},
 				{} as Record<string, unknown>
