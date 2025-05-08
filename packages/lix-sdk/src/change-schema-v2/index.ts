1caa4638768dc236ec113fbfef438401c3cd218a
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2025-05-08 16:38:44 -0400

    export types

--- packages/lix-sdk/src/change-schema-v2/index.ts
+++ packages/lix-sdk/src/change-schema-v2/index.ts
@@ -1,11 +1,5 @@
-export const VersionViewJsonSchema = {
-	$schema: "http://json-schema.org/draft-07/schema#",
-	version: "1.0",
-	type: "object",
-	properties: {
-		id: { type: "string" },
-		name: { type: "string" },
-		change_set_id: { type: "string" },
-	},
-	required: ["id", "name", "change_set_id"],
-} as const;
+export {
+	ChangeSchemaJsonSchema,
+	type ChangeSchema,
+	type FromChangeSchema,
+} from "./schema.js";
