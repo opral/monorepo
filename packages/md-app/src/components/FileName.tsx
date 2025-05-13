7ac8c5182208ad1f712c60d18e6097087acc96e9
NiklasBuchfink <niklas.buchfink@web.de>
2025-05-13 17:16:41 +0200

    chore: fileName should not be the h1, only the h1 inside the document

--- packages/md-app/src/components/FileName.tsx
+++ packages/md-app/src/components/FileName.tsx
@@ -119,7 +119,7 @@ export default function FileName() {
             className="flex items-center h-8 px-1.5 hover:bg-slate-100 rounded transition-colors cursor-pointer -my-0.5"
           onClick={() => setIsEditing(true)}
         >
-            <h1 className="font-medium text-slate-700">{fileName}</h1>
+            <span className="font-medium text-slate-700">{fileName}</span>
         </div>
       )}
     </div>
