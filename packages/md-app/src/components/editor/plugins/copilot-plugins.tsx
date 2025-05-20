c1b416e92b66c4e1fdc088e823b01a0925b0138f
NiklasBuchfink <niklas.buchfink@web.de>
2025-05-20 13:40:16 +0200

    improve autocomplete with full document as context

--- packages/md-app/src/components/editor/plugins/copilot-plugins.tsx
+++ packages/md-app/src/components/editor/plugins/copilot-plugins.tsx
@@ -4,7 +4,7 @@ import type { TElement } from '@udecode/plate';
 
 import { faker } from '@faker-js/faker';
 import { CopilotPlugin } from '@udecode/plate-ai/react';
-import { serializeMd, stripMarkdown } from '@udecode/plate-markdown';
+import { stripMarkdown } from '@udecode/plate-markdown';
 
 import { GhostText } from '@/components/plate-ui/ghost-text';
 import { ExtendedMarkdownPlugin } from './markdown/markdown-plugin';
@@ -51,11 +51,17 @@ export const copilotPlugins = [
 
         if (!contextEntry) return '';
 
-        const prompt = serializeMd(editor, {
+        const document = editor.getApi(ExtendedMarkdownPlugin).markdown.serialize()
+
+        const prompt = editor.getApi(ExtendedMarkdownPlugin).markdown.serialize({
           value: [contextEntry[0] as TElement],
         });
 
-        return `Continue the text up to the next punctuation mark:
+        return `
+Full document context:
+  ${document}
+END OF DOCUMENT
+Continue the text up to the next punctuation mark:
   """
   ${prompt}
   """`;
