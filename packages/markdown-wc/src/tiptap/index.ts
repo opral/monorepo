2e1e4240d6f7922cd25684441c6d647dddff6caf
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2025-08-26 17:06:24 -0700

    export tiptap stuff

---
+++ packages/markdown-wc/src/tiptap/index.ts
@@ -0,0 +1,4 @@
+export { markdownWcExtensions } from "./markdown-wc.js"
+export { markdownWcAstToTiptap } from "./mdwc-to-tiptap.js"
+export { tiptapDocToMarkdownWcAst } from "./tiptap-to-mdwc.js"
+export type { PMNode, PMMark } from "./mdwc-to-tiptap.js"
