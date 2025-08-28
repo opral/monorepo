253fe80aee0bc52ae59ac31af42c7c4527b78244
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2025-08-28 11:28:55 -0700

    add tailwind css

--- packages/flashtype/src/main.tsx
+++ packages/flashtype/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider, createRouter } from '@tanstack/react-router';
 import { routeTree } from './routeTree.gen';
+import './index.css';
 
 const router = createRouter({
   routeTree,
