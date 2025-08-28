dfd5e71fdadc3908629fbe164590300edc948401
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2025-08-28 16:52:47 -0700

    better padding

--- packages/flashtype/src/components/ui/sidebar.tsx
+++ packages/flashtype/src/components/ui/sidebar.tsx
@@ -305,10 +305,7 @@ function SidebarGroup({ className, ...props }: React.ComponentProps<"div">) {
 		<div
 			data-slot="sidebar-group"
 			data-sidebar="group"
-			className={cn(
-				"relative flex w-full min-w-0 flex-col p-2 pt-0",
-				className,
-			)}
+			className={cn("relative flex w-full min-w-0 flex-col px-2", className)}
 			{...props}
 		/>
 	);
