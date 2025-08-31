2da617fd617a8aae071d10bb6c530b7000fdde50
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2025-08-31 16:48:28 -0700

    format

--- packages/markdown-wc/src/markdown-wc-mermaid.ts
+++ packages/markdown-wc/src/markdown-wc-mermaid.ts
@@ -1,44 +1,46 @@
-import { LitElement, html, css } from 'lit';
-import mermaid from 'mermaid';
+import { LitElement, html, css } from "lit"
+import mermaid from "mermaid"
 
 export class Element extends LitElement {
-  static override styles = css`
-    :host {
-      display: block;
-    }
-  `;
+	static override styles = css`
+		:host {
+			display: block;
+		}
+	`
 
-  /**
-   * Render the mermaid graph as SVG.
-   * @private
-   */
-  renderGraph() {
-    const content = this.textContent?.trim() || '';
-    if (content) {
-      mermaid.render('graph', content).then((result) => {
-        this.shadowRoot!.innerHTML = result.svg;
-      }).catch((error) => {
-        this.shadowRoot!.innerHTML = `<pre>${error}</pre>`
-      });
-    } else {
-      this.shadowRoot!.innerHTML = '';
-    }
-  }
+	/**
+	 * Render the mermaid graph as SVG.
+	 * @private
+	 */
+	renderGraph() {
+		const content = this.textContent?.trim() || ""
+		if (content) {
+			mermaid
+				.render("graph", content)
+				.then((result) => {
+					this.shadowRoot!.innerHTML = result.svg
+				})
+				.catch((error) => {
+					this.shadowRoot!.innerHTML = `<pre>${error}</pre>`
+				})
+		} else {
+			this.shadowRoot!.innerHTML = ""
+		}
+	}
 
-  override connectedCallback() {
-    super.connectedCallback();
-    this.renderGraph();
-  }
+	override connectedCallback() {
+		super.connectedCallback()
+		this.renderGraph()
+	}
 
-  override updated() {
-    this.renderGraph();
-  }
+	override updated() {
+		this.renderGraph()
+	}
 
-  override render() {
-    return html`<slot></slot>`;
-  }
+	override render() {
+		return html`<slot></slot>`
+	}
 }
-
 
 if (typeof customElements !== "undefined" && !customElements.get("markdown-wc-mermaid")) {
 	customElements.define("markdown-wc-mermaid", Element)
