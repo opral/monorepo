debf63806b52b1b04a7a73d5affbb4bff0fcaae2
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2025-09-11 11:14:22 -0700

    enable diff plugin again

--- packages/lix/plugin-md/src/index.ts
+++ packages/lix/plugin-md/src/index.ts
@@ -1,13 +1,12 @@
 import { type LixPlugin } from "@lix-js/sdk";
 import { applyChanges } from "./apply-changes.js";
 import { detectChanges } from "./detect-changes.js";
-// import { DiffComponent } from "./diff.js";
+import { DiffComponent } from "./diff.js";
 
 export const plugin: LixPlugin = {
 	key: "plugin_md",
 	detectChangesGlob: "*.md",
 	detectChanges,
-	// @ts-expect-error
-	diffUiComponent: () => import("./diff.js").then((mod) => mod.DiffComponent),
+	diffUiComponent: DiffComponent,
 	applyChanges,
 };
