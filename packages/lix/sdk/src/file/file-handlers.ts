9ab5c837155c0060670fa7b1134fa6e1a427f649
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2025-09-15 16:14:58 -0700

    fix: implement file.metadata handler

--- packages/lix/sdk/src/file/file-handlers.ts
+++ packages/lix/sdk/src/file/file-handlers.ts
@@ -65,6 +65,7 @@ export function handleFileInsert(args: {
 			},
 			schema_version: LixFileDescriptorSchema["x-lix-version"],
 			version_id: args.versionId,
+			metadata: args.file.metadata ?? null,
 			untracked: args.untracked || false,
 		}),
 	});
@@ -249,6 +250,7 @@ export function handleFileUpdate(args: {
 					metadata: args.file.metadata || null,
 					hidden: args.file.hidden ?? false,
 				},
+				metadata: args.file.metadata ?? null,
 				untracked: args.untracked || false,
 			})
 			.where("entity_id", "=", args.file.id)
