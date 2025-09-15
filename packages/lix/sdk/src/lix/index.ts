83a1d8ec8f14b692b20a555480a3c8329114e7aa
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2025-09-15 09:21:36 -0700

    remove old api

--- packages/lix/sdk/src/lix/index.ts
+++ packages/lix/sdk/src/lix/index.ts
@@ -1,4 +1,2 @@
 export { type Lix, openLix } from "./open-lix.js";
 export { newLixFile } from "./new-lix.js";
-export { OpfsSahEnvironment as OpfsSahBackend } from "../environment/opfs-sah.js";
-export { InMemoryEnvironment as InMemoryBackend } from "../environment/in-memory.js";
