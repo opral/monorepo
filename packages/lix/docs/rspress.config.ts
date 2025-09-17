81ba05fb5626a5bd04ee4540cfc4b0df2629d1fc
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2025-09-17 16:53:06 -0700

    Update

--- packages/lix/docs/rspress.config.ts
+++ packages/lix/docs/rspress.config.ts
@@ -67,7 +67,6 @@ export default defineConfig({
     darkMode: false,
     nav: [
       { text: "Guide", link: "/guide/getting-started" },
-      { text: "Plugins", link: "/plugins/" },
       { text: "Reference", link: "/api/" },
     ],
     sidebar: {
