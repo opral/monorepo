36a018384795d7b99c55d380fe6ebbeebbc02ca7
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2025-09-17 18:06:43 -0700

    remove sqlite-wasm-kysely package

--- inlang/packages/sdk/src/project/loadProjectInMemory.ts
+++ inlang/packages/sdk/src/project/loadProjectInMemory.ts
@@ -1,5 +1,4 @@
 import { openLix, type LixKeyValue, type NewState } from "@lix-js/sdk";
-import { createInMemoryDatabase } from "sqlite-wasm-kysely";
 import { loadProject } from "./loadProject.js";
 
 /**
@@ -9,20 +8,16 @@ export async function loadProjectInMemory(
 	args: {
 		blob: Blob;
 		lixKeyValues?: NewState<LixKeyValue>[];
-	} & Omit<Parameters<typeof loadProject>[0], "sqlite" | "lix">
+	} & Omit<Parameters<typeof loadProject>[0], "lix">
 ) {
 	const lix = await openLix({
 		blob: args.blob,
 		account: args.account,
 		keyValues: args.lixKeyValues,
 	});
-
-	const sqlite = await createInMemoryDatabase({});
-
 	return await loadProject({
 		// pass common arguments to loadProject
 		...args,
-		sqlite,
 		lix,
 	});
 }
