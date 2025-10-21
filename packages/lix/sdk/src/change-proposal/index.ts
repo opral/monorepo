68707b709ed9155638497e4a6ea6ff4f8e62e700
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2025-10-21 14:36:08 -0700

    export change proposal type

--- packages/lix/sdk/src/change-proposal/index.ts
+++ packages/lix/sdk/src/change-proposal/index.ts
@@ -1,3 +1,7 @@
 export { createChangeProposal } from "./create-change-proposal.js";
 export { acceptChangeProposal } from "./accept-change-proposal.js";
 export { rejectChangeProposal } from "./reject-change-proposal.js";
+export {
+	type LixChangeProposal,
+	LixChangeProposalSchema,
+} from "./schema-definition.js";
