af393fb5f772436fe1a0cbac3105f84603f4d368
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2025-11-05 16:28:43 -0500

    remove old preprocessor iterations

--- packages/lix/sdk/src/engine/query-preprocessor/steps/index.ts
+++
@@ -1,3 +0,0 @@
-export { entityViewStep } from "./entity-view.js";
-export { triggerStep } from "./triggers.js";
-export { stateAccessStep } from "./state-access.js";
--- packages/lix/sdk/src/engine/query-preprocessor/steps/types.ts
+++
@@ -1,6 +0,0 @@
-import type { QueryPreprocessorResult } from "../types.js";
-import type { PreprocessContext } from "../context.js";
-
-export type PreprocessorStep = (
-	context: PreprocessContext
-) => QueryPreprocessorResult | null;
