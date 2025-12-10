bf4b7907f36b4ca1755ee90347ba59e301f5d32c
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2025-12-09 18:54:16 -0800

    add landing page

--- packages/lix/website/src/routes/__root.tsx
+++ packages/lix/website/src/routes/__root.tsx
@@ -12,7 +12,7 @@ export const Route = createRootRoute({
         content: "width=device-width, initial-scale=1",
       },
       {
-        title: "TanStack Start Starter",
+        title: "lix | Documentation",
       },
     ],
     links: [
