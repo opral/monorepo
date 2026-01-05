eff3bc65a8b558d6407cb15436a2f7fff40519f4
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2026-01-05 10:49:46 -0500

    add favicon

--- packages/website-v2/src/routes/__root.tsx
+++ packages/website-v2/src/routes/__root.tsx
@@ -16,6 +16,10 @@ export const Route = createRootRoute({
       {
         title: 'inlang',
       },
+      {
+        name: 'theme-color',
+        content: '#ffffff',
+      },
     ],
     links: [
       { rel: 'preconnect', href: 'https://rsms.me/' },
@@ -23,6 +27,29 @@ export const Route = createRootRoute({
       {
         rel: 'stylesheet',
         href: appCss,
+      },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        href: '/favicon/apple-touch-icon.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        href: '/favicon/favicon-32x32.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        href: '/favicon/favicon-16x16.png',
+      },
+      { rel: 'manifest', href: '/favicon/site.webmanifest' },
+      {
+        rel: 'mask-icon',
+        href: '/favicon/safari-pinned-tab.svg',
+        color: '#5bbad5',
       },
     ],
   }),
